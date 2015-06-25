f5c8ec852dec7759b16ea2773409bdcd101ac42a
Mihai Varga <mihai.varga@collabora.com>
2015-06-25 15:50:53 +0300

    Fixed eslint errors in Control.EditView.js

--- loleaflet/src/control/Control.EditView.js
+++ loleaflet/src/control/Control.EditView.js
@@ -4,31 +4,31 @@
 
 L.Control.EditViewSwitch = L.Control.extend({
 	options: {
-		position: 'topleft',
+		position: 'topleft'
 	},
 
-	onAdd: function (map) {
+	onAdd: function () {
 		var partName = 'leaflet-control-editviewswitch',
-		    container = L.DomUtil.create('label', partName + ' leaflet-bar');
+			container = L.DomUtil.create('label', partName + ' leaflet-bar');
 
-        this._checkBox = L.DomUtil.create('input', 'editview-cb', container);
-        this._checkBox.type = 'checkbox';
-        L.DomEvent.on(this._checkBox, 'change', this._onChange, this);
+		this._checkBox = L.DomUtil.create('input', 'editview-cb', container);
+		this._checkBox.type = 'checkbox';
+		L.DomEvent.on(this._checkBox, 'change', this._onChange, this);
 		container.appendChild(document.createTextNode('View only'));
 		return container;
 	},
 
-    _onChange: function() {
-        var className = 'leaflet-viewmode';
-        if (this._checkBox.checked) {
-            this._map.fire('viewmode');
-            L.DomUtil.addClass(this._map._container, className);
-        }
-        else {
-            this._map.fire('editmode');
-            L.DomUtil.removeClass(this._map._container, className);
-        }
-    }
+	_onChange: function() {
+		var className = 'leaflet-viewmode';
+		if (this._checkBox.checked) {
+			this._map.fire('viewmode');
+			L.DomUtil.addClass(this._map._container, className);
+		}
+		else {
+			this._map.fire('editmode');
+			L.DomUtil.removeClass(this._map._container, className);
+		}
+	}
 });
 
 L.Map.mergeOptions({
