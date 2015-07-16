1c16fd89f3021e5e7ad2f8f5861b920325cc890f
Mihai Varga <mihai.varga@collabora.com>
2015-07-16 12:50:08 +0300

    loleaflet: saveAs API

--- loleaflet/src/control/Buttons.js
+++ loleaflet/src/control/Buttons.js
@@ -6,5 +6,18 @@ L.Map.include({
 		if (this._docLayer._permission === 'edit') {
 			this.socket.send('uno .uno:' + unoState);
 		}
+	},
+
+	saveAs: function (url, format, options) {
+		if (format === undefined || format === null) {
+			format = '';
+		}
+		if (options === undefined || options === null) {
+			options = '';
+		}
+		this.socket.send('saveas ' +
+			'url=' + url + ' ' +
+			'format=' + format + ' ' +
+			'options=' + options);
 	}
 });
