287ad122d70e0d20ae0e7bfa132805a65c0031b0
Mihai Varga <mihai.varga@collabora.com>
2015-07-22 10:48:31 +0300

    loleaflet: fixed paragraph alignment commands

--- loleaflet/src/control/Control.Buttons.js
+++ loleaflet/src/control/Control.Buttons.js
@@ -12,16 +12,16 @@ L.Control.Buttons = L.Control.extend({
 		    container = L.DomUtil.create('div', buttonsName + '-container' + ' leaflet-bar');
 
 		this._buttons = {
-			'bold':          {title: 'Bold',               uno: 'Bold',                        iconName: 'bold.png'},
-			'italic':        {title: 'Italic',             uno: 'Italic',                      iconName: 'italic.png'},
-			'underline':     {title: 'Underline',          uno: 'Underline',                   iconName: 'underline.png'},
-			'strikethrough': {title: 'Strike-through',     uno: 'Strikeout',                   iconName: 'strikethrough.png'},
-			'alignleft':     {title: 'Align left',         uno: 'CommonAlignLeft',             iconName: 'alignleft.png'},
-			'aligncenter':   {title: 'Center horizontaly', uno: 'CommonAlignHorizontalCenter', iconName: 'aligncenter.png'},
-			'alignright':    {title: 'Align right',        uno: 'CommonAlignRight',            iconName: 'alignright.png'},
-			'alignblock':    {title: 'Justified',          uno: 'CommonAlignJustified',        iconName: 'alignblock.png'},
-			'save':          {title: 'Save',               uno: 'Save',                        iconName: 'save.png'},
-			'saveas':        {title: 'Save As',            uno: '',                            iconName: 'saveas.png'}
+			'bold':          {title: 'Bold',               uno: 'Bold',        iconName: 'bold.png'},
+			'italic':        {title: 'Italic',             uno: 'Italic',      iconName: 'italic.png'},
+			'underline':     {title: 'Underline',          uno: 'Underline',   iconName: 'underline.png'},
+			'strikethrough': {title: 'Strike-through',     uno: 'Strikeout',   iconName: 'strikethrough.png'},
+			'alignleft':     {title: 'Align left',         uno: 'LeftPara',    iconName: 'alignleft.png'},
+			'aligncenter':   {title: 'Center horizontaly', uno: 'CenterPara',  iconName: 'aligncenter.png'},
+			'alignright':    {title: 'Align right',        uno: 'RightPara',   iconName: 'alignright.png'},
+			'alignblock':    {title: 'Justified',          uno: 'JustifyPara', iconName: 'alignblock.png'},
+			'save':          {title: 'Save',               uno: 'Save',        iconName: 'save.png'},
+			'saveas':        {title: 'Save As',            uno: '',            iconName: 'saveas.png'}
 		};
 		for (var key in this._buttons) {
 			var button = this._buttons[key];
