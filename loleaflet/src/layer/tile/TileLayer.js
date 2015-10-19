05e81a0edb14c0d278b467d0dd74a09148a166ec
Mihai Varga <mihai.varga@collabora.com>
2015-10-19 11:24:30 +0300

    loleaflet: allow the disabling of the print handler

--- loleaflet/src/layer/tile/TileLayer.js
+++ loleaflet/src/layer/tile/TileLayer.js
@@ -278,7 +278,7 @@ L.TileLayer = L.GridLayer.extend({
 
 		if (command.id === 'print') {
 			var isFirefox = typeof InstallTrigger !== 'undefined' || navigator.userAgent.search('Firefox') >= 0;
-			if (isFirefox) {
+			if (isFirefox || this._map.options.print === false) {
 				// the print dialog doesn't work well on firefox
 				this._map.fire('print', {url: url});
 			}
