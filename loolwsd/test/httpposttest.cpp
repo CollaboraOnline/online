3ed78cce6fa1292e4bb5cfbe872f3dd2e6cffc0e
Miklos Vajna <vmiklos@collabora.co.uk>
2015-10-21 13:44:35 +0200

    loolwsd: add HTTP POST convert-to testcase
    Fails with 8f90c38844ba6840acd97c6a72eb104d1171dc8d (loolwsd convert-to:
    implement actual conversion, 2015-10-20) reverted.

--- loolwsd/test/httpposttest.cpp
+++ loolwsd/test/httpposttest.cpp
@@ -7,8 +7,18 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+#include <Poco/Net/FilePartSource.h>
+#include <Poco/Net/HTMLForm.h>
+#include <Poco/Net/HTTPClientSession.h>
+#include <Poco/Net/HTTPRequest.h>
+#include <Poco/Net/HTTPResponse.h>
+#include <Poco/StreamCopier.h>
+#include <Poco/URI.h>
 #include <cppunit/extensions/HelperMacros.h>
 
+#include <LOOLWSD.hpp>
+
+/// Tests the HTTP POST API of loolwsd. The server has to be started manually before running this test.
 class HTTPPostTest : public CPPUNIT_NS::TestFixture
 {
     CPPUNIT_TEST_SUITE(HTTPPostTest);
@@ -20,6 +30,28 @@ class HTTPPostTest : public CPPUNIT_NS::TestFixture
 
 void HTTPPostTest::testConvertTo()
 {
+    Poco::URI uri("http://127.0.0.1:" + std::to_string(LOOLWSD::DEFAULT_CLIENT_PORT_NUMBER) + "/convert-to");
+    Poco::Net::HTTPClientSession session(uri.getHost(), uri.getPort());
+
+    Poco::Net::HTTPRequest request(Poco::Net::HTTPRequest::HTTP_POST, "/convert-to");
+    Poco::Net::HTMLForm form;
+    form.setEncoding(Poco::Net::HTMLForm::ENCODING_MULTIPART);
+    form.set("format", "txt");
+    form.addPart("data", new Poco::Net::FilePartSource(TDOC "/hello.odt"));
+    form.prepareSubmit(request);
+    // If this results in a Poco::Net::ConnectionRefusedException, loolwsd is not running.
+    form.write(session.sendRequest(request));
+
+    Poco::Net::HTTPResponse response;
+    std::stringstream actualStream;
+    // receiveResponse() resulted in a Poco::Net::NoMessageException.
+    std::istream& responseStream = session.receiveResponse(response);
+    Poco::StreamCopier::copyStream(responseStream, actualStream);
+
+    std::ifstream fileStream(TDOC "/hello.txt");
+    std::stringstream expectedStream;
+    expectedStream << fileStream.rdbuf();
+    CPPUNIT_ASSERT_EQUAL(expectedStream.str(), actualStream.str());
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(HTTPPostTest);
