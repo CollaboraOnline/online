CollaboraOnline/online

commit cc24463f08239b1591ed892ed9655126b0de64dd

loolwsd: wrap lok::Document::paste()

loolwsd/LOOLSession.cpp
@@ -321,6 +321,7 @@ bool MasterProcessSession::handleInput(const char *buffer, int length)
              tokens[0] != "downloadas" &&
              tokens[0] != "getchildid" &&
              tokens[0] != "gettextselection" &&
+             tokens[0] != "paste" &&
              tokens[0] != "insertfile" &&
              tokens[0] != "invalidatetiles" &&
              tokens[0] != "key" &&
@@ -739,6 +740,7 @@ bool ChildProcessSession::handleInput(const char *buffer, int length)
         assert(tokens[0] == "downloadas" ||
                tokens[0] == "getchildid" ||
                tokens[0] == "gettextselection" ||
+               tokens[0] == "paste" ||
                tokens[0] == "insertfile" ||
                tokens[0] == "key" ||
                tokens[0] == "mouse" ||
@@ -763,6 +765,10 @@ bool ChildProcessSession::handleInput(const char *buffer, int length)
         else if (tokens[0] == "gettextselection")
         {
             return getTextSelection(buffer, length, tokens);
+        }
+        else if (tokens[0] == "paste")
+        {
+            return paste(buffer, length, tokens);
         }
         else if (tokens[0] == "insertfile")
         {
@@ -1104,6 +1110,24 @@ bool ChildProcessSession::getTextSelection(const char* /*buffer*/, int /*length*
     char *textSelection = _loKitDocument->pClass->getTextSelection(_loKitDocument, mimeType.c_str(), NULL);
 
     sendTextFrame("textselectioncontent: " + std::string(textSelection));
+    return true;
+}
+
+bool ChildProcessSession::paste(const char* /*buffer*/, int /*length*/, StringTokenizer& tokens)
+{
+    std::string mimeType;
+    std::string data;
+
+    if (tokens.count() < 3 || !getTokenString(tokens[1], "mimetype", mimeType) || !getTokenString(tokens[2], "data", data))
+    {
+        sendTextFrame("error: cmd=paste kind=syntax");
+        return false;
+    }
+
+    data = Poco::cat(std::string(" "), tokens.begin() + 2, tokens.end()).substr(strlen("data="));
+
+    _loKitDocument->pClass->paste(_loKitDocument, mimeType.c_str(), data.c_str(), std::strlen(data.c_str()));
+
     return true;
 }
 

loolwsd/LOOLSession.hpp
@@ -187,6 +187,7 @@ public:
     bool downloadAs(const char *buffer, int length, Poco::StringTokenizer& tokens);
     bool getChildId();
     bool getTextSelection(const char *buffer, int length, Poco::StringTokenizer& tokens);
+    bool paste(const char *buffer, int length, Poco::StringTokenizer& tokens);
     bool insertFile(const char *buffer, int length, Poco::StringTokenizer& tokens);
     bool keyEvent(const char *buffer, int length, Poco::StringTokenizer& tokens);
     bool mouseEvent(const char *buffer, int length, Poco::StringTokenizer& tokens);

commit 3d613a443f05c1d572e19afa0ebbca5b99927df8

loolwsd: add paste testcase
Fails with commit cc24463f08239b1591ed892ed9655126b0de64dd (loolwsd:
wrap lok::Document::paste(), 2015-10-27) reverted.

loolwsd/test/Makefile.am
@@ -6,7 +6,7 @@ test_CPPFLAGS = -DTDOC=\"$(top_srcdir)/test/data\"
 
 test_LDADD = $(CPPUNIT_LIBS)
 
-test_SOURCES = httpposttest.cpp test.cpp
+test_SOURCES = httpposttest.cpp httpwstest.cpp test.cpp ../LOOLProtocol.cpp
 
 EXTRA_DIST = data/hello.odt data/hello.txt $(test_SOURCES)
 

loolwsd/test/httpwstest.cpp
@@ -1,0 +1,82 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <Poco/Net/HTTPClientSession.h>
+#include <Poco/Net/HTTPRequest.h>
+#include <Poco/Net/HTTPResponse.h>
+#include <Poco/Net/WebSocket.h>
+#include <Poco/URI.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <LOOLProtocol.hpp>
+#include <LOOLWSD.hpp>
+
+/// Tests the HTTP WebSocket API of loolwsd. The server has to be started manually before running this test.
+class HTTPWSTest : public CPPUNIT_NS::TestFixture
+{
+    CPPUNIT_TEST_SUITE(HTTPWSTest);
+    CPPUNIT_TEST(testPaste);
+    CPPUNIT_TEST_SUITE_END();
+
+    void testPaste();
+
+    void sendTextFrame(Poco::Net::WebSocket& socket, const std::string& string);
+};
+
+void HTTPWSTest::testPaste()
+{
+    // Load a document and make it empty.
+    Poco::URI uri("http://127.0.0.1:" + std::to_string(LOOLWSD::DEFAULT_CLIENT_PORT_NUMBER));
+    Poco::Net::HTTPClientSession session(uri.getHost(), uri.getPort());
+    Poco::Net::HTTPRequest request(Poco::Net::HTTPRequest::HTTP_POST, "/ws");
+    Poco::Net::HTTPResponse response;
+    Poco::Net::WebSocket socket(session, request, response);
+    std::string documentPath = TDOC "/hello.odt";
+    std::string documentURL = "file://" + Poco::Path(documentPath).makeAbsolute().toString();
+    sendTextFrame(socket, "load url=" + documentURL);
+    sendTextFrame(socket, "uno .uno:SelectAll");
+    sendTextFrame(socket, "uno .uno:Delete");
+
+    // Paste some text into it.
+    sendTextFrame(socket, "paste mimetype=text/plain;charset=utf-8 data=aaa bbb ccc");
+
+    // Check if the document contains the pasted text.
+    sendTextFrame(socket, "uno .uno:SelectAll");
+    sendTextFrame(socket, "gettextselection mimetype=text/plain;charset=utf-8");
+    std::string selection;
+    int flags;
+    int n;
+    do
+    {
+        char buffer[100000];
+        n = socket.receiveFrame(buffer, sizeof(buffer), flags);
+        if (n > 0 && (flags & Poco::Net::WebSocket::FRAME_OP_BITMASK) != Poco::Net::WebSocket::FRAME_OP_CLOSE)
+        {
+            std::string response = LOOLProtocol::getFirstLine(buffer, n);
+            std::string prefix = "textselectioncontent: ";
+            if (response.find(prefix) == 0)
+            {
+                selection = response.substr(prefix.length());
+                break;
+            }
+        }
+    }
+    while (n > 0 && (flags & Poco::Net::WebSocket::FRAME_OP_BITMASK) != Poco::Net::WebSocket::FRAME_OP_CLOSE);
+    socket.shutdown();
+    CPPUNIT_ASSERT_EQUAL(std::string("aaa bbb ccc"), selection);
+}
+
+void HTTPWSTest::sendTextFrame(Poco::Net::WebSocket& socket, const std::string& string)
+{
+    socket.sendFrame(string.data(), string.size());
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(HTTPWSTest);
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */