449022730bdfe29e7f964cb2cb0e3e05741185d4
Ozcan Esen <ozcan.esen@collabora.com>
2015-12-12 18:27:46 +0200

    change the border style of the selected preview
    Change-Id: I0d163c0a68dbc532db08ed8735a4cdb7107ec8e5

--- loleaflet/src/control/Control.PartsPreview.js
+++ loleaflet/src/control/Control.PartsPreview.js
@@ -22,34 +22,52 @@ L.Control.PartsPreview = L.Control.extend({
 
 	_updateDisabled: function (e) {
 		var parts = e.parts;
+		var selectedPart = e.selectedPart;
 		var docType = e.docType;
 		if (docType === 'text') {
 			return;
 		}
 
-		if (!this._previewInitialized && docType === 'presentation') {
-			// make room for the preview
-			var docContainer = this._map.options.documentContainer;
-			L.DomUtil.addClass(docContainer, 'parts-preview-document');
-			setTimeout(L.bind(function () {
-				this._map.invalidateSize();
-				$('.scroll-container').mCustomScrollbar('update');
-			}, this), 500);
-			for (var i = 0; i < parts; i++) {
-				var id = 'preview-tile' + i;
-				var frame = L.DomUtil.create('div', 'preview-frame', this._partsPreviewCont);
-				L.DomUtil.create('span', 'preview-helper', frame);
-				var img = L.DomUtil.create('img', 'preview-img', frame);
-				img.id = id;
-				this._previewTiles[id] = img;
-				L.DomEvent
-					.on(img, 'click', L.DomEvent.stopPropagation)
-					.on(img, 'click', L.DomEvent.stop)
-					.on(img, 'click', this._setPart, this)
-					.on(img, 'click', this._refocusOnMap, this);
-				this._map.getPreview(i, i, 180, 180, {autoUpdate: this.options.autoUpdate});
+		if (docType === 'presentation') {
+			if (!this._previewInitialized)
+			{
+				// make room for the preview
+				var docContainer = this._map.options.documentContainer;
+				L.DomUtil.addClass(docContainer, 'parts-preview-document');
+				setTimeout(L.bind(function () {
+					this._map.invalidateSize();
+					$('.scroll-container').mCustomScrollbar('update');
+				}, this), 500);
+				for (var i = 0; i < parts; i++) {
+					var id = 'preview-tile' + i;
+					var frame = L.DomUtil.create('div', 'preview-frame', this._partsPreviewCont);
+					L.DomUtil.create('span', 'preview-helper', frame);
+					var imgClassName = 'preview-img';
+					if (i == 0) {
+						imgClassName += ' preview-img-selected';
+					}
+					var img = L.DomUtil.create('img', imgClassName, frame);
+					img.id = id;
+					this._previewTiles[id] = img;
+					L.DomEvent
+						.on(img, 'click', L.DomEvent.stopPropagation)
+						.on(img, 'click', L.DomEvent.stop)
+						.on(img, 'click', this._setPart, this)
+						.on(img, 'click', this._refocusOnMap, this);
+					this._map.getPreview(i, i, 180, 180, {autoUpdate: this.options.autoUpdate});
+				}
+				this._previewInitialized = true;
 			}
-			this._previewInitialized = true;
+			else
+			{
+				// change the border style of the selected preview.
+				for (var i = 0; i < parts; i++) {
+					var img = L.DomUtil.get('preview-tile' + i);
+					L.DomUtil.removeClass(img, 'preview-img-selected');
+				}
+				var img = L.DomUtil.get('preview-tile' + selectedPart);
+				L.DomUtil.addClass(img, 'preview-img-selected');
+			}
 		}
 	},
 
