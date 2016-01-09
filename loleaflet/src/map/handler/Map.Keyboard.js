6b9e0368ae084bd15d231f55eae88abb0f4e9d2c
Jan Holesovsky <kendy@collabora.com>
2016-01-09 23:45:09 +0100

    loleaflet: Improve page up / page down handling when viewing only.
    Without this, the first PgDown is ignored.
    
    Not sure what the previous code was supposed to handle, but dealing with that
    in the LO core seems to me as a better plan, so let's fix it there if
    something special is still needed for the use case introduced in
    97ac6736f80da773f5e23a6688acc0dc47535d01.

--- loleaflet/src/map/handler/Map.Keyboard.js
+++ loleaflet/src/map/handler/Map.Keyboard.js
@@ -153,7 +153,6 @@ L.Map.Keyboard = L.Handler.extend({
 		this._setPanOffset(map.options.keyboardPanOffset);
 		this._setZoomOffset(map.options.keyboardZoomOffset);
 		this.modifier = 0;
-		this.dopagejump = 0;
 	},
 
 	addHooks: function () {
@@ -234,21 +233,6 @@ L.Map.Keyboard = L.Handler.extend({
 			}
 		}
 
-		// page up or page down, handled by this.dopagejump
-		// to jump back to the anchor from footnote/endnote by PgUp
-		// or jump back to the main text from header/footer by PgUp or PgDown
-		if (!this.modifier && (e.originalEvent.keyCode === 33 || e.originalEvent.keyCode === 34)) {
-			if (this.dopagejump === 1) {
-				return;
-			}
-			if (e.type === 'keyup') {
-				this.dopagejump = 1;
-			}
-		}
-		else if (e.type === 'keyup') {
-			this.dopagejump = 0;
-		}
-
 		var charCode = e.originalEvent.charCode;
 		var keyCode = e.originalEvent.keyCode;
 		var unoKeyCode = this._toUNOKeyCode(keyCode);
@@ -284,6 +268,10 @@ L.Map.Keyboard = L.Handler.extend({
 				// tab would change focus to other DOM elements
 				e.originalEvent.preventDefault();
 			}
+		}
+		else if (!this.modifier && (e.originalEvent.keyCode === 33 || e.originalEvent.keyCode === 34)) {
+			// let the scrollbar handle page up / page down when viewing
+			return;
 		}
 		else if (e.type === 'keydown') {
 			var key = e.originalEvent.keyCode;
