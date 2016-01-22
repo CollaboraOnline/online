1da4769924d465f9dbe3fea6be39f74156ed7555
Jan Holesovsky <kendy@collabora.com>
2016-01-22 16:26:46 +0100

    loleaflet: Implement rich copy / paste inside the document.

--- loleaflet/src/layer/tile/TileLayer.js
+++ loleaflet/src/layer/tile/TileLayer.js
@@ -951,6 +951,10 @@ L.TileLayer = L.GridLayer.extend({
 		e.preventDefault();
 		if (this._selectionTextContent) {
 			L.Compatibility.clipboardSet(e, this._selectionTextContent);
+
+			// remember the copied text, for rich copy/paste inside a document
+			this._selectionTextHash = this._selectionTextContent;
+			this._map._socket.sendMessage('uno .uno:Copy');
 		}
 	},
 
@@ -959,6 +963,9 @@ L.TileLayer = L.GridLayer.extend({
 		e.preventDefault();
 		if (this._selectionTextContent) {
 			L.Compatibility.clipboardSet(e, this._selectionTextContent);
+
+			// remember the copied text, for rich copy/paste inside a document
+			this._selectionTextHash = this._selectionTextContent;
 			this._map._socket.sendMessage('uno .uno:Cut');
 		}
 	},
@@ -968,7 +975,14 @@ L.TileLayer = L.GridLayer.extend({
 		e.preventDefault();
 		var pasteString = L.Compatibility.clipboardGet(e);
 		if (pasteString) {
-			this._map._socket.sendMessage('paste mimetype=text/plain;charset=utf-8\n' + pasteString);
+			if (pasteString === this._selectionTextHash) {
+				// content of the clipboard did not change, we can do rich
+				// paste
+				this._map._socket.sendMessage('uno .uno:Paste');
+			}
+			else {
+				this._map._socket.sendMessage('paste mimetype=text/plain;charset=utf-8\n' + pasteString);
+			}
 		}
 	},
 
