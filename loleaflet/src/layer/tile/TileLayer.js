28a77b3a9531f245bb865e442b6bc5f1309dd324
Jan Holesovsky <kendy@collabora.com>
2016-01-25 12:32:50 +0100

    loleaflet: Position the mouse with 'drop' always (even html snippet).

--- loleaflet/src/layer/tile/TileLayer.js
+++ loleaflet/src/layer/tile/TileLayer.js
@@ -1051,8 +1051,15 @@ L.TileLayer = L.GridLayer.extend({
 	},
 
 	_onDrop: function (e) {
-		// The original event doesn't contain the coordinates.
+		// Move the cursor, so that the insert position is as close to the drop coordinates as possible.
 		var latlng = e.latlng;
+		var docLayer = this._map._docLayer;
+		var mousePos = docLayer._latLngToTwips(latlng);
+		var count = 1;
+		var buttons = 1;
+		var modifier = this._map.keyboard.modifier;
+		this._postMouseEvent('buttondown', mousePos.x, mousePos.y, count, buttons, modifier);
+		this._postMouseEvent('buttonup', mousePos.x, mousePos.y, count, buttons, modifier);
 
 		e = e.originalEvent;
 		e.preventDefault();
@@ -1082,15 +1089,6 @@ L.TileLayer = L.GridLayer.extend({
 				for (var i = 0; i < files.length; ++i) {
 					var file = files[i];
 					if (file.type.match(/image.*/)) {
-						// Move the cursor, so that the insert position is as close to the drop coordinates as possible.
-						var docLayer = this._map._docLayer;
-						var mousePos = docLayer._latLngToTwips(latlng);
-						var count = 1;
-						var buttons = 1;
-						var modifier = this._map.keyboard.modifier;
-						this._postMouseEvent('buttondown', mousePos.x, mousePos.y, count, buttons, modifier);
-						this._postMouseEvent('buttonup', mousePos.x, mousePos.y, count, buttons, modifier);
-
 						var reader = new FileReader();
 						var socket = this._map._socket;
 						reader.onload = (function(aImg) {
