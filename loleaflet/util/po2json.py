5aa0156d5fba7ecf26c763ff77ad7eb70844b76f
Pranav Kant <pranavk@collabora.com>
2016-02-10 02:12:10 +0530

    loleaflet: Added l10n framework
    'make dist' generates localized .json files from .po files
    'make pot' generates PO templates for new translations
    'make l10n' updates existing translations with latest templates
    
    Also includes translation of styles for writer, impress and calc
    in all languages taken from LibreOffice translation submodule.
    
    (Cherry-picked partly from 40251a92eba2c48740668278ded3de0f8d41a5e5)
    
    Change-Id: I31c031f38c9da8eea9551f65c251480e7cb08865
    Reviewed-on: https://gerrit.libreoffice.org/22268
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    Tested-by: Andras Timar <andras.timar@collabora.com>

---
+++ loleaflet/util/po2json.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+#
+# convert .po to .json
+#
+
+import json
+import optparse
+import os
+import polib
+import re
+import string
+import sys
+
+parser = optparse.OptionParser(usage="usage: %prog [options] pofile...")
+parser.add_option("--quiet", action="store_false", default=True, dest="verbose", help="don't print status messages to stdout")
+
+(options, args) = parser.parse_args()
+
+if args == None or len(args) == 0:
+	print("ERROR: you must specify at least one po file to translate");
+	sys.exit(1)
+
+paramFix = re.compile("(\\(([0-9])\\))")
+
+for srcfile in args:
+
+	destfile = os.path.splitext(srcfile)[0] + ".json"
+	if options.verbose:
+		print("INFO: converting %s to %s" % (srcfile, destfile))
+
+	xlate_map = {}
+
+	po = polib.pofile(srcfile, autodetect_encoding=False, encoding="utf-8", wrapwidth=-1)
+	for entry in po.translated_entries():
+		if entry.msgstr == '' or entry.msgstr == entry.msgid:
+			continue
+
+		xlate_map[entry.msgid] = entry.msgstr;
+
+	dest = open(destfile, "w")
+
+	dest.write(json.dumps(xlate_map, sort_keys = True));
+
+	dest.close()
+
