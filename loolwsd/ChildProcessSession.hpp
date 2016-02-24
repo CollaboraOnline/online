3f107ed8ca602af6d2d733d509601313a7f969bb
Tor Lillqvist <tml@collabora.com>
2016-02-24 17:48:03 +0200

    Add snide comment

--- loolwsd/ChildProcessSession.hpp
+++ loolwsd/ChildProcessSession.hpp
@@ -21,6 +21,8 @@
 #include "LOOLSession.hpp"
 
 // The client port number, which is changed via loolwsd args.
+// Except that it isn't. This is "static" so it is a *separate* variable
+// in each compilation unit.
 static int ClientPortNumber = DEFAULT_CLIENT_PORT_NUMBER;
 
 class CallbackWorker;
