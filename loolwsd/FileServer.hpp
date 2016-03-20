a5f8ba44a2b9bb26ec9196a306f9f1b4d60afc79
Pranav Kant <pranavk@collabora.com>
2016-03-20 19:37:24 +0530

    loolwsd: Allow specifying custom file server root
    By default, use git directory root.
    
    Change-Id: I4ee1173c43f313de3abb1732e6a7401169896189

--- loolwsd/FileServer.hpp
+++ loolwsd/FileServer.hpp
@@ -32,6 +32,7 @@
 #include <Poco/Util/Timer.h>
 
 #include "Common.hpp"
+#include "LOOLWSD.hpp"
 
 using Poco::Net::HTTPRequest;
 using Poco::Net::HTTPRequestHandler;
@@ -55,8 +56,8 @@ public:
             std::vector<std::string> requestSegments;
             requestUri.getPathSegments(requestSegments);
 
-            // FIXME: We might want to package all dist files from leaflet to some other dir (?)
-            const std::string loleafletPath = Poco::Path(Application::instance().commandPath()).parent().parent().toString() + "loleaflet";
+            // TODO: We might want to package all files from leaflet to some other dir and restrict
+            // file serving to it (?)
             const std::string endPoint = requestSegments[requestSegments.size() - 1];
 
             if (request.getMethod() == HTTPRequest::HTTP_GET)
@@ -79,7 +80,7 @@ public:
                         Poco::Net::HTTPCookie cookie("jwt", jwtToken);
                         response.addCookie(cookie);
                         response.setContentType(htmlMimeType);
-                        response.sendFile(loleafletPath + "/debug/document/" + endPoint, htmlMimeType);
+                        response.sendFile(LOOLWSD::FileServerRoot + requestUri.getPath(), htmlMimeType);
                     }
                     else
                     {
@@ -87,12 +88,12 @@ public:
                         throw Poco::Net::NotAuthenticatedException("Wrong credentials.");
                     }
                 }
-                else if (requestSegments.size() > 1 && requestSegments[0] == "dist")
+                else
                 {
                     const std::string filePath = requestUri.getPath();
                     const std::size_t extPoint = endPoint.find_last_of(".");
                     if (extPoint == std::string::npos)
-                        return;
+                        throw Poco::FileNotFoundException("Invalid file.");
 
                     const std::string fileType = endPoint.substr(extPoint + 1);
                     std::string mimeType;
@@ -100,15 +101,13 @@ public:
                         mimeType = "application/javascript";
                     else if (fileType == "css")
                         mimeType = "text/css";
+                    else if (fileType == "html")
+                        mimeType = "text/html";
                     else
                         mimeType = "text/plain";
 
                     response.setContentType(mimeType);
-                    response.sendFile(loleafletPath + request.getURI(), mimeType);
-                }
-                else
-                {
-                    throw Poco::FileNotFoundException("");
+                    response.sendFile(LOOLWSD::FileServerRoot + requestUri.getPath(), mimeType);
                 }
             }
         }
