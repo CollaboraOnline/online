2f603139713181b41174a5d6803eae70733cd2a2
Pranav Kant <pranavk@collabora.com>
2016-03-23 19:51:53 +0530

    loolwsd: Cancel admin stats timer when thread finishes
    Change-Id: I19d6a5368e650f0de93ee15b82c3549344a1d4b4

--- loolwsd/Admin.cpp
+++ loolwsd/Admin.cpp
@@ -493,6 +493,9 @@ void Admin::run()
     Util::pollPipeForReading(pollPipeNotify, FIFO_NOTIFY, NotifyPipe,
                             [this](std::string& message) { return handleInput(message); } );
 
+    _memStatsTimer.cancel();
+    _cpuStatsTimer.cancel();
+
     Log::debug("Thread [" + thread_name + "] finished.");
 }
 
