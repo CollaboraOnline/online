16034f66853947b309a610c49779466e68b56f50
Tor Lillqvist <tml@collabora.com>
2016-04-07 20:09:35 +0300

    Bin unused INTERVAL_PROBES

--- loolwsd/Common.hpp
+++ loolwsd/Common.hpp
@@ -18,7 +18,6 @@ constexpr int MAX_SESSIONS = 1024;
 
 constexpr int DEFAULT_CLIENT_PORT_NUMBER = 9980;
 constexpr int MASTER_PORT_NUMBER = 9981;
-constexpr int INTERVAL_PROBES = 10;
 constexpr int MAINTENANCE_INTERVAL = 1;
 constexpr int CHILD_TIMEOUT_SECS = 10;
 constexpr int POLL_TIMEOUT_MS = 1000;
