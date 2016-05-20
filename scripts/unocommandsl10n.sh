4bf7cab1a6c86075b1e1b274cebed63f4526f442
Andras Timar <andras.timar@collabora.com>
2016-05-20 18:13:44 +0200

    loleaflet: initial l10n support of context menus
    (cherry picked from commit bd6ee3a8b6c91ead3f8545ed63dc13b4bd99dbd5)

---
+++ scripts/unocommandsl10n.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+LODIR=/home/timar/cp-5.0
+LEAFLETDIR=/home/timar/online
+
+for lang in `ls -1 $LODIR/translations/source`; do
+echo -ne "{" > $lang.json
+for unocommand in `grep -Pzo "(?s)^(\s*)\N*whitelist: .*?{.*?^\1}" $LEAFLETDIR/loleaflet/src/control/Control.ContextMenu.js | grep -Po "'.*?'" | tr -d "'" | sed -e "s/^/.uno:/"`;do grep -A4 $unocommand $LODIR/translations/source/$lang/officecfg/registry/data/org/openoffice/Office/UI.po | grep -P "msgid|msgstr" | tr -d "~" | sed -e "s/msgid //" -e "s/msgstr //";done | paste -d" " - - | sort | uniq | sed -e 's/" "/": "/' -e 's/$/, /' | grep -v '""' | tr -d "\n" | sed -e "s/, $//" >> $lang.json
+echo -ne "}" >> $lang.json
+done
+
+
