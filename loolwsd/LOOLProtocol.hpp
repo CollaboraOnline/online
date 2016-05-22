f4fc6d0c29e407d60feaaf7f353c35267d4acbed
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2016-05-22 11:26:30 -0400

    loolwsd: new name=value pair parser for strings
    Change-Id: I01c56f9c452fe0ac75b3d9f2aa67787c5bb4d8c6
    Reviewed-on: https://gerrit.libreoffice.org/25336
    Reviewed-by: Ashod Nakashian <ashnakash@gmail.com>
    Tested-by: Ashod Nakashian <ashnakash@gmail.com>

--- loolwsd/LOOLProtocol.hpp
+++ loolwsd/LOOLProtocol.hpp
@@ -39,6 +39,7 @@ namespace LOOLProtocol
 
     bool stringToInteger(const std::string& input, int& value);
     bool parseNameIntegerPair(const std::string& token, std::string& name, int& value);
+    bool parseNameValuePair(const std::string& token, std::string& name, std::string& value);
 
     bool getTokenInteger(const std::string& token, const std::string& name, int& value);
     bool getTokenString(const std::string& token, const std::string& name, std::string& value);
