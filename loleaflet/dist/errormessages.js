58c65fbca492edafbf3ce04c54b5c159713a0d2d
Andras Timar <andras.timar@collabora.com>
2016-05-23 23:53:32 +0200

    loleaflet: add dist/errormessages.js

---
+++ loleaflet/dist/errormessages.js
@@ -0,0 +1,2 @@
+var wrongwopisrc = _('Wrong WOPISrc, usage: WOPISrc=valid encoded URI, or file_path, usage: file_path=/path/to/doc/');
+var emptyhosturl = _('The host URL is empty. The loolwsd server is probably misconfigured, please contact the administrator.');
