5e47eefa6edc15212f3bebc1923f95a42ff5f014
Pranav Kant <pranavk@collabora.com>
2016-05-24 22:37:08 +0530

    loleaflet: Replace gif spinner with html canvas
    The gif spinner had a white background flashes in its frames, so
    it always needed white background behind it. With this, we will
    have no such restriction, can can have transparent background.
    
    Change-Id: I1faffa326ae85114e52231cc4e33d84c7ac0675d
    (cherry picked from commit f6ca0b08cbb80697263f7204ce438b3deeedd741)

--- loleaflet/src/layer/marker/ProgressOverlay.js
+++ loleaflet/src/layer/marker/ProgressOverlay.js
@@ -4,6 +4,10 @@
 
 L.ProgressOverlay = L.Layer.extend({
 
+	options: {
+		spinnerSpeed: 1.5
+	},
+
 	initialize: function (latlng, size) {
 		this._latlng = L.latLng(latlng);
 		this._size = size;
@@ -23,6 +27,10 @@ L.ProgressOverlay = L.Layer.extend({
 		if (this._container) {
 			this.getPane().removeChild(this._container);
 		}
+
+		if (this._spinnerInterval) {
+			clearInterval(this._spinnerInterval);
+		}
 	},
 
 	update: function () {
@@ -38,6 +46,7 @@ L.ProgressOverlay = L.Layer.extend({
 	_initLayout: function () {
 		this._container = L.DomUtil.create('div', 'leaflet-progress-layer');
 		this._spinner = L.DomUtil.create('div', 'leaflet-progress-spinner', this._container);
+		this._spinnerCanvas = L.DomUtil.create('canvas', 'leaflet-progress-spinner-canvas', this._spinner);
 		this._label = L.DomUtil.create('div', 'leaflet-progress-label', this._container);
 		this._progress = L.DomUtil.create('div', 'leaflet-progress', this._container);
 		this._bar = L.DomUtil.create('span', '', this._progress);
@@ -47,9 +56,34 @@ L.ProgressOverlay = L.Layer.extend({
 
 		this._container.style.width  = this._size.x + 'px';
 
+		this._initSpinner();
+
 		L.DomEvent
 			.disableClickPropagation(this._progress)
 			.disableScrollPropagation(this._container);
+	},
+
+	_initSpinner: function () {
+		this._spinnerCanvas.width = 50;
+		this._spinnerCanvas.height = 50;
+
+		var context = this._spinnerCanvas.getContext('2d');
+		context.lineWidth = 8;
+		context.strokeStyle = 'grey';
+		var x = this._spinnerCanvas.width / 2;
+		var y = this._spinnerCanvas.height / 2;
+		var radius = y - context.lineWidth / 2;
+		var self = this;
+		this._spinnerInterval = setInterval(function() {
+			context.clearRect(0, 0, x * 2, y * 2);
+			// Move to center
+			context.translate(x, y);
+			context.rotate(self.options.spinnerSpeed * Math.PI / 180);
+			context.translate(-x, -y);
+			context.beginPath();
+			context.arc(x, y, radius, 0, Math.PI * 1.3);
+			context.stroke();
+		}, 1);
 	},
 
 	_setPos: function (pos) {
