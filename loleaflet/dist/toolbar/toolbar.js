37b71b2289d1f465d8d5096fec8f803ba5f1942c
Pranav Kant <pranavk@collabora.com>
2016-05-24 11:27:12 +0530

    loleaflet: Leave 125px from left for branding logo
    Change-Id: I92dbf92e4d140c8975198b14f6560c1d309202a8

--- loleaflet/dist/toolbar/toolbar.js
+++ loleaflet/dist/toolbar/toolbar.js
@@ -14,6 +14,7 @@ $(function () {
 	$('#toolbar-up').w2toolbar({
 		name: 'toolbar-up',
 		items: [
+			{ type: 'html', id: 'left' },
 			{ type: 'button',  id: 'save', img: 'save', hint: _("Save"), uno: 'Save' },
 			{ type: 'break' },
 			{ type: 'button',  id: 'undo',  img: 'undo', hint: _("Undo"), uno: 'Undo' },
