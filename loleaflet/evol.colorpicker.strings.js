1fe3c8c055b00dd9ff02732b82c86c5bf81619cd
Andras Timar <andras.timar@collabora.com>
2016-06-03 12:25:54 +0200

    loleaflet: enable l10n of Color picker widget
    (cherry picked from commit c3fe386dc0c8cfaed416f9770a73141ba81d6e9c)

---
+++ loleaflet/evol.colorpicker.strings.js
@@ -0,0 +1,7 @@
+/* Strings for l10n from evol.colorpicker */
+var themeColors = _('Theme Colors');
+var standardColors = _('Standard Colors');
+var webColors = _('Web Colors');
+var backToPalette = _('Back to Palette');
+var history = _('History');
+var noHistory = _('No history yet.');
