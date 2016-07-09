493c460ec119ed9e9be8e280eaa921eb006f0e6f
Pranav Kant <pranavk@collabora.co.uk>
2016-07-10 01:08:56 +0530

    loleaflet: Separate document URL from query params
    Change-Id: I9b8381b8c0d92796aadabe0bc63e5a1b144f8b41
    (cherry picked from commit 126503289613704dc490adca367bc90d35b8674f)
    
     Conflicts:
    	loleaflet/src/core/Socket.js

--- loleaflet/src/core/Socket.js
+++ loleaflet/src/core/Socket.js
@@ -9,10 +9,10 @@ L.Socket = L.Class.extend({
 	initialize: function (map) {
 		this._map = map;
 		try {
-			var params = {
-				permission: map.options.permission
-			};
-			this.socket = new WebSocket(map.options.server + '/lool/' + encodeURIComponent(map.options.doc + '?' + $.param(params)) + '/ws');
+			if (map.options.permission) {
+				map.options.docParams['permission'] = map.options.permission;
+			}
+			this.socket = new WebSocket(map.options.server + '/lool/' + encodeURIComponent(map.options.doc + '?' + $.param(map.options.docParams)) + '/ws');
 			this.socket.onerror = L.bind(this._onSocketError, this);
 			this.socket.onclose = L.bind(this._onSocketClose, this);
 			this.socket.onopen = L.bind(this._onSocketOpen, this);
