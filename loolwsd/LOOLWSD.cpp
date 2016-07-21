CollaboraOnline/online

commit 886af28bc2f6d1d27bf78de09978f3858d2db705

loolwsd: improved random directory generator
Change-Id: Ic4ea88ba77549cc4ccba83c27fffdffcdeebb984
Reviewed-on: https://gerrit.libreoffice.org/25698
Reviewed-by: Ashod Nakashian <ashnakash@gmail.com>
Tested-by: Ashod Nakashian <ashnakash@gmail.com>

loolwsd/LOOLWSD.cpp
@@ -420,7 +420,8 @@ private:
                     if (!resultURL.getPath().empty())
                     {
                         const std::string mimeType = "application/octet-stream";
-                        response.sendFile(resultURL.getPath(), mimeType);
+                        URI::encode(resultURL.getPath(), "", encodedTo);
+                        response.sendFile(encodedTo, mimeType);
                         sent = true;
                     }
 
@@ -503,6 +504,10 @@ private:
                 //TODO: Cleanup on error.
                 Util::removeFile(dirPath, true);
                 return true;
+            }
+            else
+            {
+                Log::error("Download file [" + filePath + "] not found.");
             }
         }
 

loolwsd/Util.cpp
@@ -81,12 +81,26 @@ namespace rng
         return v;
     }
 
-    std::string getString(const size_t length)
+    /// Generates a random string in Base64.
+    /// Note: May contain '/' characters.
+    std::string getB64String(const size_t length)
     {
         std::stringstream ss;
         Poco::Base64Encoder b64(ss);
         b64 << getBytes(length).data();
         return ss.str().substr(0, length);
+    }
+
+    /// Generates a random string suitable for
+    /// file/directory names.
+    std::string getFilename(const size_t length)
+    {
+        std::stringstream ss;
+        Poco::Base64Encoder b64(ss);
+        b64 << getBytes(length).data();
+        std::string s = ss.str();
+        std::replace(s.begin(), s.end(), '/', '_');
+        return s.substr(0, length);
     }
 }
 }
@@ -109,10 +123,11 @@ namespace Util
         return id;
     }
 
+    /// Create a secure, random directory path.
     std::string createRandomDir(const std::string& path)
     {
         Poco::File(path).createDirectories();
-        const auto name = rng::getString(64);
+        const auto name = rng::getFilename(64);
         Poco::File(Poco::Path(path, name)).createDirectories();
         return name;
     }

commit 39645c974d38c9494ec773ac493b2b7590e803fd

loolwsd: Fix convert-to regression; fix incorrect filepath formed
Regressed with 886af28bc2f6d1d27bf78de09978f3858d2db705

Poco::URI::encode appends to given string, and encodedTo already
had content leading to incorrect path. Use a new std::string variable.

Change-Id: Ia72ff60ed9cf7f14ff649416a2ceeeda13ff6197

loolwsd/LOOLWSD.cpp
@@ -435,8 +435,9 @@ private:
                     if (!resultURL.getPath().empty())
                     {
                         const std::string mimeType = "application/octet-stream";
-                        URI::encode(resultURL.getPath(), "", encodedTo);
-                        response.sendFile(encodedTo, mimeType);
+                        std::string encodedFilePath;
+                        URI::encode(resultURL.getPath(), "", encodedFilePath);
+                        response.sendFile(encodedFilePath, mimeType);
                         sent = true;
                     }
 