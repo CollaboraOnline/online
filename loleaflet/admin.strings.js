6c69b63be58f9a2989a5dc8a47a44c9eafc72b4e
Pranav Kant <pranavk@collabora.co.uk>
2016-08-09 19:56:05 +0530

    loleaflet: Browserify admin, more modularization, npm-isation
    Use browserify for admin console scripts too.
    
    Modularize needed files such as l10n strings for console.
    
    Use npm for managing bootstrap as dependency of admin console.

---
+++ loleaflet/admin.strings.js
@@ -0,0 +1,29 @@
+/* Stringtable for Admin Console User Interface */
+var l10nstrings = {};
+
+l10nstrings.strProductName = 'LibreOffice Online';
+l10nstrings.strAdminConsole = _('Admin console');
+l10nstrings.strToggleNavigation = _('Toggle navigation');
+l10nstrings.strSettings = _('Settings');
+l10nstrings.strOverview = _('Overview');
+l10nstrings.strCurrent = _('(current)');
+l10nstrings.strAnalytics = _('Analytics');
+l10nstrings.strDashboard = _('Dashboard');
+l10nstrings.strUsersOnline = _('Users online');
+l10nstrings.strDocumentsOpened = _('Documents opened');
+l10nstrings.strMemoryConsumed = _('Memory consumed');
+l10nstrings.strPid = _('PID');
+l10nstrings.strDocument = _('Document');
+l10nstrings.strNumberOfViews = _('Number of views');
+l10nstrings.strElapsedTime = _('Elapsed time');
+l10nstrings.strKill = _('Kill');
+l10nstrings.strGraphs = _('Graphs');
+l10nstrings.strSave = _('Save');
+l10nstrings.strMemoryStatsCachesize = _('Cache size of memory statistics');
+l10nstrings.strMemoryStatsInterval = _('Time interval of memory statistics (in ms)');
+l10nstrings.strCpuStatsCachesize = _('Cache size of CPU statistics');
+l10nstrings.strCpuStatsInterval = _('Time interval of CPU statistics (in ms)');
+
+if (module) {
+	module.exports = l10nstrings;
+}
---
+++ loleaflet/main-admin.js
@@ -0,0 +1,28 @@
+// CSS requires
+require('./node_modules/bootstrap/dist/css/bootstrap.css');
+require('./dist/admin/bootstrap/ie10-viewport-bug-workaround.css');
+require('./dist/admin/bootstrap/dashboard.css');
+require('./node_modules/vex-js/css/vex.css');
+require('./node_modules/vex-js/css/vex-theme-plain.css');
+
+var $ = require('jquery');
+global.$ = global.jQuery = $;
+
+require('json-js/json2');
+require('l10n-for-node');
+
+var vex = require('vex-js');
+vex.dialog = require('vex-js/js/vex.dialog.js');
+vex.defaultOptions.className = 'vex-theme-plain';
+global.vex = vex;
+
+global._ = function (string) {
+	return string.toLocaleString();
+};
+
+global.l10nstrings = require('./admin.strings.js');
+
+require('bootstrap/dist/js/bootstrap.js');
+require('./dist/admin/bootstrap/holder.min.js');
+require('./dist/admin/bootstrap/ie10-viewport-bug-workaround.js');
+global.Admin = require('./dist/admin/admin-src.js');
