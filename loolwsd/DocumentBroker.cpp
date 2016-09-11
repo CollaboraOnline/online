dd430a3367d49c021dc6d64202702b102f1a6eb6
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2016-09-11 13:33:25 -0400

    Revert "Revert "loolwsd: remove tile queue and simplify tile response""
    This reverts commit 1bb09798f63e3c47bc5863e5df68d77a123f44a3.

--- loolwsd/DocumentBroker.cpp
+++ loolwsd/DocumentBroker.cpp
@@ -319,7 +319,6 @@ bool DocumentBroker::sendUnoSave(const bool dontSaveIfUnmodified)
             _lastFileModifiedTime.fromEpochTime(0);
 
             // We do not want save to terminate editing mode if we are in edit mode now
-
             std::ostringstream oss;
             // arguments init
             oss << "{";
@@ -345,8 +344,10 @@ bool DocumentBroker::sendUnoSave(const bool dontSaveIfUnmodified)
             // arguments end
             oss << "}";
 
-            Log::debug(".uno:Save arguments: " + oss.str());
-            sessionIt.second->sendToInputQueue("uno .uno:Save " + oss.str());
+            const auto saveArgs = oss.str();
+            Log::trace(".uno:Save arguments: " + saveArgs);
+            const auto command = "uno .uno:Save " + saveArgs;
+            sessionIt.second->handleInput(command.data(), command.size());
             return true;
         }
     }
--- loolwsd/TileCache.cpp
+++ loolwsd/TileCache.cpp
@@ -168,19 +168,32 @@ void TileCache::saveTileAndNotify(const TileDesc& tile, const char *data, const 
     {
         if (!tileBeingRendered->_subscribers.empty())
         {
-            const std::string message = tile.serialize("tile");
-            Log::debug("Sending tile message to subscribers: " + message);
+            std::string response = tile.serialize("tile:");
+            Log::debug("Sending tile message to subscribers: " + response);
+            response += '\n';
+
+            std::vector<char> output;
+            output.reserve(static_cast<size_t>(4) * tile.getWidth() * tile.getHeight());
+            output.resize(response.size());
+            std::memcpy(output.data(), response.data(), response.size());
+
+            const auto pos = output.size();
+            output.resize(pos + size);
+            std::memcpy(output.data() + pos, data, size);
 
             for (const auto& i: tileBeingRendered->_subscribers)
             {
                 auto subscriber = i.lock();
                 if (subscriber)
                 {
-                    //FIXME: This is inefficient; should just send directly to each client (although that is risky as well!
-                    // Re-emit the tile command in the other thread(s) to re-check and hit
-                    // the cache. Construct the message from scratch to contain only the
-                    // mandatory parts of the message.
-                    subscriber->sendToInputQueue(message);
+                    try
+                    {
+                        subscriber->sendBinaryFrame(output.data(), output.size());
+                    }
+                    catch (const std::exception& ex)
+                    {
+                        Log::warn("Failed to send tile to " + subscriber->getName() + ": " + ex.what());
+                    }
                 }
             }
         }
