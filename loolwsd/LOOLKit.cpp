2ea7d9bf1466a3654a7063ef5206208f639149cd
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2016-09-17 09:18:33 -0400

    loolwsd: one anonymous namespace per file is enough
    Change-Id: I12b4967a321de45fc50ad314775d01499b5edd80
    Reviewed-on: https://gerrit.libreoffice.org/29061
    Reviewed-by: Ashod Nakashian <ashnakash@gmail.com>
    Tested-by: Ashod Nakashian <ashnakash@gmail.com>
    (cherry picked from commit bad8ae9ed99879dbdac6cb37d425c922ba047d5b)

--- loolwsd/LOOLKit.cpp
+++ loolwsd/LOOLKit.cpp
@@ -231,6 +231,24 @@ namespace
         cap_free(caps);
     }
 
+    void symlinkPathToJail(const Path& jailPath, const std::string &loTemplate,
+                           const std::string &loSubPath)
+    {
+        Path symlinkSource(jailPath, Path(loTemplate.substr(1)));
+        File(symlinkSource.parent()).createDirectories();
+
+        std::string symlinkTarget;
+        for (auto i = 0; i < Path(loTemplate).depth(); i++)
+            symlinkTarget += "../";
+        symlinkTarget += loSubPath;
+
+        Log::debug("symlink(\"" + symlinkTarget + "\",\"" + symlinkSource.toString() + "\")");
+        if (symlink(symlinkTarget.c_str(), symlinkSource.toString().c_str()) == -1)
+        {
+            Log::syserror("symlink(\"" + symlinkTarget + "\",\"" + symlinkSource.toString() + "\") failed");
+            throw Exception("symlink() failed");
+        }
+    }
 }
 
 /// Connection thread with a client (via WSD).
@@ -1291,27 +1309,6 @@ private:
     Poco::NotificationQueue _callbackQueue;
     std::atomic_size_t _clientViews;
 };
-
-namespace {
-    void symlinkPathToJail(const Path& jailPath, const std::string &loTemplate,
-                           const std::string &loSubPath)
-    {
-        Path symlinkSource(jailPath, Path(loTemplate.substr(1)));
-        File(symlinkSource.parent()).createDirectories();
-
-        std::string symlinkTarget;
-        for (auto i = 0; i < Path(loTemplate).depth(); i++)
-            symlinkTarget += "../";
-        symlinkTarget += loSubPath;
-
-        Log::debug("symlink(\"" + symlinkTarget + "\",\"" + symlinkSource.toString() + "\")");
-        if (symlink(symlinkTarget.c_str(), symlinkSource.toString().c_str()) == -1)
-        {
-            Log::syserror("symlink(\"" + symlinkTarget + "\",\"" + symlinkSource.toString() + "\") failed");
-            throw Exception("symlink() failed");
-        }
-    }
-}
 
 void lokit_main(const std::string& childRoot,
                 const std::string& sysTemplate,
