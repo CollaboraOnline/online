857c3c4f044f1e4d323576532364c890c7de283e
Tor Lillqvist <tml@collabora.com>
2016-09-21 18:44:02 +0300

    Check LOOL_NO_LOGCOLOR here, too
    Note that just like for loolwsd, LOOL_NO_LOGCOLOR is effective only
    when stderr is not going to a terminal. If it goes to a terminal, there
    is no way to avoid use of color, see Log::initialize().

--- loolwsd/test/test.cpp
+++ loolwsd/test/test.cpp
@@ -72,7 +72,7 @@ bool filterTests(CPPUNIT_NS::TestRunner& runner, CPPUNIT_NS::Test* testRegistry)
 
 int main(int /*argc*/, char** /*argv*/)
 {
-    Log::initialize("tst", "trace", true, false);
+    Log::initialize("tst", "trace", !std::getenv("LOOL_NO_LOGCOLOR"), false);
 
 
     CPPUNIT_NS::TestResult controller;
