5e19aa635e510b88397f8ea45f30c5bc07ce6f0c
Henry Castro <hcastro@collabora.com>
2016-09-28 16:04:51 -0400

    loolwsd: test: connections and documents

--- loolwsd/UserMessages.hpp
+++ loolwsd/UserMessages.hpp
@@ -15,7 +15,7 @@
 //NOTE: For whatever reason Poco seems to trim the first character.
 
 constexpr auto SERVICE_UNAVALABLE_INTERNAL_ERROR = " Service is unavailable. Please try again later and report to your administrator if the issue persists.";
-constexpr auto SERVICE_UNAVALABLE_LIMIT_REACHED = " This server has reached the number of connections or documents it supports at a given time.";
+constexpr auto SERVICE_UNAVALABLE_LIMIT_REACHED = "This development build is limited to %d documents, and %d connections - to avoid the impression that it is suitable for deployment in large enterprises. To find out more about deploying and scaling %s checkout - <a href=\"%s\">%s</a>.";
 
 #endif
 
