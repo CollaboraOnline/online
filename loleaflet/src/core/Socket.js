b4ba3c9c553ac836007b2f1c3cb4a45280b61673
Henry Castro <hcastro@collabora.com>
2016-09-29 17:57:17 -0400

    loleaflet: fix mege mistakes

--- loleaflet/src/core/Socket.js
+++ loleaflet/src/core/Socket.js
@@ -12,7 +12,7 @@ L.Socket = L.Class.extend({
 			if (map.options.permission) {
 				map.options.docParams['permission'] = map.options.permission;
 			}
-			this.socket = new WebSocket(map.options.server + '/lool/ws/' + map.options.doc + '?' + $.param(map.options.docParams));
+			this.socket = new WebSocket(map.options.server + '/lool/' + encodeURIComponent(map.options.doc + '?' + $.param(map.options.docParams)) + '/ws');
 			this.socket.onerror = L.bind(this._onSocketError, this);
 			this.socket.onclose = L.bind(this._onSocketClose, this);
 			this.socket.onopen = L.bind(this._onSocketOpen, this);
