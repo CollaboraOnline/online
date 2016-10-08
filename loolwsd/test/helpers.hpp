e88903d9c28f2994186b753e2ebd6c8108db4469
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2016-10-07 23:38:11 -0400

    loolwsd: log test name from more helpers
    Change-Id: I51240cf6dc3d2df3ed4b73f8a00dff968653de98
    Reviewed-on: https://gerrit.libreoffice.org/29606
    Reviewed-by: Ashod Nakashian <ashnakash@gmail.com>
    Tested-by: Ashod Nakashian <ashnakash@gmail.com>
    (cherry picked from commit a18c404f296b723b841b1c0baafcfd8a92ae41af)

--- loolwsd/test/helpers.hpp
+++ loolwsd/test/helpers.hpp
@@ -589,29 +589,29 @@ inline int getCharKey(char ch, SpecialKey specialKeys)
     return result | specialKeys;
 }
 
-inline void sendKeyEvent(Poco::Net::WebSocket& socket, const char* type, int chr, int key)
+inline void sendKeyEvent(Poco::Net::WebSocket& socket, const char* type, int chr, int key, const std::string testname = "")
 {
     std::ostringstream ssIn;
     ssIn << "key type=" << type << " char=" << chr << " key=" << key;
-    sendTextFrame(socket, ssIn.str());
+    sendTextFrame(socket, ssIn.str(), testname);
 }
 
-inline void sendKeyPress(Poco::Net::WebSocket& socket, int chr, int key)
+inline void sendKeyPress(Poco::Net::WebSocket& socket, int chr, int key, const std::string testname)
 {
-    sendKeyEvent(socket, "input", chr, key);
-    sendKeyEvent(socket, "up", chr, key);
+    sendKeyEvent(socket, "input", chr, key, testname);
+    sendKeyEvent(socket, "up", chr, key, testname);
 }
 
-inline void sendChar(Poco::Net::WebSocket& socket, char ch, SpecialKey specialKeys=skNone)
+inline void sendChar(Poco::Net::WebSocket& socket, char ch, SpecialKey specialKeys=skNone, const std::string testname = "")
 {
-    sendKeyPress(socket, getCharChar(ch, specialKeys), getCharKey(ch, specialKeys));
+    sendKeyPress(socket, getCharChar(ch, specialKeys), getCharKey(ch, specialKeys), testname);
 }
 
-inline void sendText(Poco::Net::WebSocket& socket, const std::string& text)
+inline void sendText(Poco::Net::WebSocket& socket, const std::string& text, const std::string testname = "")
 {
     for (char ch : text)
     {
-        sendChar(socket, ch);
+        sendChar(socket, ch, skNone, testname);
     }
 }
 
