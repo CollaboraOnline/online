ba23ab2927da87f9e24514156056d87d25c4c922
Pranav Kant <pranavk@collabora.co.uk>
2016-10-21 21:10:09 +0530

    loleaflet: Expose some context menu item for comments
    Change-Id: I242755c67f904ffbaaac63db24d735a9bfaa6bca

--- loleaflet/src/control/Control.ContextMenu.js
+++ loleaflet/src/control/Control.ContextMenu.js
@@ -32,7 +32,8 @@ L.Control.ContextMenu = L.Control.extend({
 				   'TableDeleteMenu',
 				   'DeleteRows', 'DeleteColumns', 'DeleteTable',
 				   'MergeCells', 'SetOptimalColumnWidth', 'SetOptimalRowWidth',
-				   'UpdateCurIndex','RemoveTableOf'],
+				   'UpdateCurIndex','RemoveTableOf',
+				   'ReplyComment', 'DeleteComment', 'DeleteAuthor', 'DeleteAllNotes'],
 
 			spreadsheet: ['MergeCells', 'SplitCells',
 						  'InsertAnnotation', 'EditAnnotation', 'DeleteNote', 'ShowNote', 'HideNote'],
@@ -120,6 +121,13 @@ L.Control.ContextMenu = L.Control.extend({
 
 				itemName = item.text.replace('~', '');
 				itemName = itemName.replace('Â°', '°'); // bccu#1813 double encoding in cp-5.0 branch only
+				if (commandName === 'DeleteAuthor') {
+					// In some versions of libreoffice, context menu callback returns 'Delete All Comments by $1'
+					// while in some it returns the actual username replacing $1.
+					// Also, the translations in LO core are for 'Delete All Comments by This Author'
+					// Lets use the later for simplicity and to leverage the core translations in online
+					itemName = itemName.replace(itemName.substring('Delete All Comments by '.length), 'This Author');
+				}
 				contextMenu[item.command] = {
 					name: _(itemName)
 				};
