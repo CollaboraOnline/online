c8fa6cb5b0d69493c03ac65fe2dfa70cd7cd34ca
Andras Timar <andras.timar@collabora.com>
2016-11-24 15:11:55 +0100

    loleaflet: disable zoom for spreadsheet even on touchscreens

--- loleaflet/src/map/handler/Map.TouchZoom.js
+++ loleaflet/src/map/handler/Map.TouchZoom.js
@@ -29,7 +29,12 @@ L.Map.TouchZoom = L.Handler.extend({
 		this._startDist = p1.distanceTo(p2);
 
 		this._moved = false;
-		this._zooming = true;
+		if (map.getDocType() === 'spreadsheet') {
+			this._zooming = false;
+		}
+		else {
+			this._zooming = true;
+		}
 
 		this._centerOffset = viewCenter.subtract(this._startCenter);
 
