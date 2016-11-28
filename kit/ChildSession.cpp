ddd91b4251a9d98c2c84709aa5cf6f8d9cd065ce
Henry Castro <hcastro@collabora.com>
2016-11-27 20:53:11 -0400

    kit: enable notify undo/redo commands

--- kit/ChildSession.cpp
+++ kit/ChildSession.cpp
@@ -787,7 +787,7 @@ bool ChildSession::unoCommand(const char* /*buffer*/, int /*length*/, StringToke
     }
 
     // we need to get LOK_CALLBACK_UNO_COMMAND_RESULT callback when saving
-    const bool bNotify = (tokens[1] == ".uno:Save");
+    const bool bNotify = (tokens[1] == ".uno:Save" || tokens[1] == ".uno:Undo" || tokens[1] == ".uno:Redo");
 
     std::unique_lock<std::mutex> lock(_docManager.getDocumentMutex());
 
