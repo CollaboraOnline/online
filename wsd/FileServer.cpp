0b596ae51d10d4391626b9e1ed3184f89b5e6fd0
Andras Timar <andras.timar@collabora.com>
2016-12-13 09:55:31 +0100

    wsd: do not warn about missing access_token_ttl, when there is no access_token
    Change-Id: I6ac7014dee21892dfd8b3b594cafe2dc030b6b2a

--- wsd/FileServer.cpp
+++ wsd/FileServer.cpp
@@ -237,20 +237,23 @@ void FileServerRequestHandler::preprocessFile(HTTPServerRequest& request, HTTPSe
     Poco::URI::encode(accessToken, "'", escapedAccessToken);
 
     unsigned long tokenTtl = 0;
-    if (accessTokenTtl != "")
+    if (accessToken != "")
     {
-        try
+        if (accessTokenTtl != "")
         {
-            tokenTtl = std::stoul(accessTokenTtl);
+            try
+            {
+                tokenTtl = std::stoul(accessTokenTtl);
+            }
+            catch(const std::exception& exc)
+            {
+                LOG_ERR("access_token_ttl must be represented as the number of milliseconds since January 1, 1970 UTC, when the token will expire");
+            }
         }
-        catch(const std::exception& exc)
+        else
         {
-            LOG_ERR("access_token_ttl must be a unix timestamp of when token will expire");
+            LOG_WRN("WOPI host did not pass optional access_token_ttl");
         }
-    }
-    else
-    {
-        LOG_WRN("WOPI host did not pass access_token_ttl");
     }
 
     Poco::replaceInPlace(preprocess, std::string("%ACCESS_TOKEN%"), escapedAccessToken);
