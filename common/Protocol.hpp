9436d2b00b957b1a94984f0487b6275246793edd
Tor Lillqvist <tml@collabora.com>
2016-12-14 17:23:42 +0200

    Add 'userinactive' to messages that don't indicate user interaction
    In fact, it is more or less the opposite;)
    
    Change-Id: I34eca0ed6b7d4ce369ae803eac542f0996d193eb
    (cherry picked from commit 0bdac17fa6a5e7bf730b736af99d70fa0650b4a1)

--- common/Protocol.hpp
+++ common/Protocol.hpp
@@ -192,7 +192,8 @@ namespace LOOLProtocol
 
         return (token.find("tile") == std::string::npos &&
                 token.find("status") == std::string::npos &&
-                token.find("state") == std::string::npos);
+                token.find("state") == std::string::npos &&
+                token != "userinactive");
     }
 
     /// Returns the first line of a message.
