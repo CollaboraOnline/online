fedfc75714991c28bfdad882ddc012c1d6ad0a5f
Pranav Kant <pranavk@collabora.co.uk>
2016-12-20 16:03:24 +0530

    loleaflet: reset selection after search-not-found
    Change-Id: If1c10cb10d1fce79a4697d5cc548dae781cfb2ec

--- loleaflet/dist/toolbar/toolbar.js
+++ loleaflet/dist/toolbar/toolbar.js
@@ -1045,6 +1045,7 @@ map.on('search', function (e) {
 		toolbar.disable('searchnext');
 		toolbar.hide('cancelsearch');
 		L.DomUtil.addClass(searchInput, 'search-not-found');
+		map.resetSelection();
 		setTimeout(function () {
 			L.DomUtil.removeClass(searchInput, 'search-not-found');
 		}, 500);
