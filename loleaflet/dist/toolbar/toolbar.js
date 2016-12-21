c29369231cdda89e008881690f20cdb2940b0908
Henry Castro <hcastro@collabora.com>
2016-12-21 19:50:04 -0400

    tdf#103775: Font color/Highlighting button issue
    (cherry picked from commit 65bfc63ace567501912e4b6399ff8f81cc62516e)
    
    Change-Id: Ie52972574cc07ac5826da261aab6671e9fd0272c
    Reviewed-on: https://gerrit.libreoffice.org/35479
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
    Tested-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

--- loleaflet/dist/toolbar/toolbar.js
+++ loleaflet/dist/toolbar/toolbar.js
@@ -408,6 +408,28 @@ $(function () {
 			if (!L.DomUtil.get('fontcolorindicator')) {
 				var fontColorIndicator = L.DomUtil.create('div', 'font-color-indicator', L.DomUtil.get('tb_toolbar-up_item_fontcolor'));
 				fontColorIndicator.id = 'fontcolorindicator';
+				L.DomEvent.on(fontColorIndicator, 'mouseover', function () {
+					var button = fontColorIndicator.parentNode.firstChild;
+					$(button).addClass('over');
+				});
+				L.DomEvent.on(fontColorIndicator, 'mouseout', function () {
+					var button = fontColorIndicator.parentNode.firstChild;
+					$(button).removeClass('over');
+				});
+				L.DomEvent.on(fontColorIndicator, 'mousedown', function () {
+					var button = fontColorIndicator.parentNode.firstChild;
+					$(button).addClass('down');
+				});
+				L.DomEvent.on(fontColorIndicator, 'mouseup', function () {
+					var button = fontColorIndicator.parentNode.firstChild;
+					$(button).removeClass('down');
+				});
+				fontColorIndicator.addEventListener('click', function () {
+					var toolbar = w2ui['toolbar-up'];
+					if (toolbar) {
+						toolbar.click('fontcolor', window.event);
+					}
+				}, false);
 
 				$('#fontColorPicker').colorpicker({showOn:'none', hideButton:true});
 				$('#fontColorPicker').on('change.color', onColorPick);
@@ -416,6 +438,28 @@ $(function () {
 			if (!L.DomUtil.get('backcolorindicator')) {
 				var backColorIndicator = L.DomUtil.create('div', 'back-color-indicator', L.DomUtil.get('tb_toolbar-up_item_backcolor'));
 				backColorIndicator.id = 'backcolorindicator';
+				L.DomEvent.on(backColorIndicator, 'mouseover', function () {
+					var button = backColorIndicator.parentNode.firstChild;
+					$(button).addClass('over');
+				});
+				L.DomEvent.on(backColorIndicator, 'mouseout', function () {
+					var button = backColorIndicator.parentNode.firstChild;
+					$(button).removeClass('over');
+				});
+				L.DomEvent.on(backColorIndicator, 'mousedown', function () {
+					var button = backColorIndicator.parentNode.firstChild;
+					$(button).addClass('down');
+				});
+				L.DomEvent.on(backColorIndicator, 'mouseup', function () {
+					var button = backColorIndicator.parentNode.firstChild;
+					$(button).removeClass('down');
+				});
+				backColorIndicator.addEventListener('click', function () {
+					var toolbar = w2ui['toolbar-up'];
+					if (toolbar) {
+						toolbar.click('backcolor', window.event);
+					}
+				}, false);
 
 				$('#backColorPicker').colorpicker({showOn:'none', hideButton:true});
 				$('#backColorPicker').on('change.color', onColorPick);
