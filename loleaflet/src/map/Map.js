5f544380715f3e96e6d5019dba8e33069d93e25a
Pranav Kant <pranavk@collabora.co.uk>
2016-12-22 15:46:48 +0530

    loleaflet: Focus on textarea after map activation
    Change-Id: Iaf1993a9a331ab86c668e718fae5f6a727c120cc
    (cherry picked from commit 0ffc37c0455bae4843592a25dbd2938db93b645e)
    Reviewed-on: https://gerrit.libreoffice.org/33023
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    Tested-by: Andras Timar <andras.timar@collabora.com>

--- loleaflet/src/map/Map.js
+++ loleaflet/src/map/Map.js
@@ -566,7 +566,9 @@ L.Map = L.Evented.extend({
 	},
 
 	focus: function () {
+		console.debug('focus:');
 		if (this._docLayer && document.activeElement !== this._docLayer._textArea) {
+			console.debug('focus: focussing');
 			this._docLayer._textArea.focus();
 		}
 	},
@@ -753,6 +755,7 @@ L.Map = L.Evented.extend({
 					var id = vex.dialogID;
 					vex.dialogID = -1;
 					this._startInactiveTimer();
+					this.focus();
 					return vex.close(id);
 				}
 			} else {
@@ -761,6 +764,7 @@ L.Map = L.Evented.extend({
 		}
 
 		this._startInactiveTimer();
+		this.focus();
 		return false;
 	},
 
