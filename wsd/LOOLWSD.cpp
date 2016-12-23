8d3815c32aa55a9ab5b17f98c08ce8c2359257bd
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2016-12-22 19:33:46 -0500

    wsd: log failure in else and not unconditionally
    Change-Id: I24f1ac57f350aa67cc14682d695e1e4a07453c8d
    Reviewed-on: https://gerrit.libreoffice.org/35475
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
    Tested-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

--- wsd/LOOLWSD.cpp
+++ wsd/LOOLWSD.cpp
@@ -834,9 +834,12 @@ private:
                 }
             }
         }
+        else
+        {
+            LOG_DBG("No DocumentBroker with docKey [" << docKey << "] found. New Child and Document.");
+        }
 
         Util::assertIsLocked(docBrokersLock);
-        LOG_DBG("No DocumentBroker with docKey [" << docKey << "] found. New Child and Document.");
 
         if (TerminationFlag)
         {
