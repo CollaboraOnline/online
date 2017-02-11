b66edf67bf230a8a118f288183222514620d87f3
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2017-02-11 12:13:31 -0500

    nb: add new loolnb program
    Change-Id: Ib08ff3cdc037876e9c53b87603140b3c603ee916

---
+++ net/loolnb.cpp
@@ -0,0 +1,137 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "config.h"
+
+#include <unistd.h>
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include <cassert>
+#include <cerrno>
+#include <clocale>
+#include <condition_variable>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <mutex>
+#include <sstream>
+#include <thread>
+
+#include <Poco/DOM/AutoPtr.h>
+#include <Poco/DOM/DOMParser.h>
+#include <Poco/DOM/DOMWriter.h>
+#include <Poco/DOM/Document.h>
+#include <Poco/DOM/Element.h>
+#include <Poco/DOM/NodeList.h>
+#include <Poco/Environment.h>
+#include <Poco/Exception.h>
+#include <Poco/File.h>
+#include <Poco/FileStream.h>
+#include <Poco/Net/AcceptCertificateHandler.h>
+#include <Poco/Net/ConsoleCertificateHandler.h>
+#include <Poco/Net/Context.h>
+#include <Poco/Net/HTMLForm.h>
+#include <Poco/Net/HTTPRequest.h>
+#include <Poco/Net/HTTPRequestHandler.h>
+#include <Poco/Net/HTTPRequestHandlerFactory.h>
+#include <Poco/Net/HTTPServer.h>
+#include <Poco/Net/HTTPServerParams.h>
+#include <Poco/Net/HTTPServerRequest.h>
+#include <Poco/Net/HTTPServerResponse.h>
+#include <Poco/Net/IPAddress.h>
+#include <Poco/Net/InvalidCertificateHandler.h>
+#include <Poco/Net/KeyConsoleHandler.h>
+#include <Poco/Net/MessageHeader.h>
+#include <Poco/Net/NameValueCollection.h>
+#include <Poco/Net/Net.h>
+#include <Poco/Net/NetException.h>
+#include <Poco/Net/PartHandler.h>
+#include <Poco/Net/PrivateKeyPassphraseHandler.h>
+#include <Poco/Net/SSLManager.h>
+#include <Poco/Net/SecureServerSocket.h>
+#include <Poco/Net/ServerSocket.h>
+#include <Poco/Net/SocketAddress.h>
+#include <Poco/Path.h>
+#include <Poco/Pipe.h>
+#include <Poco/Process.h>
+#include <Poco/SAX/InputSource.h>
+#include <Poco/StreamCopier.h>
+#include <Poco/StringTokenizer.h>
+#include <Poco/TemporaryFile.h>
+#include <Poco/ThreadPool.h>
+#include <Poco/URI.h>
+#include <Poco/Util/HelpFormatter.h>
+#include <Poco/Util/MapConfiguration.h>
+#include <Poco/Util/Option.h>
+#include <Poco/Util/OptionException.h>
+#include <Poco/Util/OptionSet.h>
+#include <Poco/Util/ServerApplication.h>
+
+#include "Common.hpp"
+
+using Poco::Environment;
+using Poco::Exception;
+using Poco::File;
+using Poco::Net::HTMLForm;
+using Poco::Net::HTTPRequest;
+using Poco::Net::HTTPRequestHandler;
+using Poco::Net::HTTPRequestHandlerFactory;
+using Poco::Net::HTTPResponse;
+using Poco::Net::HTTPServer;
+using Poco::Net::HTTPServerParams;
+using Poco::Net::HTTPServerRequest;
+using Poco::Net::HTTPServerResponse;
+using Poco::Net::MessageHeader;
+using Poco::Net::NameValueCollection;
+using Poco::Net::PartHandler;
+using Poco::Net::SecureServerSocket;
+using Poco::Net::ServerSocket;
+using Poco::Net::SocketAddress;
+using Poco::Net::StreamSocket;
+using Poco::Path;
+using Poco::Pipe;
+using Poco::Process;
+using Poco::ProcessHandle;
+using Poco::StreamCopier;
+using Poco::StringTokenizer;
+using Poco::TemporaryFile;
+using Poco::Thread;
+using Poco::ThreadPool;
+using Poco::URI;
+using Poco::Util::Application;
+using Poco::Util::HelpFormatter;
+using Poco::Util::IncompatibleOptionsException;
+using Poco::Util::MissingOptionException;
+using Poco::Util::Option;
+using Poco::Util::OptionSet;
+using Poco::Util::ServerApplication;
+using Poco::XML::AutoPtr;
+using Poco::XML::DOMParser;
+using Poco::XML::DOMWriter;
+using Poco::XML::Element;
+using Poco::XML::InputSource;
+using Poco::XML::NodeList;
+using Poco::XML::Node;
+
+int ClientPortNumber = DEFAULT_CLIENT_PORT_NUMBER;
+int MasterPortNumber = DEFAULT_MASTER_PORT_NUMBER;
+
+int main()
+{
+
+    return 0;
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
