663b250ed723d01a83e25febe8b7001e6d19efcf
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2017-02-25 18:54:55 -0500

    nb: move WebSocket upgrade to the ClientRequestDispatcher
    Change-Id: Id11b139563a55e50d3f7e216e2231d79e07015b3

--- net/WebSocketHandler.cpp
+++ net/WebSocketHandler.cpp
@@ -9,45 +9,4 @@
 
 #include "WebSocketHandler.hpp"
 
-#include <Poco/Net/WebSocket.h>
-
-void WebSocketHandler::upgradeToWebSocket(const Poco::Net::HTTPRequest& req)
-{
-    LOG_TRC("Upgrading to WebSocket");
-
-    // create our websocket goodness ...
-    _wsVersion = std::stoi(req.get("Sec-WebSocket-Version", "13"));
-    _wsKey = req.get("Sec-WebSocket-Key", "");
-    _wsProtocol = req.get("Sec-WebSocket-Protocol", "chat");
-    // FIXME: other sanity checks ...
-    LOG_INF("WebSocket version " << _wsVersion << " key '" << _wsKey << "'.");
-
-    std::ostringstream oss;
-    oss << "HTTP/1.1 101 Switching Protocols\r\n"
-        << "Upgrade: websocket\r\n"
-        << "Connection: Upgrade\r\n"
-        << "Sec-Websocket-Accept: " << computeAccept(_wsKey) << "\r\n"
-        << "\r\n";
-    std::string str = oss.str();
-    _socket->_outBuffer.insert(_socket->_outBuffer.end(), str.begin(), str.end());
-}
-
-namespace {
-
-/// To make the protected 'computeAccept' accessible.
-class PublicComputeAccept : public Poco::Net::WebSocket {
-public:
-    static std::string doComputeAccept(const std::string &key)
-    {
-        return computeAccept(key);
-    }
-};
-
-}
-
-std::string WebSocketHandler::computeAccept(const std::string &key)
-{
-    return PublicComputeAccept::doComputeAccept(key);
-}
-
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
