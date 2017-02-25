7c8c72e412a5885b3c4e2dd465ab7c06d6cac897
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2017-02-24 23:14:01 -0500

    nb: take DockBrokers lock where required
    Change-Id: I26a1a7f07b4f902dd9f7bbe7bf5eb2571a57099b

--- wsd/LOOLWSD.cpp
+++ wsd/LOOLWSD.cpp
@@ -2372,6 +2372,7 @@ class PlainSocketFactory : public SocketFactory
         // TODO FIXME loolnb - avoid the copy/paste between PlainSocketFactory
         // and SslSocketFactory
         // Request a kit process for this doc.
+        std::unique_lock<std::mutex> docBrokersLock(DocBrokersMutex);
         auto child = getNewChild();
         if (!child)
         {
@@ -2392,6 +2393,7 @@ class SslSocketFactory : public SocketFactory
         // TODO FIXME loolnb - avoid the copy/paste between PlainSocketFactory
         // and SslSocketFactory
         // Request a kit process for this doc.
+        std::unique_lock<std::mutex> docBrokersLock(DocBrokersMutex);
         auto child = getNewChild();
         if (!child)
         {
@@ -2674,16 +2676,18 @@ int LOOLWSD::main(const std::vector<std::string>& /*args*/)
             {
                 try
                 {
+#if 0
                     std::unique_lock<std::mutex> docBrokersLock(DocBrokersMutex);
                     cleanupDocBrokers();
                     for (auto& pair : DocBrokers)
                     {
                         auto docLock = pair.second->getDeferredLock();
-                        if (docLock.try_lock())
+                        if (doclock.try_lock())
                         {
-                            pair.second->autoSave(false, 0, docLock);
+                            pair.second->autosave(false, 0, doclock);
                         }
                     }
+#endif
                 }
                 catch (const std::exception& exc)
                 {
