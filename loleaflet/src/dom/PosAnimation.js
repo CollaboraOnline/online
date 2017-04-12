44482615ca3d355d8637ad6c04082fd1f144e10d
Henry Castro <hcastro@collabora.com>
2017-04-12 17:17:50 -0400

    loleaflet: update PosAnimation.js file
    Change-Id: I38a2643e67d1d341b486c987eb73dc5e5608a7cb

--- loleaflet/src/dom/PosAnimation.js
+++ loleaflet/src/dom/PosAnimation.js
@@ -1,71 +1,96 @@
 /*
- * L.PosAnimation powers Leaflet pan animations internally.
+ * L.PosAnimation is used by Leaflet internally for pan animations.
  */
 
-L.PosAnimation = L.Evented.extend({
+L.PosAnimation = L.Class.extend({
+	includes: L.Mixin.Events,
 
 	run: function (el, newPos, duration, easeLinearity) { // (HTMLElement, Point[, Number, Number])
 		this.stop();
 
 		this._el = el;
 		this._inProgress = true;
-		this._duration = duration || 0.25;
-        // workaround for Leaflet issue #3320
-		this._duration = 0;
-		this._easeOutPower = 1 / Math.max(easeLinearity || 0.5, 0.2);
-
-		this._startPos = L.DomUtil.getPosition(el);
-		this._offset = newPos.subtract(this._startPos);
-		this._startTime = +new Date();
+		this._newPos = newPos;
 
 		this.fire('start');
 
-		this._animate();
+		el.style[L.DomUtil.TRANSITION] = 'all ' + (duration || 0.25) +
+		        's cubic-bezier(0,0,' + (easeLinearity || 0.5) + ',1)';
+
+		L.DomEvent.on(el, L.DomUtil.TRANSITION_END, this._onTransitionEnd, this);
+		L.DomUtil.setPosition(el, newPos);
+
+		// toggle reflow, Chrome flickers for some reason if you don't do this
+		L.Util.falseFn(el.offsetWidth);
+
+		// there's no native way to track value updates of transitioned properties, so we imitate this
+		this._stepTimer = setInterval(L.bind(this._onStep, this), 50);
 	},
 
 	stop: function () {
 		if (!this._inProgress) { return; }
 
-		this._step(true);
-		this._complete();
+		// if we just removed the transition property, the element would jump to its final position,
+		// so we need to make it stay at the current position
+
+		L.DomUtil.setPosition(this._el, this._getPos());
+		this._onTransitionEnd();
+		L.Util.falseFn(this._el.offsetWidth); // force reflow in case we are about to start a new animation
 	},
 
-	_animate: function () {
-		// animation loop
-		this._animId = L.Util.requestAnimFrame(this._animate, this);
-		this._step();
-	},
-
-	_step: function (round) {
-		var elapsed = (+new Date()) - this._startTime,
-		    duration = this._duration * 1000;
-
-		if (elapsed < duration) {
-			this._runFrame(this._easeOut(elapsed / duration), round);
-		} else {
-			this._runFrame(1);
-			this._complete();
+	_onStep: function () {
+		var stepPos = this._getPos();
+		if (!stepPos) {
+			this._onTransitionEnd();
+			return;
 		}
-	},
-
-	_runFrame: function (progress, round) {
-		var pos = this._startPos.add(this._offset.multiplyBy(progress));
-		if (round) {
-			pos._round();
-		}
-		L.DomUtil.setPosition(this._el, pos);
+		/*eslint-disable camelcase*/
+		// make L.DomUtil.getPosition return intermediate position value during animation
+		this._el._leaflet_pos = stepPos;
+		/*eslint-enable camelcase*/
 
 		this.fire('step');
 	},
 
-	_complete: function () {
-		L.Util.cancelAnimFrame(this._animId);
+	// you can't easily get intermediate values of properties animated with CSS3 Transitions,
+	// we need to parse computed style (in case of transform it returns matrix string)
 
-		this._inProgress = false;
-		this.fire('end');
+	_transformRe: /([-+]?(?:\d*\.)?\d+)\D*, ([-+]?(?:\d*\.)?\d+)\D*\)/,
+
+	_getPos: function () {
+		var left, top, matches,
+		    el = this._el,
+		    style = window.getComputedStyle(el);
+
+		if (L.Browser.any3d) {
+			matches = style[L.DomUtil.TRANSFORM].match(this._transformRe);
+			if (!matches) { return; }
+			left = parseFloat(matches[1]);
+			top  = parseFloat(matches[2]);
+		} else {
+			left = parseFloat(style.left);
+			top  = parseFloat(style.top);
+		}
+
+		return new L.Point(left, top, true);
 	},
 
-	_easeOut: function (t) {
-		return 1 - Math.pow(1 - t, this._easeOutPower);
+	_onTransitionEnd: function () {
+		L.DomEvent.off(this._el, L.DomUtil.TRANSITION_END, this._onTransitionEnd, this);
+
+		if (!this._inProgress) { return; }
+		this._inProgress = false;
+
+		this._el.style[L.DomUtil.TRANSITION] = '';
+
+		/*eslint-disable camelcase*/
+		// make sure L.DomUtil.getPosition returns the final position value after animation
+		this._el._leaflet_pos = this._newPos;
+		/*eslint-enable camelcase*/
+
+		clearInterval(this._stepTimer);
+
+		this.fire('step').fire('end');
 	}
+
 });
