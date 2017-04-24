65ce26fe1433206af3a4eb5172409ff9e575a8ef
Pranav Kant <pranavk@collabora.co.uk>
2017-04-24 19:02:20 +0530

    loleaflet: Trigger layout algorithm on scrollend
    This is just a quick workaround to circumvent the problem where
    annotations dissappear when document is scrolled swiftly using the
    mousewheel.
    
    Change-Id: I51df6cecb770e5542ecdad765a5f0ccb6ec1eb98
    (cherry picked from commit 0b419faf107383957ce4d6970f459f20061c9d5b)
    Reviewed-on: https://gerrit.libreoffice.org/36889
    Reviewed-by: Jan Holesovsky <kendy@collabora.com>
    Tested-by: Jan Holesovsky <kendy@collabora.com>

--- loleaflet/src/control/Control.Scroll.js
+++ loleaflet/src/control/Control.Scroll.js
@@ -116,6 +116,12 @@ L.Control.Scroll = L.Control.extend({
 		}
 		this._prevScrollY = -e.mcs.top;
 		this._prevScrollX = -e.mcs.left;
+		// Scrolling quickly via mousewheel messes up the annotations for some reason
+		// Triggering the layouting algorithm here, though unnecessary, fixes the problem.
+		// This is just a workaround till we find the root cause of why it messes up the annotations
+		if (this._map._docLayer._annotations.layout) {
+			this._map._docLayer._annotations.layout();
+		}
 	},
 
 	_onScrollTo: function (e) {
