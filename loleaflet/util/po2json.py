21192c4306d8b6c5e2bc0db04c2f0a3b18f8de06
Jan Holesovsky <kendy@collabora.com>
2017-05-05 11:18:26 +0200

    Generate the l10n json files only when the appropriate po's change.
    Change-Id: I17c0f022775ec631e6493ba81fa41752f6841188

--- loleaflet/util/po2json.py
+++ loleaflet/util/po2json.py
@@ -13,6 +13,7 @@ import sys
 
 parser = optparse.OptionParser(usage="usage: %prog [options] pofile...")
 parser.add_option("--quiet", action="store_false", default=True, dest="verbose", help="don't print status messages to stdout")
+parser.add_option("-o", type="string", default="", dest="destfile", help="output file name (if there is exactly one input file)")
 
 (options, args) = parser.parse_args()
 
@@ -20,11 +21,18 @@ if args == None or len(args) == 0:
 	print("ERROR: you must specify at least one po file to translate");
 	sys.exit(1)
 
+if options.destfile != '' and len(args) != 1:
+	print("ERROR: when -o is provided, there has to be exactly 1 input file")
+	sys.exit(1)
+
 paramFix = re.compile("(\\(([0-9])\\))")
 
 for srcfile in args:
 
 	destfile = os.path.splitext(srcfile)[0] + ".json"
+	if options.destfile != '':
+		destfile = options.destfile
+
 	if options.verbose:
 		print("INFO: converting %s to %s" % (srcfile, destfile))
 
