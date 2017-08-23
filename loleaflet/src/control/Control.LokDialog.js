0ff2e262138ae61264143e7487f55689d081f2ea
Pranav Kant <pranavk@collabora.co.uk>
2017-08-23 19:12:48 +0530

    lokdialog: open, close, paint and invalidate the dialog systematically
    This makes simple dialog like word count dialog, which do not require
    any key/mouse interaction with the user, work correctly -
    responding to invalidation callbacks and painting it again.
    
    Change-Id: I3eee49751ec7815c8a26a687fc1f3915911d6cd4

--- loleaflet/src/control/Control.LokDialog.js
+++ loleaflet/src/control/Control.LokDialog.js
@@ -5,26 +5,67 @@
 /* global vex $ */
 L.Control.LokDialog = L.Control.extend({
 	onAdd: function (map) {
-		// TODO: Better distinction between warnings and errors
+		map.on('dialogpaint', this._onDialogPaint, this);
 		map.on('dialog', this._onDialogMsg, this);
 	},
 
-	_onDialogMsg: function (e) {
-		var dialogId = e.id.replace('.uno:', '');
+	_dialogs: {},
+
+	_isOpen: function(dialogId) {
+		return this._dialogs[dialogId];
+	},
+
+	_onDialogMsg: function(e) {
+		if (e.action === 'invalidate') {
+			// FIXME: core sends a different id for spelling dialog in 'invalidate' cb
+			if (e.dialogId === 'SpellingDialog')
+				e.dialogId = 'SpellingAndGrammarDialog';
+
+			// ignore any invalidate callbacks when we have closed the dialog
+			if (this._isOpen(e.dialogId))
+				this._map.sendDialogCommand(e.dialogId);
+		}
+	},
+
+	_openDialog: function(dialogId, width, height) {
 		var content = '<div class="lokdialog_container" id="' + dialogId + '">' +
-		    '<img class="lokdialog_content" src= ' + e.dialog + '></div>';
+		    '<img class="lokdialog_content" width="' + width + '" height="' + height + '"></div>';
 		$(document.body).append(content);
+		var that = this;
 		$('#' + dialogId).dialog({
-			width: e.width,
+			width: width,
 			height: 'auto',
 			title: 'LOK Dialog', // TODO: Get the 'real' dialog title from the backend
 			modal: false,
 			closeOnEscape: true,
 			resizable: false,
-			close: function(e, ui) {
-				$('#' + dialogId).remove();
+			close: function() {
+				that._onDialogClose(dialogId);
 			}
 		});
+
+		this._dialogs[dialogId] = true;
+	},
+
+	_onDialogClose: function(dialogId) {
+		$('#' + dialogId).remove();
+		this._dialogs[dialogId] = false;
+	},
+
+	_paintDialog: function(dialogId, img) {
+		if (!this._isOpen(dialogId))
+			return;
+
+		$('#' + dialogId + ' > .lokdialog_content').attr('src', img);
+	},
+
+	_onDialogPaint: function (e) {
+		var dialogId = e.id.replace('.uno:', '');
+		if (!this._isOpen(dialogId)) {
+			this._openDialog(dialogId, e.width, e.height);
+		}
+
+		this._paintDialog(dialogId, e.dialog);
 	}
 });
 
