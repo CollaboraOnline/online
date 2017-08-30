CollaboraOnline/online

commit 9a75040bf02e215135ca3f5366c171561eee69ea

tools: avoid string concatenation resulting in allocation of unnecessary temporary strings
By using std::stringstream instead.

Change-Id: I6fe12afd4adc13166746b1d98bf8ea75a28208e5

tools/Config.cpp
@@ -206,12 +206,12 @@ int Config::main(const std::vector<std::string>& args)
                 stream << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(pwdhash[j]);
             const std::string passwordHash = stream.str();
 
-            const std::string pwdConfigValue = "pbkdf2.sha512." +
-                                                std::to_string(_adminConfig.pwdIterations) + "." +
-                                                saltHash + "." + passwordHash;
+            std::stringstream pwdConfigValue("pbkdf2.sha512.");
+            pwdConfigValue << std::to_string(_adminConfig.pwdIterations) << ".";
+            pwdConfigValue << saltHash << "." << passwordHash;
             _loolConfig.setString("admin_console.secure_password[@desc]",
                                   "Salt and password hash combination generated using PBKDF2 with SHA512 digest.");
-            _loolConfig.setString("admin_console.secure_password", pwdConfigValue);
+            _loolConfig.setString("admin_console.secure_password", pwdConfigValue.str());
 
             std::cout << "Saving configuration to : " << ConfigFile << " ..." << std::endl;
             _loolConfig.save(ConfigFile);

commit 08efd75e3154bb8c00aeffe20311a0e0c5557bc7

wsd: fix incorrect password hash format written to config file
Regression from 9a75040bf02e215135ca3f5366c171561eee69ea

Change-Id: I82cd3db17a18702a046973cfe863fdda750b363b

tools/Config.cpp
@@ -11,6 +11,7 @@
 
 #include <iostream>
 #include <iomanip>
+#include <sstream>
 #include <termios.h>
 
 #include <openssl/rand.h>
@@ -207,7 +208,7 @@ int Config::main(const std::vector<std::string>& args)
                 stream << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(pwdhash[j]);
             const std::string passwordHash = stream.str();
 
-            std::stringstream pwdConfigValue("pbkdf2.sha512.");
+            std::stringstream pwdConfigValue("pbkdf2.sha512.", std::ios_base::in | std::ios_base::out | std::ios_base::ate);
             pwdConfigValue << std::to_string(_adminConfig.pwdIterations) << ".";
             pwdConfigValue << saltHash << "." << passwordHash;
             _loolConfig.setString("admin_console.secure_password[@desc]",