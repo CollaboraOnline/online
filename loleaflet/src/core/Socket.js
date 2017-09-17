e40ed9dce77d04a980e4ebf463e9980f84df4f57
Henry Castro <hcastro@collabora.com>
2017-09-17 11:09:02 -0400

    loleaflet: show error when the document is disconnected
    Change-Id: I43f8d48002ccb2fc3b6ac9807b72ba6b618d72d2

--- loleaflet/src/core/Socket.js
+++ loleaflet/src/core/Socket.js
@@ -260,6 +260,10 @@ L.Socket = L.Class.extend({
 			else if (textMsg === 'shuttingdown') {
 				msg = _('Server is shutting down for maintenance (auto-saving)');
 			}
+			else if (textMsg === 'docdisconnected') {
+				msg = _('Oops, there is a problem connecting the document');
+				postMsgData['Reason'] = 'DocumentDisconnected';
+			}
 			else if (textMsg === 'recycling') {
 				msg = _('Server is recycling and will be available shortly');
 
