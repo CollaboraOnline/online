18867cc98fa42462c1a99bf9a258d2b2cc29704b
Henry Castro <hcastro@collabora.com>
2017-10-21 13:42:35 -0400

    loleaflet: enable Calc spell checking
    Change-Id: I9293335e1da7a3ad35d6ed03caf8289e2f26d066

--- loleaflet/src/control/Control.Menubar.js
+++ loleaflet/src/control/Control.Menubar.js
@@ -298,7 +298,9 @@ L.Control.Menubar = L.Control.extend({
 				{name: _('Delete column'), type: 'unocommand', uno: '.uno:DeleteColumns'}]
 			},
 			{name: _('Tools'), id: 'tools', type: 'menu', menu: [
-				{name: _('Automatic spell checking'), type: 'unocommand', uno: '.uno:SpellOnline'}
+				{name: _('Automatic spell checking'), type: 'unocommand', uno: '.uno:SpellOnline'},
+				{name: _('Language for entire document'), type: 'menu', menu: [
+					{name: _('None (Do not check spelling)'), id: 'nonelanguage', type: 'unocommand', uno: '.uno:LanguageStatus?Language:string=Default_LANGUAGE_NONE'}]}
 			]},
 			{name: _('Help'), id: 'help', type: 'menu', menu: [
 				{name: _('Keyboard shortcuts'), id: 'keyboard-shortcuts', type: 'action'},
