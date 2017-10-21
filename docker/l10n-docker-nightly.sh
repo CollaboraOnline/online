7c3d7560c9b2f90048790ff9834a03fe071d4d6c
Andras Timar <andras.timar@collabora.com>
2017-10-21 23:12:44 +0200

    Docker bits
    Change-Id: I46c64573381ddb8d34817d393967316e0aac8576

---
+++ docker/l10n-docker-nightly.sh
@@ -0,0 +1,130 @@
+#! /bin/bash
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# check we can sudo without asking a pwd
+echo "Trying if sudo works without a password"
+echo
+echo "If you get a password prompt now, break, and fix your setup using 'sudo visudo'; add something like:"
+echo "yourusername ALL=(ALL) NOPASSWD: ALL"
+echo
+sudo echo "works"
+
+# check if we have jake
+which jake || { cat << EOF
+
+jake is not installed, get it like:
+
+  npm install -g jake
+EOF
+exit 1 ; }
+
+# do everything in the builddir
+SRCDIR=$(realpath `dirname $0`)
+INSTDIR="$SRCDIR/instdir"
+BUILDDIR="$SRCDIR/builddir"
+
+mkdir -p "$BUILDDIR"
+cd "$BUILDDIR"
+
+rm -rf "$INSTDIR"
+mkdir -p "$INSTDIR"
+
+##### cloning & updating #####
+
+# libreoffice repo
+if test ! -d libreoffice ; then
+    git clone git://anongit.freedesktop.org/libreoffice/core libreoffice || exit 1
+    cat > libreoffice/autogen.input << EOF
+--disable-cups
+--disable-dbus
+--disable-dconf
+--disable-epm
+--disable-evolution2
+--disable-ext-nlpsolver
+--disable-ext-wiki-publisher
+--disable-firebird-sdbc
+--disable-gio
+--disable-gstreamer-0-10
+--disable-gstreamer-1-0
+--disable-gtk
+--disable-gtk3
+--disable-kde4
+--disable-odk
+--disable-online-update
+--disable-pdfimport
+--disable-postgresql-sdbc
+--disable-report-builder
+--disable-scripting-beanshell
+--disable-scripting-javascript
+--disable-sdremote
+--disable-sdremote-bluetooth
+--enable-extension-integration
+--enable-mergelibs
+--enable-python=internal
+--enable-release-build
+--with-external-dict-dir=/usr/share/hunspell
+--with-external-hyph-dir=/usr/share/hyphen
+--with-external-thes-dir=/usr/share/mythes
+--with-fonts
+--with-galleries=no
+--with-lang=
+--with-linker-hash-style=both
+--with-system-dicts
+--with-system-zlib
+--with-theme=tango
+--without-branding
+--without-help
+--without-java
+--without-junit
+--without-myspell-dicts
+--without-package-format
+--without-system-jars
+--without-system-jpeg
+--without-system-libpng
+--without-system-libxml
+--without-system-openssl
+--without-system-poppler
+--without-system-postgresql
+EOF
+
+    ( cd libreoffice && ./autogen.sh ) || exit 1
+fi
+
+( cd libreoffice && git checkout master && git pull -r ) || exit 1
+
+# online repo
+if test ! -d online ; then
+    git clone git://anongit.freedesktop.org/libreoffice/online online || exit 1
+    ( cd online && ./autogen.sh ) || exit 1
+fi
+
+( cd online && git checkout -f master && git pull -r ) || exit 1
+
+##### LibreOffice #####
+
+# build LibreOffice
+( cd libreoffice && make ) || exit 1
+
+# copy stuff
+mkdir -p "$INSTDIR"/opt/
+cp -a libreoffice/instdir "$INSTDIR"/opt/libreoffice
+
+##### loolwsd & loleaflet #####
+
+# build
+( cd online && ./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --enable-silent-rules --with-lokit-path="$BUILDDIR"/libreoffice/include --with-lo-path="$INSTDIR"/opt/libreoffice ) || exit 1
+( cd online/loleaflet/po && ../../scripts/downloadpootle.sh )
+( cd online && make -j 8) || exit 1
+
+# copy stuff
+( cd online && DESTDIR="$INSTDIR" make install ) || exit 1
+
+# Create new docker image
+
+cd "$SRCDIR"
+docker build --no-cache -t libreoffice/online:master . || exit 1
+docker push libreoffice/online:master || exit 1
---
+++ docker/scripts/run-lool.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+export LC_CTYPE=en_US.UTF-8
+
+# Fix lool resolv.conf problem (wizdude)
+rm /opt/lool/systemplate/etc/resolv.conf
+ln -s /etc/resolv.conf /opt/lool/systemplate/etc/resolv.conf
+
+if test "${DONT_GEN_SSL_CERT-set}" == set; then
+# Generate new SSL certificate instead of using the default
+mkdir -p /opt/ssl/
+cd /opt/ssl/
+mkdir -p certs/ca
+openssl genrsa -out certs/ca/root.key.pem 2048
+openssl req -x509 -new -nodes -key certs/ca/root.key.pem -days 9131 -out certs/ca/root.crt.pem -subj "/C=DE/ST=BW/L=Stuttgart/O=Dummy Authority/CN=Dummy Authority"
+mkdir -p certs/{servers,tmp}
+mkdir -p "certs/servers/localhost"
+openssl genrsa -out "certs/servers/localhost/privkey.pem" 2048 -key "certs/servers/localhost/privkey.pem"
+openssl req -key "certs/servers/localhost/privkey.pem" -new -sha256 -out "certs/tmp/localhost.csr.pem" -subj "/C=DE/ST=BW/L=Stuttgart/O=Dummy Authority/CN=localhost"
+openssl x509 -req -in certs/tmp/localhost.csr.pem -CA certs/ca/root.crt.pem -CAkey certs/ca/root.key.pem -CAcreateserial -out certs/servers/localhost/cert.pem -days 9131
+mv certs/servers/localhost/privkey.pem /etc/loolwsd/key.pem
+mv certs/servers/localhost/cert.pem /etc/loolwsd/cert.pem
+mv certs/ca/root.crt.pem /etc/loolwsd/ca-chain.cert.pem
+fi
+
+# Replace trusted host
+perl -pi -e "s/localhost<\/host>/${domain}<\/host>/g" /etc/loolwsd/loolwsd.xml
+perl -pi -e "s/<username desc=\"The username of the admin console. Must be set.\"><\/username>/<username desc=\"The username of the admin console. Must be set.\">${username}<\/username>/" /etc/loolwsd/loolwsd.xml
+perl -pi -e "s/<password desc=\"The password of the admin console. Must be set.\"><\/password>/<password desc=\"The password of the admin console. Must be set.\">${password}<\/password>/g" /etc/loolwsd/loolwsd.xml
+
+# Start loolwsd
+su -c "/usr/bin/loolwsd --version --o:sys_template_path=/opt/lool/systemplate --o:lo_template_path=/opt/libreoffice --o:child_root_path=/opt/lool/child-roots --o:file_server_root_path=/usr/share/loolwsd" -s /bin/bash lool
