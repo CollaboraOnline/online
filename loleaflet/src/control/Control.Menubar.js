a98727b536bc5232f1a78a8147034529f6c483ac
Aron Budea <aron.budea@collabora.com>
2017-10-26 01:27:57 +0200

    Add 'Repair' menu to spreadsheet/presentation.
    Change-Id: I7651418c79c711940438ada53d64970e7674d077
    Reviewed-on: https://gerrit.libreoffice.org/43859
    Reviewed-by: Jan Holesovsky <kendy@collabora.com>
    Tested-by: Jan Holesovsky <kendy@collabora.com>

--- loleaflet/src/control/Control.Menubar.js
+++ loleaflet/src/control/Control.Menubar.js
@@ -212,6 +212,7 @@ L.Control.Menubar = L.Control.extend({
 			{name: _('Edit'), type: 'menu', menu: [
 				{name: _('Undo'), type: 'unocommand', uno: '.uno:Undo'},
 				{name: _('Redo'), type: 'unocommand', uno: '.uno:Redo'},
+				{name: _('Repair'), id: 'repair',  type: 'action'},
 				{type: 'separator'},
 				{name: _('Cut'), type: 'unocommand', uno: '.uno:Cut'},
 				{name: _('Copy'), type: 'unocommand', uno: '.uno:Copy'},
@@ -279,6 +280,7 @@ L.Control.Menubar = L.Control.extend({
 			{name: _('Edit'), type: 'menu', menu: [
 				{name: _('Undo'), type: 'unocommand', uno: '.uno:Undo'},
 				{name: _('Redo'), type: 'unocommand', uno: '.uno:Redo'},
+				{name: _('Repair'), id: 'repair',  type: 'action'},
 				{type: 'separator'},
 				{name: _('Cut'), type: 'unocommand', uno: '.uno:Cut'},
 				{name: _('Copy'), type: 'unocommand', uno: '.uno:Copy'},
