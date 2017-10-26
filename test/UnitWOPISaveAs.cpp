d3bf2638ead1d66ca3a6a78b20528bd68242a4e0
Jan Holesovsky <kendy@collabora.com>
2017-10-26 12:17:05 +0200

    SaveAs: Fix unit test when used without ssl.
    Change-Id: Ic48908c9e7449fc13458d9211fa743310834bd6b

--- test/UnitWOPISaveAs.cpp
+++ test/UnitWOPISaveAs.cpp
@@ -40,7 +40,7 @@ public:
     bool filterSendMessage(const char* data, const size_t len, const WSOpCode /* code */, const bool /* flush */, int& /*unitReturn*/) override
     {
         std::string message(data, len);
-        if (message == "saveas: url=https://127.0.0.1:9980/something%20wopi/files/1?access_token=anything filename=hello%20world.txt")
+        if (message == "saveas: url=" + helpers::getTestServerURI() + "/something%20wopi/files/1?access_token=anything filename=hello%20world.txt")
         {
             // successfully exit the test if we also got the outgoing message
             // notifying about saving the file
