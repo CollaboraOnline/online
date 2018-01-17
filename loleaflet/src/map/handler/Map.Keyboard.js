CollaboraOnline/online

commit d1c8de9bf9021dedb67446439f117b1b64c7e2d9

loleaflet: Unify dialog key handling logic with document's
Change-Id: I422e813d76df9b52a860a05d09a89362dfb0b616

loleaflet/src/map/handler/Map.Keyboard.js
@@ -239,11 +239,15 @@ L.Map.Keyboard = L.Handler.extend({
 		return this.keymap[keyCode] || keyCode;
 	},
 
-	_onKeyDown: function (e) {
+	_onKeyDown: function (e, postEventFn) {
 		if (this._map.slideShow && this._map.slideShow.fullscreen) {
 			return;
 		}
 		var docLayer = this._map._docLayer;
+		if (!postEventFn) {
+			// default is to post keyboard events on the document
+			postEventFn = docLayer._postKeyboardEvent;
+		}
 		this.modifier = 0;
 		var shift = e.originalEvent.shiftKey ? this.keyModifier.shift : 0;
 		var ctrl = e.originalEvent.ctrlKey ? this.keyModifier.ctrl : 0;
@@ -313,7 +317,7 @@ L.Map.Keyboard = L.Handler.extend({
 		if (this.modifier) {
 			unoKeyCode |= this.modifier;
 			if (e.type !== 'keyup' && (this.modifier !== shift || (keyCode === 32 && !docLayer._isCursorVisible))) {
-				docLayer._postKeyboardEvent('input', charCode, unoKeyCode);
+				postEventFn.call(this, 'input', charCode, unoKeyCode);
 				e.originalEvent.preventDefault();
 				return;
 			}
@@ -327,7 +331,7 @@ L.Map.Keyboard = L.Handler.extend({
 				this._bufferedTextInputEvent = null;
 
 				if (this._handleOnKeyDown(keyCode, this.modifier) && charCode === 0) {
-					docLayer._postKeyboardEvent('input', charCode, unoKeyCode);
+					postEventFn.call(this, 'input', charCode, unoKeyCode);
 				}
 			}
 			else if ((e.type === 'keypress' || e.type === 'compositionend') &&
@@ -344,9 +348,11 @@ L.Map.Keyboard = L.Handler.extend({
 				}
 				if (e.type === 'compositionend') {
 					// Set all keycodes to zero
-					docLayer._postKeyboardEvents('input', compCharCodes, Array.apply(null, Array(compCharCodes.length)).map(Number.prototype.valueOf, 0));
+					for (var idx = 0; i < compCharCodes.length; ++i) {
+						postEventFn.call(this, 'input', compCharCodes[idx], 0);
+					}
 				} else {
-					docLayer._postKeyboardEvent('input', charCode, unoKeyCode);
+					postEventFn.call(this, 'input', charCode, unoKeyCode);
 				}
 
 				this._keyHandled = true;
@@ -375,13 +381,11 @@ L.Map.Keyboard = L.Handler.extend({
 				if (!this._keyHandled && this._bufferedTextInputEvent) {
 					var textInputData = this._bufferedTextInputEvent.originalEvent.data;
 					charCode = e.originalEvent.keyCode;
-					compCharCodes = [];
-					for (var idx = 0; i < textInputData.length; i++) {
-						compCharCodes.push(textInputData[idx].charCodeAt());
+					for (idx = 0; i < textInputData.length; i++) {
+						postEventFn.call(this, 'input', textInputData[idx].charCodeAt(), 0);
 					}
-					docLayer._postKeyboardEvents('input', compCharCodes, Array.apply(null, Array(compCharCodes.length)).map(Number.prototype.valueOf, 0));
 				}
-				docLayer._postKeyboardEvent('up', charCode, unoKeyCode);
+				postEventFn.call(this, 'up', charCode, unoKeyCode);
 
 				this._keyHandled = true;
 				this._bufferedTextInputEvent = null;
@@ -407,7 +411,7 @@ L.Map.Keyboard = L.Handler.extend({
 			else if (key in this._panKeys && e.originalEvent.shiftKey &&
 					docLayer._selections.getLayers().length !== 0) {
 				// if there is a selection and the user wants to modify it
-				docLayer._postKeyboardEvent('input', charCode, unoKeyCode);
+				postEventFn.call(this, 'input', charCode, unoKeyCode);
 			}
 			else if (key in this._zoomKeys) {
 				map.setZoom(map.getZoom() + (e.shiftKey ? 3 : 1) * this._zoomKeys[key]);

loleaflet/src/control/Control.LokDialog.js
@@ -224,7 +224,9 @@ L.Control.LokDialog = L.Control.extend({
 			this._postWindowMouseEvent(lokEventType, this._toRawDlgId(strDlgId), e.offsetX, e.offsetY, 1, buttons, 0);
 		}, this);
 		L.DomEvent.on(dialogCanvas, 'keyup keypress keydown', function(e) {
-			this._handleDialogKeyEvent(e, this._toRawDlgId(strDlgId));
+			// _onKeyDown fn below requires this kind of structure but leaflet DomEvent.on doesn't pass it
+			e.originalEvent = e;
+			map['keyboard']._onKeyDown(e, L.bind(this._postWindowKeyboardEvent, this, this._toRawDlgId(strDlgId)));
 		}, this);
 		L.DomEvent.on(dialogCanvas, 'contextmenu', function() {
 			return false;
@@ -242,42 +244,6 @@ L.Control.LokDialog = L.Control.extend({
 	_postWindowKeyboardEvent: function(winid, type, charcode, keycode) {
 		this._map._socket.sendMessage('windowkey id=' + winid + ' type=' + type +
 		                              ' char=' + charcode + ' key=' + keycode);
-	},
-
-	_handleDialogKeyEvent: function(e, winid) {
-		var docLayer = this._map._docLayer;
-		this.modifier = 0;
-		var shift = e.shiftKey ? this._map['keyboard'].keyModifier.shift : 0;
-		var ctrl = e.ctrlKey ? this._map['keyboard'].keyModifier.ctrl : 0;
-		var alt = e.altKey ? this._map['keyboard'].keyModifier.alt : 0;
-		var cmd = e.metaKey ? this._map['keyboard'].keyModifier.ctrl : 0;
-		this.modifier = shift | ctrl | alt | cmd;
-
-		var charCode = e.charCode;
-		var keyCode = e.keyCode;
-		var unoKeyCode = this._map['keyboard']._toUNOKeyCode(keyCode);
-
-		if (this.modifier) {
-			unoKeyCode |= this.modifier;
-			if (e.type !== 'keyup') {
-				this._postWindowKeyboardEvent(winid, 'input', charCode, unoKeyCode);
-				return;
-			}
-		}
-
-		if (e.type === 'keydown' && this._map['keyboard'].handleOnKeyDownKeys[keyCode]) {
-			this._postWindowKeyboardEvent(winid, 'input', charCode, unoKeyCode);
-		}
-		else if (e.type === 'keypress' && (!this._map['keyboard'].handleOnKeyDownKeys[keyCode] || charCode !== 0)) {
-			if (charCode === keyCode && charCode !== 13) {
-				keyCode = 0;
-				unoKeyCode = this._map['keyboard']._toUNOKeyCode(keyCode);
-			}
-			this._postWindowKeyboardEvent(winid, 'input', charCode, unoKeyCode);
-		}
-		else if (e.type === 'keyup') {
-			this._postWindowKeyboardEvent(winid, 'up', charCode, unoKeyCode);
-		}
 	},
 
 	_onDialogClose: function(dialogId, notifyBackend) {

loleaflet/src/layer/tile/TileLayer.js
@@ -1430,20 +1430,6 @@ L.TileLayer = L.GridLayer.extend({
 		}
 	},
 
-	_postKeyboardEvents: function(type, charcodes, keycodes) {
-		// Both are arrays
-		if (typeof(charcodes.length) !== 'number' && typeof(keycodes.length) !== 'number')
-			return;
-
-		// both have same length
-		if (charcodes.length !== keycodes.length)
-			return;
-
-		for (var i = 0; i < charcodes.length; i++) {
-			this._postKeyboardEvent(type, charcodes[i], keycodes[i]);
-		}
-	},
-
 	_postKeyboardEvent: function(type, charcode, keycode) {
 		if (this._docType === 'spreadsheet' && this._prevCellCursor && type === 'input') {
 			if (keycode === 1030) { // PgUp

commit 7f59b9ab8cf1b5e876859f1a3fcd1141d9a5265e

Ensure we have the docLayer object as 'this' in _postKeyboardEvent.
Interestingly, we can have a 'this' that is not this object, that
means that this._clientVisibleArea is undefined, so page-up/down
don't work.

Fixes regression introduced by d1c8de9bf9021dedb67446439f117b1b64c7e2d9

Change-Id: If83b3542f17c1856af18c5c302e9d566761aeb12

loleaflet/src/map/handler/Map.Keyboard.js
@@ -244,9 +244,11 @@ L.Map.Keyboard = L.Handler.extend({
 			return;
 		}
 		var docLayer = this._map._docLayer;
+		var eventObject;
 		if (!postEventFn) {
 			// default is to post keyboard events on the document
 			postEventFn = docLayer._postKeyboardEvent;
+			eventObject = docLayer;
 		}
 		this.modifier = 0;
 		var shift = e.originalEvent.shiftKey ? this.keyModifier.shift : 0;
@@ -317,7 +319,7 @@ L.Map.Keyboard = L.Handler.extend({
 		if (this.modifier) {
 			unoKeyCode |= this.modifier;
 			if (e.type !== 'keyup' && (this.modifier !== shift || (keyCode === 32 && !docLayer._isCursorVisible))) {
-				postEventFn.call(this, 'input', charCode, unoKeyCode);
+				postEventFn.call(eventObject, 'input', charCode, unoKeyCode);
 				e.originalEvent.preventDefault();
 				return;
 			}
@@ -331,7 +333,7 @@ L.Map.Keyboard = L.Handler.extend({
 				this._bufferedTextInputEvent = null;
 
 				if (this._handleOnKeyDown(keyCode, this.modifier) && charCode === 0) {
-					postEventFn.call(this, 'input', charCode, unoKeyCode);
+					postEventFn.call(eventObject, 'input', charCode, unoKeyCode);
 				}
 			}
 			else if ((e.type === 'keypress' || e.type === 'compositionend') &&
@@ -349,10 +351,10 @@ L.Map.Keyboard = L.Handler.extend({
 				if (e.type === 'compositionend') {
 					// Set all keycodes to zero
 					for (var idx = 0; i < compCharCodes.length; ++i) {
-						postEventFn.call(this, 'input', compCharCodes[idx], 0);
+						postEventFn.call(eventObject, 'input', compCharCodes[idx], 0);
 					}
 				} else {
-					postEventFn.call(this, 'input', charCode, unoKeyCode);
+					postEventFn.call(eventObject, 'input', charCode, unoKeyCode);
 				}
 
 				this._keyHandled = true;
@@ -382,10 +384,10 @@ L.Map.Keyboard = L.Handler.extend({
 					var textInputData = this._bufferedTextInputEvent.originalEvent.data;
 					charCode = e.originalEvent.keyCode;
 					for (idx = 0; i < textInputData.length; i++) {
-						postEventFn.call(this, 'input', textInputData[idx].charCodeAt(), 0);
+						postEventFn.call(eventObject, 'input', textInputData[idx].charCodeAt(), 0);
 					}
 				}
-				postEventFn.call(this, 'up', charCode, unoKeyCode);
+				postEventFn.call(eventObject, 'up', charCode, unoKeyCode);
 
 				this._keyHandled = true;
 				this._bufferedTextInputEvent = null;
@@ -411,7 +413,7 @@ L.Map.Keyboard = L.Handler.extend({
 			else if (key in this._panKeys && e.originalEvent.shiftKey &&
 					docLayer._selections.getLayers().length !== 0) {
 				// if there is a selection and the user wants to modify it
-				postEventFn.call(this, 'input', charCode, unoKeyCode);
+				postEventFn.call(eventObject, 'input', charCode, unoKeyCode);
 			}
 			else if (key in this._zoomKeys) {
 				map.setZoom(map.getZoom() + (e.shiftKey ? 3 : 1) * this._zoomKeys[key]);