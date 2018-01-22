31130989ed0bd665f31ad1e79613f5b41095706b
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2018-01-22 14:36:16 -0500

    ut: fix HTTPWSTest::testLoadTortureODP
    Change-Id: Ibe3bd98c1cd68da962cc8d93b837597fb7167f45
    Reviewed-on: https://gerrit.libreoffice.org/48644
    Reviewed-by: Ashod Nakashian <ashnakash@gmail.com>
    Tested-by: Ashod Nakashian <ashnakash@gmail.com>

--- test/httpwstest.cpp
+++ test/httpwstest.cpp
@@ -568,7 +568,7 @@ void HTTPWSTest::testLoadTortureODP()
 
     // For ODP the view-id is always odd, and we expect not to skip any ids.
     const int number_of_loads = thread_count;
-    const int exp_sum_view_ids = number_of_loads * number_of_loads; // Odd view-ids only.
+    const int exp_sum_view_ids = number_of_loads * (number_of_loads - 1) / 2; // 0-based view-ids.
     CPPUNIT_ASSERT_EQUAL(exp_sum_view_ids, sum_view_ids);
 }
 
