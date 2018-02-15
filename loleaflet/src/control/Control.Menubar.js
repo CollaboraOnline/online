1cfe416559951ad7079a176d4c02de9aa0c1e5fe
Aron Budea <aron.budea@collabora.com>
2018-02-15 07:30:14 +0100

    loleaflet: add download as RTF option
    Change-Id: Id258461ff49bcc6d6a0b1e4df826c184e769c5e1
    Reviewed-on: https://gerrit.libreoffice.org/49787
    Reviewed-by: pranavk <pranavk@collabora.co.uk>
    Tested-by: pranavk <pranavk@collabora.co.uk>

--- loleaflet/src/control/Control.Menubar.js
+++ loleaflet/src/control/Control.Menubar.js
@@ -23,7 +23,8 @@ L.Control.Menubar = L.Control.extend({
 					{name: _('PDF Document (.pdf)'), id: 'downloadas-pdf', type: 'action'},
 					{name: _('ODF text document (.odt)'), id: 'downloadas-odt', type: 'action'},
 					{name: _('Microsoft Word 2003 (.doc)'), id: 'downloadas-doc', type: 'action'},
-					{name: _('Microsoft Word (.docx)'), id: 'downloadas-docx', type: 'action'}]},
+					{name: _('Microsoft Word (.docx)'), id: 'downloadas-docx', type: 'action'},
+					{name: _('Rich Text (.rtf)'), id: 'downloadas-rtf', type: 'action'}]},
 				{type: 'separator'},
 				{uno: '.uno:SetDocumentProperties'}
 			]},
@@ -378,7 +379,7 @@ L.Control.Menubar = L.Control.extend({
 		allowedReadonlyMenus: ['file', 'downloadas', 'view', 'help'],
 
 		allowedViewModeActions: [
-			'downloadas-pdf', 'downloadas-odt', 'downloadas-doc', 'downloadas-docx', // file menu
+			'downloadas-pdf', 'downloadas-odt', 'downloadas-doc', 'downloadas-docx', 'downloadas-rtf', // file menu
 			'downloadas-odp', 'downloadas-ppt', 'downloadas-pptx', // file menu
 			'downloadas-ods', 'downloadas-xls', 'downloadas-xlsx', // file menu
 			'fullscreen', 'zoomin', 'zoomout', 'zoomreset', // view menu
