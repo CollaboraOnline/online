CollaboraOnline/online

commit 1e91799663945fdfde84385bcfbb96fd03c6fe2e

Move the IME candidate window with cursor position
Change-Id: I7b37b0c4981b4ebcefb7606915711ca2adec3c31

loleaflet/src/map/Map.js
@@ -692,7 +692,8 @@ L.Map = L.Evented.extend({
 			throw new Error('Map container is already initialized.');
 		}
 
-		var textAreaContainer = L.DomUtil.create('div', 'clipboard-container', container.parentElement);
+		var textAreaContainer = L.DomUtil.create('div', 'clipboard-container', container);
+		textAreaContainer.id = 'doc-clipboard-container';
 		this._textArea = L.DomUtil.create('input', 'clipboard', textAreaContainer);
 		this._textArea.setAttribute('type', 'text');
 		this._textArea.setAttribute('autocorrect', 'off');

loleaflet/dist/leaflet.css
@@ -679,7 +679,7 @@ div.leaflet-cursor-container:hover > .leaflet-cursor-header {
 }
 
 .clipboard-container {
-	position: fixed;
+	position: absolute;
 	left: 0px;
 	top: 0px;
 	width: 0px;

loleaflet/src/layer/tile/TileLayer.js
@@ -1534,6 +1534,11 @@ L.TileLayer = L.GridLayer.extend({
 				this._cursorMarker.setLatLng(cursorPos, pixBounds.getSize().multiplyBy(this._map.getZoomScale(this._map.getZoom())));
 			}
 			this._map.addLayer(this._cursorMarker);
+
+			// move the hidden input field with the cursor
+			var clipContainer = L.DomUtil.get('doc-clipboard-container');
+			var pos = this._map.latLngToLayerPoint(L.latLng(cursorPos)).round();
+			L.DomUtil.setPosition(clipContainer, pos);
 		}
 		else if (this._cursorMarker) {
 			this._map.removeLayer(this._cursorMarker);

commit 77a8f5b2e7652c4e6021f23d2dbb0dc450a7df37

Fix double handling of ctrl key
Regressed from 1e91799663945fdfde84385bcfbb96fd03c6fe2e

Thanks Aron for bisecting.

Change-Id: Ida2a62d74440083e747903562a0c9e6a5a550eb1

loleaflet/src/map/Map.js
@@ -692,7 +692,7 @@ L.Map = L.Evented.extend({
 			throw new Error('Map container is already initialized.');
 		}
 
-		var textAreaContainer = L.DomUtil.create('div', 'clipboard-container', container);
+		var textAreaContainer = L.DomUtil.create('div', 'clipboard-container', container.parentElement);
 		textAreaContainer.id = 'doc-clipboard-container';
 		this._textArea = L.DomUtil.create('input', 'clipboard', textAreaContainer);
 		this._textArea.setAttribute('type', 'text');