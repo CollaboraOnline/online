08ee839562c4f2e62570c9cecdbc0a6d3ce99b2f
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2018-02-22 14:50:26 +0100

    Improve message displayed when limit reached
    Change-Id: I659d3e5156bc4ae8e6b30a7fd9f9957dd1a868ec
    Reviewed-on: https://gerrit.libreoffice.org/50179
    Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
    Tested-by: Michael Meeks <michael.meeks@collabora.com>

--- loleaflet/dist/errormessages.js
+++ loleaflet/dist/errormessages.js
@@ -1,6 +1,6 @@
 exports.diskfull = _('No disk space left on server, please contact the server administrator to continue.');
 exports.emptyhosturl = _('The host URL is empty. The loolwsd server is probably misconfigured, please contact the administrator.');
-exports.limitreached = _('This is an unsupported version of {productname}. To avoid the impression that it is suitable for deployment in enterprises, this version is limited to {docs} documents, and {connections} connections.');
+exports.limitreached = _('This is an unsupported version of {productname}. To avoid the impression that it is suitable for deployment in enterprises, this message appears when more than {docs} documents or {connections} connections are in use concurrently');
 exports.infoandsupport = _('More information and support');
 exports.limitreachedprod = _('This service is limited to %0 documents, and %1 connections total by the admin. This limit has been reached. Please try again later.');
 exports.serviceunavailable = _('Service is unavailable. Please try again later and report to your administrator if the issue persists.');
