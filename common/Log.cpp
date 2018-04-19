3c395615a2f5e43e7fac6ccc83c4d705166fc6c1
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2018-04-19 08:35:23 -0400

    wsd: log the date as day/month before the time
    Change-Id: Ic2e6c5be12fe919965971c6d203862ee6e1b74f9
    Reviewed-on: https://gerrit.libreoffice.org/53157
    Reviewed-by: Ashod Nakashian <ashnakash@gmail.com>
    Tested-by: Ashod Nakashian <ashnakash@gmail.com>

--- common/Log.cpp
+++ common/Log.cpp
@@ -110,11 +110,11 @@ namespace Log
             osTid = Util::getThreadId();
             threadName = Util::getThreadName();
         }
-
         Poco::DateTime time;
-        snprintf(buffer, 1023, "%s-%.05lu %.2u:%.2u:%.2u.%.6u [ %s ] %s  ",
+        snprintf(buffer, 1023, "%s-%.05lu %.2u/%.2u %.2u:%.2u:%.2u.%.6u [ %s ] %s  ",
                     (Source.inited ? Source.id.c_str() : "<shutdown>"),
                     osTid,
+                    time.day(), time.month(),
                     time.hour(), time.minute(), time.second(),
                     time.millisecond() * 1000 + time.microsecond(),
                     threadName, level);
