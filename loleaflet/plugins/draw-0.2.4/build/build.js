8e2d0d2f5e427d137a5f652928ac381a38e9c6a7
Henry Castro <hcastro@collabora.com>
2018-05-27 00:26:59 -0400

    loleaflet: remove unused build code
    Change-Id: I43dc41ba73ed9aac20f427cb5ff81d971849f547
    (cherry picked from commit 8002a4cb9b17df9578412d1eb1967164b81c8a63)
    Signed-off-by: Andras Timar <andras.timar@collabora.com>

--- loleaflet/plugins/draw-0.2.4/build/build.js
+++ loleaflet/plugins/draw-0.2.4/build/build.js
@@ -1,8 +1,4 @@
-var fs = require('fs'),
-	UglifyJS = require('uglify-js'),
-	zlib = require('zlib'),
-	builddir = process.env.builddir ? process.env.builddir + '/' : '',
-	deps = require('./deps.js').deps;
+var deps = require('./deps.js').deps;
 
 function getFiles(compsBase32) {
 	var memo = {},
@@ -44,104 +40,6 @@ function getFiles(compsBase32) {
 }
 
 exports.getFiles = getFiles;
-
-function getSizeDelta(newContent, oldContent, fixCRLF) {
-		if (!oldContent) {
-				return ' (new)';
-		}
-		if (newContent === oldContent) {
-				return ' (unchanged)';
-		}
-		if (fixCRLF) {
-				newContent = newContent.replace(/\r\n?/g, '\n');
-				oldContent = oldContent.replace(/\r\n?/g, '\n');
-		}
-		var delta = newContent.length - oldContent.length;
-
-		return delta === 0 ? '' : ' (' + (delta > 0 ? '+' : '') + delta + ' bytes)';
-}
-
-function loadSilently(path) {
-		try {
-				return fs.readFileSync(path, 'utf8');
-		} catch (e) {
-				return null;
-		}
-}
-
-function combineFiles(files) {
-		var content = '';
-		for (var i = 0, len = files.length; i < len; i++) {
-				content += fs.readFileSync(files[i], 'utf8') + '\n\n';
-		}
-		return content;
-}
-
-function bytesToKB(bytes) {
-	return (bytes / 1024).toFixed(2) + ' KB';
-}
-
-exports.build = function (callback, compsBase32, buildName) {
-	var files = getFiles(compsBase32);
-
-	console.log('Concatenating and compressing ' + files.length + ' files...');
-
-	var copy = fs.readFileSync('src/copyright.js', 'utf8'),
-		intro = '(function (window, document, undefined) {',
-		outro = '}(window, document));',
-		newSrc = copy + intro + combineFiles(files) + outro,
-
-		pathPart = builddir + 'dist/leaflet.draw' + (buildName ? '-' + buildName : ''),
-		srcPath = pathPart + '-src.js',
-
-		oldSrc = loadSilently(srcPath),
-		srcDelta = getSizeDelta(newSrc, oldSrc, true);
-
-	console.log('\tUncompressed: ' + bytesToKB(newSrc.length) + srcDelta);
-
-	if (srcDelta === ' (unchanged)') {
-		return;
-	}
-
-	fs.writeFileSync(srcPath, newSrc);
-	console.log('\tSaved to ' + srcPath);
-
-	var path = pathPart + '.js',
-		oldCompressed = loadSilently(path),
-		newCompressed = copy + UglifyJS.minify(newSrc, {
-			warnings: true,
-			fromString: true
-		}).code,
-		delta = getSizeDelta(newCompressed, oldCompressed);
-
-	console.log('\tCompressed: ' + bytesToKB(newCompressed.length) + delta);
-
-	var newGzipped,
-		gzippedDelta = '';
-
-	function done() {
-		if (newCompressed !== oldCompressed) {
-			fs.writeFileSync(path, newCompressed);
-			console.log('\tSaved to ' + path);
-		}
-		console.log('\tGzipped: ' + bytesToKB(newGzipped.length) + gzippedDelta);
-		callback();
-	}
-
-	zlib.gzip(newCompressed, function (err, gzipped) {
-		if (err) { return; }
-		newGzipped = gzipped;
-		if (oldCompressed && (oldCompressed !== newCompressed)) {
-			zlib.gzip(oldCompressed, function (err, oldGzipped) {
-				if (err) { return; }
-				gzippedDelta = getSizeDelta(gzipped, oldGzipped);
-				done();
-			});
-		} else {
-			done();
-		}
-	});
-};
 
 exports.test = function(callback) {
 		var karma = require('karma'),
