13f3b7848f9cd77dc7a1e82f0fba656dbd503b4c
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2018-06-23 19:52:27 -0400

    configure: support disabling anonymization
    And inform the user of the anonymization level.
    
    Change-Id: I95cf832d5c4103744207214ffbf4e85d177ff190
    Reviewed-on: https://gerrit.libreoffice.org/56567
    Reviewed-by: Jan Holesovsky <kendy@collabora.com>
    Tested-by: Jan Holesovsky <kendy@collabora.com>

--- wsd/LOOLWSD.cpp
+++ wsd/LOOLWSD.cpp
@@ -776,14 +776,14 @@ void LOOLWSD::initialize(Application& self)
     }
 
     // Get anonymization settings.
-#ifdef LOOLWSD_ANONYMIZE_USERNAMES
+#if LOOLWSD_ANONYMIZE_USERNAMES
     AnonymizeUsernames = true;
 #else
     AnonymizeUsernames = getConfigValue<bool>(conf, "logging.anonymize.usernames", false);
 #endif
     setenv("LOOL_ANONYMIZE_USERNAMES", AnonymizeUsernames ? "1" : "0", true);
 
-#ifdef LOOLWSD_ANONYMIZE_FILENAMES
+#if LOOLWSD_ANONYMIZE_FILENAMES
     AnonymizeFilenames = true;
 #else
     AnonymizeFilenames = getConfigValue<bool>(conf, "logging.anonymize.filenames", false);
