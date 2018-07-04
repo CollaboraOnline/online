1800a8ee172ce60a1f5361d14ddf49e818dc7513
Tor Lillqvist <tml@collabora.com>
2018-07-04 15:59:09 +0300

    Decide whether to display a ruler  based on ruler percentage of window height
    If the ruler would be over 5% of the window height, don't display it.
    
    Change-Id: Ieed55318fc76dfe0f9248985b816e306b6654dee

--- loleaflet/src/map/Map.js
+++ loleaflet/src/map/Map.js
@@ -118,9 +118,11 @@ L.Map = L.Evented.extend({
 			if (!this.initComplete) {
 				this._fireInitComplete('doclayerinit');
 			}
-			// Don't show the ruler if the window is small. Use 1280 pixels as the
-			// fairly arbitrary limit.
-			if ($(window).height() >= 1280 && this._docLayer._docType == 'text') {
+			// Don't show the ruler if the window is small. Use 5% of window height as
+			// the fairly arbitrary limit how high the ruler can be. We "know" (from
+			// leaflet.css, .loleaflet-ruler, height) that the ruler height is 20px.
+			// That makes the limit 400px.
+			if ($(window).height() >= 400 && this._docLayer._docType == 'text') {
 				var interactiveRuler = this._permission === 'edit' ? true : false;
 				L.control.ruler({position:'topleft', interactive:interactiveRuler}).addTo(this);
 			}
