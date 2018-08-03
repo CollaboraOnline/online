d2610ce1a4a69dfac7e59645a7dd8386c7799903
Andras Timar <andras.timar@collabora.com>
2018-08-03 13:09:16 +0200

    loleaflet: fix l10n of context menus
    Change-Id: I96ea9d572288796587bc2be194c94c83a9b53f8d
    Reviewed-on: https://gerrit.libreoffice.org/58558
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    Tested-by: Andras Timar <andras.timar@collabora.com>

--- loleaflet/src/control/Control.ContextMenu.js
+++ loleaflet/src/control/Control.ContextMenu.js
@@ -154,8 +154,8 @@ L.Control.ContextMenu = L.Control.extend({
 
 				isLastItemText = true;
 			} else if (item.type === 'menu') {
-				itemName = item.text.replace('~', '');
-				if (itemName === 'Paste Special') {
+				itemName = item.text;
+				if (itemName.replace('~', '') === 'Paste Special') {
 					itemName = _('Internal Paste Special');
 				}
 				var submenu = this._createContextMenuStructure(item);
@@ -165,7 +165,7 @@ L.Control.ContextMenu = L.Control.extend({
 				}
 
 				contextMenu[item.command] = {
-					name: _(itemName),
+					name: _(itemName).replace(/\(~[A-Za-z]\)/, '').replace('~', ''),
 					items: submenu
 				};
 				isLastItemText = true;
