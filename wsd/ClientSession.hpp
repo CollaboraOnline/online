df085d322d202687b9a6b1d9368d3c365dc543e7
Tamás Zolnai <tamas.zolnai@collabora.com>
2018-08-23 12:46:49 +0200

    Drop too old tileID's from tiles-on-fly list
    So we can avoid that tile sending stop working because server is
    waiting for tileprocessed messages which will not arrive.
    
    Change-Id: I545346c50d49340999608aadac32b5190ede43c5
    (cherry picked from commit 759d1fe72294b22669f19dabf28a4fcf4922af8c)
    
    Fix previous commit
    
    Change-Id: I7d3bce0132d124e52f7885c8cb3c26acc6f7b41d
    (cherry picked from commit 3bd7c6b5084bb2505cf0f2629450974e68b8c050)
    
    Go back using list for tilesOnFly
    
    It can handle duplicates which we need to have.
    
    Change-Id: Ia4cd813dd173bc538dd27953c4886d460b5b1c49
    (cherry picked from commit 185b933353c670ef4639ada323ed2f803f209e70)
    Reviewed-on: https://gerrit.libreoffice.org/59648
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    Tested-by: Andras Timar <andras.timar@collabora.com>
    (cherry picked from commit dce9d960fe8e059e8d60f0a93b88dba54f53a7ee)
    Signed-off-by: Andras Timar <andras.timar@collabora.com>

--- wsd/ClientSession.hpp
+++ wsd/ClientSession.hpp
@@ -19,6 +19,8 @@
 #include <Rectangle.hpp>
 #include <deque>
 #include <map>
+#include <list>
+#include <utility>
 #include <unordered_set>
 
 class DocumentBroker;
@@ -125,6 +127,7 @@ public:
     void addTileOnFly(const TileDesc& tile);
     void clearTilesOnFly();
     size_t getTilesOnFlyCount() const { return _tilesOnFly.size(); }
+    void removeOutdatedTilesOnFly();
 
     Util::Rectangle getVisibleArea() const { return _clientVisibleArea; }
     int getTileWidthInTwips() const { return _tileWidthTwips; }
@@ -230,7 +233,7 @@ private:
     bool _isTextDocument;
 
     /// TileID's of the sent tiles. Push by sending and pop by tileprocessed message from the client.
-    std::list<std::string> _tilesOnFly;
+    std::list<std::pair<std::string, std::chrono::steady_clock::time_point>> _tilesOnFly;
 
     /// Names of tiles requested from kit, which this session is subsrcibed to
     /// Track only non-thumbnail tiles (getId() == -1)
