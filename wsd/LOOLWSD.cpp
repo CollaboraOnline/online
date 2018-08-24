a2809fd27277fd01d0dafdc0c4f0435fdf85db36
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2018-08-23 21:38:01 -0400

    wsd: disable anonymization when allow_logging_pii=true
    Anonymizing is useless with trace anyway, since no
    trace log entry anonymizes, it's more useful to
    disable anonymization to help troubleshoot (which is
    the reason for enabling tracing in the first place).
    
    Change-Id: I437207e7a798c2288a5d5eb33446cb16491bfbf6
    Reviewed-on: https://gerrit.libreoffice.org/59534
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    Tested-by: Andras Timar <andras.timar@collabora.com>

--- wsd/LOOLWSD.cpp
+++ wsd/LOOLWSD.cpp
@@ -819,16 +819,12 @@ void LOOLWSD::initialize(Application& self)
 #else
     AnonymizeUsernames = getConfigValue<bool>(conf, "logging.anonymize.usernames", false);
 #endif
-    if (AnonymizeUsernames)
-        setenv("LOOL_ANONYMIZE_USERNAMES", AnonymizeUsernames ? "1" : "0", true);
 
 #if LOOLWSD_ANONYMIZE_FILENAMES
     AnonymizeFilenames = true;
 #else
     AnonymizeFilenames = getConfigValue<bool>(conf, "logging.anonymize.filenames", false);
 #endif
-    if (AnonymizeFilenames)
-        setenv("LOOL_ANONYMIZE_FILENAMES", AnonymizeFilenames ? "1" : "0", true);
 
     if ((AnonymizeFilenames || AnonymizeUsernames) && LogLevel == "trace")
     {
@@ -836,6 +832,10 @@ void LOOLWSD::initialize(Application& self)
         {
             LOG_WRN("Enabling trace logging while anonymization is enabled due to logging.anonymize.allow_logging_pii setting. "
                     "This will leak personally identifiable information!");
+
+            // Disable anonymization as it's useless now.
+            AnonymizeFilenames = false;
+            AnonymizeUsernames = false;
         }
         else
         {
@@ -851,6 +851,12 @@ void LOOLWSD::initialize(Application& self)
             _exit(Application::EXIT_SOFTWARE);
         }
     }
+
+    if (AnonymizeFilenames)
+        setenv("LOOL_ANONYMIZE_FILENAMES", "1", true);
+
+    if (AnonymizeUsernames)
+        setenv("LOOL_ANONYMIZE_USERNAMES", "1", true);
 
     {
         std::string proto = getConfigValue<std::string>(conf, "net.proto", "");
