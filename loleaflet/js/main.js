6d2ba49f9e7429b262de6a64e70703d036b6782d
Henry Castro <hcastro@collabora.com>
2018-08-26 11:52:50 -0400

    loleaflet: mobile: enable context menu
    Change-Id: If6d24d05cc59274b49059b9e05f91dcc0f6cdc6b

--- loleaflet/js/main.js
+++ loleaflet/js/main.js
@@ -86,6 +86,8 @@ window.addEventListener('beforeunload', function () {
 	}
 });
 
-L.DomEvent.on(document, 'contextmenu', L.DomEvent.preventDefault);
+if (!L.Browser.mobile) {
+	L.DomEvent.on(document, 'contextmenu', L.DomEvent.preventDefault);
+}
 
 }(window));
