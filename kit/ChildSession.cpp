1c93fd7ead50ff16994cf90936602891c0bbc8d8
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2018-09-17 07:17:31 -0400

    wsd: leaflet: support reordering of slides via drag-and-drop
    Change-Id: I1b471ba07dd0a1016a0759de729171ae968262cb
    Reviewed-on: https://gerrit.libreoffice.org/69635
    Reviewed-by: Ashod Nakashian <ashnakash@gmail.com>
    Tested-by: Ashod Nakashian <ashnakash@gmail.com>
    (cherry picked from commit 021d67f1430ff2a554b65c65d7aee752fed0b77f)
    Reviewed-on: https://gerrit.libreoffice.org/80578
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    Tested-by: Andras Timar <andras.timar@collabora.com>

--- kit/ChildSession.cpp
+++ kit/ChildSession.cpp
@@ -227,6 +227,10 @@ bool ChildSession::_handleInput(const char *buffer, int length)
     {
         return selectClientPart(buffer, length, tokens);
     }
+    else if (tokens[0] == "moveselectedclientparts")
+    {
+        return moveSelectedClientParts(buffer, length, tokens);
+    }
     else if (tokens[0] == "setpage")
     {
         return setPage(buffer, length, tokens);
@@ -1821,6 +1825,37 @@ bool ChildSession::selectClientPart(const char* /*buffer*/, int /*length*/, cons
     }
 
     return true;
+}
+
+bool ChildSession::moveSelectedClientParts(const char* /*buffer*/, int /*length*/, const std::vector<std::string>& tokens)
+{
+    int nPosition;
+    if (tokens.size() < 2 ||
+        !getTokenInteger(tokens[1], "position", nPosition))
+    {
+        sendTextFrame("error: cmd=moveselectedclientparts kind=invalid");
+        return false;
+    }
+
+    std::unique_lock<std::mutex> lock(_docManager.getDocumentMutex());
+
+    getLOKitDocument()->setView(_viewId);
+
+    if (getLOKitDocument()->getDocumentType() != LOK_DOCTYPE_TEXT)
+    {
+        getLOKitDocument()->moveSelectedParts(nPosition, false); // Move, don't duplicate.
+
+        // Get the status to recreate the previews and correctly order parts.
+        const std::string status = LOKitHelper::documentStatus(getLOKitDocument()->get());
+        if (!status.empty())
+            return sendTextFrame("statusupdate: " + status);
+    }
+    else
+    {
+        LOG_WRN("ChildSession::moveSelectedClientParts[" << getName() << "]: error moving parts on text documents.");
+    }
+
+    return true; // Non-fatal to fail.
 }
 
 bool ChildSession::setPage(const char* /*buffer*/, int /*length*/, const std::vector<std::string>& tokens)
