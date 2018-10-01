835959a617f388541edaf11b210897566b65b754
Tamás Zolnai <tamas.zolnai@collabora.com>
2018-10-01 18:04:47 +0200

    Remove accidentally pushed log line
    Change-Id: Icd7fcb96725b3cf7fded199a5eae13ec2c109a58

--- wsd/ClientSession.cpp
+++ wsd/ClientSession.cpp
@@ -1056,8 +1056,6 @@ void ClientSession::enqueueSendMessage(const std::shared_ptr<Message>& data)
     {
         const TileDesc tile = TileDesc::parse(data->firstLine());
         traceTileBySend(tile, sizeBefore == newSize);
-        if (sizeBefore != newSize)
-            LOG_INF("Sending new tile to client: " <<  tile.serialize("tile:"));
     }
 }
 
