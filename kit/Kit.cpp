bff5c176937543540c521882994900339a4191e7
Tor Lillqvist <tml@collabora.com>
2018-10-15 16:56:00 +0300

    Drop one use of TerminationFlag
    Doesn't help my current issue in the iOS app, but the less use of
    TerminationFlag the better, I think. (Doesn't break 'make check' on
    Linux.)
    
    Change-Id: I8fef271d6e0e81dea92e0da76ecb7211239adab6

--- kit/Kit.cpp
+++ kit/Kit.cpp
@@ -2433,12 +2433,12 @@ void lokit_main(
 
             if (document && document->purgeSessions() == 0)
             {
-                LOG_INF("Last session discarded. Setting TerminationFlag");
-                TerminationFlag = true;
+                LOG_INF("Last session discarded. Breaking out of loop");
+                break;
             }
         }
 
-        LOG_INF("Kit poll terminated.");
+        LOG_INF("Kit poll finished.");
 
         // Let forkit handle the jail cleanup.
     }
