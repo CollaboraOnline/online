CollaboraOnline/online

commit e27ae38bebf4b57aa7563357a188e33d0e1e0be8

Fix jumping after touch zooming also for mobile clients of normal Online
Don't check window.ThisIsAMobileApp but instead use a much more
convoluted test to catch only the cases where we do want the document
to be scrolled, and ignore the case that causes the irritating
jumping.

In _onUpdateCursor() in TileLayer.js, we know that the scroll
parameter is not undefined when and only when _onUpdateCursor() is
called from _onInvalidateCursorMsg(). We pass that information down to
the onUpdate callback handler passed to the _tweenTo() function in the
_scrollTo() function in jquery.mCustomScrollbar.js where it shows up
as property of the options object.

In that location we also make use of the incidental fact that
options.timeout==1 only when that code is called from mouse-wheel scrolling.

Quite possibly all this could be done much cleaner.

Change-Id: Iefa257bceb54137f25534ccb6786c1d2e315931c

loleaflet/src/layer/tile/TileLayer.js
@@ -1608,7 +1608,7 @@ L.TileLayer = L.GridLayer.extend({
 			if (!(this._selectionHandles.start && this._selectionHandles.start.isDragged) &&
 			    !(this._selectionHandles.end && this._selectionHandles.end.isDragged) &&
 			    !(docLayer._followEditor || docLayer._followUser)) {
-				this._map.fire('scrollto', {x: center.x, y: center.y});
+				this._map.fire('scrollto', {x: center.x, y: center.y, calledFromInvalidateCursorMsg: scroll !== undefined});
 			}
 		}
 

loleaflet/js/jquery.mCustomScrollbar.js
@@ -1,4 +1,4 @@
-/* -*- js-indent-level: 8; indent-tabs-mode: t -*- */
+/* -*- js-indent-level: 8; indent-tabs-mode: t; fill-column: 120 -*- */
 /*
 == malihu jquery custom scrollbar plugin == 
 Version: 3.1.3 
@@ -2120,7 +2120,14 @@ and dependencies (minified).
 					}
 				},onUpdate:function(){
 					if(options.callbacks && options.onUpdate){
-						if (!window.ThisIsAMobileApp || options.drag) {
+						// This condition is intended to filter out the case when this gets
+						// invoked from a touch pinch gesture, but let through the cases of
+						// scrolling with mouse-wheel or touchpad, dragging the scrollbar
+						// slider, or implicit scrolling because of editing in a previously
+						// hidden part of the document (for instance when pressing enter on the
+						// last visible line). The options.timeout==1 is a silly way to detect
+						// the mouse-wheel scrolling.
+						if(options.drag || options.timeout===1 || options.calledFromInvalidateCursorMsg==true){
 							/* callbacks: whileScrolling */
 							if(_cb("whileScrolling")){_mcs(); o.callbacks.whileScrolling.call(el[0]);}
 						}

loleaflet/src/control/Control.Scroll.js
@@ -155,7 +155,7 @@ L.Control.Scroll = L.Control.extend({
 
 	_onScrollTo: function (e) {
 		// triggered by the document (e.g. search result out of the viewing area)
-		$('.scroll-container').mCustomScrollbar('scrollTo', [e.y, e.x]);
+		$('.scroll-container').mCustomScrollbar('scrollTo', [e.y, e.x], {calledFromInvalidateCursorMsg: e.calledFromInvalidateCursorMsg});
 	},
 
 	_onScrollBy: function (e) {
@@ -169,6 +169,7 @@ L.Control.Scroll = L.Control.extend({
 		if (e.x < 0) {
 			x = '-=' + Math.abs(e.x);
 		}
+		// Note: timeout===1 is checked in my extremely ugly hack in jquery.mCustomScrollbar.js.
 		$('.scroll-container').mCustomScrollbar('scrollTo', [y, x], { timeout: 1 });
 	},
 

commit 823a4bf88e062a81618b637864cd2de2c79f904c

Fix further fallout from e27ae38bebf4b57aa7563357a188e33d0e1e0be8
One more special case where we do want to do the whileScrolling
callback from the onUpdate callback: When selecting another sheet of a
multi-sheet Calc document. Handle it by adding an optional parameter
to setPart() in Parts.js from _serPart() in Control.Tabs.js, that
eventually gets passed down as the "scroll" parameter to
_onUpdateCursor() in TileLayer.js, which turns on the special
handling.

Change-Id: I44772912a0598b78bef182f1b6147ff91d6c88e9

loleaflet/src/control/Control.Tabs.js
@@ -152,7 +152,7 @@ L.Control.Tabs = L.Control.extend({
 	_setPart: function (e) {
 		var part =  e.target.id.match(/\d+/g)[0];
 		if (part !== null) {
-			this._map.setPart(parseInt(part));
+			this._map.setPart(parseInt(part), /*external:*/ false, /*calledFromSetPartHandler:*/ true);
 		}
 	}
 });

loleaflet/src/control/Parts.js
@@ -3,7 +3,7 @@
  * Document parts switching handler
  */
 L.Map.include({
-	setPart: function (part, external) {
+	setPart: function (part, external, calledFromSetPartHandler) {
 		var docLayer = this._docLayer;
 		docLayer._prevSelectedPart = docLayer._selectedPart;
 		if (part === 'prev') {
@@ -41,7 +41,7 @@ L.Map.include({
 		docLayer.eachView(docLayer._cellViewCursors, docLayer._onUpdateCellViewCursor, docLayer);
 		docLayer.eachView(docLayer._graphicViewMarkers, docLayer._onUpdateGraphicViewSelection, docLayer);
 		docLayer.eachView(docLayer._viewSelections, docLayer._onUpdateTextViewSelection, docLayer);
-		docLayer._clearSelections();
+		docLayer._clearSelections(calledFromSetPartHandler);
 		docLayer._updateOnChangePart();
 		docLayer._pruneTiles();
 		docLayer._prevSelectedPartNeedsUpdate = true;

loleaflet/src/layer/tile/TileLayer.js
@@ -1477,9 +1477,9 @@ L.TileLayer = L.GridLayer.extend({
 		e.tile.onload = null;
 	},
 
-	_clearSelections: function () {
+	_clearSelections: function (calledFromSetPartHandler) {
 		// hide the cursor if not editable
-		this._onUpdateCursor();
+		this._onUpdateCursor(calledFromSetPartHandler);
 		// hide the text selection
 		this._selections.clearLayers();
 		// hide the selection handles