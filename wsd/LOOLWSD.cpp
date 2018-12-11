454c336614ba5d16c74c540193dc850930499ed7
Andras Timar <andras.timar@collabora.com>
2018-12-11 12:55:16 +0100

    New configure switch --enable-vereign to set the default document signing url
    Change-Id: I65225fc5f94a8ad49b75d31ef3535e8479484e69
    (cherry picked from commit 279301e51d8975074177c36c33d4b432c0247c98)

--- wsd/LOOLWSD.cpp
+++ wsd/LOOLWSD.cpp
@@ -783,6 +783,7 @@ void LOOLWSD::initialize(Application& self)
             { "net.service_root", "" },
             { "num_prespawn_children", "1" },
             { "per_document.autosave_duration_secs", "300" },
+            { "per_document.document_signing_url", VEREIGN_URL },
             { "per_document.idle_timeout_secs", "3600" },
             { "per_document.idlesave_duration_secs", "30" },
             { "per_document.limit_file_size_mb", "0" },
