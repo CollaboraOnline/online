3e69f7210589d0b99d1e82033958debf72a29890
Szymon Kłos <szymon.klos@collabora.com>
2018-12-13 20:09:53 +0100

    Don't show sidebar on start in mobile
    Change-Id: I9b8eeb6d3406e971608fc950359b315220a50e51
    (cherry picked from commit 8a1ed2c8aca416041ea5026dc902fdef107aa558)

--- loleaflet/src/map/Map.js
+++ loleaflet/src/map/Map.js
@@ -256,7 +256,7 @@ L.Map = L.Evented.extend({
 			this.initializeModificationIndicator();
 
 			// Show sidebar.
-			if (this._docLayer &&
+			if (this._docLayer && !window.mode.isMobile() &&
 				(this._docLayer._docType === 'spreadsheet' || this._docType === 'text')) {
 				// Let the first page finish loading then load the sidebar.
 				var map = this;
