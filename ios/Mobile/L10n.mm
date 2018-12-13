84570cc0b3261bebe5d903e81be665efee70c062
Tor Lillqvist <tml@collabora.com>
2018-12-13 16:02:04 +0200

    Adapt to LibreOfficeKitClass no longer having the translateGet() method
    This is a somewhat temporary quick solution. It bluntly uses the same
    code here that I had added for a while as the implementation of
    translateGet() for LibreOfficeKitClass in LO core.
    
    Ideally we should have a script here in online to pick the needed
    translation from the translations submodule of core and keep them
    around even if a translation happens to evaporate from
    core/translations. The same idea as in the scripts/unocommands.py, but
    I did not yet start modifying or copying that.
    
    Change-Id: I455ad6044e321ef59873d60f8e5f3e7032f2447e
    Reviewed-on: https://gerrit.libreoffice.org/68322
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    Tested-by: Andras Timar <andras.timar@collabora.com>

---
+++ ios/Mobile/L10n.mm
@@ -0,0 +1,28 @@
+// -*- Mode: ObjC; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*-
+//
+// This file is part of the LibreOffice project.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#import <cstring>
+
+#define LIBO_INTERNAL_ONLY
+
+#import <i18nlangtag/languagetag.hxx>
+#import <unotools/resmgr.hxx>
+
+#import "AppDelegate.h"
+#import "L10n.h"
+
+char* app_translate(const char *id, const char *catalog)
+{
+    LanguageTag tag(OUString::fromUtf8([app_locale UTF8String]));
+    std::locale locale = Translate::Create(catalog, tag);
+    OUString result = Translate::get(id, locale);
+
+    return strdup(result.toUtf8().getStr());
+}
+
+// vim:set shiftwidth=4 softtabstop=4 expandtab:
