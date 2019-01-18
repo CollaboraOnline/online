070c952dd45a67a7a355c15cd156cf1651359543
merttumer <mert.tumer@collabora.com>
2019-01-18 21:38:48 +0300

    added Watermark item on format menu
    Change-Id: I69fc50b44e4edf871f474c2baacecbb03ad7aabe
    Signed-off-by: merttumer <mert.tumer@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/66629
    Reviewed-by: Jan Holesovsky <kendy@collabora.com>
    Tested-by: Jan Holesovsky <kendy@collabora.com>
    (cherry picked from commit c3d67d6af2cdde6b2cd95cc72b3a0498a2f39c20)
    Reviewed-on: https://gerrit.libreoffice.org/66720

--- loleaflet/src/control/Control.Menubar.js
+++ loleaflet/src/control/Control.Menubar.js
@@ -181,6 +181,8 @@ L.Control.Menubar = L.Control.extend({
 				{uno: '.uno:OutlineBullet'},
 				{uno: '.uno:PageDialog'},
 				{type: 'separator'},
+				{uno: '.uno:Watermark'},
+				{type: 'separator'},
 				{uno: '.uno:FormatColumns'},
 				{type: 'separator'},
 				{uno: '.uno:ResetAttributes'}
