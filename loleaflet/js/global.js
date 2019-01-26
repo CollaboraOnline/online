032703ca164f7e363f153ef82f46e8f697902324
Henry Castro <hcastro@collabora.com>
2019-01-26 17:21:48 -0400

    loleaflet: indent global.js
    Change-Id: Ib04a18bcedb26ce267a80fd7990454f590de1a3d

--- loleaflet/js/global.js
+++ loleaflet/js/global.js
@@ -1,54 +1,52 @@
 /* -*- js-indent-level: 8 -*- */
-/* global loleafletLogging */
-/*eslint indent: [error, "tab", { "outerIIFEBody": 0 }]*/
 (function (global) {
 
-// If not debug, don't print anything on the console
-// except in tile debug mode (Ctrl-Shift-Alt-d)
-console.log2 = console.log;
-if (loleafletLogging !== 'true') {
-	var methods = ['warn', 'info', 'debug', 'trace', 'log', 'assert', 'time', 'timeEnd'];
-	for (var i = 0; i < methods.length; i++) {
-		console[methods[i]] = function() {};
-	}
-}
-
-// fix jquery-ui
-// var jQuery = require('jquery');
-global.require = function (path) {
-	if (path=='jquery') {
-		return global.jQuery;
-	}
-};
-
-global.getParameterByName = function (name) {
-	name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-	var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-	var results = regex.exec(location.search);
-	return results === null ? '' : results[1].replace(/\+/g, ' ');
-};
-
-global._ = function (string) {
-	// In the mobile app case we can't use the stuff from l10n-for-node, as that assumes HTTP.
-	if (window.ThisIsTheiOSApp) {
-		// We use another approach just for iOS for now.
-		if (window.LOCALIZATIONS.hasOwnProperty(string)) {
-			// window.postMobileDebug('_(' + string + '): YES: ' + window.LOCALIZATIONS[string]);
-			var result = window.LOCALIZATIONS[string];
-			if (window.LANG === 'de-CH') {
-				result = result.replace(/ß/g, 'ss');
-			}
-			return result;
-		} else {
-			// window.postMobileDebug('_(' + string + '): NO');
-			return string;
+	// If not debug, don't print anything on the console
+	// except in tile debug mode (Ctrl-Shift-Alt-d)
+	console.log2 = console.log;
+	if (global.loleafletLogging !== 'true') {
+		var methods = ['warn', 'info', 'debug', 'trace', 'log', 'assert', 'time', 'timeEnd'];
+		for (var i = 0; i < methods.length; i++) {
+			console[methods[i]] = function() {};
 		}
-	} else if (window.ThisIsAMobileApp) {
-		// And bail out without translations on other mobile platforms.
-		return string;
-	} else {
-		return string.toLocaleString();
 	}
-};
+
+	// fix jquery-ui
+	// var jQuery = require('jquery');
+	global.require = function (path) {
+		if (path=='jquery') {
+			return global.jQuery;
+		}
+	};
+
+	global.getParameterByName = function (name) {
+		name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+		var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+		var results = regex.exec(location.search);
+		return results === null ? '' : results[1].replace(/\+/g, ' ');
+	};
+
+	global._ = function (string) {
+		// In the mobile app case we can't use the stuff from l10n-for-node, as that assumes HTTP.
+		if (window.ThisIsTheiOSApp) {
+			// We use another approach just for iOS for now.
+			if (window.LOCALIZATIONS.hasOwnProperty(string)) {
+				// window.postMobileDebug('_(' + string + '): YES: ' + window.LOCALIZATIONS[string]);
+				var result = window.LOCALIZATIONS[string];
+				if (window.LANG === 'de-CH') {
+					result = result.replace(/ß/g, 'ss');
+				}
+				return result;
+			} else {
+				// window.postMobileDebug('_(' + string + '): NO');
+				return string;
+			}
+		} else if (window.ThisIsAMobileApp) {
+			// And bail out without translations on other mobile platforms.
+			return string;
+		} else {
+			return string.toLocaleString();
+		}
+	};
 
 }(window));
