02c679c9d6eb8a1a3a93a4b6ba38a2f4705835d3
Gülşah Köse <gulsah.kose@collabora.com>
2019-01-29 15:52:52 +0300

    android: Inital commit of the Java pieces.
    Change-Id: I895d5aaaf7217b7fd556f7805c42d3ec1bdfc22d

---
+++ android/app/src/main/java/org/libreoffice/androidapp/MainActivity.java
@@ -0,0 +1,31 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package org.libreoffice.androidapp;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView browser = findViewById(R.id.browser);
+        browser.setWebViewClient(new WebViewClient());
+        browser.loadUrl("http://www.libreoffice.org");
+    }
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
