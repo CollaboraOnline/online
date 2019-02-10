3d91ea94a90fa433337b3aea38fe4b8f8fc5cee1
Henry Castro <hcastro@collabora.com>
2019-02-10 19:53:57 -0400

    loleaflet: remove unused draw plugin
    Change-Id: I6acdec7a5e04ba9cd370b80a3a04b54d370420a0

--- loleaflet/plugins/draw-0.2.4/spec/before.js
+++
@@ -1,4 +0,0 @@
-/* -*- js-indent-level: 8 -*- */
-// set up before Leaflet files to test L#noConflict later
-L = 'test';
-
--- loleaflet/plugins/draw-0.2.4/src/copyright.js
+++
@@ -1,9 +0,0 @@
-/* -*- js-indent-level: 8 -*- */
-/*
-	Leaflet.draw, a plugin that adds drawing and editing tools to Leaflet powered maps.
-	(c) 2012-2013, Jacob Toye, Smartrak
-
-	https://github.com/Leaflet/Leaflet.draw
-	http://leafletjs.com
-	https://github.com/jacobtoye
-*/
