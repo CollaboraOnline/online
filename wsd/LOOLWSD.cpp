7add1c776f43b81f3a3ac3b941ad5cec85d03d65
Michael Meeks <michael.meeks@collabora.com>
2019-03-01 22:05:11 +0100

    Authenticate socket earlier.
    Change-Id: Idf3caba6276dd71450b86574ef39a1a16e73f61c
    Reviewed-on: https://gerrit.libreoffice.org/71753
    Reviewed-by: Jan Holesovsky <kendy@collabora.com>
    Tested-by: Jan Holesovsky <kendy@collabora.com>

--- wsd/LOOLWSD.cpp
+++ wsd/LOOLWSD.cpp
@@ -2344,11 +2344,7 @@ private:
         StringTokenizer tokens(request.getURI(), "/?");
         if (tokens.count() > 2 && tokens[2] == "convert-to")
         {
-            ConvertToPartHandler handler(/*convertTo =*/ true);
-            HTMLForm form(request, message, handler);
-
-            std::string format = (form.has("format") ? form.get("format") : "");
-
+            // Validate sender - FIXME: should do this even earlier.
             if (!allowConvertTo(socket->clientAddress(), request, true))
             {
                 LOG_TRC("Conversion not allowed from this address");
@@ -2362,6 +2358,11 @@ private:
                 socket->shutdown();
                 return;
             }
+
+            ConvertToPartHandler handler(/*convertTo =*/ true);
+            HTMLForm form(request, message, handler);
+
+            std::string format = (form.has("format") ? form.get("format") : "");
 
             // prefer what is in the URI
             if (tokens.count() > 3)
