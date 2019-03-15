7b5d10a69ad29c771c5dd5e02e4230cc2f2d31af
Michael Meeks <michael.meeks@collabora.com>
2019-03-15 10:51:19 +0100

    Anonymization: don't log when it is disabled.
    Also fix unexpected concatenation error in Poco::URI::encode generating
    eg. authorid=localhost0 xauthorid=localhost0localhost0 in the output.
    
    Change-Id: I560e47e31884eeb1c662f468436ed7541cfb082d
    Reviewed-on: https://gerrit.libreoffice.org/69299
    Reviewed-by: Ashod Nakashian <ashnakash@gmail.com>
    Tested-by: Ashod Nakashian <ashnakash@gmail.com>

--- wsd/ClientSession.cpp
+++ wsd/ClientSession.cpp
@@ -420,12 +420,14 @@ bool ClientSession::loadDocument(const char* /*buffer*/, int /*length*/,
             std::string encodedUserId;
             Poco::URI::encode(getUserId(), "", encodedUserId);
             oss << " authorid=" << encodedUserId;
+            encodedUserId = "";
             Poco::URI::encode(LOOLWSD::anonymizeUsername(getUserId()), "", encodedUserId);
             oss << " xauthorid=" << encodedUserId;
 
             std::string encodedUserName;
             Poco::URI::encode(getUserName(), "", encodedUserName);
             oss << " author=" << encodedUserName;
+            encodedUserName = "";
             Poco::URI::encode(LOOLWSD::anonymizeUsername(getUserName()), "", encodedUserName);
             oss << " xauthor=" << encodedUserName;
         }
