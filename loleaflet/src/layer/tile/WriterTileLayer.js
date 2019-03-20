9f7dca3209661a6ca54f287e9709da502223e170
Szymon Kłos <szymon.klos@collabora.com>
2019-03-20 09:34:19 +0100

    Avoid syntax errors while paring command results
    Change-Id: I7d778fd62aeb32c8304afb90c21aafd83379c170

--- loleaflet/src/layer/tile/WriterTileLayer.js
+++ loleaflet/src/layer/tile/WriterTileLayer.js
@@ -54,7 +54,12 @@ L.WriterTileLayer = L.TileLayer.extend({
 	},
 
 	_onCommandValuesMsg: function (textMsg) {
-		var values = JSON.parse(textMsg.substring(textMsg.indexOf('{')));
+		var braceIndex = textMsg.indexOf('{');
+		if (braceIndex < 0) {
+			return;
+		}
+
+		var values = JSON.parse(textMsg.substring(braceIndex));
 		if (!values) {
 			return;
 		}
