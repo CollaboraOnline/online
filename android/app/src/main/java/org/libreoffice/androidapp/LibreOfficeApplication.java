1f2866337c74ba7f02c5a5929ce967ba8d217f4c
Jan Holesovsky <kendy@collabora.com>
2019-03-27 13:54:30 +0100

    android: Copy some files from the old Android app.
    This is a snapshot from core.git at the time of commit
    9852f09b467e3c7f8058b931010b91f447905051.
    
    Change-Id: Ifdd752c0c3d11b25ee7250e37f61959b0ea05c9a

---
+++ android/app/src/main/java/org/libreoffice/androidapp/LibreOfficeApplication.java
@@ -0,0 +1,33 @@
+/*
+ *
+ *  * This file is part of the LibreOffice project.
+ *  *
+ *  * This Source Code Form is subject to the terms of the Mozilla Public
+ *  * License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+package org.libreoffice;
+
+import android.app.Application;
+import android.content.Context;
+import android.os.Handler;
+
+public class LibreOfficeApplication extends Application {
+
+    private static Handler mainHandler;
+
+    public LibreOfficeApplication() {
+        mainHandler = new Handler();
+    }
+
+    public static Handler getMainHandler() {
+        return mainHandler;
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(LocaleHelper.onAttach(base, "en"));
+    }
+}
