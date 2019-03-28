1767dcdb2a8195ba3b40bcf67876e3425fc4ae7c
Szymon Kłos <szymon.klos@collabora.com>
2019-03-28 11:49:56 +0100

    Introduce warning when leaving the editor
    Change-Id: Id9fe06015b45f37ae415f8e3607434d984a6074f

--- loleaflet/src/control/Control.AlertDialog.js
+++ loleaflet/src/control/Control.AlertDialog.js
@@ -24,6 +24,53 @@ L.Control.AlertDialog = L.Control.extend({
 		else if (e.cmd == 'load' && e.kind == 'docunloading') {
 			// Handled by transparently retrying.
 			return;
+		} else if (e.cmd == 'openlink') {
+			var url = e.url;
+			var messageText = window.errorMessages.leaving;
+
+			var isLinkValid = true;
+			if (url.trim().toLowerCase().startsWith('javascript:')) {
+				isLinkValid = false;
+				messageText = window.errorMessages.invalidLink;
+				messageText = messageText.replace('%url', url);
+			}
+
+			var buttonsList = [];
+
+			if (isLinkValid) {
+				buttonsList.push({
+					text: _('Open link'),
+					type: 'button',
+					className: 'vex-dialog-button-primary',
+					click: function openClick () {
+						window.open(url, '_blank');
+						vex.close(vex.dialogID);
+						vex.dialogID = 0;
+					}
+				});
+			}
+
+			buttonsList.push({
+				text: _('Edit'),
+				type: 'button',
+				className: 'vex-dialog-button-secondary',
+				click: function editClick () {
+					e.map.toggleCommandState('HyperlinkDialog');
+					vex.close(vex.dialogID);
+					vex.dialogID = 0;
+				}
+			});
+
+			vex.dialog.open({
+				message: messageText,
+				showCloseButton: true,
+				buttons: buttonsList,
+				callback: function() {},
+				beforeClose: function () {
+					e.map.focus();
+					e.map.enable(true);
+				}
+			});
 		} else if (e.cmd && e.kind) {
 			var msg = _('The server encountered a %0 error while parsing the %1 command.');
 			msg = msg.replace('%0', e.kind);
