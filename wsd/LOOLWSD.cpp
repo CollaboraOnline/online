545b2e4262844dacfd8c375cefb91abd9339379f
Jan Holesovsky <kendy@collabora.com>
2019-03-30 19:12:19 +0100

    Use the app name more generally, not only on iOS.
    Preparation for using it on Android too.
    
    Change-Id: Iee7778b2625a02a98daff5df87c39f4ab1d18144
    Reviewed-on: https://gerrit.libreoffice.org/70651
    Reviewed-by: Tor Lillqvist <tml@collabora.com>
    Tested-by: Tor Lillqvist <tml@collabora.com>

--- wsd/LOOLWSD.cpp
+++ wsd/LOOLWSD.cpp
@@ -2792,6 +2792,9 @@ private:
         // Hint to encourage use on mobile devices
         capabilities->set("hasMobileSupport", true);
 
+        // Set the product name
+        capabilities->set("productName", APP_NAME);
+
         std::ostringstream ostrJSON;
         capabilities->stringify(ostrJSON);
         return ostrJSON.str();
