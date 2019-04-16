5ea28575211a0cc475483892d8d9050d0adc1baf
Jan Holesovsky <kendy@collabora.com>
2019-04-16 13:05:49 +0200

    android: Update / introduce modelines in the .java files.
    Plus some whitespace changes.
    
    Change-Id: Ic35a67ad7f6d911679e6d1f717f340640cbcfee9

--- android/app/src/main/java/org/libreoffice/androidapp/LibreOfficeApplication.java
+++ android/app/src/main/java/org/libreoffice/androidapp/LibreOfficeApplication.java
@@ -1,11 +1,10 @@
+/* -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /*
+ * This file is part of the LibreOffice project.
  *
- *  * This file is part of the LibreOffice project.
- *  *
- *  * This Source Code Form is subject to the terms of the Mozilla Public
- *  * License, v. 2.0. If a copy of the MPL was not distributed with this
- *  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
 package org.libreoffice.androidapp;
@@ -31,3 +30,5 @@ public class LibreOfficeApplication extends Application {
         super.attachBaseContext(LocaleHelper.onAttach(base, "en"));
     }
 }
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
