41341c62ab45435aeb19cc8aa8571680c6500a28
Michael Meeks <michael.meeks@collabora.com>
2019-05-01 21:54:45 +0100

    Adapt test for new hiddenparts field.
    Change-Id: Id2173a38167b97327d0735dfe2a8c88e2dcc4bc7
    Reviewed-on: https://gerrit.libreoffice.org/71639
    Reviewed-by: Jan Holesovsky <kendy@collabora.com>
    Tested-by: Jan Holesovsky <kendy@collabora.com>

--- test/TileCacheTests.cpp
+++ test/TileCacheTests.cpp
@@ -1031,7 +1031,10 @@ void TileCacheTests::checkTiles(std::shared_ptr<LOOLWebSocket>& socket, const st
         std::getline(istr, line);
 
         Poco::StringTokenizer tokens(line, " ", Poco::StringTokenizer::TOK_IGNORE_EMPTY | Poco::StringTokenizer::TOK_TRIM);
-        CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(6), tokens.count());
+        if (docType == "presentation") // extra hiddenparts="..."
+            CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(7), tokens.count());
+        else
+            CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(6), tokens.count());
 
         // Expected format is something like 'type= parts= current= width= height='.
         const std::string text = tokens[0].substr(type.size());
