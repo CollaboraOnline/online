b999c6fe6d080131f96dec34b11075164ae1d9e1
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2019-05-02 23:28:04 -0400

    leaflet: Enable sidebar on writer and calc, but not yet in impress
    Does not actually show the sidebar yet...
    
    Change-Id: Iadddba04bbdb96159e84ffd1498a0845b32e1590

--- loleaflet/src/map/Map.js
+++ loleaflet/src/map/Map.js
@@ -231,17 +231,15 @@ L.Map = L.Evented.extend({
 
 			// Show sidebar.
 			if (this._docLayer && !window.mode.isMobile() && !window.mode.isTablet() &&
-				(this._docLayer._docType === 'presentation' || this._docType === 'drawing')) {
+				(this._docLayer._docType === 'spreadsheet' || this._docLayer._docType === 'text')) {
 				// Let the first page finish loading then load the sidebar.
 				var map = this;
 				setTimeout(function () {
                     // This triggers all sidebar decks, so they would
                     // be loaded and show rather quickly on first use.
                     // Also, triggers sidebar window creation in the client.
-					map._socket.sendMessage('uno .uno:MasterSlidesPanel');
-					map._socket.sendMessage('uno .uno:CustomAnimation');
-					map._socket.sendMessage('uno .uno:SlideChangeWindow');
-					map._socket.sendMessage('uno .uno:ModifyPage');
+					map._socket.sendMessage('uno .uno:ViewSidebarStyles');
+					map._socket.sendMessage('uno .uno:Sidebar');
 				}, 200);
 			}
 		}, this);
