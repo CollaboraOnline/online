203c8be2172bf70aacf12dc26add85127f284e90
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2019-05-06 08:02:52 +0200

    Fix admin dasboard after vex update
    Change-Id: I302f0213a460cd0dd3d4644373f4b75d4d419696
    Reviewed-on: https://gerrit.libreoffice.org/71839
    Reviewed-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
    Tested-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>

--- loleaflet/admin/main-admin.js
+++ loleaflet/admin/main-admin.js
@@ -3,8 +3,8 @@
 require('bootstrap/dist/css/bootstrap.css');
 require('./bootstrap/ie10-viewport-bug-workaround.css');
 require('./bootstrap/dashboard.css');
-require('vex-js/css/vex.css');
-require('vex-js/css/vex-theme-plain.css');
+require('vex-js/dist/css/vex.css');
+require('vex-js/dist/css/vex-theme-plain.css');
 
 var $ = require('jquery');
 global.$ = global.jQuery = $;
@@ -12,8 +12,7 @@ global.$ = global.jQuery = $;
 require('json-js/json2');
 require('l10n-for-node');
 
-var vex = require('vex-js');
-vex.dialog = require('vex-js/js/vex.dialog.js');
+var vex = require('vex-js/dist/js/vex.combined.js');
 vex.defaultOptions.className = 'vex-theme-plain';
 global.vex = vex;
 
