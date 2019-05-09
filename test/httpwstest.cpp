1d32dc1742a2a51343af993939f0f51538de54aa
Miklos Vajna <vmiklos@collabora.com>
2019-05-09 14:25:21 +0200

    Fix HTTPWSTest::testRenderShapeSelectionImpress() against product core
    This test only passed when core.git was built in a way so that '#if
    OSL_DEBUG_LEVEL > 0' was true (debug, dbgutil builds). Re-generate this
    against a product build, since 2 other tests already have references
    like that.
    
    We already have infrastructure for stripping <desc> XML elements from
    the actual result.
    
    Change-Id: I9cdd53bf9625266d94313a90946270bfc266bd47

--- test/httpwstest.cpp
+++ test/httpwstest.cpp
@@ -2772,6 +2772,8 @@ void HTTPWSTest::testRenderShapeSelectionImpress()
         if (it != responseSVG.end())
             responseSVG.erase(responseSVG.begin(), ++it);
 
+        stripDescriptions(responseSVG);
+
         CPPUNIT_ASSERT(svgMatch(testname, responseSVG, "shapes_impress.svg"));
     }
     catch (const Poco::Exception& exc)
