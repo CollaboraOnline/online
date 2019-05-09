aa5b28ceb08cfc5c0e19c8cc99ae353c47f8a2c0
Miklos Vajna <vmiklos@collabora.com>
2019-05-09 10:40:22 +0200

    test: fix HTTPWSTest::testRenderShapeSelectionWriterImage() failure with ...
    ... debug core.git. Same problem, need to filter out <desc> XML
    elements.
    
    Change-Id: I84973b831508e6a073d9c75868307d98a2ada370

--- test/httpwstest.cpp
+++ test/httpwstest.cpp
@@ -2830,6 +2830,8 @@ void HTTPWSTest::testRenderShapeSelectionWriterImage()
         if (it != responseSVG.end())
             responseSVG.erase(responseSVG.begin(), ++it);
 
+        stripDescriptions(responseSVG);
+
         CPPUNIT_ASSERT(svgMatch(testname, responseSVG, "non_shape_writer_image.svg"));
     }
     catch (const Poco::Exception& exc)
