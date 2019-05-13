d59956aa3d236b207c2499b26cbdde677345dac2
Tor Lillqvist <tml@collabora.com>
2019-05-13 16:47:19 +0300

    Use consistent terminology in LOG_INF() calls around runLoop() call
    Change-Id: I46d954250447dea6ffa60c6e0e341f23a9ad4ff9

--- kit/Kit.cpp
+++ kit/Kit.cpp
@@ -2743,7 +2743,7 @@ void lokit_main(
 
         loKit->runLoop(pollCallback, wakeCallback, &mainKit);
 
-        LOG_INF("Kit poll terminated.");
+        LOG_INF("Kit unipoll loop run terminated.");
 
 #if MOBILEAPP
         SocketPoll::wakeupWorld();
