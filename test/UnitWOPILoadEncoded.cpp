b934c70f4f1f9f3ee4f873570633a3b1ec931652
Henry Castro <hcastro@collabora.com>
2019-05-15 22:14:29 -0400

    wsd: fix the exception "Bad URI syntax"
    if the file name has '%' character it throws
    exception "Bad URI syntax", so use the encoded
    file name string
    
    Change-Id: I6a8d841761b46ac49a6c79956ba215e1affa53e5
    Reviewed-on: https://gerrit.libreoffice.org/72385
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    Tested-by: Andras Timar <andras.timar@collabora.com>

---
+++ test/UnitWOPILoadEncoded.cpp
@@ -0,0 +1,71 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <config.h>
+
+#include <WopiTestServer.hpp>
+#include <Log.hpp>
+#include <Unit.hpp>
+#include <UnitHTTP.hpp>
+#include <helpers.hpp>
+#include <Poco/Net/HTTPRequest.h>
+#include <Poco/Util/LayeredConfiguration.h>
+
+class UnitWOPILoadEncoded : public WopiTestServer
+{
+    enum class Phase
+    {
+        LoadEncoded,
+        CloseDoc,
+        Polling
+    } _phase;
+
+public:
+    UnitWOPILoadEncoded() :
+        _phase(Phase::LoadEncoded)
+    {
+    }
+
+    void invokeTest() override
+    {
+        constexpr char testName[] = "UnitWOPILoadEncoded";
+
+        switch (_phase)
+        {
+            case Phase::LoadEncoded:
+            {
+                initWebsocket("/wopi/files/3?access_token=anything");
+
+                helpers::sendTextFrame(*_ws->getLOOLWebSocket(), "load url=" + _wopiSrc, testName);
+                SocketPoll::wakeupWorld();
+
+                _phase = Phase::CloseDoc;
+                break;
+            }
+            case Phase::CloseDoc:
+            {
+                helpers::sendTextFrame(*_ws->getLOOLWebSocket(), "closedocument", testName);
+                _phase = Phase::Polling;
+                break;
+            }
+            case Phase::Polling:
+            {
+                exitTest(TestResult::Ok);
+                break;
+            }
+        }
+    }
+};
+
+UnitBase *unit_create_wsd(void)
+{
+    return new UnitWOPILoadEncoded();
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
