0458fbb0abb0be2ca3e1d4860f1b611f844ae7bd
Michael Meeks <michael.meeks@collabora.com>
2019-05-21 19:50:17 +0100

    tdf#123482 - cleanup convert-to folder even more reliably.
    Problems could occur if exceptiosn thrown when parsing the input stream.
    
    Change-Id: Id82b3816450194164fc2093554c730b4a94acef1
    Reviewed-on: https://gerrit.libreoffice.org/72695
    Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
    Tested-by: Michael Meeks <michael.meeks@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/72744
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    Tested-by: Andras Timar <andras.timar@collabora.com>

--- wsd/LOOLWSD.cpp
+++ wsd/LOOLWSD.cpp
@@ -570,6 +570,9 @@ class ConvertToPartHandler : public PartHandler
 public:
     std::string getFilename() const { return _filename; }
 
+    /// Afterwards someone else is responsible for cleaning that up.
+    void takeFile() { _filename.clear(); }
+
     ConvertToPartHandler(bool convertTo = false)
         : _convertTo(convertTo)
     {
@@ -577,6 +580,11 @@ public:
 
     virtual ~ConvertToPartHandler()
     {
+        if (!_filename.empty())
+        {
+            LOG_TRC("Remove un-handled temporary file '" << _filename << "'");
+            ConvertToBroker::removeFile(_filename);
+        }
     }
 
     virtual void handlePart(const MessageHeader& header, std::istream& stream) override
@@ -2371,6 +2379,7 @@ private:
 
                     LOG_DBG("New DocumentBroker for docKey [" << docKey << "].");
                     auto docBroker = std::make_shared<ConvertToBroker>(fromPath, uriPublic, docKey);
+                    handler.takeFile();
 
                     cleanupDocBrokers();
 
