b40cf51181ecf6d65f25238b84da0f98176fc249
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2019-05-22 08:31:59 +0200

    Log fatal errors also to stderr
    Change-Id: I5e6429ab007d13a2506c375923049b91c80ffba0
    Reviewed-on: https://gerrit.libreoffice.org/72722
    Reviewed-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
    Tested-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>

--- common/Log.hpp
+++ common/Log.hpp
@@ -15,6 +15,7 @@
 
 #include <cstddef>
 #include <functional>
+#include <iostream>
 #include <thread>
 #include <sstream>
 #include <string>
@@ -341,6 +342,7 @@ namespace Log
 #define LOG_FTL(X)                                  \
     do                                              \
     {                                               \
+        std::cerr << X << std::endl;                \
         auto &log_ = Log::logger();                 \
         if (log_.fatal())                           \
         {                                           \
