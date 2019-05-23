79468654f54358c77ee8240053cccdd69fc055b3
Florin Ciornei <florin.ciornei@collabora.com>
2019-05-23 13:46:22 +0200

    Set focus on document after closing copy/paste/cut warning.
    Change-Id: I4e241278bea441f77e6b7780d69e540f9e6d4771
    Reviewed-on: https://gerrit.libreoffice.org/72838
    Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
    Tested-by: Michael Meeks <michael.meeks@collabora.com>

--- loleaflet/src/control/Control.Menubar.js
+++ loleaflet/src/control/Control.Menubar.js
@@ -794,7 +794,14 @@ L.Control.Menubar = L.Control.extend({
 		} else if (id === 'repair') {
 			this._map._socket.sendMessage('commandvalues command=.uno:DocumentRepair');
 		} else if (id === 'warn-copy-paste') {
-			vex.dialog.alert(_('<p>Your browser has very limited access to the clipboard, so use these keyboard shortcuts:<ul><li><b>Ctrl+C</b>: For copying.</li><li><b>Ctrl+X</b>: For cutting.</li><li><b>Ctrl+V</b>: For pasting.</li></ul></p>'));
+			var self = this;
+			vex.dialog.alert({
+				message: _('<p>Your browser has very limited access to the clipboard, so use these keyboard shortcuts:<ul><li><b>Ctrl+C</b>: For copying.</li><li><b>Ctrl+X</b>: For cutting.</li><li><b>Ctrl+V</b>: For pasting.</li></ul></p>'),
+				callback: function () {
+					self._map.focus();
+				}
+			});
+
 		}
 		// Inform the host if asked
 		if ($(item).data('postmessage') === 'true') {
