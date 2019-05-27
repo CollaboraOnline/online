a5116600b30450f48b4f5f75e5c72861e2d2e00e
Szymon Kłos <szymon.klos@collabora.com>
2019-05-27 18:20:44 +0200

    Enable hamburger-menu button by default
    Change-Id: If73c7fd11de6fc40bc2065af958292ef51a17482

--- loleaflet/src/control/Control.Toolbar.js
+++ loleaflet/src/control/Control.Toolbar.js
@@ -2051,7 +2051,7 @@ function onUpdatePermission(e) {
 	var toolbar = w2ui['editbar'];
 	if (toolbar) {
 		// always enabled items
-		var enabledButtons = ['closemobile', 'undo', 'redo'];
+		var enabledButtons = ['closemobile', 'undo', 'redo', 'hamburger-tablet'];
 
 		// copy the first array
 		var items = toolbar.items.slice();
