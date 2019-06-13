f66c5855955928b45f4c03707bbd7ea4a4581644
Andras Timar <andras.timar@collabora.com>
2019-06-13 12:14:01 +0200

    docker: fix SSL cert generation with OpenSSL 1.1.1
    Change-Id: I1534cd3d3da19ac17e65571e6e2f7e7b0a8e61ca
    Reviewed-on: https://gerrit.libreoffice.org/73953
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    Tested-by: Andras Timar <andras.timar@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/74434

--- docker/scripts/run-lool.sh
+++ docker/scripts/run-lool.sh
@@ -15,12 +15,13 @@ if test "${DONT_GEN_SSL_CERT-set}" == set; then
 mkdir -p /opt/ssl/
 cd /opt/ssl/
 mkdir -p certs/ca
+openssl rand -writerand /root/.rnd
 openssl genrsa -out certs/ca/root.key.pem 2048
 openssl req -x509 -new -nodes -key certs/ca/root.key.pem -days 9131 -out certs/ca/root.crt.pem -subj "/C=DE/ST=BW/L=Stuttgart/O=Dummy Authority/CN=Dummy Authority"
 mkdir -p certs/{servers,tmp}
-mkdir -p "certs/servers/localhost"
-openssl genrsa -out "certs/servers/localhost/privkey.pem" 2048 -key "certs/servers/localhost/privkey.pem"
-openssl req -key "certs/servers/localhost/privkey.pem" -new -sha256 -out "certs/tmp/localhost.csr.pem" -subj "/C=DE/ST=BW/L=Stuttgart/O=Dummy Authority/CN=localhost"
+mkdir -p certs/servers/localhost
+openssl genrsa -out certs/servers/localhost/privkey.pem 2048
+openssl req -key certs/servers/localhost/privkey.pem -new -sha256 -out certs/tmp/localhost.csr.pem -subj "/C=DE/ST=BW/L=Stuttgart/O=Dummy Authority/CN=localhost"
 openssl x509 -req -in certs/tmp/localhost.csr.pem -CA certs/ca/root.crt.pem -CAkey certs/ca/root.key.pem -CAcreateserial -out certs/servers/localhost/cert.pem -days 9131
 mv certs/servers/localhost/privkey.pem /etc/loolwsd/key.pem
 chown lool: /etc/loolwsd/key.pem
