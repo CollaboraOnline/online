931ae5bb96fd07e50953429cc199854c37119b98
Jan Holesovsky <kendy@collabora.com>
2019-07-11 11:29:13 +0200

    android: Split the actual editing Activity into an own library.
    This way, it is more naturally visible what is the actuall app (with the
    initial recent documents / file picker) and the editing part.
    
    Change-Id: Ia764f2900939e980f703e3da9f9abd6c0aee7cbb

--- android/lib/src/main/java/org/libreoffice/androidlib/SlideShowActivity.java
+++ android/lib/src/main/java/org/libreoffice/androidlib/SlideShowActivity.java
@@ -7,7 +7,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-package org.libreoffice.androidapp;
+package org.libreoffice.androidlib;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
