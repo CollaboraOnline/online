a91025e998e1835947b6bc2da5b26350c6dc886b
Tor Lillqvist <tml@collabora.com>
2019-08-19 18:35:29 +0300

    tdf#126972: Don't fold the menubar in the iOS app on a tablet
    As the menubar is now then always visible in that case, no need to
    have the hamburger button in the toolbar either.
    
    Change-Id: Iec1ae28858ad994d008c294e5fa4069c5e9bc7ed
    Reviewed-on: https://gerrit.libreoffice.org/77740
    Reviewed-by: Tor Lillqvist <tml@collabora.com>
    Tested-by: Tor Lillqvist <tml@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/77787

--- loleaflet/src/control/Control.Toolbar.js
+++ loleaflet/src/control/Control.Toolbar.js
@@ -47,7 +47,10 @@ function _updateVisibilityForToolbar(toolbar) {
 	var toHide = [];
 
 	toolbar.items.forEach(function(item) {
-		if (((isMobile && item.mobile === false) || (isTablet && item.tablet === false) || (isDesktop && item.desktop === false) || (!window.ThisIsAMobileApp && item.mobilebrowser === false)) && !item.hidden) {
+		if (window.ThisIsTheiOSApp && isTablet && item.iosapptablet === false) {
+			toHide.push(item.id);
+		}
+		else if (((isMobile && item.mobile === false) || (isTablet && item.tablet === false) || (isDesktop && item.desktop === false) || (!window.ThisIsAMobileApp && item.mobilebrowser === false)) && !item.hidden) {
 			toHide.push(item.id);
 		}
 		else if (((isMobile && item.mobile === true) || (isTablet && item.tablet === true) || (isDesktop && item.desktop === true) || (window.ThisIsAMobileApp && item.mobilebrowser === true)) && item.hidden) {
@@ -822,7 +825,7 @@ function initNormalToolbar() {
 		{type: 'spacer'},
 		{type: 'button',  id: 'edit',  img: 'edit'},
 		{type: 'button',  id: 'fold',  img: 'fold', desktop: true, mobile: false, hidden: true},
-		{type: 'button',  id: 'hamburger-tablet',  img: 'hamburger', desktop: false, mobile: false, tablet: true, hidden: true}
+		{type: 'button',  id: 'hamburger-tablet',  img: 'hamburger', desktop: false, mobile: false, tablet: true, iosapptablet: false, hidden: true}
 	];
 
 	var toolbar = $('#toolbar-up');
@@ -1498,7 +1501,7 @@ function onDocLayerInit() {
 	toolbarUp.refresh();
 	statusbar.refresh();
 
-	if (window.mode.isTablet()) {
+	if (!window.ThisIsTheiOSApp && window.mode.isTablet()) {
 		map.hideMenubar();
 
 		$('#document-container').addClass('tablet');
