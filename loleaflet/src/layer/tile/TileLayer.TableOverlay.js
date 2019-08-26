45d5715e0d34fefd2c993a812d95b059f1f1963c
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2019-08-26 17:26:41 +0900

    send table border drag positions as offset, not absolute value
    Change-Id: Ie23ff9533eb258acb93a46e00f44141fc7ad12b2
    Reviewed-on: https://gerrit.libreoffice.org/78110
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    Tested-by: Tomaž Vajngerl <quikee@gmail.com>

--- loleaflet/src/layer/tile/TileLayer.TableOverlay.js
+++ loleaflet/src/layer/tile/TileLayer.TableOverlay.js
@@ -42,6 +42,7 @@ L.TileLayer.include({
 		this._map.addLayer(marker);
 		marker._type = markerType + '-' + entry.type;
 		marker._position = parseInt(entry.position);
+		marker._initialPosition = marker._position;
 		marker._min = parseInt(entry.min);
 		marker._max = parseInt(entry.max);
 		marker._index = parseInt(entry.index);
@@ -155,7 +156,6 @@ L.TileLayer.include({
 			e.target._pointTop.lat = aLatLonPosition.lat;
 			e.target._pointBottom.lat = aLatLonPosition.lat;
 		}
-
 		e.target._position = newPosition;
 
 		var bounds = new L.LatLngBounds(e.target._pointTop, e.target._pointBottom);
@@ -171,6 +171,9 @@ L.TileLayer.include({
 			this._map.removeLayer(this._rectangle);
 			this._rectangle = null;
 
+			var offset = newPosition - e.target._initialPosition;
+			e.target._initialPosition =  e.target._position;
+
 			var params = {
 				BorderType: {
 					type : 'string',
@@ -180,9 +183,9 @@ L.TileLayer.include({
 					type : 'uint16',
 					value : e.target._index
 				},
-				NewPosition: {
+				Offset: {
 					type : 'int32',
-					value : e.target._position
+					value : offset
 				}
 			}
 
