77818fbf2d5a0923cb8271e3f3c05fa797a583a9
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2019-09-19 23:42:13 -0400

    wsd: log before killing slow-loading docs
    Change-Id: I85f045d1431feb51cb9ad093833c32a39ac38215
    Reviewed-on: https://gerrit.libreoffice.org/79331
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Jan Holesovsky <kendy@collabora.com>

--- wsd/DocumentBroker.cpp
+++ wsd/DocumentBroker.cpp
@@ -267,8 +267,8 @@ void DocumentBroker::pollThread()
     uint64_t adminRecv = 0;
     auto lastBWUpdateTime = std::chrono::steady_clock::now();
 
-    int limit_load_secs = LOOLWSD::getConfigValue<int>("per_document.limit_load_secs", 100);
-    auto loadDeadline = std::chrono::steady_clock::now() + std::chrono::seconds(limit_load_secs);
+    const int limit_load_secs = LOOLWSD::getConfigValue<int>("per_document.limit_load_secs", 100);
+    const auto loadDeadline = std::chrono::steady_clock::now() + std::chrono::seconds(limit_load_secs);
 #endif
     auto last30SecCheckTime = std::chrono::steady_clock::now();
 
@@ -284,7 +284,14 @@ void DocumentBroker::pollThread()
         {
             // Brutal but effective.
             if (_childProcess)
+            {
+                LOG_WRN("Doc [" << _docKey << "] is taking too long to load. Will kill process ["
+                                << _childProcess->getPid()
+                                << "]. per_document.limit_load_secs set to " << limit_load_secs
+                                << " secs.");
                 _childProcess->terminate();
+            }
+
             stop("Load timed out");
             continue;
         }
