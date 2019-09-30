4e0341fe056300990ab7cca02fd9b9e718f22cb5
Tamás Zolnai <tamas.zolnai@collabora.com>
2019-09-30 13:42:43 +0200

    Fix up TileCacheTests::testTileInvalidateWriterPage() test
    The part number in case of writer has no actual meaning, since
    for writer we have only one part (the whole document).
    It seems the code is not consistent in that it is 0 or 1.
    
    Change-Id: I9e56079f7861f7e0d5794e6f711045cd9448dfa7
    Reviewed-on: https://gerrit.libreoffice.org/79851
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Tamás Zolnai <tamas.zolnai@collabora.com>

--- test/TileCacheTests.cpp
+++ test/TileCacheTests.cpp
@@ -815,7 +815,7 @@ void TileCacheTests::testTileInvalidateWriterPage()
     int part = -1;
     CPPUNIT_ASSERT_MESSAGE("No part# in invalidatetiles message.",
                            LOOLProtocol::getTokenIntegerFromMessage(res, "part", part));
-    CPPUNIT_ASSERT_EQUAL(1, part);
+    CPPUNIT_ASSERT_EQUAL(0, part);
 }
 
 // This isn't yet used
