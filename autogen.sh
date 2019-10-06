f5c6930e752d1d9d4ce84d6e302d6da256b54fa6
Tor Lillqvist <tml@iki.fi>
2019-10-06 14:32:22 +0300

    Use the same libtool snippet as in master
    Sadly, we do need libtool even when building for iOS, just to get the
    configure script produced, which we need to be able to run to get the
    symlink to the core build directory set up, and to copy the app icon
    and online artwork.
    
    (I tried once to get rid of the use of libtool in Online, but it was
    more complicated than I had patience.)
    
    Change-Id: I889f598f7a1134ef528376e948cd35c9f90e8e5c

--- autogen.sh
+++ autogen.sh
@@ -18,6 +18,8 @@ EOF
 
 if test `uname -s` = Linux; then
     libtoolize || failed "libtool"
+elif test `uname -s` = Darwin; then
+    glibtoolize || failed "Can't find glibtoolize. For instance use the one from https://brew.sh, 'brew install libtool', or build GNU libtool yourself."
 fi
 
 aclocal || failed "aclocal"
