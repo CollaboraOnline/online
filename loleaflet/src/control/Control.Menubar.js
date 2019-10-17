bab6017803eca2b6b31d7788ede26ec04294636d
Pedro <pedro.silva@collabora.com>
2019-10-17 09:43:42 +0200

    Style mobilewizard.menuwizard and add css classes via JS to toolbar-hamburger
    Change-Id: I8c455507aa79f4cbba6db56597b8fe350554515b
    Reviewed-on: https://gerrit.libreoffice.org/80930
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>
    Tested-by: Szymon Kłos <szymon.klos@collabora.com>

--- loleaflet/src/control/Control.Menubar.js
+++ loleaflet/src/control/Control.Menubar.js
@@ -798,7 +798,7 @@ L.Control.Menubar = L.Control.extend({
 							window.mobileMenuWizard = true;
 							var menuData = self._map.menubar.generateFullMenuStructure();
 							self._map.fire('mobilewizard', menuData);
-							$('#main-menu-btn-icon').css('filter', 'drop-shadow(0px 0px 4px #0b87e7)')
+							$('#toolbar-hamburger').addClass('menuwizard-opened');
 						}
 					} else if (!L.Browser.mobile) {
 						$menu.show().slideUp(250, function() { $menu.css('display', ''); });
@@ -806,7 +806,7 @@ L.Control.Menubar = L.Control.extend({
 					} else {
 						window.mobileMenuWizard = false;
 						self._map.fire('closemobilewizard');
-						$('#main-menu-btn-icon').css('filter', '')
+						$('#toolbar-hamburger').removeClass('menuwizard-opened').addClass('menuwizard-closed');
 					}
 				});
 				// hide mobile menu beforeunload
