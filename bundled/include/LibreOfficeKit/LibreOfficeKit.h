443ed3274fda3c678508eeeef18f6a356db9cd96
Aron Budea <aron.budea@collabora.com>
2019-10-21 20:18:15 +0200

    lokit: fix order of functions
    74ce4a9261e69cb80e034b9be2a1e6345c28389e introduced a
    different order than in core.
    
    Change-Id: I4d601e1fcd3028c7bbe9d0119c15193fe1db1f35
    Reviewed-on: https://gerrit.libreoffice.org/81270
    Reviewed-by: Aron Budea <aron.budea@collabora.com>
    Tested-by: Aron Budea <aron.budea@collabora.com>

--- bundled/include/LibreOfficeKit/LibreOfficeKit.h
+++ bundled/include/LibreOfficeKit/LibreOfficeKit.h
@@ -199,11 +199,6 @@ struct _LibreOfficeKitDocumentClass
                             int nButtons,
                             int nModifier);
 
-    /// @see lok::Document::sendDialogEvent
-    void (*sendDialogEvent) (LibreOfficeKitDocument* pThis,
-                            unsigned nLOKWindowId,
-                            const char* pArguments);
-
     /// @see lok::Document::postUnoCommand
     void (*postUnoCommand) (LibreOfficeKitDocument* pThis,
                             const char* pCommand,
@@ -418,6 +413,11 @@ struct _LibreOfficeKitDocumentClass
                                unsigned nWindowId,
                                int nBefore,
                                int nAfter);
+
+    /// @see lok::Document::sendDialogEvent
+    void (*sendDialogEvent) (LibreOfficeKitDocument* pThis,
+                            unsigned nLOKWindowId,
+                            const char* pArguments);
 
 #endif // defined LOK_USE_UNSTABLE_API || defined LIBO_INTERNAL_ONLY
 };
