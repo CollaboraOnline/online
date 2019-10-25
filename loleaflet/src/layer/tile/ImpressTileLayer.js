5e035078d6dca7d77a82f397feb22ec1e0a068f2
Pedro <pedro.silva@collabora.com>
2019-10-25 16:58:29 +0200

    Impress: Mobile: Toolbar-up: Remove next and previous record buttons, add fullscreen-presentation and icon
    couldn't find the uno action/command for fullscreen-presentation (it seems there is already a function for that action in the hamburger menu item
    
    Change-Id: I4e25f8f9ceaee15656e44775185cc8ed3856b7f7
    Reviewed-on: https://gerrit.libreoffice.org/81510
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>
    Tested-by: Szymon Kłos <szymon.klos@collabora.com>

--- loleaflet/src/layer/tile/ImpressTileLayer.js
+++ loleaflet/src/layer/tile/ImpressTileLayer.js
@@ -138,11 +138,9 @@ L.ImpressTileLayer = L.TileLayer.extend({
 			items: [
 				{type: 'button',  id: 'closemobile',  img: 'closemobile'},
 				{type: 'spacer'},
-				{type: 'button',  id: 'prev', img: 'prev', hint: _UNO('.uno:PageUp', 'text')},
-				{type: 'button',  id: 'next', img: 'next', hint: _UNO('.uno:PageDown', 'text')},
 				{type: 'button',  id: 'undo',  img: 'undo', hint: _UNO('.uno:Undo'), uno: 'Undo', disabled: true},
 				{type: 'button',  id: 'redo',  img: 'redo', hint: _UNO('.uno:Redo'), uno: 'Redo', disabled: true},
-				{type: 'button',  id: 'fullscreen', img: 'fullscreen', hint: _UNO('.uno:FullScreen', 'text')},
+				{type: 'button',  id: 'fullscreen-presentation', img: 'fullscreen-presentation', hint: _UNO('.uno:FullScreen', 'presentation')},
 				{type: 'button',  id: 'mobile_wizard', img: 'mobile_wizard', disabled: true},
 				{type: 'button',  id: 'insertion_mobile_wizard', img: 'insertion_mobile_wizard', disabled: true},
 				{type: 'button',  id: 'insertcomment', img: 'insertcomment', disabled: true},
