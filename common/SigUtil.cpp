80aa70bd732e2ce4f5ab35b684cfee7469c11421
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2019-11-25 10:49:57 -0500

    wsd: use SIGKILL to kill a child
    This is the cleanest way to achieve the goal
    of immediately exiting a child. This is used
    for cleaning up kit instances when closing
    docs, as well as in unit-tests.
    
    Reviewed-on: https://gerrit.libreoffice.org/83699
    Reviewed-by: Ashod Nakashian <ashnakash@gmail.com>
    Tested-by: Ashod Nakashian <ashnakash@gmail.com>
    (cherry picked from commit a5946b46250a7fd4a63f16c6c0606b0147b13e79)
    
    Change-Id: I76870234b130a508044044b102419646abe81ac8
    Reviewed-on: https://gerrit.libreoffice.org/83759
    Reviewed-by: Ashod Nakashian <ashnakash@gmail.com>
    Tested-by: Ashod Nakashian <ashnakash@gmail.com>

--- common/SigUtil.cpp
+++ common/SigUtil.cpp
@@ -286,7 +286,7 @@ namespace SigUtil
     bool killChild(const int pid)
     {
         LOG_DBG("Killing PID: " << pid);
-        if (kill(pid, SIGTERM) == 0 || errno == ESRCH)
+        if (kill(pid, SIGKILL) == 0 || errno == ESRCH)
         {
             // Killed or doesn't exist.
             return true;
