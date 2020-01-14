f0d6b619e98a399dd667cbdb9000700046795e59
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2020-01-14 06:46:17 -0500

    leaflet: give focus to the dialog on open
    Change-Id: I715025123edf2bcf0564b5ce43c16b5595daed5b
    Reviewed-on: https://gerrit.libreoffice.org/c/online/+/86754
    Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
    Tested-by: Michael Meeks <michael.meeks@collabora.com>

--- loleaflet/src/control/Control.LokDialog.js
+++ loleaflet/src/control/Control.LokDialog.js
@@ -1026,6 +1026,9 @@ L.Control.LokDialog = L.Control.extend({
 			if (parentId in that._dialogs) {
 				// We might have closed the dialog by the time we render.
 				that._dialogs[parentId].isPainting = false;
+				if (!that._isSidebar(parentId)) {
+					that.focus(parentId);
+				}
 			}
 		};
 		img.src = imgData;
