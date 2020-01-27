3ae759eec0aafa2a1c6bf469130e022c590c0532
Tamás Zolnai <tamas.zolnai@collabora.com>
2020-01-27 13:17:45 +0100

    mobile: Fix shape insertion
    Regression from:
    298f4297c21f4b7cd5836b90bafcc803f13adb00
    
    Change-Id: I4b31da57bde7f7ac6a5554f3ff7763f1b4ee76d5

--- loleaflet/src/control/Control.MobileWizard.js
+++ loleaflet/src/control/Control.MobileWizard.js
@@ -291,7 +291,7 @@ L.Control.MobileWizard = L.Control.extend({
 			this._reset();
 
 			var mWizardContentLength;
-			if (data.children[0].type == 'menuitem')
+			if (data.children[0].type == 'menuitem' || data.children[0].type == 'htmlcontrol')
 				mWizardContentLength = data.children.length;
 			else mWizardContentLength = data.children[0].children.length;
 
