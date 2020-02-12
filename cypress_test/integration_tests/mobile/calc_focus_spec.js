aa9d676cd927147ae938198ff1590c144ab59ab6
Tamás Zolnai <tamas.zolnai@collabora.com>
2020-02-12 12:41:05 +0100

    cypress: mobile: Test focus on second tap in Calc.
    Change-Id: I1ce1b45f17f1d75c4bc7c1a6a63dfd05f2803612
    Reviewed-on: https://gerrit.libreoffice.org/c/online/+/88520
    Tested-by: Tamás Zolnai <tamas.zolnai@collabora.com>
    Reviewed-by: Tamás Zolnai <tamas.zolnai@collabora.com>

--- cypress_test/integration_tests/mobile/calc_focus_spec.js
+++ cypress_test/integration_tests/mobile/calc_focus_spec.js
@@ -40,4 +40,34 @@ describe('Calc focus tests', function() {
 		cy.document().its('activeElement.tagName')
 			.should('be.eq', 'BODY');
 	});
+
+	it('Focus on second tap.', function() {
+		// Click on edit button
+		cy.get('#mobile-edit-button').click();
+
+		cy.get('#tb_actionbar_item_mobile_wizard')
+			.should('not.have.class', 'disabled');
+
+		// Body has the focus -> can't type in the document
+		cy.document().its('activeElement.tagName')
+			.should('be.eq', 'BODY');
+
+		// One tap on a cell -> no document focus
+		cy.get('#document-container')
+			.click();
+
+		cy.get('.leaflet-marker-icon.spreadsheet-cell-resize-marker');
+
+		// No focus
+		cy.document().its('activeElement.tagName')
+			.should('be.eq', 'BODY');
+
+		// Second tap on the same cell
+		cy.get('#document-container')
+			.click();
+
+		// Document has the focus
+		cy.document().its('activeElement.className')
+			.should('be.eq', 'clipboard');
+	});
 });
