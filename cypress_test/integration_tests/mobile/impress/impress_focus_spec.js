c692487accb3f64a9af4528d1efb5ba9d51358b9
Tamás Zolnai <tamas.zolnai@collabora.com>
2020-02-24 11:22:33 +0100

    cypress: mobile: split this test case.
    So it might be more stable.
    
    Change-Id: Ie8b8a13ebfe9b681a8e9a456f24f6322ce9cc8a6

--- cypress_test/integration_tests/mobile/impress/impress_focus_spec.js
+++ cypress_test/integration_tests/mobile/impress/impress_focus_spec.js
@@ -11,7 +11,7 @@ describe('Impress focus tests', function() {
 		helper.afterAll();
 	});
 
-	it('Basic document focus.', function() {
+	it('Single tap on a text shape.', function() {
 		// Click on edit button
 		cy.get('#mobile-edit-button').click();
 
@@ -33,14 +33,30 @@ describe('Impress focus tests', function() {
 		// No focus
 		cy.document().its('activeElement.tagName')
 			.should('be.eq', 'BODY');
+	});
+
+	it('Double tap on a text shape.', function() {
+		// Click on edit button
+		cy.get('#mobile-edit-button').click();
+
+		cy.get('#tb_actionbar_item_mobile_wizard')
+			.should('not.have.class', 'disabled');
+
+		// Body has the focus -> can't type in the document
+		cy.document().its('activeElement.tagName')
+			.should('be.eq', 'BODY');
 
 		// Double tap on a text shape gives the focus to the document
 		cy.get('#document-container')
 			.dblclick();
 
+		// Shape selection
+		cy.get('.leaflet-pane.leaflet-overlay-pane svg g')
+			.should('exist');
+
 		// Document has the focus
 		// TODO: Focus is inconsistent here.
-		//cy.document().its('activeElement.className')
-		//	.should('be.eq', 'clipboard');
+		cy.document().its('activeElement.className')
+			.should('be.eq', 'clipboard');
 	});
 });
