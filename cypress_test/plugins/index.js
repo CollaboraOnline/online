dc15a4c398a1fbe2bd686e13d88aefa960a19eb1
Tamás Zolnai <zolnaitamas2000@gmail.com>
2020-03-13 13:47:15 +0100

    cypress: integrate cypress-failed-log package.
    To get a better log in case of error.
    
    Change-Id: Id8f72c78cb8a80e30ba3012147caa8106e1e7ce2
    Reviewed-on: https://gerrit.libreoffice.org/c/online/+/90470
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Tamás Zolnai <tamas.zolnai@collabora.com>

--- cypress_test/plugins/index.js
+++ cypress_test/plugins/index.js
@@ -4,7 +4,8 @@ var tasks = require('./tasks');
 
 function plugin(on, config) {
 	on('task', {
-		copyFile: tasks.copyFile
+		copyFile: tasks.copyFile,
+		failed: require('cypress-failed-log/src/failed')()
 	});
 
 	return config;
