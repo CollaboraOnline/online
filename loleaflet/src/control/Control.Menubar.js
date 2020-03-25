b7de8a91dc79afd999411e491cb5dead19a83ec3
mert <mert.tumer@collabora.com>
2020-03-25 23:15:10 +0300

    mobile: Add search in the readonly mode
    Change-Id: I059b0337096f9eed21031724dbec22f29807a979
    Reviewed-on: https://gerrit.libreoffice.org/c/online/+/91072
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Jan Holesovsky <kendy@collabora.com>
    (cherry picked from commit f634264d89896be25f2853952241afe4e9d4c45c)
    Reviewed-on: https://gerrit.libreoffice.org/c/online/+/91722
    Tested-by: Jan Holesovsky <kendy@collabora.com>

--- loleaflet/src/control/Control.Menubar.js
+++ loleaflet/src/control/Control.Menubar.js
@@ -489,6 +489,7 @@ L.Control.Menubar = L.Control.extend({
 				{uno: '.uno:Paste'},
 				{uno: '.uno:SelectAll'}
 			]},
+			{name: _('Search'), id: 'searchdialog', type: 'action'},
 			{name: _UNO('.uno:ChangesMenu', 'text'), id: 'changesmenu', type: 'menu', menu: [
 				{uno: '.uno:TrackChanges'},
 				{uno: '.uno:ShowTrackedChanges'},
@@ -535,6 +536,7 @@ L.Control.Menubar = L.Control.extend({
 				{uno: '.uno:Paste'},
 				{uno: '.uno:SelectAll'}
 			]},
+			{name: _('Search'), id: 'searchdialog', type: 'action'},
 			{name: _UNO('.uno:TableMenu', 'text'/*HACK should be 'presentation', but not in xcu*/), id: 'tablemenu', type: 'menu', menu: [
 				{uno: '.uno:InsertRowsBefore'},
 				{uno: '.uno:InsertRowsAfter'},
@@ -580,6 +582,7 @@ L.Control.Menubar = L.Control.extend({
 				{uno: '.uno:Paste'},
 				{uno: '.uno:SelectAll'}
 			]},
+			{name: _('Search'), id: 'searchdialog', type: 'action'},
 			{name: _UNO('.uno:SheetMenu', 'spreadsheet'), id: 'sheetmenu', type: 'menu', menu: [
 				{name: _UNO('.uno:InsertRowsMenu', 'spreadsheet'), id: 'insertrowsmenu', type: 'menu', menu: [
 					{uno: '.uno:InsertRowsBefore'},
@@ -1194,6 +1197,11 @@ L.Control.Menubar = L.Control.extend({
 			}
 		} else if (id === 'repair') {
 			this._map._socket.sendMessage('commandvalues command=.uno:DocumentRepair');
+		} else if (id === 'searchdialog') {
+			$('#toolbar-down').hide();
+			$('#toolbar-search').show();
+			$('#mobile-edit-button').hide();
+			L.DomUtil.get('search-input').focus();
 		}
 		// Inform the host if asked
 		if (postmessage)
