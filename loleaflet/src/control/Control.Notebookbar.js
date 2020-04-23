8b2084928bb0147b12dc3b87f3c1dec203516428
Szymon Kłos <szymon.klos@collabora.com>
2020-04-23 13:08:49 +0200

    notebookbar: load selected tab
    Change-Id: Ic2741f972c96342abb2916111d52d8e0c3823349
    Reviewed-on: https://gerrit.libreoffice.org/c/online/+/93270
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

--- loleaflet/src/control/Control.Notebookbar.js
+++ loleaflet/src/control/Control.Notebookbar.js
@@ -12,13 +12,31 @@ L.Control.Notebookbar = L.Control.extend({
 	onAdd: function (map) {
 		this.map = map;
 
-		var homeTab = JSON.parse(this.getHomeTab());
+		this.loadTab(this.getHomeTab());
+	},
+
+	clearNotebookbar: function() {
+		$('.root-container.notebookbar').remove();
+		$('.ui-tabs.notebookbar').remove();
+	},
+
+	loadTab: function(tabJSON) {
+		this.clearNotebookbar();
+		tabJSON = JSON.parse(tabJSON);
 		var builder = new L.control.notebookbarBuilder({mobileWizard: this, map: this.map, cssClass: 'notebookbar'});
-		builder.build($('#toolbar-wrapper').get(0), [homeTab]);
+		builder.build($('#toolbar-wrapper').get(0), [tabJSON]);
 	},
 
 	setTabs: function(tabs) {
 		$('nav').prepend(tabs);
+	},
+
+	selectedTab: function(tabText) {
+		switch (tabText) {
+		case 'Home':
+			this.loadTab(this.getHomeTab());
+			break;
+		}
 	},
 
 	// required, called by builder, not needed in this container
