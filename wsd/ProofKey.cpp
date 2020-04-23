25bc0a1088d308f30f1705df96b13d650114a357
Andras Timar <andras.timar@collabora.com>
2020-04-23 15:55:32 +0200

    Proof: add loolwsd-generate-proof-key helper script
    Change-Id: Ibbd99b6431b1a2992c520d3fad5f52d0770905f6
    Reviewed-on: https://gerrit.libreoffice.org/c/online/+/92788
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Andras Timar <andras.timar@collabora.com>

--- wsd/ProofKey.cpp
+++ wsd/ProofKey.cpp
@@ -142,8 +142,11 @@ Proof::Proof()
         {
             std::string msg = e.displayText() +
                 "\nNo proof-key will be present in discovery."
-                "\nIf you need to use WOPI security, generate an RSA key using this command line:"
-                "\n    ssh-keygen -t rsa -N \"\" -m PEM -f \"" + keyPath + "\"";
+                "\nIf you need to use WOPI security, generate an RSA key using this command:"
+                "\n    loolwsd-generate-proof-key"
+                "\nor if your config dir is not /etc, you can run ssh-keygen manually:"
+                "\n    ssh-keygen -t rsa -N \"\" -m PEM -f \"" + keyPath + "\""
+                "\nNote: the proof_key file must be readable by the loolwsd process.";
             LOG_WRN(msg);
         }
         catch (const Poco::Exception& e)
