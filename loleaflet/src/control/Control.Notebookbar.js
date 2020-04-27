878ecc8ab04e2a55040c31dbbf79e48bdb1ad006
Szymon Kłos <szymon.klos@collabora.com>
2020-04-27 17:01:43 +0200

    notebookbar: handle resize
    Change-Id: Ife1be7147b9f11098ec7ed6264dd0b4a1e47c354
    Reviewed-on: https://gerrit.libreoffice.org/c/online/+/93282
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

--- loleaflet/src/control/Control.Notebookbar.js
+++ loleaflet/src/control/Control.Notebookbar.js
@@ -13,18 +13,26 @@ L.Control.Notebookbar = L.Control.extend({
 		this.map = map;
 
 		this.loadTab(this.getHomeTab());
+
+		this.createScrollButtons();
+		this.setupResizeHandler();
 	},
 
 	clearNotebookbar: function() {
 		$('.root-container.notebookbar').remove();
 		$('.ui-tabs.notebookbar').remove();
+		$('.notebookbar-scroll-wrapper').remove();
 	},
 
 	loadTab: function(tabJSON) {
 		this.clearNotebookbar();
 		tabJSON = JSON.parse(tabJSON);
 		var builder = new L.control.notebookbarBuilder({mobileWizard: this, map: this.map, cssClass: 'notebookbar'});
-		builder.build($('#toolbar-wrapper').get(0), [tabJSON]);
+
+		var parent = $('#toolbar-wrapper').get(0);
+		var container = L.DomUtil.create('div', 'notebookbar-scroll-wrapper', parent);
+
+		builder.build(container, [tabJSON]);
 	},
 
 	setTabs: function(tabs) {
@@ -58,6 +66,51 @@ L.Control.Notebookbar = L.Control.extend({
 
 	// required, called by builder, not needed in this container
 	setCurrentScrollPosition: function() {},
+
+	createScrollButtons: function() {
+		var parent = $('#toolbar-wrapper').get(0);
+
+		var left = L.DomUtil.create('div', 'w2ui-scroll-left', parent);
+		var right = L.DomUtil.create('div', 'w2ui-scroll-right', parent);
+
+		$(left).css({'height': '80px'});
+		$(right).css({'height': '80px'});
+
+		$(left).click(function () {
+			var scroll = $('.notebookbar-scroll-wrapper').scrollLeft() - 300;
+			$('.notebookbar-scroll-wrapper').animate({ scrollLeft: scroll }, 300);
+			setTimeout(function () { $(window).resize(); }, 350);
+		});
+
+		$(right).click(function () {
+			var scroll = $('.notebookbar-scroll-wrapper').scrollLeft() + 300;
+			$('.notebookbar-scroll-wrapper').animate({ scrollLeft: scroll }, 300);
+			setTimeout(function () { $(window).resize(); }, 350);
+		});
+	},
+
+	setupResizeHandler: function() {
+		$(window).resize(function() {
+			var container = $('#toolbar-wrapper').get(0);
+			var rootContainer = $('.notebookbar-scroll-wrapper table').get(0);
+
+			if ($(rootContainer).outerWidth() > $(window).width()) {
+				// we have overflowed content
+				if ($('.notebookbar-scroll-wrapper').scrollLeft() > 0)
+					$(container).find('.w2ui-scroll-left').show();
+				else
+					$(container).find('.w2ui-scroll-left').hide();
+
+				if ($('.notebookbar-scroll-wrapper').scrollLeft() < $(rootContainer).outerWidth() - $(window).width() - 1)
+					$(container).find('.w2ui-scroll-right').show();
+				else
+				$(container).find('.w2ui-scroll-right').hide();
+			} else {
+				$(container).find('.w2ui-scroll-left').hide();
+				$(container).find('.w2ui-scroll-right').hide();
+			}
+		});
+	},
 
 	getHomeTab: function() {
 		return '';
