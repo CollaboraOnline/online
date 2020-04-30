33dbdffcaf176594b6b965712376a1d59da4925b
Tamás Zolnai <tamas.zolnai@collabora.com>
2020-04-30 14:34:09 +0200

    MSForms: styling form field button.
    Change-Id: I4bbcc16aa8afb0fa7e8e84d34e1f18086f0b8615
    (cherry picked from commit 87b862a4e2fafe2c420f672af835929d588757a2)
    Reviewed-on: https://gerrit.libreoffice.org/c/online/+/93883
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Andras Timar <andras.timar@collabora.com>

--- loleaflet/src/layer/FormFieldButtonLayer.js
+++ loleaflet/src/layer/FormFieldButtonLayer.js
@@ -20,11 +20,14 @@ L.FormFieldButton = L.Layer.extend({
 	},
 
 	onAdd: function (map) {
-		if (this._button) {
-			L.DomUtil.remove(this._button);
-		}
+		this._clearButton();
 
-		this._button = L.DomUtil.create('div', 'drop-down-button', this.getPane('formfieldPane'));
+		this._buildFormButton(map);
+	},
+
+	_buildFormButton: function(map) {
+		// Create a frame around the text area
+		this._frame = L.DomUtil.create('div', 'form-field-frame', this.getPane('formfieldPane'));
 		var buttonAreaLatLng = new L.LatLngBounds(
 				map._docLayer._twipsToLatLng(this._buttonAreaTwips[0], this._map.getZoom()),
 				map._docLayer._twipsToLatLng(this._buttonAreaTwips[1], this._map.getZoom()));
@@ -33,17 +36,40 @@ L.FormFieldButton = L.Layer.extend({
 				this._map.latLngToLayerPoint(buttonAreaLatLng.getNorthWest()),
 				this._map.latLngToLayerPoint(buttonAreaLatLng.getSouthEast()));
 
+		// Use a small padding between the text and the frame
+		var extraPadding = 2;
 		var size = buttonAreaLayer.getSize();
-		this._button.style.width  = size.x + 'px';
-		this._button.style.height = size.y + 'px';
+		this._frame.style.width = (size.x + 1.5 * extraPadding) + 'px';
 
-		var pos = buttonAreaLayer.min;
-		L.DomUtil.setPosition(this._button, pos);
+		this.getPane('formfieldPane').style.height = (size.y + 1.5 * extraPadding) + 'px';
+
+		var framePos = new L.Point(buttonAreaLayer.min.x - extraPadding, buttonAreaLayer.min.y - extraPadding);
+		L.DomUtil.setPosition(this._frame, framePos);
+
+		// Add a drop down button to open the list
+		this._button = L.DomUtil.create('button', 'form-field-button', this.getPane('formfieldPane'));
+		var buttonPos = new L.Point(buttonAreaLayer.max.x + extraPadding, buttonAreaLayer.min.y - extraPadding);
+		L.DomUtil.setPosition(this._button, buttonPos);
+		this._button.style.width = this.getPane('formfieldPane').style.height;
+
+		var image = L.DomUtil.create('img', 'form-field-button-image', this._button);
+		image.src = 'images/unfold.svg';
 	},
 
 	onRemove: function () {
-		L.DomUtil.remove(this._button);
-		this._button = undefined;
+		this._clearButton();
 	},
+
+	_clearButton: function() {
+		this.getPane('formfieldPane').innerHTML = '';
+		if (this._frame) {
+			L.DomUtil.remove(this._frame);
+			this._frame = undefined;
+		}
+		if (this._button) {
+			L.DomUtil.remove(this._button);
+			this._button = undefined;
+		}
+	}
 
 });
