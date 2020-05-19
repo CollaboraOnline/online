f0b0eed3876f96523670945e298c3f968df3cd4f
Dennis Francis <dennis.francis@collabora.com>
2020-05-19 18:51:11 +0530

    handle LOK_CALLBACK_INVALIDATE_SHEET_GEOMETRY
    Change-Id: I2f894f32d4c9e852d89159a55c0dd9effb45c09e

--- kit/ChildSession.cpp
+++ kit/ChildSession.cpp
@@ -2268,6 +2268,7 @@ void ChildSession::rememberEventsForInactiveUser(const int type, const std::stri
              type == LOK_CALLBACK_GRAPHIC_SELECTION ||
              type == LOK_CALLBACK_DOCUMENT_SIZE_CHANGED ||
              type == LOK_CALLBACK_INVALIDATE_HEADER ||
+             type == LOK_CALLBACK_INVALIDATE_SHEET_GEOMETRY ||
              type == LOK_CALLBACK_CELL_ADDRESS ||
              type == LOK_CALLBACK_REFERENCE_MARKS)
     {
@@ -2592,6 +2593,9 @@ void ChildSession::loKitCallback(const int type, const std::string& payload)
         break;
     case LOK_CALLBACK_FORM_FIELD_BUTTON:
         sendTextFrame("formfieldbutton: " + payload);
+        break;
+    case LOK_CALLBACK_INVALIDATE_SHEET_GEOMETRY:
+        sendTextFrame("invalidatesheetgeometry: " + payload);
         break;
 
 #if !ENABLE_DEBUG
