bec19d433805d14776d0f3cf8d333b1e1924b37e
Michael Meeks <michael.meeks@collabora.com>
2020-05-20 21:48:24 +0100

    Cleanup string and substitute protocol.
    Change-Id: Iaa229ec346037788b9de7e5f59038b8afe0f7136
    Reviewed-on: https://gerrit.libreoffice.org/c/online/+/94607
    Tested-by: Jenkins
    Reviewed-by: Andras Timar <andras.timar@collabora.com>

--- loleaflet/admin/src/AdminSocketOverview.js
+++ loleaflet/admin/src/AdminSocketOverview.js
@@ -401,7 +401,10 @@ var AdminSocketOverview = AdminSocketBase.extend({
 				msg =  _('Failed to set jwt authentication cookie over insecure connection');
 			}
 			else
-				msg =  _('Failed to authenticate this session over');
+			{
+				msg =  _('Failed to authenticate this session over protocol %0');
+				msg = msg.replace('%0', window.location.protocol);
+			}
 			vex.dialog.alert({ message: msg });
 		}
 	},
