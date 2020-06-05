2e51ecb0a4e81b3370a52bc214cf377e58570eec
Tor Lillqvist <tml@collabora.com>
2020-06-05 14:20:47 +0300

    Avoid assertion failure in the iOS app
    Change-Id: I5e0733579db01616f8c6e7743bb01a9a2feca8aa
    Reviewed-on: https://gerrit.libreoffice.org/c/online/+/95591
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Tor Lillqvist <tml@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/online/+/95768
    Reviewed-by: Andras Timar <andras.timar@collabora.com>

--- wsd/RequestDetails.cpp
+++ wsd/RequestDetails.cpp
@@ -74,13 +74,16 @@ RequestDetails::RequestDetails(Poco::Net::HTTPRequest &request)
 }
 
 RequestDetails::RequestDetails(const std::string &mobileURI)
-    : _isGet(false)
+    : _isGet(true)
     , _isHead(false)
     , _isProxy(false)
     , _isWebSocket(false)
 {
     _isMobile = true;
     _uriString = mobileURI;
+    // Not sure if these are correct in the case of file names that need URI-encoding.
+    _fields[Field::LegacyDocumentURI] = _uriString;
+    _fields[Field::DocumentURI] = _uriString;
 }
 
 std::string RequestDetails::getDocumentURI() const
