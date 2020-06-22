f0ecb354471ed230abfe4c83af45cf18b817e758
Tor Lillqvist <tml@collabora.com>
2020-06-22 13:34:22 +0300

    Bin spurious misleading output in the MOBILEAPP case
    Change-Id: Ia731ac97174aa85842a156753bcd9e8a0fef9718
    Reviewed-on: https://gerrit.libreoffice.org/c/online/+/96842
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Tested-by: Jenkins
    Reviewed-by: Tor Lillqvist <tml@collabora.com>

--- wsd/LOOLWSD.cpp
+++ wsd/LOOLWSD.cpp
@@ -3787,7 +3787,10 @@ int LOOLWSD::innerMain()
 
     /// The main-poll does next to nothing:
     SocketPoll mainWait("main");
+
+#if !MOBILEAPP
     std::cerr << "Ready to accept connections on port " << ClientPortNumber <<  ".\n" << std::endl;
+#endif
 
     // Reset the child-spawn timeout to the default, now that we're set.
     ChildSpawnTimeoutMs = CHILD_TIMEOUT_MS;
