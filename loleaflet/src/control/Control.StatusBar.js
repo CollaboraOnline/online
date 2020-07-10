962838ca82cd077147bd2a7c097fbb609044d0c8
gokaysatir <gokaysatir@collabora.com>
2020-07-10 16:36:08 +0300

    loleaflet: Selected cell status alignment.
    Change-Id: Ibdf5c7ae7d95a97682bac1574b6abb0d00471445
    Reviewed-on: https://gerrit.libreoffice.org/c/online/+/98528
    Tested-by: Jenkins
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Henry Castro <hcastro@collabora.com>

--- loleaflet/src/control/Control.StatusBar.js
+++ loleaflet/src/control/Control.StatusBar.js
@@ -65,10 +65,10 @@ L.Control.StatusBar = L.Control.extend({
 	_updateVisibilityForToolbar: function(toolbar) {
 		if (!toolbar)
 			return;
-	
+
 		var toShow = [];
 		var toHide = [];
-	
+
 		toolbar.items.forEach(function(item) {
 			if (window.ThisIsTheiOSApp && window.mode.isTablet() && item.iosapptablet === false) {
 				toHide.push(item.id);
@@ -80,10 +80,10 @@ L.Control.StatusBar = L.Control.extend({
 				toShow.push(item.id);
 			}
 		});
-	
+
 		console.log('explicitly hiding: ' + toHide);
 		console.log('explicitly showing: ' + toShow);
-	
+
 		toHide.forEach(function(item) { toolbar.hide(item); });
 		toShow.forEach(function(item) { toolbar.show(item); });
 	},
@@ -287,7 +287,7 @@ L.Control.StatusBar = L.Control.extend({
 		case 'spreadsheet':
 			if (statusbar)
 				statusbar.remove('prev', 'next', 'prevnextbreak');
-			
+
 			if (!window.mode.isMobile()) {
 				statusbar.insert('left', [
 					{type: 'break', id: 'break1'},
@@ -298,7 +298,7 @@ L.Control.StatusBar = L.Control.extend({
 					{type: 'break', id: 'break2'},
 					{
 						type: 'html', id: 'RowColSelCount',
-						html: '<div id="RowColSelCount" class="loleaflet-font" title="' + _('Selected range of cells') + '" style="padding: 5px 5px;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp</div>'
+						html: '<div id="RowColSelCount" class="loleaflet-font" title="' + _('Selected range of cells') + '" style="padding: 5px 5px;line-height:0;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp</div>'
 					},
 					{type: 'break', id: 'break3', tablet: false},
 					{
@@ -383,7 +383,7 @@ L.Control.StatusBar = L.Control.extend({
 					{type: 'break', id: 'break8', mobile: false}
 				]);
 			}
-		
+
 		// FALLTHROUGH intended
 		case 'drawing':
 			if (statusbar)
@@ -412,7 +412,7 @@ L.Control.StatusBar = L.Control.extend({
 			// odd, but on mobile we need to invoke it twice
 			toolbar.hide('cancelsearch');
 		}
-	
+
 		this.map._onGotFocus();
 	},
 
@@ -445,7 +445,7 @@ L.Control.StatusBar = L.Control.extend({
 		}
 		else if (commandName === '.uno:InsertMode') {
 			this.updateToolbarItem(statusbar, 'InsertMode', $('#InsertMode').html(state ? L.Styles.insertMode[state].toLocaleString() : '<span class="ToolbarStatusInactive">&nbsp;Insert mode: inactive&nbsp;</span>').parent().html());
-	
+
 			if (!state && this.map.hyperlinkPopup) {
 				this.map.hyperlinkUnderCursor = null;
 				this.map.closePopup(this.map.hyperlinkPopup);
@@ -503,13 +503,13 @@ L.Control.StatusBar = L.Control.extend({
 			languages.sort(function (a, b) {
 				return a.translated < b.translated ? -1 : a.translated > b.translated ? 1 : 0;
 			});
-	
+
 			var toolbaritems = [];
 			toolbaritems.push({ text: noneLang,
 			 id: 'nonelanguage',
 			 uno: constLang + constNone });
-	
-	
+
+
 			for (var lang in languages) {
 				translated = languages[lang].translated;
 				neutral = languages[lang].neutral;
@@ -517,9 +517,9 @@ L.Control.StatusBar = L.Control.extend({
 				var splitNeutral = neutral.split(';');
 				toolbaritems.push({ id: neutral, text: splitTranslated[0], uno: constLang + encodeURIComponent('Default_' + splitNeutral[0]) });
 			}
-	
+
 			toolbaritems.push({ id: 'reset', text: resetLang, uno: constLang + constDefault });
-	
+
 			w2ui['actionbar'].set('LanguageStatus', {items: toolbaritems});
 		}
 	},
