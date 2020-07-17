b8ef29e554c4e9277d0526f4f9cd452dfdbef146
gokaysatir <gokaysatir@collabora.com>
2020-07-17 16:37:40 +0300

    cypress: Bottom bar click quick views test.
    Change-Id: I54d5d819f04890fd7ac666728258804d1bb0d93a
    Reviewed-on: https://gerrit.libreoffice.org/c/online/+/98975
    Tested-by: Jenkins
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Tamás Zolnai <tamas.zolnai@collabora.com>

---
+++ cypress_test/integration_tests/desktop/calc/bottom_bar_spec.js
@@ -0,0 +1,26 @@
+/* global describe it cy beforeEach require afterEach */
+
+var helper = require('../../common/helper');
+var calcHelper = require('../../common/calc_helper');
+
+describe('Calc bottom bar tests.', function() {
+	var testFileName = 'BottomBar.ods';
+
+	beforeEach(function() {
+		helper.beforeAllDesktop(testFileName, 'calc');
+		// Wait until the Formula-Bar is loaded.
+		cy.get('.inputbar_container', {timeout : 10000});
+	});
+
+	afterEach(function() {
+		helper.afterAll(testFileName);
+	});
+
+	it('Bottom tool bar.', function() {
+		cy.get('#map').focus();
+		calcHelper.clickOnFirstCell();
+		cy.get('#tb_actionbar_item_StateTableCellMenu .w2ui-button').click();
+		// If it clicks, it passes.
+		cy.contains('.w2ui-drop-menu .menu-text', 'CountA').click();
+	});
+});
