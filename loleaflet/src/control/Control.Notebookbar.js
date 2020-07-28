c424141287c23eecf6fd0ead9c84269e108574a9
Pedro Pinto Silva <pedro.silva@collabora.com>
2020-07-28 10:30:29 +0200

    Notebookbar: Add Menubar icon
    Change-Id: I49f3fda9d765f7934a80b8f71007adfc384fd3e2
    Reviewed-on: https://gerrit.libreoffice.org/c/online/+/99574
    Tested-by: Jenkins
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Pedro Silva <pedro.silva@collabora.com>

--- loleaflet/src/control/Control.Notebookbar.js
+++ loleaflet/src/control/Control.Notebookbar.js
@@ -120,6 +120,11 @@ L.Control.Notebookbar = L.Control.extend({
 				'children': [
 					{
 						'type': 'toolitem',
+						'text': _('Menu'),
+						'command': '.uno:Menubar'
+					},
+					{
+						'type': 'toolitem',
 						'text': _('Save'),
 						'command': '.uno:Save'
 					},
@@ -143,6 +148,7 @@ L.Control.Notebookbar = L.Control.extend({
 		$('nav').prepend(shortcutsBar);
 		var builder = new L.control.notebookbarBuilder({mobileWizard: this, map: this.map, cssClass: 'notebookbar'});
 		builder.build(shortcutsBar, this.getShortcutsBarData());
+		$('#Menubar').html('<span id="shortcuts-menubar-icon"></span>');
 	},
 
 	setCurrentScrollPosition: function() {
