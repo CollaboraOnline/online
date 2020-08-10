5fb98cde3523dd470e59d4838e7e0174448b9f72
Szymon Kłos <szymon.klos@collabora.com>
2020-08-10 13:24:15 +0200

    Don't ignore 'transparent' value for color
    Change-Id: Ie843e85fca76b5aad875426c661e9305c954d627
    Reviewed-on: https://gerrit.libreoffice.org/c/online/+/100429
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Jan Holesovsky <kendy@collabora.com>
    (cherry picked from commit 97fe8ac3209f952262e515ec05eac655a44f00c9)
    Reviewed-on: https://gerrit.libreoffice.org/c/online/+/100523
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

--- loleaflet/src/control/Control.JSDialogBuilder.js
+++ loleaflet/src/control/Control.JSDialogBuilder.js
@@ -1760,6 +1760,13 @@ L.Control.JSDialogBuilder = L.Control.extend({
 		 return false;
 	},
 
+	parseHexColor: function(color) {
+		if (color === 'transparent')
+			return -1;
+		else
+			return parseInt('0x' + color);
+	},
+
 	_sendColorCommand: function(builder, data, color) {
 		var gradientItem;
 
@@ -1790,7 +1797,7 @@ L.Control.JSDialogBuilder = L.Control.extend({
 			var params = {};
 			params[data.id] = {
 				type : 'long',
-				value : parseInt('0x' + color)
+				value : builder.parseHexColor(color)
 			};
 
 			builder.map['stateChangeHandler'].setItemValue(data.command, params[data.id].value);
@@ -1802,7 +1809,7 @@ L.Control.JSDialogBuilder = L.Control.extend({
 
 		// update the item state as we send
 		var items = builder.map['stateChangeHandler'];
-		items.setItemValue(data.command, parseInt('0x' + color));
+		items.setItemValue(data.command, builder.parseHexColor(color));
 
 		builder.map.sendUnoCommand(command);
 	},
