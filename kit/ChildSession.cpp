36f34129cc2ccd112679b1579965c4c0d70403fe
Pranam Lashkari <lpranam@collabora.com>
2020-08-26 22:53:37 +0530

    leaflet: wsd: select the page before opening the slide wizard
    problem:
    In the mobile view taping on the selected slide preview would open the wizard
    but when some object is selected on the slide
    wizard would open for that object
    this patch helps us to set the Page as selection and as result
    mobile wizard opens for the slide even when some object on slide is selected
    
    Change-Id: Ia4f0d5fe6a4d82d101ee26b75f557a44e0627704
    Reviewed-on: https://gerrit.libreoffice.org/c/online/+/101363
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Andras Timar <andras.timar@collabora.com>

--- kit/ChildSession.cpp
+++ kit/ChildSession.cpp
@@ -2149,7 +2149,7 @@ bool ChildSession::setClientPart(const char* /*buffer*/, int /*length*/, const S
 
     getLOKitDocument()->setView(_viewId);
 
-    if (getLOKitDocument()->getDocumentType() != LOK_DOCTYPE_TEXT && part != getLOKitDocument()->getPart())
+    if (getLOKitDocument()->getDocumentType() != LOK_DOCTYPE_TEXT)
     {
         getLOKitDocument()->setPart(part);
     }
