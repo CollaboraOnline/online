a5f44dfad8fb90e90d3c67fac0dec23537c10dc8
Michael Meeks <michael.meeks@collabora.com>
2020-09-15 19:53:16 +0100

    calc grid: re-render the canvas when we get grid details.
    Change-Id: I3d1d1485e561d8c807daa0dfe0a9f2cb5651d31b
    Reviewed-on: https://gerrit.libreoffice.org/c/online/+/102952
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Jan Holesovsky <kendy@collabora.com>

--- loleaflet/src/layer/tile/CanvasTileLayer.js
+++ loleaflet/src/layer/tile/CanvasTileLayer.js
@@ -375,6 +375,7 @@ L.CanvasTileLayer = L.TileLayer.extend({
 		}
 		this._map.on('resize zoomend', this._painter.update, this._painter);
 		this._map.on('splitposchanged', this._painter.update, this._painter);
+		this._map.on('sheetgeometrychanged', this._painter.update, this._painter);
 		this._map.on('move', this._syncTilePanePos, this);
 
 		this._map.on('viewrowcolumnheaders', this._updateRenderBackground, this);
