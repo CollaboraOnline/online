e25379af2295f2e4356e8a12a28d66a28f2446a5
Szymon Kłos <szymon.klos@collabora.com>
2020-09-16 10:57:05 +0200

    Encode wopisrc part in download link
    Avoid browser warnings/errors wrt encoding when
    trying to download.
    
    Change-Id: I66820c1adec1a612f773b41416bc623c42cdb138
    Reviewed-on: https://gerrit.libreoffice.org/c/online/+/102843
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

--- loleaflet/src/layer/tile/TileLayer.js
+++ loleaflet/src/layer/tile/TileLayer.js
@@ -923,7 +923,7 @@ L.TileLayer = L.GridLayer.extend({
 
 		var wopiSrc = '';
 		if (this._map.options.wopiSrc != '') {
-			wopiSrc = '?WOPISrc=' + this._map.options.wopiSrc;
+			wopiSrc = '?WOPISrc=' + encodeURIComponent(this._map.options.wopiSrc);
 		}
 		var url = this._map.options.webserver + this._map.options.serviceRoot + '/' + this._map.options.urlPrefix + '/' +
 			encodeURIComponent(this._map.options.doc) + '/download/' + command.downloadid + wopiSrc;
