c8b1b8623a7bc7d36027001d2c6ce369009dc0f0
Tor Lillqvist <tml@collabora.com>
2020-09-30 16:58:46 +0300

    tdf#133279: Add another workaround for loleaflet weirdness
    Ignore a hide command that quickly followed a display command.
    
    Change-Id: I7be71dbc3ccdffb9db78de4a6b70d59baee14457
    Reviewed-on: https://gerrit.libreoffice.org/c/online/+/103713
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Tor Lillqvist <tml@collabora.com>

--- ios/CollaboraOnlineWebViewKeyboardManager/CollaboraOnlineWebViewKeyboardManager.m
+++ ios/CollaboraOnlineWebViewKeyboardManager/CollaboraOnlineWebViewKeyboardManager.m
@@ -140,6 +140,7 @@
     WKWebView *webView;
     _COWVKMKeyInputControl *control;
     BOOL lastCommandIsHide;
+    BOOL lastActionIsDisplay;
 }
 
 @end
@@ -222,6 +223,7 @@
         control.autocapitalizationType = UITextAutocapitalizationTypeNone;
 
         lastCommandIsHide = NO;
+        lastActionIsDisplay = YES;
 
         [self->webView addSubview:control];
         NSLog(@"COKbdMgr: Added _COWVKMKeyInputControl to webView");
@@ -242,7 +244,12 @@
                 NSLog(@"COKbdMgr: Ignoring hide command that was quickly followed by a display command");
                 return;
             }
+            if (self->lastActionIsDisplay) {
+                NSLog(@"COKbdMgr: Ignoring hide command that quickly followed a display command");
+                return;
+            }
             if (self->control != nil) {
+                self->lastActionIsDisplay = NO;
                 [self->control removeFromSuperview];
                 NSLog(@"COKbdMgr: Removed _COWVKMKeyInputControl from webView");
                 self->control = nil;
