CollaboraOnline/online

commit 6fdad3ba94ddbc35791b89da475d11446fea420f

Replace 'includes' with '$.inArray' for IE11
Regression from b36d56bf5b022bf4b41d062a82326b5ea57b8dbf.

Change-Id: I270afb92366fa8992c08a79bd1c561117a18bb9c
Reviewed-on: https://gerrit.libreoffice.org/c/online/+/89560
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Aron Budea <aron.budea@collabora.com>

loleaflet/src/control/Control.ContextMenu.js
@@ -109,7 +109,7 @@ L.Control.ContextMenu = L.Control.extend({
 		var contextMenu = this._createContextMenuStructure(obj);
 		var spellingContextMenu = false;
 		for (var menuItem in contextMenu) {
-			if (menuItem.includes('.uno:SpellCheckIgnore')) {
+			if ($.inArray('.uno:SpellCheckIgnore', menuItem)) {
 				spellingContextMenu = true;
 				break;
 			}

commit 5b3f3f8210c31ab409c38f088c576e6b42deaf11

Fix-up spelling context menu related code.
Some code was not actually hit because
$.inArray('.uno:SpellCheckIgnore', menuItem)
does nothing, since menuItem is not an array.
Broke since:
6fdad3ba94ddbc35791b89da475d11446fea420f

It seems delaying spelling context menu is
not needed anymore, so we can remove that part.

Change-Id: I6cc96ee0f5b39d2090052fec29eae463285b3dfc

loleaflet/src/control/Control.ContextMenu.js
@@ -3,7 +3,7 @@
 * Control.ContextMenu
 */
 
-/* global $ _ _UNO vex */
+/* global $ _ _UNO */
 L.Control.ContextMenu = L.Control.extend({
 	options: {
 		SEPARATOR: '---------',
@@ -125,7 +125,7 @@ L.Control.ContextMenu = L.Control.extend({
 		var contextMenu = this._createContextMenuStructure(obj);
 		var spellingContextMenu = false;
 		for (var menuItem in contextMenu) {
-			if ($.inArray('.uno:SpellCheckIgnore', menuItem) !== -1) {
+			if (menuItem.indexOf('.uno:SpellCheckIgnore') !== -1) {
 				spellingContextMenu = true;
 				break;
 			}
@@ -133,14 +133,7 @@ L.Control.ContextMenu = L.Control.extend({
 		if (window.mode.isMobile()) {
 			window.contextMenuWizard = true;
 			var menuData = L.Control.JSDialogBuilder.getMenuStructureForMobileWizard(contextMenu, true, '');
-			if (spellingContextMenu === true) {
-				vex.timer = setInterval(function() {
-					map.fire('mobilewizard', menuData);
-					clearTimeout(vex.timer);
-				}, 200);
-			} else {
-				map.fire('mobilewizard', menuData);
-			}
+			map.fire('mobilewizard', menuData);
 		} else {
 			L.installContextMenu({
 				selector: '.leaflet-layer',