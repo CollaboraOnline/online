c05b0d3fda7592935abe6cf185f0f11e3697183a
bugrakurt <bugrakurt26@gmail.com>
2020-10-14 17:08:14 +0000

    loleaflet: Bump eslint version to 4.0.0
    Change-Id: Icf889d6df542cf9625125cae6816a0dc3c60fde4

--- loleaflet/src/control/Control.MobileTopBar.js
+++ loleaflet/src/control/Control.MobileTopBar.js
@@ -45,7 +45,7 @@ L.Control.MobileTopBar = L.Control.extend({
 				{type: 'button', hidden: true, id: 'cancelformula',  img: 'cancel', hint: _('Cancel')},
 				{type: 'button',  id: 'mobile_wizard', img: 'mobile_wizard', disabled: true},
 				{type: 'button',  id: 'insertion_mobile_wizard', img: 'insertion_mobile_wizard', disabled: true},
-//				{type: 'button',  id: 'insertcomment', img: 'insertcomment', disabled: true},
+				//				{type: 'button',  id: 'insertcomment', img: 'insertcomment', disabled: true},
 				{type: 'button',  id: 'fullscreen', img: 'fullscreen', hint: _UNO('.uno:FullScreen', 'text')},
 				{type: 'drop', id: 'userlist', img: 'users', hidden: true, html: L.control.createUserListWidget()},
 			];
