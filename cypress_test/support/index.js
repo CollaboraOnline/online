8367d996b7f9543b37d505f3271b8c98e7d19e8d
Tamás Zolnai <tamas.zolnai@collabora.com>
2020-10-19 12:59:50 +0200

    cypress: slow down test execution in interactive test runner.
    Otherwise things happens very fast, so hard to follow what
    happens.
    Also the new changes of tile rendering broke the screenshot
    feature of cypress, so the screenshots taken during the test
    became useless. So an alternative is to run test slower,
    so we can see what happens on the UI.
    
    Change-Id: I09b48ca606cf45768cba04f5639a5d4b5317545d

--- cypress_test/support/index.js
+++ cypress_test/support/index.js
@@ -1,4 +1,4 @@
-/* global require Cypress */
+/* global require Cypress Promise */
 
 require('cypress-failed-log');
 
@@ -7,5 +7,25 @@ if (Cypress.env('INTEGRATION') === 'php-proxy') {
 		ignore: function() {
 			return true;
 		}
+	});
+}
+
+var COMMAND_DELAY = 1000;
+
+if (Cypress.browser.isHeaded) {
+	Cypress.Commands.overwrite('get', function(originalFn, selector, options) {
+		return new Promise(function(resolve) {
+			setTimeout(function() {
+				resolve(originalFn(selector, options));
+			}, COMMAND_DELAY);
+		});
+	});
+
+	Cypress.Commands.overwrite('contains', function(originalFn, selector, content, options) {
+		return new Promise(function(resolve) {
+			setTimeout(function() {
+				resolve(originalFn(selector, content, options));
+			}, COMMAND_DELAY);
+		});
 	});
 }
