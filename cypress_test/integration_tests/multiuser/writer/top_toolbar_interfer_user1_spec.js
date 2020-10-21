7826ba9aacdcbc4eac08b10f8c6446a56413a66b
Tamás Zolnai <tamas.zolnai@collabora.com>
2020-10-21 16:31:17 +0200

    cypress: protoype an interference test.
    The idea it to run an existing cypress test while
    the same document is opened by a second user. The second
    user does some activity (e.g. moving cursor, clicking, etc)
    and we check whether it makes any interference with the first
    user's view. The first user is doing the actual test's steps,
    so we can see if interference breaks the assertions.
    
    Now it's just a prototype for testing it in the CI setup.
    
    Change-Id: I3e02c037754862d628c73bbb4b2d82ea84b8a719
    (cherry picked from commit 5b0676bc3acaeb2d9cf9ff418b5b4c6a9f6c57c5)
    Signed-off-by: Tamás Zolnai <tamas.zolnai@collabora.com>

---
+++ cypress_test/integration_tests/multiuser/writer/top_toolbar_interfer_user1_spec.js
@@ -0,0 +1,104 @@
+/* global describe it cy beforeEach require afterEach Cypress */
+
+var helper = require('../../common/helper');
+
+describe('Top toolbar interfering test: user-1.', function() {
+	var testFileName = 'top_toolbar_interfer.odt';
+
+	beforeEach(function() {
+		helper.beforeAll(testFileName, 'writer');
+
+		cy.get('#tb_actionbar_item_userlist', { timeout: Cypress.config('defaultCommandTimeout') * 2.0 })
+			.should('be.visible');
+
+		cy.wait(2000);
+
+		helper.selectAllText(false);
+	});
+
+	afterEach(function() {
+		helper.afterAll(testFileName);
+	});
+
+	it('Apply bold font.', function() {
+		cy.get('#tb_editbar_item_bold')
+			.click();
+
+		helper.selectAllText(false);
+
+		cy.get('#copy-paste-container p b')
+			.should('exist');
+	});
+
+	it('Apply italic font.', function() {
+		cy.get('#tb_editbar_item_italic')
+			.click();
+
+		helper.selectAllText(false);
+
+		cy.get('#copy-paste-container p i')
+		    .should('exist');
+	});
+
+	it('Apply underline.', function() {
+		cy.get('#tb_editbar_item_underline')
+			.click();
+		helper.reselectAllText();
+
+		cy.get('#copy-paste-container p u')
+			.should('exist');
+	});
+
+	it('Apply strikethrough.', function() {
+		cy.get('#tb_editbar_item_strikeout')
+			.click();
+
+		helper.reselectAllText();
+
+		cy.get('#copy-paste-container p strike')
+			.should('exist');
+	});
+
+	it('Apply left alignment.', function() {
+		cy.get('#tb_editbar_item_centerpara')
+			.click();
+
+		helper.selectAllText(false);
+
+		cy.get('#copy-paste-container p')
+			.should('have.attr', 'align', 'center');
+
+		cy.get('#tb_editbar_item_leftpara')
+			.click();
+
+		helper.selectAllText(false);
+
+		cy.get('#copy-paste-container p')
+			.should('have.attr', 'align', 'left');
+	});
+
+	it('Insert comment.', function() {
+		cy.get('#toolbar-up .w2ui-scroll-right')
+			.click();
+
+		cy.get('#tb_editbar_item_insertannotation')
+			.click();
+
+		// Comment insertion dialog is opened
+		cy.get('.loleaflet-annotation-table')
+			.should('exist');
+
+		// Add some comment
+		cy.get('.loleaflet-annotation-edit:nth-of-type(2) .loleaflet-annotation-textarea')
+			.type('some text');
+
+		cy.get('#annotation-save')
+			.click();
+
+		cy.get('.loleaflet-annotation')
+			.should('exist');
+
+		cy.get('.loleaflet-annotation-content.loleaflet-dont-break')
+			.should('have.text', 'some text');
+	});
+});
