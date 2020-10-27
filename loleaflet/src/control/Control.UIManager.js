9e73101eccef032766fc02398fcf3b93daca491e
Szymon Kłos <szymon.klos@collabora.com>
2020-10-27 08:57:14 +0100

    jsdialog: handle autofilter dropdown
    Change-Id: Ib47185359526f030a80aa9f7d7f9ef6e2ab93b34
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>

--- loleaflet/src/control/Control.UIManager.js
+++ loleaflet/src/control/Control.UIManager.js
@@ -52,6 +52,7 @@ L.Control.UIManager = L.Control.extend({
 		this.map.addControl(L.control.alertDialog());
 		this.mobileWizard = L.control.mobileWizard();
 		this.map.addControl(this.mobileWizard);
+		this.map.addControl(L.control.autofilterDropdown());
 		this.map.addControl(L.control.languageDialog());
 		this.map.dialog = L.control.lokDialog();
 		this.map.addControl(this.map.dialog);
