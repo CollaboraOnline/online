CollaboraOnline/online

commit 0da5c3a2d6be4c0800ecf1b8a742c4ca2ec5d45d

Remove unused function setHelpTarget()
Change-Id: I9f3338c79de627befc5eac71f2418f1014b08eeb

loleaflet/src/control/Control.Menubar.js
@@ -1331,23 +1331,6 @@ L.Control.Menubar = L.Control.extend({
 			self._map.focus();
 	},
 
-	_onMouseEnter: function(e, item) {
-		var self = e.data.self;
-		var type = $(item).data('type');
-		if (type === 'unocommand') {
-			var unoCommand = $(item).data('uno');
-			self._map.setHelpTarget(unoCommand);
-		} else if (type === 'action') {
-			var id = $(item).data('id');
-			self._map.setHelpTarget('modules/online/menu/' + id);
-		}
-	},
-
-	_onMouseLeave: function(e) {
-		var self = e.data.self;
-		self._map.setHelpTarget(null);
-	},
-
 	_onKeyDown: function(e) {
 		var self = e.data.self;
 

loleaflet/src/map/Map.js
@@ -113,7 +113,6 @@ L.Map = L.Evented.extend({
 		this._debugAlwaysActive = false; // disables the dimming / document inactivity when true
 		this._serverRecycling = false;
 		this._documentIdle = false;
-		this._helpTarget = null; // help page that fits best the current context
 		this._disableDefaultAction = {}; // The events for which the default handler is disabled and only issues postMessage.
 		this.showSidebar = false;
 
@@ -1045,10 +1044,6 @@ L.Map = L.Evented.extend({
 	// On mobile, this signifies that the keyboard should be visible.
 	canAcceptKeyboardInput: function() {
 		return this._textInput.canAcceptKeyboardInput();
-	},
-
-	setHelpTarget: function(page) {
-		this._helpTarget = page;
 	},
 
 	isSearching: function() {

commit 1d3fc85344d4598c3502f4434755653806d22242

leaflet: fixed jquery error on hovering over menubar
regression caused by 0da5c3a2

Change-Id: I005943ddfff7d4208dc432fec5836862c1f3638e
Signed-off-by: Pranam Lashkari <lpranam@collabora.com>

loleaflet/src/control/Control.Menubar.js
@@ -934,8 +934,6 @@ L.Control.Menubar = L.Control.extend({
 		this._onRefresh();
 
 		$('#main-menu').bind('select.smapi', {self: this}, this._onItemSelected);
-		$('#main-menu').bind('mouseenter.smapi', {self: this}, this._onMouseEnter);
-		$('#main-menu').bind('mouseleave.smapi', {self: this}, this._onMouseLeave);
 
 		$('#main-menu').bind('beforeshow.smapi', {self: this}, this._beforeShow);
 		$('#main-menu').bind('click.smapi', {self: this}, this._onClicked);