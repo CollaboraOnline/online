2581ab44f9f367ff5f1fb94165a1b84748fb2e7f
Tamás Zolnai <tamas.zolnai@collabora.com>
2020-11-11 17:35:19 +0100

    cypress: remove core/master blacklist.
    It is outdated.
    
    Change-Id: I29de4a7ddbacb62cd091446929fe9ba56ce9baf9
    Signed-off-by: Tamás Zolnai <tamas.zolnai@collabora.com>

--- cypress_test/plugins/blacklists.js
+++ cypress_test/plugins/blacklists.js
@@ -1,46 +1,5 @@
 var coreBlackLists = {
 	'master': [
-		['mobile/writer/table_properties_spec.js',
-			[]
-		],
-		['mobile/writer/shape_properties_spec.js',
-			[
-				'Change size with keep ratio enabled.',
-				'Change line color',
-				'Change line style',
-				'Change line width',
-				'Change line transparency',
-				'Arrow style items are hidden.'
-			]
-		],
-		['mobile/writer/apply_paragraph_properties_spec.js',
-			[
-				'Apply default bulleting.',
-				'Apply default numbering.',
-				'Apply background color.'
-			]
-		],
-		['mobile/writer/insert_object_spec.js',
-			[
-				'Insert default table.',
-				'Insert custom table.'
-			]
-		],
-		['mobile/writer/apply_font_spec.js',
-			[
-				'Insert default table.',
-				'Insert custom table.'
-			]
-		],
-		['mobile/calc/number_format_spec.js',
-			[
-				'Select percent format from list.',
-				'Push percent button.',
-				'Select currency format from list.',
-				'Push currency button.',
-				'Push number button.'
-			]
-		],
 	],
 
 	'cp-6-4': [
