baf81faf8610539136192914d14b863977e1c000
Thais Vieira <thais.vieira@collabora.com>
2020-11-11 10:31:53 -0300

    impress/top_toolbar: Add Cypress test for italic attribute on desktop.
    Change-Id: I7cf9e31beba87cd12d562b0e899b83c895ba68d7
    Signed-off-by: Thais Vieira <thais.vieira@collabora.com>
    (cherry picked from commit 062f43dfa7c6f382f85ac85ac57227ce36e75fed)

--- cypress_test/integration_tests/desktop/impress/top_toolbar_spec.js
+++ cypress_test/integration_tests/desktop/impress/top_toolbar_spec.js
@@ -33,6 +33,16 @@ describe('Top toolbar tests.', function() {
 			.should('have.attr', 'font-weight', '700');
 	});
 
+	it('Apply italic on text shape.', function() {
+		cy.get('#tb_editbar_item_italic')
+			.click();
+
+		impressHelper.triggerNewSVGForShapeInTheCenter();
+
+		cy.get('.leaflet-pane.leaflet-overlay-pane g.Page .TextParagraph')
+			.should('have.attr', 'font-style', 'italic');
+	});	
+
 	it('Apply left/right alignment on text seleced text.', function() {
 		cy.get('.leaflet-pane.leaflet-overlay-pane g.Page .TextParagraph .TextPosition')
 			.should('have.attr', 'x', '1400');
