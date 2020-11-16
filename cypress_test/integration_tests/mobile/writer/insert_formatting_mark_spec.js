bb3c3f6a67338cdce33ac487ee98cb6799fadfff
Tamás Zolnai <tamas.zolnai@collabora.com>
2020-11-16 15:39:19 +0100

    cypress: merge some selection helper methods.
    To reuse as much code as we can. We can use the same
    method on mobile and on desktop.
    
    Change-Id: I15b1038313145db6cb6ea07489c0d30a0fc2ce64
    Signed-off-by: Tamás Zolnai <tamas.zolnai@collabora.com>
    (cherry picked from commit 3f4468a497e72c0f1de6ac581f394e18d607800b)

--- cypress_test/integration_tests/mobile/writer/insert_formatting_mark_spec.js
+++ cypress_test/integration_tests/mobile/writer/insert_formatting_mark_spec.js
@@ -2,7 +2,7 @@
 
 var helper = require('../../common/helper');
 var mobileHelper = require('../../common/mobile_helper');
-var writerMobileHelper = require('./writer_mobile_helper');
+var writerHelper = require('../../common/writer_helper');
 
 describe('Insert formatting mark via insertion wizard.', function() {
 	var testFileName = 'insert_formatting_mark.odt';
@@ -31,7 +31,7 @@ describe('Insert formatting mark via insertion wizard.', function() {
 		cy.contains('.menu-entry-with-icon', 'Insert non-breaking space')
 			.click();
 
-		writerMobileHelper.selectAllMobile();
+		writerHelper.selectAllTextOfDoc();
 
 		cy.get('#copy-paste-container p')
 			.should('contain.text', '\u00a0');
@@ -41,7 +41,7 @@ describe('Insert formatting mark via insertion wizard.', function() {
 		cy.contains('.menu-entry-with-icon', 'Insert non-breaking hyphen')
 			.click();
 
-		writerMobileHelper.selectAllMobile();
+		writerHelper.selectAllTextOfDoc();
 
 		cy.get('#copy-paste-container p')
 			.should('contain.text', '\u2011');
@@ -51,7 +51,7 @@ describe('Insert formatting mark via insertion wizard.', function() {
 		cy.contains('.menu-entry-with-icon', 'Insert soft Hyphen')
 			.click();
 
-		writerMobileHelper.selectAllMobile();
+		writerHelper.selectAllTextOfDoc();
 
 		cy.get('#copy-paste-container p')
 			.should('contain.text', '\u00ad');
@@ -61,7 +61,7 @@ describe('Insert formatting mark via insertion wizard.', function() {
 		cy.contains('.menu-entry-with-icon', 'No-width Optional Break')
 			.click();
 
-		writerMobileHelper.selectAllMobile();
+		writerHelper.selectAllTextOfDoc();
 
 		cy.get('#copy-paste-container p')
 			.should('contain.text', '\u200b');
@@ -71,7 +71,7 @@ describe('Insert formatting mark via insertion wizard.', function() {
 		cy.contains('.menu-entry-with-icon', 'No-width No Break')
 			.click();
 
-		writerMobileHelper.selectAllMobile();
+		writerHelper.selectAllTextOfDoc();
 
 		cy.get('#copy-paste-container p')
 			.should('contain.text', '\u2060');
@@ -81,7 +81,7 @@ describe('Insert formatting mark via insertion wizard.', function() {
 		cy.contains('.menu-entry-with-icon', 'Left-to-right Mark')
 			.click();
 
-		writerMobileHelper.selectAllMobile();
+		writerHelper.selectAllTextOfDoc();
 
 		cy.get('#copy-paste-container p')
 			.should('contain.text', '\u200e');
@@ -91,7 +91,7 @@ describe('Insert formatting mark via insertion wizard.', function() {
 		cy.contains('.menu-entry-with-icon', 'Right-to-left Mark')
 			.click();
 
-		writerMobileHelper.selectAllMobile();
+		writerHelper.selectAllTextOfDoc();
 
 		cy.get('#copy-paste-container p')
 			.should('contain.text', '\u200f');
