e6ba50a0792d0815aa33ff392c4e493e39d4df78
Thais Vieira <thais.vieira@collabora.com>
2020-11-17 10:28:39 -0300

    cypress_test/impress: Add test for underline text shape.
    Signed-off-by: Thais Vieira <thais.vieira@collabora.com>
    Change-Id: Ia71b891d78bf621aec24d90e105fed1b32152528

--- cypress_test/integration_tests/desktop/impress/top_toolbar_spec.js
+++ cypress_test/integration_tests/desktop/impress/top_toolbar_spec.js
@@ -43,6 +43,16 @@ describe('Top toolbar tests.', function() {
 			.should('have.attr', 'font-style', 'italic');
 	});	
 
+	it('Apply underline on text shape.', function() {
+		cy.get('#tb_editbar_item_underline')
+			.click();
+
+		impressHelper.triggerNewSVGForShapeInTheCenter();
+
+		cy.get('.leaflet-pane.leaflet-overlay-pane g.Page .TextParagraph')
+			.should('have.attr', 'text-decoration', 'underline');
+	});
+
 	it('Apply left/right alignment on text seleced text.', function() {
 		cy.get('.leaflet-pane.leaflet-overlay-pane g.Page .TextParagraph .TextPosition')
 			.should('have.attr', 'x', '1400');
