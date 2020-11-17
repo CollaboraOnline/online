3ff80dc358520b86de7b72f7597a12ac711f0e11
Ezinne Nnamani <nnamani.ezinne@collabora.com>
2020-11-17 13:11:50 +0100

    Created a cypress test for Apply horizontal borders, Apply all inner and outer borders in Desktop Calc
    Signed-off-by: Ezinne Nnamani <nnamani.ezinne@collabora.com>
    Change-Id: I0298ed82345ea4e8541645b91d23167e64077cd4
    Signed-off-by: Ezinne Nnamani <nnamani.ezinne@collabora.com>

--- cypress_test/integration_tests/desktop/calc/top_toolbar_spec.js
+++ cypress_test/integration_tests/desktop/calc/top_toolbar_spec.js
@@ -1,4 +1,4 @@
-/* global describe it cy beforeEach require afterEach */
+/* global describe it cy beforeEach require afterEach expect*/
 
 var helper = require('../../common/helper');
 var calcHelper = require('../../common/calc_helper');
@@ -131,6 +131,103 @@ describe('Top toolbar tests.', function() {
 
 		cy.get('#copy-paste-container table td')
 			.should('have.attr', 'style', 'border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000');
+	});
+
+	it('Apply horizontal borders for multiple cells', function() {
+		calcHelper.selectAllMobile();
+		cy.get('#tb_editbar_item_setborderstyle')
+			.click();
+
+		cy.get('.w2ui-tb-image.w2ui-icon.frame09')
+			 .click();
+
+		calcHelper.selectAllMobile();
+
+		cy.get('#copy-paste-container table td')
+			.should(function(cells) {
+				expect(cells).to.have.lengthOf(4);
+				for (var i = 0; i < cells.length; i++) {
+					expect(cells[i]).to.have.attr('style', 'border-top: 1px solid #000000; border-bottom: 1px solid #000000');
+				}
+			});
+	});
+
+	it('Apply horizontal inner borders and vertical outer borders', function() {
+		calcHelper.selectAllMobile();
+		cy.get('#tb_editbar_item_setborderstyle')
+			.click();
+
+		cy.get('.w2ui-tb-image.w2ui-icon.frame10')
+			 .click();
+
+		calcHelper.selectAllMobile();
+
+		cy.get('#copy-paste-container table td')
+			.should(function(cells) {
+				expect(cells).to.have.lengthOf(4);
+				for (var i = 0; i < cells.length; i++) {
+					if (i == 0)
+						expect(cells[i]).to.have.attr('style', 'border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000');
+					else if (i == 1)
+						expect(cells[i]).to.have.attr('style', 'border-top: 1px solid #000000; border-bottom: 1px solid #000000');
+					else if (i == 2)
+						expect(cells[i]).to.have.attr('style', 'border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000');
+					else
+						expect(cells[i]).to.have.attr('style', 'border-top: 1px solid #000000; border-bottom: 1px solid #000000');
+				}
+			});
+	});
+
+	it('Apply vertical inner borders and horizontal outer borders', function() {
+		calcHelper.selectAllMobile();
+		cy.get('#tb_editbar_item_setborderstyle')
+			.click();
+
+		cy.get('.w2ui-tb-image.w2ui-icon.frame11')
+			 .click();
+
+		calcHelper.selectAllMobile();
+
+		cy.get('#copy-paste-container table td')
+			.should(function(cells) {
+				expect(cells).to.have.lengthOf(4);
+				for (var i = 0; i < cells.length; i++) {
+					if (i == 0)
+						expect(cells[i]).to.have.attr('style', 'border-top: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000');
+					else if (i == 1)
+						expect(cells[i]).to.have.attr('style', 'border-top: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000');
+					else if (i == 2)
+						expect(cells[i]).to.have.attr('style', 'border-left: 1px solid #000000; border-right: 1px solid #000000');
+					else
+						expect(cells[i]).to.have.attr('style', 'border-left: 1px solid #000000; border-right: 1px solid #000000');
+				}
+			});
+	});
+
+	it('Apply all inner and outer borders', function() {
+		calcHelper.selectAllMobile();
+		cy.get('#tb_editbar_item_setborderstyle')
+			.click();
+
+		cy.get('.w2ui-tb-image.w2ui-icon.frame12')
+			 .click();
+
+		calcHelper.selectAllMobile();
+
+		cy.get('#copy-paste-container table td')
+			.should(function(cells) {
+				expect(cells).to.have.lengthOf(4);
+				for (var i = 0; i < cells.length; i++) {
+					if (i == 0)
+						expect(cells[i]).to.have.attr('style', 'border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000');
+					else if (i == 1)
+						expect(cells[i]).to.have.attr('style', 'border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000');
+					else if (i == 2)
+						expect(cells[i]).to.have.attr('style', 'border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000');
+					else
+						expect(cells[i]).to.have.attr('style', 'border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000');
+				}
+			});
 	});
 
 	it('Clear Direct formatting.', function() {
