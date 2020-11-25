9e3cede1238ea173cf6ccad030b58f25d9fb07d5
Szymon Kłos <szymon.klos@collabora.com>
2020-11-25 14:28:35 +0100

    jsdialog: move build JSDialogBuilder -> MobileWizardBuilder
    Change-Id: Ib1e6d9163b2806c020ff6a85f426d41a95831965
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>

--- loleaflet/src/control/Control.JSDialogBuilder.js
+++ loleaflet/src/control/Control.JSDialogBuilder.js
@@ -2429,56 +2429,6 @@ L.Control.JSDialogBuilder = L.Control.extend({
 		// so no need to recurse here over 'data'.
 		this._addMissingLabels(data);
 	},
-
-	build: function(parent, data) {
-		this._amendJSDialogData(data);
-		for (var childIndex in data) {
-			if (!data[childIndex])
-				continue;
-
-			var childData = data[childIndex];
-			if (!childData)
-				continue;
-			this._parentize(childData);
-			var childType = childData.type;
-			var processChildren = true;
-			var needsToCreateContainer =
-				childType == 'panel' || childType == 'frame';
-
-			if ((childData.id === undefined || childData.id === '' || childData.id === null)
-				&& (childType == 'checkbox' || childType == 'radiobutton')) {
-				continue;
-			}
-
-			var childObject = needsToCreateContainer ? L.DomUtil.createWithId('div', childData.id, parent) : parent;
-
-			var handler = this._controlHandlers[childType];
-			var twoPanelsAsChildren =
-					childData.children && childData.children.length == 2
-					&& childData.children[0] && childData.children[0].type == 'panel'
-					&& childData.children[1] && childData.children[1].type == 'panel';
-
-			if (childData.children && childData.children.length == 1
-				&& childData.children[0] && childData.children[0].type == 'panel') {
-				handler = this._controlHandlers['singlepanel'];
-				processChildren = handler(childObject, childData.children, this);
-			} else if (twoPanelsAsChildren) {
-				handler = this._controlHandlers['paneltabs'];
-				processChildren = handler(childObject, childData.children, this);
-			} else {
-				if (handler)
-					processChildren = handler(childObject, childData, this);
-				else
-					console.warn('JSDialogBuilder: Unsupported control type: \"' + childType + '\"');
-
-				if (processChildren && childData.children != undefined)
-					this.build(childObject, childData.children);
-				else if (childData.visible && (childData.visible === false || childData.visible === 'false')) {
-					$('#' + childData.id).addClass('hidden-from-event');
-				}
-			}
-		}
-	}
 });
 
 L.Control.JSDialogBuilder.getMenuStructureForMobileWizard = function(menu, mainMenu, itemCommand) {
