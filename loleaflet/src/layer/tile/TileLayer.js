20476e147ba94b1020b444b254423794e795d244
mert <mert.tumer@collabora.com>
2020-11-26 15:11:46 +0300

    Fix different size thumbnail rendering problem
    We dont send broadcasts anymore for thumbnails
    therefore no need to use higher dpi in case
    we have highdpi views. This causes glitching on the thumbnail
    especially when a watermark is applied. It gets smaller and bigger
    back and forth and also this mean x2 more rendering for single preview.
    
    Change-Id: I80ba8c70bcd9d5d3c00c2ddffadae11a6b0b2e61
    Signed-off-by: mert <mert.tumer@collabora.com>

--- loleaflet/src/layer/tile/TileLayer.js
+++ loleaflet/src/layer/tile/TileLayer.js
@@ -3489,7 +3489,7 @@ L.TileLayer = L.GridLayer.extend({
 				preview = this._map._docPreviews[key];
 				if (preview.autoUpdate) {
 					if (preview.index >= 0) {
-						this._map.getPreview(preview.id, preview.index, preview.maxWidth, preview.maxHeight, {autoUpdate: true, broadcast: true});
+						this._map.getPreview(preview.id, preview.index, preview.maxWidth, preview.maxHeight, {autoUpdate: true});
 					}
 					else {
 						this._map.getCustomPreview(preview.id, preview.part, preview.width, preview.height, preview.tilePosX,
