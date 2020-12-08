8a120a3d2d771d58439cedce0e75695078c0fe82
Michael Meeks <michael.meeks@collabora.com>
2020-12-08 13:21:32 +0000

    Simulate EAGAIN in debug mode, every seventh operation.
    Hopefully reasonably simple; we perturb the count in the poll to
    avoid starving a seventh socket in a poll.
    
    Change-Id: I1a39cc36b9599ffe82186b896c6fd91d792c4127
    Signed-off-by: Michael Meeks <michael.meeks@collabora.com>

--- net/Socket.hpp
+++ net/Socket.hpp
@@ -1264,6 +1264,11 @@ protected:
         if (_readType == UseRecvmsgExpectFD)
             return readFD(buf, len, _incomingFD);
 
+#if ENABLE_DEBUG
+        if (simulateSocketError(true))
+            return -1;
+#endif
+
         return ::read(getFD(), buf, len);
 #else
         return fakeSocketRead(getFD(), buf, len);
@@ -1275,6 +1280,10 @@ protected:
     {
         assertCorrectThread();
 #if !MOBILEAPP
+#if ENABLE_DEBUG
+        if (simulateSocketError(false))
+            return -1;
+#endif
         return ::write(getFD(), buf, len);
 #else
         return fakeSocketWrite(getFD(), buf, len);
@@ -1295,6 +1304,12 @@ protected:
     {
         return _socketHandler;
     }
+
+protected:
+#if ENABLE_DEBUG
+    /// Return true and set errno to simulate an error
+    virtual bool simulateSocketError(bool read);
+#endif
 
   private:
     /// Client handling the actual data.
