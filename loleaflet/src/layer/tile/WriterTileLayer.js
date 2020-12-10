79ad9b08a249f9fd614e682d512b7f17b4937088
Gökay Şatır <gokaysatir@collabora.com>
2020-12-10 12:34:17 +0300

    Revert "leaflet: User may continue writing a comment after it loses focus/online part."
    We will use another approach, comment box was lingering.
    
    This reverts commit 1beb1fde3e76f7d86f7ccb9e55cd00fa204797c9.
    
    Signed-off-by: Gökay Şatır <gokaysatir@collabora.com>
    Change-Id: I43de65acdbe5ff02fabc5e0d3b606ac9b21b651d

--- loleaflet/src/layer/tile/WriterTileLayer.js
+++ loleaflet/src/layer/tile/WriterTileLayer.js
@@ -7,25 +7,23 @@
 L.WriterTileLayer = L.CanvasTileLayer.extend({
 
 	newAnnotation: function (comment) {
-		if (this._annotations.hasDraftComment() === false || comment.id !== 'new') {
-			if (!comment.anchorPos && this._map._isCursorVisible) {
-				comment.anchorPos = L.bounds(this._latLngToTwips(this._visibleCursor.getSouthWest()),
-					this._latLngToTwips(this._visibleCursor.getNorthEast()));
-				comment.anchorPix = this._twipsToPixels(comment.anchorPos.min);
-			} else if (this._graphicSelection && !this._isEmptyRectangle(this._graphicSelection)) {
-				// An image is selected, then guess the anchor based on the graphic
-				// selection.
-				comment.anchorPos = L.bounds(this._latLngToTwips(this._graphicSelection.getSouthWest()),
-					this._latLngToTwips(this._graphicSelection.getNorthEast()));
-				comment.anchorPix = this._twipsToPixels(comment.anchorPos.min);
-			}
-			if (comment.anchorPos) {
-				this._annotations.modify(this._annotations.add(comment));
-			}
-			if (window.mode.isMobile() || window.mode.isTablet()) {
-				var that = this;
-				this.newAnnotationVex(comment, function(annotation) { that._annotations._onAnnotationSave(annotation); });
-			}
+		if (!comment.anchorPos && this._map._isCursorVisible) {
+			comment.anchorPos = L.bounds(this._latLngToTwips(this._visibleCursor.getSouthWest()),
+				this._latLngToTwips(this._visibleCursor.getNorthEast()));
+			comment.anchorPix = this._twipsToPixels(comment.anchorPos.min);
+		} else if (this._graphicSelection && !this._isEmptyRectangle(this._graphicSelection)) {
+			// An image is selected, then guess the anchor based on the graphic
+			// selection.
+			comment.anchorPos = L.bounds(this._latLngToTwips(this._graphicSelection.getSouthWest()),
+				this._latLngToTwips(this._graphicSelection.getNorthEast()));
+			comment.anchorPix = this._twipsToPixels(comment.anchorPos.min);
+		}
+		if (comment.anchorPos) {
+			this._annotations.modify(this._annotations.add(comment));
+		}
+		if (window.mode.isMobile() || window.mode.isTablet()) {
+			var that = this;
+			this.newAnnotationVex(comment, function(annotation) { that._annotations._onAnnotationSave(annotation); });
 		}
 	},
 
