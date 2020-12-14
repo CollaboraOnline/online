d6c847aa313ceef9e1091acbcdb913389a573cc9
Tamás Zolnai <tamas.zolnai@collabora.com>
2020-12-14 15:22:08 +0100

    cypress: add text cursor tests for Writer (mobile).
    I've seen some inconsistencies related to cursor
    movements in the existing cypress tests. I added
    these tests which checks cursor behavior directly,
    which might help to find out the issue with cursor
    handling.
    
    Signed-off-by: Tamás Zolnai <tamas.zolnai@collabora.com>
    Change-Id: If158ccef359bb28ade8d7336594fdafdd46868f4
    (cherry picked from commit b5d5129f0db9f00a80265f65c8156cff7984eb0f)

---
+++ cypress_test/integration_tests/mobile/writer/cursor_spec.js
@@ -0,0 +1,68 @@
+/* global describe it require afterEach cy */
+
+var helper = require('../../common/helper');
+var mobileHelper = require('../../common/mobile_helper');
+var writerHelper = require('../../common/writer_helper');
+
+describe('Text cursor tests.', function() {
+	var testFileName;
+
+	function before(fileName) {
+		testFileName = fileName;
+		helper.beforeAll(testFileName, 'writer');
+
+		mobileHelper.enableEditingMobile();
+	}
+
+	afterEach(function() {
+		helper.afterAll(testFileName, this.currentTest.state);
+	});
+
+	it('Extensive cursor movements.', function() {
+		before('cursor.odt');
+
+		for (var i = 0; i < 5; i++) {
+			helper.moveCursor('right');
+		}
+		for (var j = 0; j < 5; j++) {
+			helper.moveCursor('left');
+		}
+	});
+
+	it('View jumps by cursor movement.', function() {
+		before('cursor.odt');
+
+		for (var i = 0; i < 5; i++) {
+			helper.moveCursor('end');
+			helper.moveCursor('home');
+		}
+	});
+
+	it('Cursor is visible after text selection.', function() {
+		before('cursor.odt');
+
+		writerHelper.selectAllTextOfDoc();
+
+		cy.get('.blinking-cursor')
+			.should('be.visible');
+
+		// Blinking cursor and so the view should be at the end of the text selection.
+		cy.get('.leaflet-selection-marker-end')
+			.should('be.visible');
+
+		cy.get('.leaflet-selection-marker-start')
+			.should('not.be.visible');
+	});
+
+	it('Move cursor through table.', function() {
+		before('cursor_in_table.odt');
+
+		for (var i = 0; i < 5; i++) {
+			helper.moveCursor('down');
+		}
+
+		for (var j = 0; j < 5; j++) {
+			helper.moveCursor('up');
+		}
+	});
+});
