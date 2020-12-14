68cd8bce9da15eec414019158a045fbedf24b7e1
Tamás Zolnai <tamas.zolnai@collabora.com>
2020-12-14 17:47:59 +0100

    cypress: pass test state to afterAll() method in multi-user tests too.
    Signed-off-by: Tamás Zolnai <tamas.zolnai@collabora.com>
    Change-Id: Ia353c6933fae8098ae983d2196added263ba5784

--- cypress_test/integration_tests/multiuser/impress/slide_operations_user2_spec.js
+++ cypress_test/integration_tests/multiuser/impress/slide_operations_user2_spec.js
@@ -11,7 +11,7 @@ describe('Slide operations: user-2.', function() {
 	});
 
 	afterEach(function() {
-		helper.afterAll(testFileName);
+		helper.afterAll(testFileName, this.currentTest.state);
 	});
 
 	it('Insert/delete slide.', function() {
