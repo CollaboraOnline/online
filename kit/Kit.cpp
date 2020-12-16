b34755e06fcd476dae513f6b2eb44dcdca9bd50a
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2020-12-15 23:59:33 -0500

    wsd: reduce jail size
    Change-Id: I9fd1c76aff9c97b800c6b7b6154beb9c596170fb
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- kit/Kit.cpp
+++ kit/Kit.cpp
@@ -184,7 +184,8 @@ namespace
                 strcmp(path, "share/Scripts/java") != 0 &&
                 strcmp(path, "share/Scripts/javascript") != 0 &&
                 strcmp(path, "share/config/wizard") != 0 &&
-                strcmp(path, "readmes") != 0;
+                strcmp(path, "readmes") != 0 &&
+                strcmp(path, "help") != 0;
         default: // LinkOrCopyType::All
             return true;
         }
@@ -196,7 +197,11 @@ namespace
         {
         case LinkOrCopyType::LO:
         {
-            const char *dot = strrchr(path, '.');
+            if (strstr(path, "LICENSE") || strstr(path, "EULA") || strstr(path, "CREDITS")
+                || strstr(path, "NOTICE"))
+                return false;
+
+            const char* dot = strrchr(path, '.');
             if (!dot)
                 return true;
 
