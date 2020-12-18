a663907d4cd6ae84e9468f548ec9cb9e5e940290
Pranam Lashkari <lpranam@collabora.com>
2020-12-19 01:50:40 +0530

    leaflet: don't create container for frame in mobile wizard
    problem:
    creating an additional container for frames causes problem in wizard
    while trying to go level up because changing level depends upon
    finding the sibling of the current not and making them visible or invisible
    having extra container breaks this relation
    
    in addition not creating an additional container does not affect anything visually
    
    Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
    Change-Id: I5f6342febcf444c71eef964da5585e5f73ae367e

--- loleaflet/src/control/Control.MobileWizardBuilder.js
+++ loleaflet/src/control/Control.MobileWizardBuilder.js
@@ -260,7 +260,7 @@ L.Control.MobileWizardBuilder = L.Control.JSDialogBuilder.extend({
 			var childType = childData.type;
 			var processChildren = true;
 			var needsToCreateContainer =
-				childType == 'panel' || childType == 'frame';
+				childType == 'panel';
 
 			if ((childData.id === undefined || childData.id === '' || childData.id === null)
 				&& (childType == 'checkbox' || childType == 'radiobutton')) {
