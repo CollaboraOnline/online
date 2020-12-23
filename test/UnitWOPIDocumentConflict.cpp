afdcd9d173196f0076caa0af25533a5eb9a2445b
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2020-12-22 19:08:57 -0500

    wsd: test: improved UnitWOPIDocumentConflict
    This adds proper phases instead of arbitrary
    sleeping and similar workarounds. Also,
    each phase documents what it expects, so
    debugging failures are now self-explanatory.
    
    Change-Id: Id51f4c285c5cf98aceba3415f0ffe14bd7605acc
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>
    (cherry picked from commit 39e69e8168f4a57021be0340ae05a2d92763b8e9)

--- test/UnitWOPIDocumentConflict.cpp
+++ test/UnitWOPIDocumentConflict.cpp
@@ -5,6 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+#include "Util.hpp"
 #include "config.h"
 
 #include "WopiTestServer.hpp"
@@ -12,6 +13,7 @@
 #include "Unit.hpp"
 #include "UnitHTTP.hpp"
 #include "helpers.hpp"
+#include "lokassert.hpp"
 
 #include <Poco/Net/HTTPRequest.h>
 #include <Poco/Timestamp.h>
@@ -21,13 +23,26 @@
  * discarded in case document is changed in storage behind our back. We don't
  * want to overwrite the document which is in storage when the user asks us to
  * do so.
+ *
+ * The way this works is as follows:
+ * 1. Load a document.
+ * 2. When we get 'status:' in filterSendMessage, we modify it.
+ * 3. Simulate content-change in storage and attempt to save it.
+ * 4. Saving should fail with 'error:' in filterSendMessage.
+ * 5. Load the document again and verify the storage-chagned contents.
+ * 6. Finish when the second load is processed in assertGetFileRequest.
  */
 class UnitWOPIDocumentConflict : public WopiTestServer
 {
     enum class Phase
     {
         Load,
-        ModifyAndChangeStorageDoc,
+        WaitLoadStatus,
+        ModifyDoc,
+        WaitModifiedStatus,
+        ChangeStorageDoc,
+        WaitSaveResponse,
+        WaitDocClose,
         LoadNewDocument,
         Polling
     } _phase;
@@ -38,6 +53,8 @@ class UnitWOPIDocumentConflict : public WopiTestServer
         Doc2
     } _docLoaded;
 
+    static constexpr auto ExpectedDocContent = "Modified content in storage";
+
 public:
     UnitWOPIDocumentConflict()
         : WopiTestServer("UnitWOPIDocumentConflict")
@@ -47,11 +64,14 @@ public:
 
     void assertGetFileRequest(const Poco::Net::HTTPRequest& /*request*/) override
     {
+        LOG_TST("assertGetFileRequest: Doc " << (_docLoaded == DocLoaded::Doc1 ? "1" : "2"));
         if (_docLoaded == DocLoaded::Doc2)
         {
             // On second doc load, we should have the document in storage which
             // was changed beneath us, not the one which we modified by pressing 'a'
-            if (getFileContent() != "Modified content in storage")
+            LOK_ASSERT_EQUAL_MESSAGE("File contents not modified in storage",
+                                     std::string(ExpectedDocContent), getFileContent());
+            if (getFileContent() != ExpectedDocContent)
                 exitTest(TestResult::Failed);
             else
                 exitTest(TestResult::Ok);
@@ -61,13 +81,67 @@ public:
     bool filterSendMessage(const char* data, const size_t len, const WSOpCode /* code */,
                            const bool /* flush */, int& /*unitReturn*/) override
     {
-        std::string message(data, len);
-        if (message == "error: cmd=storage kind=documentconflict")
+        const std::string message(data, len);
+        switch (_phase)
         {
-            // we don't want to save current changes because doing so would
-            // overwrite the document which was changed underneath us
-            helpers::sendTextFrame(*getWs()->getLOOLWebSocket(), "closedocument", getTestname());
-            _phase = Phase::LoadNewDocument;
+            case Phase::WaitLoadStatus:
+            {
+                LOG_TST("filterSendMessage: Doc " << (_docLoaded == DocLoaded::Doc1 ? "1" : "2")
+                                                  << "(WaitLoadStatus): [" << message << ']');
+                if (_docLoaded == DocLoaded::Doc1 && Util::startsWith(message, "status:"))
+                {
+                    _phase = Phase::ModifyDoc;
+                    LOG_TST("filterSendMessage: Switching to Phase::ModifyDoc");
+                    SocketPoll::wakeupWorld();
+                }
+            }
+            break;
+            case Phase::WaitModifiedStatus:
+            {
+                LOG_TST("filterSendMessage: Doc " << (_docLoaded == DocLoaded::Doc1 ? "1" : "2")
+                                                  << "(WaitModifiedStatus): [" << message << ']');
+                if (_docLoaded == DocLoaded::Doc1
+                    && message == "statechanged: .uno:ModifiedStatus=true")
+                {
+                    _phase = Phase::ChangeStorageDoc;
+                    LOG_TST("filterSendMessage: Switching to Phase::ChangeStorageDoc");
+                    SocketPoll::wakeupWorld();
+                }
+            }
+            break;
+            case Phase::WaitSaveResponse:
+            {
+                LOG_TST("filterSendMessage: Doc " << (_docLoaded == DocLoaded::Doc1 ? "1" : "2")
+                                                  << "(WaitSaveResponse): [" << message << ']');
+                if (message == "error: cmd=storage kind=documentconflict")
+                {
+                    _phase = Phase::WaitDocClose;
+                    LOG_TST("filterSendMessage: Switching to Phase::WaitDocClose");
+
+                    // we don't want to save current changes because doing so would
+                    // overwrite the document which was changed underneath us
+                    helpers::sendTextFrame(*getWs()->getLOOLWebSocket(), "closedocument",
+                                           getTestname());
+                }
+            }
+            break;
+            case Phase::WaitDocClose:
+            {
+                LOG_TST("filterSendMessage: Doc " << (_docLoaded == DocLoaded::Doc1 ? "1" : "2")
+                                                  << "(WaitDocClose): [" << message << ']');
+                if (message == "exit")
+                {
+                    _phase = Phase::LoadNewDocument;
+                    LOG_TST("filterSendMessage: Switching to Phase::LoadNewDocument");
+                    SocketPoll::wakeupWorld();
+                }
+            }
+            break;
+            default:
+            {
+                // Nothing to do.
+            }
+            break;
         }
 
         return false;
@@ -79,59 +153,90 @@ public:
         {
             case Phase::Load:
             {
-                initWebsocket("/wopi/files/0?access_token=anything");
+                LOG_TST("Phase::Load");
                 _docLoaded = DocLoaded::Doc1;
+                _phase = Phase::WaitLoadStatus;
+
+                initWebsocket("/wopi/files/0?access_token=anything");
 
                 helpers::sendTextFrame(*getWs()->getLOOLWebSocket(), "load url=" + getWopiSrc(),
                                        getTestname());
-
-                _phase = Phase::ModifyAndChangeStorageDoc;
-                break;
             }
-            case Phase::ModifyAndChangeStorageDoc:
+            break;
+            case Phase::WaitLoadStatus:
             {
+                // Nothing to do.
+            }
+            break;
+            case Phase::ModifyDoc:
+            {
+                LOG_TST("Phase::ModifyAndChangeStorageDoc");
+                _phase = Phase::WaitModifiedStatus;
+
                 // modify the currently opened document; type 'a'
                 helpers::sendTextFrame(*getWs()->getLOOLWebSocket(), "key type=input char=97 key=0",
                                        getTestname());
                 helpers::sendTextFrame(*getWs()->getLOOLWebSocket(), "key type=up char=0 key=512",
                                        getTestname());
                 SocketPoll::wakeupWorld();
+            }
+            break;
+            case Phase::WaitModifiedStatus:
+            {
+                // Nothing to do.
+            }
+            break;
+            case Phase::ChangeStorageDoc:
+            {
+                // Change the document underneath, in storage.
+                LOG_TST("Phase::ChangeStorageDoc: changing contents in storage");
+                _phase = Phase::WaitSaveResponse;
 
-                // ModifiedStatus=true is a bit slow; let's sleep and hope that
-                // it is received before we wake up
-                std::this_thread::sleep_for(std::chrono::microseconds(POLL_TIMEOUT_MICRO_S));
+                setFileContent(ExpectedDocContent);
 
-                // change the document underneath, in storage
-                setFileContent("Modified content in storage");
-
-                // save the document; wsd should detect now that document has
+                // Save the document; wsd should detect now that document has
                 // been changed underneath it and send us:
                 // "error: cmd=storage kind=documentconflict"
+                // When we get it (in filterSendMessage, above),
+                // we will switch to Phase::LoadNewDocument.
+                LOG_TST("Phase::ChangeStorageDoc: saving");
                 helpers::sendTextFrame(*getWs()->getLOOLWebSocket(), "save", getTestname());
-
-                _phase = Phase::Polling;
-
-                break;
             }
+            break;
+            case Phase::WaitSaveResponse:
+            case Phase::WaitDocClose:
+            {
+                // Nothing to do.
+            }
+            break;
             case Phase::LoadNewDocument:
             {
-                initWebsocket("/wopi/files/0?access_token=anything");
-                _docLoaded = DocLoaded::Doc2;
+                // Now load the document again and, when we hit
+                // assertGetFileRequest, verify that its contents
+                // are the changed-in-storage (and not our modified).
+                // Unfortunately we don't have a way to find out
+                // if the previous document's DocBroker is gone
+                // since sending 'exit' to the kit, so wait a bit.
+                // We could add a call back after cleaning the
+                // DocBroker, but a short wait will do for now.
+                LOG_TST("Phase::LoadNewDocument: Reloading.");
+                _docLoaded = DocLoaded::Doc2; // Update before loading!
                 _phase = Phase::Polling;
-                break;
+
+                std::this_thread::sleep_for(std::chrono::seconds(1));
+                initWebsocket("/wopi/files/0?access_token=anything");
             }
+            break;
             case Phase::Polling:
             {
+                LOG_TST("Phase::Polling");
                 // just wait for the results
-                break;
             }
+            break;
         }
     }
 };
 
-UnitBase *unit_create_wsd(void)
-{
-    return new UnitWOPIDocumentConflict();
-}
+UnitBase* unit_create_wsd(void) { return new UnitWOPIDocumentConflict(); }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
