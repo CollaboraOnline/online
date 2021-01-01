496a3b314853b65e57a8c20d68421f03d3a09dff
mert <mert.tumer@collabora.com>
2021-01-01 19:16:17 +0300

    Added FontworkGalleryFloater dialog button on toolbar
    Change-Id: Ic878f3bef862fdecfba641907c6b5f70f8b37bd5
    Signed-off-by: mert <mert.tumer@collabora.com>
    Signed-off-by: Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>

--- loleaflet/src/unocommands.js
+++ loleaflet/src/unocommands.js
@@ -109,6 +109,7 @@ var unoCommandsArray = {
 	FilterMenu:{spreadsheet:{menu:_('More ~Filters'),},},
 	FontColor:{global:{menu:_('Font Color'),},text:{menu:_('Font Color'),},},
 	FontDialog:{global:{menu:_('C~haracter...'),},},
+	FontworkGalleryFloater:{global:{menu:_('Fontwork Style'),},},
 	FormatArea:{global:{menu:_('A~rea...'),},},
 	FormatAxis:{global:{menu:_('Format Axis...'),},},
 	FormatBulletsMenu:{global:{menu:_('Lis~ts'),},},
