8776d7b42bee7d6e0352527d7c47f2b8acf9e107
Pranam Lashkari <lpranam@collabora.com>
2021-01-01 08:05:51 +0530

    cypress: modified annotation tests to work with comment wizard
    Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
    Change-Id: I25ff9c590aa12d1787d8966cc4f68151ed8d505f

--- cypress_test/integration_tests/mobile/writer/insert_object_spec.js
+++ cypress_test/integration_tests/mobile/writer/insert_object_spec.js
@@ -32,7 +32,7 @@ describe('Insert objects via insertion wizard.', function() {
 			.should('exist');
 	});
 
-	it.skip('Insert comment.', function() {
+	it('Insert comment.', function() {
 		mobileHelper.openInsertionWizard();
 
 		cy.contains('.menu-entry-with-icon', 'Comment')
@@ -49,10 +49,10 @@ describe('Insert objects via insertion wizard.', function() {
 		cy.get('.vex-dialog-button-primary')
 			.click();
 
-		cy.get('.loleaflet-annotation')
+		cy.get('.wizard-comment-box.loleaflet-annotation-content-wrapper')
 			.should('exist');
 
-		cy.get('.loleaflet-annotation-content.loleaflet-dont-break')
+		cy.get('.wizard-comment-box .loleaflet-annotation-content')
 			.should('have.text', 'some text');
 	});
 
