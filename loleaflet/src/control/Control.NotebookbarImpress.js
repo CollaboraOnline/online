b8da6fc474a3240adf35a16ef656b6ddc1886f94
Pedro Pinto Silva <pedro.silva@collabora.com>
2021-01-08 13:30:38 +0100

    Impress: Expose Insert Line on classic and NotebookBar mode
    related: #1139
    Signed-off-by: Pedro Pinto Silva <pedro.silva@collabora.com>
    Change-Id: Iaa520f925eaf5808d7b1f9c3cf90bac0591c6640

--- loleaflet/src/control/Control.NotebookbarImpress.js
+++ loleaflet/src/control/Control.NotebookbarImpress.js
@@ -2366,6 +2366,19 @@ L.Control.NotebookbarImpress = L.Control.NotebookbarWriter.extend({
 																			'vertical': 'false'
 																		},
 																		{
+																			'id': 'Insert-Section-Line',
+																			'type': 'toolbox',
+																			'text': '',
+																			'enabled': 'true',
+																			'children': [
+																				{
+																					'type': 'bigtoolitem',
+																					'text': _UNO('.uno:Line', 'text'),
+																					'command': '.uno:Line'
+																				}
+																			]
+																		},
+																		{
 																			'id': 'Insert-Section-HeaderFooter',
 																			'type': 'container',
 																			'text': '',
