ee3c8aa7373158791c5c5d4c022a49a27a72510b
Pedro Pinto Silva <pedro.silva@collabora.com>
2021-01-12 13:10:45 +0100

    NotebookBar: Promote shareas to bigtoolitem
    Signed-off-by: Pedro Pinto Silva <pedro.silva@collabora.com>
    Change-Id: I6526ec70eac50d9295f9a53687e4ba41613205ec

--- loleaflet/src/control/Control.NotebookbarWriter.js
+++ loleaflet/src/control/Control.NotebookbarWriter.js
@@ -142,9 +142,9 @@ L.Control.NotebookbarWriter = L.Control.Notebookbar.extend({
 						'children': [
 							{
 								'id': 'shareas',
-								'type': 'menubartoolitem',
-								'text': _('Share...'),
-								'command': ''
+								'type': 'bigtoolitem',
+								'text': _('Share'),
+								'command': '.uno:shareas'
 							}
 						]
 					} : {},
