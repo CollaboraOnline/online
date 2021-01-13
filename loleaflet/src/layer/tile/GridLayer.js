1961a9b022d24eeb14462ec2297b35bca73ab103
mert <mert.tumer@collabora.com>
2021-01-13 16:54:50 +0300

    Fix calc text-input focus problem on Android
    Change-Id: Iacd279f741c35703807f14a9798fbe2ef0f45d50
    Signed-off-by: mert <mert.tumer@collabora.com>

--- loleaflet/src/layer/tile/GridLayer.js
+++ loleaflet/src/layer/tile/GridLayer.js
@@ -703,7 +703,8 @@ L.GridLayer = L.Layer.extend({
 			// this._cellCursorXY = new L.Point(-1, -1);
 			// this._prevCellCursorXY = new L.Point(0, 0);
 		}
-		this._initPreFetchPartTiles();
+		if (this._docType === 'presentation' || this._docType === 'drawing')
+			this._initPreFetchPartTiles();
 	},
 
 	_requestNewTiles: function () {
@@ -1017,7 +1018,8 @@ L.GridLayer = L.Layer.extend({
 			}
 			this._sendTileCombineRequest(coords.part, tilePositionsX, tilePositionsY);
 		}
-		this._initPreFetchPartTiles();
+		if (this._docType === 'presentation' || this._docType === 'drawing')
+			this._initPreFetchPartTiles();
 	},
 
 	_tileReady: function (coords, err, tile) {
