9c988b4b3ca9a14d84a9c53f88d37fd6d50b7c17
Szymon Kłos <szymon.klos@collabora.com>
2021-01-14 07:45:49 +0100

    notebookbar: cache calc review tab
    Change-Id: Ibf662b9794e5b68be448f24c6aa22e428d20905c
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>

--- loleaflet/src/unocommands.js
+++ loleaflet/src/unocommands.js
@@ -2,6 +2,7 @@
 
 var unoCommandsArray = {
 	AcceptAllTrackedChanges:{text:{menu:_('Accept All'),},},
+	AcceptChanges:{spreadsheet:{context:_('Manage Track Changes'),menu:_('~Manage...'),},},
 	AcceptTrackedChange:{text:{context:_('Accept Change'),menu:_('Accept'),},},
 	AcceptTrackedChanges:{text:{menu:_('~Manage...'),},},
 	AcceptTrackedChangeToNext:{text:{context:_('Accept Track Change and select the next one'),menu:_('Accept and Move to Next'),},},
@@ -408,6 +409,7 @@ var unoCommandsArray = {
 	TitlePageDialog:{text:{menu:_('Title Page...'),},},
 	ToggleMergeCells:{spreadsheet:{menu:_('M~erge and Center Cells'),},},
 	ToolsMenu:{global:{menu:_('~Tools'),},},
+	TraceChangeMode:{spreadsheet:{context:_('Record Track Changes'),menu:_('~Record'),},},
 	TrackChanges:{text:{menu:_('~Record'),},},
 	TransformDialog:{global:{menu:_('Position and Si~ze...'),},},
 	Underline:{global:{menu:_('Underline'),},},
