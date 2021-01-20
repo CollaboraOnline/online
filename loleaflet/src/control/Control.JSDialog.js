0f7e11aefa01d3737e8b7eaece52131a01571061
Szymon Kłos <szymon.klos@collabora.com>
2021-01-20 15:41:47 +0100

    jsdialog: warn when update not applied
    Change-Id: I8adaae41cb04dceea768f04b00200cd1e9867122
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>

--- loleaflet/src/control/Control.JSDialog.js
+++ loleaflet/src/control/Control.JSDialog.js
@@ -105,8 +105,10 @@ L.Control.JSDialog = L.Control.extend({
 			return;
 
 		var control = dialog.querySelector('#' + data.control.id);
-		if (!control)
+		if (!control) {
+			console.warn('jsdialogupdate: not found control with id: "' + data.control.id + '"');
 			return;
+		}
 
 		var parent = control.parentNode;
 		if (!parent)
