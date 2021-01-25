96f0ebb7c61553ece55b2b9beeb72f4ec27c4197
Dennis Francis <dennis.francis@collabora.com>
2021-01-25 16:47:42 +0530

    allow canvas-context getter from container
    Signed-off-by: Dennis Francis <dennis.francis@collabora.com>
    Change-Id: I6b5ba64220dae32c6155b7f6edb12c949ead33d9
    (cherry picked from commit 4f7662484e2895d6dd1df401a9b16c0b943eaed3)

--- loleaflet/src/layer/tile/CanvasSectionContainer.ts
+++ loleaflet/src/layer/tile/CanvasSectionContainer.ts
@@ -197,6 +197,10 @@ class CanvasSectionContainer {
 		this.canvas.ontouchcancel = this.onTouchCancel.bind(this);
 	}
 
+	getContext () {
+		return this.context;
+	}
+
 	setClearColor (color: string) {
 		this.clearColor = color;
 	}
