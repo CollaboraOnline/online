b4dd9f82a34f81fea576fb8003a851f0ee57e1d8
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2021-01-26 20:50:21 -0500

    wsd: saveToStorageInternal -> uploadToStorageInternal
    Change-Id: I0273314d65ebee91092a883bebeb53ab31051194
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- wsd/DocumentBroker.hpp
+++ wsd/DocumentBroker.hpp
@@ -354,12 +354,12 @@ private:
     /// with the child and cleans up ChildProcess etc.
     void terminateChild(const std::string& closeReason);
 
-    /// Saves the doc to the storage.
-    void saveToStorageInternal(const std::string& sesionId, bool success,
-                               const std::string& result = std::string(),
-                               const std::string& saveAsPath = std::string(),
-                               const std::string& saveAsFilename = std::string(),
-                               const bool isRename = false, const bool force = false);
+    /// Upload the doc to the storage.
+    void uploadToStorageInternal(const std::string& sesionId, bool success,
+                                 const std::string& result = std::string(),
+                                 const std::string& saveAsPath = std::string(),
+                                 const std::string& saveAsFilename = std::string(),
+                                 const bool isRename = false, const bool force = false);
 
     struct StorageUploadDetails
     {
