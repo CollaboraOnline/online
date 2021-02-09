29b7fdf360441287450b6273d4f0311c41151bae
Dennis Francis <dennis.francis@collabora.com>
2021-02-09 15:16:37 +0530

    remove hardcoding of debug-mode calc-grid drawingOrder
    Signed-off-by: Dennis Francis <dennis.francis@collabora.com>
    Change-Id: I1b074e73b5e8dd8110f66e3f639433a07c73ae57

--- loleaflet/src/layer/tile/TileLayer.js
+++ loleaflet/src/layer/tile/TileLayer.js
@@ -3841,7 +3841,7 @@ L.TileLayer = L.GridLayer.extend({
 		if (this._map._docLayer._docType === 'spreadsheet') {
 			var section = this._map._docLayer._painter._sectionContainer.getSectionWithName('calc grid');
 			if (section) {
-				section.drawingOrder = 7;
+				section.drawingOrder = L.CSections.CalcGrid.drawingOrderDebug;
 				section.sectionProperties.strokeStyle = 'blue';
 			}
 			this._map._docLayer._painter._addSplitsSection();
