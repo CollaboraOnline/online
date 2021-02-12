6a750c7d4c874157851e3d078ea48142cdef2299
Dennis Francis <dennis.francis@collabora.com>
2021-02-12 17:37:46 +0530

    missing header selections: set proper unit converter
    Signed-off-by: Dennis Francis <dennis.francis@collabora.com>
    Change-Id: Ib4f4fa326996ed29e044e392e58b19b342927cba
    (cherry picked from commit 7e8975dc238130df2f7c73f890bf6f3194c8ca2e)

--- loleaflet/src/control/Control.ColumnHeader.js
+++ loleaflet/src/control/Control.ColumnHeader.js
@@ -23,7 +23,7 @@ L.Control.ColumnHeader = L.Control.Header.extend({
 	onInitialize: function () {
 		this._map = L.Map.THIS;
 		this._isColumn = true;
-		this.converter = null;
+		this._setConverter();
 		this._current = -1;
 		this._resizeHandleSize = 15 * this.dpiScale;
 		this._selection = {start: -1, end: -1};
