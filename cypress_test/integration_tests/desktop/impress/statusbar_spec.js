b2ecbb044c8cd60e8060bede6e1ef597f8a1d023
Tamás Zolnai <tamas.zolnai@collabora.com>
2021-02-26 11:38:16 +0100

    cypress: add tests for statusbar (impress, desktop).
    Signed-off-by: Tamás Zolnai <tamas.zolnai@collabora.com>
    Change-Id: I5615c78d146510ce1f755d77a5d618bf3476522b
    (cherry picked from commit fdaacc4faa46ca2ecb7692660fb71fae0f2d254d)

---
+++ cypress_test/integration_tests/desktop/impress/statusbar_spec.js
@@ -0,0 +1,63 @@
+/* global describe it cy beforeEach require afterEach Cypress */
+
+var helper = require('../../common/helper');
+var desktopHelper = require('../../common/desktop_helper');
+
+describe('Statubar tests.', function() {
+	var testFileName = 'statusbar.odp';
+
+	beforeEach(function() {
+		helper.beforeAll(testFileName, 'impress');
+
+		if (Cypress.env('INTEGRATION') === 'nextcloud') {
+			desktopHelper.showStatusBarIfHidden ();
+		}
+	});
+
+	afterEach(function() {
+		helper.afterAll(testFileName, this.currentTest.state);
+	});
+
+	it('Selected slide.', function() {
+		cy.get('#PageStatus')
+			.should('have.text', 'Slide 1 of 2');
+
+		cy.get('#tb_actionbar_item_next')
+			.click();
+
+		cy.get('#PageStatus')
+			.should('have.text', 'Slide 2 of 2');
+
+		cy.get('#tb_actionbar_item_prev')
+			.click();
+
+		cy.get('#PageStatus')
+			.should('have.text', 'Slide 1 of 2');
+	});
+
+	it('Change zoom level.', function() {
+		cy.get('#tb_actionbar_item_zoomreset')
+			.click();
+
+		desktopHelper.shouldHaveZoomLevel('100');
+
+		desktopHelper.zoomIn();
+
+		desktopHelper.shouldHaveZoomLevel('120');
+
+		desktopHelper.zoomOut();
+
+		desktopHelper.shouldHaveZoomLevel('100');
+	});
+
+	it('Select zoom level.', function() {
+		cy.get('#tb_actionbar_item_zoomreset')
+			.click();
+
+		desktopHelper.shouldHaveZoomLevel('100');
+
+		desktopHelper.selectZoomLevel('280');
+
+		desktopHelper.shouldHaveZoomLevel('280');
+	});
+});
