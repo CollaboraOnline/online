45ee07e5831b3b760e383e2c453ecf6670151a2c
Dennis Francis <dennis.francis@collabora.com>
2021-02-26 21:52:02 +0530

    allow popups binding to CPath
    For this CPath needs capability to add handler(s) for arbitrary event
    types. This achieved by introducing a base class CEventsHandler which
    supports leaflet like functionality.
    
    """
    this.on('swipe', this.onSwip)
    this.off('popupclosed', this.popupClosed)
    """
    
    Signed-off-by: Dennis Francis <dennis.francis@collabora.com>
    Change-Id: Ia5d017096b98383491a25eec7353625a51703611
    (cherry picked from commit 0760c969c00afdeb34df415c45094acef87cc467)

---
+++ loleaflet/src/layer/vector/CEventsHandler.ts
@@ -0,0 +1,70 @@
+/* eslint-disable */
+
+abstract class CEventsHandler {
+
+	protected supportedEventNames = [
+		'add',
+		'remove',
+		'mouseenter',
+		'mouseleave'
+	];
+
+	private handlers = new Map<string, Set<Function>>();
+
+	constructor() {
+		var handlers = this.handlers;
+		this.supportedEventNames.forEach(function (eName) {
+			handlers.set(eName, new Set<Function>());
+		});
+	}
+
+	protected addSupportedEvents(eventNames: string[]) {
+		for (var i = 0; i < eventNames.length; ++i) {
+			var eName = eventNames[i];
+			if (this.handlers.has(eName))
+				continue;
+
+			this.supportedEventNames.push(eName);
+			this.handlers.set(eName, new Set<Function>());
+		}
+	}
+
+	on(eventName: string, handler: Function): boolean {
+		var handlerSet = this.handlers.get(eventName);
+		if (handlerSet === undefined) {
+			console.warn('Unknown event type: ' + eventName + ' used to register a handler');
+			return false;
+		}
+
+		handlerSet.add(handler);
+	}
+
+	off(eventName: string, handler: Function): boolean {
+		var handlerSet = this.handlers.get(eventName);
+		if (handlerSet === undefined) {
+			console.warn('Unknown event type: ' + eventName + ' used to unregister a handler');
+			return false;
+		}
+
+		var removed = handlerSet.delete(handler);
+		if (!removed) {
+			console.warn('Unregistered handler!');
+			return false;
+		}
+
+		return true;
+	}
+
+	fire(eventName: string): boolean {
+		var handlerSet = this.handlers.get(eventName);
+		if (handlerSet === undefined) {
+			console.warn('Unknown event type: ' + eventName);
+			return false;
+		}
+
+		var that = this;
+		handlerSet.forEach(function (handler) {
+			handler.call(that);
+		});
+	}
+}
