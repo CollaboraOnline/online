3982c7b34f14f651c66a3a3a2f554056b1dc22f8
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2021-02-28 14:17:45 -0500

    wsd: set errno when simulating socket errors for SSL
    The API is the same for both SSL and non-SSL sockets
    (they are virtual functions), and the API relies on
    errno to detect errors and decide to retry, log, or
    break. As such, we must set the same EAGAIN when
    simulating socket errors for both SSL and non-SSL
    cases, so that the behavior would also be the same.
    
    Change-Id: Ib1695cc94d93f5ecc53d7b22872f459ac8b11bbd
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- net/Socket.cpp
+++ net/Socket.cpp
@@ -74,11 +74,11 @@ bool StreamSocket::simulateSocketError(bool)
 {
     if ((socketErrorCount++ % 7) == 0)
     {
+        LOG_DBG("Simulating socket error.");
         errno = EAGAIN;
         return true;
     }
 
-    LOG_DBG("Simulating socket error.");
     return false;
 }
 
@@ -87,11 +87,12 @@ bool SslStreamSocket::simulateSocketError(bool read)
 {
     if ((socketErrorCount++ % 7) == 0)
     {
+        LOG_DBG("Simulating socket error.");
         _sslWantsTo = read ? SslWantsTo::Read : SslWantsTo::Write;
+        errno = EAGAIN;
         return true;
     }
 
-    LOG_DBG("Simulating socket error.");
     return false;
 }
 #endif
