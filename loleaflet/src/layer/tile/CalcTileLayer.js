f10bce9d9cf20801c899c00ae5c859efa6bbea7b
Dennis Francis <dennis.francis@collabora.com>
2021-03-01 15:36:32 +0530

    move message replay logic to TileLayer
    In canvas section based overlay objects the coordinates are stored in
    core pixels and they need to get updated and redrawn every time zoom is
    changed. Both Writer and Impress are also using canvas based overlays
    for selections and will soon be using it for the rest.
    
    Signed-off-by: Dennis Francis <dennis.francis@collabora.com>
    Change-Id: Iecd5bf8c875d4c4a6f8d055f2abccfeedd2cae8a

--- loleaflet/src/layer/tile/CalcTileLayer.js
+++ loleaflet/src/layer/tile/CalcTileLayer.js
@@ -263,66 +263,7 @@ L.CalcTileLayer = L.CanvasTileLayer.extend({
 		}
 	},
 
-	_saveMessageForReplay: function (textMsg, viewId) {
-		// If this.options.printTwipsMsgsEnabled is set, we will not get some messages (with coordinates)
-		// from core when zoom changes because print-twips coordinates are zoom-invariant. So we need to
-		// remember the last version of them and replay, when zoom is changed.
-		// We also need to replay the messages, when sheet-geometry changes. This is because it is possible for
-		// the updated print-twips messages to arrive before the sheet-geometry update message arrives.
-		if (!this.options.printTwipsMsgsEnabled) {
-			return;
-		}
-
-		if (!this._printTwipsMessagesForReplay) {
-			var ownViewTypes = [
-				'cellcursor',
-				'referencemarks',
-				'cellselectionarea',
-				'textselection',
-				'invalidatecursor',
-				'textselectionstart',
-				'textselectionend',
-				'graphicselection',
-			];
-
-			var otherViewTypes = [
-				'cellviewcursor',
-				'textviewselection',
-				'invalidateviewcursor',
-				'graphicviewselection',
-			];
-
-			this._printTwipsMessagesForReplay = new L.MessageStore(ownViewTypes, otherViewTypes);
-		}
-
-		var colonIndex = textMsg.indexOf(':');
-		if (colonIndex === -1) {
-			return;
-		}
-
-		var msgType = textMsg.substring(0, colonIndex);
-		this._printTwipsMessagesForReplay.save(msgType, textMsg, viewId);
-	},
-
-	_clearMsgReplayStore: function () {
-		if (!this.options.printTwipsMsgsEnabled || !this._printTwipsMessagesForReplay) {
-			return;
-		}
-
-		this._printTwipsMessagesForReplay.clear();
-	},
-
-	// See _saveMessageForReplay.
-	_replayPrintTwipsMsgs: function () {
-		if (!this.options.printTwipsMsgsEnabled || !this._printTwipsMessagesForReplay) {
-			return;
-		}
-
-		this._printTwipsMessagesForReplay.forEach(this._onMessage.bind(this));
-	},
-
 	_onMessage: function (textMsg, img) {
-		this._saveMessageForReplay(textMsg);
 		if (textMsg.startsWith('comment:')) {
 			var obj = JSON.parse(textMsg.substring('comment:'.length + 1));
 			obj.comment.tab = parseInt(obj.comment.tab);
@@ -1237,75 +1178,6 @@ L.CalcSplitPanesContext = L.SplitPanesContext.extend({
 		this.setSplitRow(newSplitCell.y) && this._docLayer.sendSplitIndex(newSplitCell.y, false /* isSplitCol */);
 	},
 });
-
-L.MessageStore = L.Class.extend({
-
-	// ownViewTypes : The types of messages related to own view.
-	// otherViewTypes: The types of messages related to other views.
-	initialize: function (ownViewTypes, otherViewTypes) {
-
-		if (!Array.isArray(ownViewTypes) || !Array.isArray(otherViewTypes)) {
-			console.error('Unexpected argument types');
-			return;
-		}
-
-		var ownMessages = {};
-		ownViewTypes.forEach(function (msgType) {
-			ownMessages[msgType] = '';
-		});
-		this._ownMessages = ownMessages;
-
-		var othersMessages = {};
-		otherViewTypes.forEach(function (msgType) {
-			othersMessages[msgType] = [];
-		});
-		this._othersMessages = othersMessages;
-	},
-
-	clear: function () {
-		var msgs = this._ownMessages;
-		Object.keys(msgs).forEach(function (msgType) {
-			msgs[msgType] = '';
-		});
-
-		msgs = this._othersMessages;
-		Object.keys(msgs).forEach(function (msgType) {
-			msgs[msgType] = [];
-		});
-	},
-
-	save: function (msgType, textMsg, viewId) {
-
-		var othersMessage = (typeof viewId === 'number');
-
-		if (!othersMessage && Object.prototype.hasOwnProperty.call(this._ownMessages, msgType)) {
-			this._ownMessages[msgType] = textMsg;
-			return;
-		}
-
-		if (othersMessage && Object.prototype.hasOwnProperty.call(this._othersMessages, msgType)) {
-			this._othersMessages[msgType][viewId] = textMsg;
-		}
-	},
-
-	forEach: function (callback) {
-		if (typeof callback !== 'function') {
-			console.error('Invalid callback type');
-			return;
-		}
-
-		var ownMessages = this._ownMessages;
-		Object.keys(this._ownMessages).forEach(function (msgType) {
-			callback(ownMessages[msgType]);
-		});
-
-		var othersMessages = this._othersMessages;
-		Object.keys(othersMessages).forEach(function (msgType) {
-			othersMessages[msgType].forEach(callback);
-		});
-	}
-});
-
 
 // TODO: Move these somewhere more appropriate.
 
