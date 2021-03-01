006096faea605a9540bd709c6a380b0af49c920e
Szymon Kłos <szymon.klos@collabora.com>
2021-03-01 12:43:11 +0100

    Add ids for shortcutsbar in draw
    needed for show/hide functionality using postmessage
    
    Change-Id: I9dc486c886ae7537963b339c7a4f08104a337e85
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>

--- loleaflet/src/control/Control.NotebookbarDraw.js
+++ loleaflet/src/control/Control.NotebookbarDraw.js
@@ -13,21 +13,25 @@ L.Control.NotebookbarDraw = L.Control.NotebookbarImpress.extend({
 				'type': 'toolbox',
 				'children': [
 					{
+						'id': 'menu',
 						'type': 'toolitem',
 						'text': _('Menu'),
 						'command': '.uno:Menubar'
 					},
 					{
+						'id': 'save',
 						'type': 'toolitem',
 						'text': _('Save'),
 						'command': '.uno:Save'
 					},
 					{
+						'id': 'undo',
 						'type': 'toolitem',
 						'text': _('Undo'),
 						'command': '.uno:Undo'
 					},
 					{
+						'id': 'redo',
 						'type': 'toolitem',
 						'text': _('Redo'),
 						'command': '.uno:Redo'
