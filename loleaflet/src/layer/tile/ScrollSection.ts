c84ad0fcea8cafcc275edaa5fd5c50f644d024cd
Gökay Şatır <gokay.satir@collabora.com>
2021-03-09 20:55:46 +0300

    ScrollSection: Improve scrolling with touchpad.
    Signed-off-by: Gökay Şatır <gokay.satir@collabora.com>
    Change-Id: I831df582ec21aa040f6af2df7b5e6c4f8883c988

--- loleaflet/src/layer/tile/ScrollSection.ts
+++ loleaflet/src/layer/tile/ScrollSection.ts
@@ -513,11 +513,7 @@ class ScrollSection {
 	}
 
 	private performVerticalScroll (delta: number) {
-		if (delta > 0)
-			this.scrollVerticalWithOffset(30);
-		else
-			this.scrollVerticalWithOffset(-30);
-
+		this.scrollVerticalWithOffset(delta / 2);
 		this.sectionProperties.drawVerticalScrollBar = true;
 		this.containerObject.requestReDraw();
 		this.sectionProperties.drawVerticalScrollBar = false;
@@ -525,11 +521,7 @@ class ScrollSection {
 	}
 
 	private performHorizontalScroll (delta: number) {
-		if (delta > 0)
-			this.scrollHorizontalWithOffset(30);
-		else
-			this.scrollHorizontalWithOffset(-30);
-
+		this.scrollHorizontalWithOffset(delta / 2);
 		this.sectionProperties.drawHorizontalScrollBar = true;
 		this.containerObject.requestReDraw();
 		this.sectionProperties.drawVerticalScrollBar = false;
