5af9f5ff9f2ebcfe94a6289f68cb38c409602ad2
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2021-03-11 21:10:59 -0500

    wsd: better logging
    Change-Id: I9c6bfcc22b1d80a27a4b4fa7229766d95fc62f9a
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- wsd/LOOLWSD.cpp
+++ wsd/LOOLWSD.cpp
@@ -2743,9 +2743,10 @@ private:
 
         if (serverId != Util::getProcessIdentifier())
         {
-            std::string errMsg = "Cluster configuration error: mis-matching "
-                "serverid " + serverId + " vs. " + Util::getProcessIdentifier() +
-                "on request to URL: "+ request.getURI();
+            const std::string errMsg = "Cluster configuration error: mis-matching "
+                                       "serverid "
+                                       + serverId + " vs. " + Util::getProcessIdentifier()
+                                       + "on request to URL: " + request.getURI();
             LOG_ERR(errMsg);
             // we got the wrong request.
             std::ostringstream oss;
