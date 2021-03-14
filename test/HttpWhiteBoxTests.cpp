97c44ef26ed904d36cf39e3129c154dafd1bd65f
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2021-03-14 18:59:33 -0400

    wsd: http: StatusLine tests
    Change-Id: Iac39191b407463d2691ec3c58af2a4b4abfc6645
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- test/HttpWhiteBoxTests.cpp
+++ test/HttpWhiteBoxTests.cpp
@@ -21,14 +21,72 @@ class HttpWhiteBoxTests : public CPPUNIT_NS::TestFixture
 {
     CPPUNIT_TEST_SUITE(HttpWhiteBoxTests);
 
+    CPPUNIT_TEST(testStatusLineParserValidComplete);
+    CPPUNIT_TEST(testStatusLineParserValidIncomplete);
+
     CPPUNIT_TEST(testRequestParserValidComplete);
     CPPUNIT_TEST(testRequestParserValidIncomplete);
 
     CPPUNIT_TEST_SUITE_END();
 
+    void testStatusLineParserValidComplete();
+    void testStatusLineParserValidIncomplete();
     void testRequestParserValidComplete();
     void testRequestParserValidIncomplete();
 };
+
+void HttpWhiteBoxTests::testStatusLineParserValidComplete()
+{
+    const int expVersionMajor = 1;
+    const int expVersionMinor = 1;
+    const std::string expVersion
+        = "HTTP/" + std::to_string(expVersionMajor) + '.' + std::to_string(expVersionMinor);
+    const int expStatusCode = 101;
+    const std::string expReasonPhrase = "Something Something";
+    const std::string data
+        = expVersion + ' ' + std::to_string(expStatusCode) + ' ' + expReasonPhrase + "\r\n";
+
+    http::StatusLine statusLine;
+
+    int64_t len = data.size();
+    LOK_ASSERT_EQUAL(http::FieldParseState::Valid, statusLine.parse(data.c_str(), len));
+    LOK_ASSERT_EQUAL(expVersion, statusLine.httpVersion());
+    LOK_ASSERT_EQUAL(expVersionMajor, statusLine.versionMajor());
+    LOK_ASSERT_EQUAL(expVersionMinor, statusLine.versionMinor());
+    LOK_ASSERT_EQUAL(expStatusCode, statusLine.statusCode());
+    LOK_ASSERT_EQUAL(expReasonPhrase, statusLine.reasonPhrase());
+}
+
+void HttpWhiteBoxTests::testStatusLineParserValidIncomplete()
+{
+    const int expVersionMajor = 1;
+    const int expVersionMinor = 1;
+    const std::string expVersion
+        = "HTTP/" + std::to_string(expVersionMajor) + '.' + std::to_string(expVersionMinor);
+    const int expStatusCode = 101;
+    const std::string expReasonPhrase = "Something Something";
+    const std::string data
+        = expVersion + ' ' + std::to_string(expStatusCode) + ' ' + expReasonPhrase + "\r\n";
+
+    http::StatusLine statusLine;
+
+    // Pass incomplete data to the reader.
+    for (std::size_t i = 0; i < data.size(); ++i)
+    {
+        // Should return 0 to signify data is incomplete.
+        int64_t len = i;
+        LOK_ASSERT_EQUAL_MESSAGE("i = " + std::to_string(i), http::FieldParseState::Incomplete,
+                                 statusLine.parse(data.c_str(), len));
+    }
+
+    int64_t len = data.size();
+    LOK_ASSERT_EQUAL(http::FieldParseState::Valid, statusLine.parse(data.c_str(), len));
+    LOK_ASSERT_EQUAL(expVersion, statusLine.httpVersion());
+    LOK_ASSERT_EQUAL(expVersionMajor, statusLine.versionMajor());
+    LOK_ASSERT_EQUAL(expVersionMinor, statusLine.versionMinor());
+    LOK_ASSERT_EQUAL(expStatusCode, statusLine.statusCode());
+    LOK_ASSERT_EQUAL(expReasonPhrase, statusLine.reasonPhrase());
+}
 
 void HttpWhiteBoxTests::testRequestParserValidComplete()
 {
