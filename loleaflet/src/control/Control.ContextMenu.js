317585ef6b623aad29ed0df031b8d1c10e9326c0
Michael Meeks <michael.meeks@collabora.com>
2021-03-17 09:02:03 +0000

    Enable pivot context menu items now we have the dialogs.
    Change-Id: I8673dfa1cce2f5ddf57e28558617699263681185
    Signed-off-by: Michael Meeks <michael.meeks@collabora.com>

--- loleaflet/src/control/Control.ContextMenu.js
+++ loleaflet/src/control/Control.ContextMenu.js
@@ -52,8 +52,8 @@ L.Control.ContextMenu = L.Control.extend({
 				   'SpellingAndGrammarDialog', 'FontDialog', 'FontDialogForParagraph',
 				   'SpellCheckIgnore'],
 
-			spreadsheet: ['MergeCells', 'SplitCell', 'RecalcPivotTable', 'FormatCellDialog',
-				          'ShowNote', 'HideNote', 'DeleteNote', 'SetAnchorToCell', 'SetAnchorToCellResize'],
+			spreadsheet: ['MergeCells', 'SplitCell', 'RecalcPivotTable', 'DataDataPilotRun', 'DeletePivotTable',
+				      'FormatCellDialog', 'ShowNote', 'HideNote', 'DeleteNote', 'SetAnchorToCell', 'SetAnchorToCellResize'],
 
 			presentation: ['SetDefault'],
 			drawing: []
@@ -81,7 +81,7 @@ L.Control.ContextMenu = L.Control.extend({
 			// text
 			'SpellingAndGrammarDialog', 'FontDialog', 'FontDialogForParagraph',
 			// spreadsheet
-			'FormatCellDialog',
+			'FormatCellDialog', 'DataDataPilotRun'
 		]
 	},
 
