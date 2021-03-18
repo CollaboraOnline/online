610cc4f2b8648ccabfde655e4a5c9c9445f623c9
Gökay Şatır <gokay.satir@collabora.com>
2021-03-18 18:32:44 +0300

    CanvasTileLayer: Update cursor position.
    When on mobile, if user clicks somewhere near to bottom of the screen,
    virtual keyboard opens.
    
    When virtual keyboard opens, "_onUpdateCursor" function is called.
    Then onResize function is called.
    Since onResize is called last, document thinks that user can still see the
    cursor. In this case, we call _onUpdateCursor function one more time,
    for scrolling the document if needed.
    
    Signed-off-by: Gökay Şatır <gokay.satir@collabora.com>
    Change-Id: I47690defa21c7469c7dabe11a5021b26d696a426

--- loleaflet/src/layer/tile/CanvasTileLayer.js
+++ loleaflet/src/layer/tile/CanvasTileLayer.js
@@ -574,6 +574,8 @@ L.CanvasTileLayer = L.TileLayer.extend({
 		var tileContainer = this._container;
 		if (tileContainer) {
 			var size = this._map.getPixelBounds().getSize();
+			var heightIncreased = parseInt(this._painter._sectionContainer.canvas.style.height.replace('px', '')) < size.y;
+
 			if (this._docType === 'spreadsheet') {
 				var offset = this._getUIWidth() + this._getGroupWidth();
 				offset += (this._getGroupWidth() > 0 ? 3: 1);
@@ -605,6 +607,9 @@ L.CanvasTileLayer = L.TileLayer.extend({
 				this._painter._sectionContainer.getSectionWithName(L.CSections.RowHeader.name)._updateCanvas();
 				this._painter._sectionContainer.getSectionWithName(L.CSections.ColumnHeader.name)._updateCanvas();
 			}
+
+			if (!heightIncreased)
+				this._onUpdateCursor(true);
 		}
 	},
 
