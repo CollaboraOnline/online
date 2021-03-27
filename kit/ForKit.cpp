12bdeb547410eb9570243dd2f910f644386b5398
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2021-03-26 21:38:31 -0400

    wsd: simplify lokit_main call
    By defining NoCapsForKit and NoSeccomp
    as const values for the KIT_IN_PROCESS
    case, we have a more consistent and
    simpler call-site.
    
    Change-Id: Ife553eab9eaffd96560d4cfb4b8fdcdf26a1b9db
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>
    (cherry picked from commit e0aa90dd3a4002dadf9c0909f98944bb8f8e0f54)

--- kit/ForKit.cpp
+++ kit/ForKit.cpp
@@ -49,6 +49,9 @@ static bool NoSeccomp = false;
 #if ENABLE_DEBUG
 static bool SingleKit = false;
 #endif
+#else
+static const bool NoCapsForKit = true; // NoCaps for in-process kit.
+static const bool NoSeccomp = true; // NoSeccomp for in-process kit.
 #endif
 
 static std::string UserInterface;
@@ -369,11 +372,8 @@ static int createLibreOfficeKit(const std::string& childRoot,
             }
         }
 
-#ifndef KIT_IN_PROCESS
-        lokit_main(childRoot, jailId, sysTemplate, loTemplate, loSubPath, NoCapsForKit, NoSeccomp, queryVersion, DisplayVersion, spareKitId);
-#else
-        lokit_main(childRoot, jailId, sysTemplate, loTemplate, loSubPath, true, true, queryVersion, DisplayVersion, spareKitId);
-#endif
+        lokit_main(childRoot, jailId, sysTemplate, loTemplate, loSubPath, NoCapsForKit, NoSeccomp,
+                   queryVersion, DisplayVersion, spareKitId);
     }
     else
     {
