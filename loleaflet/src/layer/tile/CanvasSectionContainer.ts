a818672575b0305e0da289aa4e4ce7a25f450870
Gökay Şatır <gokay.satir@collabora.com>
2021-03-30 13:40:12 +0300

    Remove extra check for positionOnMouseDown
    We unintendedly solved the same bug with Szymon (while working on
    different tasks), i removed the second
    check.
    
    Signed-off-by: Gökay Şatır <gokay.satir@collabora.com>
    Change-Id: I33b22364b58036fb57809476ebc689d9b542b4bc

--- loleaflet/src/layer/tile/CanvasSectionContainer.ts
+++ loleaflet/src/layer/tile/CanvasSectionContainer.ts
@@ -1128,13 +1128,11 @@ class CanvasSectionContainer {
 			this.mousePosition = this.convertPositionToCanvasLocale(e);
 			this.draggingSomething = true;
 
-			if (this.positionOnMouseDown !== null) {
-				this.dragDistance = [this.mousePosition[0] - this.positionOnMouseDown[0], this.mousePosition[1] - this.positionOnMouseDown[1]];
+			this.dragDistance = [this.mousePosition[0] - this.positionOnMouseDown[0], this.mousePosition[1] - this.positionOnMouseDown[1]];
 
-				var section: CanvasSectionObject = this.getSectionWithName(this.sectionOnMouseDown);
-				if (section) {
-					this.propagateOnMouseMove(section, this.convertPositionToSectionLocale(section, this.mousePosition), this.dragDistance, <MouseEvent><any>e);
-				}
+			var section: CanvasSectionObject = this.getSectionWithName(this.sectionOnMouseDown);
+			if (section) {
+				this.propagateOnMouseMove(section, this.convertPositionToSectionLocale(section, this.mousePosition), this.dragDistance, <MouseEvent><any>e);
 			}
 		}
 		else if (e.touches.length === 2) {
