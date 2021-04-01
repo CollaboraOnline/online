9fe7d4569365fffd72f71a063414a10c3717e7f6
Gökay Şatır <gokay.satir@collabora.com>
2021-04-01 15:04:26 +0300

    Enable AutoFillMarkerSection.
    Signed-off-by: Gökay Şatır <gokay.satir@collabora.com>
    Change-Id: Ic2cf7edbec1ca09259b555fee5d5972467cea291

--- loleaflet/src/layer/tile/AutoFillMarkerSection.ts
+++ loleaflet/src/layer/tile/AutoFillMarkerSection.ts
@@ -25,6 +25,7 @@ class AutoFillMarkerSection {
 	zIndex: number = L.CSections.AutoFillMarker.zIndex;
 	interactable: boolean = true;
 	sectionProperties: any = {};
+	stopPropagating: Function; // Implemented by section container.
 	map: any;
 
 	constructor () {
@@ -32,6 +33,12 @@ class AutoFillMarkerSection {
 
 		this.sectionProperties.docLayer = this.map._docLayer;
 		this.sectionProperties.tsManager = this.sectionProperties.docLayer._painter;
+		this.sectionProperties.draggingStarted = false;
+		this.sectionProperties.cursorRectangle = null; // Selected area or cell cursor, in core pixels.
+		this.sectionProperties.autoFillRectangle = null; // Autofill marker's rectangle.
+		this.sectionProperties.skipThisDraggingEvent = false; // If it is not handled at first mouse move, we shouldn't handle it until the event ends.
+		this.sectionProperties.dragStartPosition = null;
+		this.sectionProperties.handlingMouseEvent = false;
 	}
 
 	public onInitialize () {
@@ -43,13 +50,103 @@ class AutoFillMarkerSection {
 	}
 
 	public onDraw () {
+		if (this.sectionProperties.docLayer) {
+			if (this.sectionProperties.docLayer._cellSelectionAreaPixels)
+				this.sectionProperties.cursorRectangle = this.sectionProperties.docLayer._cellSelectionAreaPixels.clone();
+			else if (this.sectionProperties.docLayer._cellCursorPixels)
+				this.sectionProperties.cursorRectangle = this.sectionProperties.docLayer._cellCursorPixels.clone();
+			else
+				this.sectionProperties.cursorRectangle = null;
 
+			if (this.sectionProperties.cursorRectangle) {
+				var topLeft: Array<number>;
+				var size: number;
+				if ((<any>window).mode.isDesktop()) {
+					size = 4 * this.dpiScale;
+					this.sectionProperties.cursorRectangle.moveBy(-size, -size);
+					topLeft = [this.sectionProperties.cursorRectangle.getX2(), this.sectionProperties.cursorRectangle.getY2()];
+
+				}
+				else {
+					size = 8 * this.dpiScale;
+					this.sectionProperties.cursorRectangle.moveBy(-size, -size);
+					topLeft = [this.sectionProperties.cursorRectangle.getX2() - this.sectionProperties.cursorRectangle.getWidth() * 0.5, this.sectionProperties.cursorRectangle.getY2()];
+				}
+
+				this.context.fillStyle = 'black';
+				this.context.fillRect(topLeft[0] - this.documentTopLeft[0], topLeft[1] - this.documentTopLeft[1], 2 * size, 2 * size);
+
+				this.sectionProperties.autoFillRectangle = L.LOUtil.createRectangle(topLeft[0], topLeft[1], size * 2, size * 2);
+			}
+			else {
+				this.sectionProperties.autoFillRectangle = null;
+			}
+		}
+	}
+
+	public onMouseMove (point: Array<number>, dragDistance: Array<number>, e: MouseEvent) {
+		if (dragDistance === null || !this.sectionProperties.docLayer._cellAutoFillAreaPixels || !this.sectionProperties.handlingMouseEvent)
+			return; // No dragging or no event handling or auto fill marker is not visible.
+
+		var pos: any;
+
+		if (!this.sectionProperties.draggingStarted) { // Is it first move?
+			this.sectionProperties.draggingStarted = true;
+			this.sectionProperties.dragStartPosition = this.sectionProperties.docLayer._cellAutoFillAreaPixels.getCenter();
+			pos = new L.Point(this.sectionProperties.dragStartPosition[0], this.sectionProperties.dragStartPosition[1]);
+			pos = this.sectionProperties.docLayer._corePixelsToTwips(pos);
+			this.sectionProperties.docLayer._postMouseEvent('buttondown', pos.x, pos.y, 1, 1, 0);
+		}
+
+		point[0] = this.sectionProperties.dragStartPosition[0] + dragDistance[0];
+		point[1] = this.sectionProperties.dragStartPosition[1] + dragDistance[1];
+		pos = this.sectionProperties.docLayer._corePixelsToTwips(new L.Point(point[0], point[1]));
+
+		console.log('pos:' + JSON.stringify(pos));
+		this.sectionProperties.docLayer._postMouseEvent('move', pos.x, pos.y, 1, 1, 0);
+
+		if (this.sectionProperties.handlingMouseEvent) {
+			this.map.scrollingIsHandled = true;
+			this.stopPropagating(); // Stop propagating to sections.
+			e.stopPropagation(); // Stop native event.
+		}
+	}
+
+	public onMouseUp (point: Array<number>, e: MouseEvent) {
+		if (this.sectionProperties.draggingStarted) {
+			this.sectionProperties.draggingStarted = false;
+			point[0] += this.sectionProperties.cursorRectangle.getWidth() * 0.5;
+			point[1] += this.sectionProperties.cursorRectangle.getHeight() * 0.5;
+			var pos = this.sectionProperties.docLayer._corePixelsToTwips(new L.Point(point[0], point[1]));
+			this.sectionProperties.docLayer._postMouseEvent('buttonup', pos.x, pos.y, 1, 1, 0);
+		}
+
+		this.sectionProperties.skipThisDraggingEvent = false;
+		this.map.scrollingIsHandled = false;
+
+		if (this.sectionProperties.handlingMouseEvent) {
+			this.stopPropagating();
+			e.stopPropagation();
+			this.sectionProperties.handlingMouseEvent = false;
+			(<any>window).IgnorePanning = false;
+		}
+	}
+
+	public onMouseDown (point: Array<number>, e: MouseEvent) {
+		// Just to be safe. We don't need this, but it makes no harm.
+		this.sectionProperties.handlingMouseEvent = false;
+
+		if (this.sectionProperties.docLayer._cellAutoFillAreaPixels && this.sectionProperties.autoFillRectangle) {
+			if (this.sectionProperties.autoFillRectangle.containsPoint(point[0] + this.documentTopLeft[0], point[1] + this.documentTopLeft[1])) {
+				this.sectionProperties.handlingMouseEvent = true;
+				this.stopPropagating();
+				e.stopPropagation();
+				(<any>window).IgnorePanning = true; // We'll keep this until we have consistent sections and remove map element.
+			}
+		}
 	}
 
 	public onMouseWheel () {}
-	public onMouseMove () {}
-	public onMouseDown () {}
-	public onMouseUp () {}
 	public onMouseEnter () {}
 	public onMouseLeave () {}
 	public onClick () {}
