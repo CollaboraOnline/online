acf6a3742224e4b5650a35e8f8d0f8dd071132c5
Tor Lillqvist <tml@collabora.com>
2021-04-15 15:11:44 +0300

    Avoid intentional newlines in log messages to avoid risk of messing up log post-processing
    Signed-off-by: Tor Lillqvist <tml@collabora.com>
    Change-Id: If2ea9d6454e62f1971505510ff5c79ee4f2c96c2
    Signed-off-by: Tor Lillqvist <tml@collabora.com>

--- wsd/ProofKey.cpp
+++ wsd/ProofKey.cpp
@@ -148,12 +148,12 @@ Proof::Proof()
         catch (const Poco::FileNotFoundException& e)
         {
             std::string msg = e.displayText() +
-                "\nNo proof-key will be present in discovery."
-                "\nIf you need to use WOPI security, generate an RSA key using this command:"
-                "\n    loolwsd-generate-proof-key"
-                "\nor if your config dir is not /etc, you can run ssh-keygen manually:"
-                "\n    ssh-keygen -t rsa -N \"\" -m PEM -f \"" + keyPath + "\""
-                "\nNote: the proof_key file must be readable by the loolwsd process.";
+                ". No proof-key will be present in discovery."
+                " If you need to use WOPI security, generate an RSA key using this command:"
+                " loolwsd-generate-proof-key ."
+                " If your config dir is not /etc, you can run ssh-keygen manually:"
+                " ssh-keygen -t rsa -N \"\" -m PEM -f \"" + keyPath + "\" ."
+                " Note: the proof_key file must be readable by the loolwsd process.";
             LOG_WRN(msg);
         }
         catch (const Poco::Exception& e)
