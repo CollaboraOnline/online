342133054e28dbe24f0408336de19f5c7cfdf4bd
Henry Castro <hcastro@collabora.com>
2021-04-16 11:16:37 -0400

    loleaflet: fix error messages when a document is not loaded
    It happens when reconnecting, a Macro Warning dialog is opened,
    but the document is not loaded yet.
    
    Change-Id: If98cf4d68ac8ef71d7c0ad3d8aa5151a77adbe4b
    Signed-off-by: Henry Castro <hcastro@collabora.com>

--- loleaflet/src/layer/tile/CanvasTileLayer.js
+++ loleaflet/src/layer/tile/CanvasTileLayer.js
@@ -1111,6 +1111,8 @@ L.CanvasTileLayer = L.TileLayer.extend({
 	},
 
 	_sendClientVisibleArea: function (forceUpdate) {
+		if (!this._map._docLoaded)
+			return;
 
 		var splitPos = this._splitPanesContext ? this._splitPanesContext.getSplitPos() : new L.Point(0, 0);
 
@@ -1265,9 +1267,9 @@ L.CanvasTileLayer = L.TileLayer.extend({
 					'height=' + this._tileHeightPx + ' ' +
 					'tileposx=' + tilePositionsX + ' ' +
 					'tileposy=' + tilePositionsY + ' ' +
-				        'wid=' + tileWids + ' ' +
+					'wid=' + tileWids + ' ' +
 					'tilewidth=' + this._tileWidthTwips + ' ' +
-				        'tileheight=' + this._tileHeightTwips;
+					'tileheight=' + this._tileHeightTwips;
 
 				this._map._socket.sendMessage(message, '');
 			}
