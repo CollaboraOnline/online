5f84435693cc052be340ed69a277e169cdafd6a9
Gökay Şatır <gokay.satir@collabora.com>
2021-04-20 13:06:07 +0300

    Session: Send file mode to the client. We need to know if comments are editable or not.
    Signed-off-by: Gökay Şatır <gokay.satir@collabora.com>
    Change-Id: I607e165b373ca6605ae5d72bf45821e3a3870f76

--- wsd/DocumentBroker.cpp
+++ wsd/DocumentBroker.cpp
@@ -655,6 +655,15 @@ bool DocumentBroker::download(const std::shared_ptr<ClientSession>& session, con
                 session->setAllowChangeComments();
             }
         }
+        else if (wopifileinfo->getUserCanWrite())
+        {
+            session->setReadOnly(false);
+            session->setAllowChangeComments(true);
+        }
+
+        // We will send the client about information of the usage type of the file.
+        // Some file types may be treated differently than others.
+        session->sendFileMode(session->isReadOnly(), session->isAllowChangeComments());
 
         // Construct a JSON containing relevant WOPI host properties
         Object::Ptr wopiInfo = new Object();
@@ -745,6 +754,7 @@ bool DocumentBroker::download(const std::shared_ptr<ClientSession>& session, con
                     session->setAllowChangeComments();
                 }
             }
+            session->sendFileMode(session->isReadOnly(), session->isAllowChangeComments());
         }
     }
 
