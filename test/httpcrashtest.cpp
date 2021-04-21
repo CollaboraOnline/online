1c832412ca4f371c90a0f8c09776397d1e49112f
Miklos Vajna <vmiklos@collabora.com>
2021-04-21 10:24:10 +0200

    unit-crash: avoid socket deletion before _socketPoll completes the async shutdown
    (cherry picked from commit 1ff7f2704bf1058d977744eb2b0258579d418df2)
    
    Signed-off-by: Miklos Vajna <vmiklos@collabora.com>
    Change-Id: Ibb9f7d7681d502d06de92c2f5d8305c3f27a76c0

--- test/httpcrashtest.cpp
+++ test/httpcrashtest.cpp
@@ -246,6 +246,10 @@ void HTTPCrashTest::testCrashForkit()
         socket->asyncShutdown();
         LOK_ASSERT_MESSAGE("Expected successful disconnection of the WebSocket",
                            socket->waitForDisconnection(std::chrono::seconds(5)));
+
+        // Wait till async shutdown fully completes, as _socketPoll has no owning reference to
+        // socket.
+        _socketPoll->wakeupWorld();
     }
     catch (const Poco::Exception& exc)
     {
