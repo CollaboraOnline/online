e0f409ff9f13e2d969706dd89e48115adf0c3624
Andreas_K <andreas_k@abwesend.de>
2021-04-23 15:33:01 +0200

    Notebookbar Insert Tab update #2007
    Update the structure of the Insert Notebookbar Tab for all apps.
    
    ### general sections:
    1. section: app specific
    2. section: Image/Table/Chartbigtoolitem will be the most used item depending on the app
    3+4. section: Hyperlink and Field values
    5. section: Textbox Shapes + *new* inkl. Fontwork and Line
    6. section: Headers Symbol and Comment
    
    ### writer:
    - Fontwork move to Textbox/Shape section
    - Line move to Textbox/Shape Section
    - Formating Mark + Symbol + Comment are a new Section (Formating Mark is bigtoolitem hardcoded)
    
    ### calc:
    - Fontwork move to Textbox/Shape section same with Line
    - Header and Footer + Symbol and Comment are is a new section
    
    ### impress/draw:
    - Fontwork move to Textbox/Shape section same with Line
    - Header and Footer + Symbol and Comment are is a new section
    
    Signed-off-by: Andreas_K <andreas_k@abwesend.de>

--- loleaflet/src/control/Control.NotebookbarWriter.js
+++ loleaflet/src/control/Control.NotebookbarWriter.js
@@ -741,11 +741,6 @@ L.Control.NotebookbarWriter = L.Control.Notebookbar.extend({
 				'command': '.uno:HyperlinkDialog'
 			},
 			{
-				'type': 'bigtoolitem',
-				'text': _UNO('.uno:FontworkGalleryFloater'),
-				'command': '.uno:FontworkGalleryFloater'
-			},
-			{
 				'id': 'Insert-Section-Bookmark',
 				'type': 'container',
 				'children': [
@@ -814,7 +809,7 @@ L.Control.NotebookbarWriter = L.Control.Notebookbar.extend({
 				'command': '.uno:DrawText'
 			},
 			{
-				'id': 'Insert-BasicShapes-VerticalText',
+				'id': 'Insert-BasicShapes-Shapes',
 				'type': 'container',
 				'children': [
 					{
@@ -834,6 +829,35 @@ L.Control.NotebookbarWriter = L.Control.Notebookbar.extend({
 						'children': [
 							{
 								'type': 'toolitem',
+								'text': _UNO('.uno:Line', 'text'),
+								'command': '.uno:Line'
+							}
+						]
+					}
+				],
+				'vertical': 'true'
+			},
+			{
+				'id': 'Insert-Text-Fontwork',
+				'type': 'container',
+				'children': [
+					{
+						'id': 'LineA153',
+						'type': 'toolbox',
+						'children': [
+							{
+								'type': 'toolitem',
+								'text': _UNO('.uno:FontworkGalleryFloater'),
+								'command': '.uno:FontworkGalleryFloater'
+							}
+						]
+					},
+					{
+						'id': 'LineB163',
+						'type': 'toolbox',
+						'children': [
+							{
+								'type': 'toolitem',
 								'text': _UNO('.uno:VerticalText', 'text'),
 								'command': '.uno:VerticalText'
 							}
@@ -843,25 +867,39 @@ L.Control.NotebookbarWriter = L.Control.Notebookbar.extend({
 				'vertical': 'true'
 			},
 			{
-				'type': 'bigtoolitem',
-				'text': _UNO('.uno:CharmapControl'),
-				'command': '.uno:CharmapControl'
-			},
-			{
-				'type': 'bigtoolitem',
-				'text': _UNO('.uno:Line', 'text'),
-				'command': '.uno:Line'
-			},
-			{
-				'type': 'bigtoolitem',
-				'text': _UNO('.uno:InsertAnnotation', 'text'),
-				'command': '.uno:InsertAnnotation'
-			},
-			{
 				'id': 'FormattingMarkMenu:FormattingMarkMenu',
 				'type': 'menubutton',
 				'text': _UNO('.uno:FormattingMarkMenu', 'text'),
 				'command': '.uno:FormattingMarkMenu'
+			},
+			{
+				'id': 'Insert-Charmap-Annotation',
+				'type': 'container',
+				'children': [
+					{
+						'id': 'LineA153',
+						'type': 'toolbox',
+						'children': [
+							{
+								'type': 'toolitem',
+								'text': _UNO('.uno:CharmapControl'),
+								'command': '.uno:CharmapControl'
+							}
+						]
+					},
+					{
+						'id': 'LineB163',
+						'type': 'toolbox',
+						'children': [
+							{
+								'type': 'toolitem',
+								'text': _UNO('.uno:InsertAnnotation', 'text'),
+								'command': '.uno:InsertAnnotation'
+							}
+						]
+					}
+				],
+				'vertical': 'true'
 			}
 		];
 
