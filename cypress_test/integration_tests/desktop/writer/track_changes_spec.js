171cacb764f83d98354dd41b331add684ab8f914
Rash419 <rashesh.padia@collabora.com>
2021-04-27 11:52:56 +0530

    cypress:added tests for track changes in writer
    Signed-off-by: Rash419 <rashesh.padia@collabora.com>
    Change-Id: Ifad0ecc0527b4bb1f1c76e66c79285c045b7af9a

---
+++ cypress_test/integration_tests/desktop/writer/track_changes_spec.js
@@ -0,0 +1,87 @@
+/* global describe it cy beforeEach require afterEach*/
+
+var helper = require('../../common/helper');
+
+describe('Track Changes', function () {
+	var testFileName = 'shape_operations.odt';
+
+	beforeEach(function () {
+		helper.beforeAll(testFileName, 'writer');
+	});
+
+	afterEach(function () {
+		helper.afterAll(testFileName, this.currentTest.state);
+	});
+
+	function confirmChange(action) {
+		cy.get('#menu-editmenu')
+			.click()
+			.get('#menu-changesmenu')
+			.click()
+			.contains(action)
+			.click();
+	}
+
+	//enable record for track changes
+	function enableRecord() {
+		cy.get('#menu-editmenu')
+			.click()
+			.get('#menu-changesmenu')
+			.click()
+			.contains('Record')
+			.click();
+
+		//if we don't wait , the test will fail in CLI
+		cy.wait(200);
+
+		cy.get('#menu-editmenu')
+			.click()
+			.get('#menu-changesmenu')
+			.click()
+			.contains('Record')
+			.should('have.class', 'lo-menu-item-checked');
+
+		//to close
+		cy.get('#menu-changesmenu')
+			.click();
+	}
+
+	it('Accept All', function () {
+
+		helper.typeIntoDocument('Hello World');
+
+		enableRecord();
+
+		helper.clearAllText();
+		//if we don't wait , the test will fail in CLI
+		cy.wait(200);
+
+		helper.selectAllText();
+
+		confirmChange('Accept All');
+
+		helper.typeIntoDocument('{ctrl}a');
+
+		helper.textSelectionShouldNotExist();
+	});
+
+	it('Reject All', function () {
+
+		helper.typeIntoDocument('Hello World');
+
+		enableRecord();
+
+		helper.clearAllText();
+
+		//if we don't wait , the test will fail in CLI
+		cy.wait(400);
+
+		confirmChange('Reject All');
+
+		cy.get('.leaflet-layer').click();
+
+		helper.selectAllText();
+
+		helper.expectTextForClipboard('\nHello World');
+	});
+});
