9114d98a7e2176b003ab1debfb7085f61fffbbb3
merttumer <mert.tumer@collabora.com>
2021-04-28 07:56:15 +0000

    android: Hide progress dialog on initial mobilewizard pop up
    Text import dialog is displayed on initial document load
    so we need to hide progressbar after that in order to
    interact with the ui
    
    Signed-off-by: merttumer <mert.tumer@collabora.com>
    Change-Id: I840bfea9d078260751622cbb09d256153821ff0e

--- android/lib/src/main/java/org/libreoffice/androidlib/LOActivity.java
+++ android/lib/src/main/java/org/libreoffice/androidlib/LOActivity.java
@@ -1031,6 +1031,11 @@ public class LOActivity extends AppCompatActivity {
                 }
                 return false;
             }
+            case "hideProgressbar": {
+                if (mProgressDialog != null)
+                    mProgressDialog.dismiss();
+                return false;
+            }
             case "loadwithpassword": {
                 mProgressDialog.determinate(R.string.loading);
                 return true;
