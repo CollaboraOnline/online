f97941e1598af97d3e28c074c69c74d3bd7d87d1
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2021-05-01 16:39:48 -0400

    wsd: http: complete the http response only once
    Change-Id: I2c015f004883272bb6efe4ffc5a14e3cc101f60f
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- net/HttpRequest.hpp
+++ net/HttpRequest.hpp
@@ -1174,10 +1174,10 @@ private:
             _socket->shutdown(); // Flag for shutdown for housekeeping in SocketPoll.
             _socket->closeConnection(); // Immediately disconnect.
             _socket.reset();
+            _response->complete();
         }
 
         _connected = false;
-        _response->complete();
     }
 
     bool connect()
