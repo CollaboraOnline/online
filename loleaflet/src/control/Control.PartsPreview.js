CollaboraOnline/online

commit 651a4f546aa07a42e0a5416af317ef46851c69b3

fix Keyboard pops up when switching slides
Change-Id: I6b0953e4868867786a8c63c5ccbf132baa0d0324
Reviewed-on: https://gerrit.libreoffice.org/84935
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Mert Tümer <mert.tumer@collabora.com>

loleaflet/src/control/Control.PartsPreview.js
@@ -170,6 +170,7 @@ L.Control.PartsPreview = L.Control.extend({
 			.on(img, 'click', this._map.focus, this._map)
 			.on(img, 'click', function() {
 				this.partsFocused = true;
+				document.activeElement.blur();
 			}, this);
 
 		var topBound = this._previewContTop;

commit 4636f533d0f03fbd86130a3539b451ee422bac9e

loleaflet: Make PgUp/Down work in slide sorter
Regression from 651a4f546aa07a42e0a5416af317ef46851c69b3

Signed-off-by: Aron Budea <aron.budea@collabora.com>
Change-Id: I2b97f84fd50ba4259bbfbf266e28ce7299012804

loleaflet/src/control/Control.PartsPreview.js
@@ -207,7 +207,11 @@ L.Control.PartsPreview = L.Control.extend({
 				this._setPart(e);
 				this._map.focus();
 				this.partsFocused = true;
-				document.activeElement.blur();
+				if (!window.mode.isDesktop()) {
+					// needed so on-screen keyboard doesn't pop up when switching slides,
+					// but would cause PgUp/Down to not work on desktop in slide sorter
+					document.activeElement.blur();
+				}
 			}
 		}, this);
 