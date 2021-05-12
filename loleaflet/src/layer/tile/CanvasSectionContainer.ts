f8eb5ec3f77a420e524c6a7d87837eafb048464f
Gökay Şatır <gokay.satir@collabora.com>
2021-05-12 17:55:26 +0300

    CanvasSectionContainer: Don't search not-shown sections.
    Signed-off-by: Gökay Şatır <gokay.satir@collabora.com>
    Change-Id: I97e948652793bdb9caa21ca1b3d34fbfe7e209b1

--- loleaflet/src/layer/tile/CanvasSectionContainer.ts
+++ loleaflet/src/layer/tile/CanvasSectionContainer.ts
@@ -982,7 +982,7 @@ class CanvasSectionContainer {
 
 	findSectionContainingPoint (point: Array<number>): any {
 		for (var i: number = this.sections.length - 1; i > -1; i--) { // Search from top to bottom. Top section will be sent as target section.
-			if (this.sections[i].isLocated && (!this.sections[i].documentObject || this.sections[i].isVisible) && this.doesSectionIncludePoint(this.sections[i], point))
+			if (this.sections[i].isLocated && this.sections[i].showSection && (!this.sections[i].documentObject || this.sections[i].isVisible) && this.doesSectionIncludePoint(this.sections[i], point))
 				return this.sections[i];
 		}
 
