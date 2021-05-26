ec8ddad0d4ebbcfea1e53faa235763a40cd44974
Szymon Kłos <szymon.klos@collabora.com>
2021-05-26 12:44:43 +0200

    Add UI blocking functions to UIManager
    - when UI is blocked any interactions with sidebar/dialogs,
      jsdialogs or menu/toolbar items are blocked.
    
    to use: fire 'blockUI' / 'unblockUI' events
    or call directly: map.uiManager.blockUI() / .unblockUI()
    
    Change-Id: If96b066fecc610f4a6f4d3b4457f6dfce9a681cd
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>

--- loleaflet/src/control/Control.UIManager.js
+++ loleaflet/src/control/Control.UIManager.js
@@ -7,6 +7,7 @@
 /* global $ setupToolbar w2ui w2utils toolbarUpMobileItems _ */
 L.Control.UIManager = L.Control.extend({
 	mobileWizard: null,
+	blockedUI: false,
 
 	onAdd: function (map) {
 		this.map = map;
@@ -31,6 +32,9 @@ L.Control.UIManager = L.Control.extend({
 			history.pushState({context: 'app-started'}, 'app-started');
 			history.pushState({context: 'app-started'}, 'app-started');
 		}
+
+		map.on('blockUI', this.blockUI, this);
+		map.on('unblockUI', this.unblockUI, this);
 	},
 
 	// UI initialization
@@ -563,6 +567,20 @@ L.Control.UIManager = L.Control.extend({
 				this.enterReadonlyOrClose();
 			}
 		}
+	},
+
+	// Blocking UI
+
+	isUIBlocked: function() {
+		return this.blockedUI;
+	},
+
+	blockUI: function() {
+		this.blockedUI = true;
+	},
+
+	unblockUI: function() {
+		this.blockedUI = false;
 	},
 
 	// Helper functions
