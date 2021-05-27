80e01baeb2f683339617c5c8c90f21032ec99bec
Andreas-Kainz <kainz.a@gmail.com>
2021-05-27 12:55:03 +0200

    Mobile Sidebar Remove Grow Shrink commands
    As there is already the FontSize DropDown menu available in the Character Sidebar an additional Grow/Shrink command isn't needed.
    All Character Direct Formating commands are within one line without other commands
    
    Signed-off-by: Andreas-Kainz <kainz.a@gmail.com>
    Change-Id: I0da32f4e678045fdacefdd4ef7f953f1be9c99ee

--- loleaflet/src/control/Control.MobileWizardBuilder.js
+++ loleaflet/src/control/Control.MobileWizardBuilder.js
@@ -26,6 +26,8 @@ L.Control.MobileWizardBuilder = L.Control.JSDialogBuilder.extend({
 		this._toolitemHandlers['.uno:FontworkAlignmentFloater'] = function () { return false; };
 		this._toolitemHandlers['.uno:FontworkCharacterSpacingFloater'] = function () { return false; };
 		this._toolitemHandlers['.uno:ExtrusionToggle'] = function () { return false; };
+		this._toolitemHandlers['.uno:Grow'] = function () { return false; };
+		this._toolitemHandlers['.uno:Shrink'] = function () { return false; };
 
 		this._toolitemHandlers['.uno:FontworkShapeType'] = this._fontworkShapeControl;
 	},
