6360e818250eb9cb039804944baefb216a5c3dc8
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2021-06-01 18:47:49 -0400

    wsd: http: sync requests accept external poller
    Reusing a SocketPoll is encouraged rather than
    creating throwaway instances. Session now supports
    external SocketPoll instances for the sync members
    as it does for the async (which is unavoidable).
    
    Also, use TerminatingPoll to break the poll
    when the terminating the process is flagged.
    
    Change-Id: Ide6f674080ce20bb764c482871e48c94d83d4214
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- net/HttpRequest.hpp
+++ net/HttpRequest.hpp
@@ -978,8 +978,8 @@ public:
     /// Note: when the server returns an error, the response body,
     /// if any, will be stored in memory and can be read via getBody().
     /// I.e. when statusLine().statusCategory() != StatusLine::StatusCodeClass::Successful.
-    const std::shared_ptr<const Response> syncDownload(const Request& req,
-                                                       const std::string& saveToFilePath)
+    const std::shared_ptr<const Response>
+    syncDownload(const Request& req, const std::string& saveToFilePath, SocketPoll& poller)
     {
         LOG_TRC("syncDownload: " << req.getVerb() << ' ' << host() << ':' << port() << ' '
                                  << req.getUrl());
@@ -989,7 +989,27 @@ public:
         if (!saveToFilePath.empty())
             _response->saveBodyToFile(saveToFilePath);
 
-        syncRequestImpl();
+        syncRequestImpl(poller);
+        return _response;
+    }
+
+    /// Make a synchronous request to download a file to the given path.
+    const std::shared_ptr<const Response> syncDownload(const Request& req,
+                                                       const std::string& saveToFilePath)
+    {
+        TerminatingPoll poller("HttpSynReqPoll");
+        return syncDownload(req, saveToFilePath, poller);
+    }
+
+    /// Make a synchronous request.
+    /// The payload body of the response, if any, can be read via getBody().
+    const std::shared_ptr<const Response> syncRequest(const Request& req, SocketPoll& poller)
+    {
+        LOG_TRC("syncRequest: " << req.getVerb() << ' ' << host() << ':' << port() << ' '
+                                << req.getUrl());
+
+        newRequest(req);
+        syncRequestImpl(poller);
         return _response;
     }
 
@@ -997,12 +1017,8 @@ public:
     /// The payload body of the response, if any, can be read via getBody().
     const std::shared_ptr<const Response> syncRequest(const Request& req)
     {
-        LOG_TRC("syncRequest: " << req.getVerb() << ' ' << host() << ':' << port() << ' '
-                                << req.getUrl());
-
-        newRequest(req);
-        syncRequestImpl();
-        return _response;
+        TerminatingPoll poller("HttpSynReqPoll");
+        return syncRequest(req, poller);
     }
 
     /// Make a synchronous request with the given timeout.
@@ -1023,6 +1039,11 @@ public:
         return responsePtr;
     }
 
+    /// Start an asynchronous request on the given SocketPoll.
+    /// Return true when it dispatches the socket to the SocketPoll.
+    /// Note: when reusing this Session, it is assumed that the socket
+    /// is already added to the SocketPoll on a previous call (do not
+    /// use multiple SocketPoll instances on the same Session).
     bool asyncRequest(const Request& req, SocketPoll& poll)
     {
         LOG_TRC("asyncRequest: " << req.getVerb() << ' ' << host() << ':' << port() << ' '
@@ -1048,7 +1069,7 @@ public:
 
 private:
     /// Make a synchronous request.
-    bool syncRequestImpl()
+    bool syncRequestImpl(SocketPoll& poller)
     {
         const std::chrono::microseconds timeout = getTimeout();
         const auto deadline = std::chrono::steady_clock::now() + timeout;
@@ -1057,8 +1078,6 @@ private:
 
         if (!isConnected() && !connect())
             return false;
-
-        SocketPoll poller("HttpSynReqPoll");
 
         poller.insertNewSocket(_socket);
         poller.poll(timeout.count());
