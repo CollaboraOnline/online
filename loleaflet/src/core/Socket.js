7c856538a645c776a120ea89f44775386f0e5d5e
Michael Meeks <michael.meeks@collabora.com>
2021-06-11 17:48:38 +0100

    Remove 1ms timeout thrash while loading resources asynchronously.
    Saves spinning the mainloop while waiting for images to load.
    
    Change-Id: I7978d30443493047325ad20e3278ba75d0533133
    Signed-off-by: Michael Meeks <michael.meeks@collabora.com>

--- loleaflet/src/core/Socket.js
+++ loleaflet/src/core/Socket.js
@@ -284,6 +284,17 @@ app.definitions.Socket = L.Class.extend({
 			     'background:#ddf;color:black', color, 'color:black');
 	},
 
+	_queueSlurpEventEmission: function() {
+		var that = this;
+		if (!that._slurpTimer)
+		{
+			that._slurpTimer = setTimeout(function () {
+				that._slurpTimer = undefined;
+				that._emitSlurpedEvents();
+			}, 1 /* ms */);
+		}
+	},
+
 	_emitSlurpedEvents: function() {
 		var queueLength = this._slurpQueue.length;
 		var completeEventWholeFunction = this.createCompleteTraceEvent('loleaflet._emitSlurpedEvents',
@@ -323,12 +334,8 @@ app.definitions.Socket = L.Class.extend({
 							completeEventOneMessage.finish();
 					}
 				} else {
-					// Stop emitting, continue in the next timer from where we left off.
+					// Stop emitting, re-start when we async images load.
 					this._slurpQueue = this._slurpQueue.slice(i, queueLength);
-					var that = this;
-					this._slurpTimer = setTimeout(function () {
-						that._emitSlurpedEvents();
-					}, 1 /* ms */);
 					complete = false;
 					break;
 				}
@@ -365,9 +372,7 @@ app.definitions.Socket = L.Class.extend({
 	_slurpMessage: function(e) {
 		var that = this;
 		if (!this._slurpQueue || !this._slurpQueue.length) {
-			setTimeout(function() {
-				that._emitSlurpedEvents();
-			}, 1 /* ms */);
+			this._queueSlurpEventEmission();
 			that._slurpQueue = [];
 		}
 		this._extractTextImg(e);
@@ -443,14 +448,20 @@ app.definitions.Socket = L.Class.extend({
 		if (e.textMsg.indexOf(' nopng') !== -1)
 			return;
 
+		var that = this;
 		var img = this._extractImage(e);
-
 		e.image = new Image();
 		e.image.onload = function() {
 			e.imageIsComplete = true;
 			if (window.ThisIsTheiOSApp) {
 				window.webkit.messageHandlers.lool.postMessage('REMOVE ' + e.image.src, '*');
 			}
+			that._queueSlurpEventEmission();
+		};
+		e.image.onerror = function(err) {
+			console.log('Failed to load image ' + img + ' fun ' + err);
+			e.imageIsComplete = true;
+			that._queueSlurpEventEmission();
 		};
 		e.image.src = img;
 	},
