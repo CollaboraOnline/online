236f0cfc46013d2071e7485f4419b2dae852e6e3
Tor Lillqvist <tml@collabora.com>
2021-06-17 13:31:27 +0300

    Output a Trace Event metadata event identifying each named thread
    Move the generic dummy implementation of
    TraceEvent::emitOneRecording() to a source file of its own. (That is
    the one which is used in test and tool executables.)
    
    Signed-off-by: Tor Lillqvist <tml@collabora.com>
    Change-Id: I81cab07e5a6852b42d278a5446c13c3825cf546e

--- kit/ForKit.cpp
+++ kit/ForKit.cpp
@@ -514,7 +514,6 @@ int main(int argc, char** argv)
     SigUtil::setTerminationSignals();
 #endif
 
-    Util::setThreadName("forkit");
     Util::setApplicationPath(Poco::Path(argv[0]).parent().toString());
 
     // Initialization
@@ -682,6 +681,8 @@ int main(int argc, char** argv)
     const auto conf = std::getenv("LOOL_CONFIG");
     config::initialize(std::string(conf ? conf : std::string()));
 #endif
+
+    Util::setThreadName("forkit");
 
     LOG_INF("Preinit stage OK.");
 
