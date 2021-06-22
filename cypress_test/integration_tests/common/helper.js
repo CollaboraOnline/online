954f96be4e9f1e88aace266c90eb9d882b47fc96
Dennis Francis <dennis.francis@collabora.com>
2021-06-22 18:05:16 +0530

    cypress: restore shape text alignment test
    There is a problem with slides that does not fit into cypress tests
    viewport.  Use the center of the visible area of the svg to
    click/dblclick else clicking on the true center the event will not end
    up in the svg. For example the click event could hit the slide
    sorter(empty space in this case) or the invisible area outside the
    viewport.
    
    Signed-off-by: Dennis Francis <dennis.francis@collabora.com>
    Change-Id: Iefa73f56b67be0b4c251921aa91404dbbfbed671

--- cypress_test/integration_tests/common/helper.js
+++ cypress_test/integration_tests/common/helper.js
@@ -1076,6 +1076,14 @@ class Bounds {
 			&& this.height !== undefined);
 	}
 
+	get right() {
+		return this.left + this.width;
+	}
+
+	get bottom() {
+		return this.top + this.height;
+	}
+
 	static parseBoundsJson(boundsJsonString) {
 		var jsonObject = JSON.parse(boundsJsonString);
 		return new Bounds(jsonObject.top, jsonObject.left, jsonObject.width, jsonObject.height);
@@ -1163,6 +1171,14 @@ function typeIntoInputField(selector, text, clearBefore = true, prop = true)
 	cy.log('Typing into input field - end.');
 }
 
+function getVisibleBounds(domRect) {
+	return new Bounds(
+		Math.max(0, domRect.top),
+		Math.max(0, domRect.left),
+		domRect.width,
+		domRect.height);
+}
+
 module.exports.loadTestDoc = loadTestDoc;
 module.exports.checkIfDocIsLoaded = checkIfDocIsLoaded;
 module.exports.assertCursorAndFocus = assertCursorAndFocus;
@@ -1202,3 +1218,4 @@ module.exports.getOverlayItemBounds = getOverlayItemBounds;
 module.exports.overlayItemHasBounds = overlayItemHasBounds;
 module.exports.overlayItemHasDifferentBoundsThan = overlayItemHasDifferentBoundsThan;
 module.exports.typeIntoInputField = typeIntoInputField;
+module.exports.getVisibleBounds = getVisibleBounds;
