729413d8b8a4383c0730687674af085462a68b0d
Rash419 <rashesh.padia@collabora.com>
2021-06-24 17:23:25 +0530

    cypress:added table_operation tests for writer and impress
    Signed-off-by: Rash419 <rashesh.padia@collabora.com>
    Change-Id: Ife9398bc6b2fe92aafe9df3b26a99c47b04f8a16

--- cypress_test/plugins/whitelists.js
+++ cypress_test/plugins/whitelists.js
@@ -1,6 +1,8 @@
 var notebookbarOnlyList = [
 	'desktop/writer/top_toolbar_spec.js',
 	'desktop/calc/cell_appearance_spec.js',
+	'desktop/writer/table_operation_spec.js',
+	'desktop/impress/table_operation_spec.js',
 ];
 
 module.exports.notebookbarOnlyList = notebookbarOnlyList;
