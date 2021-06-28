e3a35290eec83b4469e3138d94fd7d32ba2e6fac
Gökay ŞATIR <gokaysatir@gmail.com>
2021-06-28 15:57:23 +0300

    PDF View: Added Cypress test for page up & down functionality.
    Signed-off-by: Gökay ŞATIR <gokaysatir@gmail.com>
    Change-Id: Icc6f752e1cb406fb7c90e6d32520be531515d6eb

---
+++ cypress_test/integration_tests/desktop/draw/pdf_page_up_down_spec.js
@@ -0,0 +1,23 @@
+/* global describe it cy require afterEach beforeEach */
+
+var helper = require('../../common/helper');
+
+describe('PDF View Tests', function() {
+	var testFileName = 'pdf_page_up_down.pdf';
+
+	beforeEach(function() {
+		helper.beforeAll(testFileName, 'draw');
+	});
+
+	afterEach(function() {
+		helper.afterAll(testFileName, this.currentTest.state);
+	});
+
+	it('PDF page down', { env: { 'pdf-view': true } }, function() {
+		cy.get('#map').type('{pagedown}');
+		cy.get('#preview-frame-part-1').should('have.attr', 'style', 'border: 2px solid darkgrey;');
+
+		cy.get('#map').type('{pageup}');
+		cy.get('#preview-frame-part-0').should('have.attr', 'style', 'border: 2px solid darkgrey;');
+	});
+});
