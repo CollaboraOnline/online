cbf9dd67c3b1eea08f62069cb4d6008758e0edb3
Tor Lillqvist <tml@collabora.com>
2021-07-05 17:49:58 +0300

    Bypass updateFreemiumStatus unless ENABLE_FREEMIUM
    Fixes build of iOS app.
    
    Change-Id: Icf4e49c7d4d8f77a37557e1990f37673e1b9c91d
    Signed-off-by: Tor Lillqvist <tml@collabora.com>

--- kit/ChildSession.cpp
+++ kit/ChildSession.cpp
@@ -272,7 +272,9 @@ bool ChildSession::_handleInput(const char *buffer, int length)
     }
     else if (tokens.equals(0, "freemiumstatus"))
     {
+#ifdef ENABLE_FREEMIUM
         return updateFreemiumStatus(buffer, length, tokens);
+#endif
     }
     else
     {
@@ -2526,6 +2528,7 @@ int ChildSession::getSpeed()
     return _cursorInvalidatedEvent.size();
 }
 
+#ifdef ENABLE_FREEMIUM
 bool ChildSession::updateFreemiumStatus(const char* /*buffer*/, int /*length*/, const StringVector& tokens)
 {
     std::string status;
@@ -2539,6 +2542,7 @@ bool ChildSession::updateFreemiumStatus(const char* /*buffer*/, int /*length*/, 
     getLOKitDocument()->setFreemiumView(_viewId, status == "true");
     return true;
 }
+#endif
 
 void ChildSession::loKitCallback(const int type, const std::string& payload)
 {
