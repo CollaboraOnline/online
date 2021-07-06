8ffc1f9b16300bb5099b96f5e1498cfaa5aa6ba1
Szymon Kłos <szymon.klos@collabora.com>
2021-07-06 21:07:56 +0200

    mobile-wizard: add menubutton/toolbar popups
    popup is injected into container prepared by toolitem/menubutton
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Signed-off-by: Pedro Pinto Silva <pedro.silva@collabora.com>
    Change-Id: Ic75474c4da757f6401f6d347ab8942048b2db5c4

--- loleaflet/src/control/Control.MobileWizardBuilder.js
+++ loleaflet/src/control/Control.MobileWizardBuilder.js
@@ -21,12 +21,14 @@ L.Control.MobileWizardBuilder = L.Control.JSDialogBuilder.extend({
 		this._controlHandlers['basespinfield'] = this.baseSpinField;
 		this._controlHandlers['radiobutton'] = this._radiobuttonControl;
 		this._controlHandlers['edit'] = this._editControl;
+		this._controlHandlers['mobile-popup-container'] = this._mobilePopupContainer;
 
 		this._toolitemHandlers['.uno:FontworkAlignmentFloater'] = function () { return false; };
 		this._toolitemHandlers['.uno:FontworkCharacterSpacingFloater'] = function () { return false; };
 		this._toolitemHandlers['.uno:ExtrusionToggle'] = function () { return false; };
 
 		this._toolitemHandlers['.uno:FontworkShapeType'] = this._fontworkShapeControl;
+		this._toolitemHandlers['.uno:XLineStyle'] = this._explorableToolItemHandler;
 	},
 
 	baseSpinField: function(parentContainer, data, builder, customCallback) {
@@ -492,6 +494,27 @@ L.Control.MobileWizardBuilder = L.Control.JSDialogBuilder.extend({
 		builder.build(parentContainer, json);
 	},
 
+	_mobilePopupContainer: function(parentContainer, data) {
+		var container = L.DomUtil.create('div', 'mobile-popup-container', parentContainer);
+		container.id = 'popup-' + data.id;
+		return false;
+	},
+
+	_explorableToolItemHandler: function(parentContainer, data, builder) {
+		data.text = builder._cleanText(data.text);
+
+		var onShow = function() {
+			builder.callback('toolbox', 'togglemenu', {id: data.parent.id}, data.command, builder);
+		};
+
+		var nodeId = data.command.indexOf('.uno:') === 0 ? data.command.substr('.uno:'.length) : data.command;
+		var contentNode = {id: nodeId, type: 'mobile-popup-container', children: [], onshow: onShow};
+		var iconPath = builder._createIconURL(data.command);
+
+		builder._explorableEntry(parentContainer, data, contentNode, builder, null, iconPath);
+
+		return false;
+	},
 	build: function(parent, data) {
 		this._amendJSDialogData(data);
 		for (var childIndex in data) {
--- loleaflet/src/layer/tile/TileLayer.js
+++ loleaflet/src/layer/tile/TileLayer.js
@@ -1210,7 +1210,7 @@ L.TileLayer = L.GridLayer.extend({
 	},
 
 	_openMobileWizard: function(data) {
-		this._map.fire('mobilewizard', data);
+		this._map.fire('mobilewizard', {data: data});
 	},
 
 	_closeMobileWizard: function() {
@@ -4218,7 +4218,7 @@ L.TileLayer = L.GridLayer.extend({
 	_openCommentWizard: function(annotation) {
 		window.commentWizard = true;
 		var menuData = this._map._docLayer.getCommentWizardStructure();
-		this._map.fire('mobilewizard', menuData);
+		this._map.fire('mobilewizard', {data: menuData});
 
 		// if annotation is provided we can select perticular comment
 		if (annotation) {
