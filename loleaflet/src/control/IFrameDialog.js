0233bc6aa4d19c4c91b5bf6e131ad432bc272ce7
Pedro Pinto Silva <pedro.silva@collabora.com>
2021-07-07 15:50:33 +0200

    Feedback: Use proper location properties and pass isMobile
    Signed-off-by: Pedro Pinto Silva <pedro.silva@collabora.com>
    Change-Id: Ic1e46c1722efa416d7fffb55cbf71e5efa9e9a31

--- loleaflet/src/control/IFrameDialog.js
+++ loleaflet/src/control/IFrameDialog.js
@@ -13,12 +13,18 @@ L.IFrameDialog = L.Class.extend({
 		this._content = L.DomUtil.create('div', 'iframe-dialog-content', this._container);
 		this._iframe = L.DomUtil.create('iframe', 'iframe-dialog-modal', this._content);
 		this._iframe.id = 'iframe-feedback';
+		this._isMobile = false;
+		if (window.mode.isMobile()) {
+			this._isMobile = true;
+		}
+		console.debug(options);
 
 		console.debug('Getting co-bg-color: ');
 		var cssVar = getComputedStyle(document.documentElement).getPropertyValue('--co-primary-element');
 		console.debug(cssVar);
 		cssVar = cssVar.replace(/\s/g, '');
-		url += '?'+cssVar;
+		url += '?'+this._isMobile;
+		url += cssVar;
 		this._iframe.src = url;
 	},
 
