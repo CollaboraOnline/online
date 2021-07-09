8d74e3557a7bfb48b3d6d59f3ca4d105168a76ea
Gökay ŞATIR <gokaysatir@gmail.com>
2021-07-09 12:43:53 +0300

    Calc: Issues with comments.
    Open new comments directly in the cell's position, without animation.
    
    Focus on the comment box.
    
    Signed-off-by: Gökay ŞATIR <gokaysatir@gmail.com>
    Change-Id: I0dda23baaa9c8cdd940fc9d429e0e72e5464b854

--- loleaflet/src/layer/tile/CanvasTileLayer.js
+++ loleaflet/src/layer/tile/CanvasTileLayer.js
@@ -2095,6 +2095,7 @@ L.CanvasTileLayer = L.Layer.extend({
 			this._cellCursor = L.LatLngBounds.createDefault();
 			this._cellCursorXY = new L.Point(-1, -1);
 			this._cellCursorPixels = null;
+			app.file.calc.cellCursor.visible = false;
 			if (autofillMarkerSection)
 				autofillMarkerSection.calculatePositionViaCellCursor(null);
 		}
@@ -2113,6 +2114,10 @@ L.CanvasTileLayer = L.Layer.extend({
 			var start = this._twipsToCorePixels(this._cellCursorTwips.min);
 			var offsetPixels = this._twipsToCorePixels(this._cellCursorTwips.getSize());
 			this._cellCursorPixels = L.LOUtil.createRectangle(start.x, start.y, offsetPixels.x, offsetPixels.y);
+			app.file.calc.cellCursor.address = [parseInt(strTwips[4]), parseInt(strTwips[5])];
+			app.file.calc.cellCursor.rectangle.pixels = [Math.round(start.x), Math.round(start.y), Math.round(offsetPixels.x), Math.round(offsetPixels.y)];
+			app.file.calc.cellCursor.rectangle.twips = [parseInt(strTwips[0]), parseInt(strTwips[1]), parseInt(strTwips[2]), parseInt(strTwips[3])];
+			app.file.calc.cellCursor.visible = true;
 			if (autofillMarkerSection)
 				autofillMarkerSection.calculatePositionViaCellCursor([this._cellCursorPixels.getX2(), this._cellCursorPixels.getY2()]);
 
