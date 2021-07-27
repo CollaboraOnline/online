186e63a9bc9ae6c6eab75a1f92cde323771aebc4
Szymon Kłos <szymon.klos@collabora.com>
2021-07-27 10:50:20 +0200

    Rename mobile popup to be more general busypopup -> popup
    we will use it not only for busypopup
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I775d5af3838d2e6caf6bdbda9ff4864b919eb34b

--- loleaflet/src/control/Control.MobileWizard.js
+++ loleaflet/src/control/Control.MobileWizard.js
@@ -64,7 +64,7 @@ L.Control.MobileWizard = L.Control.extend({
 		$('#mobile-wizard-titlebar').css('top', '0px');
 		$('#mobile-wizard').removeClass('menuwizard');
 		$('#mobile-wizard').removeClass('funcwizard');
-		$('#mobile-wizard').removeClass('busypopup');
+		$('#mobile-wizard').removeClass('popup');
 		this._isTabMode = false;
 		this._currentPath = [];
 		this._tabs = [];
@@ -499,7 +499,7 @@ L.Control.MobileWizard = L.Control.extend({
 				// force hide scroll indicator while its showing/hidding is not fixed
 				$('#mobile-wizard-scroll-indicator').hide();
 
-				$('#mobile-wizard').addClass('busypopup');
+				$('#mobile-wizard').addClass('popup');
 				$('#mobile-wizard-titlebar').hide();
 			}
 
