9fc8f7d94f30f0e45a25cfa53d03be874404cfc9
Szymon Kłos <szymon.klos@collabora.com>
2021-07-27 18:05:56 +0200

    cypress: add simple sidebar tests
    - test menubuttons
    - test toolbutton popups
    - test deck switch
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: Ia928345982b855e9dbf850e969165566cc74f9d5

---
+++ cypress_test/integration_tests/desktop/impress/sidebar_spec.js
@@ -0,0 +1,86 @@
+/* global describe it cy require afterEach beforeEach */
+
+var helper = require('../../common/helper');
+var impressHelper = require('../../common/impress_helper');
+
+describe('Sidebar Tests', function() {
+	var testFileName = 'sidebar.odp';
+
+	beforeEach(function() {
+		helper.beforeAll(testFileName, 'impress');
+	});
+
+	afterEach(function() {
+		helper.afterAll(testFileName, this.currentTest.state);
+	});
+
+
+	it('Switch to slide transition Deck', function() {
+		cy.get('#tb_editbar_item_slidechangewindow .w2ui-button')
+			.should('not.have.class', 'checked');
+
+		cy.get('#layoutvalueset')
+			.should('be.visible');
+
+		cy.get('#tb_editbar_item_slidechangewindow .w2ui-button')
+			.click({force: true});
+
+		cy.get('#tb_editbar_item_slidechangewindow .w2ui-button')
+			.should('have.class', 'checked');
+
+		cy.get('#layoutvalueset')
+			.should('not.exist');
+
+		cy.get('#transitions_icons')
+			.should('be.visible');
+	});
+
+	it('Set gradient background color', function() {
+		cy.get('#fillattr2')
+			.should('not.be.visible');
+
+		cy.get('#fillstyle select')
+			.select('Gradient');
+
+		cy.get('#fillattr2')
+			.should('be.visible');
+
+		cy.get('#fillattr3')
+			.should('be.visible');
+
+		cy.get('#fillattr2')
+			.click();
+
+		cy.get('.modalpopup')
+			.should('be.visible');
+
+		cy.get('#colorset')
+			.should('be.visible');
+	});
+
+	it('Set underline using popup', function() {
+		cy.get('#layoutvalueset')
+			.should('be.visible');
+
+		impressHelper.selectTextShapeInTheCenter();
+
+		impressHelper.selectTextOfShape();
+
+		cy.get('#layoutvalueset')
+			.should('not.be.visible');
+
+		cy.get('#Underline .arrowbackground')
+			.click();
+
+		cy.get('.modalpopup')
+			.should('be.visible');
+
+		cy.get('#single')
+			.click();
+
+		impressHelper.triggerNewSVGForShapeInTheCenter();
+
+		cy.get('.leaflet-pane.leaflet-overlay-pane g.Page .TextParagraph')
+			.should('have.attr', 'text-decoration', 'underline');
+	});
+});
