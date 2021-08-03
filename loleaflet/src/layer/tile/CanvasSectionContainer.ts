c93b1c8ab2b49d84c4d9d8b3620e067c73ab5b8a
Michael Meeks <michael.meeks@collabora.com>
2021-08-03 13:10:57 +0100

    Pause and resume drawing while clearing comment & redline sections.
    Change-Id: Iecf59d48122c15e8baa4de0fbe2b486168b4de41
    Signed-off-by: Michael Meeks <michael.meeks@collabora.com>

--- loleaflet/src/layer/tile/CanvasSectionContainer.ts
+++ loleaflet/src/layer/tile/CanvasSectionContainer.ts
@@ -1824,8 +1824,12 @@ class CanvasSectionContainer {
 		}
 
 		if (found) {
-			this.updateBoundSectionLists();
-			this.reNewAllSections();
+			if (this.drawingPaused)
+			    this.sectionsDirty = true;
+			else {
+			    this.updateBoundSectionLists();
+			    this.reNewAllSections();
+			}
 			return true;
 		}
 		else {
