e39a220ffdf02f9ed10be514d1d14f1960345faf
Dennis Francis <dennis.francis@collabora.com>
2021-08-12 15:57:00 +0530

    calc: use document bg color from core to paint canvas bg
    Signed-off-by: Dennis Francis <dennis.francis@collabora.com>
    Change-Id: Ief814741df4818000cef7499a8b1c9c1ce1f4371

--- kit/ChildSession.cpp
+++ kit/ChildSession.cpp
@@ -2855,6 +2855,8 @@ void ChildSession::loKitCallback(const int type, const std::string& payload)
     case LOK_CALLBACK_INVALIDATE_SHEET_GEOMETRY:
         sendTextFrame("invalidatesheetgeometry: " + payload);
         break;
+    case LOK_CALLBACK_DOCUMENT_BACKGROUND_COLOR:
+        sendTextFrame("documentbackgroundcolor: " + payload);
 
 #if !ENABLE_DEBUG
     // we want a compilation-time failure in the debug builds; but ERR in the
