400f7ec9168fa320c4b37d16e48cfee74c7f9083
Szymon Kłos <szymon.klos@collabora.com>
2021-09-01 13:32:08 +0200

    comments: use vex insertion dialog when collapsed and fix cypress
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I134f3de1d16621e849fd414c634da717d3f8dc17

--- cypress_test/integration_tests/desktop/writer/annotation_spec.js
+++ cypress_test/integration_tests/desktop/writer/annotation_spec.js
@@ -1,13 +1,15 @@
 /* global describe it cy require afterEach beforeEach */
 
 var helper = require('../../common/helper');
-var { insertMultipleComment } = require('../../common/desktop_helper');
+var { insertMultipleComment, hideSidebar } = require('../../common/desktop_helper');
 
 describe('Annotation Tests', function() {
 	var testFileName = 'annotation.odt';
 
 	beforeEach(function() {
+		cy.viewport(1400, 600);
 		helper.beforeAll(testFileName, 'writer');
+		hideSidebar();
 	});
 
 	afterEach(function() {
@@ -29,6 +31,82 @@ describe('Annotation Tests', function() {
 
 		cy.get('#annotation-content-area-1').should('contain','some text0');
 
+		cy.get('#comment-annotation-menu-1').click();
+
+		cy.contains('.context-menu-item','Modify').click();
+
+		cy.get('#annotation-modify-textarea-1').type('some other text, ');
+
+		cy.get('#annotation-save-1').click();
+
+		cy.get('.loleaflet-annotation-content-wrapper').should('exist');
+
+		cy.get('#annotation-content-area-1').should('contain','some other text, some text0');
+	});
+
+	it('Reply',function() {
+		insertMultipleComment('writer');
+
+		cy.get('.loleaflet-annotation-content-wrapper').should('exist');
+
+		cy.get('#annotation-content-area-1').should('contain','some text');
+
+		cy.get('#comment-annotation-menu-1').click();
+
+		cy.contains('.context-menu-item','Reply').click();
+
+		cy.get('#annotation-reply-textarea-1').type('some reply text');
+
+		cy.get('#annotation-reply-1').click();
+
+		cy.get('#annotation-content-area-2').should('contain','some reply text');
+	});
+
+	it('Remove',function() {
+		insertMultipleComment('writer');
+
+		cy.get('.loleaflet-annotation-content-wrapper').should('exist');
+
+		cy.get('.loleaflet-annotation-content > div')
+			.should('contain','some text');
+
+		cy.get('.loleaflet-annotation-menu').click();
+
+		cy.contains('.context-menu-item','Remove')
+			.click();
+
+		cy.get('.loleaflet-annotation-content-wrapper')
+			.should('not.exist');
+	});
+
+});
+
+describe('Collapsed Annotation Tests', function() {
+	var testFileName = 'annotation.odt';
+
+	beforeEach(function() {
+		helper.beforeAll(testFileName, 'writer');
+	});
+
+	afterEach(function() {
+		helper.afterAll(testFileName, this.currentTest.state);
+	});
+
+	it('Insert',function() {
+		insertMultipleComment('writer', 1, true);
+
+		cy.get('.loleaflet-annotation-content-wrapper').should('exist');
+
+		cy.get('#annotation-content-area-1').should('contain','some text0');
+	});
+
+	it('Modify',function() {
+		insertMultipleComment('writer', 1, true);
+
+		cy.get('.loleaflet-annotation-content-wrapper').should('exist');
+
+		cy.get('#annotation-content-area-1').should('contain','some text0');
+
 		cy.get('#comment-container-1 .loleaflet-annotation-collapsed .avatar-img').click();
 
 		cy.get('#mobile-wizard-popup #comment-annotation-menu-1').click();
@@ -45,7 +123,7 @@ describe('Annotation Tests', function() {
 	});
 
 	it('Reply',function() {
-		insertMultipleComment('writer');
+		insertMultipleComment('writer', 1, true);
 
 		cy.get('.loleaflet-annotation-content-wrapper').should('exist');
 
@@ -65,7 +143,7 @@ describe('Annotation Tests', function() {
 	});
 
 	it('Remove',function() {
-		insertMultipleComment('writer');
+		insertMultipleComment('writer', 1, true);
 
 		cy.get('.loleaflet-annotation-content-wrapper').should('exist');
 
