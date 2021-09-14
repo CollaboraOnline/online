029d4195ad87093d581b128338c459641e9eb5bd
Szymon Kłos <szymon.klos@collabora.com>
2021-09-14 17:05:31 +0200

    android fix: use correct instance for Unit classes getter
    On android we had issue with not working release builds
    (worked when optimization flag -O0 was used for Unit.cpp).
    
    Global which stored instance of UnitBase subtype was returned
    after cast to a choosen subtype. Unfortunately in case when
    android didn't work UnitWSD was returned in UnitKit::get() !
    
    Thich patch makes us sure we don't cast to not-correct type.
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I98e91d8fa82a8e81a8f7cee2fe3109b67c4f64f7

--- common/Unit.hpp
+++ common/Unit.hpp
@@ -255,6 +255,9 @@ private:
         return false;
     }
 
+    /// setup global instance for get() method
+    static void rememberGlobalInstance(UnitType type, UnitBase* instance);
+
     void *_dlHandle;
     static char *UnitLibPath;
     bool _setRetValue;
@@ -279,11 +282,7 @@ public:
 
     virtual ~UnitWSD();
 
-    static UnitWSD& get()
-    {
-        assert(Global && Global->_type == UnitType::Wsd);
-        return *static_cast<UnitWSD *>(Global);
-    }
+    static UnitWSD& get();
 
     enum class TestRequest
     {
@@ -402,14 +401,7 @@ class UnitKit : public UnitBase
 public:
     explicit UnitKit(std::string testname = std::string());
     virtual ~UnitKit();
-    static UnitKit& get()
-    {
-        assert(Global);
-#if !MOBILEAPP && !defined(KIT_IN_PROCESS)
-        assert(Global->_type == UnitType::Kit);
-#endif
-        return *static_cast<UnitKit *>(Global);
-    }
+    static UnitKit& get();
 
     // ---------------- ForKit hooks ----------------
 
