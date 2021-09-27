862e3d2ee3fa495cd72daa7cd1f83d1602b97366
Tor Lillqvist <tml@collabora.com>
2021-09-27 15:41:21 +0300

    Make it possible to build the iOS app against core master
    Signed-off-by: Tor Lillqvist <tml@collabora.com>
    Change-Id: I6c680bff803d738c950c57b9da9dbeb097d6a472

--- ios/Mobile/L10n.h
+++ ios/Mobile/L10n.h
@@ -4,8 +4,20 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+#include <config_version.h>
+
 #define _(id, catalog) app_translate(id, catalog)
 
+#if (LIBO_VERSION_MAJOR > 7 && LIBO_VERSION_MAJOR < 21) || (LIBO_VERSION_MAJOR == 7 && LIBO_VERSION_MINOR >= 3)
+
+#define LIBO_INTERNAL_ONLY
+
+#import <rtl/string.hxx>
+#import <unotools/resmgr.hxx>
+
+char *app_translate(TranslateId id, const char *catalog);
+#else
 char *app_translate(const char *id, const char *catalog);
+#endif
 
 // vim:set shiftwidth=4 softtabstop=4 expandtab:
