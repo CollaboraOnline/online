6c4a41d142138f31f833b7b2f6defe209a6d0e7f
Szymon Kłos <szymon.klos@collabora.com>
2021-10-06 13:20:08 +0200

    Turn off desynchronized for non-desktop to avoid flickering
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: Iae659504b37b57f6140a35ecc99fe63479b0b1e2

--- loleaflet/src/layer/tile/CanvasSectionContainer.ts
+++ loleaflet/src/layer/tile/CanvasSectionContainer.ts
@@ -298,8 +298,10 @@ class CanvasSectionContainer {
 	private stoppingEventTypes: Array<string>; // Events those stop the animation.
 
 	constructor (canvasDOMElement: HTMLCanvasElement, disableDrawing?: boolean) {
+		const isDesktop : boolean = (window as any).mode.isDesktop();
 		this.canvas = canvasDOMElement;
-		this.context = canvasDOMElement.getContext('2d', { alpha: false , desynchronized: true });
+		// on mobile desynchronized causes flickering when showing/hiding keyboard
+		this.context = canvasDOMElement.getContext('2d', { alpha: false , desynchronized: isDesktop });
 		this.context.setTransform(1,0,0,1,0,0);
 		document.addEventListener('mousemove', this.onMouseMove.bind(this));
 		this.canvas.onmousedown = this.onMouseDown.bind(this);
