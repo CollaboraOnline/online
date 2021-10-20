ed36fd6e31e8d5f881ef131cc23dfea8e553b94e
Henry Castro <hcastro@collabora.com>
2021-10-20 08:22:05 -0400

    browser: add parameters to IFrameDialog
    It creates a form element and populated with
    parameters to submit according to the method.
    
    Change-Id: Ia72fa09e9cc283a404c8e2e7cca34de7f792e735
    Signed-off-by: Henry Castro <hcastro@collabora.com>

--- browser/src/control/IFrameDialog.js
+++ browser/src/control/IFrameDialog.js
@@ -10,26 +10,55 @@ L.IFrameDialog = L.Class.extend({
 		method: 'get'
 	},
 
-	initialize: function (url, options) {
+	initialize: function (url, params, element, options) {
+		var content, form, iframe;
+
 		L.setOptions(this, options);
 
-		this._container = L.DomUtil.create('div', this.options.prefix + '-wrap', document.body);
+		this._container = L.DomUtil.create('div', this.options.prefix + '-wrap');
 		this._container.style.display = 'none';
-		this._content = L.DomUtil.create('div', this.options.prefix + '-content', this._container);
-		this._iframe = L.DomUtil.create('iframe', this.options.prefix + '-modal', this._content);
+
+		content = L.DomUtil.create('div', this.options.prefix + '-content', this._container);
+		form = L.DomUtil.create('form', '', content);
+
+		this.fillParams(url, params, form);
+
+		iframe = L.DomUtil.create('iframe', this.options.prefix + '-modal', content);
+		iframe.name = form.target;
 
 		if (this.options.id) {
-			this._iframe.id = this.options.id;
+			iframe.id = this.options.id;
 		}
 
-		this._iframe.src = url;
+		if (element) {
+			document.body.insertBefore(this._container, element);
+		} else {
+			document.body.appendChild(this._container);
+		}
+
+		form.submit();
+	},
+
+	fillParams: function (url, params, form) {
+		var input, keys;
+		form.action = url;
+		form.target = this.options.prefix + '-form';
+		form.method = this.options.method;
+
+		for (var item in params) {
+			keys = Object.keys(params[item]);
+			if (keys.length > 0) {
+				input = L.DomUtil.create('input', '', form);
+				input.type = 'hidden';
+				input.name = String(keys[0]);
+				input.value = String(params[item][keys[0]]);
+			}
+		}
 	},
 
 	remove: function () {
 		L.DomUtil.remove(this._container);
-		this._iframe = this._content
-			= this._container
-			= this._urlFrame = null;
+		this._container = null;
 	},
 
 	hasLoaded: function () {
@@ -41,6 +70,6 @@ L.IFrameDialog = L.Class.extend({
 	}
 });
 
-L.iframeDialog = function (url, options) {
-	return new L.IFrameDialog(url, options);
+L.iframeDialog = function (url, params, element, options) {
+	return new L.IFrameDialog(url, params, element, options);
 };
