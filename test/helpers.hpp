0a9098308fc3787580fb1a462a6cf5609513fcab
Miklos Vajna <vmiklos@collabora.com>
2021-11-03 08:25:35 +0100

    test: double the timeout in isDocumentLoaded() for sanitizers
    Signed-off-by: Miklos Vajna <vmiklos@collabora.com>
    Change-Id: I8627f16fb7ad67561312d3053d855bab8008ab75

--- test/helpers.hpp
+++ test/helpers.hpp
@@ -533,6 +533,9 @@ bool isDocumentLoaded(LOOLWebSocket& ws, const std::string& testname, bool isVie
 {
     const std::string prefix = isView ? "status:" : "statusindicatorfinish:";
     std::chrono::seconds timeout(60);
+#if defined(__SANITIZE_ADDRESS__)
+    timeout = std::chrono::seconds(120);
+#endif
     const auto message = getResponseString(ws, prefix, testname, timeout);
     bool success = LOOLProtocol::matchPrefix(prefix, message);
     if (!success)
