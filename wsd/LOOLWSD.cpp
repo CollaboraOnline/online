40702f1d0e380042d3b02707da878bca002bebe3
Pranam Lashkari <lpranam@collabora.com>
2021-11-03 12:38:22 +0530

    quarantine: keep quarantine off by default
    allow path specification for the quarantine directory
    
    Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
    Change-Id: I407288db60fe5fbf0f6a9346175c3d4acfd35add

--- wsd/LOOLWSD.cpp
+++ wsd/LOOLWSD.cpp
@@ -1378,12 +1378,15 @@ void LOOLWSD::initialize(Application& self)
     LOG_DBG("FileServerRoot after config: " << FileServerRoot);
 
     //creating quarantine directory
-    QuarantinePath = FileServerRoot + "quarantine/";
-    Poco::File p(QuarantinePath);
-    p.createDirectories();
-    LOG_INF("Created quarantine directory " + p.path());
+    if(getConfigValue<bool>(conf, "quarantine_files[@enable]", false))
+    {
+        QuarantinePath = getPathFromConfig("quarantine_files.path");
+        Poco::File p(QuarantinePath);
+        p.createDirectories();
+        LOG_INF("Created quarantine directory " + p.path());
 
-    Quarantine::createQuarantineMap();
+        Quarantine::createQuarantineMap();
+    }
 
     WelcomeFilesRoot = getPathFromConfig("welcome.path");
     if (!getConfigValue<bool>(conf, "welcome.enable", true))
