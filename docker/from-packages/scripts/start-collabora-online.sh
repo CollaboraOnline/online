bf5bce766954ff82d74838eb236a60cdc1b44114
Andras Timar <andras.timar@collabora.com>
2021-11-17 22:06:34 +0100

    rename: loolwsd-generate-proof-key -> coolwsd-generate-proof-key
    Signed-off-by: Andras Timar <andras.timar@collabora.com>
    Change-Id: I3aa30ce4e5f6df2068fa2e0788034e83cb0ae26b

--- docker/from-packages/scripts/start-collabora-online.sh
+++ docker/from-packages/scripts/start-collabora-online.sh
@@ -64,7 +64,7 @@ fi
 ) &
 
 # Generate WOPI proof key
-loolwsd-generate-proof-key
+coolwsd-generate-proof-key
 
 # Start loolwsd
 exec /usr/bin/loolwsd --version --o:sys_template_path=/opt/cool/systemplate --o:child_root_path=/opt/cool/child-roots --o:file_server_root_path=/usr/share/loolwsd --o:logging.color=false ${extra_params}
--- wsd/ProofKey.cpp
+++ wsd/ProofKey.cpp
@@ -140,7 +140,7 @@ Proof::Proof()
             std::string msg = e.displayText() +
                 "\nNo proof-key will be present in discovery."
                 "\nIf you need to use WOPI security, generate an RSA key using this command:"
-                "\n    loolwsd-generate-proof-key"
+                "\n    coolwsd-generate-proof-key"
                 "\nor if your config dir is not /etc, you can run ssh-keygen manually:"
                 "\n    ssh-keygen -t rsa -N \"\" -m PEM -f \"" + keyPath + "\""
                 "\nNote: the proof_key file must be readable by the loolwsd process.";
