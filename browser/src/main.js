a04d5cb6317814aed8803796675f388f34d4cf45
Szymon Kłos <szymon.klos@collabora.com>
2021-11-19 16:59:04 +0100

    Fix reconnecting after rename
    Regression from f92e85dacce713ff019d5360c50e74703f9c9688
    Ensure that options are always initialized.
    
    Socket.js code expects that access_token and other params
    are under map.options.docParams not map.options.options.docParams.
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: Ie6d7055c3865e6fce1754bce5272cee7f4973b68

--- browser/src/main.js
+++ browser/src/main.js
@@ -44,7 +44,7 @@ var notWopiButIframe = getParameterByName('NotWOPIButIframe') != '';
 var map = L.map('map', {
 	server: host,
 	doc: docURL,
-	options: { docParams: docParams },
+	docParams: docParams,
 	permission: permission,
 	timestamp: timestamp,
 	documentContainer: 'document-container',
