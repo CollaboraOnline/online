2ac0d01461320e86a1fee79ab2b06b8c003ed669
Pranam Lashkari <lpranam@collabora.com>
2021-11-22 18:45:29 +0530

    added method to get inode number from Stat object
    Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
    Change-Id: I110f5bd0908a1c457af65429e377425e2055290a

--- common/FileUtil.hpp
+++ common/FileUtil.hpp
@@ -151,6 +151,7 @@ namespace FileUtil
         bool isFile() const { return S_ISREG(_sb.st_mode); }
         bool isLink() const { return S_ISLNK(_sb.st_mode); }
         std::size_t hardLinkCount() const { return _sb.st_nlink; }
+        ino_t inodeNumber() const { return _sb.st_ino; }
 
         /// Returns the filesize in bytes.
         std::size_t size() const { return _sb.st_size; }
