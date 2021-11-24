c42ce69ced01dfb7dcd4e9b6a9bd18a6c00e0e28
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2021-11-24 09:08:41 -0500

    wsd: cache misses are not errors
    Change-Id: I2e33825ad4f301cc0746a3cc3a226a2161878778
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- wsd/TileCache.cpp
+++ wsd/TileCache.cpp
@@ -249,7 +249,9 @@ bool TileCache::getTextStream(StreamType type, const std::string& fileName, std:
     Tile textStream = lookupCachedStream(type, fileName);
     if (!textStream)
     {
-        LOG_ERR("Could not open " << fileName);
+        // This is not an error because the first time
+        // we lookup a file, it won't be in the cache.
+        LOG_INF("Could not open " << fileName);
         return false;
     }
 
