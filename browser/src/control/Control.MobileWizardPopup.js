4764937ad44a5cca99a7d5b962e661f491d18cb1
Szymon Kłos <szymon.klos@collabora.com>
2021-11-25 13:34:51 +0100

    fix: undefined usage in _hideWizard
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I085c5e4882a25a56b11346276e61aa5196caf4f7

--- browser/src/control/Control.MobileWizardPopup.js
+++ browser/src/control/Control.MobileWizardPopup.js
@@ -52,8 +52,10 @@ L.Control.MobileWizardPopup = L.Control.extend({
 	_hideWizard: function () {
 		this.removeContainer();
 
-		app.sectionContainer.getSectionWithName(L.CSections.CommentList.name).removeHighlighters();
-		app.sectionContainer.getSectionWithName(L.CSections.CommentList.name).layout();
+		if (app.sectionContainer) {
+			app.sectionContainer.getSectionWithName(L.CSections.CommentList.name).removeHighlighters();
+			app.sectionContainer.getSectionWithName(L.CSections.CommentList.name).layout();
+		}
 	},
 
 	_setTitle: function(title) {
