a5de33496ccd27ca6d11a245c69f70fd28457092
Luboš Luňák <l.lunak@collabora.com>
2021-11-29 19:02:53 +0100

    make stringifyHexLine() simply work on std::string
    Trying to construct a string using sprintf() and std::stringstream
    is unnecessarily complicated for something as simple as this,
    and it shows up in my profiling. This commit makes the hex values
    change to uppercase because that's what hexFromByte() returns,
    but I don't think it matters.
    
    Signed-off-by: Luboš Luňák <l.lunak@collabora.com>
    Change-Id: I31c7b8e7cf4c2a495eca0bf03ae4cab53b26a04b

---
+++ test/UtilTests.cpp
@@ -0,0 +1,39 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <config.h>
+
+#include <test/lokassert.hpp>
+
+#include <Util.hpp>
+
+/// Util unit-tests.
+class UtilTests : public CPPUNIT_NS::TestFixture
+{
+    CPPUNIT_TEST_SUITE(UtilTests);
+
+    CPPUNIT_TEST(testStringifyHexLine);
+
+    CPPUNIT_TEST_SUITE_END();
+
+    void testStringifyHexLine();
+};
+
+void UtilTests::testStringifyHexLine()
+{
+    std::string test("hello here\ntest");
+    std::string result1("68 65 6C 6C 6F 20 68 65  72 65 0A 74 65 73 74"
+                        "                                                       "
+                        "| hello here.test                 ");
+    std::string result2("68 65 72 65 0A 74  | here.t");
+    LOK_ASSERT_EQUAL(result1, Util::stringifyHexLine(test, 0));
+    LOK_ASSERT_EQUAL(result2, Util::stringifyHexLine(test, 6, 6));
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(UtilTests);
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
