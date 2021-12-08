7fd639365b2beed3ccc12788184349a9ec344e0f
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2021-12-08 20:46:49 +0100

    support for a custom path to NSS certdb files for sign/encrypt
    The path of the NSS certdb is defined in the coolwsd.xml config
    file and if the config file is set and contains the certdb files,
    then the db files are copied to the jail into /tmp/certdb folder.
    Also the /tmp/certdb path is set to LO_CERTIFICATE_DATABASE_PATH
    env. var, which is then used as the default certdb in LibreOffice
    when LOKit starts up.
    
    Signed-off-by: Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
    Change-Id: I72e8f28f27a0306fef9319bc6212cd99cb3f8212

--- wsd/LOOLWSD.cpp
+++ wsd/LOOLWSD.cpp
@@ -1009,6 +1009,7 @@ void LOOLWSD::initialize(Application& self)
             { "security.seccomp", "true" },
             { "security.jwt_expiry_secs", "1800" },
             { "security.enable_metrics_unauthenticated", "false" },
+            { "certificates.database_path", "" },
             { "server_name", "" },
             { "ssl.ca_file_path", LOOLWSD_CONFIGDIR "/ca-chain.cert.pem" },
             { "ssl.cert_file_path", LOOLWSD_CONFIGDIR "/cert.pem" },
