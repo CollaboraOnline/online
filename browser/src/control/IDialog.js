a8aa328ea4b2dcf4f53f5e52eb189df01f4c601a
Henry Castro <hcastro@collabora.com>
2021-12-15 15:53:20 -0400

    browser: create dialog buttons
    Change-Id: I1f2fbfa000878c9c353c1afe21f8e4425af4ae2d
    Signed-off-by: Henry Castro <hcastro@collabora.com>

--- browser/src/control/IDialog.js
+++ browser/src/control/IDialog.js
@@ -13,6 +13,7 @@ L.IDialog = L.Class.extend({
 			container = L.DomUtil.create('div', options.prefix + '-wrap');
 			content = L.DomUtil.create('div', options.prefix + '-content', container);
 			content.innerHTML = L.IDialog.innerHtml(options.message);
+			content.appendChild(L.IDialog.createButtons(options.prefix, options.buttons));
 			document.body.appendChild(container);
 			L.IDialog.container = container;
 		},
@@ -24,6 +25,18 @@ L.IDialog = L.Class.extend({
 			var elem = L.DomUtil.create('div', '');
 			elem.appendChild(document.createTextNode(string));
 			return elem.innerHTML;
+		},
+
+		createButtons: function (prefix, buttons) {
+			var container, button, item, data;
+
+			container = L.DomUtil.create('div', prefix + '-footer');
+			for (item in buttons) {
+				data = buttons[item];
+				button = L.DomUtil.create(data.type, data.className, container);
+				button.textContent = data.text;
+			}
+			return container;
 		},
 
 		isVisible: function () {
