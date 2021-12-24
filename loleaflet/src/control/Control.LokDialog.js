6d385877a7be30c59afea7d8cb78d14e829eb1ef
Henry Castro <hcastro@collabora.com>
2021-12-24 15:31:23 -0400

    browser: add 'dropdown' dialog type
    Adjust the drop-down dialog to the #map element
    to show the correct position sent by the server.
    
    Change-Id: I74a6d7402438241aae77a18699c3297d64b34098
    Signed-off-by: Henry Castro <hcastro@collabora.com>

--- loleaflet/src/control/Control.LokDialog.js
+++ loleaflet/src/control/Control.LokDialog.js
@@ -289,7 +289,8 @@ L.Control.LokDialog = L.Control.extend({
 		    e.winType !== 'calc-input-win' &&
 		    e.winType !== 'child' &&
 		    e.winType !== 'deck' &&
-		    e.winType !== 'tooltip') {
+		    e.winType !== 'tooltip' &&
+		    e.winType !== 'dropdown') {
 			return;
 		}
 
@@ -317,9 +318,9 @@ L.Control.LokDialog = L.Control.extend({
 		}
 
 		if (e.action === 'created') {
-			if (e.winType === 'dialog' && !window.mode.isMobile()) {
+			if ((e.winType === 'dialog' || e.winType === 'dropdown') && !window.mode.isMobile()) {
 				// When left/top are invalid, the dialog shows in the center.
-				this._launchDialog(e.id, left, top, width, height, e.title);
+				this._launchDialog(e.id, left, top, width, height, e.title, e.winType);
 			} else if (e.winType === 'calc-input-win') {
 				lines = parseInt(e.lines);
 				this._launchCalcInputBar(e.id, left, top, width, height, lines);
@@ -788,7 +789,7 @@ L.Control.LokDialog = L.Control.extend({
 		return changed;
 	},
 
-	_launchDialog: function(id, leftTwips, topTwips, width, height, title) {
+	_launchDialog: function(id, leftTwips, topTwips, width, height, title, type) {
 		if (window.ThisIsTheiOSApp) {
 			if (w2ui['editbar'])
 				w2ui['editbar'].disable('closemobile');
@@ -839,7 +840,11 @@ L.Control.LokDialog = L.Control.extend({
 			var top = pixels.y + panePos.y - origin.y;
 
 			if (left >= 0 && top >= 0) {
-				$(dialogContainer).dialog('option', 'position', { my: 'left top', at: 'left+' + left + ' top+' + top, of: '#document-container' });
+				$(dialogContainer).dialog('option', 'position',
+							  { my: 'left top',
+							    at: 'left+' + left + ' top+' + top,
+							    of: type === 'dropdown' ? '#map' :
+							    '#document-container' });
 			}
 		}
 
