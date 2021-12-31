ab019970357064aff3eb765f799e71ad2cab3a6a
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2021-12-31 13:36:35 -0500

    wsd: do not create client sessions on docs marked to destroy
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>
    (cherry picked from commit 3eb310d7609e98572959fece196d9bc00147c2ff)
    
    Change-Id: Ic1faa042975816bd15ccc4f5667eeccfb0cf73d7

--- wsd/COOLWSD.cpp
+++ wsd/COOLWSD.cpp
@@ -6,6 +6,7 @@
  */
 
 #include <config.h>
+#include <stdexcept>
 #include "COOLWSD.hpp"
 #include "ProofKey.hpp"
 
@@ -3710,6 +3711,7 @@ private:
                 else
                 {
                     LOG_WRN("Failed to create Client Session with id [" << _id << "] on docKey [" << docKey << "].");
+                    throw std::runtime_error("Cannot create client session for doc " + docKey);
                 }
             }
             else
