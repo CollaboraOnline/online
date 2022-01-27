6d3d2935d7dfb738d1e41991d380c71756303607
Pedro Pinto Silva <pedro.silva@collabora.com>
2022-01-27 10:33:08 +0100

    Rename: outdated loleflet strings
    Signed-off-by: Pedro Pinto Silva <pedro.silva@collabora.com>
    Change-Id: Ibfb5de3dc5d4c5a57f4d50bf0a9bbc45df2ecadb

--- wsd/FileServer.hpp
+++ wsd/FileServer.hpp
@@ -29,7 +29,7 @@ class FileServerRequestHandler
                                     const RequestDetails &requestDetails,
                                     const std::shared_ptr<StreamSocket>& socket);
 
-    /// Construct a JSON to be accepted by the loleflet.html from a list like
+    /// Construct a JSON to be accepted by the cool.html from a list like
     /// UIMode=classic;TextRuler=true;PresentationStatusbar=false
     /// that is passed as "ui_defaults" hidden input during the iframe setup.
     /// Also returns the UIMode from uiDefaults in uiMode output param
