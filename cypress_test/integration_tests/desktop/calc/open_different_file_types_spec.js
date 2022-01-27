3efcffb19242f999ef47beb1339ba3c08559acd7
Rash419 <rashesh.padia@collabora.com>
2022-01-27 18:20:14 +0530

    cypress: added open different LO files test for impress, writer and calc impress: ppt, pptm, pot, potx, potm, ppsx, fodp calc: xls, xla, xltx, xltm, xlsb, xlsm, fods writer: doc, dot, dotx, dotm, docm, fodt
    Signed-off-by: Rash419 <rashesh.padia@collabora.com>
    Change-Id: Ib695e3f235acafd203c7041aecce319cd6efb285

---
+++ cypress_test/integration_tests/desktop/calc/open_different_file_types_spec.js
@@ -0,0 +1,141 @@
+/* global describe it cy require Cypress afterEach */
+var helper = require('../../common/helper');
+var calcHelper = require('../../common/calc_helper');
+const { insertImage } = require('../../common/desktop_helper');
+
+describe('Open different file types', function () {
+
+	var testFileName = '';
+
+	function before(filename) {
+		var origTestFileName = filename;
+
+		testFileName = helper.beforeAll(origTestFileName, 'calc');
+	}
+
+	function openReadOnlyFile(filename) {
+		testFileName = helper.loadTestDocNoIntegration(filename, 'calc', false, false, false);
+
+		//check doc is loaded
+		cy.get('.leaflet-canvas-container canvas', {timeout : Cypress.config('defaultCommandTimeout') * 2.0});
+
+		helper.canvasShouldNotBeFullWhite('.leaflet-canvas-container canvas');
+
+		cy.get('#PermissionMode').should('be.visible')
+			.should('have.text', ' Read-only ');
+	}
+
+	afterEach(function () {
+		helper.afterAll(testFileName, this.currentTest.state);
+	});
+
+	function assertData() {
+		//select all the content of doc
+		calcHelper.selectEntireSheet();
+
+		helper.waitUntilIdle('#copy-paste-container');
+
+		var expectedData = [
+			'0', 'First Name', 'Last Name', 'Gender', 'Country', 'Age', 'Date', 'Id',
+			'1', 'Dulce', 'Abril', 'Female', 'United States', '32', '15/10/2017', '1562',
+			'2', 'Mara', 'Hashimoto', 'Female', 'Great Britain', '25', '16/08/2016', '1582',
+			'3', 'Philip', 'Gent', 'Male', 'France', '36', '21/05/2015', '2587',
+			'4', 'Kathleen', 'Hanner', 'Female', 'United States', '25', '15/10/2017', '3549',
+		];
+
+		calcHelper.assertDataClipboardTable(expectedData);
+	}
+
+	it('Open xls file', function () {
+		before('testfile.xls');
+
+		assertData();
+
+		insertImage();
+	});
+
+	it('Open xlsx file', function () {
+		before('testfile.xlsx');
+
+		assertData();
+	});
+
+	//we are not using before because it loads the document and directly asserts if document is loaded but in
+	//case of csv file 1st when you try to load the doc it opens up jsdialog to import csv which requires user
+	//input and after click ok the doc starts to load
+	it('Open csv file', function() {
+		//to fit csv jsdialog in window
+		cy.viewport(1280, 960);
+
+		testFileName = helper.loadTestDocNoIntegration('testfile.csv', 'calc', false, false, false);
+
+		cy.get('form.jsdialog-container.lokdialog_container')
+			.should('exist');
+
+		cy.contains('.ui-pushbutton', 'OK')
+			.click();
+
+		//check doc is loaded
+		cy.get('.leaflet-canvas-container canvas', {timeout : Cypress.config('defaultCommandTimeout') * 2.0});
+
+		helper.canvasShouldNotBeFullWhite('.leaflet-canvas-container canvas');
+
+		cy.get('#mobile-edit-button')
+			.should('be.visible')
+			.click();
+
+		cy.get('.vex-content button').click();
+
+		assertData();
+	});
+
+	it('Open xlsb file' ,function() {
+		openReadOnlyFile('testfile.xlsb');
+
+		cy.get('#mobile-edit-button').should('be.visible')
+			.click();
+
+		cy.get('.vex-overlay').should('be.visible');
+
+		cy.get('.vex-dialog-message')
+			.should('have.text', 'This document may contain formatting or content that cannot be saved in the current file format.');
+
+		cy.get('.vex-dialog-buttons button')
+			.should('have.text', 'Continue read only')
+			.click();
+
+		cy.get('#PermissionMode').should('be.visible')
+			.should('have.text', ' Read-only ');
+
+		cy.get('.vex-overlay').should('be.visible');
+	});
+
+	it('Open xlsm file' ,function() {
+		before('testfile.xlsm');
+
+		assertData();
+
+		insertImage();
+	});
+
+	it('Open xltm file' ,function() {
+		openReadOnlyFile('testfile.xltm');
+
+		cy.get('#mobile-edit-button').should('not.be.visible');
+	});
+
+	it('Open xltx file' ,function() {
+		openReadOnlyFile('testfile.xltm');
+
+		cy.get('#mobile-edit-button').should('not.be.visible');
+	});
+
+	it('Open fods file', function() {
+		before('testfile.fods');
+
+		//select all the content of doc
+		assertData();
+
+		insertImage();
+	});
+});
---
+++ cypress_test/integration_tests/desktop/impress/open_different_file_types_spec.js
@@ -0,0 +1,111 @@
+/* global describe it cy require expect afterEach */
+var helper = require('../../common/helper');
+const { selectZoomLevel } = require('../../common/desktop_helper');
+// const { selectTextShapeInTheCenter } = require('../../common/impress_helper');
+
+describe('Open different file types', function() {
+
+	var testFileName = '';
+
+	function before(filename) {
+		var origTestFileName = filename;
+
+		testFileName = helper.beforeAll(origTestFileName, 'impress');
+
+		selectZoomLevel('50');
+
+		cy.get('#toolbar-up .w2ui-scroll-right').click();
+
+		cy.get('#tb_editbar_item_modifypage').click();
+	}
+
+	afterEach(function() {
+		helper.afterAll(testFileName, this.currentTest.state);
+	});
+
+	function assertData() {
+		//select all the content of doc
+		helper.typeIntoDocument('{ctrl}{a}');
+
+		//assert image and size
+		cy.get('.leaflet-overlay-pane g.Graphic image').should('exist')
+		    .then($ele => {
+		        const width = parseInt($ele.attr('width'));
+		        const height = parseInt($ele.attr('height'));
+		        expect(width).to.be.closeTo(18969, 10);
+		        expect(height).to.be.closeTo(7397, 10);
+		    });
+
+		var selector = '.leaflet-pane.leaflet-overlay-pane g.Page';
+
+		cy.get(selector + ' g')
+			.should('have.class', 'com.sun.star.drawing.TableShape');
+
+		//assert the number of table cells
+		cy.get(selector + ' path[fill^="rgb"]')
+			.should(function(cells) {
+				expect(cells).to.have.lengthOf(6);
+			});
+
+		//assert text properties
+		cy.get(selector + ' g')
+			.should('have.class', 'com.sun.star.drawing.CustomShape');
+
+		cy.get(selector + ' .TextParagraph')
+			.should('have.attr', 'font-family', 'Calibri, sans-serif');
+
+		cy.get(selector + ' .TextParagraph')
+			.should('have.attr', 'font-size', '1552px');
+
+		cy.get(selector + ' .TextParagraph .TextPosition tspan')
+			.should('have.text', 'LibreOffice');
+	}
+
+	it('Open pptx file', function() {
+		before('testfile.pptx');
+
+		assertData();
+	});
+
+	it('Open ppt file', function() {
+		before('testfile.ppt');
+
+		assertData();
+	});
+
+	it('Open pptm file', function() {
+		before('testfile.pptm');
+
+		assertData();
+	});
+
+	it('Open pot file', function() {
+		before('testfile.pot');
+
+		assertData();
+	});
+
+	it('Open potx file', function() {
+		before('testfile.potx');
+
+		assertData();
+	});
+
+	it('Open potm file', function() {
+		before('testfile.potm');
+
+		assertData();
+	});
+
+	it('Open fodp file', function() {
+		before('testfile.fodp');
+
+		assertData();
+	});
+
+	it('Open ppsx file', function() {
+		before('testfile.ppsx');
+
+		assertData();
+	});
+});
