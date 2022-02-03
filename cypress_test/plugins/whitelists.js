fb0ff45c1ca8fb703f831340175ee9bacf0cccd5
Rash419 <rashesh.padia@collabora.com>
2022-02-03 14:27:24 +0530

    cypress: temporarily disable few NB tests
    Signed-off-by: Rash419 <rashesh.padia@collabora.com>
    Change-Id: Iae5ea1f891a149e49aea0cdea3fcd25317877c06

--- cypress_test/plugins/whitelists.js
+++ cypress_test/plugins/whitelists.js
@@ -1,8 +1,8 @@
 var notebookbarOnlyList = [
-	'desktop/writer/top_toolbar_spec.js',
-	'desktop/calc/cell_appearance_spec.js',
-	'desktop/writer/table_operation_spec.js',
-	'desktop/impress/table_operation_spec.js',
+	// 'desktop/writer/top_toolbar_spec.js',
+	// 'desktop/calc/cell_appearance_spec.js',
+	// 'desktop/writer/table_operation_spec.js',
+	// 'desktop/impress/table_operation_spec.js',
 	'desktop/writer/annotation_spec.js',
 	'desktop/calc/annotation_spec.js',
 	'desktop/impress/annotation_spec.js',
