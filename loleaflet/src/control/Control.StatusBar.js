dde99c8856c87bf005f61a69e21f1746dc3e477f
Pranam Lashkari <lpranam@collabora.com>
2022-02-18 16:23:27 +0530

    draw: added more details in the draw status bar
    Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
    Change-Id: Iff349ec26139ce4f561ce2e38a2492bda1e56780

--- loleaflet/src/control/Control.StatusBar.js
+++ loleaflet/src/control/Control.StatusBar.js
@@ -381,6 +381,7 @@ L.Control.StatusBar = L.Control.extend({
 			break;
 
 		case 'presentation':
+		case 'drawing':
 			if (!window.mode.isMobile()) {
 				statusbar.insert('left', [
 					{type: 'break', id: 'break1'},
@@ -399,11 +400,6 @@ L.Control.StatusBar = L.Control.extend({
 					}
 				]);
 			}
-
-		// FALLTHROUGH intended
-		case 'drawing':
-			if (statusbar)
-				statusbar.show('prev', 'next');
 			break;
 		}
 
