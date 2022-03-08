f0207cc05c5fd358105e790210850fb444d38f03
Rash419 <rashesh.padia@collabora.com>
2022-03-08 16:21:42 +0530

    cypress:fix: coordinate based test failing writer/shape_properties_spec.js impress/scrolling_spec.js
    Signed-off-by: Rash419 <rashesh.padia@collabora.com>
    Change-Id: Ib576e9f4d90ec53dc21c2955d61e8ee85bd3e2ca

--- cypress_test/integration_tests/mobile/writer/shape_properties_spec.js
+++ cypress_test/integration_tests/mobile/writer/shape_properties_spec.js
@@ -4,7 +4,7 @@ var helper = require('../../common/helper');
 var mobileHelper = require('../../common/mobile_helper');
 
 describe('Change shape properties via mobile wizard.', function() {
-	const defaultStartPoint = [1953, 4717];
+	const defaultStartPoint = [1953, 4796];
 	const defaultBase = 5992;
 	const defaultAltitude = 5992;
 	const unitScale = 2540.37;
