defef6a171dc87e70a496a2792d33cd6bae3b020
Pranam Lashkari <lpranam@collabora.com>
2022-03-20 16:03:56 +0530

    util: removed duplicate string trimming function
    Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
    Change-Id: Ia150975467d1df8a9d05c8543bcb5d7369d29099

--- common/Util.hpp
+++ common/Util.hpp
@@ -522,20 +522,6 @@ namespace Util
         return trimmed(std::string(s));
     }
 
-    // Trim all type of whitespace from left and right
-    inline std::string trim_whitespace(std::string s)
-    {
-        auto last =
-            std::find_if(s.rbegin(), s.rend(), [](char ch) { return !std::isspace(ch); });
-        s.erase(last.base(), s.end()); //trim from right
-
-        auto first =
-            std::find_if(s.begin(), s.end(), [](char ch) { return !std::isspace(ch); });
-        s.erase(s.begin(), first); //trim from left
-
-        return s;
-    }
-
     /// Return true iff s starts with t.
     inline bool startsWith(const std::string& s, const std::string& t)
     {
