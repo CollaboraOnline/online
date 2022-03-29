2a7207a8fcc8776e9c1088ea7beec3ec03c2ce85
Pranam Lashkari <lpranam@collabora.com>
2022-03-29 19:37:14 +0530

    l10n: if English variant is not specified then choose US as default
    Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
    Change-Id: Iba4f96c75d69e52044f6460c4e26d0ab1d187d3c

--- common/Session.cpp
+++ common/Session.cpp
@@ -157,7 +157,10 @@ void Session::parseDocOptions(const StringVector& tokens, int& part, std::string
         }
         else if (name == "lang")
         {
-            _lang = value;
+            if (value == "en")
+                _lang = "en-US";
+            else
+                _lang = value;
             ++offset;
         }
         else if (name == "watermarkText")
