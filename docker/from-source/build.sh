446ab463ed59626ebc279a52a2530e02cf876627
Rash419 <rashesh.padia@collabora.com>
2022-03-31 21:32:30 +0530

    docker: added start-collabora-online.pl in docker files
    Signed-off-by: Rash419 <rashesh.padia@collabora.com>
    Change-Id: I224c9dc017257403c7c92ba4be8c3d76e77f5270

--- docker/from-source/build.sh
+++ docker/from-source/build.sh
@@ -131,6 +131,7 @@ cp -a core/instdir "$INSTDIR"/opt/lokit
 if [ -z "$NO_DOCKER_IMAGE" ]; then
   cd "$SRCDIR"
   cp ../from-packages/scripts/start-collabora-online.sh .
+  cp ../from-packages/scripts/start-collabora-online.pl .
   docker build --no-cache -t $DOCKER_HUB_REPO:$DOCKER_HUB_TAG -f $HOST_OS . || exit 1
 else
   echo "Skipping docker image build"
