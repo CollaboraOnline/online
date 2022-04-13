2209f0d6f5b431c1091a3db5618310f33ed2af89
Henry Castro <hcastro@collabora.com>
2022-04-13 14:53:49 -0400

    wsd: add parameter --signal
    To signal SIGUSR2 to parent to notify
    that the server is ready to accept connections,
    and begin the tests.
    
    Change-Id: Ie936217243aa7a2e836f3d45cb508ede13fae8c5
    Signed-off-by: Henry Castro <hcastro@collabora.com>
    Signed-off-by: Gökay Şatır <gokaysatir@collabora.com>

--- wsd/COOLWSD.cpp
+++ wsd/COOLWSD.cpp
@@ -850,6 +850,7 @@ bool COOLWSD::NoCapsForKit = false;
 bool COOLWSD::NoSeccomp = false;
 bool COOLWSD::AdminEnabled = true;
 bool COOLWSD::UnattendedRun = false;
+bool COOLWSD::SignalParent = false;
 #if ENABLE_DEBUG
 bool COOLWSD::SingleKit = false;
 #endif
@@ -2543,6 +2544,10 @@ void COOLWSD::defineOptions(OptionSet& optionSet)
                         .required(false)
                         .repeatable(false));
 
+    optionSet.addOption(Option("signal", "", "Send signal SIGUSR2 to parent process when server is ready to accept connections")
+                        .required(false)
+                        .repeatable(false));
+
 #if ENABLE_DEBUG
     optionSet.addOption(Option("unitlib", "", "Unit testing library path.")
                         .required(false)
@@ -2603,6 +2608,9 @@ void COOLWSD::handleOption(const std::string& optionName,
         ConfigDir = value;
     else if (optionName == "lo-template-path")
         LoTemplate = value;
+    else if (optionName == "signal")
+        SignalParent = true;
+
 #if ENABLE_DEBUG
     else if (optionName == "unitlib")
         UnitTestLibrary = value;
@@ -5026,6 +5034,10 @@ int COOLWSD::innerMain()
 
 #if !MOBILEAPP
     std::cerr << "Ready to accept connections on port " << ClientPortNumber <<  ".\n" << std::endl;
+    if (SignalParent)
+    {
+        kill(getppid(), SIGUSR2);
+    }
 #endif
 
 #if !MOBILEAPP
