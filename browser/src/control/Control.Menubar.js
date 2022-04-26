9d91c01bb75a9386a248b7a95ea46ec8eb3364ae
Szymon Kłos <szymon.klos@collabora.com>
2022-04-26 13:10:26 +0200

    notebookbar: fix ui mode switch
    previously without data in localstorage it started with
    compact mode always
    
    - reduce calls to local storage
    - unify local storage entry name (no doctype)
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I2bf4776a6296d2c35ac974b3d108bce5e77a7614

--- browser/src/control/Control.Menubar.js
+++ browser/src/control/Control.Menubar.js
@@ -1540,7 +1540,7 @@ L.Control.Menubar = L.Control.extend({
 						}
 
 					} else if (id === 'toggleuimode') {
-						if (self._map.uiManager.useNotebookbarMode()) {
+						if (self._map.uiManager.shouldUseNotebookbarMode()) {
 							$(aItem).text(_('Use Compact view'));
 						} else {
 							$(aItem).text(_('Use Tabbed view'));
@@ -1684,7 +1684,7 @@ L.Control.Menubar = L.Control.extend({
 		} else if (id === 'showruler') {
 			this._map.uiManager.toggleRuler();
 		} else if (id === 'toggleuimode') {
-			if (this._map.uiManager.useNotebookbarMode()) {
+			if (this._map.uiManager.shouldUseNotebookbarMode()) {
 				this._map.uiManager.onChangeUIMode({mode: 'classic', force: true});
 			} else {
 				this._map.uiManager.onChangeUIMode({mode: 'notebookbar', force: true});
--- browser/src/control/Control.UIManager.js
+++ browser/src/control/Control.UIManager.js
@@ -41,16 +41,17 @@ L.Control.UIManager = L.Control.extend({
 	// UI initialization
 
 	getCurrentMode: function() {
-		return this.useNotebookbarMode() ? 'notebookbar' : 'classic';
+		return this.shouldUseNotebookbarMode() ? 'notebookbar' : 'classic';
 	},
 
-	useNotebookbarMode: function() {
-		var forceCompact = this.getSavedStateOrDefault('CompactMode');
-		return window.userInterfaceMode === 'notebookbar' && !forceCompact;
+	shouldUseNotebookbarMode: function() {
+		var forceCompact = this.getSavedStateOrDefault('CompactMode', null);
+		return (window.userInterfaceMode === 'notebookbar' && forceCompact === null)
+			|| forceCompact === false;
 	},
 
 	initializeBasicUI: function() {
-		var enableNotebookbar = this.useNotebookbarMode();
+		var enableNotebookbar = this.shouldUseNotebookbarMode();
 		var that = this;
 
 		if (window.mode.isMobile() || !enableNotebookbar) {
@@ -144,9 +145,10 @@ L.Control.UIManager = L.Control.extend({
 
 	initializeSpecializedUI: function(docType) {
 		var isDesktop = window.mode.isDesktop();
-		var enableNotebookbar = this.useNotebookbarMode();
+		var currentMode = this.getCurrentMode();
+		var enableNotebookbar = currentMode === 'notebookbar';
 
-		document.body.setAttribute('data-userInterfaceMode', this.getCurrentMode());
+		document.body.setAttribute('data-userInterfaceMode', currentMode);
 
 		if (window.mode.isMobile()) {
 			$('#mobile-edit-button').show();
@@ -251,8 +253,6 @@ L.Control.UIManager = L.Control.extend({
 		this.map._docLayer._requestNewTiles();
 
 		this.map.topToolbar.updateControlsState();
-
-		this.setSavedState('CompactMode', true);
 	},
 
 	createNotebookbarControl: function(docType) {
@@ -282,8 +282,6 @@ L.Control.UIManager = L.Control.extend({
 		this.map.sendInitUNOCommands();
 		this.map._docLayer._resetClientVisArea();
 		this.map._docLayer._requestNewTiles();
-
-		this.setSavedState('CompactMode', false);
 	},
 
 	removeNotebookbarUI: function() {
@@ -298,7 +296,9 @@ L.Control.UIManager = L.Control.extend({
 		if (window.mode.isMobile())
 			return;
 
-		if (uiMode.mode === this.getCurrentMode() && !uiMode.force)
+		var currentMode = this.getCurrentMode();
+
+		if (uiMode.mode === currentMode && !uiMode.force)
 			return;
 
 		if (uiMode.mode !== 'classic' && uiMode.mode !== 'notebookbar')
@@ -308,7 +308,7 @@ L.Control.UIManager = L.Control.extend({
 
 		this.map.fire('postMessage', {msgId: 'Action_ChangeUIMode_Resp', args: {Mode: uiMode}});
 
-		switch (this.getCurrentMode()) {
+		switch (currentMode) {
 		case 'classic':
 			this.removeClassicUI();
 			break;
@@ -330,6 +330,7 @@ L.Control.UIManager = L.Control.extend({
 			break;
 		}
 
+		this.setSavedState('CompactMode', uiMode.mode === 'classic');
 		this.initializeSidebar();
 	},
 
@@ -572,7 +573,8 @@ L.Control.UIManager = L.Control.extend({
 			}
 		}
 
-		var enableNotebookbar = this.useNotebookbarMode();
+		var enableNotebookbar = this.shouldUseNotebookbarMode();
+		console.error(enableNotebookbar);
 		if (enableNotebookbar && !window.mode.isMobile()) {
 			if (e.perm === 'edit') {
 				if (this.map.menubar) {
@@ -703,13 +705,15 @@ L.Control.UIManager = L.Control.extend({
 	},
 
 	setSavedState: function(name, state) {
+		var docType = (name === 'CompactMode') ? null : this.map.getDocType();
 		if (window.isLocalStorageAllowed)
-			localStorage.setItem('UIDefaults_' + this.map.getDocType() + '_' + name, state);
+			localStorage.setItem('UIDefaults_' + docType + '_' + name, state);
 	},
 
-	getSavedStateOrDefault: function(name) {
-		var retval = true;
-		var docType = this.map.getDocType();
+	getSavedStateOrDefault: function(name, forcedDefault) {
+		var retval = forcedDefault !== undefined ? forcedDefault : true;
+		// we request CompactMode very early, no info about doctype so unify all the calls
+		var docType = (name === 'CompactMode') ? null : this.map.getDocType();
 		var state = null;
 		if (window.isLocalStorageAllowed)
 			state = localStorage.getItem('UIDefaults_' + docType + '_' + name);
@@ -723,9 +727,12 @@ L.Control.UIManager = L.Control.extend({
 			if (window.uiDefaults && window.uiDefaults[docType])
 				retval = window.uiDefaults[docType][name];
 
-			if (retval === undefined || retval === null)
-				return true;
-			else
+			if (retval === undefined || retval === null) {
+				if (forcedDefault !== undefined)
+					return forcedDefault;
+				else
+					return true;
+			} else
 				return retval;
 		}
 	},
