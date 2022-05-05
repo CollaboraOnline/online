ea9421c33069f566ef20d2daa7d317ba7a6c9587
Henry Castro <hcastro@collabora.com>
2022-05-05 15:18:10 -0400

    infobar: show a snackbar message
    When data is received from the server.
    
    Change-Id: I53b888acb7624d1ea909bf6fe9eadbf1948dc6b9
    Signed-off-by: Henry Castro <hcastro@collabora.com>

--- browser/src/map/handler/Map.Infobar.js
+++ browser/src/map/handler/Map.Infobar.js
@@ -2,14 +2,24 @@
 /*
  * L.Map.Infobar.
  */
-/* global app */
+/* global app _ */
 
 L.Map.Infobar = L.Handler.extend({
+	addHooks: function () {
+		this._map.on('updateviewslist', this.onUpdateList, this);
+		this._map.on('infobar', this.onInfobar, this);
+	},
+
+	removeHooks: function () {
+		this._map.off('updateviewslist', this.onUpdateList, this);
+		this._map.off('infobar', this.onInfobar, this);
+	},
+
 	onUpdateList: function () {
 		var docLayer = this._map._docLayer || {};
 		var viewInfo = this._map._viewInfo[docLayer._viewId];
 
-		if (viewInfo && !this.enabled() && viewInfo.userextrainfo &&
+		if (viewInfo && viewInfo.userextrainfo &&
 		    viewInfo.userextrainfo.is_admin) {
 			var laterDate = new Date();
 			var currentDate = new Date();
@@ -24,16 +34,15 @@ L.Map.Infobar = L.Handler.extend({
 			}
 
 			if (currentDate > laterDate)
-				this.enable();
+				app.socket.sendMessage('infobar');
 		}
 	},
 
-	onMessage: function (e) {
-		var data = e.data;
-
-		if (data.startsWith('updatecheck-show-')) {
-			var latestVersion = data.replace('updatecheck-show-', '').split('.');
+	onInfobar: function (e) {
+		if (e && e.coolwsd_version) {
+			var latestVersion = e.coolwsd_version.split('.');
 			var currentVersion = app.socket.WSDServer.Version.split('.');
+			var snackbarMessage = _('Your Collabora Online server needs updating. Version %0 is available.');
 			var length = Math.max(latestVersion.length, currentVersion.length);
 			for (var i = 0; i < length; i++) {
 				var v1 = i < latestVersion.length ? parseInt(latestVersion[i]) : 0;
@@ -42,19 +51,18 @@ L.Map.Infobar = L.Handler.extend({
 				if (v1 > v2) {
 					var currentDate = new Date();
 					window.localStorage.setItem('InfoBarLaterDate', currentDate.getTime());
-					this._iframeInfobar.show();
+					snackbarMessage = snackbarMessage.replace('%0', latestVersion);
+					this._map.uiManager.showSnackbar(snackbarMessage);
 					break;
 				}
 				if (v1 < v2) {
 					break;
 				}
 			}
-		} else if (data === 'updatecheck-close') {
-			this._map.infobar.disable();
 		}
 	}
 });
 
-if (window.infobarUrl && window.isLocalStorageAllowed) {
+if (window.isLocalStorageAllowed) {
 	L.Map.addInitHook('addHandler', 'infobar', L.Map.Infobar);
 }
