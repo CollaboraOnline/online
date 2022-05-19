6ecdb82e6127d6284261999ba23212a3d9ff830d
Michael Meeks <michael.meeks@collabora.com>
2022-05-19 15:12:23 +0100

    Proxy: send explicit charset=utf-8
    https: //github.com/nextcloud/richdocuments/issues/2196
    Signed-off-by: Michael Meeks <michael.meeks@collabora.com>
    Change-Id: I4fce74290e8bf8a6681922a2165772933443703e

--- wsd/ProxyProtocol.cpp
+++ wsd/ProxyProtocol.cpp
@@ -57,7 +57,7 @@ void DocumentBroker::handleProxyRequest(
             "Last-Modified: " << Util::getHttpTimeNow() << "\r\n"
             "User-Agent: " WOPI_AGENT_STRING "\r\n"
             "Content-Length: " << sessionId.size() << "\r\n"
-            "Content-Type: application/json\r\n"
+            "Content-Type: application/json; charset=utf-8\r\n"
             "X-Content-Type-Options: nosniff\r\n"
             "\r\n" << sessionId;
 
@@ -342,7 +342,7 @@ bool ProxyProtocolHandler::flushQueueTo(const std::shared_ptr<StreamSocket> &soc
         "Last-Modified: " << Util::getHttpTimeNow() << "\r\n"
         "User-Agent: " WOPI_AGENT_STRING "\r\n"
         "Content-Length: " << totalSize << "\r\n"
-        "Content-Type: application/json\r\n"
+        "Content-Type: application/json; charset=utf-8\r\n"
         "X-Content-Type-Options: nosniff\r\n"
         "\r\n";
     socket->send(oss.str());
