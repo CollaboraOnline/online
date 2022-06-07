949e20ba16c9405511d2f9a6429390c8a3e3301c
Henry Castro <hcastro@collabora.com>
2022-06-07 09:08:23 -0400

    wsd: add fetch external method
    After file server has been initialized,
    it fetches external logo image.
    
    Change-Id: I5d47287d5fa875c3e5825567d4ff284abe2ff500
    Signed-off-by: Henry Castro <hcastro@collabora.com>

--- wsd/COOLWSD.cpp
+++ wsd/COOLWSD.cpp
@@ -2396,6 +2396,9 @@ void COOLWSD::innerInitialize(Application& self)
                   << getServiceURI("/hosting/discovery") << '\n';
 
     std::cerr << std::endl;
+#else
+    // ---------------- from here on we start getting external messages ----------------
+    FileServerRequestHandler::fetchExternal();
 #endif
 
 #endif
