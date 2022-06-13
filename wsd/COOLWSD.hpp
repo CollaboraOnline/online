6cd96ac224c0f8d9fec1dbaaba750aabc54215f7
Andras Timar <andras.timar@collabora.com>
2022-06-13 16:15:34 +0200

    Revert "wsd: add getter function to obtain the ..."
    This reverts commit 62021bce16b9423d4b5578a51c39b2096fd03c15.
    
    Signed-off-by: Andras Timar <andras.timar@collabora.com>
    Change-Id: I0132bf6993fb4546210f74717370e2b1b6f94898

--- wsd/COOLWSD.hpp
+++ wsd/COOLWSD.hpp
@@ -311,8 +311,6 @@ public:
 #endif
     }
 
-    static std::shared_ptr<TerminatingPoll> getWebServerPoll();
-
     /// Return true if extension is marked as view action in discovery.xml.
     static bool IsViewFileExtension(const std::string& extension)
     {
