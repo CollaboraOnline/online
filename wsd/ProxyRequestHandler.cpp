9b827f5750e939b1e2c1b14a1bcc72578737b8a2
Henry Castro <hcastro@collabora.com>
2022-06-22 09:44:29 -0400

    wsd: proxy: add ProxyRequestHandler files
    Change-Id: Ic61f70316e921ba3cb988a3b0f8f8d2c7df89cc6
    Signed-off-by: Henry Castro <hcastro@collabora.com>

---
+++ wsd/ProxyRequestHandler.cpp
@@ -0,0 +1,17 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <config.h>
+
+#include <Poco/URI.h>
+
+#include <COOLWSD.hpp>
+#include "ProxyRequestHandler.hpp"
+#include <net/HttpRequest.hpp>
+#include <net/HttpHelper.hpp>
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
