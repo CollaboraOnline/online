0b1a9624069580c16abc4f73486882d57550edaf
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2022-08-04 22:02:11 -0400

    wsd: move storage attributes to DocBroker
    There are a number of races with having Storage
    track the attributes. To fix them, we move all
    attributes to DocBroker and correct a number
    of issues.
    
    The idea of the design is based on the fact that
    we want to capture the attributes between
    uploads, but based on the saved document.
    
    That is, when we upload a document version, we
    want to pass to the storage whether from the
    perspective of the *Storage* there has been
    any user-modifications or not. Since saving
    to disk may happen multiple times between
    uploads (not least because of failures), and
    since saving resets the modified flag, we need
    to capture the modified flag at each save and
    propagate it until we upload successfully.
    Upon uploading successfully, we reset the
    attributes.
    
    For this reason we have two attribute instances;
    one is the 'current' attributes as being uploaded
    and the other the 'next' one. We capture the
    current state at saving into 'next' and we merge
    with 'current' when saving succeeds and we
    aren't already uploading (otherwise, we update
    it and then discard it when uploading succeeds,
    losing the last attributes).
    
    Furthermore, because the modified flag is
    reset after each save, and because we might
    save and upload immediately after a
    modification, we may not have the modified flag.
    This means that we need some heuristics to
    decide if there has been user-issued
    modifications. (It is better to be conservative
    here.) We try to detect this by introspecting
    the commands we receive from users.
    
    In effect, we capture the attributes when issuing
    an internal save, we transfer the captured
    attributes only when saving succeeds and we aren't
    uploading, and from then on, uploading has to
    succeed to reset the 'current' attributes. In the
    meantime, if we fail to upload and issue another
    save, the new attributes will be captured and
    merged with the 'current' and the next upload
    will not have any lost attributes.
    
    Change-Id: I8c5e75d25ac235c6232318343678bf5c0933c31e
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- wsd/DocumentBroker.cpp
+++ wsd/DocumentBroker.cpp
@@ -939,7 +939,8 @@ bool DocumentBroker::download(const std::shared_ptr<ClientSession>& session, con
         // Only lock the document on storage for editing sessions
         // FIXME: why not lock before downloadStorageFileToLocal? Would also prevent race conditions
         if (!session->isReadOnly() &&
-            !_storage->updateLockState(session->getAuthorization(), *_lockCtx, true))
+            !_storage->updateLockState(session->getAuthorization(), *_lockCtx, true,
+                                       _currentStorageAttrs))
         {
             LOG_ERR("Failed to lock!");
             session->setLockFailed(_lockCtx->_lockFailureReason);
@@ -1146,7 +1147,8 @@ void DocumentBroker::endRenameFileCommand()
 
 bool DocumentBroker::attemptLock(const ClientSession& session, std::string& failReason)
 {
-    const bool bResult = _storage->updateLockState(session.getAuthorization(), *_lockCtx, true);
+    const bool bResult = _storage->updateLockState(session.getAuthorization(), *_lockCtx, true,
+                                                   _currentStorageAttrs);
     if (!bResult)
         failReason = _lockCtx->_lockFailureReason;
     return bResult;
@@ -1273,6 +1275,16 @@ void DocumentBroker::handleSaveResponse(const std::string& sessionId, bool succe
     // Record that we got a response to avoid timing out on saving.
     _saveManager.setLastSaveResult(success || result == "unmodified");
 
+    if (success && !isAsyncUploading())
+    {
+        // Update the storage attributes to capture what's
+        // new and applies to this new version and reset.
+        // These are the attributes of the next version to be uploaded.
+        // Note: these are owned by us and this is thread-safe.
+        _currentStorageAttrs.merge(_nextStorageAttrs);
+        _nextStorageAttrs.reset();
+    }
+
     // The the clients know of any save failures.
     if (!success && result != "unmodified")
     {
@@ -1367,9 +1379,11 @@ void DocumentBroker::uploadToStorage(const std::string& sessionId, bool force)
 {
     assertCorrectThread();
 
-    if (force && _storage)
+    if (force)
     {
-        _storage->forceSave();
+        // Don't reset the force flag if it was set
+        // (which would imply we failed to upload).
+        _currentStorageAttrs.setForced(force);
     }
 
     if (force || _storageManager.lastUploadSuccessful() || _storageManager.canUploadNow())
@@ -1535,7 +1549,8 @@ void DocumentBroker::uploadToStorageInternal(const std::string& sessionId,
     };
 
     _storage->uploadLocalFileToStorageAsync(session->getAuthorization(), *_lockCtx, saveAsPath,
-                                            saveAsFilename, isRename, *_poll, asyncUploadCallback);
+                                            saveAsFilename, isRename, _currentStorageAttrs, *_poll,
+                                            asyncUploadCallback);
 }
 
 void DocumentBroker::handleUploadToStorageResponse(const StorageBase::UploadResult& uploadResult)
@@ -1585,6 +1600,9 @@ void DocumentBroker::handleUploadToStorageResponse(const StorageBase::UploadResu
 
             // After a successful save, we are sure that document in the storage is same as ours
             _documentChangedInStorage = false;
+
+            // Reset the storage attributes; They've been used and we can discard them.
+            _currentStorageAttrs.reset();
 
             LOG_DBG("Uploaded docKey ["
                     << _docKey << "] to URI [" << _uploadRequest->uriAnonym()
@@ -1872,7 +1890,8 @@ void DocumentBroker::refreshLock()
     else
     {
         std::shared_ptr<ClientSession> session = it->second;
-        if (!session || !_storage->updateLockState(session->getAuthorization(), *_lockCtx, true))
+        if (!session || !_storage->updateLockState(session->getAuthorization(), *_lockCtx, true,
+                                                   _currentStorageAttrs))
             LOG_ERR("Failed to refresh lock");
     }
 }
@@ -2160,15 +2179,13 @@ bool DocumentBroker::sendUnoSave(const std::string& sessionId, bool dontTerminat
         // arguments end
         oss << '}';
 
-        if (_storage)
-        {
-            // If we can't upload, we will quarantine.
-            //FIXME: these must be tracked in DocBroker as they will
-            // get clobbered when uploading fails and we save again.
-            _storage->setIsAutosave(isAutosave || UnitWSD::get().isAutosave());
-            _storage->setIsExitSave(isExitSave);
-            _storage->setExtendedData(extendedData);
-        }
+        // At this point, if we have any potential modifications, we need to capture the fact.
+        _nextStorageAttrs.setUserModified(isModified() || haveModifyActivityAfterSaveRequest());
+
+        //FIXME: It's odd to capture these here, but this function is used from ClientSession too.
+        _nextStorageAttrs.setIsAutosave(isAutosave || UnitWSD::get().isAutosave());
+        _nextStorageAttrs.setIsExitSave(isExitSave);
+        _nextStorageAttrs.setExtendedData(extendedData);
 
         const std::string saveArgs = oss.str();
         LOG_TRC(".uno:Save arguments: " << saveArgs);
@@ -2405,7 +2422,8 @@ void DocumentBroker::disconnectSessionInternal(const std::string& id)
             // Unlock the document, if last editable sessions, before we lose a token that can unlock.
             if (lastEditableSession && _lockCtx->_isLocked && _storage)
             {
-                if (!_storage->updateLockState(it->second->getAuthorization(), *_lockCtx, false))
+                if (!_storage->updateLockState(it->second->getAuthorization(), *_lockCtx, false,
+                                               _currentStorageAttrs))
                     LOG_ERR("Failed to unlock!");
             }
 
@@ -3130,13 +3148,6 @@ void DocumentBroker::setModified(const bool value)
     }
 #endif
 
-    if (value || _storageManager.lastUploadSuccessful())
-    {
-        // Set the X-COOL-WOPI-IsModifiedByUser header flag sent to storage.
-        // But retain the last value if we failed to upload, so we don't clobber it.
-        _storage->setUserModified(value);
-    }
-
     LOG_DBG("Modified state set to " << value << " for Doc [" << _docId << ']');
     _isModified = value;
 }
@@ -3670,6 +3681,11 @@ void DocumentBroker::dumpState(std::ostream& os)
 
     os << "\n  StorageManager:";
     _storageManager.dumpState(os, "\n    ");
+
+    os << "\n    Current StorageAttributes:";
+    _currentStorageAttrs.dumpState(os, "\n      ");
+    os << "\n    Next StorageAttributes:";
+    _nextStorageAttrs.dumpState(os, "\n      ");
 
     _lockCtx->dumpState(os);
 
