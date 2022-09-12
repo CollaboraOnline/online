CollaboraOnline/online

commit 14d96957cdae7c49cf91000002c20ee37ee69fa2

wsd: log macro cleanup
* Simplify log macros.
* Reduce expanded lines (faster compilation).
* Avoid flushing (since we always use ostringstream).

Change-Id: Id10c1a75b5af5d8cdba62d768192ab0a6a9e0aca
Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

common/Log.hpp
@@ -247,35 +247,29 @@ inline constexpr void logPrefix(std::ostream&) {}
 #define STRINGIFY(X) #X
 #define STRING(X) STRINGIFY(X)
 
-#define LOG_END(LOG, FILEP)                                                                        \
-    do                                                                                             \
-    {                                                                                              \
-        if (FILEP)                                                                                 \
-            LOG << "| " LOG_FILE_NAME(__FILE__) ":" STRING(__LINE__);                              \
-        LOG.flush();                                                                               \
-    } while (false)
-
 #ifdef __ANDROID__
 
-#define LOG_BODY_(LOG, PRIO, LVL, X, FILEP)                                                        \
-    char b_[1024];                                                                                 \
-    std::ostringstream oss_(Log::prefix<sizeof(b_) - 1>(b_, LVL), std::ostringstream::ate);        \
-    logPrefix(oss_);                                                                               \
-    oss_ << std::boolalpha << X;                                                                   \
-    LOG_END(oss_, FILEP);                                                                          \
-    ((void)__android_log_print(ANDROID_LOG_DEBUG, "coolwsd", "%s %s", LVL, oss_.str().c_str()))
+#define LOG_LOG(LOG, PRIO, LVL, STR)                                                               \
+    ((void)__android_log_print(ANDROID_LOG_DEBUG, "coolwsd", "%s %s", LVL, STR.c_str()))
 
 #else
 
-#define LOG_BODY_(LOG, PRIO, LVL, X, FILEP)                                                        \
+#define LOG_LOG(LOG, PRIO, LVL, STR)                                                               \
+    LOG.log(Poco::Message(LOG.name(), STR, Poco::Message::PRIO_##PRIO))
+
+#endif
+
+#define LOG_END_NOFILE(LOG) (void)0
+
+#define LOG_END(LOG) LOG << "| " LOG_FILE_NAME(__FILE__) ":" STRING(__LINE__)
+
+#define LOG_BODY_(LOG, PRIO, LVL, X, END)                                                          \
     char b_[1024];                                                                                 \
     std::ostringstream oss_(Log::prefix<sizeof(b_) - 1>(b_, LVL), std::ostringstream::ate);        \
     logPrefix(oss_);                                                                               \
     oss_ << std::boolalpha << X;                                                                   \
-    LOG_END(oss_, FILEP);                                                                          \
-    LOG.log(Poco::Message(LOG.name(), oss_.str(), Poco::Message::PRIO_##PRIO));
-
-#endif
+    END(oss_);                                                                                     \
+    LOG_LOG(LOG, PRIO, LVL, oss_.str())
 
 #define LOG_TRC(X)                                                                                 \
     do                                                                                             \
@@ -283,7 +277,7 @@ inline constexpr void logPrefix(std::ostream&) {}
         auto& log_ = Log::logger();                                                                \
         if (!Log::isShutdownCalled() && log_.trace())                                              \
         {                                                                                          \
-            LOG_BODY_(log_, TRACE, "TRC", X, true);                                                \
+            LOG_BODY_(log_, TRACE, "TRC", X, LOG_END);                                             \
         }                                                                                          \
     } while (false)
 
@@ -293,7 +287,7 @@ inline constexpr void logPrefix(std::ostream&) {}
         auto& log_ = Log::logger();                                                                \
         if (!Log::isShutdownCalled() && log_.trace())                                              \
         {                                                                                          \
-            LOG_BODY_(log_, TRACE, "TRC", X, false);                                               \
+            LOG_BODY_(log_, TRACE, "TRC", X, LOG_END_NOFILE);                                      \
         }                                                                                          \
     } while (false)
 
@@ -303,7 +297,7 @@ inline constexpr void logPrefix(std::ostream&) {}
         auto& log_ = Log::logger();                                                                \
         if (!Log::isShutdownCalled() && log_.debug())                                              \
         {                                                                                          \
-            LOG_BODY_(log_, DEBUG, "DBG", X, true);                                                \
+            LOG_BODY_(log_, DEBUG, "DBG", X, LOG_END);                                             \
         }                                                                                          \
     } while (false)
 
@@ -313,7 +307,7 @@ inline constexpr void logPrefix(std::ostream&) {}
         auto& log_ = Log::logger();                                                                \
         if (!Log::isShutdownCalled() && log_.information())                                        \
         {                                                                                          \
-            LOG_BODY_(log_, INFORMATION, "INF", X, true);                                          \
+            LOG_BODY_(log_, INFORMATION, "INF", X, LOG_END);                                       \
         }                                                                                          \
     } while (false)
 
@@ -323,7 +317,7 @@ inline constexpr void logPrefix(std::ostream&) {}
         auto& log_ = Log::logger();                                                                \
         if (!Log::isShutdownCalled() && log_.information())                                        \
         {                                                                                          \
-            LOG_BODY_(log_, INFORMATION, "INF", X, false);                                         \
+            LOG_BODY_(log_, INFORMATION, "INF", X, LOG_END_NOFILE);                                \
         }                                                                                          \
     } while (false)
 
@@ -333,7 +327,7 @@ inline constexpr void logPrefix(std::ostream&) {}
         auto& log_ = Log::logger();                                                                \
         if (!Log::isShutdownCalled() && log_.warning())                                            \
         {                                                                                          \
-            LOG_BODY_(log_, WARNING, "WRN", X, true);                                              \
+            LOG_BODY_(log_, WARNING, "WRN", X, LOG_END);                                           \
         }                                                                                          \
     } while (false)
 
@@ -343,7 +337,7 @@ inline constexpr void logPrefix(std::ostream&) {}
         auto& log_ = Log::logger();                                                                \
         if (!Log::isShutdownCalled() && log_.error())                                              \
         {                                                                                          \
-            LOG_BODY_(log_, ERROR, "ERR", X, true);                                                \
+            LOG_BODY_(log_, ERROR, "ERR", X, LOG_END);                                             \
         }                                                                                          \
     } while (false)
 
@@ -367,7 +361,7 @@ inline constexpr void logPrefix(std::ostream&) {}
         auto& log_ = Log::logger();                                                                \
         if (!Log::isShutdownCalled() && log_.fatal())                                              \
         {                                                                                          \
-            LOG_BODY_(log_, FATAL, "FTL", X, true);                                                \
+            LOG_BODY_(log_, FATAL, "FTL", X, LOG_END);                                             \
         }                                                                                          \
     } while (false)
 

kit/ForKit.cpp
@@ -135,7 +135,7 @@ protected:
                 logger << tokens.getParam(token) << ' ';
             }
 
-            LOG_END(logger, true);
+            LOG_END(logger);
         }
 
         // Note: Syntax or parsing errors here are unexpected and fatal.

kit/Kit.cpp
@@ -2202,7 +2202,7 @@ protected:
                 logger << tokens.getParam(token) << ' ';
             }
 
-            LOG_END(logger, true);
+            LOG_END(logger);
         }
 
         // Note: Syntax or parsing errors here are unexpected and fatal.

net/Socket.cpp
@@ -1068,7 +1068,7 @@ bool StreamSocket::parseHeader(const char *clientName,
                 logger << " / " << it.first << ": " << it.second;
             }
 
-            LOG_END(logger, true);
+            LOG_END(logger);
         }
 
         const std::streamsize contentLength = request.getContentLength();

tools/WebSocketDump.cpp
@@ -112,7 +112,7 @@ private:
                     logger << " / " << it.first << ": " << it.second;
                 }
 
-                LOG_END(logger, true);
+                LOG_END(logger);
             }
 
             const std::streamsize contentLength = request.getContentLength();

wsd/COOLWSD.cpp
@@ -396,7 +396,7 @@ void cleanupDocBrokers()
                 logger << "DocumentBroker [" << pair.first << "].\n";
             }
 
-            LOG_END(logger, true);
+            LOG_END(logger);
         }
 
 #if !MOBILEAPP && ENABLE_DEBUG

wsd/DocumentBroker.cpp
@@ -2446,7 +2446,7 @@ void DocumentBroker::finalRemoveSession(const std::string& id)
                 for (const auto& pair : _sessions)
                     logger << pair.second->getId() << ' ';
 
-                LOG_END(logger, true);
+                LOG_END(logger);
             }
 
             if (UnitWSD::isUnitTesting())

wsd/Storage.cpp
@@ -624,7 +624,7 @@ WopiStorage::getWOPIFileInfoForUri(Poco::URI uriObject, const Authorization& aut
                 logger << '\t' << pair.first << ": " << pair.second << " / ";
             }
 
-            LOG_END(logger, true);
+            LOG_END(logger);
         }
 
         const std::shared_ptr<const http::Response> httpResponse
@@ -672,7 +672,7 @@ WopiStorage::getWOPIFileInfoForUri(Poco::URI uriObject, const Authorization& aut
             if (failed)
                 logRes << "\tBody: [" << wopiResponse << "]";
 
-            LOG_END(logRes, true);
+            LOG_END(logRes);
         }
 
         if (failed)
@@ -1099,7 +1099,7 @@ std::string WopiStorage::downloadDocument(const Poco::URI& uriObject, const std:
                 logger << '\t' << pair.first << ": " << pair.second << " / ";
             }
 
-            LOG_END(logger, true);
+            LOG_END(logger);
         }
     }
     else if (httpResponse->statusLine().statusCode() == Poco::Net::HTTPResponse::HTTP_FOUND ||

commit e5143e2b1a2c18d892fc6b011feda510e644733f

wsd: fix multi-part logging
This was a regression introduced in
14d96957cdae7c49cf91000002c20ee37ee69fa2.

The result was that multi-part logs were
not flushed and therefore not written to
the output.

Change-Id: Ib22a1da83209b00872d91ca05a940226cbb4552d
Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

common/Log.hpp
@@ -272,6 +272,14 @@ static constexpr std::size_t skipPathPrefix(const char (&s)[N], std::size_t n = 
 
 #define LOG_END(LOG) LOG << "| " << LOG_FILE_NAME(__FILE__) << ":" STRING(__LINE__)
 
+/// Used to end multi-statement logging via Log::StreamLogger.
+#define LOG_END_FLUSH(LOG)                                                                         \
+    do                                                                                             \
+    {                                                                                              \
+        LOG_END(LOG);                                                                              \
+        LOG.flush();                                                                               \
+    } while (false)
+
 #define LOG_BODY_(LOG, PRIO, LVL, X, END)                                                          \
     char b_[1024];                                                                                 \
     std::ostringstream oss_(Log::prefix<sizeof(b_) - 1>(b_, LVL), std::ostringstream::ate);        \

kit/ForKit.cpp
@@ -136,7 +136,7 @@ protected:
                 logger << tokens.getParam(token) << ' ';
             }
 
-            LOG_END(logger);
+            LOG_END_FLUSH(logger);
         }
 
         // Note: Syntax or parsing errors here are unexpected and fatal.

kit/Kit.cpp
@@ -2308,7 +2308,7 @@ protected:
                 logger << tokens.getParam(token) << ' ';
             }
 
-            LOG_END(logger);
+            LOG_END_FLUSH(logger);
         }
 
         // Note: Syntax or parsing errors here are unexpected and fatal.

net/Socket.cpp
@@ -1081,7 +1081,7 @@ bool StreamSocket::parseHeader(const char *clientName,
                 logger << " / " << it.first << ": " << it.second;
             }
 
-            LOG_END(logger);
+            LOG_END_FLUSH(logger);
         }
 
         const std::streamsize contentLength = request.getContentLength();

tools/WebSocketDump.cpp
@@ -112,7 +112,7 @@ private:
                     logger << " / " << it.first << ": " << it.second;
                 }
 
-                LOG_END(logger);
+                LOG_END_FLUSH(logger);
             }
 
             const std::streamsize contentLength = request.getContentLength();

wsd/COOLWSD.cpp
@@ -407,7 +407,7 @@ void cleanupDocBrokers()
                 logger << "DocumentBroker [" << pair.first << "].\n";
             }
 
-            LOG_END(logger);
+            LOG_END_FLUSH(logger);
         }
 
 #if !MOBILEAPP && ENABLE_DEBUG

wsd/DocumentBroker.cpp
@@ -2502,7 +2502,7 @@ void DocumentBroker::finalRemoveSession(const std::string& id)
                 for (const auto& pair : _sessions)
                     logger << pair.second->getId() << ' ';
 
-                LOG_END(logger);
+                LOG_END_FLUSH(logger);
             }
 
             if (UnitWSD::isUnitTesting())

wsd/Storage.cpp
@@ -634,7 +634,7 @@ WopiStorage::getWOPIFileInfoForUri(Poco::URI uriObject, const Authorization& aut
                 logger << '\t' << pair.first << ": " << pair.second << " / ";
             }
 
-            LOG_END(logger);
+            LOG_END_FLUSH(logger);
         }
 
         const std::shared_ptr<const http::Response> httpResponse
@@ -682,7 +682,7 @@ WopiStorage::getWOPIFileInfoForUri(Poco::URI uriObject, const Authorization& aut
             if (failed)
                 logRes << "\tBody: [" << wopiResponse << "]";
 
-            LOG_END(logRes);
+            LOG_END_FLUSH(logRes);
         }
 
         if (failed)
@@ -1110,7 +1110,7 @@ std::string WopiStorage::downloadDocument(const Poco::URI& uriObject, const std:
                 logger << '\t' << pair.first << ": " << pair.second << " / ";
             }
 
-            LOG_END(logger);
+            LOG_END_FLUSH(logger);
         }
     }
     else if (httpResponse->statusLine().statusCode() == Poco::Net::HTTPResponse::HTTP_FOUND ||