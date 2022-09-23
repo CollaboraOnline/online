19c70b89f411dcf95fd246c942be2be448553e40
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2022-09-23 09:13:05 -0400

    wsd: re-create the jail directory when mounting fails
    This guarantees that the jail directory is always
    created, especially in case mounting fails and
    we cleanup and fallback.
    
    Also, move the directory creation and setting
    the perms into a helper, and reuse.
    
    Change-Id: If6e9ef86afa5107638aa27050554e94c0af22b6e
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- common/JailUtil.cpp
+++ common/JailUtil.cpp
@@ -206,11 +206,18 @@ void cleanupJails(const std::string& root)
         LOG_WRN("Jails root directory [" << root << "] is not empty. Will not remove it.");
 }
 
+void createJailPath(const std::string& path)
+{
+    LOG_INF("Creating jail path (if missing): " << path);
+    Poco::File(path).createDirectories();
+    chmod(path.c_str(), S_IXUSR | S_IWUSR | S_IRUSR);
+}
+
 void setupChildRoot(bool bindMount, const std::string& childRoot, const std::string& sysTemplate)
 {
     // Start with a clean slate.
     cleanupJails(childRoot);
-    Poco::File(childRoot + CHILDROOT_TMP_INCOMING_PATH).createDirectories();
+    createJailPath(childRoot + CHILDROOT_TMP_INCOMING_PATH);
 
     disableBindMounting(); // Clear to avoid surprises.
 
