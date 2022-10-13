f7b6f2e98354ca1d9a815fc44b0791c276d96402
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2022-10-13 05:10:53 -0400

    wsd: notify the client when the doc URI is invalid
    Change-Id: I39a4c8d3dd9ca2899a9a8eacdb865a7121b75d3e
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- wsd/COOLWSD.cpp
+++ wsd/COOLWSD.cpp
@@ -3738,6 +3738,16 @@ private:
                 return;
             }
         }
+        catch (const BadRequestException& ex)
+        {
+            LOG_ERR('#' << socket->getFD() << " bad request: ["
+                        << COOLProtocol::getAbbreviatedMessage(socket->getInBuffer())
+                        << "]: " << ex.what());
+
+            // Bad request.
+            HttpHelper::sendErrorAndShutdown(400, socket);
+            return;
+        }
         catch (const std::exception& exc)
         {
             LOG_ERR('#' << socket->getFD() << " Exception while processing incoming request: [" <<
