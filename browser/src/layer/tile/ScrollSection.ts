af1b01047cd29d5bd7643475c533f9e70993a5fb
Szymon Kłos <szymon.klos@collabora.com>
2022-11-04 12:56:41 +0100

    Don't mix mouse move and auto-scroll-to-point handling
    If user moves mouse - reset current auto scroll settings.
    So we don't get scheduled events changing current state
    and causing movement in the other direction.
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: Ic7e59c87ec7b9a2cba327b5ba8e84ca14b8066e0

--- browser/src/layer/tile/ScrollSection.ts
+++ browser/src/layer/tile/ScrollSection.ts
@@ -763,6 +763,8 @@ class ScrollSection {
 	}
 
 	public onMouseMove (position: Array<number>, dragDistance: Array<number>, e: MouseEvent) {
+		this.clearQuickScrollTimeout();
+
 		if (this.sectionProperties.clickScrollVertical && this.containerObject.draggingSomething) {
 			if (!this.sectionProperties.previousDragDistance) {
 				this.sectionProperties.previousDragDistance = [0, 0];
