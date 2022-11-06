c571bb227ef9fb74f826a7a3ec527a29d92564aa
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2022-11-06 16:45:46 -0500

    wsd: test: reduce the duration of UnitWOPIStuckSave
    Change-Id: Idcb4760d879b595eedcf2b92c15113dfe93c3aa4
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- test/UnitWOPIStuckSave.cpp
+++ test/UnitWOPIStuckSave.cpp
@@ -36,7 +36,7 @@ public:
         , _phase(Phase::Load)
     {
         // We need more time to retry saving.
-        setTimeout(std::chrono::seconds(90));
+        setTimeout(std::chrono::seconds(200));
     }
 
     void configure(Poco::Util::LayeredConfiguration& config) override
@@ -44,7 +44,7 @@ public:
         WopiTestServer::configure(config);
 
         // Small value to shorten the test run time.
-        config.setUInt("per_document.limit_store_failures", 3);
+        config.setUInt("per_document.limit_store_failures", 2);
         config.setBool("per_document.always_save_on_exit", true);
     }
 
