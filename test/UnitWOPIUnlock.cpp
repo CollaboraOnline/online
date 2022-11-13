006abdde5336f0ee1bc63db69c8ddf1fabbfcfab
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2022-11-13 12:51:41 -0500

    wsd: test: fix UnitWOPILock once and for all
    This fixes a race in UnitWOPILock and the
    fact that the second view wasn't really
    read-only due to empty extensions being
    commentable, by default.
    
    Also, improve UnitWOPIUnlock.
    
    Change-Id: I628852d3474042c9e7f54c3d6780e06f694ce141
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- test/UnitWOPIUnlock.cpp
+++ test/UnitWOPIUnlock.cpp
@@ -22,7 +22,7 @@ class UnitWopiUnlock : public WopiTestServer
     STATE_ENUM(Phase, Load, Lock, Modify, Upload, Unlock, Done) _phase;
 
     std::string _lockState;
-    std::string _lockString;
+    std::string _lockToken;
     std::size_t _sessionCount;
 
 public:
@@ -60,17 +60,19 @@ public:
         {
             LOK_ASSERT_EQUAL_MESSAGE("Expected X-WOPI-Override:LOCK", std::string("LOCK"),
                                      newLockState);
-            LOK_ASSERT_MESSAGE("Lock String cannot be empty", !lock.empty());
+            LOK_ASSERT_MESSAGE("Lock token cannot be empty", !lock.empty());
             _lockState = newLockState;
-            _lockString = lock;
+            _lockToken = lock;
             TRANSITION_STATE(_phase, Phase::Modify);
         }
         else if (_phase == Phase::Unlock)
         {
             LOK_ASSERT_EQUAL_MESSAGE("Expected X-WOPI-Override:UNLOCK", std::string("UNLOCK"),
                                      newLockState);
-            LOK_ASSERT_MESSAGE("Document is not unlocked", _lockState != "UNLOCK");
-            LOK_ASSERT_EQUAL(_lockString, lock);
+            LOK_ASSERT_EQUAL_MESSAGE("Document is not unlocked", std::string("LOCK"), _lockState);
+            LOK_ASSERT_EQUAL_MESSAGE("The lock token has changed", _lockToken, lock);
+
+            TRANSITION_STATE(_phase, Phase::Done);
             exitTest(TestResult::Ok);
         }
         else
