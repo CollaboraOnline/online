CollaboraOnline/online

commit 3aa3334a7b2b0abad7a07beefa881be62a83020b

fix: memory leak with remote config
when eTag header is not passed from remote server it keeps on parsing the new settings without clearing old ones

Signed-off-by: Rash419 <rashesh.padia@collabora.com>
Change-Id: Iaad9fb6c0e7fd45be3e121b0c0bebaba17d90aab

wsd/HostUtil.cpp
@@ -80,6 +80,8 @@ void HostUtil::parseAliases(Poco::Util::LayeredConfiguration& conf)
     }
 
     AliasHosts.clear();
+    WopiHosts.clear();
+    hostList.clear();
 #if ENABLE_FEATURE_LOCK
     CommandControl::LockManager::unlockLinkMap.clear();
 #endif

wsd/COOLWSD.cpp
@@ -1199,6 +1199,7 @@ public:
 
         AutoPtr<AppConfigMap> newConfig(new AppConfigMap(newAppConfig));
         _conf.addWriteable(newConfig, PRIO_JSON);
+        newConfig->clear();
 
 #if ENABLE_FEATURE_LOCK
         CommandControl::LockManager::parseLockedHost(_conf);

commit 8c851e554cfab72e3a6d73f90d81407592a35ca2

wsd: fix: wopi.host not working when alias_groups are not defined
regression from commit 3aa3334a7b2b0abad7a07beefa881be62a83020b

Signed-off-by: Rash419 <rashesh.padia@collabora.com>
Change-Id: I601ea3bde0419129e39c1a1454c22c676d4870bd

wsd/HostUtil.cpp
@@ -65,8 +65,10 @@ void HostUtil::parseAliases(Poco::Util::LayeredConfiguration& conf)
     if (!conf.has("storage.wopi.alias_groups"))
     {
         conf.setString("storage.wopi.alias_groups[@mode]", "compat");
+        return;
     }
-    else if (conf.has("storage.wopi.alias_groups.group[0]"))
+
+    if (conf.has("storage.wopi.alias_groups.group[0]"))
     {
         // group defined in alias_groups
         if (Util::iequal(config::getString("storage.wopi.alias_groups[@mode]", "first"), "first"))