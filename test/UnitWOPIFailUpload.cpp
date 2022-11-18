4ef9c3d0130bdc1ffcc722f0442f6b55d30eb9ca
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2022-11-18 07:37:48 -0500

    wsd: test: add expired-token test
    Change-Id: I0055fcb287b45d3a4e1fbaf4e364e8fa323c9b30
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- test/UnitWOPIFailUpload.cpp
+++ test/UnitWOPIFailUpload.cpp
@@ -202,6 +202,111 @@ public:
     }
 };
 
-UnitBase* unit_create_wsd(void) { return new UnitWOPIFailUpload(); }
+/// This test simulates an expired token when uploading.
+class UnitWOPIExpiredToken : public WopiTestServer
+{
+    STATE_ENUM(Phase, Load, WaitLoadStatus, ModifyAndClose, WaitPutFile, Done) _phase;
+
+public:
+    UnitWOPIExpiredToken()
+        : WopiTestServer("UnitWOPIExpiredToken")
+        , _phase(Phase::Load)
+    {
+    }
+
+    std::unique_ptr<http::Response>
+    assertPutFileRequest(const Poco::Net::HTTPRequest& request) override
+    {
+        // Expect PutFile after closing, since the document is modified.
+        if (_phase == Phase::WaitPutFile)
+        {
+            LOG_TST("assertPutFileRequest: First PutFile, which will fail");
+
+            TRANSITION_STATE(_phase, Phase::Done);
+
+            // We requested the save.
+            LOK_ASSERT_EQUAL(std::string("false"), request.get("X-COOL-WOPI-IsAutosave"));
+
+            LOK_ASSERT_EQUAL(std::string("true"), request.get("X-COOL-WOPI-IsModifiedByUser"));
+
+            // File unknown/User unauthorized.
+            return Util::make_unique<http::Response>(http::StatusLine(404));
+        }
+
+        // This during closing the document.
+        LOG_TST("assertPutFileRequest: Second PutFile, unexpected");
+
+        LOK_ASSERT_FAIL("PutFile multiple times.");
+        failTest("PutFile multiple times.");
+
+        return nullptr;
+    }
+
+    /// The document is loaded.
+    bool onDocumentLoaded(const std::string& message) override
+    {
+        LOG_TST("onDocumentLoaded: [" << message << ']');
+        LOK_ASSERT_STATE(_phase, Phase::WaitLoadStatus);
+
+        TRANSITION_STATE(_phase, Phase::ModifyAndClose);
+
+        return true;
+    }
+
+    bool onDataLoss(const std::string& reason) override
+    {
+        LOG_TST("Modified document being unloaded: " << reason);
+
+        // We expect this to happen, because there should be
+        // no upload attempts with expired tockens. And we
+        // only have one session.
+        LOK_ASSERT_MESSAGE("Expected reason to be 'Data-loss detected'",
+                           Util::startsWith(reason, "Data-loss detected"));
+        LOK_ASSERT_MESSAGE("Expected to be in Phase::WaitDocClose but was " + toString(_phase),
+                           _phase == Phase::Done);
+
+        passTest("Data-loss detected as expected");
+        return failed();
+    }
+
+    void invokeWSDTest() override
+    {
+        switch (_phase)
+        {
+            case Phase::Load:
+            {
+                TRANSITION_STATE(_phase, Phase::WaitLoadStatus);
+
+                LOG_TST("Load: initWebsocket.");
+                initWebsocket("/wopi/files/" + getTestname() + "?access_token=anything");
+
+                WSD_CMD("load url=" + getWopiSrc());
+                break;
+            }
+            case Phase::WaitLoadStatus:
+                break;
+            case Phase::ModifyAndClose:
+            {
+                TRANSITION_STATE(_phase, Phase::WaitPutFile);
+
+                WSD_CMD("key type=input char=97 key=0");
+                WSD_CMD("key type=up char=0 key=512");
+                WSD_CMD("closedocument");
+                break;
+            }
+            case Phase::WaitPutFile:
+            case Phase::Done:
+            {
+                // just wait for the results
+                break;
+            }
+        }
+    }
+};
+
+UnitBase** unit_create_wsd_multi(void)
+{
+    return new UnitBase* [3] { new UnitWOPIExpiredToken(), new UnitWOPIFailUpload(), nullptr };
+}
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
