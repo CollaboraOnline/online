CollaboraOnline/online

commit e0752419575f09fef3d770f8cccf78c92a2d8612

fix: memory leak with remote config
when eTag header is not passed from remote server it keeps on parsing the new settings without clearing old ones

Signed-off-by: Rash419 <rashesh.padia@collabora.com>
Change-Id: Iaad9fb6c0e7fd45be3e121b0c0bebaba17d90aab

wsd/COOLWSD.cpp
@@ -1199,6 +1199,7 @@ public:
 
         AutoPtr<AppConfigMap> newConfig(new AppConfigMap(newAppConfig));
         _conf.addWriteable(newConfig, PRIO_JSON);
+        newConfig->clear();
 
 #if ENABLE_FEATURE_LOCK
         CommandControl::LockManager::parseLockedHost(_conf);

wsd/HostUtil.cpp
@@ -80,6 +80,8 @@ void HostUtil::parseAliases(Poco::Util::LayeredConfiguration& conf)
     }
 
     AliasHosts.clear();
+    WopiHosts.clear();
+    hostList.clear();
 #if ENABLE_FEATURE_LOCK
     CommandControl::LockManager::unlockLinkMap.clear();
 #endif

commit 1a08ec256b1154a61a89b59287eab1ccd3f43074

wsd:fix: memory leak properly
introduced in e0752419575f09fef3d770f8cccf78c92a2d8612 commit, update AppConfigMap instead of
creating new one on each of remote_config
poll make sure to update the feature_lock.lock_hosts[@allow] first
before returnng

Signed-off-by: Rash419 <rashesh.padia@collabora.com>
Change-Id: If11bd6e4b3a0a9f4ee32ae9bb43b076479e9bd84

wsd/COOLWSD.cpp
@@ -972,6 +972,15 @@ public:
             setRaw(pair.first, pair.second);
         }
     }
+
+    void reset(const std::map<std::string, std::string>& map)
+    {
+        clear();
+        for (const auto& pair : map)
+        {
+            setRaw(pair.first, pair.second);
+        }
+    }
 };
 
 #if !MOBILEAPP
@@ -1160,14 +1169,15 @@ public:
     RemoteConfigPoll(LayeredConfiguration& config) :
         RemoteJSONPoll(config, "remote_config.remote_url", "remoteconfig_poll", "configuration")
     {
+        constexpr int PRIO_JSON = -200; // highest priority
+        _persistConfig = new AppConfigMap(std::map<std::string, std::string>{});
+        conf.addWriteable(_persistConfig, PRIO_JSON);
     }
 
     virtual ~RemoteConfigPoll() { }
 
     void handleJSON(Poco::JSON::Object::Ptr remoteJson) override
     {
-        constexpr int PRIO_JSON = -200; // highest priority
-
         std::map<std::string, std::string> newAppConfig;
 
         fetchAliasGroups(newAppConfig, remoteJson);
@@ -1179,10 +1189,7 @@ public:
 #endif
 
         fetchRemoteFontConfig(newAppConfig, remoteJson);
-
-        AutoPtr<AppConfigMap> newConfig(new AppConfigMap(newAppConfig));
-        conf.addWriteable(newConfig, PRIO_JSON);
-        newConfig->clear();
+        _persistConfig->reset(newAppConfig);
 
 #if ENABLE_FEATURE_LOCK
         CommandControl::LockManager::parseLockedHost(conf);
@@ -1227,12 +1234,13 @@ public:
                 allow = lockedHost->get("allow");
             }
 
+            newAppConfig.insert(
+                std::make_pair("feature_lock.locked_hosts[@allow]", booleanToString(allow)));
             if (booleanToString(allow) == "false")
             {
                 LOG_INF("locked_hosts feature is disabled, set feature_lock->locked_hosts->allow to true to enable");
                 return;
             }
-            newAppConfig.insert(std::make_pair("feature_lock.locked_hosts[@allow]", booleanToString(allow)));
 
             std::size_t i;
             for (i = 0; i < lockedHostPatterns->size(); i++)
@@ -1505,6 +1513,10 @@ public:
         }
         return booleanFlag.toString();
     }
+
+private:
+    // keeps track of remote config layer
+    Poco::AutoPtr<AppConfigMap> _persistConfig = nullptr;
 };
 
 class RemoteFontConfigPoll : public RemoteJSONPoll