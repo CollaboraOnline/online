6adb68ac0c03a1d58003ac061adb8e92f6183d86
Balázs Varga (allotropia) <balazs.varga.extern@allotropia.de>
2022-11-28 15:46:32 +0100

    WASM init fakesocket on a new thread and adding some necessary headers for later
    Also feature/wasm rebased with online master.
    
    Signed-off-by: Balázs Varga (allotropia) <balazs.varga.extern@allotropia.de>
    Change-Id: I1ecba4091c22878aacc3d6033ad350b0aa2276dc

--- wasm/wasmapp.hpp
+++ wasm/wasmapp.hpp
@@ -5,7 +5,30 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+#include <config.h>
+
+#include <chrono>
+#include <thread>
+
+#include <FakeSocket.hpp>
+#include <Kit.hpp>
+#include <Log.hpp>
+#include <COOLWSD.hpp>
+#include <Protocol.hpp>
+#include <SetupKitEnvironment.hpp>
+#include <Util.hpp>
+
+#include <Poco/Base64Encoder.h>
+
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#include <emscripten/bind.h>
+#include <emscripten/html5.h>
+#include <emscripten/val.h>
+#endif
+
 extern int coolwsd_server_socket_fd;
-extern const char *user_name;
+
+extern const char* user_name;
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
