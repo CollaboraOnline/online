d9e2cbaf2bc5af5193be40054979faba646ac31a
Rash419 <rashesh.padia@collabora.com>
2022-12-03 12:13:02 +0530

    js: added routeToken as parameter in wopisrc
    fetch route_token from indirectionurl and add them in wopisrc
    parameter
    
    Signed-off-by: Rash419 <rashesh.padia@collabora.com>
    Change-Id: I6e724d0c59e12d4f7f6c125ec076e90d20b9b3c8

--- browser/html/cool.html.m4
+++ browser/html/cool.html.m4
@@ -316,7 +316,8 @@ m4_ifelse(MOBILEAPP,[true],
       window.uiDefaults = {};
       window.useIntegrationTheme = 'false';
       window.checkFileInfoOverride = {};
-      window.deeplEnabled = false;],
+      window.deeplEnabled = false;
+      window.indirectionUrl='';],
      [window.host = '%HOST%';
       window.serviceRoot = '%SERVICE_ROOT%';
       window.hexifyUrl = %HEXIFY_URL%;
@@ -342,7 +343,8 @@ m4_ifelse(MOBILEAPP,[true],
       window.groupDownloadAsForNb = %GROUP_DOWNLOAD_AS%;
       window.uiDefaults = %UI_DEFAULTS%;
       window.checkFileInfoOverride = %CHECK_FILE_INFO_OVERRIDE%;
-	  window.deeplEnabled = %DEEPL_ENABLED%;])
+	    window.deeplEnabled = %DEEPL_ENABLED%;
+      window.indirectionUrl='%INDIRECTION_URL%';])
 
 // This is GLOBAL_JS:
 m4_syscmd([cat ]GLOBAL_JS)m4_dnl
--- wsd/FileServer.cpp
+++ wsd/FileServer.cpp
@@ -1073,6 +1073,8 @@ void FileServerRequestHandler::preprocessFile(const HTTPRequest& request,
     Poco::replaceInPlace(preprocess, std::string("%WELCOME_URL%"), std::string(WELCOME_URL));
     Poco::replaceInPlace(preprocess, std::string("%BUYPRODUCT_URL%"), buyProduct);
     Poco::replaceInPlace(preprocess, std::string("%DEEPL_ENABLED%"), (config.getBool("deepl.enabled", false) ? std::string("true"): std::string("false")));
+    Poco::URI indirectionURI(config.getString("indirection_endpoint.url", ""));
+    Poco::replaceInPlace(preprocess, std::string("%INDIRECTION_URL%"), indirectionURI.toString());
 
     const std::string mimeType = "text/html";
 
@@ -1082,7 +1084,7 @@ void FileServerRequestHandler::preprocessFile(const HTTPRequest& request,
     cspOss << "Content-Security-Policy: default-src 'none'; "
         "frame-src 'self' " << WELCOME_URL << " " << FEEDBACK_URL << " " << buyProduct <<
         " blob: " << documentSigningURL << "; "
-           "connect-src 'self' https://www.zotero.org https://api.zotero.org " << cnxDetails.getWebSocketUrl() << "; "
+           "connect-src 'self' https://www.zotero.org https://api.zotero.org " << cnxDetails.getWebSocketUrl() << " " << indirectionURI.getAuthority() << "; "
            "script-src 'unsafe-inline' 'self'; "
            "style-src 'self' 'unsafe-inline'; "
            "font-src 'self' data:; "
