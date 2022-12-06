c5fd8056197bd75098d26f5fc69303c7e404e2b4
Rash419 <rashesh.padia@collabora.com>
2022-12-06 17:30:32 +0530

    admin: added new admin->client command
    routing_rmdoc command is added specifically to notify indirection url
    server about document getting removed with its wopiSrc
    
    Signed-off-by: Rash419 <rashesh.padia@collabora.com>
    Change-Id: Ic077f210d520394e3ef8ced778604b7343436328

--- wsd/Admin.cpp
+++ wsd/Admin.cpp
@@ -554,9 +554,9 @@ void Admin::modificationAlert(const std::string& dockey, pid_t pid, bool value){
 
 void Admin::addDoc(const std::string& docKey, pid_t pid, const std::string& filename,
                    const std::string& sessionId, const std::string& userName, const std::string& userId,
-                   const int smapsFD, const std::string& wopiHost)
+                   const int smapsFD, const Poco::URI& wopiSrc)
 {
-    addCallback([=] { _model.addDocument(docKey, pid, filename, sessionId, userName, userId, smapsFD, wopiHost); });
+    addCallback([=] { _model.addDocument(docKey, pid, filename, sessionId, userName, userId, smapsFD, wopiSrc); });
 }
 
 void Admin::rmDoc(const std::string& docKey, const std::string& sessionId)
