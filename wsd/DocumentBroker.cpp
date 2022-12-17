753b571d400cdd31f150e51b185f613c90dc49a2
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2022-12-17 12:21:17 -0500

    wsd: make sessions readonly on disk errors
    Change-Id: I0c59c4fb3c99dee0f6e18850ca795467a9421b33
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- wsd/DocumentBroker.cpp
+++ wsd/DocumentBroker.cpp
@@ -1867,6 +1867,7 @@ void DocumentBroker::handleUploadToStorageResponse(const StorageBase::UploadResu
         for (const auto& sessionIt : _sessions)
         {
             sessionIt.second->sendTextFrameAndLogError("error: cmd=storage kind=savetoolarge");
+            sessionIt.second->setWritable(false);
         }
 
         broadcastSaveResult(false, "Too large", uploadResult.getReason());
@@ -1881,6 +1882,7 @@ void DocumentBroker::handleUploadToStorageResponse(const StorageBase::UploadResu
         for (const auto& sessionIt : _sessions)
         {
             sessionIt.second->sendTextFrameAndLogError("error: cmd=storage kind=savediskfull");
+            sessionIt.second->setWritable(false);
         }
 
         broadcastSaveResult(false, "Disk full", uploadResult.getReason());
