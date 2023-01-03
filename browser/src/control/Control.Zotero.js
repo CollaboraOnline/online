9f4a82aa1d76dbf7596a4069876269654c8fe137
Pranam Lashkari <lpranam@collabora.com>
2023-01-03 23:13:57 +0530

    zotero: avoid asking unnecessary bibliography data
    Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
    Change-Id: I6aab932715cdfc9511f0b96b4f6aaf671f0b6efb

--- browser/src/control/Control.Zotero.js
+++ browser/src/control/Control.Zotero.js
@@ -283,7 +283,7 @@ L.Control.Zotero = L.Control.extend({
 	},
 
 	getZoteroItemQuery: function() {
-		return '?v=3&key=' + this.apiKey + '&include=data,citation,bib,csljson&style=' + this.settings.style + '&locale=' + this.settings.locale;
+		return '?v=3&key=' + this.apiKey + '&include=data,citation,csljson&style=' + this.settings.style + '&locale=' + this.settings.locale;
 	},
 
 	_getDefaultSubCollections: function () {
