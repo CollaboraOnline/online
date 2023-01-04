7599a7ef13d0c8f35f99cab537f6357fd089240b
Pranam Lashkari <lpranam@collabora.com>
2023-01-04 06:32:32 +0530

    zotero: keep citation JSON in same sequence as the desktop
    makes it easy to compare for debugging
    
    Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
    Change-Id: I948b79d1f17224dfe40dd70671abfb35f49d7336

--- browser/src/control/Control.Zotero.js
+++ browser/src/control/Control.Zotero.js
@@ -318,8 +318,6 @@ L.Control.Zotero = L.Control.extend({
 		var resultJSON = {};
 		resultJSON['citationID'] = Math.random().toString(36).substring(2,12);
 
-		resultJSON['schema'] = 'https://raw.githubusercontent.com/citation-style-language/schema/master/schemas/input/csl-citation.json';
-
 		var properties = {
 			formattedCitation: item['citation'],
 			plainCitation: new DOMParser().parseFromString(item['citation'], 'text/html').documentElement.textContent,
@@ -336,6 +334,8 @@ L.Control.Zotero = L.Control.extend({
 		citationItems['itemData'] = item['csljson'];
 
 		resultJSON['citationItems'] = [citationItems];
+
+		resultJSON['schema'] = 'https://raw.githubusercontent.com/citation-style-language/schema/master/schemas/input/csl-citation.json';
 
 		return JSON.stringify(resultJSON);
 	},
