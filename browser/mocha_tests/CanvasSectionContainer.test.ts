e53a46046ee94dc3b5131cd263eb35dc156fad15
Dennis Francis <dennis.francis@collabora.com>
2023-01-11 14:26:08 +0530

    mocha: tests for 2-horiz section container
    Signed-off-by: Dennis Francis <dennis.francis@collabora.com>
    Change-Id: I26b29113fcb627e1f22a3578eb82641d105a67ae

--- browser/mocha_tests/CanvasSectionContainer.test.ts
+++ browser/mocha_tests/CanvasSectionContainer.test.ts
@@ -50,3 +50,66 @@ describe('Singleton section container', function() {
         assertPosSize(onlyRect, {x: 0, y: 0, width: 1024, height: 768});
     });
 });
+
+describe('Horizontally packed two section container', function() {
+
+    const sectionContainer = setupCanvasContainer(canvasWidth, canvasHeight);
+
+    const docLayer = {};
+    const tsManager = {};
+
+    sectionContainer.createSection({
+        name: 'LeftSection',
+        anchor: 'top left',
+        position: [0, 0],
+        size: [Math.round(canvasWidth / 2), 1],
+        expand: 'bottom',
+        processingOrder: 1,
+        drawingOrder: 1,
+        zIndex: 1,
+        interactable: false,
+        sectionProperties: {
+            docLayer: docLayer,
+            tsManager: tsManager,
+            strokeStyle: '#c0c0c0'
+        },
+    });
+
+    sectionContainer.createSection({
+        name: 'RightSection',
+        anchor: ['top', ['LeftSection', 'right', 'left']],
+        position: [0, 0],
+        size: [1, 1],
+        expand: 'bottom right',
+        processingOrder: 2,
+        drawingOrder: 2,
+        zIndex: 1,
+        interactable: false,
+        sectionProperties: {
+            docLayer: docLayer,
+            tsManager: tsManager,
+            strokeStyle: '#c0c0c0'
+        },
+    });
+
+    sectionContainer.enableDrawing();
+    it('Container should have LeftSection', function() {
+        assert.ok(sectionContainer.doesSectionExist('LeftSection'));
+    });
+
+    it('Container should have RightSection', function() {
+        assert.ok(sectionContainer.doesSectionExist('RightSection'));
+    });
+
+    it('LeftSection PosSize checks', function () {
+        const left = sectionContainer.getSectionWithName('LeftSection');
+        const leftRect = getSectionRectangle(left);
+        assertPosSize(leftRect, {x: 0, y: 0, width: 512, height: 768});
+    });
+
+    it('RightSection PosSize checks', function () {
+        const right = sectionContainer.getSectionWithName('RightSection');
+        const rightRect = getSectionRectangle(right);
+        assertPosSize(rightRect, {x: 513, y: 0, width: 511, height: 768});
+    });
+});
