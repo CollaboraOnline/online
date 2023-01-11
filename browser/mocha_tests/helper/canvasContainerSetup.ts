13f746781b78193a3b2a36299a07b17e4bd8e879
Dennis Francis <dennis.francis@collabora.com>
2023-01-11 14:23:35 +0530

    mocha: move jsdom setup string to helper function
    Signed-off-by: Dennis Francis <dennis.francis@collabora.com>
    Change-Id: I8a04aabee4691d1c0d914618a1be49bd8dac7fb2

---
+++ browser/mocha_tests/helper/canvasContainerSetup.ts
@@ -0,0 +1,14 @@
+
+function canvasDomString() {
+    return `
+    <!DOCTYPE html>
+    <html>
+      <head></head>
+      <body>
+        <div id="canvas-container">
+            <canvas id="document-canvas"></canvas>
+        </div>
+      </body>
+    </html>`;
+}
+
