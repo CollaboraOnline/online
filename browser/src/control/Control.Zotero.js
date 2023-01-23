ccda1de44587e78efff0b77fbf982c4cd4d8e796
Pranam Lashkari <lpranam@collabora.com>
2023-01-23 16:17:31 +0530

    zotero: added unlink citation support for section
    sections are used to represent bibliography when citations are used as refmark
    
    Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
    Change-Id: I4094cf053cb3fe1c86eb7f7f0a95816fb51fa8c7

--- browser/src/control/Control.Zotero.js
+++ browser/src/control/Control.Zotero.js
@@ -1252,6 +1252,14 @@ L.Control.Zotero = L.Control.extend({
 				}
 			};
 			command = '.uno:DeleteFields';
+
+			var sectionUnlinkParameter = {
+				'SectionNamePrefix': {
+					'type': 'string',
+					'value': 'ZOTERO_BIBL'
+				}
+			};
+			this.map.sendUnoCommand('.uno:DeleteSections', sectionUnlinkParameter);
 		}
 		this.map.sendUnoCommand(command, parametes);
 		this.resetCitation();
