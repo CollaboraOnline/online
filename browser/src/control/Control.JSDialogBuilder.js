54d17588adbb1f2a4c0c6559ded8a2ecbb410ebf
Pedro Pinto Silva <pedro.silva@collabora.com>
2023-02-13 17:09:24 +0100

    Add sheet RTL switch to Calc tabbed view
    Signed-off-by: Pedro Pinto Silva <pedro.silva@collabora.com>
    Change-Id: I8932c149452fba8952824e5313876645093c0074

--- browser/src/control/Control.JSDialogBuilder.js
+++ browser/src/control/Control.JSDialogBuilder.js
@@ -2141,6 +2141,7 @@ L.Control.JSDialogBuilder = L.Control.extend({
 		}
 
 		var iconURLAliases = {
+			'sheetrighttoleft' : 'pararighttoleft',
 			'alignleft': 'leftpara',
 			'alignright': 'rightpara',
 			'alignhorizontalcenter': 'centerpara',
