f072289574d332bdd0ea48aca64278104cd85c72
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2023-02-16 18:12:03 -0500

    wsd: better logging around new child management
    Change-Id: Id108ef7a635975bb4fd30ceb509fd2d39c3456f1
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>
    (cherry picked from commit ed341a61ad7f3d68ae545cd4683cbe63211f7a0b)
    Signed-off-by: luigiiucci <luigi.iucci@collabora.com>

--- wsd/COOLWSD.cpp
+++ wsd/COOLWSD.cpp
@@ -481,7 +481,9 @@ static int rebalanceChildren(int balance)
 {
     Util::assertIsLocked(NewChildrenMutex);
 
-    LOG_TRC("rebalance children to " << balance);
+    const size_t available = NewChildren.size();
+    LOG_TRC("Rebalance children to " << balance << ", have " << available << " and "
+                                     << OutstandingForks << " outstanding requests");
 
     // Do the cleanup first.
     const bool rebalance = cleanupChildren();
@@ -497,15 +499,15 @@ static int rebalanceChildren(int balance)
         OutstandingForks = 0;
     }
 
-    const size_t available = NewChildren.size();
     balance -= available;
     balance -= OutstandingForks;
 
     if (balance > 0 && (rebalance || OutstandingForks == 0))
     {
-        LOG_DBG("prespawnChildren: Have " << available << " spare " <<
-                (available == 1 ? "child" : "children") << ", and " <<
-                OutstandingForks << " outstanding, forking " << balance << " more.");
+        LOG_DBG("prespawnChildren: Have " << available << " spare "
+                                          << (available == 1 ? "child" : "children") << ", and "
+                                          << OutstandingForks << " outstanding, forking " << balance
+                                          << " more. Time since last request: " << durationMs);
         return forkChildren(balance);
     }
 
@@ -543,7 +545,8 @@ static size_t addNewChild(std::shared_ptr<ChildProcess> child)
         ChildSpawnTimeoutMs = CHILD_TIMEOUT_MS;
     }
 
-    LOG_TRC("Adding a new child " << pid << " to NewChildren");
+    LOG_TRC("Adding a new child " << pid << " to NewChildren, have " << OutstandingForks
+                                  << " outstanding requests");
     NewChildren.emplace_back(std::move(child));
     const size_t count = NewChildren.size();
     lock.unlock();
@@ -563,16 +566,16 @@ std::mutex COOLWSD::lokit_main_mutex;
 
 std::shared_ptr<ChildProcess> getNewChild_Blocks(unsigned mobileAppDocId)
 {
-    std::unique_lock<std::mutex> lock(NewChildrenMutex);
-
     const auto startTime = std::chrono::steady_clock::now();
+
+    std::unique_lock<std::mutex> lock(NewChildrenMutex);
 
 #if !MOBILEAPP
     (void) mobileAppDocId;
 
-    LOG_DBG("getNewChild: Rebalancing children.");
     int numPreSpawn = COOLWSD::NumPreSpawnedChildren;
     ++numPreSpawn; // Replace the one we'll dispatch just now.
+    LOG_DBG("getNewChild: Rebalancing children to " << numPreSpawn);
     if (rebalanceChildren(numPreSpawn) < 0)
     {
         LOG_DBG("getNewChild: rebalancing of children failed. Scheduling housekeeping to recover.");
@@ -3466,13 +3469,17 @@ private:
 #endif
             if (requestURI.getPath() != NEW_CHILD_URI)
             {
-                LOG_ERR("Invalid incoming URI.");
+                LOG_ERR("Invalid incoming child URI [" << requestURI.getPath() << ']');
                 return;
             }
 
+            const auto duration = (std::chrono::steady_clock::now() - LastForkRequestTime);
+            const auto durationMs = std::chrono::duration_cast<std::chrono::milliseconds>(duration);
+            LOG_TRC("New child spawned after " << durationMs << " of requesting");
+
             // New Child is spawned.
             const Poco::URI::QueryParameters params = requestURI.getQueryParameters();
-            int pid = socket->getPid();
+            const int pid = socket->getPid();
             std::string jailId;
             for (const auto& param : params)
             {
