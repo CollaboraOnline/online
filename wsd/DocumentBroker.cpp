e6180f31f3e638897a6032c95e5ad49f0e32290e
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2023-03-11 17:46:32 -0500

    wsd: remove single-use getWriteableSessionId
    Change-Id: I66a77be784aaef61472cd02cb49168813d75548c
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- wsd/DocumentBroker.cpp
+++ wsd/DocumentBroker.cpp
@@ -2027,12 +2027,6 @@ std::shared_ptr<ClientSession> DocumentBroker::getWriteableSession() const
     return savingSession;
 }
 
-std::string DocumentBroker::getWriteableSessionId() const
-{
-    const auto session = getWriteableSession();
-    return session ? session->getId() : std::string();
-}
-
 void DocumentBroker::refreshLock()
 {
     assertCorrectThread();
