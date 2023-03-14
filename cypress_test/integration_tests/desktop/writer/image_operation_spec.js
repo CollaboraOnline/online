e93a193ae08129d2fa4a25ca45c60c3b5a179820
Rash419 <rashesh.padia@collabora.com>
2023-03-14 15:42:17 +0530

    Revert "Remove unnecessary `.wait`s from image tests"
    - image test consistently fails without waits in CI
    
    This reverts commit b529c96a488eb23716d99599cb9280360b4c53e7.
    
    Signed-off-by: Rash419 <rashesh.padia@collabora.com>
    Change-Id: I66bd85c8fe1ce28ccab8d644a98265145d3de64b

--- cypress_test/integration_tests/desktop/writer/image_operation_spec.js
+++ cypress_test/integration_tests/desktop/writer/image_operation_spec.js
@@ -32,19 +32,29 @@ describe('Image Operation Tests', function() {
 		// if window is too small sidebar won't popup
 		cy.viewport(1000, 660);
 
+		helper.waitUntilIdle('#selectwidth input');
+
 		cy.get('#selectwidth input').clear({force:true})
 			.type('3{enter}', {force:true});
 
+		helper.waitUntilIdle('#selectheight input');
+
 		cy.get('#selectheight input').clear({force:true})
 			.type('2{enter}', {force:true});
+
+		cy.wait(1000);
 
 		assertImageSize(139, 93);
 
 		//Keep ratio checked
 		cy.get('#ratio input').check();
 
+		helper.waitUntilIdle('#selectheight input');
+
 		cy.get('#selectheight input').clear({force:true})
 			.type('5{enter}', {force:true});
+
+		cy.wait(1000);
 
 		assertImageSize(347, 232);
 	});
