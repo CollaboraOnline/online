d6f513ae1dd3498fe9277228a320d8877edfb21e
Szymon Kłos <szymon.klos@collabora.com>
2023-03-15 13:39:16 +0100

    jsdialog: handle key events in drawing area
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I9ecd4b734dfb3c3bb7698bf019836b142e57f8ae

--- browser/src/control/jsdialog/Widget.DrawingArea.js
+++ browser/src/control/jsdialog/Widget.DrawingArea.js
@@ -17,7 +17,7 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-/* global JSDialog $ */
+/* global JSDialog $ UNOKey UNOModifier */
 
 function _drawingAreaControl (parentContainer, data, builder) {
 	var container = L.DomUtil.create('div', builder.options.cssClass + ' ui-drawing-area-container', parentContainer);
@@ -30,6 +30,7 @@ function _drawingAreaControl (parentContainer, data, builder) {
 	image.src = data.image.replace(/\\/g, '');
 	image.alt = data.text;
 	image.title = data.text;
+	image.tabIndex = 0;
 	builder.map.uiManager.enableTooltip(image);
 
 	if (data.loading && data.loading === 'true') {
@@ -76,6 +77,103 @@ function _drawingAreaControl (parentContainer, data, builder) {
 			builder.callback('drawingarea', 'dblclick', container, coordinates, builder);
 		}
 	}, this);
+
+	var modifier = 0;
+
+	container.addEventListener('keydown', function(event) {
+		if (event.key === 'Enter') {
+			builder.callback('drawingarea', 'keypress', container, UNOKey.RETURN | modifier, builder);
+			event.preventDefault();
+		} else if (event.key === 'Escape' || event.key === 'Esc') {
+			builder.callback('drawingarea', 'keypress', container, UNOKey.ESCAPE | modifier, builder);
+			event.preventDefault();
+		} else if (event.key === 'Left' || event.key === 'ArrowLeft') {
+			builder.callback('drawingarea', 'keypress', container, UNOKey.LEFT | modifier, builder);
+			event.preventDefault();
+		} else if (event.key === 'Right' || event.key === 'ArrowRight') {
+			builder.callback('drawingarea', 'keypress', container, UNOKey.RIGHT | modifier, builder);
+			event.preventDefault();
+		} else if (event.key === 'Up' || event.key === 'ArrowUp') {
+			builder.callback('drawingarea', 'keypress', container, UNOKey.UP | modifier, builder);
+			event.preventDefault();
+		} else if (event.key === 'Down' || event.key === 'ArrowDown') {
+			builder.callback('drawingarea', 'keypress', container, UNOKey.DOWN | modifier, builder);
+			event.preventDefault();
+		} else if (event.key === 'Home') {
+			builder.callback('drawingarea', 'keypress', container, UNOKey.HOME | modifier, builder);
+			event.preventDefault();
+		} else if (event.key === 'End') {
+			builder.callback('drawingarea', 'keypress', container, UNOKey.END | modifier, builder);
+			event.preventDefault();
+		} else if (event.key === 'Backspace') {
+			builder.callback('drawingarea', 'keypress', container, UNOKey.BACKSPACE | modifier, builder);
+			event.preventDefault();
+		} else if (event.key === 'Delete') {
+			builder.callback('drawingarea', 'keypress', container, UNOKey.DELETE | modifier, builder);
+			event.preventDefault();
+		} else if (event.key === 'Space') {
+			builder.callback('drawingarea', 'keypress', container, UNOKey.SPACE | modifier, builder);
+			event.preventDefault();
+		} else if (event.key === 'Tab') {
+			builder.callback('drawingarea', 'keypress', container, UNOKey.TAB | modifier, builder);
+			event.preventDefault();
+		} else if (event.key === 'Shift') {
+			modifier = modifier | UNOModifier.SHIFT;
+			event.preventDefault();
+		} else if (event.key === 'Control') {
+			modifier = modifier | UNOModifier.CTRL;
+			event.preventDefault();
+		} else if (event.key === 'a' && event.ctrlKey) {
+			builder.callback('drawingarea', 'keypress', container, UNOKey.A | UNOModifier.CTRL, builder);
+		}
+	});
+
+	container.addEventListener('keyup', function(event) {
+		if (event.key === 'Shift') {
+			modifier = modifier & (~UNOModifier.SHIFT);
+			event.preventDefault();
+		} else if (event.key === 'Control') {
+			modifier = modifier & (~UNOModifier.CTRL);
+			event.preventDefault();
+		}
+	});
+
+	container.addEventListener('blur', function() {
+		modifier = 0;
+	});
+
+	container.addEventListener('keypress', function(event) {
+		if (event.key === 'Enter' ||
+			event.key === 'Escape' ||
+			event.key === 'Esc' ||
+			event.key === 'Left' ||
+			event.key === 'ArrowLeft' ||
+			event.key === 'Right' ||
+			event.key === 'ArrowRight' ||
+			event.key === 'Up' ||
+			event.key === 'ArrowUp' ||
+			event.key === 'Down' ||
+			event.key === 'ArrowDown' ||
+			event.key === 'Home' ||
+			event.key === 'End' ||
+			event.key === 'Backspace' ||
+			event.key === 'Delete' ||
+			event.key === 'Space' ||
+			event.key === 'Tab') {
+			// skip
+		} else {
+			var keyCode = event.keyCode;
+			if (event.ctrlKey) {
+				keyCode = event.key.toUpperCase().charCodeAt(0);
+				keyCode = builder.map.keyboard._toUNOKeyCode(keyCode);
+				keyCode |= UNOModifier.CTRL;
+			}
+
+			builder.callback('drawingarea', 'keypress', container, keyCode, builder);
+		}
+
+		event.preventDefault();
+	});
 
 	return false;
 }
