32da340c7dd84e82ef99d993bf6d7aae66350258
Gökay Şatır <gokaysatir@collabora.com>
2023-03-24 18:52:30 +0300

    Remove vex from code.
    Signed-off-by: Gökay Şatır <gokaysatir@collabora.com>
    Change-Id: I054977b5c71364c59c7f1fdd1f5d691380951ebb

--- browser/admin/main-admin.js
+++ browser/admin/main-admin.js
@@ -1,17 +1,11 @@
 /* -*- js-indent-level: 8 -*- */
 // CSS requires
-require('vex-js/dist/css/vex.css');
-require('vex-js/dist/css/vex-theme-plain.css');
 
 var $ = require('jquery');
 global.$ = global.jQuery = $;
 
 require('json-js/json2');
 require('l10n-for-node');
-
-var vex = require('vex-js/dist/js/vex.combined.js');
-vex.defaultOptions.className = 'vex-theme-plain';
-global.vex = vex;
 
 global._ = function (string) {
 	return string.toLocaleString();
