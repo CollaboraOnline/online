7d23ebc9a19f4fd592d3b49aa36129eee9ac140e
Paris Oplopoios <paris.oplopoios@collabora.com>
2023-03-29 13:56:56 +0300

    Handle LOK_CALLBACK_APPLICATION_BACKGROUND_COLOR in ChildSession
    Signed-off-by: Paris Oplopoios <paris.oplopoios@collabora.com>
    Change-Id: Icef1e490e847ca21bc525dfd989190940b746b78

--- kit/ChildSession.cpp
+++ kit/ChildSession.cpp
@@ -3289,6 +3289,9 @@ void ChildSession::loKitCallback(const int type, const std::string& payload)
     case LOK_CALLBACK_DOCUMENT_BACKGROUND_COLOR:
         sendTextFrame("documentbackgroundcolor: " + payload);
         break;
+    case LOK_CALLBACK_APPLICATION_BACKGROUND_COLOR:
+        sendTextFrame("applicationbackgroundcolor: " + payload);
+        break;
     case LOK_CALLBACK_MEDIA_SHAPE:
         sendTextFrame("mediashape: " + payload);
         break;
