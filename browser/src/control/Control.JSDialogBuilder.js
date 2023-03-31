9d3d0cdee73e84b8446324cf55fb1feefe720cd8
Szymon Kłos <szymon.klos@collabora.com>
2023-03-31 13:33:29 +0200

    jsdialog: make tabs accessible using keybaord
    This changes tabs into button elements so they can
    be switched using keyboard Enter key. It's also better
    from a11y point of view
    
    Styles has to be !important to override button styling
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: Ia4ced59544be18ae79d41b4f837eeb31ab80b8ef

--- browser/src/control/Control.JSDialogBuilder.js
+++ browser/src/control/Control.JSDialogBuilder.js
@@ -1060,9 +1060,10 @@ L.Control.JSDialogBuilder = L.Control.extend({
 
 				var title = builder._cleanText(item.text);
 
-				var tab = L.DomUtil.create('div', 'ui-tab ' + builder.options.cssClass, tabsContainer);
+				var tab = L.DomUtil.create('button', 'ui-tab ' + builder.options.cssClass, tabsContainer);
 				tab.id = item.name + '-tab-label';
 				tab.number = item.id - 1;
+				tab.setAttribute('tabIndex', '0');
 
 				var isSelectedTab = data.selected == item.id;
 				if (isSelectedTab) {
@@ -3036,6 +3037,8 @@ L.Control.JSDialogBuilder = L.Control.extend({
 		// natural tab-order when using keyboard navigation
 		if (control && !control.hasAttribute('tabIndex')
 			&& data.type !== 'container'
+			&& data.type !== 'tabpage'
+			&& data.type !== 'tabcontrol'
 			&& data.type !== 'grid'
 			&& data.type !== 'toolbox'
 			&& data.type !== 'listbox'
