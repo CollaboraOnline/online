2491f2974c0762d050af67c5c6cef356521e0de7
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2023-04-12 07:50:52 -0400

    wsd: announce the debug URLs to the user after server runs
    The port used for client connections does change when
    we cannot connect on the default/first one. There is
    no point in announcing the default when we don't
    yet know the actual port, and similarly it's not
    helpful to do so before the server is even started.
    
    We also move the remote-font-config thread to an
    earlier point and merge it with another #if !MOBILEAPP.
    
    Change-Id: Icc28817c7a03d49a7e130b076e545fe3ce2fb81a
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>
    (cherry picked from commit 9dd0636bcd424658e248e2e1ed83ff28b1160e7f)

--- wsd/COOLWSD.cpp
+++ wsd/COOLWSD.cpp
@@ -2682,28 +2682,6 @@ void COOLWSD::innerInitialize(Application& self)
 
     Admin::instance().setDefDocProcSettings(docProcSettings, false);
 
-#if ENABLE_DEBUG
-    std::string postMessageURI =
-        getServiceURI("/browser/dist/framed.doc.html?file_path="
-                      DEBUG_ABSSRCDIR "/" COOLWSD_TEST_DOCUMENT_RELATIVE_PATH_WRITER);
-    std::cerr << "\nLaunch one of these in your browser:\n\n"
-              << "    Writer:      " << getLaunchURI(COOLWSD_TEST_DOCUMENT_RELATIVE_PATH_WRITER) << '\n'
-              << "    Calc:        " << getLaunchURI(COOLWSD_TEST_DOCUMENT_RELATIVE_PATH_CALC) << '\n'
-              << "    Impress:     " << getLaunchURI(COOLWSD_TEST_DOCUMENT_RELATIVE_PATH_IMPRESS) << '\n'
-              << "    Draw:        " << getLaunchURI(COOLWSD_TEST_DOCUMENT_RELATIVE_PATH_DRAW) << '\n'
-              << "    postMessage: " << postMessageURI << std::endl;
-
-    const std::string adminURI = getServiceURI(COOLWSD_TEST_ADMIN_CONSOLE, true);
-    if (!adminURI.empty())
-        std::cerr << "\nOr for the admin, monitoring, capabilities & discovery:\n\n"
-                  << adminURI << '\n'
-                  << getServiceURI(COOLWSD_TEST_METRICS, true) << '\n'
-                  << getServiceURI("/hosting/capabilities") << '\n'
-                  << getServiceURI("/hosting/discovery") << '\n';
-
-    std::cerr << std::endl;
-#endif
-
 #else
     (void) self;
 #endif
@@ -5636,6 +5614,19 @@ int COOLWSD::innerMain()
     if (Log::logger().getLevel() >= Poco::Message::Priority::PRIO_INFORMATION)
         LOG_ERR("Log level is set very high to '" << LogLevel << "' this will have a "
                 "significant performance impact. Do not use this in production.");
+
+    // Start the remote font downloading polling thread.
+    std::unique_ptr<RemoteFontConfigPoll> remoteFontConfigThread;
+    try
+    {
+        // Fetch font settings from server if configured
+        remoteFontConfigThread = Util::make_unique<RemoteFontConfigPoll>(config());
+        remoteFontConfigThread->start();
+    }
+    catch (const Poco::Exception&)
+    {
+        LOG_DBG("No remote_font_config");
+    }
 #endif
 
     // URI with /contents are public and we don't need to anonymize them.
@@ -5662,19 +5653,30 @@ int COOLWSD::innerMain()
     }
 #endif
 
-#if !MOBILEAPP
-    // Start the remote font downloading polling thread.
-    std::unique_ptr<RemoteFontConfigPoll> remoteFontConfigThread;
-    try
-    {
-        // Fetch font settings from server if configured
-        remoteFontConfigThread = Util::make_unique<RemoteFontConfigPoll>(config());
-        remoteFontConfigThread->start();
-    }
-    catch (const Poco::Exception&)
-    {
-        LOG_DBG("No remote_font_config");
-    }
+#if ENABLE_DEBUG
+    std::string postMessageURI =
+        getServiceURI("/browser/dist/framed.doc.html?file_path=" DEBUG_ABSSRCDIR
+                      "/" COOLWSD_TEST_DOCUMENT_RELATIVE_PATH_WRITER);
+    std::cerr << "\nLaunch one of these in your browser:\n\n"
+              << "    Writer:      " << getLaunchURI(COOLWSD_TEST_DOCUMENT_RELATIVE_PATH_WRITER)
+              << '\n'
+              << "    Calc:        " << getLaunchURI(COOLWSD_TEST_DOCUMENT_RELATIVE_PATH_CALC)
+              << '\n'
+              << "    Impress:     " << getLaunchURI(COOLWSD_TEST_DOCUMENT_RELATIVE_PATH_IMPRESS)
+              << '\n'
+              << "    Draw:        " << getLaunchURI(COOLWSD_TEST_DOCUMENT_RELATIVE_PATH_DRAW)
+              << '\n'
+              << "    postMessage: " << postMessageURI << std::endl;
+
+    const std::string adminURI = getServiceURI(COOLWSD_TEST_ADMIN_CONSOLE, true);
+    if (!adminURI.empty())
+        std::cerr << "\nOr for the admin, monitoring, capabilities & discovery:\n\n"
+                  << adminURI << '\n'
+                  << getServiceURI(COOLWSD_TEST_METRICS, true) << '\n'
+                  << getServiceURI("/hosting/capabilities") << '\n'
+                  << getServiceURI("/hosting/discovery") << '\n';
+
+    std::cerr << std::endl;
 #endif
 
     const auto startStamp = std::chrono::steady_clock::now();
