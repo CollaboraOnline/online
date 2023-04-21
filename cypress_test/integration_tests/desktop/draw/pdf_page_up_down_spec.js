5f6a6d6f9f684152451c584121a6086f5e6d0b4c
Gökay Şatır <gokaysatir@collabora.com>
2023-04-21 11:35:07 +0300

    Required Cypress changes after replacing popups.
    Signed-off-by: Gökay Şatır <gokaysatir@collabora.com>
    Change-Id: Ice48a7c83afb9311b2eb9ebcf1d3dbccb9ad5f8b

--- cypress_test/integration_tests/desktop/draw/pdf_page_up_down_spec.js
+++ cypress_test/integration_tests/desktop/draw/pdf_page_up_down_spec.js
@@ -26,7 +26,7 @@ describe('PDF View Tests', function() {
 	it('PDF insert comment', { env: { 'pdf-view': true } }, function() {
 
 		// Insert some comment into the PDF.
-		desktopHelper.insertMultipleComment('draw', 1, true);
+		desktopHelper.insertMultipleComment('draw', 1, false);
 		cy.get('.cool-annotation-content-wrapper').should('exist');
 		cy.get('#annotation-content-area-1').should('contain','some text0');
 
