f734d1bde4ad00074c382d8cbaf2f8df1db0cd6a
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2023-04-22 12:40:06 -0400

    wsd: test: rulerupdate is an expected message
    Change-Id: I42a73914988731fd9d5bdf116d491a8a96070384
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- test/httpwstest.cpp
+++ test/httpwstest.cpp
@@ -373,6 +373,7 @@ void HTTPWSTest::testInactiveClient()
                                             token == "editor:" ||
                                             token == "context:" ||
                                             token == "window:" ||
+                                            token == "rulerupdate:" ||
                                             token == "tableselected:");
 
                     // End when we get state changed.
