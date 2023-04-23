9dc5a162581fe539b4374621bb11ad5710fade04
Patrick Luby <patrick.luby@collabora.com>
2023-04-23 14:57:04 -0400

    Fix issue #6145 by adding aliases for the PDF and EPUB icons
    The fix for issues #6103 and #6104 changes the name of these
    icons so map the new names to the old names.
    
    Signed-off-by: Patrick Luby <patrick.luby@collabora.com>
    Change-Id: I96b2678534827ce94c11edfa00718eab2a6a9eb8

--- browser/src/control/Control.JSDialogBuilder.js
+++ browser/src/control/Control.JSDialogBuilder.js
@@ -2303,6 +2303,11 @@ L.Control.JSDialogBuilder = L.Control.extend({
 			'zoterounlink': 'unlinkcitation',
 			'zoteroaddeditbibliography': 'addeditbibliography',
 			'zoterosetdocprefs': 'formproperties',
+			// Fix issue #6145 by adding aliases for the PDF and EPUB icons
+			// The fix for issues #6103 and #6104 changes the name of these
+			// icons so map the new names to the old names.
+			'downloadas-pdf': 'exportpdf',
+			'downloadas-epub': 'exportepub',
 		};
 		if (iconURLAliases[cleanName]) {
 			cleanName = iconURLAliases[cleanName];
