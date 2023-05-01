5deb47468583791d4f95359e0eb38cce87352d71
Gökay Şatır <gokaysatir@gmail.com>
2023-05-01 17:30:11 +0300

    Update impress tests.
    Signed-off-by: Gökay Şatır <gokaysatir@gmail.com>
    Change-Id: I0b47edf6e2788b8e3faaef0d0f6aad2ca033782b
    Signed-off-by: Gökay Şatır <gokaysatir@gmail.com>

--- cypress_test/integration_tests/mobile/impress/annotation_spec.js
+++ cypress_test/integration_tests/mobile/impress/annotation_spec.js
@@ -28,7 +28,7 @@ describe('Annotation tests.', function() {
 
 		mobileHelper.openCommentWizard();
 
-		cy.get('#mobile-wizard .wizard-comment-box .cool-annotation-content')
+		cy.cGet('#mobile-wizard .wizard-comment-box .cool-annotation-content')
 			.should('have.text', 'some text');
 	});
 
@@ -37,62 +37,36 @@ describe('Annotation tests.', function() {
 
 		mobileHelper.selectAnnotationMenuItem('Modify');
 
-		cy.get('.cool-annotation-table')
-			.should('exist');
-
-		cy.get('#annotation-content-area-1')
-			.should('have.text', 'some text');
-
-		cy.get('#input-modal-input').type('modified');
-
-		cy.get('#response-ok').click();
-
-		cy.get('#tb_actionbar_item_comment_wizard').click();
-
-		cy.get('#annotation-content-area-1')
-			.should('exist');
-
-		cy.get('#annotation-content-area-1')
-			.should('have.text', 'some textmodified');
+		cy.cGet('.cool-annotation-table').should('exist');
+		cy.cGet('#annotation-content-area-1').should('have.text', 'some text');
+		cy.cGet('#input-modal-input').type('modified');
+		cy.cGet('#response-ok').click();
+		cy.cGet('#tb_actionbar_item_comment_wizard').click();
+		cy.cGet('#annotation-content-area-1').should('exist');
+		cy.cGet('#annotation-content-area-1').should('have.text', 'some textmodified');
 	});
 
 	it('Remove comment.', function() {
 		mobileHelper.insertComment();
 
-		cy.get('.leaflet-marker-icon.annotation-marker')
-			.should('be.visible');
-
-		cy.get('#mobile-wizard .wizard-comment-box .cool-annotation-content')
-			.should('have.text', 'some text');
+		cy.cGet('.leaflet-marker-icon.annotation-marker').should('be.visible');
+		cy.cGet('#mobile-wizard .wizard-comment-box .cool-annotation-content').should('have.text', 'some text');
 
 		mobileHelper.selectAnnotationMenuItem('Remove');
 
-		cy.get('#mobile-wizard .wizard-comment-box .cool-annotation-content')
-			.should('not.exist');
-
-		cy.get('.leaflet-marker-icon.annotation-marker')
-			.should('not.exist');
+		cy.cGet('#mobile-wizard .wizard-comment-box .cool-annotation-content').should('not.exist');
+		cy.cGet('.leaflet-marker-icon.annotation-marker').should('not.exist');
 	});
 
 	it('Try to insert empty comment.', function() {
 		mobileHelper.openInsertionWizard();
 
-		cy.contains('.menu-entry-with-icon', 'Comment')
-			.click();
+		cy.cGet('body').contains('.menu-entry-with-icon', 'Comment').click();
 
-		cy.get('.cool-annotation-table')
-			.should('exist');
-
-		cy.get('#input-modal-input')
-			.should('have.text', '');
-
-		cy.get('#response-ok')
-			.click();
-
-		cy.get('.cool-annotation-content-wrapper.wizard-comment-box')
-			.should('not.exist');
-
-		cy.get('#mobile-wizard .wizard-comment-box .cool-annotation-content')
-			.should('not.exist');
+		cy.cGet('.cool-annotation-table').should('exist');
+		cy.cGet('#input-modal-input').should('have.text', '');
+		cy.cGet('#response-ok').click();
+		cy.cGet('.cool-annotation-content-wrapper.wizard-comment-box').should('not.exist');
+		cy.cGet('#mobile-wizard .wizard-comment-box .cool-annotation-content').should('not.exist');
 	});
 });
