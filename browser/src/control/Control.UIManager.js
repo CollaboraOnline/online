c41a3fbd91b20e1c63ff30dd0dc049e109f21349
Szymon Kłos <szymon.klos@collabora.com>
2023-05-02 11:19:57 +0200

    Allow quotes in button hint
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I9278dd0f33fda998016c47492feac239b9dccf67

--- browser/src/control/Control.UIManager.js
+++ browser/src/control/Control.UIManager.js
@@ -428,7 +428,7 @@ L.Control.UIManager = L.Control.extend({
 						uno: button.unoCommand,
 						id: button.id,
 						img: button.id,
-						hint: _(button.hint.replace('\"', '&quot;')), /* "Try" to localize ! */
+						hint: _(button.hint.replaceAll('\"', '&quot;')), /* "Try" to localize ! */
 						/* Notify the host back when button is clicked (only when unoCommand is not set) */
 						postmessage: !Object.prototype.hasOwnProperty.call(button, 'unoCommand')
 					}
