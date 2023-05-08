4aa17225f19b641a274e402761c135f8b0999a2d
Gökay Şatır <gokaysatir@gmail.com>
2023-05-08 10:03:34 +0300

    Skip some mobile tests.
    Signed-off-by: Gökay Şatır <gokaysatir@gmail.com>
    Change-Id: I964e6f1a690f074fe97607bf9a8394740044554d

--- cypress_test/integration_tests/mobile/calc/focus_spec.js
+++ cypress_test/integration_tests/mobile/calc/focus_spec.js
@@ -4,7 +4,7 @@ var helper = require('../../common/helper');
 var mobileHelper = require('../../common/mobile_helper');
 var calcHelper = require('../../common/calc_helper');
 
-describe(['tagmobile'], 'Calc focus tests', function() {
+describe.skip(['tagmobile'], 'Calc focus tests', function() {
 	var origTestFileName = 'focus.ods';
 	var testFileName;
 
