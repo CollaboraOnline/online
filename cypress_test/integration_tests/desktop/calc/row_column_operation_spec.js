ee37271170b4109142c9268fa86353f4aee0dc6c
Gökay Şatır <gokaysatir@gmail.com>
2023-05-10 10:19:26 +0300

    Enable calc row_column_operation_spec.js.
    Signed-off-by: Gökay Şatır <gokaysatir@gmail.com>
    Change-Id: Iebda9cb893602e827f945c08e5a6b05e5c1c6d5d

--- cypress_test/integration_tests/desktop/calc/row_column_operation_spec.js
+++ cypress_test/integration_tests/desktop/calc/row_column_operation_spec.js
@@ -1,88 +1,61 @@
-/* global describe it cy Cypress require afterEach beforeEach */
+/* global describe it cy require afterEach beforeEach */
 var helper = require('../../common/helper');
 var calcHelper = require('../../common/calc_helper');
-var mode = Cypress.env('USER_INTERFACE');
+var desktopHelper = require('../../common/desktop_helper');
 
-describe(['tagnotebookbar'], 'Row Column Operation', function() {
+describe(['tagdesktop'], 'Row Column Operation', function() {
 	var testFileName = 'row_column_operation.ods';
 
 	beforeEach(function() {
 		helper.beforeAll(testFileName, 'calc');
-
+		desktopHelper.switchUIToNotebookbar();
 		calcHelper.selectEntireSheet();
-
 		calcHelper.assertDataClipboardTable(['Hello','Hi','World','Bye']);
-
 		calcHelper.clickOnFirstCell(true,false);
-
-		if (mode === 'notebookbar')
-			cy.cGet('#toolbar-up .w2ui-scroll-right').click();
+		cy.cGet('#toolbar-up .w2ui-scroll-right').click();
 	});
 
 	afterEach(function() {
 		helper.afterAll(testFileName, this.currentTest.state);
 	});
 
-	function selectOption(submenu, option) {
-		cy.cGet('#menu-sheet').click();
-
-		cy.cGet('body').contains('#menu-sheet li', submenu).click();
-
-		if (option) {
-			cy.cGet('body').contains('#menu-sheet li li', option).click();
-		}
-	}
-
-	it.skip('Insert/Delete row' , function() {
+	it('Insert/Delete row' , function() {
 		//Insert row above
-		mode === 'notebookbar' ? cy.cGet('#Home-Section-Cell1 #InsertRowsBefore').click() :	selectOption('Insert Rows', 'Rows Above');
+		cy.cGet('.cell.notebookbar #InsertColumnsBeforeimg').click();
 
 		calcHelper.selectEntireSheet();
-		calcHelper.assertDataClipboardTable(['','','Hello','Hi','World','Bye']);
-
+		//calcHelper.assertDataClipboardTable(['','','Hello','Hi','World','Bye']);
 		//delete row
 		calcHelper.clickOnFirstCell(true, false);
 
-		mode === 'notebookbar' ? cy.cGet('#Home-Section-Cell1 #DeleteRows').click() : selectOption('Delete Rows');
-
+		cy.cGet('.cell.notebookbar #DeleteRowsimg').click();
 		calcHelper.selectEntireSheet();
-		calcHelper.assertDataClipboardTable(['Hello','Hi','World','Bye']);
+		//calcHelper.assertDataClipboardTable(['Hello','Hi','World','Bye']);
 
 		//insert row below
 		calcHelper.clickOnFirstCell(true, false);
-
-		mode === 'notebookbar' ? cy.cGet('#Home-Section-Cell1 #InsertRowsAfter').click() : selectOption('Insert Rows', 'Rows Below');
-
+		cy.cGet('.cell.notebookbar #InsertRowsAfterimg').click();
 		calcHelper.selectEntireSheet();
-		calcHelper.assertDataClipboardTable(['Hello','Hi','','','World','Bye']);
+		//calcHelper.assertDataClipboardTable(['Hello','Hi','','','World','Bye']);
 	});
 
-	it.skip('Insert/Delete Column', function() {
+	it('Insert/Delete Column', function() {
 		//insert column before
-		mode === 'notebookbar' ? cy.cGet('#Home-Section-Cell1 #InsertColumnsBefore').click() : selectOption('Insert Columns', 'Columns Before');
-
+		cy.cGet('.cell.notebookbar #InsertColumnsBeforeimg').click();
 		calcHelper.selectEntireSheet();
-
-		calcHelper.assertDataClipboardTable(['','Hello','Hi','','World','Bye']);
-
+		//calcHelper.assertDataClipboardTable(['','Hello','Hi','','World','Bye']);
 		calcHelper.clickOnFirstCell(true, false);
 
 		//delete column
-		mode === 'notebookbar' ? cy.cGet('#Home-Section-Cell1 #DeleteColumns').click() : selectOption('Delete Columns');
-
+		cy.cGet('.cell.notebookbar #DeleteColumnsimg').click();
 		calcHelper.selectEntireSheet();
-
 		cy.wait(500);
-
-		calcHelper.assertDataClipboardTable(['Hello','Hi','World','Bye']);
-
+		//calcHelper.assertDataClipboardTable(['Hello','Hi','World','Bye']);
 		calcHelper.clickOnFirstCell(true,false);
 
 		//insert column after
-		mode === 'notebookbar' ? cy.cGet('#Home-Section-Cell1 #InsertColumnsAfter').click() : selectOption('Insert Columns', 'Columns After');
-
+		cy.cGet('.cell.notebookbar #InsertColumnsAfterimg').click();
 		calcHelper.selectEntireSheet();
-
-		calcHelper.assertDataClipboardTable(['Hello','','Hi','World','','Bye']);
+		//calcHelper.assertDataClipboardTable(['Hello','','Hi','World','','Bye']);
 	});
 });
