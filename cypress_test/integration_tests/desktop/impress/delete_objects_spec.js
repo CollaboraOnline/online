43e701084eceea4c479b1597cf86391380f98fde
Gökay Şatır <gokaysatir@gmail.com>
2023-05-10 16:06:33 +0300

    Enable impress delete objects spec.
    Signed-off-by: Gökay Şatır <gokaysatir@gmail.com>
    Change-Id: Ib26227b453a8e21e9c5393872aaa04fe6142fd77

--- cypress_test/integration_tests/desktop/impress/delete_objects_spec.js
+++ cypress_test/integration_tests/desktop/impress/delete_objects_spec.js
@@ -1,4 +1,4 @@
-/* global describe it cy require afterEach beforeEach */
+/* global describe it cy require afterEach beforeEach expect Cypress */
 
 var helper = require('../../common/helper');
 var desktopHelper = require('../../common/desktop_helper');
@@ -15,10 +15,8 @@ describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'Delete Objects', function(
 		helper.afterAll(testFileName, this.currentTest.state);
 	});
 
-
 	it('Delete Text', function() {
 		cy.cGet('.leaflet-layer').dblclick('center');
-		cy.wait(1000);
 		cy.cGet('.leaflet-pane.leaflet-overlay-pane svg g').should('exist');
 		helper.typeIntoDocument('text');
 		helper.selectAllText();
@@ -28,86 +26,53 @@ describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'Delete Objects', function(
 		helper.textSelectionShouldNotExist();
 	});
 
-
 	it('Delete Shapes', function() {
 		cy.cGet('#toolbar-up > .w2ui-scroll-right').click();
 		//insert
 		cy.cGet('#tb_editbar_item_insertshapes').click();
-		cy.cGet('.col.w2ui-icon.symbolshapes').click();
-
-		cy.cGet('.leaflet-control-buttons-disabled path.leaflet-interactive')
-			.should('exist');
+		cy.cGet('.col.w2ui-icon.symbolshapes').should($el => { expect(Cypress.dom.isDetached($el)).to.eq(false); }).click();
+		cy.cGet('.leaflet-control-buttons-disabled path.leaflet-interactive').should('exist');
 
 		//delete
 		helper.typeIntoDocument('{del}');
-
-		cy.cGet('.leaflet-control-buttons-disabled path.leaflet-interactive')
-			.should('not.exist');
+		cy.cGet('.leaflet-control-buttons-disabled path.leaflet-interactive').should('not.exist');
 	});
-
 
 	it('Delete Chart' , function() {
 		cy.cGet('#toolbar-up > .w2ui-scroll-right').click();
 		//insert
-		cy.cGet('#tb_editbar_item_insertobjectchart')
-			.click();
-
-		cy.cGet('.leaflet-control-buttons-disabled path.leaflet-interactive')
-			.should('exist');
-
+		cy.cGet('#tb_editbar_item_insertobjectchart').click();
+		cy.cGet('.leaflet-control-buttons-disabled path.leaflet-interactive').should('exist');
 		//delete
 		helper.typeIntoDocument('{del}');
-
-		cy.cGet('.leaflet-control-buttons-disabled path.leaflet-interactive')
-			.should('not.exist');
+		cy.cGet('.leaflet-control-buttons-disabled path.leaflet-interactive').should('not.exist');
 	});
 
-	it.skip('Delete Table',function() {
+	it('Delete Table',function() {
 		desktopHelper.selectZoomLevel('50');
 
-		cy.cGet('#menu-table')
-			.click()
-			.contains('Insert Table...')
-			.click();
-
+		cy.cGet('#menu-table').click();
+		cy.cGet('body').contains('Insert Table...').click();
 		cy.cGet('.lokdialog_canvas').click();
 
 		helper.typeIntoDocument('{shift}{enter}');
 
-		cy.wait(2000);
-
 		// Table is inserted with the markers shown
-		cy.cGet('.leaflet-marker-icon.table-column-resize-marker')
-			.should('exist');
-
-		cy.cGet('path.leaflet-interactive')
-			.rightclick({force:true});
-
-		cy.contains('.context-menu-item','Delete')
-			.click();
-
-		cy.wait(1000);
-
-		cy.cGet('.leaflet-marker-icon.table-column-resize-marker')
-			.should('not.exist');
+		cy.cGet('.leaflet-marker-icon.table-column-resize-marker').should('exist');
+		cy.cGet('path.leaflet-interactive').rightclick({force:true});
+		cy.cGet('body').contains('.context-menu-item', 'Delete').click();
+		cy.cGet('.leaflet-marker-icon.table-column-resize-marker').should('not.exist');
 	});
 
 	it('Delete Fontwork', function() {
-		cy.cGet('#menu-insert')
-			.click()
-			.contains('a','Fontwork...')
-			.click();
-
-		cy.cGet('#ok')
-			.click();
-
-		cy.cGet('.leaflet-control-buttons-disabled path.leaflet-interactive')
-			.should('exist');
+		cy.cGet('#menu-insert').click();
+		cy.cGet('body').contains('a','Fontwork...').click();
+		cy.cGet('#ok').click();
+		cy.cGet('.leaflet-control-buttons-disabled path.leaflet-interactive').should('exist');
 
 		//delete
 		helper.typeIntoDocument('{del}');
 
-		cy.cGet('.leaflet-control-buttons-disabled path.leaflet-interactive')
-			.should('not.exist');
+		cy.cGet('.leaflet-control-buttons-disabled path.leaflet-interactive').should('not.exist');
 	});
 });
