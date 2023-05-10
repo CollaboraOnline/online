cecec27d5b8ffcd23432c813f468672ad00155c3
Gökay Şatır <gokaysatir@gmail.com>
2023-05-10 11:21:07 +0300

    Remove notebookbar tag and add switchUIToNotebookbar to impress desktop annotation spec.
    Signed-off-by: Gökay Şatır <gokaysatir@gmail.com>
    Change-Id: I19140e62566cd341ebb035ee0c4e43271661fb30

--- cypress_test/integration_tests/desktop/impress/annotation_spec.js
+++ cypress_test/integration_tests/desktop/impress/annotation_spec.js
@@ -5,13 +5,14 @@ var helper = require('../../common/helper');
 var { addSlide, changeSlide } = require('../../common/impress_helper');
 var { insertMultipleComment } = require('../../common/desktop_helper');
 
-describe(['tagnotebookbar'], 'Annotation Tests', function() {
+describe(['tagdesktop'], 'Annotation Tests', function() {
 	var origTestFileName = 'comment_switching.odp';
 	var testFileName;
 
 	beforeEach(function() {
 		cy.viewport(1500, 600);
 		testFileName = helper.beforeAll(origTestFileName, 'impress');
+		desktopHelper.switchUIToNotebookbar();
 
 		if (Cypress.env('INTEGRATION') === 'nextcloud') {
 			desktopHelper.hideSidebarIfVisible();
@@ -28,7 +29,6 @@ describe(['tagnotebookbar'], 'Annotation Tests', function() {
 		helper.afterAll(testFileName, this.currentTest.state);
 	});
 
-
 	it('Insert', function() {
 		insertMultipleComment('impress');
 		cy.cGet('.leaflet-marker-icon').should('exist');
@@ -37,7 +37,6 @@ describe(['tagnotebookbar'], 'Annotation Tests', function() {
 
 	it('Modify', function() {
 		insertMultipleComment('impress');
-
 		cy.cGet('.leaflet-marker-icon').should('exist');
 		cy.cGet('#annotation-content-area-1').should('contain','some text0');
 		cy.cGet('.cool-annotation-content-wrapper:visible .cool-annotation-menu').click();
@@ -51,7 +50,6 @@ describe(['tagnotebookbar'], 'Annotation Tests', function() {
 
 	it('Remove',function() {
 		insertMultipleComment('impress');
-
 		cy.cGet('.leaflet-marker-icon').should('exist');
 		cy.cGet('.cool-annotation-content > div').should('contain','some text');
 		cy.cGet('.cool-annotation-content-wrapper:visible .cool-annotation-menu').click();
@@ -71,11 +69,12 @@ describe(['tagnotebookbar'], 'Annotation Tests', function() {
 	});
 });
 
-describe(['tagnotebookbar'], 'Collapsed Annotation Tests', function() {
+describe(['tagdesktop'], 'Collapsed Annotation Tests', function() {
 	var testFileName = 'comment_switching.odp';
 
 	beforeEach(function() {
 		helper.beforeAll(testFileName, 'impress');
+		desktopHelper.switchUIToNotebookbar();
 
 		if (Cypress.env('INTEGRATION') === 'nextcloud') {
 			desktopHelper.hideSidebarIfVisible();
@@ -92,7 +91,6 @@ describe(['tagnotebookbar'], 'Collapsed Annotation Tests', function() {
 		helper.afterAll(testFileName, this.currentTest.state);
 	});
 
-
 	it('Insert', function() {
 		insertMultipleComment('impress', 1, false);
 		cy.cGet('.leaflet-marker-icon').should('exist');
@@ -101,7 +99,6 @@ describe(['tagnotebookbar'], 'Collapsed Annotation Tests', function() {
 
 	it('Modify', function() {
 		insertMultipleComment('impress', 1, false);
-
 		cy.cGet('.leaflet-marker-icon').should('exist');
 		cy.cGet('#annotation-content-area-1').should('contain','some text0');
 		cy.cGet('.avatar-img').click();
@@ -116,7 +113,6 @@ describe(['tagnotebookbar'], 'Collapsed Annotation Tests', function() {
 
 	it('Remove',function() {
 		insertMultipleComment('impress', 1, false);
-
 		cy.cGet('.leaflet-marker-icon').should('exist');
 		cy.cGet('.cool-annotation-content > div').should('contain','some text');
 		cy.cGet('.avatar-img').click();
@@ -127,7 +123,6 @@ describe(['tagnotebookbar'], 'Collapsed Annotation Tests', function() {
 
 	it('Reply',function() {
 		insertMultipleComment('impress', 1, false);
-
 		cy.cGet('.leaflet-marker-icon').should('exist');
 		cy.cGet('.cool-annotation-content > div').should('contain','some text');
 		cy.cGet('.avatar-img').click();
@@ -139,12 +134,13 @@ describe(['tagnotebookbar'], 'Collapsed Annotation Tests', function() {
 	});
 });
 
-describe(['tagnotebookbar'], 'Comment Scrolling',function() {
+describe(['tagdesktop'], 'Comment Scrolling',function() {
 	var origTestFileName = 'comment_switching.odp';
 	var testFileName;
 
 	beforeEach(function() {
 		testFileName = helper.beforeAll(origTestFileName, 'impress');
+		desktopHelper.switchUIToNotebookbar();
 
 		if (Cypress.env('USER_INTERFACE') === 'notebookbar') {
 			cy.cGet('.unospan-optionstoolboxdown.unoModifyPage').click();
@@ -159,14 +155,12 @@ describe(['tagnotebookbar'], 'Comment Scrolling',function() {
 	});
 
 	it('no comment or one comment', function() {
-		cy.wait(1000);
 		cy.cGet('.leaflet-control-scroll-down').should('not.exist');
 		insertMultipleComment('impress', 1, false);
 		cy.cGet('.leaflet-marker-icon').should('exist');
 	});
 
 	it('omit slides without comments', function() {
-		cy.wait(1000);
 		//scroll up
 		insertMultipleComment('impress', 1, false);
 		addSlide(2);
