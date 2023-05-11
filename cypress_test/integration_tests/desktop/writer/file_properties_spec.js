92e60b903511fd9bba01f8f2e81501c941ed8411
Gökay Şatır <gokaysatir@gmail.com>
2023-05-11 14:25:34 +0300

    Enable 1 desktop writer file properties test.
    Signed-off-by: Gökay Şatır <gokaysatir@gmail.com>
    Change-Id: Iad0e4959d79c2394d6174fd81e5e349460fe5246

--- cypress_test/integration_tests/desktop/writer/file_properties_spec.js
+++ cypress_test/integration_tests/desktop/writer/file_properties_spec.js
@@ -17,11 +17,8 @@ describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'File Property Tests', func
 
 	it('Add File Description.', function() {
 		writerHelper.openFileProperties();
-
 		cy.cGet('#description-tab-label').click();
-
 		helper.waitUntilIdle('#title.ui-edit');
-
 		cy.cGet('#title.ui-edit').type('New Title');
 		// sometimes it doesn't finish typing
 		helper.waitUntilIdle('#title.ui-edit');
@@ -36,13 +33,10 @@ describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'File Property Tests', func
 		cy.wait(500);
 
 		helper.waitUntilIdle('#comments.ui-textarea');
-
 		cy.cGet('#ok.ui-pushbutton').click();
-
 		writerHelper.openFileProperties();
 
 		cy.cGet('#description-tab-label').click();
-
 		cy.cGet('#title.ui-edit').should('have.value', 'New Title');
 		cy.cGet('#comments.ui-textarea').should('have.value', 'New');
 
@@ -51,27 +45,21 @@ describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'File Property Tests', func
 
 	it.skip('Add Custom Property.', function() {
 		writerHelper.openFileProperties();
-
 		cy.cGet('#customprops-tab-label').click();
-
 		cy.wait(200);
 
 		// Add property
 		helper.clickOnIdle('.ui-pushbutton', 'Add Property');
-
 		helper.waitUntilIdle('#namebox');
 		cy.cGet('#namebox select').select('Mailstop');
 
 		helper.waitUntilIdle('#valueedit');
 		cy.cGet('#valueedit').type('123 Address');
-
 		cy.cGet('#ok.ui-pushbutton').click();
 
 		// Check property saved
 		writerHelper.openFileProperties();
-
 		cy.cGet('#customprops-tab-label').click();
-
 		cy.cGet('#valueedit.ui-edit').should('have.value', '123 Address');
 
 		helper.clickOnIdle('#cancel.ui-pushbutton');
@@ -79,22 +67,16 @@ describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'File Property Tests', func
 
 	it.skip('Add Custom Duration Property.', function() {
 		writerHelper.openFileProperties();
-
 		helper.clickOnIdle('#customprops-tab-label');
-
 		cy.wait(200);
 
 		// Add property
 		helper.clickOnIdle('.ui-pushbutton', 'Add Property');
-
 		helper.waitUntilIdle('#namebox');
 		cy.cGet('#namebox select').select('Received from');
-
 		helper.waitUntilIdle('#typebox');
 		cy.cGet('#typebox select').select('Duration');
-
 		helper.clickOnIdle('#durationbutton');
-
 		cy.cGet('#negative-input').check();
 		cy.cGet('#years-input').type('1');
 		cy.cGet('#days-input').type('2');
@@ -106,41 +88,30 @@ describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'File Property Tests', func
 
 		// Check property saved
 		writerHelper.openFileProperties();
-
 		helper.clickOnIdle('#customprops-tab-label');
-
 		cy.cGet('#duration.ui-edit').should('have.value', '- Y: 1 M: 0 D: 2 H: 0 M: 0 S: 3');
-
 		helper.clickOnIdle('#cancel.ui-pushbutton');
 	});
 
 	it.skip('Add Custom Yes/No Property.', function() {
 		writerHelper.openFileProperties();
-
 		cy.cGet('#customprops-tab-label').click();
-
 		cy.wait(200);
 
 		// Add property
 		helper.clickOnIdle('.ui-pushbutton', 'Add Property');
-
 		helper.waitUntilIdle('#namebox');
 		cy.cGet('#namebox select').select('Telephone number');
-
 		helper.waitUntilIdle('#typebox');
 		cy.cGet('#typebox select').select('Yes or no');
-
 		helper.waitUntilIdle('#yes-input');
 		cy.cGet('#yes-input').check();
-
 		cy.cGet('#ok.ui-pushbutton').click();
 
 		// Check property saved
 		writerHelper.openFileProperties();
-
 		cy.cGet('#customprops-tab-label').click();
 		cy.cGet('#yes-input').should('be.checked');
-
 		helper.clickOnIdle('#cancel.ui-pushbutton');
 	});
 });
