012b00d6d4530fb7e74fac5c32696c82de028036
Szymon Kłos <szymon.klos@collabora.com>
2023-05-17 09:44:26 +0200

    linking-api: always response for thumbnail render requests #6229
    when we request target which doesn't exist or it is the first
    character in the document, we receive only one cursor invalidation
    
    in that case we didn't respond at all for the request because we
    were waiting
    
    this patch added fallback, so when we receve statechanged messages
    we know we already loaded document so we don't need to wait more
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: Ib25ab4f4d68809e3ef97ccb826ba44aeee2f71dc

--- wsd/ClientSession.cpp
+++ wsd/ClientSession.cpp
@@ -2055,6 +2055,22 @@ bool ClientSession::handleKitToClientMessage(const std::shared_ptr<Message>& pay
             handleTileInvalidation(firstLine, docBroker);
             return ret;
         }
+        else if (tokens.equals(0, "statechanged:"))
+        {
+            if (_thumbnailSession)
+            {
+                bool cursorAlreadyAtTargetPosition = getThumbnailTarget().empty();
+                if (cursorAlreadyAtTargetPosition)
+                {
+                    // fallback in case we setup target at first character in the text document,
+                    // or not existing target and we will not enter invalidatecursor second time
+                    std::ostringstream renderThumbnailCmd;
+                    auto position = getThumbnailPosition();
+                    renderThumbnailCmd << "getthumbnail x=" << position.first << " y=" << position.second;
+                    docBroker->forwardToChild(client_from_this(), renderThumbnailCmd.str());
+                }
+            }
+        }
         else if (tokens.equals(0, "invalidatecursor:"))
         {
             assert(firstLine.size() == payload->size() &&
@@ -2084,6 +2100,8 @@ bool ClientSession::handleKitToClientMessage(const std::shared_ptr<Message>& pay
                     // session used for thumbnailing and target already was set
                     if (_thumbnailSession)
                     {
+                        setThumbnailPosition(std::make_pair(x, y));
+
                         bool cursorAlreadyAtTargetPosition = getThumbnailTarget().empty();
                         if (cursorAlreadyAtTargetPosition)
                         {
