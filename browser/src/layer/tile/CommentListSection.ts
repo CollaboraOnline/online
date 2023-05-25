dacb9c595eabfbfd15fbdb90177b34e22e3259c8
Gökay Şatır <gokaysatir@gmail.com>
2023-05-25 14:54:14 +0300

    Wait for the annotation animation before editing.
    Signed-off-by: Gökay Şatır <gokaysatir@gmail.com>
    Change-Id: I76c665b2a83084955949929ecf3daf3fdd393aa4

--- browser/src/layer/tile/CommentListSection.ts
+++ browser/src/layer/tile/CommentListSection.ts
@@ -581,12 +581,25 @@ export class CommentSection extends CanvasSectionObject {
 			}.bind(this), /* isMod */ true);
 		}
 		else {
-			if (this.sectionProperties.docLayer._docType !== 'spreadsheet')
+			if (this.sectionProperties.docLayer._docType !== 'spreadsheet' && this.sectionProperties.selectedComment !== annotation) {
 				this.unselect();
+				this.select(annotation);
+			}
 
-			annotation.edit();
-			this.select(annotation);
-			annotation.focus();
+			// Make sure that comment is not transitioning and comment menu is not open.
+			var tempFunction = function() {
+				setTimeout(function() {
+					if (String(annotation.sectionProperties.container.dataset.transitioning) === 'true' || annotation.sectionProperties.contextMenu === true) {
+						tempFunction();
+					}
+					else {
+						annotation.edit();
+						this.select(annotation);
+						annotation.focus();
+					}
+				}.bind(this), 1);
+			}.bind(this);
+			tempFunction();
 		}
 	}
 
@@ -815,6 +828,7 @@ export class CommentSection extends CanvasSectionObject {
 			className: 'cool-font',
 			build: function ($trigger: any) {
 				return {
+					autoHide: true,
 					items: {
 						modify: {
 							name: _('Modify'),
@@ -858,10 +872,13 @@ export class CommentSection extends CanvasSectionObject {
 			events: {
 				show: function (options: any) {
 					options.$trigger[0].annotation.sectionProperties.contextMenu = true;
+					setTimeout(function() {
+						options.items.modify.$node[0].tabIndex = 0;
+						options.items.modify.$node[0].focus();
+					}.bind(this), 10);
 				},
 				hide: function (options: any) {
 					options.$trigger[0].annotation.sectionProperties.contextMenu = false;
-					docLayer._map.focus();
 				}
 			}
 		});
