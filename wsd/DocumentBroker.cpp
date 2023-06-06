CollaboraOnline/online

commit 418ffa39ffa18ee8172e6839663a4d43c8c13298

wsd: faster forwardToChild
The 'load' command is special-cased in the
forwardToChild function. To that end, we
had to parse the message in full, which
is very costly. This patch optimizes
this special casing logic.

Change-Id: I54ff8899b8d250ce996ddabf873a45d92e0f59b1
Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

wsd/DocumentBroker.cpp
@@ -3263,31 +3263,26 @@ bool DocumentBroker::forwardToChild(const std::string& viewId, const std::string
     if (Log::traceEnabled() && Util::startsWith(message, "paste "))
         LOG_TRC("Logging paste payload (" << message.size() << " bytes) '" << message << "' end paste");
 
-    std::string msg = "child-" + viewId + ' ' + message;
-
-    const auto it = _sessions.find(viewId);
-    if (it != _sessions.end())
+    std::string msg = "child-" + viewId + ' ';
+    if (Util::startsWith(message, "load "))
     {
-        assert(!_uriJailed.empty());
-
-        StringVector tokens = StringVector::tokenize(msg);
-        if (tokens.size() > 1 && tokens.equals(1, "load"))
+        // Special-case loading.
+        const StringVector tokens = StringVector::tokenize(message);
+        if (tokens.size() > 1 && tokens.equals(0, "load"))
         {
+            LOG_ASSERT_MSG(!_uriJailed.empty(), "Must have valid _uriJailed");
+
             // The json options must come last.
-            msg = tokens[0] + ' ' + tokens[1] + ' ' + tokens[2];
+            msg += "load " + tokens[1];
             msg += " jail=" + _uriJailed;
             msg += " xjail=" + _uriJailedAnonym;
             msg += ' ' + tokens.cat(' ', 3);
+            return _childProcess->sendFrame(msg, binary);
         }
-
-        _childProcess->sendFrame(msg, binary);
-
-        return true;
     }
 
-    // try the not yet created sessions
-    LOG_WRN("Child session [" << viewId << "] not found to forward message: " << getAbbreviatedMessage(message));
-    return false;
+    // Forward message with prefix to the Kit.
+    return _childProcess->sendFrame(msg + message, binary);
 }
 
 bool DocumentBroker::forwardToClient(const std::shared_ptr<Message>& payload)

commit 1a719370e5946969f35e28d5b54668bf52bbcf48

fix a bug introduced in 418ffa3
Signed-off-by: Jaume Pujantell <jaume.pujantell@collabora.com>
Change-Id: I938e02db8ad6adc85d6464d3260dfc8c0bb7ca9a

wsd/DocumentBroker.cpp
@@ -3525,7 +3525,7 @@ bool DocumentBroker::forwardToChild(const std::shared_ptr<ClientSession>& sessio
             msg += "load " + tokens[1];
             msg += " jail=" + _uriJailed;
             msg += " xjail=" + _uriJailedAnonym;
-            msg += ' ' + tokens.cat(' ', 3);
+            msg += ' ' + tokens.cat(' ', 2);
             return _childProcess->sendFrame(msg, binary);
         }
     }