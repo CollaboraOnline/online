4f6853ad2a8cce87e00655bca56998265a2b3ef6
Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>
2023-06-09 15:03:05 +0530

    Calc: Inconsistent behaviour when switch/reloading dark mode in Calc
    Refresh Calc header after mode change
    Signed-off-by: Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>
    Change-Id: Ifcc8c29f6b8316580570a7a9c4728965297ddc8b

--- browser/src/control/Control.RowHeader.ts
+++ browser/src/control/Control.RowHeader.ts
@@ -45,7 +45,7 @@ export class RowHeader extends cool.Header {
 
 		this._selectionBackgroundGradient = [ '#3465A4', '#729FCF', '#004586' ];
 
-		this._map.on('move zoomchanged sheetgeometrychanged splitposchanged', this._updateCanvas, this);
+		this._map.on('move zoomchanged sheetgeometrychanged splitposchanged darkmodechanged', this._updateCanvas, this);
 
 		this._initHeaderEntryStyles('spreadsheet-header-row');
 		this._initHeaderEntryHoverStyles('spreadsheet-header-row-hover');
