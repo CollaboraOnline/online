e282a33bb645265a328efc62e0d64916ee4af928
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2023-06-10 16:21:23 -0400

    wsd: signal handlers must preserve errno
    Change-Id: I13ee808f73d739c9a30724799532d04b0b8b577a
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- common/SigUtil.cpp
+++ common/SigUtil.cpp
@@ -295,6 +295,8 @@ namespace SigUtil
     static
     void handleTerminationSignal(const int signal)
     {
+        const auto onrre = errno; // Save.
+
         bool hardExit = false;
         const char *domain;
         if (!ShutdownRequestFlag && (signal == SIGINT || signal == SIGTERM))
@@ -329,8 +331,11 @@ namespace SigUtil
 #endif
 
             ::signal (signal, SIG_DFL);
+            errno = onrre; // Restore.
             ::raise (signal);
         }
+
+        errno = onrre; // Restore.
     }
 
     void requestShutdown()
