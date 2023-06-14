CollaboraOnline/online

commit 17b50868ea379b3271c4cb8e678b14f1a2725870

impress: introduce slide hiding/showing in presentation
Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
Change-Id: If91726a1a66e1679b58603e496a5b90cde5f0b5b

browser/src/control/Control.Notebookbar.js
@@ -49,6 +49,8 @@ L.Control.Notebookbar = L.Control.extend({
 		this.map.on('statusbarchanged', this.onStatusbarChange, this);
 		this.map.on('rulerchanged', this.onRulerChange, this);
 		this.map.on('darkmodechanged', this.onDarkModeToggleChange, this);
+		this.map.on('updateparts', this.onSlideHideToggle, this);
+		this.map.on('toggleslidehide', this.onSlideHideToggle, this);
 
 		this.map.sendUnoCommand('.uno:ToolbarMode?Mode:string=notebookbar_online.ui');
 
@@ -458,6 +460,18 @@ L.Control.Notebookbar = L.Control.extend({
 			contextTab.click();
 		else if (defaultTab)
 			defaultTab.click();
+	},
+
+	onSlideHideToggle: function() {
+		if (!this.map._docLayer.isHiddenSlide(this.map.getCurrentPartNumber()))
+			$('#showslide').hide();
+		else
+			$('#showslide').show();
+
+		if (this.map._docLayer.isHiddenSlide(this.map.getCurrentPartNumber()))
+			$('#hideslide').hide();
+		else
+			$('#hideslide').show();
 	},
 
 	onStatusbarChange: function() {

browser/css/partsPreviewControl.css
@@ -53,3 +53,7 @@
 .preview-img-dropsite {
 	border-bottom: 20px solid var(--color-primary);
 }
+
+.hidden-slide {
+	opacity: 0.5;
+}

browser/src/control/Control.Menubar.js
@@ -488,7 +488,10 @@ L.Control.Menubar = L.Control.extend({
 				{name: _UNO('.uno:DeleteSlide', 'presentation'), id: 'deletepage', type: 'action'},
 				{type: 'separator', id: 'fullscreen-presentation-separator'},
 				{name: _('Fullscreen presentation'), id: 'fullscreen-presentation', type: 'action'},
-				{name: _('Present current slide'), id: 'presentation-currentslide', type: 'action'}]
+				{name: _('Present current slide'), id: 'presentation-currentslide', type: 'action'},
+				{type: 'separator', id: 'slide-show-hide-separator'},
+				{name: _UNO('.uno:ShowSlide', 'presentation'), id: 'showslide', type: 'action'},
+				{name: _UNO('.uno:HideSlide', 'presentation'), id: 'hideslide', type: 'action'}]
 			},
 			{name: _UNO('.uno:ToolsMenu', 'presentation'), id: 'tools', type: 'menu', menu: [
 				{uno: '.uno:SpellDialog'},
@@ -1681,6 +1684,16 @@ L.Control.Menubar = L.Control.extend({
 						} else {
 							$(aItem).text(_('Use Tabbed view'));
 						}
+					} else if (id === 'showslide') {
+						if (!self._map._docLayer.isHiddenSlide(self._map.getCurrentPartNumber()))
+							$(aItem).hide();
+						else
+							$(aItem).show();
+					} else if (id === 'hideslide') {
+						if (self._map._docLayer.isHiddenSlide(self._map.getCurrentPartNumber()))
+							$(aItem).hide();
+						else
+							$(aItem).show();
 					} else if (self._map.getDocType() === 'presentation' && (id === 'deletepage' || id === 'insertpage' || id === 'duplicatepage')) {
 						if (id === 'deletepage') {
 							itemState = self._map['stateChangeHandler'].getItemValue('.uno:DeletePage');
@@ -1888,6 +1901,10 @@ L.Control.Menubar = L.Control.extend({
 			this._map.sendUnoCommand('.uno:LOKSidebarWriterPage');
 			this._map.fire('showwizardsidebar', {noRefresh: true});
 			window.pageMobileWizard = true;
+		} else if (id === 'showslide') {
+			this._map.showSlide();
+		} else if (id === 'hideslide') {
+			this._map.hideSlide();
 		} else if (id.indexOf('morelanguages-') != -1) {
 			this._map.fire('morelanguages', { applyto: id.substr('morelanguages-'.length) });
 		}

browser/src/control/Control.NotebookbarImpress.js
@@ -428,6 +428,18 @@ L.Control.NotebookbarImpress = L.Control.NotebookbarWriter.extend({
 				'text': _UNO('.uno:Sidebar'),
 				'command': '.uno:SidebarDeck.PropertyDeck'
 			},
+			{
+				'id': 'showslide',
+				'type': 'bigmenubartoolitem',
+				'text': _UNO('.uno:ShowSlide', 'presentation'),
+				'command': '.uno:ShowSlide'
+			},
+			{
+				'id': 'hideslide',
+				'type': 'bigmenubartoolitem',
+				'text': _UNO('.uno:HideSlide', 'presentation'),
+				'command': '.uno:HideSlide'
+			}
 		];
 
 		return this.getTabPage('View', content);

browser/src/control/Control.PartsPreview.js
@@ -139,6 +139,10 @@ L.Control.PartsPreview = L.Control.extend({
 			for (i = 0; i < parts; i++) {
 				L.DomUtil.removeClass(this._previewTiles[i], removePreviewImg);
 				L.DomUtil.addClass(this._previewTiles[i], addPreviewImg);
+				if (this._map._docLayer._hiddenSlides.has(i))
+					L.DomUtil.addClass(this._previewTiles[i], 'hidden-slide');
+				else
+					L.DomUtil.removeClass(this._previewTiles[i], 'hidden-slide');
 			}
 
 			var previewFrame = $(this._partsPreviewCont).find('.preview-frame');
@@ -268,6 +272,32 @@ L.Control.PartsPreview = L.Control.extend({
 						name: _UNO(that._map._docLayer._docType == 'presentation' ? '.uno:SlideSetup' : '.uno:PageSetup', 'presentation'),
 						callback: function() {
 							app.socket.sendMessage('uno .uno:PageSetup');
+						}
+					},
+					showslide: {
+						name: _UNO('.uno:ShowSlide', 'presentation'),
+						callback: function(key, options) {
+							var part = that._findClickedPart(options.$trigger[0].parentNode);
+							if (part !== null) {
+								that._map.showSlide(parseInt(part) - 1);
+							}
+						},
+						visible: function(key, options) {
+							var part = that._findClickedPart(options.$trigger[0].parentNode);
+							return that._map._docLayer._docType == 'presentation' && that._map._docLayer.isHiddenSlide(parseInt(part) - 1);
+						}
+					},
+					hideslide: {
+						name: _UNO('.uno:HideSlide', 'presentation'),
+						callback: function(key, options) {
+							var part = that._findClickedPart(options.$trigger[0].parentNode);
+							if (part !== null) {
+								that._map.hideSlide(parseInt(part) - 1);
+							}
+						},
+						visible: function(key, options) {
+							var part = that._findClickedPart(options.$trigger[0].parentNode);
+							return that._map._docLayer._docType == 'presentation' && !that._map._docLayer.isHiddenSlide(parseInt(part) - 1);
 						}
 					}
 				}

browser/src/control/Parts.js
@@ -445,6 +445,24 @@ L.Map.include({
 		}
 	},
 
+	hideSlide: function(slideNum) {
+		if (slideNum === undefined)
+			slideNum = this.getCurrentPartNumber();
+		L.DomUtil.addClass(this._docLayer._preview._previewTiles[slideNum], 'hidden-slide');
+		this._docLayer._hiddenSlides.add(slideNum);
+		app.socket.sendMessage('uno .uno:HideSlide');
+		this.fire('toggleslidehide');
+	},
+
+	showSlide: function(slideNum) {
+		if (slideNum === undefined)
+			slideNum = this.getCurrentPartNumber();
+		L.DomUtil.removeClass(this._docLayer._preview._previewTiles[slideNum], 'hidden-slide');
+		this._docLayer._hiddenSlides.delete(slideNum);
+		app.socket.sendMessage('uno .uno:ShowSlide');
+		this.fire('toggleslidehide');
+	},
+
 	isHiddenPart: function (part) {
 		if (this.getDocType() !== 'spreadsheet')
 			return false;

browser/src/layer/tile/ImpressTileLayer.js
@@ -3,7 +3,7 @@
  * Impress tile layer is used to display a presentation document
  */
 
-/* global app $ L */
+/* global app $ L Set */
 
 L.ImpressTileLayer = L.CanvasTileLayer.extend({
 
@@ -298,6 +298,7 @@ L.ImpressTileLayer = L.CanvasTileLayer.extend({
 			var partMatch = textMsg.match(/[^\r\n]+/g);
 			// only get the last matches
 			this._partHashes = partMatch.slice(partMatch.length - this._parts);
+			this._hiddenSlides = new Set(command.hiddenparts);
 			this._map.fire('updateparts', {
 				selectedPart: this._selectedPart,
 				selectedParts: this._selectedParts,
@@ -324,5 +325,15 @@ L.ImpressTileLayer = L.CanvasTileLayer.extend({
 	_removeHighlightSelectedWizardComment: function() {
 		if (this.lastWizardCommentHighlight)
 			this.lastWizardCommentHighlight.removeClass('impress-comment-highlight');
-	}
+	},
+
+	isHiddenSlide: function(slideNum) {
+		return this._hiddenSlides.has(slideNum);
+	},
+
+	hiddenSlides: function () {
+		if (!this._hiddenSlides)
+			return 0;
+		return this._hiddenSlides.length;
+	},
 });

browser/src/unocommands.js
@@ -222,6 +222,7 @@ var unoCommandsArray = {
 	'HideColumn':{spreadsheet:{context:_('H~ide Columns'),menu:_('~Hide'),},},
 	'HideDetail':{global:{menu:_('~Hide Details'),},},
 	'HideRow':{spreadsheet:{context:_('H~ide Rows'),menu:_('H~ide'),},},
+	'HideSlide':{presentation:{menu:_('~Hide Slide'),},},
 	'HyperlinkDialog':{global:{context:_('Insert Hyperlink'),menu:_('~Hyperlink...'),},},
 	'Hyphenate':{spreadsheet:{menu:_('~Hyphenation...'),},text:{menu:_('~Hyphenation...'),},},
 	'Hyphenation':{presentation:{menu:_('~Hyphenation'),},},
@@ -422,6 +423,7 @@ var unoCommandsArray = {
 	'ShowDetail':{global:{menu:_('~Show Details'),},},
 	'ShowResolvedAnnotations':{text:{menu:_('Resolved Comments'),},},
 	'ShowRow':{spreadsheet:{context:_('Sho~w Rows'),menu:_('~Show'),},},
+	'ShowSlide':{presentation:{menu:_('Sho~w Slide'),},},
 	'ShowTrackedChanges':{text:{context:_('Show Track Changes'),menu:_('~Show'),},},
 	'Shrink':{global:{context:_('Decrease Font Size'),menu:_('Decrease Size'),},},
 	'Sidebar':{global:{menu:_('Sidebar'),},},

commit 4cf2adbe127259d42f06089d909f670be73d1531

When Calc starts with the notebook ui, sheet tabs are missing
This patch fix a regression started from
17b50868ea379b3271c4cb8e678b14f1a2725870
"impress: introduce slide hiding/showing in presentation"

Signed-off-by: Marco Cecchetti <marco.cecchetti@collabora.com>
Change-Id: I55263b07088d16fe07171e004b5d505eb3b70500

browser/src/control/Control.Notebookbar.js
@@ -49,8 +49,10 @@ L.Control.Notebookbar = L.Control.extend({
 		this.map.on('statusbarchanged', this.onStatusbarChange, this);
 		this.map.on('rulerchanged', this.onRulerChange, this);
 		this.map.on('darkmodechanged', this.onDarkModeToggleChange, this);
-		this.map.on('updateparts', this.onSlideHideToggle, this);
-		this.map.on('toggleslidehide', this.onSlideHideToggle, this);
+		if (docType === 'presentation') {
+			this.map.on('updateparts', this.onSlideHideToggle, this);
+			this.map.on('toggleslidehide', this.onSlideHideToggle, this);
+		}
 
 		this.map.sendUnoCommand('.uno:ToolbarMode?Mode:string=notebookbar_online.ui');
 