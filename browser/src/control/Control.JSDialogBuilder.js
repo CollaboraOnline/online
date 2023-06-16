047eb57f4b98bc91304829bb1892f3429a0a556c
Henry Castro <hcastro@collabora.com>
2023-06-16 18:30:06 -0400

    browser: use 'checkValidity' for spin field element
    Prevent send data to server if not validated.
    
    Change-Id: I3a60ef52c842eab18c1e94063eb1c5769e6929b9
    Signed-off-by: Henry Castro <hcastro@collabora.com>

--- browser/src/control/Control.JSDialogBuilder.js
+++ browser/src/control/Control.JSDialogBuilder.js
@@ -430,7 +430,8 @@ L.Control.JSDialogBuilder = L.Control.extend({
 
 		spinfield.addEventListener('change', function() {
 			var attrdisabled = $(spinfield).attr('disabled');
-			if (attrdisabled !== 'disabled') {
+			var isValid = this.checkValidity();
+			if (attrdisabled !== 'disabled' && isValid) {
 				if (customCallback)
 					customCallback('spinfield', 'change', div, this.value, builder);
 				else
@@ -443,6 +444,9 @@ L.Control.JSDialogBuilder = L.Control.extend({
 
 	listenNumericChanges: function (data, builder, controls, customCallback) {
 		controls.spinfield.addEventListener('change', function() {
+			if (!this.checkValidity())
+				return;
+
 			if (customCallback)
 				customCallback();
 			else
