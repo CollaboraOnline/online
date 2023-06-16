95cf60ed5cfa841388ac71dc7c3385632d7c7f0b
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2023-06-16 06:20:21 -0400

    killpoco: use own file stat helper
    Change-Id: I668c2aab04083689a867e7d1f77e2b52c9351969
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- common/JailUtil.cpp
+++ common/JailUtil.cpp
@@ -330,7 +330,7 @@ void setupJailDevNodes(const std::string& root)
 
 #ifndef __FreeBSD__
     // Create the urandom and random devices.
-    if (!Poco::File(root + "/dev/random").exists())
+    if (!FileUtil::Stat(root + "/dev/random").exists())
     {
         LOG_DBG("Making /dev/random node in [" << root << "/dev].");
         if (mknod((root + "/dev/random").c_str(),
@@ -342,7 +342,7 @@ void setupJailDevNodes(const std::string& root)
         }
     }
 
-    if (!Poco::File(root + "/dev/urandom").exists())
+    if (!FileUtil::Stat(root + "/dev/urandom").exists())
     {
         LOG_DBG("Making /dev/urandom node in [" << root << "/dev].");
         if (mknod((root + "/dev/urandom").c_str(),
@@ -354,7 +354,7 @@ void setupJailDevNodes(const std::string& root)
         }
     }
 #else
-    if (!Poco::File(root + "/dev/random").exists())
+    if (!FileUtil::Stat(root + "/dev/random").exists())
     {
          const bool res = coolmount("-d", "", root + "/dev");
          if (res)
