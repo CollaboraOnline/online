f7c3f62d2d405755b684b97a6f35ff12c77cc800
Pedro Pinto Silva <pedro.silva@collabora.com>
2023-07-05 16:34:36 +0200

    Add missing img alt attribute to unoToolButton
    Without this commit screenreader will spit some non identified image
    blurb
    
    Signed-off-by: Pedro Pinto Silva <pedro.silva@collabora.com>
    Change-Id: Idf2cbede00127027e7a1162fc8e182728055936e

--- browser/src/control/Control.JSDialogBuilder.js
+++ browser/src/control/Control.JSDialogBuilder.js
@@ -2517,6 +2517,7 @@ L.Control.JSDialogBuilder = L.Control.extend({
 				label.htmlFor = buttonId;
 				label.textContent = builder._cleanText(data.text);
 				button.setAttribute('alt', label.textContent);
+				buttonImage.alt = label.textContent;
 				builder._stressAccessKey(label, button.accessKey);
 				controls['label'] = label;
 				$(div).addClass('has-label');
@@ -2525,6 +2526,7 @@ L.Control.JSDialogBuilder = L.Control.extend({
 			} else {
 				div.title = data.text;
 				button.setAttribute('alt', data.text);
+				buttonImage.alt = data.text;
 				builder.map.uiManager.enableTooltip(div);
 				$(div).addClass('no-label');
 			}
