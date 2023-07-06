d3af37d3be0675ef9fbbba8f6e5b510a4a5b5a8b
Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>
2023-07-06 14:34:43 +0530

    Focus back to document when Escape key pressed
     * Focus back to document when escape key pressed on any element in Notebookbar
     * Exception: When drop down is already open then focus back to element.
    Signed-off-by: Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>
    Change-Id: I4b34ae795465ecc402c5228478b7797298acc5a7

--- browser/src/control/Control.JSDialogBuilder.js
+++ browser/src/control/Control.JSDialogBuilder.js
@@ -2637,6 +2637,14 @@ L.Control.JSDialogBuilder = L.Control.extend({
 			e.stopPropagation();
 		});
 
+		div.addEventListener('keydown', function(e) {
+			switch (e.key) {
+			case 'Escape':
+				builder.map.focus();
+				break;
+			}
+		});
+
 		builder._preventDocumentLosingFocusOnClick(div);
 
 		if (data.enabled === 'false' || data.enabled === false)
