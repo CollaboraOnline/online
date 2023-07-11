65d3ac9ad775180289df6a8574b5d50876f46cfc
Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>
2023-07-11 17:26:14 +0530

    Fix "Dark Mode" Button Jumps to "Home" Tab
    After change to dark mode in notebookbar focus will stays to `view` tab.
    Signed-off-by: Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>
    Change-Id: Id0df0ded18a5858efa15eb0942b1ec69b6a716c2

--- browser/src/control/Control.UIManager.js
+++ browser/src/control/Control.UIManager.js
@@ -429,8 +429,10 @@ L.Control.UIManager = L.Control.extend({
 	},
 
 	refreshNotebookbar: function() {
+		var selectedTab = $('.ui-tab.notebookbar[aria-selected="true"]').attr('id') || 'Home-tab-label';
 		this.removeNotebookbarUI();
 		this.createNotebookbarControl(this.map.getDocType());
+		$('#' + selectedTab).click();
 		this.makeSpaceForNotebookbar();
 		this.notebookbar._showNotebookbar = true;
 		this.notebookbar.showTabs();
