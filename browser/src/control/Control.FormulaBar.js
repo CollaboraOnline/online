CollaboraOnline/online

commit 919db1674fcf00a6317ba9ccaf45ec12c532d727

keyboard: show keyboard only when explicitly requested
problem:
on mobile phone keyboard used to flicker when you tap on cell,
even if you were not editing inside cell

Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
Change-Id: I47d940ef32297fdf846ed4370411743982fcd3b7

browser/src/control/Control.FormulaBar.js
@@ -120,6 +120,7 @@ L.Control.FormulaBar = L.Control.extend({
 	onAddressInput: function(e) {
 		if (e.keyCode === 13) {
 			// address control should not have focus anymore
+			$('#addressInput').blur();
 			this.map.focus();
 			var value = L.DomUtil.get('addressInput').value;
 			var command = {
@@ -132,6 +133,7 @@ L.Control.FormulaBar = L.Control.extend({
 			this.map.sendUnoCommand('.uno:GoToCell', command);
 		} else if (e.keyCode === 27) { // 27 = esc key
 			this.map.sendUnoCommand('.uno:Cancel');
+			$('#addressInput').blur();
 			this.map.focus();
 		}
 	}

browser/src/layer/marker/TextInput.js
@@ -266,7 +266,7 @@ L.TextInput = L.Layer.extend({
 				this._textArea.setAttribute('readonly', true);
 		}
 
-		if (!window.ThisIsTheiOSApp && navigator.platform !== 'iPhone') {
+		if (!window.ThisIsTheiOSApp && navigator.platform !== 'iPhone' && !window.ThisIsAMobileApp && !window.mode.isMobile()) {
 			this._textArea.focus();
 		} else if (acceptInput === true) {
 			// On the iPhone, only call the textarea's focus() when we get an explicit

commit 2dd821c37f7c9779fe0f65ebc1f5795da51f4445

calc: blur address bar when tapped on sheet
problem:
regression from 919db16
when address input has focus and keyboard is open,
tapping on sheet would not close keyboard and bring focus back to map
only way to close keyboard and focus map was to accept the input

Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
Change-Id: I2c6f23be9efcbbe8e4d47d11505074a75abc000f

browser/src/control/Control.FormulaBar.js
@@ -120,7 +120,6 @@ L.Control.FormulaBar = L.Control.extend({
 	onAddressInput: function(e) {
 		if (e.keyCode === 13) {
 			// address control should not have focus anymore
-			$('#addressInput').blur();
 			this.map.focus();
 			var value = L.DomUtil.get('addressInput').value;
 			var command = {
@@ -133,7 +132,6 @@ L.Control.FormulaBar = L.Control.extend({
 			this.map.sendUnoCommand('.uno:GoToCell', command);
 		} else if (e.keyCode === 27) { // 27 = esc key
 			this.map.sendUnoCommand('.uno:Cancel');
-			$('#addressInput').blur();
 			this.map.focus();
 		}
 	}
@@ -188,6 +186,7 @@ L.Map.include({
 		}, 250);
 
 		map.formulabar.blurField();
+		$('#addressInput').blur();
 	}
 });
 