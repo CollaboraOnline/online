5653bf56c87a54e80bd93ae9bdb9de538754fb07
Vikas Wadhwa <mr.vikas.wadhwa@outlook.com>
2023-07-20 17:57:59 -0500

    Syntax repair

--- browser/src/core/Socket.js
+++ browser/src/core/Socket.js
@@ -993,12 +993,12 @@ app.definitions.Socket = L.Class.extend({
 		}
 		else if (textMsg.startsWith('error:') && !this._map._docLayer) {
 			textMsg = textMsg.substring(6);
-			// if (command.errorKind === 'hardlimitreached') {
+			if (command.errorKind === 'hardlimitreached') {
 
-			// 	textMsg = errorMessages.limitreachedprod;
-			// 	textMsg = textMsg.replace(/%0/g, command.params[0]);
-			// 	textMsg = textMsg.replace(/%1/g, command.params[1]);
-			// }
+				// textMsg = errorMessages.limitreachedprod;
+				// textMsg = textMsg.replace(/%0/g, command.params[0]);
+				// textMsg = textMsg.replace(/%1/g, command.params[1]);
+			}
 			else if (command.errorKind === 'serviceunavailable') {
 				textMsg = errorMessages.serviceunavailable;
 			}
