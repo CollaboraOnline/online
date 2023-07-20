80a390f740ac3b27202eb37d5ca7ea47811db4c9
Aron Budea <aron.budea@collabora.com>
2023-07-20 05:24:05 +0200

    Revert "Fix wrong device detection logic."
    On actual iPad (2048x1536 native, 1024x768 browser), in
    landscape view in Safari, mobile phone view is shown in
    online instead of tablet.
    
    This reverts commit f1e9ee72fe48d21ac7a78b7702055b91ba8f3ba9.
    
    Signed-off-by: Aron Budea <aron.budea@collabora.com>
    Change-Id: I2cd4ba6cdcd616760c06c7db479e6d13be3d83b4

--- browser/js/global.js
+++ browser/js/global.js
@@ -253,7 +253,7 @@ window.app = {
 				return true;
 			}
 
-			return L.Browser.mobile && (window.innerWidth < 768 || window.innerHeight < 768);
+			return L.Browser.mobile && (screen.width < 768 || screen.height < 768);
 		},
 		// Mobile device with big screen size.
 		isTablet: function() {
