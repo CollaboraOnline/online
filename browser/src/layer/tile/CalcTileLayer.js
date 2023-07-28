c3dabe282ec4e84e32fb2edb297d8641686824c3
Caolán McNamara <caolan.mcnamara@collabora.com>
2023-07-28 10:07:53 +0100

    calc insert comment checks undefined tab field
    and so this loop never finds anything, the intent is clearly to find
    an existing comment in this cell so use the right field for that.
    
    possibly a problem since:
    
    commit 922ae4924ad36b501eff187b57aa940bb1a02d9a
    Date:   Wed Jan 26 15:08:31 2022 +0300
    
        calc: Fix new comments do not check tabid on insert
    
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: I11208493ef7eae6a01576d90129b25ed9f792d62

--- browser/src/layer/tile/CalcTileLayer.js
+++ browser/src/layer/tile/CalcTileLayer.js
@@ -23,7 +23,7 @@ L.CalcTileLayer = L.CanvasTileLayer.extend({
 
 		for (var i = 0; i < commentList.length; i++) {
 			if (this._cellCursorTwips.contains(commentList[i].sectionProperties.data.cellPos)) {
-				if (commentList[i].tab == this._selectedPart) {
+				if (commentList[i].sectionProperties.data.tab == this._selectedPart) {
 					comment = commentList[i];
 					break;
 				}
