51094a78c2043f827a7585b2d58bcd9ac1f6f945
Henry Castro <hcastro@collabora.com>
2023-07-31 12:15:24 -0400

    browser: admin: use access token
    The GetCookie was removed due to sensitive
    access data because the browser can block to
    security concerns.
    
    Change-Id: Ifaeb495202a53125bda9e3705b6e33b7e54e56df
    Signed-off-by: Henry Castro <hcastro@collabora.com>

--- browser/admin/src/Util.js
+++ browser/admin/src/Util.js
@@ -57,17 +57,5 @@ var Util = Base.extend({
 		}
 
 		return res;
-	},
-
-	getCookie: function(name) {
-		var cookies = document.cookie.split(';');
-		for (var i = 0; i < cookies.length; i++) {
-			var cookie = cookies[i].trim();
-			if (cookie.indexOf(name) === 0) {
-				return cookie;
-			}
-		}
-
-		return '';
 	}
 });
