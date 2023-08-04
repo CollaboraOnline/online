2119a198819682dad105fde71bf2c2016a9a1533
Caolán McNamara <caolan.mcnamara@collabora.com>
2023-08-04 09:57:30 +0100

    cid#321981 AUTO_CAUSES_COPY
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: I6c2d30c8db254beb6b20aae7891b089a986a4df8

--- wsd/Admin.cpp
+++ wsd/Admin.cpp
@@ -1085,7 +1085,7 @@ void Admin::updateMonitors(std::vector<std::pair<std::string,int>>& oldMonitors)
     }
 
     // shutdown monitors which doesnot not exist in currentMonitorMap
-    for (auto monitor : oldMonitors)
+    for (const auto& monitor : oldMonitors)
     {
         if (!currentMonitorMap[monitor.first])
         {
