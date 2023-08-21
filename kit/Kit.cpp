f34efeecd373296a868c83aa433c5912c8f86b98
Michael Meeks <michael.meeks@collabora.com>
2023-08-21 16:04:03 +0100

    Add trimming to activity log in case of crash.
    Also rename trimIfExcessive to trimAfterInactivity.
    
    Signed-off-by: Michael Meeks <michael.meeks@collabora.com>
    Change-Id: I62166a3ed5d70d1e6fd9804e91e07452978523fd

--- kit/Kit.cpp
+++ kit/Kit.cpp
@@ -962,11 +962,12 @@ public:
         // FIXME: be more clever - detect if we rendered recently,
         // measure memory pressure etc.
         LOG_WRN("Sessions are all inactive - trim memory");
+        SigUtil::addActivity("trimIfInactive");
         _loKit->trimMemory(4096);
         _deltaGen.dropCache();
     }
 
-    void trimIfExcessive()
+    void trimAfterInactivity()
     {
         LOG_TRC("Should we trim our caches ?");
         if (std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now() -
@@ -985,6 +986,7 @@ public:
         if (minInactivityMs >= 9999)
         {
             LOG_DBG("Trimming Core caches");
+            SigUtil::addActivity("trimAfterInactivity");
             _loKit->trimMemory(4096);
 
             _lastMemTrimTime = std::chrono::steady_clock::now();
@@ -2402,9 +2404,7 @@ public:
         drainQueue();
 
         if (_document)
-        {
-            _document->trimIfExcessive();
-        }
+            _document->trimAfterInactivity();
 
 #if !MOBILEAPP
         flushTraceEventRecordings();
