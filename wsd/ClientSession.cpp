f1f86cfa2afe62e3dfe535b4a1cf1c49e1a84a4a
Caolán McNamara <caolan.mcnamara@collabora.com>
2023-08-23 12:34:32 +0100

    cid#318912 Result is not floating-point
    we presumably want the current behaviour so silence with
    an appropiate explicit cast
    
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: Ibed4cb2cc31b26195e407f25f7f0a2ea7172c67d

--- wsd/ClientSession.cpp
+++ wsd/ClientSession.cpp
@@ -517,7 +517,7 @@ bool ClientSession::_handleInput(const char *buffer, int length)
                 endptr = nullptr;
                 double counter = strtod(str, &endptr);
                 if (*endptr == '\0' && counter > 0 &&
-                    (counter < (double)(std::numeric_limits<uint64_t>::max() / 1000)))
+                    (counter < (uint64_t)(std::numeric_limits<uint64_t>::max() / 1000)))
                 {
                     // Now we know how to translate from the client's performance.now() values to
                     // microseconds since the epoch.
