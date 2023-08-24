CollaboraOnline/online

commit f8e53bd851a267fd50f369a1beb3c398d46f620a

Assign tab ids while defining tabs.
Signed-off-by: Gökay Şatır <gokaysatir@gmail.com>
Change-Id: I51dbadb5d168f7b7f762a20a445773c6d53a511e

browser/src/control/Control.JSDialogBuilder.js
@@ -1177,8 +1177,7 @@ L.Control.JSDialogBuilder = L.Control.extend({
 				var title = builder._cleanText(item.text);
 
 				var tab = L.DomUtil.create('button', 'ui-tab ' + builder.options.cssClass, tabsContainer);
-				tab.id = item.name + '-tab-label';
-				tab.number = item.id - 1;
+				tab.id = item.id;
 				tab.textContent = title;
 				tab.setAttribute('role', 'tab');
 				tab.setAttribute('aria-label', title);

browser/src/control/Control.Notebookbar.js
@@ -13,7 +13,7 @@ L.Control.Notebookbar = L.Control.extend({
 	container: null,
 	builder: null,
 
-	HOME_TAB_ID: '-10',
+	HOME_TAB_ID: 'Home-tab-label',
 
 	additionalShortcutButtons: [],
 	hiddenShortcutButtons: [],

browser/src/control/Control.NotebookbarCalc.js
@@ -10,7 +10,7 @@ L.Control.NotebookbarCalc = L.Control.NotebookbarWriter.extend({
 		return [
 			{
 				'text': _('~File'),
-				'id': '-1',
+				'id': 'File-tab-label',
 				'name': 'File',
 			},
 			{
@@ -21,43 +21,43 @@ L.Control.NotebookbarCalc = L.Control.NotebookbarWriter.extend({
 			},
 			{
 				'text': _('~Insert'),
-				'id': '-4',
+				'id': 'Insert-tab-label',
 				'name': 'Insert'
 			},
 			{
 				'text': _('~Layout'),
-				'id': '-3',
+				'id': 'Layout-tab-label',
 				'name': 'Layout'
 			},
 			{
 				'text': _('~Data'),
-				'id': '-5',
+				'id': 'Data-tab-label',
 				'name': 'Data'
 			},
 			{
 				'text': _('~Review'),
-				'id': '-6',
+				'id': 'Review-tab-label',
 				'name': 'Review'
 			},
 			{
 				'text': _('F~ormat'),
-				'id': '-7',
+				'id': 'Format-tab-label',
 				'name': 'Format'
 			},
 			{
 				'text': _('Dra~w'),
-				'id': '-9',
+				'id': 'Draw-tab-label',
 				'name': 'Draw',
 				'context': 'Draw|DrawLine|3DObject|MultiObject|Graphic|DrawFontwork'
 			},
 			{
 				'text': _('~View'),
-				'id': 'View',
+				'id': 'View-tab-label',
 				'name': 'View',
 			},
 			{
 				'text': _('~Help'),
-				'id': '-2',
+				'id': 'Help-tab-label',
 				'name': 'Help',
 			}
 		];

browser/src/control/Control.NotebookbarDraw.js
@@ -47,7 +47,7 @@ L.Control.NotebookbarDraw = L.Control.NotebookbarImpress.extend({
 		return [
 			{
 				'text': _('~File'),
-				'id': '-1',
+				'id': 'File-tab-label',
 				'name': 'File',
 			},
 			{
@@ -58,44 +58,44 @@ L.Control.NotebookbarDraw = L.Control.NotebookbarImpress.extend({
 			},
 			{
 				'text': _('~Insert'),
-				'id': '-4',
+				'id': 'Insert-tab-label',
 				'name': 'Insert'
 			},
 			{
 				'text': _('~Layout'),
-				'id': '-5',
+				'id': 'Layout-tab-label',
 				'name': 'Layout'
 			},
 			{
 				'text': _('~Review'),
-				'id': '-7',
+				'id': 'Review-tab-label',
 				'name': 'Review'
 			},
 			{
 				'text': _('F~ormat'),
-				'id': '-3',
+				'id': 'Format-tab-label',
 				'name': 'Format',
 			},
 			{
 				'text': _('~Table'),
-				'id': '-8',
+				'id': 'Table-tab-label',
 				'name': 'Table',
 				'context': 'Table'
 			},
 			{
 				'text': _('Dra~w'),
-				'id': '-11',
+				'id': 'Draw-tab-label',
 				'name': 'Draw',
 				'context': 'Draw|DrawLine|3DObject|MultiObject|Graphic|DrawFontwork'
 			},
 			{
 				'text': _('~View'),
-				'id': 'View',
+				'id': 'View-tab-label',
 				'name': 'View',
 			},
 			{
 				'text': _('~Help'),
-				'id': '-2',
+				'id': 'Help-tab-label',
 				'name': 'Help',
 			}
 		];

browser/src/control/Control.NotebookbarImpress.js
@@ -59,7 +59,7 @@ L.Control.NotebookbarImpress = L.Control.NotebookbarWriter.extend({
 		return [
 			{
 				'text': _('~File'),
-				'id': '-1',
+				'id': 'File-tab-label',
 				'name': 'File',
 			},
 			{
@@ -70,50 +70,50 @@ L.Control.NotebookbarImpress = L.Control.NotebookbarWriter.extend({
 			},
 			{
 				'text': _('~Insert'),
-				'id': '-4',
+				'id': 'Insert-tab-label',
 				'name': 'Insert'
 			},
 			{
 				'text': _('~Layout'),
-				'id': '-5',
+				'id': 'Layout-tab-label',
 				'name': 'Layout'
 			},
 			{
 				'text': _('~Review'),
-				'id': '-7',
+				'id': 'Review-tab-label',
 				'name': 'Review'
 			},
 			{
 				'text': _('F~ormat'),
-				'id': '-3',
+				'id': 'Format-tab-label',
 				'name': 'Format',
 			},
 			{
 				'text': _('~Table'),
-				'id': '-8',
+				'id': 'Table-tab-label',
 				'name': 'Table',
 				'context': 'Table'
 			},
 			{
 				'text': 'Dra~w',
-				'id': '-11',
+				'id': 'Draw-tab-label',
 				'name': 'Draw',
 				'context': 'Draw|DrawLine|3DObject|MultiObject|Graphic|DrawFontwork'
 			},
 			{
 				'text': _('~Master'),
-				'id': '-15',
+				'id': 'MasterPage-tab-label',
 				'name': 'MasterPage',
 				'context': 'MasterPage'
 			},
 			{
 				'text': _('~View'),
-				'id': 'View',
+				'id': 'View-tab-label',
 				'name': 'View',
 			},
 			{
 				'text': _('~Help'),
-				'id': '-2',
+				'id': 'Help-tab-label',
 				'name': 'Help',
 			}
 		];

browser/src/control/Control.NotebookbarWriter.js
@@ -10,7 +10,7 @@ L.Control.NotebookbarWriter = L.Control.Notebookbar.extend({
 		return [
 			{
 				'text': _('~File'),
-				'id': '-1',
+				'id': 'File-tab-label',
 				'name': 'File',
 			},
 			{
@@ -21,54 +21,54 @@ L.Control.NotebookbarWriter = L.Control.Notebookbar.extend({
 			},
 			{
 				'text': _('~Insert'),
-				'id': '-4',
+				'id': 'Insert-tab-label',
 				'name': 'Insert'
 			},
 			{
 				'text': _('~Layout'),
-				'id': '-5',
+				'id': 'Layout-tab-label',
 				'name': 'Layout'
 			},
 			{
 				'text': _('Reference~s'),
-				'id': '-6',
+				'id': 'References-tab-label',
 				'name': 'References'
 			},
 			{
 				'text': _('~Review'),
-				'id': '-7',
+				'id': '-Review-tab-label',
 				'name': 'Review'
 			},
 			{
 				'text': _('F~ormat'),
-				'id': '-3',
+				'id': 'Format-tab-label',
 				'name': 'Format',
 			},
 			{
 				'text': _('For~m'),
-				'id': 'Form',
+				'id': 'Form-tab-label',
 				'name': 'Form',
 			},
 			{
 				'text': _('~Table'),
-				'id': '-8',
+				'id': 'Table-tab-label',
 				'name': 'Table',
 				'context': 'Table'
 			},
 			{
 				'text': _('Dra~w'),
-				'id': '-9',
+				'id': 'Draw-tab-label',
 				'name': 'Draw',
 				'context': 'Draw|DrawLine|3DObject|MultiObject|Graphic|DrawFontwork'
 			},
 			{
 				'text': _('~View'),
-				'id': 'View',
+				'id': 'View-tab-label',
 				'name': 'View',
 			},
 			{
 				'text': _('~Help'),
-				'id': '-2',
+				'id': 'Help-tab-label',
 				'name': 'Help',
 			}
 		];
@@ -1664,7 +1664,7 @@ L.Control.NotebookbarWriter = L.Control.Notebookbar.extend({
 		return this.getTabPage('References', content);
 	},
 
-	 getReviewTab: function() {
+	getReviewTab: function() {
 		var content = [
 			{
 				'type': 'bigtoolitem',

commit 74dfa1cec52e84674c267639af0ccc30e5c84b17

Fix regression in tab cycle in dialogs
this is regression from commit:
f8e53bd851a267fd50f369a1beb3c398d46f620a
Assign tab ids while defining tabs.

When opened Format -> Page Style dialog in Writer.
There was "123" label next to the tabs.
Also when switching between widgets using tab key
it was moving focus outside window.

Core is sending id as plain number what is easy to duplicate.
Let's add some unique name.

Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
Change-Id: Iebf70c7968f336761baf19442101ac98a948218c

browser/src/control/Control.JSDialogBuilder.js
@@ -1177,7 +1177,8 @@ L.Control.JSDialogBuilder = L.Control.extend({
 				var title = builder._cleanText(item.text);
 
 				var tab = L.DomUtil.create('button', 'ui-tab ' + builder.options.cssClass, tabsContainer);
-				tab.id = item.id;
+				// avoid duplicated ids: we receive plain number from core, append prefix
+				tab.id = Number.isInteger(parseInt(item.id)) ? data.id + '-' + item.id : item.id;
 				tab.textContent = title;
 				tab.setAttribute('role', 'tab');
 				tab.setAttribute('aria-label', title);

cypress_test/integration_tests/desktop/writer/file_properties_spec.js
@@ -19,7 +19,7 @@ describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'File Property Tests', func
 
 	it('Add File Description.', function() {
 		writerHelper.openFileProperties();
-		cy.cGet('#tabcontrol #2').click();
+		cy.cGet('#tabcontrol-2').click();
 		helper.waitUntilIdle('#title.ui-edit');
 		cy.cGet('#title.ui-edit').type('New Title');
 		// sometimes it doesn't finish typing
@@ -38,7 +38,7 @@ describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'File Property Tests', func
 		cy.cGet('#ok.ui-pushbutton').click();
 		writerHelper.openFileProperties();
 
-		cy.cGet('#tabcontrol #2').click();
+		cy.cGet('#tabcontrol-2').click();
 		cy.cGet('#title.ui-edit').should('have.value', 'New Title');
 		cy.cGet('#comments.ui-textarea').should('have.value', 'New');
 