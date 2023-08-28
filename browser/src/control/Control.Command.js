CollaboraOnline/online

commit ec17f72fb516e8b68fac669f9bd0a28c1264f9a3

feature-locking: fixed broken feature unlock popup
Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
Change-Id: I5ec30ef6be17aa18ff0ec8b4ff7c6a8da831fa04

browser/src/control/Control.Command.js
@@ -62,7 +62,7 @@ L.Map.include({
 			return;
 		var message = [
 			'<div class="container">',
-			'<div id="unlock-image" class="item illustration"></div>',
+			'<img id="unlock-image" class="item illustration">',
 			'<div class="item">',
 			'<h1>' + this.Locking.unlockTitle + '</h1>',
 			'<p>' + this.Locking.unlockDescription + '<p>',
@@ -75,19 +75,28 @@ L.Map.include({
 		});
 		message.push('</ul>', '</div>', '<div>');
 
-		message = message.join();
+		message = message.join('');
 
 		this.uiManager.showInfoModal('unlock-features-popup', this.Locking.unlockTitle, ' ', ' ', _('Unlock'), function() {
 			window.open(this.Locking.unlockLink, '_blank');
 			this.uiManager.closeModal(this.uiManager.generateModalId('unlock-features-popup'));
 		}.bind(this), true);
-		document.getElementById('unlock-features-popup').querySelectorAll('p')[0].outerHTML = message;
+
+		var paraTag = document.getElementById('unlock-features-popup').querySelectorAll('p')[0];
+		if (paraTag)
+			paraTag.outerHTML = message;
+		else {
+			var popup = document.getElementById('unlock-features-popup');
+			var el = document.createElement('p');
+			popup.insertBefore(el, popup.firstChild);
+			el.outerHTML = message;
+		}
 
 		var unlockImage = L.DomUtil.get('unlock-image');
 		if (this.Locking.unlockImageUrlPath) {
-			unlockImage.style.backgroundImage = 'url(remote' + this.Locking.unlockImageUrlPath + ')';
+			unlockImage.src = this.Locking.unlockImageUrlPath;
 		} else {
-			unlockImage.style.backgroundImage = 'url(images/lock-illustration.svg)';
+			unlockImage.src = 'images/lock-illustration.svg';
 		}
 	},
 

commit 47509f0be9eb054aed7cb60ba8b76eebd6d17416

feature_lock: fix unlock_image option not working
- regression from ec17f72fb516e8b68fac669f9bd0a28c1264f9a3
- also removed the restriction to have "static" in unlock_image
  url

Signed-off-by: Rash419 <rashesh.padia@collabora.com>
Change-Id: Ie4f1170fb6a12db5218fb28f21d78f88ebc61d56

browser/src/control/Control.Command.js
@@ -62,7 +62,7 @@ L.Map.include({
 			return;
 		var message = [
 			'<div class="container">',
-			'<img id="unlock-image" class="item illustration">',
+			'<img id="unlock-image">',
 			'<div class="item">',
 			'<h1>' + this.Locking.unlockTitle + '</h1>',
 			'<p>' + this.Locking.unlockDescription + '<p>',
@@ -93,8 +93,10 @@ L.Map.include({
 		}
 
 		var unlockImage = L.DomUtil.get('unlock-image');
+		L.DomUtil.setStyle(unlockImage, 'width', '100%');
+		L.DomUtil.setStyle(unlockImage, 'height', 'auto');
 		if (this.Locking.unlockImageUrlPath) {
-			unlockImage.src = this.Locking.unlockImageUrlPath;
+			unlockImage.src = 'remote/static' + this.Locking.unlockImageUrlPath;
 		} else {
 			unlockImage.src = 'images/lock-illustration.svg';
 		}

configure.ac
@@ -1602,7 +1602,7 @@ if test "$enable_feature_lock" = "yes"; then
         <unlock_title desc=\"Title to show user when prompting the unlock popup\" default=\"$UNLOCK_TITLE\">$UNLOCK_TITLE</unlock_title>
         <unlock_link desc=\"Link from where user can unlock the features\" default=\"$UNLOCK_LINK\">$UNLOCK_LINK</unlock_link>
         <unlock_description desc=\"Description about unlocking features\" default=\"$UNLOCK_DESCRIPTION\">$UNLOCK_DESCRIPTION</unlock_description>
-        <unlock_image desc=\"URL of image shown when unlock dialog get displayed\" default=\"\"></unlock_image><!-- URL structure - https://<hostname>/static/<image_endpoint> -->
+        <unlock_image desc=\"URL of image shown when unlock dialog get displayed\" default=\"\"></unlock_image>
         <writer_unlock_highlights desc=\"Short note about Writer benefits of unlocking\" default=\"$WRITER_UNLOCK_HIGHLIGHTS\">$WRITER_UNLOCK_HIGHLIGHTS</writer_unlock_highlights>
         <calc_unlock_highlights desc=\"Short note about Calc benefits of unlocking\" default=\"$CALC_UNLOCK_HIGHLIGHTS\">$CALC_UNLOCK_HIGHLIGHTS</calc_unlock_highlights>
         <impress_unlock_highlights desc=\"Short note about Impress benefits of unlocking\" default=\"$IMPRESS_UNLOCK_HIGHLIGHTS\">$IMPRESS_UNLOCK_HIGHLIGHTS</impress_unlock_highlights>

wsd/COOLWSD.cpp
@@ -3932,7 +3932,7 @@ private:
                         {
                             const std::string& serverUri =
                                 unlockImageUri.getScheme() + "://" + unlockImageUri.getAuthority();
-                            ProxyRequestHandler::handleRequest(uri.substr(pos + ProxyRemoteLen),
+                            ProxyRequestHandler::handleRequest(uri.substr(pos + sizeof("/remote/static") - 1),
                                                                socket, serverUri);
                         }
                     }