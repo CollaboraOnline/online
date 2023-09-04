713b3424df0c787ea998f1ac04fff6ad98a1a683
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2023-09-04 06:14:30 -0400

    wsd: stop the rendering thread pool before exiting
    Change-Id: I8a084ee1fd583f6c37ba4ca8c1ebf8abe74288a2
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- kit/Kit.cpp
+++ kit/Kit.cpp
@@ -824,8 +824,7 @@ public:
             if (num_sessions == 0)
             {
                 LOG_FTL("Document [" << anonymizeUrl(_url) << "] has no more views, exiting bluntly.");
-                flushTraceEventRecordings();
-                Util::forcedExit(EX_OK);
+                flushAndExit(EX_OK);
             }
 #endif
         }
@@ -1258,8 +1257,7 @@ private:
             if (_sessions.empty())
             {
                 LOG_INF("Document [" << anonymizeUrl(_url) << "] has no more views, exiting bluntly.");
-                flushTraceEventRecordings();
-                Util::forcedExit(EX_OK);
+                flushAndExit(EX_OK);
             }
 #endif
             LOG_INF("Document [" << anonymizeUrl(_url) << "] has no more views, but has " <<
@@ -1881,6 +1879,7 @@ public:
                 {
                     LOG_INF("_stop or TerminationFlag is set, breaking Document::drainQueue of loop");
                     tileRequests.clear();
+                    _pngPool.stop();
                     break;
                 }
 
@@ -2001,16 +2000,14 @@ public:
         {
             LOG_FTL("drainQueue: Exception: " << exc.what());
 #if !MOBILEAPP
-            flushTraceEventRecordings();
-            Util::forcedExit(EX_SOFTWARE);
+            flushAndExit(EX_SOFTWARE);
 #endif
         }
         catch (...)
         {
             LOG_FTL("drainQueue: Unknown exception");
 #if !MOBILEAPP
-            flushTraceEventRecordings();
-            Util::forcedExit(EX_SOFTWARE);
+            flushAndExit(EX_SOFTWARE);
 #endif
         }
     }
@@ -2043,6 +2040,16 @@ private:
     {
         sendTextFrame(msg);
     }
+
+#if !MOBILEAPP
+    /// Stops theads, flushes buffers, and exits the process.
+    void flushAndExit(int code)
+    {
+        flushTraceEventRecordings();
+        _pngPool.stop();
+        Util::forcedExit(code);
+    }
+#endif
 
 public:
     void dumpState(std::ostream& oss)
@@ -2576,6 +2583,7 @@ protected:
 #if !MOBILEAPP
             LOG_INF("Terminating immediately due to parent 'exit' command.");
             flushTraceEventRecordings();
+            _document.reset();
             Util::forcedExit(EX_OK);
 #else
 #ifdef IOS
