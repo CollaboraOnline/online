cd5d0b9d457174b4f9d750bd418807295cafb927
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2023-09-14 07:33:01 -0400

    wsd: test: improvements to unit-load test
    This disables the unreliable reload test.
    
    On a slow box, sometimes the load takes
    a little longer than 5 seconds. Waiting
    a bit longer doesn't hurt in these cases.
    
    Also, remove duplicate test and give each
    test case its own testname.
    
    Change-Id: I556f6da9e49a7fa4cd9917bc9964dd9561b93817
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- test/UnitLoad.cpp
+++ test/UnitLoad.cpp
@@ -129,6 +129,7 @@ UnitBase::TestResult UnitLoad::testConnectNoLoad()
 
 UnitBase::TestResult UnitLoad::testLoadSimple()
 {
+    testname = __func__;
     std::string documentPath, documentURL;
     helpers::getDocumentPathAndURL("hello.odt", documentPath, documentURL, testname);
     loadDoc(documentURL, "load ");
@@ -137,6 +138,7 @@ UnitBase::TestResult UnitLoad::testLoadSimple()
 
 UnitBase::TestResult UnitLoad::testBadLoad()
 {
+    testname = __func__;
     try
     {
         // Load a document and get its status.
@@ -165,6 +167,7 @@ UnitBase::TestResult UnitLoad::testBadLoad()
 
 UnitBase::TestResult UnitLoad::testExcelLoad()
 {
+    testname = __func__;
     try
     {
         // Load a document and get status.
@@ -192,6 +195,7 @@ UnitBase::TestResult UnitLoad::testExcelLoad()
 
 UnitBase::TestResult UnitLoad::testReload()
 {
+    testname = __func__;
     std::string documentPath, documentURL;
     helpers::getDocumentPathAndURL("hello.odt", documentPath, documentURL, testname);
     for (int i = 0; i < 3; ++i)
@@ -206,6 +210,7 @@ UnitBase::TestResult UnitLoad::testReload()
 
 UnitBase::TestResult UnitLoad::testLoad()
 {
+    testname = __func__;
     std::string documentPath, documentURL;
     helpers::getDocumentPathAndURL("hello.odt", documentPath, documentURL, testname);
 
@@ -218,13 +223,13 @@ UnitBase::TestResult UnitLoad::testLoad()
     TST_LOG("Loading " << documentURL);
     wsSession->sendMessage("load url=" + documentURL);
 
-    std::vector<char> message = wsSession->waitForMessage("status:", std::chrono::seconds(5));
+    std::vector<char> message = wsSession->waitForMessage("status:", std::chrono::seconds(10));
     LOK_ASSERT_MESSAGE("Failed to load the document", !message.empty());
 
     wsSession->asyncShutdown();
 
     LOK_ASSERT_MESSAGE("Expected success disconnection of the WebSocket",
-                       wsSession->waitForDisconnection(std::chrono::seconds(5)));
+                       wsSession->waitForDisconnection(std::chrono::seconds(10)));
 
     return TestResult::Ok;
 }
@@ -243,17 +248,15 @@ void UnitLoad::invokeWSDTest()
     if (result != TestResult::Ok)
         exitTest(result);
 
-    result = testLoadSimple();
-    if (result != TestResult::Ok)
-        exitTest(result);
-
     result = testExcelLoad();
     if (result != TestResult::Ok)
         exitTest(result);
 
-    result = testReload();
-    if (result != TestResult::Ok)
-        exitTest(result);
+    // Disabling because it doesn't handle 'error: cmd=load kind=docunloading'
+    // when the document unloads. This leads to unreliable results.
+    // result = testReload();
+    // if (result != TestResult::Ok)
+    //     exitTest(result);
 
     exitTest(TestResult::Ok);
 }
