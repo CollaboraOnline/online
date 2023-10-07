246e0eb191606d296ce3c2aadbe21b7aa9275245
Rash419 <rashesh.padia@collabora.com>
2023-10-07 12:32:41 +0530

    fix: docker failed to initialize when aliasgroups is passed
    wsd-00001-00001 2023-09-07 06:57:38.412560 +0000 [ coolwsd ] FTL  Failed to initialize COOLWSD: Syntax error: Cannot convert to boolean: | wsd/COOLWSD.hpp:513
    Syntax error: Cannot convert to boolean:
    <shutdown>-00001 2023-09-07 06:57:38.413335 +0000 [ coolwsd ] SIG   Fatal signal received: SIGABRT code: 18446744073709551610 for address: 0x6400000001
    
    Signed-off-by: Rash419 <rashesh.padia@collabora.com>
    Change-Id: I446bc9b2c2172d820da2ea148eefd1d7f964be80

--- wsd/COOLWSD.cpp
+++ wsd/COOLWSD.cpp
@@ -3141,7 +3141,7 @@ void COOLWSD::initializeEnvOptions()
             {
                 const std::string path = "storage.wopi.alias_groups.group[" + std::to_string(n) + "].host";
                 _overrideSettings[path] = alias;
-                _overrideSettings[path + "[@allow]"] = true;
+                _overrideSettings[path + "[@allow]"] = "true";
                 first = false;
             }
             else
