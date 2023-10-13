b82a562460ef2e32b0d55e04c87187d0369f6383
Caolán McNamara <caolan.mcnamara@collabora.com>
2023-10-13 15:20:53 +0100

    cool#7374 very little delta threading
    We only run one task in each thread when run() is called, then complete
    the remainder of tasks in the main thread while the other threads wait.
    
    https: //github.com/CollaboraOnline/online/issues/7374
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: Id4fc0a620d98b0bb55310b495eeff7411a4544b5
    (cherry picked from commit d6c1916cb201dcd0c851b31ed776c1cbf24540a0)

--- common/RenderTiles.hpp
+++ common/RenderTiles.hpp
@@ -119,7 +119,7 @@ public:
         while (!_shutdown)
         {
             _cond.wait(lock);
-            if (!_shutdown && !_work.empty())
+            while (!_shutdown && !_work.empty())
                 runOne(lock);
         }
     }
