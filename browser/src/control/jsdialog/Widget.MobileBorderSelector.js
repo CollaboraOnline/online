56fd43689bdf89433a15797493ac4a142e17abd8
Szymon Kłos <szymon.klos@collabora.com>
2023-10-15 12:41:09 +0200

    jsdialog: move mobile border selector to separate file
    to make main JSDialogBuilder shorter and easier to read.
    And move handler to mobile builder as control exists only
    on mobile.
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I9a14c7050711658638d22f0fd7b47e038b5952a1

---
+++ browser/src/control/jsdialog/Widget.MobileBorderSelector.js
@@ -0,0 +1,112 @@
+/* -*- js-indent-level: 8 -*- */
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+/*
+ * JSDialog.MobileBorderSelector - widget for selecting border style on mobile
+ *
+ * Example JSON:
+ * {
+ *     id: 'id',
+ *     type: 'borderstyle'
+ * }
+ */
+
+/* global _ JSDialog $ */
+
+function _getCurrentBorderNumber(builder) {
+	var outer = builder.map['stateChangeHandler'].getItemValue('.uno:BorderOuter');
+	var inner = builder.map['stateChangeHandler'].getItemValue('.uno:BorderInner');
+
+	if (!outer || !inner)
+		return 0;
+
+	var left = outer.left === 'true';
+	var right = outer.right === 'true';
+	var bottom = outer.bottom === 'true';
+	var top = outer.top === 'true';
+	var horiz = inner.horizontal === 'true';
+	var vert = inner.vertical === 'true';
+
+	if (left && !right && !bottom && !top && !horiz && !vert) {
+		return 2;
+	} else if (!left && right && !bottom && !top && !horiz && !vert) {
+		return 3;
+	} else if (left && right && !bottom && !top && !horiz && !vert) {
+		return 4;
+	} else if (!left && !right && !bottom && top && !horiz && !vert) {
+		return 5;
+	} else if (!left && !right && bottom && !top && !horiz && !vert) {
+		return 6;
+	} else if (!left && !right && bottom && top && !horiz && !vert) {
+		return 7;
+	} else if (left && right && bottom && top && !horiz && !vert) {
+		return 8;
+	} else if (!left && !right && bottom && top && horiz && !vert) {
+		return 9;
+	} else if (left && right && bottom && top && horiz && !vert) {
+		return 10;
+	} else if (left && right && bottom && top && !horiz && vert) {
+		return 11;
+	} else if (left && right && bottom && top && horiz && vert) {
+		return 12;
+	}
+
+	return 1;
+}
+
+function _borderControlItem(parentContainer, data, builder, i, selected) {
+	var button = null;
+
+	var div = builder._createIdentifiable('div', 'unotoolbutton mobile-wizard ui-content unospan', parentContainer, data);
+
+	var buttonId = 'border-' + i;
+	button = L.DomUtil.create('img', 'ui-content borderbutton', div);
+	L.LOUtil.setImage(button, 'fr0' + i + '.svg', builder.map.getDocType());
+	button.id = buttonId;
+	if (selected)
+		$(button).addClass('selected');
+
+	$(div).click(function () {
+		var color = 0;
+		// Find our associated color picker
+		var item = L.LOUtil.findItemWithAttributeRecursive(data.parent, 'command', '.uno:FrameLineColor');
+		if (item)
+			color = builder._getCurrentColor(item, builder);
+		window.setBorderStyle(i, color);
+	});
+}
+
+JSDialog.mobileBorderSelector = function _frameHandler(parentContainer, data, builder) {
+	var bordercontrollabel = L.DomUtil.create('label', builder.options.cssClass + ' ui-text', parentContainer);
+	bordercontrollabel.textContent = _('Cell borders');
+	bordercontrollabel.id = data.id + 'label';
+	var current = _getCurrentBorderNumber(builder);
+	for (var i = 1; i < 13; ++i)
+		_borderControlItem(parentContainer, data, builder, i, i === current);
+
+	var updateFunction = function() {
+		var current = _getCurrentBorderNumber(builder);
+		for (var i = 1; i < 13; ++i) {
+			if (i !== current)
+				$('#border-' + i).removeClass('selected');
+			else
+				$('#border-' + i).addClass('selected');
+		}
+
+		if (builder.wizard && builder.wizard._refreshSidebar) {
+			builder.wizard._refreshSidebar(0);
+			builder.refreshSidebar = false;
+		}
+	};
+
+	builder.map.on('commandstatechanged', function(e) {
+		if (e.commandName === '.uno:BorderOuter' || e.commandName === '.uno:BorderInner')
+			updateFunction();
+	}, this);
+
+	return false;
+};
