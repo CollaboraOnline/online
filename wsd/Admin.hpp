7203a26a8aa5af972ccc7e65aa15b67fb2886bcc
Caolán McNamara <caolan.mcnamara@collabora.com>
2023-10-20 09:40:12 +0100

    cid#321979 Big parameter passed by value
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: Id514b14c6bfda08cd561d5c75ab2430b933a8919

--- wsd/Admin.hpp
+++ wsd/Admin.hpp
@@ -110,7 +110,7 @@ public:
     /// Calls with same pid will increment view count, if pid already exists
     void addDoc(const std::string& docKey, pid_t pid, const std::string& filename,
                 const std::string& sessionId, const std::string& userName, const std::string& userId,
-                const int smapsFD, const Poco::URI& wopiSrc, bool readOnly);
+                const int smapsFD, const std::string& wopiSrc, bool readOnly);
 
     /// Decrement view count till becomes zero after which doc is removed
     void rmDoc(const std::string& docKey, const std::string& sessionId);
