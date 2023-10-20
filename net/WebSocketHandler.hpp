21f826f40526f389e38e71b576f6a54d330370e0
Caolán McNamara <caolan.mcnamara@collabora.com>
2023-10-20 09:28:01 +0100

    cid#318905 Untrusted allocation size
    rearrange so coverity can see the size check
    
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: Ifad60b1a2aba2cf9c6cfbf4ea8e450b9846de576

--- net/WebSocketHandler.hpp
+++ net/WebSocketHandler.hpp
@@ -312,7 +312,7 @@ private:
             headerLen += 8;
         }
 
-        unsigned char *data, *mask = nullptr;
+        unsigned char *mask = nullptr;
 
         if (hasMask)
         {
@@ -320,7 +320,7 @@ private:
             headerLen += 4;
         }
 
-        if (payloadLen + headerLen > len)
+        if (headerLen > len || payloadLen > len - headerLen)
         { // partial read wait for more data.
             LOG_TRC("Still incomplete WebSocket frame, have "
                     << len << " bytes, frame is " << payloadLen + headerLen << " bytes");
@@ -338,7 +338,7 @@ private:
                 << len << " bytes: "
                 << Util::stringifyHexLine(socket->getInBuffer(), 0, std::min((size_t)32, len)));
 
-        data = p + headerLen;
+        unsigned char *data = p + headerLen;
 
         if (isControlFrame(code))
         {
