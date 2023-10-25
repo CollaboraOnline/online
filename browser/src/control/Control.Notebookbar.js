CollaboraOnline/online

commit 8607aa73c44ac3d80ae6440ed225e6cc674c5da0

Fix tabbed view top bar not being scrollable sometimes
Before this, when using a narrow window, the top bar wouldn't be
scrollable until we would press the avatar list. Also when switching the
modes it would becomes unscrollable yet again.

Signed-off-by: Pedro Pinto Silva <pedro.silva@collabora.com>
Change-Id: I01b2afcb125a2addf9847e2581f9498105b54b00

browser/src/control/Control.Notebookbar.js
@@ -105,7 +105,7 @@ L.Control.Notebookbar = L.Control.extend({
 		this.map.off('jsdialogupdate', this.onJSUpdate, this);
 		this.map.off('jsdialogaction', this.onJSAction, this);
 		$('.main-nav #document-header').remove();
-		$('.main-nav.hasnotebookbar').css('overflow', 'visible');
+		$('.main-nav.hasnotebookbar').css('overflow', 'scroll hidden');
 		$('.main-nav').removeClass('hasnotebookbar');
 		$('#toolbar-wrapper').removeClass('hasnotebookbar');
 		$('.main-nav').removeClass(this._map.getDocType() + '-color-indicator');

browser/css/menubar.css
@@ -87,7 +87,7 @@
 .main-nav.hasnotebookbar:not(.readonly) {
 	background: var(--color-main-background);
 	padding: 0px;
-	overflow-x: auto;
+	overflow: scroll hidden;
 }
 
 /* Customizations to sm-simple theme to make it look like LO menu, lo-menu class */

commit 16d096b8c94558e9ec2a4c20b010e828c46a54e2

browser: fix readonly document menubar not working - regression from 8607aa7
Signed-off-by: Rash419 <rashesh.padia@collabora.com>
Change-Id: Id94b98c832d8801dd9c05cdd6223bad17d202d71

browser/src/control/Control.Notebookbar.js
@@ -109,7 +109,11 @@ L.Control.Notebookbar = L.Control.extend({
 		this.map.off('jsdialogupdate', this.onJSUpdate, this);
 		this.map.off('jsdialogaction', this.onJSAction, this);
 		$('.main-nav #document-header').remove();
-		$('.main-nav.hasnotebookbar').css('overflow', 'scroll hidden');
+		if (this._map.isReadOnlyMode()) {
+			$('.main-nav.hasnotebookbar').css('overflow', 'visible');
+		} else {
+			$('.main-nav.hasnotebookbar').css('overflow', 'scroll hidden');
+		}
 		$('.main-nav').removeClass('hasnotebookbar');
 		$('#toolbar-wrapper').removeClass('hasnotebookbar');
 		$('.main-nav').removeClass(this._map.getDocType() + '-color-indicator');