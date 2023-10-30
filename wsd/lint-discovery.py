7ea7f9c61842fdde72191296527f206fa6843f46
Michael Meeks <michael.meeks@collabora.com>
2023-10-30 21:58:54 +0000

    spdx: improve machine and human readability of headers.
    Change-Id: I1b6dcd2ec1fbef6556d70b8af3ccfd5d6a95c59a
    Signed-off-by: Michael Meeks <michael.meeks@collabora.com>

--- wsd/lint-discovery.py
+++ wsd/lint-discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 #
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, you can obtain one at http://mozilla.org/MPL/2.0/.
+# Copyright the Collabora Online contributors.
+#
+# SPDX-License-Identifier: MPL-2.0
 #
 # Makes sure that discovery.xml in online.git is in sync with
 # filter/source/config/fragments/ in core.git.
