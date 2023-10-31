CollaboraOnline/online

commit 762ce28e22f4c11e0f4d7c2ba0ba66144586f11c

browser: make reconnnect experience smoother - don't invalidate the tiles - don't reload the document if serverId changes - writer: fix cursor jumps to top the document
Signed-off-by: Rash419 <rashesh.padia@collabora.com>
Change-Id: Ie42850e77f9011cd1c820115919f10283407f216

browser/src/layer/tile/CanvasTileLayer.js
@@ -1299,6 +1299,13 @@ L.CanvasTileLayer = L.Layer.extend({
 		this._update();
 	},
 
+	_refreshTilesInBackground: function() {
+		for (var key in this._tiles) {
+			this._tiles[key].wireId = 1;
+			this._tiles[key].invalidFrom = 0;
+		}
+	},
+
 	_sendClientZoom: function (forceUpdate) {
 		if (!this._map._docLoaded)
 			return;
@@ -2643,7 +2650,8 @@ L.CanvasTileLayer = L.Layer.extend({
 		textMsg = textMsg.substring('invalidatecursor:'.length + 1);
 		var obj = JSON.parse(textMsg);
 		var recCursor = this._getEditCursorRectangle(obj);
-		if (recCursor === undefined) {
+		if (recCursor === undefined || this.persistCursorPositionInWriter) {
+			this.persistCursorPositionInWriter = false;
 			return;
 		}
 		var modifierViewId = parseInt(obj.viewId);

browser/src/core/Socket.js
@@ -541,15 +541,12 @@ app.definitions.Socket = L.Class.extend({
 		var command = this.parseServerCmd(textMsg);
 		if (textMsg.startsWith('coolserver ')) {
 			// This must be the first message, unless we reconnect.
-			var oldId = null;
 			var oldVersion = null;
 			var sameFile = true;
 			// Check if we are reconnecting.
 			if (this.WSDServer && this.WSDServer.Id) {
 				// Yes we are reconnecting.
-				// If server is restarted, we have to refresh the page.
 				// If our connection was lost and is ready again, we will not need to refresh the page.
-				oldId = this.WSDServer.Id;
 				oldVersion = this.WSDServer.Version;
 
 				window.app.console.assert(this._map.options.wopiSrc === window.wopiSrc,
@@ -563,8 +560,8 @@ app.definitions.Socket = L.Class.extend({
 
 			this.WSDServer = JSON.parse(textMsg.substring(textMsg.indexOf('{')));
 
-			if (oldId && oldVersion && sameFile) {
-				if ((this.WSDServer.Id !== oldId || this.WSDServer.Version !== oldVersion) && !window.migrating && !this.IndirectSocketReconnectCount) {
+			if (oldVersion && sameFile) {
+				if (this.WSDServer.Version !== oldVersion) {
 					var reloadMessage = _('Server is now reachable. We have to refresh the page now.');
 					if (window.mode.isMobile())
 						reloadMessage = _('Server is now reachable...');
@@ -575,10 +572,6 @@ app.definitions.Socket = L.Class.extend({
 					else
 						this._map.fire('postMessage', {msgId: 'Reloading', args: {Reason: 'Reconnected'}});
 					setTimeout(reloadFunc, 5000);
-				}
-				var docType = this._map.getDocType();
-				if (docType === 'presentation' || docType === 'drawing' || docType === 'spreadsheet') {
-					this._map._docLayer._reconnected = true;
 				}
 			}
 			if (window.indirectSocket) {
@@ -1393,13 +1386,15 @@ app.definitions.Socket = L.Class.extend({
 		}
 		else if (this._reconnecting) {
 			// we are reconnecting ...
-			this._reconnecting = false;
-			this._map._docLayer._resetClientVisArea();
-			this._map._docLayer._requestNewTiles();
+			this._map._docLayer._refreshTilesInBackground();
 			this._map.fire('statusindicator', {statusType: 'reconnected'});
 			this._map._isNotebookbarLoadedOnCore = false;
 			var uiMode = this._map.uiManager.getCurrentMode();
-			this._map.fire('changeuimode', {mode: uiMode, force: !window.migrating});
+			if (uiMode === 'notebookbar') {
+				this._map.sendUnoCommand('.uno:ToolbarMode?Mode:string=notebookbar_online.ui');
+			}
+			// close all the popups otherwise document textArea will not get focus
+			this._map.uiManager.closeAll();
 			this._map.setPermission(app.file.permission);
 			window.migrating = false;
 		}
@@ -1407,6 +1402,7 @@ app.definitions.Socket = L.Class.extend({
 		this._map.fire('docloaded', {status: true});
 		if (this._map._docLayer) {
 			this._map._docLayer._onMessage(textMsg);
+			this._reconnecting = false;
 		}
 	},
 
@@ -1530,8 +1526,13 @@ app.definitions.Socket = L.Class.extend({
 		if (this._map._docLayer) {
 			this._map._docLayer.removeAllViews();
 			this._map._docLayer._resetClientVisArea();
-			this._map._docLayer._graphicSelection = new L.LatLngBounds(new L.LatLng(0, 0), new L.LatLng(0, 0));
-			this._map._docLayer._onUpdateGraphicSelection();
+			var graphicSelection = new L.LatLngBounds(new L.LatLng(0, 0), new L.LatLng(0, 0));
+			if (!this._map._docLayer._graphicSelection.equals(graphicSelection)) {
+				this._map._docLayer._graphicSelection = graphicSelection;
+				this._map._docLayer._onUpdateGraphicSelection();
+			}
+			if (this._map._docLayer._docType === 'presentation')
+				this._map._isCursorVisible = false;
 		}
 
 		if (isActive && this._reconnecting) {

browser/src/layer/marker/TextInput.js
@@ -1432,7 +1432,7 @@ L.TextInput = L.Layer.extend({
 			return;
 		window.app.console.log(msg);
 	},
-	
+
 	_statusLog: function(header) {
 		if (!this._isDebugOn)
 			return;

browser/src/layer/tile/CalcTileLayer.js
@@ -385,9 +385,8 @@ L.CalcTileLayer = L.CanvasTileLayer.extend({
 			app.view.size.pixels = app.file.size.pixels.slice();
 			this._docType = command.type;
 			this._parts = command.parts;
-			if (this._reconnected) {
+			if (app.socket._reconnecting) {
 				app.socket.sendMessage('setclientpart part=' + this._selectedPart);
-				this._map._reconnected = false;
 			} else {
 				this._selectedPart = command.selectedPart;
 			}

browser/src/layer/tile/ImpressTileLayer.js
@@ -255,9 +255,8 @@ L.ImpressTileLayer = L.CanvasTileLayer.extend({
 			this._updateMaxBounds(true);
 			this._documentInfo = textMsg;
 			this._viewId = parseInt(command.viewid);
-			if (this._reconnected) {
+			if (app.socket._reconnecting) {
 				app.socket.sendMessage('setclientpart part=' + this._selectedPart);
-				this._map._reconnected = false;
 			} else {
 				this._selectedPart = command.selectedPart;
 				this._selectedParts = command.selectedParts || [command.selectedPart];

browser/src/layer/tile/WriterTileLayer.js
@@ -130,6 +130,15 @@ L.WriterTileLayer = L.CanvasTileLayer.extend({
 
 	_onStatusMsg: function (textMsg) {
 		var command = app.socket.parseServerCmd(textMsg);
+		if (app.socket._reconnecting) {
+			// persist cursor position on reconnection
+			// In writer, core always sends the cursor coordinates
+			// of the first paragraph of the document so we want to ignore that
+			// to eliminate document jumping while reconnecting
+			this.persistCursorPositionInWriter = true;
+			this._postMouseEvent('buttondown', this.lastCursorPos.x, this.lastCursorPos.y, 1, 1, 0);
+			this._postMouseEvent('buttonup', this.lastCursorPos.x, this.lastCursorPos.y, 1, 1, 0);
+		}
 		if (!command.width || !command.height || this._documentInfo === textMsg)
 			return;
 

commit fd14482acc05d50740156d2cc7e6c484f61326ee

browser: wireId out of sync on reconnect - regression from 762ce28e22f4c11e0f4d7c2ba0ba66144586f11c
Signed-off-by: Rash419 <rashesh.padia@collabora.com>
Change-Id: I86b913213135982031d33b4b29f54df2e8320ea4

browser/src/layer/tile/CanvasTileLayer.js
@@ -1306,7 +1306,7 @@ L.CanvasTileLayer = L.Layer.extend({
 	_refreshTilesInBackground: function() {
 		for (var key in this._tiles) {
 			this._tiles[key].wireId = 1;
-			this._tiles[key].invalidFrom = 0;
+			this._tiles[key].invalidFrom = 1;
 		}
 	},
 