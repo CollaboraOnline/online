CollaboraOnline/online

commit 46c1248c770c2409ebb3b38c796690220ea98880

Prevent right mouse button up event to click on menu item.
In some case[*] when we right click on row/col header and release mouse button,
first element of the context menu is invoked accidentally. To prevent this,
just prevent the right mouseup event on context menu item.

[*] 1920x1080 resolution with %75 browser zoom

Signed-off-by: Gülşah Köse <gulsah.kose@collabora.com>
Change-Id: I28299e7a7cf83eaed27ef4ed6c3555fb2cc80682

browser/src/control/Control.ColumnHeader.ts
@@ -143,6 +143,8 @@ export class ColumnHeader extends Header {
 		this.context.strokeStyle = this._borderColor;
 		this.context.lineWidth = app.dpiScale;
 		this.context.strokeRect(startX - 0.5, 0.5, entry.size, this.size[1]);
+
+        this._map._contextMenu.stopRightMouseUpEvent();
 	}
 
 	getHeaderEntryBoundingClientRect (index: number): Partial<DOMRect> {

browser/src/control/Control.RowHeader.ts
@@ -140,6 +140,8 @@ export class RowHeader extends cool.Header {
 		this.context.strokeStyle = this._borderColor;
 		this.context.lineWidth = app.dpiScale;
 		this.context.strokeRect(0.5, startY - 0.5, this.size[0], entry.size);
+
+        this._map._contextMenu.stopRightMouseUpEvent();
 	}
 
 	getHeaderEntryBoundingClientRect (index: number): Partial<DOMRect> {

browser/src/control/Control.ContextMenu.js
@@ -296,7 +296,19 @@ L.Control.ContextMenu = L.Control.extend({
 		}
 
 		return contextMenu;
-	}
+	},
+
+    // Prevents right mouse button's mouseup event from triggering menu item accidentally.
+    stopRightMouseUpEvent: function() {
+        var menuItems = document.getElementsByClassName('context-menu-item');
+
+        for (var i = 0 ; i < menuItems.length; i++) {
+            menuItems[i].addEventListener('mouseup', function(e) {
+                if (e.button == 2) // Is a right mouse button event?
+                    e.stopPropagation();
+            });
+        }
+    }
 });
 
 L.control.contextMenu = function (options) {

commit 1a5ec434f250bcf14f6d17b59e875f3caa29d11e

perf: do not call addEventListener on every header update
This helps with performance of typing into spreadsheets.

Regression from commit 46c1248c770c2409ebb3b38c796690220ea98880
Prevent right mouse button up event to click on menu item.

Do not add new event listener on every header update what
happens often but rather apply right click blocker when
context menu is opened. Original commit was trying to prevent
menu items activation on right click.

Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
Change-Id: If468703a4728d12238c5748f182453cd2b8812c9

browser/src/control/Control.ColumnHeader.ts
@@ -143,8 +143,6 @@ export class ColumnHeader extends Header {
 		this.context.strokeStyle = this._borderColor;
 		this.context.lineWidth = app.dpiScale;
 		this.context.strokeRect(startX - 0.5, 0.5, entry.size, this.size[1]);
-
-        this._map._contextMenu.stopRightMouseUpEvent();
 	}
 
 	getHeaderEntryBoundingClientRect (index: number): Partial<DOMRect> {

browser/src/control/Control.Header.ts
@@ -477,6 +477,7 @@ export class Header extends CanvasSectionObject {
 			callback: function() { return; }
 		});
 		$('#document-canvas').contextMenu('update');
+		this._map._contextMenu.stopRightMouseUpEvent();
 	}
 
 	_unBindContextMenu(): void {

browser/src/control/Control.RowHeader.ts
@@ -140,8 +140,6 @@ export class RowHeader extends cool.Header {
 		this.context.strokeStyle = this._borderColor;
 		this.context.lineWidth = app.dpiScale;
 		this.context.strokeRect(0.5, startY - 0.5, this.size[0], entry.size);
-
-        this._map._contextMenu.stopRightMouseUpEvent();
 	}
 
 	getHeaderEntryBoundingClientRect (index: number): Partial<DOMRect> {