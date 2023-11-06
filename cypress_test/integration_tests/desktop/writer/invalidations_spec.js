7229eed150b2bfec3ef319e1f4633a15f404e0e8
Caolán McNamara <caolan.mcnamara@collabora.com>
2023-11-06 17:07:41 +0000

    add regression tests via cypress for unwanted no-change invalidations
    single user scenarios
    
    a) Click Empty Header: fails with core:
    https://cgit.freedesktop.org/libreoffice/core/commit/?id=f425c0aab6e33b8029bf7e87ac9b10b4b4489ac9
    reverted
    
    b) Enter Numbering Portion: fails with core:
    https://cgit.freedesktop.org/libreoffice/core/commit/?id=643c6b9a96020d5498d981c7ab081226415ab016
    reverted
    
    c) Click Existing Header: fails with core:
    https://cgit.freedesktop.org/libreoffice/core/commit/?id=ce948053b1fa9a2843886dce4642cc36c94dd0e0
    reverted
    
    https://github.com/CollaboraOnline/online/issues/7167
    
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: I7d7283cabe6803d9258a09a858beb29083634738

---
+++ cypress_test/integration_tests/desktop/writer/invalidations_spec.js
@@ -0,0 +1,124 @@
+/* global describe it cy beforeEach expect require afterEach*/
+
+var helper = require('../../common/helper');
+var desktopHelper = require('../../common/desktop_helper');
+var ceHelper = require('../../common/contenteditable_helper');
+var writerHelper = require('../../common/writer_helper');
+
+describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'Invalidation tests.', function() {
+	var origTestFileName = 'invalidations.odt';
+	var testFileName;
+
+	beforeEach(function() {
+		testFileName = helper.beforeAll(origTestFileName, 'writer');
+		desktopHelper.switchUIToNotebookbar();
+		cy.cGet('div.clipboard').as('clipboard');
+	});
+
+	afterEach(function() {
+		helper.afterAll(testFileName, this.currentTest.state);
+	});
+
+	// Clicking in an empty header area shouldn't invalidate anything
+	it('Click Empty Header.', function() {
+
+		// Add some main body text of X
+		ceHelper.type('X');
+		cy.cGet('#tb_actionbar_item_StateWordCount').should('have.text', '    1 word, 1 character');
+
+		cy.cGet('.empty-deltas').then(($before) => {
+			const beforeCount = $before.text();
+
+			// Click in header area (there is no actual header, We are testing that nothing
+			// happens if there is no header in the document)
+			cy.cGet('.leaflet-layer').click(200, 50);
+
+			// Wait until we have round trip of selection of 'X' and tile updates will have arrived.
+			writerHelper.selectAllTextOfDoc();
+			cy.cGet('#tb_actionbar_item_StateWordCount').should('have.text', '    Selected: 1 word, 1 character');
+
+			cy.cGet('.empty-deltas').should(($after) => {
+				expect($after.text()).to.eq(beforeCount);
+			});
+		});
+	});
+
+	// Clicking in an existing header area shouldn't result in useless invalidations
+	it('Click Existing Header.', function() {
+
+		// Add some main body text of X
+		ceHelper.type('X');
+
+		// Add a header with YY in it
+		cy.cGet('#Insert-tab-label').click();
+		cy.cGet('.cell.notebookbar > .unoInsertPageHeader > button').click();
+		ceHelper.type('YY');
+
+		// Click back in main document
+		cy.cGet('.leaflet-layer').click(200, 200);
+		writerHelper.selectAllTextOfDoc();
+		cy.cGet('#tb_actionbar_item_StateWordCount').should('have.text', '    Selected: 1 word, 1 character');
+
+		cy.cGet('.empty-deltas').then(($before) => {
+			const beforeCount = $before.text();
+
+			// click in header area
+			cy.cGet('.leaflet-layer').click(200, 50);
+
+			// verify the content is 'YY'
+			writerHelper.selectAllTextOfDoc();
+			cy.cGet('#tb_actionbar_item_StateWordCount').should('have.text', '    Selected: 1 word, 2 characters');
+
+			// verify empty deltas is unchanged
+			cy.cGet('.empty-deltas').should(($after) => {
+				expect($after.text()).to.eq(beforeCount);
+			});
+		});
+
+		cy.cGet('.empty-deltas').then(($before) => {
+			const beforeCount = $before.text();
+
+			// click in main document
+			cy.cGet('.leaflet-layer').click(200, 200);
+
+			// verify the content is 'X'
+			writerHelper.selectAllTextOfDoc();
+			cy.cGet('#tb_actionbar_item_StateWordCount').should('have.text', '    Selected: 1 word, 1 character');
+
+			// verify empty deltas is unchanged
+			cy.cGet('.empty-deltas').should(($after) => {
+				expect($after.text()).to.eq(beforeCount);
+			});
+		});
+	});
+
+	// Entering a bullet portion shouldn't invalidate anything
+	it('Enter Numbering Portion.', function() {
+
+		// Add some main body text of X and bullet
+		ceHelper.type('XX');
+		cy.cGet('.cell.notebookbar > .unoDefaultBullet > button').click();
+		cy.cGet('#tb_actionbar_item_StateWordCount').should('have.text', '    2 words, 3 characters');
+
+		cy.cGet('.empty-deltas').then(($before) => {
+			const beforeCount = $before.text();
+
+			// move caret before 'X' and after bullet
+			ceHelper.moveCaret('home');
+
+			// enter numbering portion, this used to invalidate
+			ceHelper.moveCaret('left');
+
+			// leave numbering portion, this used to invalidate
+			ceHelper.moveCaret('end');
+
+			ceHelper.moveCaret('left', 'shift');
+			cy.cGet('#tb_actionbar_item_StateWordCount').should('have.text', '    Selected: 1 word, 1 character');
+
+			cy.cGet('.empty-deltas').should(($after) => {
+				expect($after.text()).to.eq(beforeCount);
+			});
+		});
+	});
+
+});
