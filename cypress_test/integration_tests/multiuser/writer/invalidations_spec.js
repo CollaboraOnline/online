8535065fe5ac0f5b4f9cd69f0a0d0c7424ed5119
Caolán McNamara <caolan.mcnamara@collabora.com>
2023-11-13 14:29:26 +0000

    add regression tests via cypress for unwanted no-change invalidations
    multi-user scenarios
    
    d) Rejoin triggers invalidations: fails with core:
    https://cgit.freedesktop.org/libreoffice/core/commit/?id=f1633dc0e06b046a9ca0f2fa53e7858be1584ffb
    reverted
    
    e) Rejoin after save triggers constant invalidations: fails with core:
    https://cgit.freedesktop.org/libreoffice/core/commit/?id=770df258bc0a66ee7551a59807d79cebcdcbb85e
    reverted
    
    https://github.com/CollaboraOnline/online/issues/7167
    
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: I27795027a1bd186953c71807c5cfc35685af0e70

---
+++ cypress_test/integration_tests/multiuser/writer/invalidations_spec.js
@@ -0,0 +1,108 @@
+/* global describe it cy beforeEach expect require afterEach */
+
+var helper = require('../../common/helper');
+var desktopHelper = require('../../common/desktop_helper');
+var ceHelper = require('../../common/contenteditable_helper');
+var writerHelper = require('../../common/writer_helper');
+
+describe(['tagmultiuser'], 'Joining a document should not trigger an invalidation', function() {
+	var origTestFileName = 'invalidations.odt';
+	var testFileName;
+
+	beforeEach(function() {
+		testFileName = helper.beforeAll(origTestFileName, 'writer', undefined, true);
+		desktopHelper.switchUIToNotebookbar();
+	});
+
+	afterEach(function() {
+		helper.afterAll(testFileName, this.currentTest.state);
+	});
+
+	it('Join document', function() {
+		cy.cSetActiveFrame('#iframe1');
+		cy.cGet('div.clipboard').as('clipboard');
+
+		// Disable Grammar (and SpellChecking)
+		// TODO: Grammar checking, when available, adds an extra empty update when
+		// it kicks in after a change
+		cy.cGet('#Review-tab-label').click();
+		cy.cGet('.cell.notebookbar > .unoSpellOnline > button').click();
+
+		ceHelper.type('X');
+
+		cy.cSetActiveFrame('#iframe2');
+		cy.cGet('#tb_actionbar_item_StateWordCount').should('have.text', '    1 word, 1 character');
+
+		cy.cSetActiveFrame('#iframe1');
+		writerHelper.selectAllTextOfDoc();
+		cy.cGet('#tb_actionbar_item_StateWordCount').should('have.text', '    Selected: 1 word, 1 character');
+		cy.cGet('.leaflet-layer').click();
+		cy.cGet('#tb_actionbar_item_StateWordCount').should('have.text', '    1 word, 1 character');
+
+		cy.cGet('.empty-deltas').then(($before) => {
+			const beforeCount = $before.text();
+
+			// joining triggered a theme related invalidation
+			cy.cSetActiveFrame('#iframe2');
+			cy.get('#form2').submit();
+
+			cy.cSetActiveFrame('#iframe1');
+			writerHelper.selectAllTextOfDoc();
+			cy.cGet('#tb_actionbar_item_StateWordCount').should('have.text', '    Selected: 1 word, 1 character');
+			cy.cGet('.leaflet-layer').click();
+			cy.cGet('#tb_actionbar_item_StateWordCount').should('have.text', '    1 word, 1 character');
+
+			cy.cGet('.empty-deltas').should(($after) => {
+				expect($after.text()).to.eq(beforeCount);
+			});
+		});
+	});
+
+	it('Join after document save and modify', function() {
+		cy.cSetActiveFrame('#iframe1');
+		cy.cGet('div.clipboard').as('clipboard');
+
+		// Disable Grammar (and SpellChecking)
+		// TODO: Grammar checking, when available, adds an extra empty update when
+		// it kicks in after a change
+		cy.cGet('#Review-tab-label').click();
+		cy.cGet('.cell.notebookbar > .unoSpellOnline > button').click();
+
+		ceHelper.type('X');
+
+		cy.cSetActiveFrame('#iframe2');
+		cy.cGet('#tb_actionbar_item_StateWordCount').should('have.text', '    1 word, 1 character');
+
+		cy.cSetActiveFrame('#iframe1');
+		writerHelper.selectAllTextOfDoc();
+		cy.cGet('#tb_actionbar_item_StateWordCount').should('have.text', '    Selected: 1 word, 1 character');
+		cy.cGet('.leaflet-layer').click();
+		cy.cGet('#tb_actionbar_item_StateWordCount').should('have.text', '    1 word, 1 character');
+
+		cy.cGet('.empty-deltas').then(($before) => {
+			var beforeCount = parseInt($before.text());
+
+			// joins after a save triggered excessive invalidations on changes
+			cy.cGet('#File-tab-label').click();
+			cy.cGet('.cell.notebookbar > .unoSave > button').click();
+
+			cy.cSetActiveFrame('#iframe2');
+			cy.get('#form2').submit();
+
+			cy.cSetActiveFrame('#iframe1');
+			writerHelper.selectAllTextOfDoc();
+			cy.cGet('#tb_actionbar_item_StateWordCount').should('have.text', '    Selected: 1 word, 1 character');
+			cy.cGet('.leaflet-layer').click();
+			cy.cGet('#tb_actionbar_item_StateWordCount').should('have.text', '    1 word, 1 character');
+			ceHelper.type('X');
+			cy.cGet('#tb_actionbar_item_StateWordCount').should('have.text', '    1 word, 2 characters');
+
+			cy.cGet('.empty-deltas').should(($after) => {
+				// allow one row of empty deltas, the case this protects regression against
+				// is a whole document invalidation
+				expect(parseInt($after.text())).to.be.lessThan(beforeCount + 10);
+			});
+		});
+	});
+
+});
