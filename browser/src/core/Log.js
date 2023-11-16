87d49cdd747a3827306998b49315e61f037d7d04
Michael Meeks <michael.meeks@collabora.com>
2023-11-16 15:01:51 +0000

    Logging - dump our log history to the console when asked.
    Remove unused co-ordinates parameter, and unhelpful L.Log call
    locations, ensure that all protocol messages are logged.
    
    Increase the buffer to record startup and replay it for easier
    debugging after startup.
    
    Now when enabling "Protocol Logging" in the first minute from
    document load, we get:
    
    INCOMING[!fullyLoadedAndReady].statusindicator: find
    INCOMING[!fullyLoadedAndReady].statusindicator: connect
    INCOMING[!fullyLoadedAndReady].statusindicator: ready
    INCOMING[!fullyLoadedAndReady].perm: edit
    INCOMING[!fullyLoadedAndReady].filemode:{"readOnly": false, "editComment": true}
    etc.
    
    Signed-off-by: Michael Meeks <michael.meeks@collabora.com>
    Change-Id: I5d2a8639e8038dbcc31d6e8fd1b8f8ebf2fff7bc

--- browser/src/core/Log.js
+++ browser/src/core/Log.js
@@ -1,28 +1,37 @@
 /* -*- js-indent-level: 8 -*- */
 /*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
  * L.Log contains methods for logging the activity
  */
 
 L.Log = {
-	log: function (msg, direction, tileCoords, time) {
-		if (!time) {
-			time = Date.now();
-		}
-		if (!this._logs) {
+	log: function (msg, direction) {
+		if (!this._logs)
 			this._logs = [];
-		}
+		var time = Date.now();
+		if (!this.startTime)
+			this.startTime = time;
+
 		// Limit memory usage of log by only keeping the latest entries
 		var maxEntries = 100;
+		if (time - this.startTime < 60 * 1000 /* ms */)
+			maxEntries = 500; // enough to capture early start.
 		while (this._logs.length > maxEntries)
 			this._logs.shift();
+
 		// Limit memory usage of log by limiting length of message
 		var maxMsgLen = 128;
 		if (msg.length > maxMsgLen)
 			msg = msg.substring(0, maxMsgLen);
 		msg = msg.replace(/(\r\n|\n|\r)/gm, ' ');
-		this._logs.push({msg : msg, direction : direction,
-			coords : tileCoords, time : time});
-		//window.app.console.log(time + '-' + direction + ': ' + msg);
+		this._logs.push({msg : msg, direction : direction, time : time});
 	},
 
 	_getEntries: function () {
@@ -34,14 +43,16 @@ L.Log = {
 		var data = '';
 		for (var i = 0; i < this._logs.length; i++) {
 			data += this._logs[i].time + '.' + this._logs[i].direction + '.' +
-					this._logs[i].msg + '.' + this._logs[i].coords;
+				this._logs[i].msg;
 			data += '\n';
 		}
 		return data;
 	},
 
 	print: function () {
-		// window.app.console.log(this._getEntries());
+		window.app.console.log('Queued log messages:');
+		window.app.console.log(this._getEntries());
+		window.app.console.log('End of queued log messages:');
 	},
 
 	save: function () {
