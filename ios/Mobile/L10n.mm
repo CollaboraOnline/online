61d98cfdf00f1d469805f94074e8247633d86303
Michael Meeks <michael.meeks@collabora.com>
2023-11-18 14:13:14 +0000

    spdx: improve machine and human readability of headers.
    Change-Id: I71f1421debf31b8c6cd0fd75c0cb45e2563b8a5a
    Signed-off-by: Michael Meeks <michael.meeks@collabora.com>

--- ios/Mobile/L10n.mm
+++ ios/Mobile/L10n.mm
@@ -1,8 +1,13 @@
 // -*- Mode: ObjC; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*-
-//
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 
 #import <cstring>
 
