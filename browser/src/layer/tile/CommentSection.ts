43d261b7c5a229decf6b25d6b6646dae8990ff97
Pranam Lashkari <lpranam@collabora.com>
2023-11-20 22:33:59 +0530

    annotation: reset comment id if autosave is cancelled
    problem:
    User A writes Comment 1 and saves
    User A replies to himself, write something, click away to autosave, Cancel
    user A modifies Comment 1 and OK - comment disappears, although it is there, seen after reload, but without modification.
    
    Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
    Change-Id: I5d83936f26939b5a05a0ce3099c01923a55c9606

--- browser/src/layer/tile/CommentSection.ts
+++ browser/src/layer/tile/CommentSection.ts
@@ -826,6 +826,8 @@ export class Comment extends CanvasSectionObject {
 			return;
 		if (app.view.commentAutoAdded) {
 			this.sectionProperties.commentListSection.remove(this.sectionProperties.data.id);
+			app.view.commentAutoAdded = false;
+			this.sectionProperties.data.id = this.sectionProperties.data.oldId;
 			if (this.sectionProperties.commentListSection.sectionProperties.selectedComment.sectionProperties.container.classList.contains('reply-annotation-container')) {
 				this.show();
 				return;
