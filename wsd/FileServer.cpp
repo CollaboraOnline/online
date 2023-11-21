a6eb3a915cd8d7c553148e983babec9458109138
Caolán McNamara <caolan.mcnamara@collabora.com>
2023-11-21 15:26:17 +0000

    hack to get this up and working
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: I6d3d273f59f58a0c3b2027039dcc7de95ad971b9

--- wsd/FileServer.cpp
+++ wsd/FileServer.cpp
@@ -660,6 +660,7 @@ void FileServerRequestHandler::handleRequest(const HTTPRequest& request,
             {
                 response.add("Cross-Origin-Opener-Policy", "same-origin");
                 response.add("Cross-Origin-Embedder-Policy", "require-corp");
+                response.add("Cross-Origin-Resource-Policy", "cross-origin");
             }
 
             const bool brotli = request.hasToken("Accept-Encoding", "br");
@@ -1257,6 +1258,7 @@ void FileServerRequestHandler::preprocessFile(const HTTPRequest& request,
         LOG_ASSERT(COOLWSD::WASMState != COOLWSD::WASMActivationState::Disabled);
         oss << "Cross-Origin-Opener-Policy: same-origin\r\n";
         oss << "Cross-Origin-Embedder-Policy: require-corp\r\n";
+        oss << "Cross-Origin-Resource-Policy: cross-origin\r\n";
 
         csp.appendDirective("script-src", "'unsafe-eval'");
     }
