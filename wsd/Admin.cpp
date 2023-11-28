79d54dd4bba55432c04fd787e8d4e9c86b07ac36
Miklos Vajna <vmiklos@collabora.com>
2023-11-28 08:39:49 +0100

    wsd: handle Poco::URISyntaxException in AdminSocketHandler::handleMessage()
    terminate called after throwing an instance of 'Poco::URISyntaxException'
        #8 0x7f014e70dc86 in std::terminate() (/usr/lib64/libstdc++.so.6+0xb7c86) (BuildId: c74eca671e2dd0f063706372d103f8acef88f1e3)
        #9 0x7f014e70dee7 in __cxa_throw (/usr/lib64/libstdc++.so.6+0xb7ee7) (BuildId: c74eca671e2dd0f063706372d103f8acef88f1e3)
        #10 0x7f014dcea45b in Poco::URI::decode(std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char>> const&, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char>>&, bool) (/usr/lib64/libPocoFoundation.so.92+0x15145b) (BuildId: 32601204eb877c998d0122c4bef1c7c08c8d0285)
        #11 0x56496c48320b in Util::decodeURIComponent(std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char>> const&) /home/vmiklos/git/collaboraonline/online-fuzz/common/Util.cpp:800:9
        #12 0x56496bb21ec0 in AdminSocketHandler::handleMessage(std::vector<char, std::allocator<char>> const&) /home/vmiklos/git/collaboraonline/online-fuzz/wsd/Admin.cpp:84:20
    
    Signed-off-by: Miklos Vajna <vmiklos@collabora.com>
    Change-Id: I07551d92be5ae677d1cf63f0f0092fb838583337

--- wsd/Admin.cpp
+++ wsd/Admin.cpp
@@ -81,10 +81,18 @@ void AdminSocketHandler::handleMessage(const std::vector<char> &payload)
         std::string jwtToken;
         COOLProtocol::getTokenString(tokens[1], "jwt", jwtToken);
 
-        jwtToken = Util::decodeURIComponent(jwtToken);
+        bool decoded = true;
+        try
+        {
+            jwtToken = Util::decodeURIComponent(jwtToken);
+        }
+        catch (const Poco::URISyntaxException&)
+        {
+            decoded = false;
+        }
         LOG_INF("Verifying JWT token: " << jwtToken);
         JWTAuth authAgent("admin", "admin", "admin");
-        if (authAgent.verify(jwtToken))
+        if (decoded && authAgent.verify(jwtToken))
         {
             LOG_TRC("JWT token is valid");
             _isAuthenticated = true;
