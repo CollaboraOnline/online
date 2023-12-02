df398afa1153639529cd090b72a11823a12da7d1
Michael Meeks <michael.meeks@collabora.com>
2023-12-02 20:33:00 +0000

    spdx: improve machine and human readability of headers.
    Change-Id: I84c93d81f0aea1f79e2ede9186b2864bf11f723a
    Signed-off-by: Michael Meeks <michael.meeks@collabora.com>

--- browser/src/main.js
+++ browser/src/main.js
@@ -1,4 +1,13 @@
 /* -*- js-indent-level: 8 -*- */
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 /* global errorMessages getParameterByName accessToken accessTokenTTL accessHeader createOnlineModule */
 /* global app $ L host idleTimeoutSecs outOfFocusTimeoutSecs _ */
 /*eslint indent: [error, "tab", { "outerIIFEBody": 0 }]*/
