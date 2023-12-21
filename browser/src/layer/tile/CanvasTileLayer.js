3881d0077eaaacf9c6c01836f374f38fbe880d50
Szymon Kłos <szymon.klos@collabora.com>
2023-12-21 15:19:02 +0100

    calc: avoid jumping on large cell selection
    If we had very long or tall cell - maybe bigger than a screen
    on every selection we tried to show it on the screen what
    caused view to jump. If cell is bigger and partially visible
    left view at the same position to avoid "jumping".
    
    Steps to reproduce:
    1. merge many cells - longer than one screen width
    2. scroll in the middle of merged cell
    3. select the cell
    Result: jump to the begining
    Expected: no jump if it is paritally visible
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I04da5cd346f175728222888a039a796b271e00d1

--- browser/src/layer/tile/CanvasTileLayer.js
+++ browser/src/layer/tile/CanvasTileLayer.js
@@ -4586,6 +4586,8 @@ L.CanvasTileLayer = L.Layer.extend({
 		this._updateCursorAndOverlay();
 	},
 
+	// TODO: unused variables: horizontalDirection, verticalDirection
+	// TODO: used only in calc: move to CalcTileLayer
 	_onUpdateCellCursor: function (horizontalDirection, verticalDirection, onPgUpDn, scrollToCursor) {
 		this._onUpdateCellResizeMarkers();
 		if (this._cellCursor && !this._isEmptyRectangle(this._cellCursor)) {
