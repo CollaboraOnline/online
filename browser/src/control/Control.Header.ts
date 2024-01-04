82bca03b2a299d5d2ecfe32a8ce228823f43f12a
Michael Meeks <michael.meeks@collabora.com>
2024-01-04 15:52:38 +0000

    spdx: improve machine and human readability of headers.
    Change-Id: Id0127edf671a1751ba6cbda3ed36515bd09c84ac
    Signed-off-by: Michael Meeks <michael.meeks@collabora.com>

--- browser/src/control/Control.Header.ts
+++ browser/src/control/Control.Header.ts
@@ -1,10 +1,19 @@
 /* -*- js-indent-level: 8 -*- */
 /*
-* Control.Header
-*
-* Abstract class, basis for ColumnHeader and RowHeader controls.
-* Used only in spreadsheets, implements the row/column headers.
-*/
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+/*
+ * Control.Header
+ *
+ * Abstract class, basis for ColumnHeader and RowHeader controls.
+ * Used only in spreadsheets, implements the row/column headers.
+ */
 /* global $ L app */
 
 namespace cool {
