ea6551ff2bfc4626166b96eade918f7422bc6d8c
Michael Meeks <michael.meeks@collabora.com>
2024-01-09 09:23:35 +0000

    spdx: improve machine and human readability of headers.
    Signed-off-by: Michael Meeks <michael.meeks@collabora.com>
    Change-Id: I2432e3c437f3a68c361da55d3aaf7a53c3f37838

--- browser/src/control/Control.IdleHandler.ts
+++ browser/src/control/Control.IdleHandler.ts
@@ -1,6 +1,15 @@
 /*
-	* Class for idle handling of the view.
-*/
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+/*
+ * Class for idle handling of the view.
+ */
 
 /* global app L */
 
--- browser/src/control/Control.MobileSlide.js
+++ browser/src/control/Control.MobileSlide.js
@@ -1,4 +1,13 @@
 /*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+/*
  * L.Control.MobileSlide is used to add new slide button on the Impress document.
  */
 
