b0d1d35d737000bf846e8333cc4830ba401519b9
Michael Meeks <michael.meeks@collabora.com>
2024-01-09 09:23:35 +0000

    spdx: improve machine and human readability of headers.
    Signed-off-by: Michael Meeks <michael.meeks@collabora.com>
    Change-Id: I2432e3c437f3a68c361da55d3aaf7a53c3f37838

--- browser/src/control/Control.Mention.js
+++ browser/src/control/Control.Mention.js
@@ -1,7 +1,16 @@
 /* -*- js-indent-level: 8 -*- */
 /*
-* Control.Mention
-*/
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+/*
+ * Control.Mention
+ */
 L.Control.Mention = L.Control.extend({
 	onAdd: function(map) {
 		this.map = map;
