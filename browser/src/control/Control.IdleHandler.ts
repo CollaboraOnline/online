CollaboraOnline/online

commit 86e8491707c8f25b106909a340f45edd65e704e5

For .eslintrc change, see: https://github.com/typescript-eslint/typescript-eslint/issues/1824
idle handler is moved into typescript and outside of map variable.

vex dialog of handler is replaced with JDSdialog.

Signed-off-by: Gökay Şatır <gokaysatir@collabora.com>
Change-Id: I341c28e9e3f1659987b871a235a50757f42d4d2e

browser/src/control/Control.IdleHandler.ts
@@ -1,0 +1,117 @@
+/*
+	* Class for idle handling of the view.
+*/
+
+/* global app L */
+
+declare var isAnyVexDialogActive: any;
+declare var mode: any;
+declare var ThisIsTheAndroidApp: any;
+declare var postMobileMessage: any;
+
+/**/
+
+class IdleHandler {
+    _serverRecycling: boolean = false;
+    _documentIdle: boolean = false;
+    _active: boolean = true;
+    map: any;
+	dimId: 'inactive_user_message';
+
+	isDimActive(): boolean {
+		return !!document.getElementById(this.dimId);
+	}
+
+	_activate() {
+		if (this._serverRecycling || this._documentIdle) {
+			return false;
+		}
+
+		if (!this._active) {
+			// Only activate when we are connected.
+			if (app.socket.connected()) {
+				app.socket.sendMessage('useractive');
+				this._active = true;
+				var docLayer = this.map._docLayer;
+				if (docLayer && docLayer.isCalc() && docLayer.options.sheetGeometryDataEnabled) {
+					docLayer.requestSheetGeometryData();
+				}
+				app.socket.sendMessage('commandvalues command=.uno:ViewAnnotations');
+
+				if (this.isDimActive()) {
+					this.map.jsdialog.closeDialog(this.dimId, false);
+					return true;
+				}
+			} else {
+				this.map.loadDocument();
+			}
+		}
+
+		if (window.mode.isDesktop() && !isAnyVexDialogActive()) {
+			this.map.focus();
+		}
+
+		return false;
+	}
+
+	_dim(message: string) {
+		this._active = false;
+
+		var restartConnectionFn = function() {
+			if (app.idleHandler._documentIdle)
+			{
+				window.app.console.debug('idleness: reactivating');
+				app.idleHandler._documentIdle = false;
+				app.idleHandler.map._docLayer._setCursorVisible();
+				return app.idleHandler._activate();
+			}
+			return false;
+		};
+
+		this.map._textInput.hideCursor();
+
+		this.map.uiManager.showInfoModal('inactive_user_message');
+		document.getElementById('inactive_user_message').textContent = message;
+		document.getElementById('inactive_user_message').tabIndex = 0;
+		document.getElementById('inactive_user_message').focus(); // We hid the OK button, we need to set focus manually on the popup.
+
+		if (message === '') {
+			document.getElementById(this.map.uiManager.generateModalId('inactive_user_message')).style.display = 'none';
+			L.LOUtil.onRemoveHTMLElement(document.getElementById('inactive_user_message'), function() { restartConnectionFn(); }.bind(this));
+		}
+		else {
+			var overlayId = this.map.uiManager.generateModalId('inactive_user_message') + '-overlay';
+			L.LOUtil.onRemoveHTMLElement(document.getElementById(overlayId), function() { restartConnectionFn(); }.bind(this));
+		}
+
+		this.map._doclayer && this.map._docLayer._onMessage('textselection:', null);
+		this.map.fire('postMessage', {msgId: 'User_Idle'});
+	}
+
+	notifyActive() {
+		if (window.ThisIsTheAndroidApp) {
+			window.postMobileMessage('LIGHT_SCREEN');
+		}
+	}
+
+	_deactivate() {
+		if (this._serverRecycling || this._documentIdle || !this.map._docLoaded) {
+			return;
+		}
+
+		if (!this._active || isAnyVexDialogActive() || this.map.jsdialog.hasDialogOpened()) {
+			// A dialog is already dimming the screen and probably
+			// shows an error message. Leave it alone.
+			this._active = false;
+			this.map._docLayer && this.map._docLayer._onMessage('textselection:', null);
+			if (app.socket.connected()) {
+				app.socket.sendMessage('userinactive');
+			}
+
+			return;
+		}
+	}
+}
+
+// Initiate the class.
+app.idleHandler = new IdleHandler();

browser/.eslintrc
@@ -9,7 +9,7 @@
     "keyword-spacing": 2,
     "no-lonely-if": 2,
     "comma-style": 2,
-    "indent": [2, "tab", {"VariableDeclarator": 0}],
+    "indent": "off",
     "no-underscore-dangle": 0,
     "no-constant-condition": 0,
     "no-multi-spaces": 0,

browser/Makefile.am
@@ -219,6 +219,7 @@ COOL_JS_LST =\
 	src/geo/crs/CRS.Simple.js \
 	src/control/IFrameDialog.js \
 	src/map/Map.js \
+	src/control/Control.IdleHandler.ts \
 	src/map/Clipboard.js \
 	src/layer/tile/SheetSwitch.ts \
 	src/layer/Layer.js \

browser/src/control/Control.JSDialog.js
@@ -56,6 +56,12 @@ L.Control.JSDialog = L.Control.extend({
 		}
 	},
 
+	closeAll: function() {
+		var dialogs = Object.keys(this.dialogs);
+		for (var i = 0; i < dialogs.length; i++)
+			this.close(dialogs[i]);
+	},
+
 	closeDialog: function(id, sendCloseEvent) {
 		if (!id || !this.dialogs[id]) {
 			console.warn('missing dialog data');

browser/src/control/Control.LokDialog.js
@@ -831,16 +831,12 @@ L.Control.LokDialog = L.Control.extend({
 		app.socket.sendMessage('windowmouse id=' + winid +  ' type=' + type +
 		                              ' x=' + x + ' y=' + y + ' count=' + count +
 		                              ' buttons=' + buttons + ' modifier=' + modifier);
-		// Keep map active while user is playing with dialog.
-		this._map.lastActiveTime = Date.now();
 	},
 
 	_postWindowGestureEvent: function(winid, type, x, y, offset) {
 		// window.app.console.log('x ' + x + ' y ' + y + ' o ' + offset);
 		app.socket.sendMessage('windowgesture id=' + winid +  ' type=' + type +
 		                              ' x=' + x + ' y=' + y + ' offset=' + offset);
-		// Keep map active while user is playing with dialog.
-		this._map.lastActiveTime = Date.now();
 	},
 
 	_closeChildWindows: function(dialogId) {

browser/src/core/LOUtil.js
@@ -19,6 +19,25 @@ L.LOUtil = {
 		[209, 118,   0]
 	],
 
+	// https://stackoverflow.com/a/32726412
+	onRemoveHTMLElement: function(element, onDetachCallback) {
+		var observer = new MutationObserver(function () {
+			function isDetached(el) {
+				return !el.closest('html');
+			}
+
+			if (isDetached(element)) {
+				onDetachCallback();
+				observer.disconnect();
+			}
+		});
+
+		observer.observe(document, {
+			 childList: true,
+			 subtree: true
+		});
+	},
+
 	startSpinner: function (spinnerCanvas, spinnerSpeed) {
 		var spinnerInterval;
 		spinnerCanvas.width = 50;

browser/src/core/Socket.js
@@ -125,7 +125,7 @@ app.definitions.Socket = L.Class.extend({
 			return;
 		}
 
-		if (!this._map._active) {
+		if (!app.idleHandler._active) {
 			// Avoid communicating when we're inactive.
 			if (typeof msg !== 'string')
 				return;
@@ -167,8 +167,8 @@ app.definitions.Socket = L.Class.extend({
 
 	_onSocketOpen: function () {
 		window.app.console.debug('_onSocketOpen:');
-		this._map._serverRecycling = false;
-		this._map._documentIdle = false;
+		app.idleHandler._serverRecycling = false;
+		app.idleHandler._documentIdle = false;
 
 		// Always send the protocol version number.
 		// TODO: Move the version number somewhere sensible.
@@ -225,7 +225,7 @@ app.definitions.Socket = L.Class.extend({
 		}
 		this._msgQueue = [];
 
-		this._map._activate();
+		app.idleHandler._activate();
 	},
 
 	_utf8ToString: function (data) {
@@ -701,7 +701,7 @@ app.definitions.Socket = L.Class.extend({
 				} else {
 					msg = _('Idle document - please tap to reload and resume editing');
 				}
-				this._map._documentIdle = true;
+				app.idleHandler._documentIdle = true;
 				this._map._docLayer._documentInfo = undefined;
 				postMsgData['Reason'] = 'DocumentIdle';
 				if (textMsg === 'oom')
@@ -717,8 +717,8 @@ app.definitions.Socket = L.Class.extend({
 			}
 			else if (textMsg === 'recycling') {
 				msg = _('Server is down, restarting automatically. Please wait.');
-				this._map._active = false;
-				this._map._serverRecycling = true;
+				app.idleHandler._active = false;
+				app.idleHandler._serverRecycling = true;
 
 				// Prevent reconnecting the world at the same time.
 				var min = 5000;
@@ -726,12 +726,12 @@ app.definitions.Socket = L.Class.extend({
 				var timeoutMs = Math.floor(Math.random() * (max - min) + min);
 
 				var socket = this;
-				map = this._map;
-				clearTimeout(vex.timer);
-				vex.timer = setInterval(function() {
+				var map = this._map;
+				clearTimeout(this.timer);
+				this.timer = setInterval(function() {
 					if (socket.connected()) {
 						// We're connected: cancel timer and dialog.
-						clearTimeout(vex.timer);
+						clearTimeout(this.timer);
 						vex.closeAll();
 						return;
 					}
@@ -769,13 +769,13 @@ app.definitions.Socket = L.Class.extend({
 				this.close();
 
 				// Reload the document
-				this._map._active = false;
+				app.idleHandler._active = false;
 				map = this._map;
-				clearTimeout(vex.timer);
-				vex.timer = setInterval(function() {
+				clearTimeout(this.timer);
+				this.timer = setInterval(function() {
 					try {
 						// Activate and cancel timer and dialogs.
-						map._activate();
+						app.idleHandler._activate();
 					} catch (error) {
 						window.app.console.warn('Cannot activate map');
 					}
@@ -784,39 +784,15 @@ app.definitions.Socket = L.Class.extend({
 
 			// Close any open dialogs first.
 			vex.closeAll();
+			this._map.jsdialog.closeAll();
 
 			var message = '';
 			if (!this._map['wopi'].DisableInactiveMessages) {
 				message = msg;
 			}
 
-			var dialogOptions = {
-				message: message,
-				contentClassName: 'cool-user-idle'
-			};
-
-			var restartConnectionFn;
 			if (textMsg === 'idle' || textMsg === 'oom') {
-				var map = this._map;
-				restartConnectionFn = function() {
-					if (map._documentIdle)
-					{
-						window.app.console.debug('idleness: reactivating');
-						map._documentIdle = false;
-						map._docLayer._setCursorVisible();
-						return map._activate();
-					}
-					return false;
-				};
-				dialogOptions.afterClose = restartConnectionFn;
-
-				var dialogOpened = vex.dialog.open(dialogOptions);
-				this._map._textInput.hideCursor();
-				dialogOpened.contentEl.onclick = restartConnectionFn;
-				$('.vex-overlay').addClass('cool-user-idle-overlay');
-
-				if (message === '')
-					$('.cool-user-idle').css('display', 'none');
+				app.idleHandler._dim(message);
 			}
 
 			if (postMsgData['Reason']) {
@@ -1017,19 +993,18 @@ app.definitions.Socket = L.Class.extend({
 				this._map.fire('error', {msg: errorMessages.docloadtimeout});
 			} else if (errorKind.startsWith('docunloading')) {
 				// The document is unloading. Have to wait a bit.
-				this._map._active = false;
+				app.idleHandler._active = false;
 
-				clearTimeout(vex.timer);
+				clearTimeout(this.timer);
 				if (this.ReconnectCount++ >= 10) {
 					this._map.fire('error', {msg: errorMessages.docunloadinggiveup});
 					return; // Give up.
 				}
 
-				map = this._map;
-				vex.timer = setInterval(function() {
+				this.timer = setInterval(function() {
 					try {
 						// Activate and cancel timer and dialogs.
-						map._activate();
+						app.idleHandler._activate();
 					} catch (error) {
 						window.app.console.warn('Cannot activate map');
 					}
@@ -1082,7 +1057,7 @@ app.definitions.Socket = L.Class.extend({
 				textMsg = errorMessages.serviceunavailable;
 			}
 			this._map._fatal = true;
-			this._map._active = false; // Practically disconnected.
+			app.idleHandler._active = false; // Practically disconnected.
 			this._map.fire('error', {msg: textMsg});
 		}
 		else if (textMsg.startsWith('fontsmissing:')) {
@@ -1154,7 +1129,7 @@ app.definitions.Socket = L.Class.extend({
 			if (textMsg.startsWith('statusindicator: ready')) {
 				// We're connected: cancel timer and dialog.
 				this.ReconnectCount = 0;
-				clearTimeout(vex.timer);
+				clearTimeout(this.timer);
 				vex.closeAll();
 			}
 		}
@@ -1552,9 +1527,9 @@ app.definitions.Socket = L.Class.extend({
 		if (this.ReconnectCount > 0)
 			return;
 
-		var isActive = this._map._active;
+		var isActive = app.idleHandler._active;
 		this._map.hideBusy();
-		this._map._active = false;
+		app.idleHandler._active = false;
 
 		if (this._map._docLayer) {
 			this._map._docLayer.removeAllViews();
@@ -1581,9 +1556,9 @@ app.definitions.Socket = L.Class.extend({
 		setTimeout(function () {
 			if (!that._reconnecting) {
 				that._reconnecting = true;
-				if (!that._map._documentIdle)
+				if (!app.idleHandler._documentIdle)
 					that._map.showBusy(_('Reconnecting...'), false);
-				that._map._activate();
+				app.idleHandler._activate();
 			}
 		}, 1 /* ms */);
 

browser/src/layer/marker/TextInput.js
@@ -200,7 +200,7 @@ L.TextInput = L.Layer.extend({
 		onoff(this._textArea, 'keyup', this._onKeyUp, this);
 		onoff(this._textArea, 'copy cut paste', this._map._handleDOMEvent, this._map);
 
-		this._map.notifyActive();
+		app.idleHandler.notifyActive();
 
 		if (ev.type === 'blur' && this._isComposing) {
 			this._abortComposition(ev);
@@ -589,7 +589,7 @@ L.TextInput = L.Layer.extend({
 		if (this._map.uiManager.isUIBlocked())
 			return;
 
-		this._map.notifyActive();
+		app.idleHandler.notifyActive();
 
 		if (this._ignoreInputCount > 0) {
 			window.app.console.log('ignoring synthetic input ' + this._ignoreInputCount);
@@ -837,7 +837,7 @@ L.TextInput = L.Layer.extend({
 	// Handled only in legacy situations ('input' events with an inputType
 	// property are preferred).
 	_onCompositionUpdate: function(ev) {
-		this._map.notifyActive();
+		app.idleHandler.notifyActive();
 		this._onInput(ev);
 	},
 
@@ -847,7 +847,7 @@ L.TextInput = L.Layer.extend({
 	// The approach here is to use "compositionend" events *only in Chrome* to mark
 	// the composing text as committed to the text area.
 	_onCompositionEnd: function(ev) {
-		this._map.notifyActive();
+		app.idleHandler.notifyActive();
 		this._isComposing = false;
 		this._onInput(ev);
 	},
@@ -907,7 +907,7 @@ L.TextInput = L.Layer.extend({
 		if (this._map.uiManager.isUIBlocked())
 			return;
 
-		this._map.notifyActive();
+		app.idleHandler.notifyActive();
 		if (!this._isComposing && (ev.key === 'ArrowLeft' || ev.key === 'ArrowRight' ||
 			ev.key === 'ArrowUp' || ev.key === 'ArrowDown' ||
 			ev.key === 'Home' || ev.key === 'End' ||

commit 759246f9c7df8b088376799034b77c884415a384

IdleHandler: bring back idle timeout for view
This fixes regression introduced in
commit 86e8491707c8f25b106909a340f45edd65e704e5
For .eslintrc change, see: https://github.com/typescript-eslint/typescript-eslint/issues/1824

Where per_view.idle_timeout_secs and out_of_focus_timeout_secs
settings didn't have any effect.

<out_of_focus_timeout_secs desc="The maximum number of seconds before dimming and stopping updates when the browser tab is no longer in focus. Defaults to 120 seconds." type="uint" default="120">120</out_of_focus_timeout_secs>

<idle_timeout_secs desc="The maximum number of seconds before dimming and stopping updates when the user is no longer active (even if the browser is in focus). Defaults to 15 minutes." type="uint" default="900">900</idle_timeout_secs>

Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
Change-Id: Iff3b1c26ec07c67fd9be0fb7264468b0be479978

browser/src/control/Control.IdleHandler.ts
@@ -7,6 +7,8 @@
 declare var mode: any;
 declare var ThisIsTheAndroidApp: any;
 declare var postMobileMessage: any;
+declare var idleTimeoutSecs: number;
+declare var outOfFocusTimeoutSecs: number;
 
 /**/
 
@@ -14,6 +16,8 @@ class IdleHandler {
     _serverRecycling: boolean = false;
     _documentIdle: boolean = false;
 	_lastActivity: number = Date.now();
+	_inactivityTimer: ReturnType<typeof setTimeout> = null;
+	_outOfFocusTimer: ReturnType<typeof setTimeout> = null;
     _active: boolean = true;
     map: any;
 	dimId: string = 'inactive_user_message';
@@ -44,6 +48,9 @@ class IdleHandler {
 			return false;
 		}
 
+		this._startInactiveTimer();
+		this._stopOutOfFocusTimer();
+
 		if (!this._active) {
 			// Only activate when we are connected.
 			if (app.socket.connected()) {
@@ -71,10 +78,46 @@ class IdleHandler {
 		return false;
 	}
 
+	_startInactiveTimer() {
+		if (this._serverRecycling || this._documentIdle || !this.map._docLoaded) {
+			return;
+		}
+
+		clearTimeout(this._inactivityTimer);
+
+		this._inactivityTimer = setTimeout(() => {
+			this._dimIfInactive();
+		}, 1 * 60 * 1000); // Check once a minute
+	}
+
+	_startOutOfFocusTimer() {
+		if (this._serverRecycling || this._documentIdle || !this.map._docLoaded) {
+			return;
+		}
+
+		this._stopOutOfFocusTimer();
+
+		this._outOfFocusTimer = setTimeout(() => {
+			this._dim();
+		}, window.outOfFocusTimeoutSecs * 1000);
+	}
+
+	_stopOutOfFocusTimer() {
+		clearTimeout(this._outOfFocusTimer);
+	}
+
+	_dimIfInactive() {
+		if (this.map._docLoaded && (this.getElapsedFromActivity() >= window.idleTimeoutSecs)) {
+			this._dim();
+		} else {
+			this._startInactiveTimer();
+		}
+	}
+
 	_dim() {
 		const message = this.getIdleMessage();
 
-		window.app.console.debug('IdleHandler: _dim(' + message + ')');
+		window.app.console.debug('IdleHandler: _dim()');
 
 		this._active = false;
 		var map = this.map;
@@ -86,9 +129,8 @@ class IdleHandler {
 				map.fire('postMessage', {msgId: 'User_Active'});
 				app.idleHandler._documentIdle = false;
 				app.idleHandler.map._docLayer._setCursorVisible();
-				return app.idleHandler._activate();
 			}
-			return false;
+			return app.idleHandler._activate();
 		};
 
 		this.map._textInput.hideCursor();
@@ -135,6 +177,8 @@ class IdleHandler {
 
 			return;
 		}
+
+		this._startOutOfFocusTimer();
 	}
 }
 