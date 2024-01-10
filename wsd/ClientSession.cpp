CollaboraOnline/online

commit d118874bee9196206d3de24a89df8700f62cd89b

wsd: fix: failed to load passwordprotected and csv files - send blockingcommandstatus after document is loaded - also fix odd nodocloaded dialog
Signed-off-by: Rash419 <rashesh.padia@collabora.com>
Change-Id: I8f0ae405ffa77b47e09c01caf51ddc4fd5e1d862

wsd/ClientSession.cpp
@@ -1252,19 +1252,7 @@ bool ClientSession::loadDocument(const char* /*buffer*/, int /*length*/,
         sendRestrictionInfo();
 #endif
 
-        bool result = forwardToChild(oss.str(), docBroker);
-#if ENABLE_FEATURE_LOCK || ENABLE_FEATURE_RESTRICTION
-        if (result)
-        {
-            forwardToChild(
-                std::string("blockingcommandstatus isRestrictedUser=") +
-                    (CommandControl::RestrictionManager::isRestrictedUser() ? "true" : "false") +
-                    std::string(" isLockedUser=") +
-                    (CommandControl::LockManager::isLockedUser() ? "true" : "false"),
-                docBroker);
-        }
-#endif
-        return result;
+        return forwardToChild(oss.str(), docBroker);;
     }
     catch (const Poco::SyntaxException&)
     {
@@ -2016,7 +2004,18 @@ bool ClientSession::handleKitToClientMessage(const std::shared_ptr<Message>& pay
             _canonicalViewId = canonicalId;
         }
     }
-
+#if ENABLE_FEATURE_LOCK || ENABLE_FEATURE_RESTRICTION
+    else if (tokens.equals(0, "statusindicatorfinish:"))
+    {
+        std::ostringstream blockingCommandStatus;
+        blockingCommandStatus << "blockingcommandstatus isRestrictedUser="
+                              << (CommandControl::RestrictionManager::isRestrictedUser() ? "true"
+                                                                                         : "false")
+                              << " isLockedUser="
+                              << (CommandControl::LockManager::isLockedUser() ? "true" : "false");
+        docBroker->forwardToChild(client_from_this(), blockingCommandStatus.str());
+    }
+#endif
     if (!isDocPasswordProtected())
     {
         if (tokens.equals(0, "tile:"))

commit 3e5ca1f424c5af9c68156e8e414d72ba4e483f91

fix: can't open readonly files
- regression from d118874bee9196206d3de24a89df8700f62cd89b which was a
  fix for regression from 93b5bdf
- previously we used to send blockingcommandstatus message to the core
  when we were getting statusindicatorfinish but when file is readonly
  we load the document first therefore we get the statusindicatorfinish
  first then wait for the input from the to whether to open the readonly
  document or not
- now we send the blockingcommandstatus only after we get the first
  status message which indicates document is loaded and opened

Signed-off-by: Rash419 <rashesh.padia@collabora.com>
Change-Id: I0f4934e085befa6f9a918ac61a265391b217a2f2

wsd/ClientSession.cpp
@@ -2040,7 +2040,7 @@ bool ClientSession::handleKitToClientMessage(const std::shared_ptr<Message>& pay
         }
     }
 #if ENABLE_FEATURE_LOCK || ENABLE_FEATURE_RESTRICTION
-    else if (tokens.equals(0, "statusindicatorfinish:"))
+    else if (tokens.equals(0, "status:") && !isViewLoaded())
     {
         std::ostringstream blockingCommandStatus;
         blockingCommandStatus << "blockingcommandstatus isRestrictedUser="