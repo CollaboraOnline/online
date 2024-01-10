b1f3e227b5f768e74c6ef6f2d17bd8b60040af54
Caolán McNamara <caolan.mcnamara@collabora.com>
2024-01-10 17:21:17 +0000

    cool#7951 add cypress test
    add regression test for:
    https://github.com/CollaboraOnline/online/issues/7951
    
    confirmed test fails with reverted core commit of:
    
    commit 754cd4309b5b055a05b57f5d25ed128c439f460b
    Date:   Mon Jan 8 15:04:07 2024 +0000
    
        Related: cool#7951 don't invalidate when creating a new view
    
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: I4e2bf09f8fbe36919a9826e8cf29cc5250520dc6

---
+++ cypress_test/integration_tests/multiuser/calc/invalidations_spec.js
@@ -0,0 +1,52 @@
+/* global describe it cy beforeEach expect require afterEach */
+
+var helper = require('../../common/helper');
+var desktopHelper = require('../../common/desktop_helper');
+
+describe(['tagmultiuser'], 'Joining a document should not trigger an invalidation', function() {
+	var origTestFileName = 'invalidations.ods';
+	var testFileName;
+
+	beforeEach(function() {
+		testFileName = helper.beforeAll(origTestFileName, 'calc', undefined, true);
+		desktopHelper.switchUIToNotebookbar();
+	});
+
+	afterEach(function() {
+		helper.afterAll(testFileName, this.currentTest.state);
+	});
+
+	it('Join document', function() {
+		cy.cSetActiveFrame('#iframe1');
+
+		cy.cGet('#InsertMode').should('have.text', '\u00a0Insert mode: inactive\u00a0');
+		helper.typeIntoDocument('X');
+		cy.cGet('#InsertMode').should('have.text', 'Insert');
+		helper.typeIntoDocument('{enter}');
+		cy.cGet('#InsertMode').should('have.text', '\u00a0Insert mode: inactive\u00a0');
+		cy.cGet('input#addressInput').should('have.prop', 'value', 'A2');
+		helper.typeIntoDocument('{uparrow}');
+		// wait until round trip of cell address
+		cy.cGet('input#addressInput').should('have.prop', 'value', 'A1');
+
+		cy.cGet('.empty-deltas').then(($before) => {
+			const beforeCount = $before.text();
+
+			// joining triggered some theme related invalidations
+			cy.cSetActiveFrame('#iframe2');
+			cy.get('#form2').submit();
+			// Wait for page to finish loading
+			helper.checkIfDocIsLoaded(true);
+
+			cy.cSetActiveFrame('#iframe1');
+			cy.cGet('input#addressInput').should('have.prop', 'value', 'A1');
+			// wait until round trip of cell address
+			helper.typeIntoDocument('{rightarrow}');
+			cy.cGet('input#addressInput').should('have.prop', 'value', 'B1');
+
+			cy.cGet('.empty-deltas').should(($after) => {
+				expect($after.text()).to.eq(beforeCount);
+			});
+		});
+	});
+});
