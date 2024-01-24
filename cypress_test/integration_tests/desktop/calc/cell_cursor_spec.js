b24a9183d8ffca15939bdc23140d2eebd1dd2c35
Szymon Kłos <szymon.klos@collabora.com>
2024-01-24 13:55:26 +0100

    cypress: test view jump on merged cell with split pane
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: Iec6ff179a28dce802d330a3f59ac484189a25c48

--- cypress_test/integration_tests/desktop/calc/cell_cursor_spec.js
+++ cypress_test/integration_tests/desktop/calc/cell_cursor_spec.js
@@ -1,10 +1,10 @@
-/* global describe it cy beforeEach require afterEach */
+/* global describe it cy beforeEach expect require afterEach */
 
 var helper = require('../../common/helper');
 var calcHelper = require('../../common/calc_helper');
 var desktopHelper = require('../../common/desktop_helper');
 
-describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'Scroll through document', function() {
+describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'Test jumping on large cell selection', function() {
 	var testFileName = 'cell_cursor.ods';
 
 	beforeEach(function() {
@@ -23,5 +23,36 @@ describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'Scroll through document', 
 		calcHelper.clickOnFirstCell(true, false, false);
 		cy.cGet('input#addressInput').should('have.prop', 'value', 'A1:Z1');
 		desktopHelper.assertScrollbarPosition('horizontal', 310, 315);
+	});
+});
+
+describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'Test jumping on large cell selection with split panes', function() {
+	var testFileName = 'cell_cursor_split.ods';
+
+	beforeEach(function() {
+		helper.beforeAll(testFileName, 'calc');
+		desktopHelper.switchUIToCompact();
+		cy.cGet('#toolbar-up .w2ui-scroll-right').click();
+		cy.cGet('#tb_editbar_item_sidebar').click();
+	});
+
+	afterEach(function() {
+		helper.afterAll(testFileName, this.currentTest.state);
+	});
+
+	it('No jump on long merged cell with split panes', function() {
+		desktopHelper.assertScrollbarPosition('horizontal', 380, 390);
+
+		// Click on second cell in second row
+		cy.cGet('#map')
+		.then(function(items) {
+			expect(items).to.have.lengthOf(1);
+			var XPos = items[0].getBoundingClientRect().left + 140;
+			var YPos = items[0].getBoundingClientRect().top + 30;
+			cy.cGet('body').click(XPos, YPos);
+		});
+
+		cy.cGet('input#addressInput').should('have.prop', 'value', 'B2:AA2');
+		desktopHelper.assertScrollbarPosition('horizontal', 380, 390);
 	});
 });
