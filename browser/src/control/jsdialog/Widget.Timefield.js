f7491e1f62de5401529db40b8b899e5c0563badc
Méven Car <meven.car@collabora.com>
2024-01-26 11:41:42 +0100

    widgets: Add a time field
    Signed-off-by: Méven Car <meven.car@collabora.com>
    Change-Id: I9e3e280db4a0b1f3e3a9df5fe8b6e0bfd97ddaa5

---
+++ browser/src/control/jsdialog/Widget.Timefield.js
@@ -0,0 +1,47 @@
+/* -*- js-indent-level: 8 -*- */
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * JSDialog.Timefield - input field for time data
+ *
+ * Example JSON:
+ * {
+ *     id: 'time',
+ *     type: 'time',
+ *     text: '01:01:01'
+ * }
+ */
+
+/* global JSDialog */
+
+JSDialog.timeField = function (parentContainer, data, builder) {
+	var inputTimeField = L.DomUtil.create('input', builder.options.cssClass + ' ui-timefield', parentContainer);
+	inputTimeField.setAttribute('type', 'time');
+	inputTimeField.setAttribute('step', 1); // forces the display of seconds
+	inputTimeField.setAttribute('id', data.id);
+	inputTimeField.value = data.text;
+
+	inputTimeField.addEventListener('change', function (event) {
+		var timefield = event.target;
+
+		var attrdisabled = timefield.getAttribute('disabled');
+		if (attrdisabled !== 'disabled') {
+			builder.callback('spinfield', 'change', timefield, timefield.value, builder);
+		}
+	});
+
+	var disabled = data.enabled === 'false' || data.enabled === false;
+	if (disabled) {
+		inputTimeField.disabled = true;
+	}
+
+	return false;
+};
