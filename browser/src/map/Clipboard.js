3d8059cee5357521751dd73bf718046a0db12679
Miklos Vajna <vmiklos@collabora.com>
2024-01-26 16:27:06 +0100

    Revert "cool#8023 clipboard: remove old marker"
    This reverts commit cc12a1b060adb31afe037bb5e18f1cbe4885c3e6, because
    there is not much short-term benefit of this and it may cause problems,
    as it affects builds produced without --enable-experimental, too.
    
    Signed-off-by: Miklos Vajna <vmiklos@collabora.com>
    Change-Id: I4c27614e9ee26c8aaa2c510ee6bd115cd19a45f3

--- browser/src/map/Clipboard.js
+++ browser/src/map/Clipboard.js
@@ -127,6 +127,7 @@ L.Clipboard = L.Class.extend({
 		if (isStub)
 			text += '    ' + this._getHtmlStubMarker() + '\n';
 		text +=     '    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>\n' +
+			    '    <meta name="origin" content="' + encodedOrigin + '"/>\n' +
 			    '  </head>\n' +
 			    '  <body lang="' + lang + '" dir="ltr"><div id="meta-origin" data-coolorigin="' + encodedOrigin + '">\n' +
 			    body +
@@ -367,6 +368,12 @@ L.Clipboard = L.Class.extend({
 		//   cf. ClientSession.cpp /textselectioncontent:/
 
 		var meta = this._getMetaOrigin(htmlText, '<div id="meta-origin" data-coolorigin="');
+		var newStyle = true;
+		if (meta === '')
+		{
+			meta = this._getMetaOrigin(htmlText, '<meta name="origin" content="');
+			newStyle = false;
+		}
 		var id = this.getMetaPath(0);
 		var idOld = this.getMetaPath(1);
 
@@ -375,7 +382,7 @@ L.Clipboard = L.Class.extend({
 		    (meta.indexOf(id) >= 0 || meta.indexOf(idOld) >= 0))
 		{
 			// Home from home: short-circuit internally.
-			window.app.console.log('short-circuit, internal paste');
+			window.app.console.log('short-circuit, internal paste, new style? ' + newStyle);
 			this._doInternalPaste(this._map, usePasteKeyEvent);
 			return true;
 		}
