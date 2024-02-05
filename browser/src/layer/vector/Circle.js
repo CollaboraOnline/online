0ef41c26340e2ef7e63e4c521c17945bd0008809
Michael Meeks <michael.meeks@collabora.com>
2024-02-05 10:02:16 +0000

    js: remove obsolete CRS.Earth references.
    Change-Id: I3c5a25bd3ff1ee7af01c6190244400ec8d68e47c
    Signed-off-by: Michael Meeks <michael.meeks@collabora.com>

--- browser/src/layer/vector/Circle.js
+++ browser/src/layer/vector/Circle.js
@@ -38,26 +38,10 @@ L.Circle = L.CircleMarker.extend({
 		    map = this._map,
 		    crs = map.options.crs;
 
-		if (crs.distance === L.CRS.Earth.distance) {
-			var d = Math.PI / 180,
-			    latR = (this._mRadius / L.CRS.Earth.R) / d,
-			    top = map.project([lat + latR, lng]),
-			    bottom = map.project([lat - latR, lng]),
-			    p = top.add(bottom).divideBy(2),
-			    lat2 = map.unproject(p).lat,
-			    lngR = Math.acos((Math.cos(latR * d) - Math.sin(lat * d) * Math.sin(lat2 * d)) /
-			            (Math.cos(lat * d) * Math.cos(lat2 * d))) / d;
+		var latlng2 = crs.unproject(crs.project(this._latlng).subtract([this._mRadius, 0]));
 
-			this._point = p.subtract(map.getPixelOrigin());
-			this._radius = isNaN(lngR) ? 0 : Math.max(Math.round(p.x - map.project([lat2, lng - lngR]).x), 1);
-			this._radiusY = Math.max(Math.round(p.y - top.y), 1);
-
-		} else {
-			var latlng2 = crs.unproject(crs.project(this._latlng).subtract([this._mRadius, 0]));
-
-			this._point = map.latLngToLayerPoint(this._latlng);
-			this._radius = this._point.x - map.latLngToLayerPoint(latlng2).x;
-		}
+		this._point = map.latLngToLayerPoint(this._latlng);
+		this._radius = this._point.x - map.latLngToLayerPoint(latlng2).x;
 
 		this._updateBounds();
 	}
