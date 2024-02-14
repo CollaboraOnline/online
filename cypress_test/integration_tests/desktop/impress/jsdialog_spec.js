5f055fa949301ec5d146f242117ea21827dfa86e
Szymon Kłos <szymon.klos@collabora.com>
2024-02-14 07:32:35 +0100

    cypress: test disabled state updates
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: Ib0c123e82360edcdf6c4bad533b40b3d5cc04bc2

---
+++ cypress_test/integration_tests/desktop/impress/jsdialog_spec.js
@@ -0,0 +1,47 @@
+/* global describe it cy require afterEach beforeEach */
+
+var helper = require('../../common/helper');
+
+describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'JSDialog Tests', function() {
+	var testFileName = 'jsdialog.odp';
+
+	beforeEach(function() {
+		helper.beforeAll(testFileName, 'impress');
+	});
+
+	afterEach(function() {
+		helper.afterAll(testFileName, this.currentTest.state);
+	});
+
+	it('Check disabled state in animation sidebar', function() {
+		// open animation deck
+		cy.cGet('#options-custom-animation-button').should('not.have.class', 'selected');
+		cy.cGet('#options-custom-animation-button').click();
+		cy.cGet('#options-custom-animation-button').should('have.class', 'selected');
+
+		// all options are disabled
+		cy.cGet('#start_effect_list-input').should('be.disabled');
+		cy.cGet('#combo-input').should('be.disabled');
+		cy.cGet('#anim_duration-input').should('be.disabled');
+		cy.cGet('#delay_value-input').should('be.disabled');
+
+		// select animation entry
+		cy.cGet('#custom_animation_list').contains('.jsdialog.sidebar.ui-treeview-cell-text', 'Shape 1').click();
+
+		// some options are enabled
+		cy.cGet('#start_effect_list-input').should('not.be.disabled');
+		cy.cGet('#combo-input').should('be.disabled');
+		cy.cGet('#anim_duration-input').should('not.be.disabled');
+		cy.cGet('#delay_value-input').should('not.be.disabled');
+
+		// use different type of animation
+		cy.cGet('#categorylb-input').select('Entrance');
+		cy.cGet('#effect_list').contains('.jsdialog.sidebar.ui-treeview-cell-text', 'Fly In').click();
+
+		// all options are enabled
+		cy.cGet('#start_effect_list-input').should('not.be.disabled');
+		cy.cGet('#combo-input').should('not.be.disabled');
+		cy.cGet('#anim_duration-input').should('not.be.disabled');
+		cy.cGet('#delay_value-input').should('not.be.disabled');
+	});
+});
