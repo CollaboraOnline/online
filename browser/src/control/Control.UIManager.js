7b3960b7e60791b445f9523e630334d7794f0bb1
Gülşah Köse <gulsah.kose@collabora.com>
2024-02-19 16:10:06 +0300

    Show tooltips over the cell while typing a function in a cell.
    Signed-off-by: Gülşah Köse <gulsah.kose@collabora.com>
    Change-Id: I85354a6c17947824408ae0458f29b858f31ab0e3

--- browser/src/control/Control.UIManager.js
+++ browser/src/control/Control.UIManager.js
@@ -1027,26 +1027,34 @@ L.Control.UIManager = L.Control.extend({
 		this.map.fire('hidebusy');
 	},
 
-	// Document area tooltip
+	// Calc function tooltip
 
-	/// Shows tooltip in the document area
-	/// tooltipInfo contains rectangle (position in twips) and text properties
-	/// elem has to be jQuery selector output eg. $('.leaflet-layer')
-	showDocumentTooltip: function(tooltipInfo, elem) {
-		var split = tooltipInfo.rectangle.split(',');
-		var latlng = this.map._docLayer._twipsToLatLng(new L.Point(+split[0], +split[1]));
-		var pt = this.map.latLngToContainerPoint(latlng);
+	/// Shows tooltip over the cell while typing a function in a cell.
+	/// tooltipInfo contains possible function list. If you type a valid
+	/// function it'll show the usage of the function. elem has to be
+	/// jQuery selector output eg. $('.leaflet-layer')
+	showDocumentTooltip: function(tooltipInfo, elem, pos) {
+		var pt = this.map.latLngToContainerPoint(pos);
+		pt.y -=35; //Show tooltip above the cursor.
 
-		elem.tooltip();
-		elem.tooltip('enable');
-		elem.tooltip('option', 'content', tooltipInfo.text);
-		elem.tooltip('option', 'items', elem[0]);
-		elem.tooltip('option', 'position', { my: 'left bottom',  at: 'left+' + pt.x + ' top+' + pt.y, collision: 'fit fit' });
-		elem.tooltip('open');
-		document.addEventListener('mousemove', function() {
-			elem.tooltip('close');
-			elem.tooltip('disable');
-		}, {once: true});
+		if ($('.ui-tooltip').length > 0) {
+			elem.tooltip('option', 'content', tooltipInfo);
+		}
+		else {
+			elem.tooltip({
+				tooltipClass: 'functiontooltip',
+				content: tooltipInfo,
+				items: elem[0],
+                position: { my: 'left top', at: 'left+' + pt.x +  ' top+' +pt.y, collision: 'fit fit' }
+			});
+			elem.tooltip('option', 'customClass', 'functiontooltip');
+			elem.tooltip('open');
+			elem.off('mouseleave');
+		}
+	},
+
+	hideDocumentTooltip: function(elem) {
+		elem.tooltip('option', 'disabled', true);
 	},
 
 	// Snack bar
