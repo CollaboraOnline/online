cf3448f7d177fef3eb0bd88679d36d8cef7d3e05
codewithvk <vivek.javiya@collabora.com>
2024-02-20 21:35:22 +0530

    Fix: Multiple Encoding  During Document Renaming
    On the client side, when setting up the socket, we encode  beforehand, so there's no need to encode it again.
    
    Signed-off-by: codewithvk <vivek.javiya@collabora.com>
    Change-Id: I09e59c6fdda4c93de5dd36fca5023ac88926c4d4

--- browser/src/core/Socket.js
+++ browser/src/core/Socket.js
@@ -1296,7 +1296,7 @@ app.definitions.Socket = L.Class.extend({
 			var docUrl = url.split('?')[0];
 			this._map.options.doc = docUrl;
 			this._map.options.previousWopiSrc = this._map.options.wopiSrc; // After save-as op, we may connect to another server, then code will think that server has restarted. In this case, we don't want to reload the page (detect the file name is different).
-			this._map.options.wopiSrc = encodeURIComponent(docUrl);
+			this._map.options.wopiSrc = docUrl;
 			window.wopiSrc = this._map.options.wopiSrc;
 
 			if (textMsg.startsWith('renamefile:')) {
