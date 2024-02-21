d6b897ff74247f62151c8e67a254a99e19ca3fd1
Miklos Vajna <vmiklos@collabora.com>
2024-02-21 13:29:11 +0100

    cool#8342 browser, clipboard: start improving the paste special dialog's button
    If the user doesn't read the text of the dialog, make it clear that
    pressing the button will cancel the process, this is not a warning
    dialog to be just ignored.
    
    This is a minimal fix that introduces no new l10n strings.
    
    Signed-off-by: Miklos Vajna <vmiklos@collabora.com>
    Change-Id: Ie68ce5bbfe9f9c8d600f6eadfef63301728984b5

--- browser/src/map/Clipboard.js
+++ browser/src/map/Clipboard.js
@@ -1037,7 +1037,7 @@ L.Clipboard = L.Class.extend({
 		this.pasteSpecialDialogId = this._map.uiManager.generateModalId('paste_special_dialog') + '-box';
 
 		var id = 'paste_special_dialog';
-		this._map.uiManager.showYesNoButton(id + '-box', '', '', _('OK'), null, null, null, true);
+		this._map.uiManager.showYesNoButton(id + '-box', '', '', _('Cancel'), null, null, null, true);
 		var box = document.getElementById(id + '-box');
 		var innerDiv = L.DomUtil.create('div', '', null);
 		box.insertBefore(innerDiv, box.firstChild);
