2d31aafa11dc9e8e1b801df5d66543db3498ee13
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2024-02-26 17:34:35 -0500

    wsd: fix assertion and better logging
    In some cases the WOPISrc is inconsistently
    encoded. So now we decode before comparing.
    
    Also, improve related log entries.
    
    Change-Id: Ifd6cca76c91a34761dd0ce8f6d78c3af5d47fd43
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- wsd/ClientRequestDispatcher.cpp
+++ wsd/ClientRequestDispatcher.cpp
@@ -34,6 +34,7 @@
 #include <ConfigUtil.hpp>
 #include <JailUtil.hpp>
 #include <ProofKey.hpp>
+#include <Util.hpp>
 
 #include <Poco/File.h>
 #include <Poco/StreamCopier.h>
@@ -564,8 +565,9 @@ void ClientRequestDispatcher::handleIncomingMessage(SocketDisposition& dispositi
                                                            accessDetails);
                 if (accessDetails.isValid())
                 {
-                    LOG_ASSERT_MSG(requestDetails.getField(RequestDetails::Field::WOPISrc) ==
-                                       accessDetails.wopiSrc(),
+                    LOG_ASSERT_MSG(Util::decodeURIComponent(
+                                       requestDetails.getField(RequestDetails::Field::WOPISrc)) ==
+                                       Util::decodeURIComponent(accessDetails.wopiSrc()),
                                    "Expected identical WOPISrc in the request as in cool.html");
 
                     const std::string requestKey = RequestDetails::getRequestKey(
