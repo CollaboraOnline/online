75c003d973b1192218f0e7f64df0e6f4890f0241
Szymon Kłos <szymon.klos@collabora.com>
2024-02-27 19:32:44 +0100

    cypress: test for jumps when switching tabs in calc
    Protect us from regressions.
    
    We had issues with jumping view in calc, it was related to
    switching tabs (not all was visible in the UI).
    
    One case was repeated selection of the same tab.
    Second one was using a keyboard shortcut to select multiple
    sheets (ctrl + shift + page up/down).
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: Icda723ee51b1a4aa8876208d52b4739ca380bb78

---
+++ cypress_test/integration_tests/desktop/calc/sheet_switch_spec.js
@@ -0,0 +1,85 @@
+/* global describe it cy beforeEach require afterEach */
+
+var helper = require('../../common/helper');
+var desktopHelper = require('../../common/desktop_helper');
+
+describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'Sheet switching tests', function() {
+	var testFileName = 'switch.ods';
+
+	beforeEach(function() {
+		helper.beforeAll(testFileName, 'calc');
+	});
+
+	afterEach(function() {
+		helper.afterAll(testFileName, this.currentTest.state);
+	});
+
+	/* switch.ods has 2 sheets, 1st with data in G45, 2nd with data in F720*/
+
+	it('Check view position on sheet switch', function() {
+		// go to sheet 1
+		cy.cGet('#spreadsheet-tab0').click();
+
+		desktopHelper.assertScrollbarPosition('vertical', 35, 45);
+		cy.cGet('input#addressInput').should('have.prop', 'value', 'G45');
+
+		// go to sheet 2
+		cy.cGet('#spreadsheet-tab1').click();
+		cy.cGet('input#addressInput').should('have.prop', 'value', 'F720');
+		desktopHelper.assertScrollbarPosition('vertical', 320, 330);
+
+		cy.cGet('input#addressInput').type('{selectAll}A2{enter}');
+		desktopHelper.assertScrollbarPosition('vertical', 15, 25);
+	});
+
+	it('Check view position on repeated selection of currently selected sheet', function() {
+		// initially we are on sheet 2 tab
+		cy.cGet('input#addressInput').should('have.prop', 'value', 'F720');
+		desktopHelper.assertScrollbarPosition('vertical', 320, 330);
+
+		// click on sheet 2 tab (yes, current one)
+		cy.cGet('#spreadsheet-tab1').click();
+		cy.cGet('input#addressInput').should('have.prop', 'value', 'F720');
+		desktopHelper.assertScrollbarPosition('vertical', 320, 330);
+
+		// go to different place in the spreadsheet
+		cy.cGet('input#addressInput').type('{selectAll}A2{enter}');
+		desktopHelper.assertScrollbarPosition('vertical', 15, 25);
+
+		// validate we didn't jump back after some time
+		cy.wait(1000);
+		desktopHelper.assertScrollbarPosition('vertical', 15, 25);
+	});
+
+	it.skip('Check view position when having multiple sheet selection', function() {
+		// go to sheet 1
+		cy.cGet('#spreadsheet-tab0').click();
+
+		desktopHelper.assertScrollbarPosition('vertical', 35, 45);
+		cy.cGet('input#addressInput').should('have.prop', 'value', 'G45');
+
+		// add sheet 2 to sheets selection
+		helper.typeIntoDocument('{ctrl}{shift}{pageDown}');
+
+		desktopHelper.assertScrollbarPosition('vertical', 320, 330);
+		cy.cGet('input#addressInput').should('have.prop', 'value', 'F720');
+
+		// go to sheet 1 using keyboard shortcut
+		helper.typeIntoDocument('{ctrl}{alt}{pageUp}');
+
+		// we still have selected two sheets so we see cell data from sheet 2
+		cy.cGet('input#addressInput').should('have.prop', 'value', 'F720');
+
+		// go to sheet 2 using tab
+		cy.cGet('#spreadsheet-tab1').click();
+		cy.cGet('input#addressInput').should('have.prop', 'value', 'F720');
+
+		// go to different place in the spreadsheet
+		cy.cGet('input#addressInput').type('{selectAll}A2{enter}');
+		helper.typeIntoDocument('some text');
+
+		// validate we didn't jump back after some time
+		cy.wait(1000);
+		desktopHelper.assertScrollbarPosition('vertical', 15, 25);
+	});
+});
