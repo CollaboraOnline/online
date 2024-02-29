CollaboraOnline/online

commit 74a4484e1136d9e35b877631ad5e667c3702b96d

calc: on tab switching document view can be restored to a wrong position
In calc when a tab is moved to a new position or a sheet is inserted
or deleted, saved positions in SheetSwitchViewRestore which are used
to restore the correct document.
position when user switches back to a tab are no more valid.
In fact the internal map: sheet index -> sheet position becomes wrong.
This patch takes care to update such internal map when one of the
listed event occurs.

Signed-off-by: Marco Cecchetti <marco.cecchetti@collabora.com>
Change-Id: Ia4587b14179a35ff369d1f030657c4e5c64f5715

browser/src/control/Control.Tabs.js
@@ -332,6 +332,7 @@ L.Control.Tabs = L.Control.extend({
 
 	//selected sheet is moved to new index
 	_moveSheet: function (newIndex) {
+		this._map._docLayer._sheetSwitch.updateOnSheetMoved(this._map.getPartNumber(), newIndex - 1);
 		this._map.sendUnoCommand('.uno:Move?Copy:bool=false&UseCurrentDocument:bool=true&Index=' + newIndex);
 	},
 
@@ -351,6 +352,8 @@ L.Control.Tabs = L.Control.extend({
 		var contextMenuTab = this._tabForContextMenu;
 		if (contextMenuTab <= 0) return;
 
+		this._map._docLayer._sheetSwitch.updateOnSheetMoved(contextMenuTab, contextMenuTab - 1);
+
 		// core handles the decreasing of contextMenuTab by 1
 		// so, no need to do it here (for the second parameter)
 		this._moveSheetLR(contextMenuTab, contextMenuTab);
@@ -358,6 +361,8 @@ L.Control.Tabs = L.Control.extend({
 
 	_moveSheetRight: function () {
 		var contextMenuTab = this._tabForContextMenu;
+
+		this._map._docLayer._sheetSwitch.updateOnSheetMoved(contextMenuTab, contextMenuTab + 1);
 
 		/*
 		Why there is '+3' ?

browser/src/layer/tile/SheetSwitch.ts
@@ -26,6 +26,7 @@ export class SheetSwitchViewRestore {
 	private mayRestore: boolean;
 	private restorePart: number;
 	private setPartRecvd: boolean;
+	private currentSheetIndexReassigned: boolean;
 
 	// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 	constructor (map: any) {
@@ -37,16 +38,94 @@ export class SheetSwitchViewRestore {
 		this.mayRestore = false;
 		this.restorePart = -1;
 		this.setPartRecvd = false;
-
+		this.currentSheetIndexReassigned = false;
 
 	}
 
 	public save (toPart: number): void {
-
-		this.centerOfSheet.set(this.docLayer._selectedPart as number, this.map.getCenter());
+		if (!this.currentSheetIndexReassigned) {
+			this.centerOfSheet.set(this.docLayer._selectedPart as number, this.map.getCenter());
+		} else {
+			this.currentSheetIndexReassigned = false;
+		}
 		this.mayRestore = this.centerOfSheet.has(toPart);
 		this.restorePart = this.mayRestore ? toPart : -1;
 		this.setPartRecvd = false;
+	}
+
+	public updateOnSheetMoved(oldIndex: number, newIndex: number): void {
+		window.app.console.log('SheetSwitchViewRestore.updateOnSheetMoved: oldIndex: ' + oldIndex + ', newIndex: ' + newIndex);
+		if (oldIndex < 0 || newIndex < 0 || oldIndex === newIndex)
+			return;
+
+		const movedSheetCenter = this.centerOfSheet.get(oldIndex);
+
+		if (oldIndex < newIndex) {
+			for (let i = oldIndex; i < newIndex; ++i) {
+				const center = this.centerOfSheet.get(i + 1);
+				if (center)
+					this.centerOfSheet.set(i, center);
+				else
+					this.centerOfSheet.delete(i);
+			}
+		} else {
+			for (let i = oldIndex; i > newIndex; --i) {
+				const center = this.centerOfSheet.get(i - 1);
+				if (center)
+					this.centerOfSheet.set(i, center);
+				else
+					this.centerOfSheet.delete(i);
+			}
+		}
+
+		if (movedSheetCenter)
+			this.centerOfSheet.set(newIndex, movedSheetCenter);
+		else
+			this.centerOfSheet.delete(newIndex);
+	}
+
+	public updateOnSheetInsertion(index: number): void {
+		if (index < 0)
+			return;
+
+		// when insert a sheet
+		this.centerOfSheet.set(this.docLayer._selectedPart as number, this.map.getCenter());
+
+		const numberOfSheets: number = this.map.getNumberOfParts();
+		for (let i = numberOfSheets; i > index; --i) {
+			const center = this.centerOfSheet.get(i - 1);
+			if (center)
+				this.centerOfSheet.set(i, center);
+			else
+				this.centerOfSheet.delete(i);
+		}
+		this.centerOfSheet.delete(index);
+
+		const currentSheetNumber: number = this.map.getCurrentPartNumber();
+		this.currentSheetIndexReassigned = index <= currentSheetNumber;
+		if (this.currentSheetIndexReassigned) {
+			this.centerOfSheet.set(currentSheetNumber + 1, this.map.getCenter());
+		}
+	}
+
+	public updateOnSheetDeleted(index: number): void {
+		if (index < 0)
+			return;
+
+		const numberOfSheets: number = this.map.getNumberOfParts();
+		for (let i = index; i < numberOfSheets; ++i) {
+			const center = this.centerOfSheet.get(i + 1);
+			if (center)
+				this.centerOfSheet.set(i, center);
+			else
+				this.centerOfSheet.delete(i);
+		}
+
+		const currentSheetNumber: number = this.map.getCurrentPartNumber();
+		this.currentSheetIndexReassigned = index <= currentSheetNumber;
+		if (index < currentSheetNumber) {
+			this.centerOfSheet.set(currentSheetNumber - 1, this.map.getCenter());
+		}
 	}
 
 	public gotSetPart(part: number): void {

browser/src/control/Parts.js
@@ -302,6 +302,7 @@ L.Map.include({
 			app.socket.sendMessage('uno .uno:InsertPage');
 		}
 		else if (this.getDocType() === 'spreadsheet') {
+			this._docLayer._sheetSwitch.updateOnSheetInsertion(nPos);
 			var command = {
 				'Name': {
 					'type': 'string',
@@ -362,6 +363,7 @@ L.Map.include({
 			app.socket.sendMessage('uno .uno:DeletePage');
 		}
 		else if (this.getDocType() === 'spreadsheet') {
+			this._docLayer._sheetSwitch.updateOnSheetDeleted(nPos);
 			var command = {
 				'Index': {
 					'type': 'long',

commit fdb9bb98d21785a41c55cc1722dc0ae212e23cbd

calc: tab drag&drop does not work properly
This is a regression from 74a4484e "calc: on tab switching document
view can be restored to a wrong position"

Moreover the passed index was wrong when tab is dragged on the right
wrt to current tab position. Now the correct zero based index is
always computed.

Signed-off-by: Marco Cecchetti <marco.cecchetti@collabora.com>
Change-Id: I7395773da5a846ce61d8508bbd1d6e2e833d4511

browser/src/control/Control.Tabs.js
@@ -332,7 +332,11 @@ L.Control.Tabs = L.Control.extend({
 
 	//selected sheet is moved to new index
 	_moveSheet: function (newIndex) {
-		this._map._docLayer._sheetSwitch.updateOnSheetMoved(this._map.getPartNumber(), newIndex - 1);
+		var currentTab = this._map.getCurrentPartNumber();
+		// odd but true: the base index changes according to the tab is dragged
+		// on the left or on the right wrt the current tab position
+		var newIndexZeroBased = newIndex > currentTab ? newIndex - 2 : newIndex - 1;
+		this._map._docLayer._sheetSwitch.updateOnSheetMoved(currentTab, newIndexZeroBased);
 		this._map.sendUnoCommand('.uno:Move?Copy:bool=false&UseCurrentDocument:bool=true&Index=' + newIndex);
 	},
 

browser/src/layer/tile/SheetSwitch.ts
@@ -54,7 +54,6 @@ export class SheetSwitchViewRestore {
 	}
 
 	public updateOnSheetMoved(oldIndex: number, newIndex: number): void {
-		window.app.console.log('SheetSwitchViewRestore.updateOnSheetMoved: oldIndex: ' + oldIndex + ', newIndex: ' + newIndex);
 		if (oldIndex < 0 || newIndex < 0 || oldIndex === newIndex)
 			return;
 