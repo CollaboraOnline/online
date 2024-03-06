1a0fc1bf43a8f7ab553cd522ee2dcbf658607998
Michael Meeks <michael.meeks@collabora.com>
2024-03-06 16:36:51 +0000

    Fix JS lint warnings during make check.
    Signed-off-by: Michael Meeks <michael.meeks@collabora.com>
    Change-Id: If1444e25d86268580ac9a92f8541ddf344c062cf

--- browser/src/docstatefunctions.js
+++ browser/src/docstatefunctions.js
@@ -1,4 +1,4 @@
-/* global Proxy _ */
+/* -*- js-indent-level: 8 -*- */
 /*
  * Copyright the Collabora Online contributors.
  *
@@ -8,11 +8,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-
 /*
-    Doc state functions.
-    This file is meant to be used for setting and getting the document states.
-*/
+ * Doc state functions.
+ * This file is meant to be used for setting and getting the document states.
+ */
 
 /* global app */
 
