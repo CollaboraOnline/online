dfc523f15a0a7fb83be41475600356ff2161fb45
Gökay Şatır <gokaysatir@gmail.com>
2024-03-11 18:41:18 +0300

    Merge onContextMenu and onLongPress events.
    Signed-off-by: Gökay Şatır <gokaysatir@gmail.com>
    Change-Id: Icd4452a30479591035282a789ad6c9ae742c1b0b

--- browser/src/layer/tile/AutoFillMarkerSection.ts
+++ browser/src/layer/tile/AutoFillMarkerSection.ts
@@ -267,7 +267,6 @@ class AutoFillMarkerSection extends CanvasSectionObject {
 		e.stopPropagation(); // Stop native event.
 	}
 	public onContextMenu () { return; }
-	public onLongPress () { return; }
 	public onMultiTouchStart () { return; }
 	public onMultiTouchMove () { return; }
 	public onMultiTouchEnd () { return; }
