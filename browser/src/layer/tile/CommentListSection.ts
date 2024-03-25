f0991d57801af193451429d363f4d5fabcac634c
Pranam Lashkari <lpranam@collabora.com>
2024-03-25 20:42:09 +0530

    annotation: refectored reply node count code
    now reply count node  multipurpose, it also indicates
    if a comment which was being edited is collapsed
    
    Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
    Change-Id: I18a676885deea0afa30eb20505a76239e11c5493

--- browser/src/layer/tile/CommentListSection.ts
+++ browser/src/layer/tile/CommentListSection.ts
@@ -683,7 +683,7 @@ export class CommentSection extends CanvasSectionObject {
 			this.sectionProperties.commentList[rootIndex].sectionProperties.container.style.visibility = '';
 			rootIndex++;
 		}
-		rootComment.updateThreadCountIndicator();
+		rootComment.updateThreadInfoIndicator();
 	}
 
 	private collapseReplies(rootIndex: number, rootId: number) {
@@ -691,8 +691,8 @@ export class CommentSection extends CanvasSectionObject {
 
 		for (var i = lastChild; i > rootIndex; i--) {
 			this.sectionProperties.commentList[i].sectionProperties.container.style.display = 'none';
-			this.sectionProperties.commentList[i].updateThreadCountIndicator();
 		}
+		this.sectionProperties.commentList[i].updateThreadInfoIndicator();
 	}
 
 	private cssToCorePixels(cssPixels: number) {
@@ -764,9 +764,7 @@ export class CommentSection extends CanvasSectionObject {
 
 			if (this.isCollapsed) {
 				this.showCollapsedReplies(idx);
-				if (docType === 'text') {
-					selectedComment.sectionProperties.replyCountNode.style.display = 'none';
-				}
+				selectedComment.updateThreadInfoIndicator();
 			}
 
 			this.update();
@@ -1382,7 +1380,7 @@ export class CommentSection extends CanvasSectionObject {
 		}
 
 		if (this.sectionProperties.docLayer._docType === 'text')
-			this.updateReplyCount();
+			this.updateThreadInfoIndicator();
 	}
 
 	public selectById (commentId: any): void {
@@ -1758,11 +1756,11 @@ export class CommentSection extends CanvasSectionObject {
 
 	private update (): void {
 		if (this.sectionProperties.docLayer._docType === 'text')
-			this.updateReplyCount();
+			this.updateThreadInfoIndicator();
 		this.layout();
 	}
 
-	private updateReplyCount(): void {
+	private updateThreadInfoIndicator(): void {
 		for (var i = 0; i < this.sectionProperties.commentList.length; i++) {
 			var comment = this.sectionProperties.commentList[i];
 			var replyCount = 0;
@@ -1784,9 +1782,9 @@ export class CommentSection extends CanvasSectionObject {
 				}
 			}
 			if (anyEdit)
-				comment.updateThreadCountIndicator('!');
+				comment.updateThreadInfoIndicator('!');
 			else
-				comment.updateThreadCountIndicator(replyCount);
+				comment.updateThreadInfoIndicator(replyCount);
 		}
 	}
 
--- cypress_test/integration_tests/desktop/writer/annotation_spec.js
+++ cypress_test/integration_tests/desktop/writer/annotation_spec.js
@@ -127,8 +127,8 @@ describe(['tagdesktop'], 'Collapsed Annotation Tests', function() {
 		desktopHelper.insertComment(undefined, false);
 		cy.cGet('#map').focus();
 		helper.typeIntoDocument('{home}');
-		cy.cGet('.cool-annotation-reply-count-collapsed').should('have.text','!');
-		cy.cGet('.cool-annotation-reply-count-collapsed').should('be.visible');
+		cy.cGet('.cool-annotation-info-collapsed').should('have.text','!');
+		cy.cGet('.cool-annotation-info-collapsed').should('be.visible');
 		cy.cGet('.cool-annotation-img').click();
 		cy.cGet('.cool-annotation-autosavelabel').should('be.visible');
 		cy.cGet('#annotation-save-1').click();
@@ -136,10 +136,10 @@ describe(['tagdesktop'], 'Collapsed Annotation Tests', function() {
 		cy.cGet('.cool-annotation-img').click();
 		cy.cGet('#annotation-content-area-1').should('have.text','some text0');
 		cy.cGet('.cool-annotation-autosavelabel').should('be.not.visible');
-		cy.cGet('.cool-annotation-reply-count-collapsed').should('not.have.text','!');
+		cy.cGet('.cool-annotation-info-collapsed').should('not.have.text','!');
 		cy.cGet('#map').focus();
 		helper.typeIntoDocument('{home}');
-		cy.cGet('.cool-annotation-reply-count-collapsed').should('be.not.visible');
+		cy.cGet('.cool-annotation-info-collapsed').should('be.not.visible');
 
 		helper.closeDocument(testFileName, '');
 		helper.beforeAll(testFileName, 'writer', true, false, false, true);
