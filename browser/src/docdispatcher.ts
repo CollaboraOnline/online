CollaboraOnline/online

commit c402ecd41a38c24336a3e9f51c143b6b00926d1e

Introduce docdispatcher.js.
In order to handle commands in a more structured way.

Signed-off-by: Gökay Şatır <gokaysatir@gmail.com>
Change-Id: If7e2aed089cda1df59b2bf2b454b540d36391df0

browser/src/docdispatcher.ts
@@ -1,0 +1,384 @@
+/* global Proxy _ */
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/* global app _ */
+
+// app.dispatcher.dispatch() will be used to call some actions so we can share the code
+
+class Dispatcher {
+	private actionsMap: any = {};
+
+	private addGeneralCommands() {
+		this.actionsMap['remotelink'] = function () {
+			app.map.fire('postMessage', { msgId: 'UI UI_PickLink' });
+		};
+		this.actionsMap['hyperlinkdialog'] = function () {
+			app.map.showHyperlinkDialog();
+		};
+		this.actionsMap['rev-history'] = function () {
+			app.map.openRevisionHistory();
+		};
+		this.actionsMap['shareas'] = function () {
+			app.map.openShare();
+		};
+
+		this.actionsMap['savecomments'] = function () {
+			if (app.isCommentEditingAllowed()) {
+				app.map.fire('postMessage', { msgId: 'UI_Save' });
+				if (!app.map._disableDefaultAction['UI_Save']) {
+					app.map.save(false, false);
+				}
+			}
+		};
+
+		this.actionsMap['charmapcontrol'] = function () {
+			app.map.sendUnoCommand('.uno:InsertSymbol');
+		};
+		this.actionsMap['closetablet'] = function () {
+			app.map.uiManager.enterReadonlyOrClose();
+		};
+
+		this.actionsMap['toggledarktheme'] = function () {
+			app.map.uiManager.toggleDarkMode();
+		};
+		this.actionsMap['home-search'] = function () {
+			app.map.uiManager.focusSearch();
+		};
+		this.actionsMap['renamedocument'] = function () {
+			app.map.uiManager.renameDocument();
+		};
+		this.actionsMap['togglewasm'] = function () {
+			app.map.uiManager.toggleWasm();
+		};
+
+		this.actionsMap['languagemenu'] = function () {
+			app.map.fire('morelanguages');
+		};
+		this.actionsMap['morelanguages-selection'] = function () {
+			app.map.fire('morelanguages', { applyto: 'selection' });
+		};
+		this.actionsMap['morelanguages-paragraph'] = function () {
+			app.map.fire('morelanguages', { applyto: 'paragraph' });
+		};
+		this.actionsMap['morelanguages-all'] = function () {
+			app.map.fire('morelanguages', { applyto: 'all' });
+		};
+		this.actionsMap['localgraphic'] = function () {
+			L.DomUtil.get('insertgraphic').click();
+		};
+		this.actionsMap['remotegraphic'] = function () {
+			app.map.fire('postMessage', { msgId: 'UI_InsertGraphic' });
+		};
+
+		this.actionsMap['showhelp'] = function () {
+			app.map.showHelp('online-help-content');
+		};
+
+		this.actionsMap['focustonotebookbar'] = function () {
+			const tabsContainer = document.getElementsByClassName(
+				'notebookbar-tabs-container',
+			)[0].children[0];
+			let elementToFocus: HTMLButtonElement;
+			if (tabsContainer) {
+				for (let i = 0; i < tabsContainer.children.length; i++) {
+					if (tabsContainer.children[i].classList.contains('selected')) {
+						elementToFocus = tabsContainer.children[i] as HTMLButtonElement;
+						break;
+					}
+				}
+			}
+			if (!elementToFocus)
+				elementToFocus = document.getElementById(
+					'Home-tab-label',
+				) as HTMLButtonElement;
+
+			elementToFocus.focus();
+		};
+
+		this.actionsMap['saveas'] = function () {
+			if (app.map && app.map.uiManager.getCurrentMode() === 'notebookbar') {
+				app.map.openSaveAs(); // Opens save as dialog if integrator supports it.
+			}
+		};
+
+		this.actionsMap['insertcomment'] = function () {
+			app.map.insertComment();
+		};
+	}
+
+	private addExportCommands() {
+		this.actionsMap['exportpdf'] = function () {
+			app.map.sendUnoCommand('.uno:ExportToPDF', {
+				SynchronMode: {
+					type: 'boolean',
+					value: false,
+				},
+			});
+		};
+
+		this.actionsMap['exportdirectpdf'] = function () {
+			app.map.sendUnoCommand('.uno:ExportDirectToPDF', {
+				SynchronMode: {
+					type: 'boolean',
+					value: false,
+				},
+			});
+		};
+
+		this.actionsMap['exportepub'] = function () {
+			app.map.sendUnoCommand('.uno:ExportToEPUB', {
+				SynchronMode: {
+					type: 'boolean',
+					value: false,
+				},
+			});
+		};
+	}
+
+	private addCalcCommands() {
+		this.actionsMap['acceptformula'] = function () {
+			if (window.mode.isMobile()) {
+				app.map.focus();
+				app.map._docLayer.postKeyboardEvent(
+					'input',
+					app.map.keyboard.keyCodes.enter,
+					app.map.keyboard._toUNOKeyCode(app.map.keyboard.keyCodes.enter),
+				);
+			} else {
+				this.sendUnoCommand('.uno:AcceptFormula');
+			}
+
+			app.map.onFormulaBarBlur();
+			app.map.formulabarBlur();
+			app.map.formulabarSetDirty();
+		};
+
+		this.actionsMap['cancelformula'] = function () {
+			app.map.sendUnoCommand('.uno:Cancel');
+			app.map.onFormulaBarBlur();
+			app.map.formulabarBlur();
+			app.map.formulabarSetDirty();
+		};
+
+		this.actionsMap['startformula'] = function () {
+			app.map.sendUnoCommand('.uno:StartFormula');
+			app.map.onFormulaBarFocus();
+			app.map.formulabarFocus();
+			app.map.formulabarSetDirty();
+		};
+
+		this.actionsMap['functiondialog'] = function () {
+			if (window.mode.isMobile() && app.map._functionWizardData) {
+				app.map._docLayer._closeMobileWizard();
+				app.map._docLayer._openMobileWizard(app.map._functionWizardData);
+				app.map.formulabarSetDirty();
+			} else {
+				app.map.sendUnoCommand('.uno:FunctionDialog');
+			}
+		};
+
+		this.actionsMap['print-active-sheet'] = function () {
+			const currentSheet = app.map._docLayer._selectedPart + 1;
+			const options = {
+				ExportFormFields: {
+					type: 'boolean',
+					value: false,
+				},
+				ExportNotes: {
+					type: 'boolean',
+					value: false,
+				},
+				SheetRange: {
+					type: 'string',
+					value: currentSheet + '-' + currentSheet,
+				},
+			};
+			const optionsString = JSON.stringify(options);
+			this.print(optionsString);
+		};
+
+		this.actionsMap['print-all-sheets'] = function () {
+			app.map.print();
+		};
+		this.actionsMap['togglerelative'] = function () {
+			app.map.sendUnoCommand('.uno:ToggleRelative');
+		};
+		this.actionsMap['focusonaddressinput'] = function () {
+			document.getElementById('addressInput').focus();
+		};
+	}
+
+	private addImpressAndDrawCommands() {
+		this.actionsMap['presentation'] = function () {
+			app.map.fire('fullscreen');
+		};
+		this.actionsMap['presentinwindow'] = function () {
+			app.map.fire('presentinwindow');
+		};
+
+		this.actionsMap['deletepage'] = function () {
+			let msg: string;
+			if (app.map.getDocType() === 'presentation') {
+				msg = _('Are you sure you want to delete this slide?');
+			} else {
+				/* drawing */
+				msg = _('Are you sure you want to delete this page?');
+			}
+			app.map.uiManager.showInfoModal(
+				'deleteslide-modal',
+				_('Delete'),
+				msg,
+				'',
+				_('OK'),
+				function () {
+					app.map.deletePage();
+				},
+				true,
+				'deleteslide-modal-response',
+			);
+		};
+
+		this.actionsMap['previouspart'] = function () {
+			app.map._docLayer._preview._scrollViewByDirection('prev');
+			if (app.file.fileBasedView) app.map._docLayer._checkSelectedPart();
+		};
+
+		this.actionsMap['nextpart'] = function () {
+			app.map._docLayer._preview._scrollViewByDirection('next');
+			if (app.file.fileBasedView) app.map._docLayer._checkSelectedPart();
+		};
+
+		this.actionsMap['lastpart'] = function () {
+			if (app && app.file.fileBasedView === true) {
+				const partToSelect = app.map._docLayer._parts - 1;
+				app.map._docLayer._preview._scrollViewToPartPosition(partToSelect);
+				app.map._docLayer._checkSelectedPart();
+			}
+		};
+
+		this.actionsMap['firstpart'] = function () {
+			if (app && app.file.fileBasedView === true) {
+				const partToSelect = 0;
+				app.map._docLayer._preview._scrollViewToPartPosition(partToSelect);
+				app.map._docLayer._checkSelectedPart();
+			}
+		};
+	}
+
+	private addZoteroCommands() {
+		this.actionsMap['zoteroaddeditcitation'] = function () {
+			app.map.zotero.handleItemList();
+		};
+		this.actionsMap['zoterosetdocprefs'] = function () {
+			app.map.zotero.handleStyleList();
+		};
+		this.actionsMap['zoteroaddeditbibliography'] = function () {
+			app.map.zotero.insertBibliography();
+		};
+		this.actionsMap['zoteroaddnote'] = function () {
+			app.map.zotero.handleInsertNote();
+		};
+		this.actionsMap['zoterorefresh'] = function () {
+			app.map.zotero.refreshCitationsAndBib();
+		};
+		this.actionsMap['zoterounlink'] = function () {
+			app.map.zotero.unlinkCitations();
+		};
+	}
+
+	private addWriterCommands() {
+		this.actionsMap['.uno:ShowResolvedAnnotations'] = function () {
+			const items = this['stateChangeHandler'];
+			let val = items.getItemValue('.uno:ShowResolvedAnnotations');
+			val = val === 'true' || val === true;
+			app.map.showResolvedComments(!val);
+		};
+
+		this.actionsMap['.uno:AcceptAllTrackedChanges'] = function () {
+			app.map.sendUnoCommand('.uno:AcceptAllTrackedChanges');
+			app.socket.sendMessage('commandvalues command=.uno:ViewAnnotations');
+		};
+
+		this.actionsMap['.uno:RejectAllTrackedChanges'] = function () {
+			app.map.sendUnoCommand('.uno:RejectAllTrackedChanges');
+			const commentSection = app.sectionContainer.getSectionWithName(
+				L.CSections.CommentList.name,
+			);
+			commentSection.rejectAllTrackedCommentChanges();
+		};
+	}
+
+	constructor() {
+		this.addGeneralCommands();
+		this.addExportCommands();
+
+		if (app.map._docLayer._docType === 'text') {
+			this.addWriterCommands();
+			this.addZoteroCommands();
+		} else if (app.map._docLayer._docType === 'spreadsheet') {
+			this.addCalcCommands();
+		} else if (
+			['presentation', 'drawing'].includes(app.map._docLayer._docType)
+		) {
+			this.addImpressAndDrawCommands();
+		}
+	}
+
+	public dispatch(action: string) {
+		// Don't allow to execute new actions while any dialog is visible.
+		// It prevents launching multiple instances of the same dialog.
+		if (
+			app.map.dialog.hasOpenedDialog() ||
+			(app.map.jsdialog && app.map.jsdialog.hasDialogOpened())
+		) {
+			app.map.dialog.blinkOpenDialog();
+			console.debug('Cannot dispatch: ' + action + ' when dialog is opened.');
+			return;
+		}
+
+		if (action.indexOf('saveas-') === 0) {
+			const format = action.substring('saveas-'.length);
+			app.map.openSaveAs(format);
+			return;
+		} else if (action.indexOf('downloadas-') === 0) {
+			const format = action.substring('downloadas-'.length);
+			let fileName = app.map['wopi'].BaseFileName;
+			fileName = fileName.substr(0, fileName.lastIndexOf('.'));
+			fileName = fileName === '' ? 'document' : fileName;
+			app.map.downloadAs(fileName + '.' + format, format);
+			return;
+		}
+
+		if (action.indexOf('exportas-') === 0) {
+			const format = action.substring('exportas-'.length);
+			app.map.openSaveAs(format);
+			return;
+		}
+
+		if (
+			action === '.uno:Copy' ||
+			action === '.uno:Cut' ||
+			action === '.uno:Paste' ||
+			action === '.uno:PasteSpecial'
+		) {
+			app.map._clip.filterExecCopyPaste(action);
+		}
+
+		if (this.actionsMap[action] !== undefined) {
+			this.actionsMap[action]();
+			return;
+		}
+
+		console.error('unknown dispatch: "' + action + '"');
+	}
+}
+
+app.definitions['dispatcher'] = Dispatcher;

browser/Makefile.am
@@ -194,6 +194,7 @@ COOL_LIBS_JS_DST = $(patsubst %.js,$(DIST_FOLDER)/%.js,$(COOL_LIBS_JS))
 COOL_JS_LST =\
 	src/docstate.js \
 	src/docstatefunctions.js \
+	src/docdispatcher.ts \
 	src/Leaflet.js \
 	src/errormessages.js \
 	src/unocommands.js \
@@ -815,6 +816,7 @@ pot:
 		src/core/Debug.js \
 		src/docstate.js \
 		src/docstatefunctions.js \
+		src/docdispatcher.ts \
 		src/errormessages.js \
 		src/layer/tile/CanvasTileLayer.js \
 		src/layer/tile/CommentListSection.ts \

browser/src/control/Control.JSDialogBuilder.js
@@ -2443,7 +2443,7 @@ L.Control.JSDialogBuilder = L.Control.extend({
 					if (e.commandName === data.command)
 					{
 						// in some cases we will get both property like state and disabled
-						// to handle it we will set disable var based on INCOMING info (ex: .uno:ParaRightToLft) 
+						// to handle it we will set disable var based on INCOMING info (ex: .uno:ParaRightToLft)
 						disabled = e.disabled || e.state == 'disabled';
 						updateFunction();
 					}
@@ -2549,7 +2549,7 @@ L.Control.JSDialogBuilder = L.Control.extend({
 
 		if (!builder.map.isLockedItem(data)) {
 			$(control.container).click(function () {
-				builder.map.dispatch(data.command);
+				app.dispatcher.dispatch(data.command);
 			});
 		}
 
@@ -2573,7 +2573,7 @@ L.Control.JSDialogBuilder = L.Control.extend({
 		if (!builder.map.isLockedItem(data)) {
 			$(control.container).click(function (e) {
 				e.preventDefault();
-				builder.map.dispatch(data.command);
+				app.dispatcher.dispatch(data.command);
 			});
 		}
 

browser/src/control/Control.Menubar.js
@@ -1873,9 +1873,9 @@ L.Control.Menubar = L.Control.extend({
 		} else if (id === 'saveas' && type !== 'menu') { // jsdialog has no type='action'
 			this._map.openSaveAs();
 		} else if (id === 'savecomments') {
-			this._map.dispatch('savecomments');
+			app.dispatcher.dispatch('savecomments');
 		} else if (id === 'shareas' || id === 'ShareAs') {
-			this._map.dispatch('shareas');
+			app.dispatcher.dispatch('shareas');
 		} else if (id === 'print') {
 			this._map.print();
 		} else if (id.startsWith('downloadas-')
@@ -1889,7 +1889,7 @@ L.Control.Menubar = L.Control.extend({
 			|| id === 'home-search'
 			|| id === 'print-active-sheet'
 			|| id == 'print-all-sheets') {
-			this._map.dispatch(id);
+			app.dispatcher.dispatch(id);
 		} else if (id === 'insertcomment') {
 			this._map.insertComment();
 		} else if (id === 'insertgraphic') {
@@ -1901,7 +1901,7 @@ L.Control.Menubar = L.Control.extend({
 		} else if (id === 'zoomin' && this._map.getZoom() < this._map.getMaxZoom()) {
 			this._map.zoomIn(1, null, true /* animate? */);
 		} else if (id === 'showresolved') {
-			this._map.dispatch('.uno:ShowResolvedAnnotations');
+			app.dispatcher.dispatch('.uno:ShowResolvedAnnotations');
 		} else if (id === 'zoomout' && this._map.getZoom() > this._map.getMinZoom()) {
 			this._map.zoomOut(1, null, true /* animate? */);
 		} else if (id === 'zoomreset') {
@@ -1947,11 +1947,11 @@ L.Control.Menubar = L.Control.extend({
 		} else if (id === 'forum') {
 			window.open('https://forum.collaboraonline.com', '_blank');
 		} else if (id === 'inserthyperlink') {
-			this._map.dispatch('hyperlinkdialog');
+			app.dispatcher.dispatch('hyperlinkdialog');
 		} else if (id === 'keyboard-shortcuts' || id === 'online-help') {
 			this._map.showHelp(id + '-content');
 		} else if (L.Params.revHistoryEnabled && (id === 'rev-history' || id === 'Rev-History' || id === 'last-mod')) {
-			this._map.dispatch('rev-history');
+			app.dispatcher.dispatch('rev-history');
 		} else if (id === 'closedocument') {
 			window.onClose();
 		} else if (id === 'repair') {
@@ -1979,9 +1979,9 @@ L.Control.Menubar = L.Control.extend({
 		} else if (id.indexOf('morelanguages-') != -1) {
 			this._map.fire('morelanguages', { applyto: id.substr('morelanguages-'.length) });
 		} else if (id === 'acceptalltrackedchanges') {
-			this._map.dispatch('.uno:AcceptAllTrackedChanges');
+			app.dispatcher.dispatch('.uno:AcceptAllTrackedChanges');
 		} else if (id === 'rejectalltrackedchanges') {
-			this._map.dispatch('.uno:RejectAllTrackedChanges');
+			app.dispatcher.dispatch('.uno:RejectAllTrackedChanges');
 		}
 		// Inform the host if asked
 		if (postmessage)

browser/src/control/Control.MobileTopBar.js
@@ -120,10 +120,10 @@ L.Control.MobileTopBar = L.Control.extend({
 			this.map.executeUnoAction(item);
 		}
 		else if (id === 'cancelformula') {
-			this.map.dispatch('cancelformula');
+			app.dispatcher.dispatch('cancelformula');
 		}
 		else if (id === 'acceptformula') {
-			this.map.dispatch('acceptformula');
+			app.dispatcher.dispatch('acceptformula');
 		}
 		else if (id === 'comment_wizard') {
 			if (window.commentWizard) {

browser/src/control/Control.PartsPreview.js
@@ -308,7 +308,7 @@ L.Control.PartsPreview = L.Control.extend({
 					},
 					delete: {
 						name: _UNO(that._map._docLayer._docType == 'presentation' ? '.uno:DeleteSlide' : '.uno:DeletePage', 'presentation'),
-						callback: function() { that._map.dispatch('deletepage'); },
+						callback: function() { app.dispatcher.dispatch('deletepage'); },
 						visible: function() {
 							return that._map._docLayer._parts > 1;
 						}

browser/src/control/Control.PresentationBar.js
@@ -12,7 +12,7 @@
  * L.Control.PresentationBar
  */
 
-/* global $ w2ui _ _UNO */
+/* global $ w2ui _ _UNO app */
 L.Control.PresentationBar = L.Control.extend({
 	options: {
 		shownavigation: true
@@ -109,7 +109,7 @@ L.Control.PresentationBar = L.Control.extend({
 			this.map.duplicatePage();
 		}
 		else if (id === 'deletepage') {
-			this.map.dispatch('deletepage');
+			app.dispatcher.dispatch('deletepage');
 		}
 		else if (id === 'showslide') {
 			this.map.showSlide();

browser/src/control/Control.Toolbar.js
@@ -103,7 +103,7 @@ function onClick(e, id, item) {
 		map.executeUnoAction(item);
 	}
 	else if (item.id === 'print-active-sheet' || item.id === 'print-all-sheets') {
-		map.dispatch(item.id);
+		app.dispatcher.dispatch(item.id);
 	}
 	else if (id === 'print') {
 		map.print();

commit 31551628b3aac8a40ffc6c1f1a7fc927d429826a

annotation: fixed show resolve button state indication
regression from c402ecd41a38c24336a3e9f51c143b6b00926d1e

Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
Change-Id: I2dfae0161db18ed2707c26e8b380f94991d61710

browser/src/docdispatcher.ts
@@ -334,8 +334,9 @@ class Dispatcher {
 	}
 
 	private addWriterCommands() {
+
 		this.actionsMap['.uno:ShowResolvedAnnotations'] = function () {
-			const items = this['stateChangeHandler'];
+			const items = app.map['stateChangeHandler'];
 			let val = items.getItemValue('.uno:ShowResolvedAnnotations');
 			val = val === 'true' || val === true;
 			app.map.showResolvedComments(!val);