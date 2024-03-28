b95943c36c769388a47fa35f8c14959c59e64ad9
Szymon Kłos <szymon.klos@collabora.com>
2024-03-28 14:49:00 +0100

    remove-w2ui: permission mode indicator without hover effect
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: Ibd540c00a7aaf53b0a831847ae0eb57137e418c4

--- browser/src/control/jsdialog/Widget.HTMLContent.ts
+++ browser/src/control/jsdialog/Widget.HTMLContent.ts
@@ -27,8 +27,7 @@ interface HtmlContentJson {
 }
 
 function getPermissionModeHtml(isReadOnlyMode: boolean, canUserWrite: boolean) {
-	var permissionModeDiv =
-		'<div id="PermissionMode" class="cool-font jsdialog unotoolbutton ';
+	var permissionModeDiv = '<div id="PermissionMode" class="cool-font jsdialog ';
 	if (isReadOnlyMode && !canUserWrite) {
 		permissionModeDiv +=
 			' status-readonly-mode" title="' +
