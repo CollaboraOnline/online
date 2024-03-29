22aff9cc0f530b52e25cb0f1367228cef758274c
Szymon Kłos <szymon.klos@collabora.com>
2024-03-29 16:09:35 +0100

    cypress: fix multiuser tests after w2ui removing
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I53c5ed04a3ba3f63ebbca8f2322e34cf8f707a04

--- cypress_test/integration_tests/multiuser/writer/invalidations_spec.js
+++ cypress_test/integration_tests/multiuser/writer/invalidations_spec.js
@@ -31,13 +31,13 @@ describe(['tagmultiuser'], 'Joining a document should not trigger an invalidatio
 		ceHelper.type('X');
 
 		cy.cSetActiveFrame('#iframe2');
-		cy.cGet('#toolbar-down #StateWordCount').should('have.text', '    1 word, 1 character');
+		cy.cGet('#toolbar-down #StateWordCount').should('have.text', '1 word, 1 character');
 
 		cy.cSetActiveFrame('#iframe1');
 		writerHelper.selectAllTextOfDoc();
-		cy.cGet('#toolbar-down #StateWordCount').should('have.text', '    Selected: 1 word, 1 character');
+		cy.cGet('#toolbar-down #StateWordCount').should('have.text', 'Selected: 1 word, 1 character');
 		cy.cGet('.leaflet-layer').click();
-		cy.cGet('#toolbar-down #StateWordCount').should('have.text', '    1 word, 1 character');
+		cy.cGet('#toolbar-down #StateWordCount').should('have.text', '1 word, 1 character');
 
 		cy.cGet('.empty-deltas').then(($before) => {
 			const beforeCount = $before.text();
@@ -48,9 +48,9 @@ describe(['tagmultiuser'], 'Joining a document should not trigger an invalidatio
 
 			cy.cSetActiveFrame('#iframe1');
 			writerHelper.selectAllTextOfDoc();
-			cy.cGet('#toolbar-down #StateWordCount').should('have.text', '    Selected: 1 word, 1 character');
+			cy.cGet('#toolbar-down #StateWordCount').should('have.text', 'Selected: 1 word, 1 character');
 			cy.cGet('.leaflet-layer').click();
-			cy.cGet('#toolbar-down #StateWordCount').should('have.text', '    1 word, 1 character');
+			cy.cGet('#toolbar-down #StateWordCount').should('have.text', '1 word, 1 character');
 
 			cy.cGet('.empty-deltas').should(($after) => {
 				expect($after.text()).to.eq(beforeCount);
@@ -71,13 +71,13 @@ describe(['tagmultiuser'], 'Joining a document should not trigger an invalidatio
 		ceHelper.type('X');
 
 		cy.cSetActiveFrame('#iframe2');
-		cy.cGet('#toolbar-down #StateWordCount').should('have.text', '    1 word, 1 character');
+		cy.cGet('#toolbar-down #StateWordCount').should('have.text', '1 word, 1 character');
 
 		cy.cSetActiveFrame('#iframe1');
 		writerHelper.selectAllTextOfDoc();
-		cy.cGet('#toolbar-down #StateWordCount').should('have.text', '    Selected: 1 word, 1 character');
+		cy.cGet('#toolbar-down #StateWordCount').should('have.text', 'Selected: 1 word, 1 character');
 		cy.cGet('.leaflet-layer').click();
-		cy.cGet('#toolbar-down #StateWordCount').should('have.text', '    1 word, 1 character');
+		cy.cGet('#toolbar-down #StateWordCount').should('have.text', '1 word, 1 character');
 
 		cy.cGet('.empty-deltas').then(($before) => {
 			var beforeCount = parseInt($before.text());
@@ -94,11 +94,11 @@ describe(['tagmultiuser'], 'Joining a document should not trigger an invalidatio
 
 			cy.cSetActiveFrame('#iframe1');
 			writerHelper.selectAllTextOfDoc();
-			cy.cGet('#toolbar-down #StateWordCount').should('have.text', '    Selected: 1 word, 1 character');
+			cy.cGet('#toolbar-down #StateWordCount').should('have.text', 'Selected: 1 word, 1 character');
 			cy.cGet('.leaflet-layer').click();
-			cy.cGet('#toolbar-down #StateWordCount').should('have.text', '    1 word, 1 character');
+			cy.cGet('#toolbar-down #StateWordCount').should('have.text', '1 word, 1 character');
 			ceHelper.type('X');
-			cy.cGet('#toolbar-down #StateWordCount').should('have.text', '    1 word, 2 characters');
+			cy.cGet('#toolbar-down #StateWordCount').should('have.text', '1 word, 2 characters');
 
 			cy.cGet('.empty-deltas').should(($after) => {
 				// allow one row of empty deltas, the case this protects regression against
