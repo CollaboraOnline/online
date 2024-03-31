4af7d734f461ef411429ccbaac5146433abfc879
Szymon Kłos <szymon.klos@collabora.com>
2024-03-31 22:42:43 +0200

    remove-w2ui: stop using old toolbars on tablet
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: Id8fd8e67c80eab7f48d1c519ecddb3d8dfd16062

--- browser/src/control/Control.UIManager.js
+++ browser/src/control/Control.UIManager.js
@@ -54,8 +54,7 @@ L.Control.UIManager = L.Control.extend({
 		map.on('unblockUI', this.unblockUI, this);
 
 		$('#toolbar-wrapper').on('click', function (event) {
-			if (event.target.parentElement.id === 'toolbar-up' || // checks if clicked on empty part of the toolbar on tabbed view
-				event.target.id === 'tb_editbar_item_item_64') // checks if clicked on empty part of the toolbar on compact view
+			if (event.target.parentElement.id === 'toolbar-up') // checks if clicked on empty part of the toolbar on tabbed view
 				that.map.fire('editorgotfocus');
 		});
 
@@ -348,7 +347,7 @@ L.Control.UIManager = L.Control.extend({
 			JSDialog.PresentationBar(this.map);
 		}
 
-		if (window.mode.isMobile() || (window.mode.isTablet() && !enableNotebookbar)) {
+		if (window.mode.isMobile()) {
 			this.map.on('updatetoolbarcommandvalues', function() {
 				w2ui['editbar'].refresh();
 			});
@@ -782,7 +781,7 @@ L.Control.UIManager = L.Control.extend({
 		var obj = $('.unfold');
 		obj.removeClass('w2ui-icon unfold');
 		obj.addClass('w2ui-icon fold');
-		$('#tb_editbar_item_fold').prop('title', _('Hide Menu'));
+		$('#fold').prop('title', _('Hide Menu'));
 
 		if (this._notebookbarShouldBeCollapsed)
 			this.collapseNotebookbar();
@@ -805,7 +804,7 @@ L.Control.UIManager = L.Control.extend({
 		var obj = $('.fold');
 		obj.removeClass('w2ui-icon fold');
 		obj.addClass('w2ui-icon unfold');
-		$('#tb_editbar_item_fold').prop('title', _('Show Menu'));
+		$('#fold').prop('title', _('Show Menu'));
 	},
 
 	isMenubarHidden: function() {
