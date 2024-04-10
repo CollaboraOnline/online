CollaboraOnline/online

commit e855aa6114bda90fdd64fbcbb32a77adcf7938f4

User avatar should be consistent after mode change
- User avatar should first consider is there is already extract info available or not
- if we already have that image source data then please do not set default value
- also when mode change we do preform some refresh function on all icons
    - `map.on('themechanged', setupIcon, this);`
- in that case as well, first check if we have user extract info or not
- and based on that set image source URL
Signed-off-by: Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>

Change-Id: Ifbd543322f5222fa7717b40a5ae565ed2f3c9891
Signed-off-by: Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>

browser/src/control/Control.UserList.ts
@@ -163,17 +163,7 @@ class UserList extends L.Control {
 			img = L.DomUtil.create('img', 'avatar-img') as HTMLImageElement;
 		}
 
-		const defaultImage = L.LOUtil.getImageURL(
-			'user.svg',
-			this.map._docLayer._docType,
-		);
-
-		if (extraInfo !== undefined && extraInfo.avatar !== undefined) {
-			img.src = extraInfo.avatar;
-			img.addEventListener('error', () => (img.src = defaultImage));
-		} else {
-			img.src = defaultImage;
-		}
+		L.LOUtil.setUserImage(img, this.map);
 
 		img.alt = this.options.userAvatarAlt.replace('%user', username);
 
@@ -182,8 +172,6 @@ class UserList extends L.Control {
 		img.style.backgroundColor = 'var(--color-background-lighter)';
 
 		img.setAttribute('data-view-id', viewId.toString());
-
-		L.LOUtil.checkIfImageExists(img, true);
 
 		return img;
 	}

browser/src/core/LOUtil.js
@@ -118,6 +118,25 @@ L.LOUtil = {
 
 		map.on('themechanged', setupIcon, this);
 	},
+	setUserImage: function(img, map) {
+		var docType = map.getDocType();
+		// set avatar image if it exist in user extract info
+		var defaultImage = L.LOUtil.getImageURL('user.svg', docType);
+		var myViewId = map._docLayer._viewId;
+		var extraInfo = map._viewInfo[myViewId].userextrainfo;
+		if (extraInfo !== undefined && extraInfo.avatar !== undefined) {
+			// set user avatar
+			img.src = extraInfo.avatar;
+			// Track if error event is already bound to this image
+			img.addEventListener('error', function () {
+				img.src = defaultImage;
+				this.checkIfImageExists(img, true);
+			}.bind(this), {once:true});
+			return;
+		}
+		img.src = defaultImage;
+		this.checkIfImageExists(img, true);
+	},
 	getImageURL: function(imgName, docType) {
 		var defaultImageURL = this.getURL('images/' + imgName);
 		if (window.isLocalStorageAllowed) {

browser/src/layer/tile/CommentListSection.ts
@@ -462,7 +462,7 @@ export class CommentSection extends CanvasSectionObject {
 		var tdImg = L.DomUtil.create(tagTd, 'cool-annotation-img', tr);
 		var tdAuthor = L.DomUtil.create(tagTd, 'cool-annotation-author', tr);
 		var imgAuthor = L.DomUtil.create('img', 'avatar-img', tdImg);
-		L.LOUtil.setImage(imgAuthor, 'user.svg', this.map);
+		L.LOUtil.setUserImage(imgAuthor, this.map);
 		imgAuthor.setAttribute('width', 32);
 		imgAuthor.setAttribute('height', 32);
 		var authorAvatarImg = imgAuthor;

browser/src/layer/tile/CommentSection.ts
@@ -270,7 +270,7 @@ export class Comment extends CanvasSectionObject {
 		var tdAuthor = L.DomUtil.create('td', 'cool-annotation-author', tr);
 		var imgAuthor = L.DomUtil.create('img', 'avatar-img', tdImg);
 
-		L.LOUtil.setImage(imgAuthor, 'user.svg', this.map);
+		L.LOUtil.setUserImage(imgAuthor, this.map);
 		imgAuthor.setAttribute('width', this.sectionProperties.imgSize[0]);
 		imgAuthor.setAttribute('height', this.sectionProperties.imgSize[1]);
 

commit 3588919a902446a8277240ebc9222d2baa064171

Fix header avatar have the same image of current UserList
Problem :
 - Open the document which have some comment from other User
 - we can see that they are same as the logged in user avatar

regression
    - this is a regression from commit https://github.com/CollaboraOnline/online/pull/8640/commits/e855aa6114bda90fdd64fbcbb32a77adcf7938f4
    - we were paasing url of user which is logged in and that should not happning for other user avatar image

Solution: - while setUserImage function pass viewId
    - this will help to identify the user Info from map
Signed-off-by: Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>
Change-Id: I5ee8b41c2e58a4ba15ecf551b264f944aa926da8

browser/src/control/Control.UserList.ts
@@ -163,7 +163,7 @@ class UserList extends L.Control {
 			img = L.DomUtil.create('img', 'avatar-img') as HTMLImageElement;
 		}
 
-		L.LOUtil.setUserImage(img, this.map);
+		L.LOUtil.setUserImage(img, this.map, viewId);
 
 		img.alt = this.options.userAvatarAlt.replace('%user', username);
 

browser/src/core/LOUtil.js
@@ -118,12 +118,11 @@ L.LOUtil = {
 
 		map.on('themechanged', setupIcon, this);
 	},
-	setUserImage: function(img, map) {
+	setUserImage: function(img, map, viewId) {
 		var docType = map.getDocType();
 		// set avatar image if it exist in user extract info
 		var defaultImage = L.LOUtil.getImageURL('user.svg', docType);
-		var myViewId = map._docLayer._viewId;
-		var extraInfo = map._viewInfo[myViewId].userextrainfo;
+		var extraInfo = map._viewInfo[viewId].userextrainfo;
 		if (extraInfo !== undefined && extraInfo.avatar !== undefined) {
 			// set user avatar
 			img.src = extraInfo.avatar;

browser/src/layer/tile/CommentListSection.ts
@@ -462,7 +462,8 @@ export class CommentSection extends CanvasSectionObject {
 		var tdImg = L.DomUtil.create(tagTd, 'cool-annotation-img', tr);
 		var tdAuthor = L.DomUtil.create(tagTd, 'cool-annotation-author', tr);
 		var imgAuthor = L.DomUtil.create('img', 'avatar-img', tdImg);
-		L.LOUtil.setUserImage(imgAuthor, this.map);
+		var viewId = this.map._docLayer._viewId;
+		L.LOUtil.setUserImage(imgAuthor, this.map, viewId);
 		imgAuthor.setAttribute('width', 32);
 		imgAuthor.setAttribute('height', 32);
 		var authorAvatarImg = imgAuthor;

browser/src/layer/tile/CommentSection.ts
@@ -269,8 +269,8 @@ export class Comment extends CanvasSectionObject {
 		var tdImg = L.DomUtil.create('td', 'cool-annotation-img', tr);
 		var tdAuthor = L.DomUtil.create('td', 'cool-annotation-author', tr);
 		var imgAuthor = L.DomUtil.create('img', 'avatar-img', tdImg);
-
-		L.LOUtil.setUserImage(imgAuthor, this.map);
+		var viewId = this.map._docLayer._viewId;
+		L.LOUtil.setUserImage(imgAuthor, this.map, viewId);
 		imgAuthor.setAttribute('width', this.sectionProperties.imgSize[0]);
 		imgAuthor.setAttribute('height', this.sectionProperties.imgSize[1]);
 