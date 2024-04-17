4c5adb70e6946d8d9e01bf8d7b45744b02f1310b
Miklos Vajna <vmiklos@collabora.com>
2024-04-17 08:06:06 +0200

    cool#8648 clipboard: fix mobile/calc/apply_font_spec.js
    To work even if we don't automatically fetch HTML when a text selection
    is created. This was the first mobile suite that needed fixing.
    
    Signed-off-by: Miklos Vajna <vmiklos@collabora.com>
    Change-Id: Iad772d878d20d76bcf3770b7acc6abafa74859b2

--- cypress_test/integration_tests/mobile/calc/apply_font_spec.js
+++ cypress_test/integration_tests/mobile/calc/apply_font_spec.js
@@ -25,30 +25,38 @@ describe(['tagmobile', 'tagnextcloud', 'tagproxy'], 'Apply font changes.', funct
 	});
 
 	it('Apply bold.', function() {
+		helper.setDummyClipboardForCopy();
 		helper.clickOnIdle('#Bold');
 		calcHelper.selectEntireSheet();
+		helper.copy();
 		cy.cGet('#copy-paste-container table td b').should('exist');
 	});
 
 	it('Apply italic.', function() {
+		helper.setDummyClipboardForCopy();
 		helper.clickOnIdle('#Italic');
 		calcHelper.selectEntireSheet();
+		helper.copy();
 		cy.cGet('#copy-paste-container table td i').should('exist');
 	});
 
 	it('Apply underline.', function() {
+		helper.setDummyClipboardForCopy();
 		helper.clickOnIdle('#Underline');
 
 		calcHelper.selectEntireSheet();
+		helper.copy();
 
 		cy.cGet('#copy-paste-container table td u')
 			.should('exist');
 	});
 
 	it('Apply strikeout.', function() {
+		helper.setDummyClipboardForCopy();
 		helper.clickOnIdle('#Strikeout');
 
 		calcHelper.selectEntireSheet();
+		helper.copy();
 
 		cy.cGet('#copy-paste-container table td s')
 			.should('exist');
@@ -63,33 +71,39 @@ describe(['tagmobile', 'tagnextcloud', 'tagproxy'], 'Apply font changes.', funct
 	});
 
 	it('Apply font name.', function() {
+		helper.setDummyClipboardForCopy();
 		cy.cGet('#TextPropertyPanel').click();
 		cy.cGet('#fontnamecombobox').click();
 		cy.cGet('#fontnamecombobox').contains('.mobile-wizard.ui-combobox-text', 'Linux Libertine G').click();
 
 		calcHelper.selectEntireSheet();
+		helper.copy();
 
 		cy.cGet('#copy-paste-container table td font')
 			.should('have.attr', 'face', 'Linux Libertine G');
 	});
 
 	it('Apply font size.', function() {
+		helper.setDummyClipboardForCopy();
 		cy.cGet('#TextPropertyPanel').click();
 		cy.cGet('#fontsizecombobox').click();
 		cy.cGet('#fontsizecombobox').contains('.mobile-wizard.ui-combobox-text', '14 pt').click();
 
 		calcHelper.selectEntireSheet();
+		helper.copy();
 
 		cy.cGet('#copy-paste-container table td font')
 			.should('have.attr', 'size', '4');
 	});
 
 	it('Apply font color.', function() {
+		helper.setDummyClipboardForCopy();
 		helper.clickOnIdle('#Color .ui-header');
 
 		mobileHelper.selectFromColorPicker('#Color', 5);
 
 		calcHelper.selectEntireSheet();
+		helper.copy();
 
 		cy.cGet('#copy-paste-container table td font')
 			.should('have.attr', 'color', '#00FF00');
