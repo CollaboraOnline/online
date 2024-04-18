b408b1ee706359e17e7746acfdd0131bd7aefec6
Caolán McNamara <caolan.mcnamara@collabora.com>
2024-04-18 09:09:05 +0100

    move these common calc annotation test blocks to calcHelper
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: Ia67e29e447b333f4dfdfe25ec717cbdfc6dce043

--- cypress_test/integration_tests/mobile/calc/sheet_operation_spec.js
+++ cypress_test/integration_tests/mobile/calc/sheet_operation_spec.js
@@ -2,6 +2,7 @@
 
 var helper = require('../../common/helper');
 var mobileHelper = require('../../common/mobile_helper');
+var calcHelper = require('../../common/calc_helper');
 
 describe(['tagmobile', 'tagnextcloud', 'tagproxy'], 'Sheet Operation', function () {
 	var origTestFileName = 'sheet_operation.ods';
@@ -17,41 +18,22 @@ describe(['tagmobile', 'tagnextcloud', 'tagproxy'], 'Sheet Operation', function 
 		helper.afterAll(testFileName, this.currentTest.state);
 	});
 
-	function assertNumberofSheets(n) {
-		cy.cGet('button.spreadsheet-tab').should('have.length', n);
-	}
-
-	function selectOptionMobileWizard(menu) {
-		var eventOptions = {
-			force: true,
-			button: 0,
-			pointerType: 'mouse'
-		};
-
-		cy.cGet('.spreadsheet-tab.spreadsheet-tab-selected')
-			.trigger('pointerdown', eventOptions)
-			.wait(1000);
-
-		cy.cGet('body').contains('.ui-header.level-0.mobile-wizard.ui-widget', menu)
-			.click();
-	}
-
 	it('Insert sheet', function () {
-		assertNumberofSheets(1);
+		calcHelper.assertNumberofSheets(1);
 
 		cy.cGet('#tb_spreadsheet-toolbar_item_insertsheet').click();
 
-		assertNumberofSheets(2);
+		calcHelper.assertNumberofSheets(2);
 	});
 
 	it('Insert sheet before', function () {
-		assertNumberofSheets(1);
+		calcHelper.assertNumberofSheets(1);
 
 		cy.cGet('#spreadsheet-tab0').should('have.text', 'Sheet1');
 
-		selectOptionMobileWizard('Insert sheet before this');
+		calcHelper.selectOptionMobileWizard('Insert sheet before this');
 
-		assertNumberofSheets(2);
+		calcHelper.assertNumberofSheets(2);
 
 		cy.cGet('#spreadsheet-tab0').should('have.text', 'Sheet2');
 
@@ -59,13 +41,13 @@ describe(['tagmobile', 'tagnextcloud', 'tagproxy'], 'Sheet Operation', function 
 	});
 
 	it('Insert sheet after', function () {
-		assertNumberofSheets(1);
+		calcHelper.assertNumberofSheets(1);
 
 		cy.cGet('#spreadsheet-tab0').should('have.text', 'Sheet1');
 
-		selectOptionMobileWizard('Insert sheet after this');
+		calcHelper.selectOptionMobileWizard('Insert sheet after this');
 
-		assertNumberofSheets(2);
+		calcHelper.assertNumberofSheets(2);
 
 		cy.cGet('#spreadsheet-tab0').should('have.text', 'Sheet1');
 
@@ -73,25 +55,25 @@ describe(['tagmobile', 'tagnextcloud', 'tagproxy'], 'Sheet Operation', function 
 	});
 
 	it('Delete sheet', function () {
-		assertNumberofSheets(1);
+		calcHelper.assertNumberofSheets(1);
 
 		cy.cGet('#tb_spreadsheet-toolbar_item_insertsheet').click();
 
-		assertNumberofSheets(2);
+		calcHelper.assertNumberofSheets(2);
 
-		selectOptionMobileWizard('Delete Sheet...');
+		calcHelper.selectOptionMobileWizard('Delete Sheet...');
 
 		cy.cGet('#delete-sheet-modal-response').click();
 
-		assertNumberofSheets(1);
+		calcHelper.assertNumberofSheets(1);
 	});
 
 	it('Rename sheet', function () {
-		assertNumberofSheets(1);
+		calcHelper.assertNumberofSheets(1);
 
 		cy.cGet('.spreadsheet-tab.spreadsheet-tab-selected').should('have.text', 'Sheet1');
 
-		selectOptionMobileWizard('Rename Sheet...');
+		calcHelper.selectOptionMobileWizard('Rename Sheet...');
 
 		cy.cGet('#mobile-wizard-content-modal-dialog-rename-calc-sheet').should('exist');
 		cy.cGet('#input-modal-input').clear().type('renameSheet');
@@ -100,43 +82,43 @@ describe(['tagmobile', 'tagnextcloud', 'tagproxy'], 'Sheet Operation', function 
 	});
 
 	it('Hide/Show sheet', function () {
-		assertNumberofSheets(1);
+		calcHelper.assertNumberofSheets(1);
 
 		cy.cGet('#tb_spreadsheet-toolbar_item_insertsheet').click();
 
-		assertNumberofSheets(2);
+		calcHelper.assertNumberofSheets(2);
 
 		//hide sheet
-		selectOptionMobileWizard('Hide Sheet');
+		calcHelper.selectOptionMobileWizard('Hide Sheet');
 
-		assertNumberofSheets(1);
+		calcHelper.assertNumberofSheets(1);
 
 		//show sheet
-		selectOptionMobileWizard('Show Sheet');
+		calcHelper.selectOptionMobileWizard('Show Sheet');
 
 		cy.cGet('#mobile-wizard-content-modal-dialog-show-sheets-modal').should('exist');
 		cy.cGet('#hidden-part-checkbox-1').check();
 		cy.cGet('#show-sheets-modal-response').click();
 
-		assertNumberofSheets(2);
+		calcHelper.assertNumberofSheets(2);
 	});
 
 	it('Move sheet left/right', function () {
-		assertNumberofSheets(1);
+		calcHelper.assertNumberofSheets(1);
 
 		cy.cGet('#tb_spreadsheet-toolbar_item_insertsheet').click();
 
-		assertNumberofSheets(2);
+		calcHelper.assertNumberofSheets(2);
 
 		cy.cGet('#spreadsheet-tab0').should('have.text', 'Sheet1');
 
 		//left
-		selectOptionMobileWizard('Move Sheet Left');
+		calcHelper.selectOptionMobileWizard('Move Sheet Left');
 
 		cy.cGet('#spreadsheet-tab0').should('have.text', 'Sheet2');
 
 		//right
-		selectOptionMobileWizard('Move Sheet Right');
+		calcHelper.selectOptionMobileWizard('Move Sheet Right');
 
 		cy.cGet('#spreadsheet-tab0').should('have.text', 'Sheet1');
 	});
