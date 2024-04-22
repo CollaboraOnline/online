CollaboraOnline/online

commit bac41615833648e9bcec11826e6ec3da5b1798a9

annotation: resize comment list section on view size change
problem:
in writer when comment is added at the end of document,
it may go out of view and covered by the status bar and can't scroll any further

Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
Change-Id: Ia40ea128cd36a904ee5030bb7399da183e21f44d

browser/src/layer/tile/CommentListSection.ts
@@ -1723,8 +1723,12 @@ export class CommentSection extends CanvasSectionObject {
 		}
 
 		lastY += this.containerObject.getDocumentTopLeft()[1];
-		if (lastY > app.file.size.pixels[1])
-			app.view.size.pixels[1] = lastY;
+		if (lastY > app.file.size.pixels[1]) {
+			if (app.view.size.pixels[1] !== lastY) {
+				app.view.size.pixels[1] = lastY;
+				this.onResize(); // Annotation goes beyond document and can't be scrolled further unless resized
+			  }
+		}
 		else
 			app.view.size.pixels[1] = app.file.size.pixels[1];
 	}

commit f199b12f47ad1799de15ca42410bd0bb58ff7705

annotation: fixed comments could not expand
problem:
when comment was on the edge of the document/screen(vertically)
resize was called which ultimately collapsed the comments in small screen
which could not be expanded unless scrolled further from edge.
It was more problematic for the threads,
if entire thread could not fit in the screen it was always failing to expand

regression from bac4161

Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
Change-Id: I48de52864f557416b704b48de8ab78cd8ad25239

browser/src/layer/tile/CommentListSection.ts
@@ -1031,8 +1031,9 @@ export class CommentSection extends CanvasSectionObject {
 		});
 	}
 
-	public onResize (): void {
-		this.checkCollapseState();
+	public onResize (verticalOnly: boolean = false): void {
+		if (!verticalOnly)
+			this.checkCollapseState();
 		this.update();
 		// When window is resized, it may mean that comment wizard is closed. So we hide the highlights.
 		this.removeHighlighters();
@@ -1725,7 +1726,7 @@ export class CommentSection extends CanvasSectionObject {
 		if (lastY > app.file.size.pixels[1]) {
 			if (app.view.size.pixels[1] !== lastY) {
 				app.view.size.pixels[1] = lastY;
-				this.onResize(); // Annotation goes beyond document and can't be scrolled further unless resized
+				this.onResize(true); // Annotation goes beyond document and can't be scrolled further unless resized
 				this.select(this.sectionProperties.selectedComment, true); // Reselecting will bring entire comment in view
 			  }
 		}