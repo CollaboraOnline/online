2bae9f0eb9fda72817f9bed3b1b73ed61a6bba61
Rashesh Padia <rashesh.padia@collabora.com>
2024-05-01 20:01:50 +0530

    fix: don't wait for migrate msg if indirection server closes the connection because deployment gets deleted
    - if deployment gets deleted than there is no where to migrate the
    documents in such cases indirection will close the monitor connection
    but we need to set the _migrateMsgReceived flag so that docBroker don't
    wait for the message and continue to cleanup the document
    
    Signed-off-by: Rashesh Padia <rashesh.padia@collabora.com>
    Change-Id: Ia2732740927ea16e368f91bea7421576a3cf2beb

--- wsd/Admin.cpp
+++ wsd/Admin.cpp
@@ -720,6 +720,11 @@ void Admin::pollingThread()
         }
         poll(closeMonitorMsgTimeout - elapsedMicroS);
     }
+
+    // if monitor closes early we set the _migrateMsgReceived for each docbroker
+    // so that docbroker can cleanup the documents
+    if (_closeMonitor)
+        COOLWSD::setAllMigrationMsgReceived();
 }
 
 void Admin::modificationAlert(const std::string& docKey, pid_t pid, bool value){
