49c0c560ad3cdf43fbc09b091b65b8a722d0b9cf
Rashesh Padia <rashesh.padia@collabora.com>
2024-05-01 19:02:02 +0530

    fix: can't reconnect and get to edit mode after full server shutdown
    - regresssion from eadaf3f60694c8a3855fe1c842c5070b646e06f3
    
    Signed-off-by: Rashesh Padia <rashesh.padia@collabora.com>
    Change-Id: If2b20c7960e3c242064fc04425b4f893612f6362

--- browser/src/core/Socket.js
+++ browser/src/core/Socket.js
@@ -1617,6 +1617,7 @@ app.definitions.Socket = L.Class.extend({
 		var isActive = app.idleHandler._active;
 		this._map.hideBusy();
 		app.idleHandler._active = false;
+		app.idleHandler._serverRecycling = false;
 
 		if (this._map._docLayer) {
 			this._map._docLayer.removeAllViews();
