56ae33dd347641e9b77674e65d08cc49fa0fbd7b
Bayram Çiçek <bayram.cicek@collabora.com>
2024-05-24 15:39:28 +0300

    make instance.isAutofilter false for autoFillPopup
    - create instance.isAutoFillPopup
    - change closeAutoFilterDialogsOnTabChange() to closePopupsOnTabChange()
    
    Signed-off-by: Bayram Çiçek <bayram.cicek@collabora.com>
    Change-Id: I32c5d26f8567a156ff0e94bc6ff1ff5bdf7be8b3

--- browser/src/canvas/sections/AutoFillMarkerSection.ts
+++ browser/src/canvas/sections/AutoFillMarkerSection.ts
@@ -240,6 +240,7 @@ class AutoFillMarkerSection extends CanvasSectionObject {
 	}
 
 	public onMouseLeave () {
+		this.map._docLayer.isAutoFillFromOnMouseUp = false;
 		this.sectionProperties.mapPane.style.cursor = 'default';
 	}
 
