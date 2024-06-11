CollaboraOnline/online

commit 1a0d289e13a5395079209ca38999814eef05cf57

Added API to send server audit information
we use { code: string, status: string } entries to describe a
problem

Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
Change-Id: I21dacc0fa7b2c48f08ea2923e73da58fe33f976e

wsd/ClientSession.cpp
@@ -2338,6 +2338,17 @@ bool ClientSession::handleKitToClientMessage(const std::shared_ptr<Message>& pay
         LOG_INF("Ignoring notification on password protected document: " << firstLine);
     }
 
+#if !MOBILEAPP
+    if (tokens.equals(0, "viewinfo:"))
+    {
+        // send server audit results after we received information about users (who is admin)
+        const std::string message = std::string("serveraudit: ") + docBroker->getServerAudit();
+        bool status = forwardToClient(payload);
+        forwardToClient(std::make_shared<Message>(message, Message::Dir::In));
+        return status;
+    }
+#endif
+
     // Forward everything else.
     return forwardToClient(payload);
 }

browser/src/control/Control.ServerAuditDialog.ts
@@ -40,17 +40,24 @@ class ServerAuditDialog {
 
 		if (!app.serverAudit) return entries;
 
+		const errorIcon = { collapsed: 'serveraudit.svg' };
+
 		app.serverAudit.forEach(function (entry: any) {
 			switch (entry.code) {
 				case 'is_admin':
 					{
-						entries.push({
-							row: 0,
-							columns: [
-								{ collapsed: 'serveraudit.svg' },
-								{ text: _('Admin user property not set') },
-							],
-						} as TreeEntryJSON);
+						if (entry.status === '0') {
+							entries.push({
+								row: 0,
+								columns: [
+									errorIcon,
+									{ text: _('Admin user property not set') },
+									{
+										text: 'https://sdk.collaboraonline.com/docs/advanced_integration.html?highlight=userprivateinfo#userprivateinfo',
+									},
+								],
+							} as TreeEntryJSON);
+						}
 					}
 					break;
 			}

browser/src/core/Socket.js
@@ -1249,6 +1249,12 @@ app.definitions.Socket = L.Class.extend({
 			return;
 		}
 
+		if (textMsg.startsWith('serveraudit:')) {
+			var json = JSON.parse(textMsg.substr(12));
+			app.serverAudit = json.serverAudit;
+			app.map.fire('receivedserveraudit');
+		}
+
 		// These can arrive very early during the startup, and never again.
 		if (textMsg.startsWith('progress:')) {
 			var info = JSON.parse(textMsg.substring(textMsg.indexOf('{')));

browser/src/docstate.js
@@ -104,6 +104,8 @@ window.app = {
 		'DocumentColors': { name: _('Document colors'), colors: [] },
 	},
 	colorLastSelection: {}, // last used colors for uno commands
+
+	serverAudit: null, // contains list of warnings / errors detected on the server instance
 };
 
 var activateValidation = false;

test/httpwstest.cpp
@@ -283,7 +283,8 @@ void HTTPWSTest::testInactiveClient()
                                             token == "vrulerupdate:" ||
                                             token == "tableselected:" ||
                                             token == "colorpalettes:" ||
-                                            token == "jsdialog:");
+                                            token == "jsdialog:" ||
+                                            token == "serveraudit:");
 
                     // End when we get state changed.
                     return (token != "statechanged:");

wsd/DocumentBroker.cpp
@@ -4475,6 +4475,13 @@ void DocumentBroker::onUrpMessage(const char* data, size_t len)
     }
 }
 
+std::string DocumentBroker::getServerAudit() const
+{
+    return "{ \"serverAudit\": [ \
+        { \"code\": \"is_admin\", \"status\": \"0\" } \
+    ] }";
+}
+
 #if !MOBILEAPP && !WASMAPP
 
 void DocumentBroker::switchMode(const std::shared_ptr<ClientSession>& session,

wsd/DocumentBroker.hpp
@@ -592,6 +592,9 @@ public:
 
     void setMigrationMsgReceived() { _migrateMsgReceived = true; }
 
+    // Get JSON with audit status
+    std::string getServerAudit() const;
+
 #if !MOBILEAPP && !WASMAPP
     /// Switch between Online and Offline modes.
     void switchMode(const std::shared_ptr<ClientSession>& session, const std::string& mode);

commit baf528f8cf1d516c5e084c53d025419973107137

Fix failing unit tests on disconnect
Randomnly one of:
- unit-save-torture
- unit-wopi-languages

This can be caused by closed document and docBroker usage.
Regression from commit 1a0d289e13a5395079209ca38999814eef05cf57
Added API to send server audit information

Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
Change-Id: Ibe4f524f33d5c2100f3c442d79463c737e466f48

wsd/ClientSession.cpp
@@ -2399,17 +2399,6 @@ bool ClientSession::handleKitToClientMessage(const std::shared_ptr<Message>& pay
         LOG_INF("Ignoring notification on password protected document: " << firstLine);
     }
 
-#if !MOBILEAPP
-    if (tokens.equals(0, "viewinfo:"))
-    {
-        // send server audit results after we received information about users (who is admin)
-        const std::string message = std::string("serveraudit: ") + docBroker->getServerAudit();
-        bool status = forwardToClient(payload);
-        forwardToClient(std::make_shared<Message>(message, Message::Dir::In));
-        return status;
-    }
-#endif
-
     // Forward everything else.
     return forwardToClient(payload);
 }