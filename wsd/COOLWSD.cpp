ba17fb0d6d03a7825c0e48a78aa62915301a68fd
Szymon Kłos <szymon.klos@collabora.com>
2024-06-20 14:51:23 +0200

    Disable overwrite mode by default
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: Iff465e44cf8a7be830bb1636975b979501405e8a

--- wsd/COOLWSD.cpp
+++ wsd/COOLWSD.cpp
@@ -2121,7 +2121,7 @@ void COOLWSD::innerInitialize(Poco::Util::Application& self)
         { "welcome.enable", "false" },
         { "home_mode.enable", "false" },
         { "feedback.show", "true" },
-        { "overwrite_mode.enable", "true" },
+        { "overwrite_mode.enable", "false" },
 #if ENABLE_FEATURE_LOCK
         { "feature_lock.locked_hosts[@allow]", "false" },
         { "feature_lock.locked_hosts.fallback[@read_only]", "false" },
