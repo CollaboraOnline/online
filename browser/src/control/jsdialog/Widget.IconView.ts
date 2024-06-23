cecf6a8987413140d141334c4d002f501b3dc117
Szymon Kłos <szymon.klos@collabora.com>
2024-06-23 11:01:29 +0200

    jsdialog: fix TypeError in combobox onSelect
    Prevents error:
    Exception TypeError: Cannot set properties of undefined (setting 'selected') emitting event jsdialog: { "jsontype": "sidebar", "action": "action", "id": 0, "data": { "control_id": "applystyle", "position": "11", "action_type": "select"}}
    
    when selecting entry from the styles previews in Writer
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I2e75412d152e91e556715118a1b2dd75265dc506

--- browser/src/control/jsdialog/Widget.IconView.ts
+++ browser/src/control/jsdialog/Widget.IconView.ts
@@ -25,54 +25,87 @@
 
 declare var JSDialog: any;
 
-function _iconViewEntry (parentContainer: Element, parentData: any, entry: any, builder: any) {
+function _iconViewEntry(
+	parentContainer: Element,
+	parentData: any,
+	entry: any,
+	builder: any,
+) {
 	var disabled = parentData.enabled === false;
 
 	if (entry.separator && entry.separator === true) {
-		L.DomUtil.create('hr', builder.options.cssClass + ' ui-iconview-separator', parentContainer);
+		L.DomUtil.create(
+			'hr',
+			builder.options.cssClass + ' ui-iconview-separator',
+			parentContainer,
+		);
 		return;
 	}
 
-	var entryContainer = L.DomUtil.create('div', builder.options.cssClass + ' ui-iconview-entry', parentContainer);
+	var entryContainer = L.DomUtil.create(
+		'div',
+		builder.options.cssClass + ' ui-iconview-entry',
+		parentContainer,
+	);
 	if (entry.selected && entry.selected === true)
 		$(entryContainer).addClass('selected');
 
-	var icon = L.DomUtil.create('div', builder.options.cssClass + ' ui-iconview-icon', entryContainer);
+	var icon = L.DomUtil.create(
+		'div',
+		builder.options.cssClass + ' ui-iconview-icon',
+		entryContainer,
+	);
 	var img = L.DomUtil.create('img', builder.options.cssClass, icon);
-	if (entry.image)
-		img.src = entry.image;
+	if (entry.image) img.src = entry.image;
 	img.alt = entry.text;
-	if (entry.tooltip)
-		img.title = entry.tooltip;
-	else
-		img.title = entry.text;
+	if (entry.tooltip) img.title = entry.tooltip;
+	else img.title = entry.text;
 
 	if (!disabled) {
 		var singleClick = parentData.singleclickactivate === true;
-		$(entryContainer).click(function() {
+		$(entryContainer).click(function () {
 			$('#' + parentData.id + ' .ui-treeview-entry').removeClass('selected');
 			builder.callback('iconview', 'select', parentData, entry.row, builder);
 			if (singleClick) {
-				builder.callback('iconview', 'activate', parentData, entry.row, builder);
+				builder.callback(
+					'iconview',
+					'activate',
+					parentData,
+					entry.row,
+					builder,
+				);
 			}
 		});
 		if (!singleClick) {
-			$(entryContainer).dblclick(function() {
+			$(entryContainer).dblclick(function () {
 				$('#' + parentData.id + ' .ui-treeview-entry').removeClass('selected');
-				builder.callback('iconview', 'activate', parentData, entry.row, builder);
+				builder.callback(
+					'iconview',
+					'activate',
+					parentData,
+					entry.row,
+					builder,
+				);
 			});
 		}
 		builder._preventDocumentLosingFocusOnClick(entryContainer);
 	}
 }
 
-JSDialog.iconView = function (parentContainer: Element, data: any, builder: any) {
-	var container = L.DomUtil.create('div', builder.options.cssClass + ' ui-iconview', parentContainer);
+JSDialog.iconView = function (
+	parentContainer: Element,
+	data: any,
+	builder: any,
+) {
+	var container = L.DomUtil.create(
+		'div',
+		builder.options.cssClass + ' ui-iconview',
+		parentContainer,
+	);
 	container.id = data.id;
 
 	var disabled = data.enabled === false;
-	if (disabled)
-		L.DomUtil.addClass(container, 'disabled');
+	if (disabled) L.DomUtil.addClass(container, 'disabled');
 
 	for (var i in data.entries) {
 		_iconViewEntry(container, data, data.entries[i], builder);
@@ -81,19 +114,32 @@ JSDialog.iconView = function (parentContainer: Element, data: any, builder: any)
 	var firstSelected = $(container).children('.selected').get(0);
 	var blockOption = JSDialog._scrollIntoViewBlockOption('nearest');
 	if (firstSelected)
-		firstSelected.scrollIntoView({behavior: 'smooth', block: blockOption, inline: 'nearest'});
+		firstSelected.scrollIntoView({
+			behavior: 'smooth',
+			block: blockOption,
+			inline: 'nearest',
+		});
 
 	container.onSelect = (position: number) => {
 		$(container).children('.selected').removeClass('selected');
 
-		var entry = container.children.length > position ? container.children[position] : null;
+		var entry =
+			container.children.length > position
+				? container.children[position]
+				: null;
 
 		if (entry) {
 			L.DomUtil.addClass(entry, 'selected');
 			var blockOption = JSDialog._scrollIntoViewBlockOption('nearest');
-			entry.scrollIntoView({behavior: 'smooth', block: blockOption, inline: 'nearest'});
+			entry.scrollIntoView({
+				behavior: 'smooth',
+				block: blockOption,
+				inline: 'nearest',
+			});
 		} else if (position != -1)
-			console.warn('not found entry: "' + position + '" in: "' + container.id + '"');
+			console.warn(
+				'not found entry: "' + position + '" in: "' + container.id + '"',
+			);
 	};
 
 	return false;
