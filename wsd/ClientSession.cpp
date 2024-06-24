2b9fd71539833f917e04d91c1fbcec60205a0b0c
Henry Castro <hcastro@collabora.com>
2024-06-24 14:30:58 -0400

    wsd: fix ‘ssl’ has not been declared
    Change-Id: I028eacced01ea67baec45a233630a5d0b1c78c75
    Signed-off-by: Henry Castro <hcastro@collabora.com>

--- wsd/ClientSession.cpp
+++ wsd/ClientSession.cpp
@@ -1261,11 +1261,12 @@ bool ClientSession::loadDocument(const char* /*buffer*/, int /*length*/,
         std::ostringstream oss;
         oss << "load url=" << docBroker->getPublicUri().toString();
 
+#if ENABLE_SSL
         // if ssl client verification was disabled in online for the wopi server,
         // then exempt that host from ssl host verification also in core
         if (ssl::Manager::getClientVerification() == ssl::CertificateVerification::Disabled)
             oss << " verifyHost=false";
-
+#endif
         if (!getUserId().empty() && !getUserName().empty())
         {
             std::string encodedUserId;
