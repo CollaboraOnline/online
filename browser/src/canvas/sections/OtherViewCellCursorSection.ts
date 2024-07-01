8440dd8dc3afa1fdf8218187cce7005dd9761bda
Gökay Şatır <gokaysatir@collabora.com>
2024-07-01 15:13:11 +0300

    Add otherViewCellCursorSection.ts for view cell cursors.
    Signed-off-by: Gökay Şatır <gokaysatir@collabora.com>
    Change-Id: Idfcdd0262ba0d418de8b0463bafd1b66d9904d6b

---
+++ browser/src/canvas/sections/OtherViewCellCursorSection.ts
@@ -0,0 +1,101 @@
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+// This is used for other views' graphic selections.
+
+class OtherViewCellCursorSection extends CanvasSectionObject {
+    documentObject: boolean = true;
+    interactable: boolean = false; // We don't bother with events.
+    zIndex: number = L.CSections.DefaultForDocumentObjects.processingOrder;
+    drawingOrder: number = L.CSections.DefaultForDocumentObjects.drawingOrder;
+    processingOrder: number = L.CSections.DefaultForDocumentObjects.processingOrder;
+
+    static sectionNamePrefix = 'OtherViewCellCursorSection ';
+    static sectionPointers: Array<OtherViewCellCursorSection> = [];
+
+    constructor(viewId: number, rectangle: cool.SimpleRectangle, part: number) {
+        super();
+
+        this.size = [rectangle.pWidth, rectangle.pHeight];
+        this.position = [rectangle.pX1, rectangle.pY1];
+        this.sectionProperties.color = L.LOUtil.rgbToHex(L.LOUtil.getViewIdColor(viewId));
+        this.name = OtherViewCellCursorSection.sectionNamePrefix + viewId;
+
+        this.sectionProperties.viewId = viewId;
+        this.sectionProperties.part = part;
+    }
+
+    onDraw(frameCount?: number, elapsedTime?: number, subsetBounds?: Bounds): void {
+        this.context.strokeStyle = this.sectionProperties.color;
+        this.context.lineWidth = 2;
+        this.context.strokeRect(-0.5, -0.5, this.size[0], this.size[1]);
+    }
+
+    checkMyVisibility() {
+        if (app.map._docLayer._selectedPart !== this.sectionProperties.part)
+            return false;
+        else
+            return true;
+    }
+
+    public static addOrUpdateOtherViewCellCursor(viewId: number, rectangleData: Array<string>, part: number) {
+        let rectangle = new cool.SimpleRectangle(0, 0, 0, 0);
+        if (rectangleData)
+            rectangle = new app.definitions.simpleRectangle(parseInt(rectangleData[0]), parseInt(rectangleData[1]), parseInt(rectangleData[2]), parseInt(rectangleData[3]));
+
+        const sectionName = OtherViewCellCursorSection.sectionNamePrefix + viewId;
+        let section: OtherViewCellCursorSection;
+        if (app.sectionContainer.doesSectionExist(sectionName)) {
+            section = app.sectionContainer.getSectionWithName(sectionName);
+            section.sectionProperties.part = part;
+            section.size[0] = rectangle.pWidth;
+            section.size[1] = rectangle.pHeight;
+            section.setPosition(rectangle.pX1, rectangle.pY1);
+        }
+        else {
+            section = new OtherViewCellCursorSection(viewId, rectangle, part);
+            app.sectionContainer.addSection(section);
+            OtherViewCellCursorSection.sectionPointers.push(section);
+        }
+
+        section.setShowSection(section.checkMyVisibility());
+        app.sectionContainer.requestReDraw();
+    }
+
+    public static removeView(viewId: number) {
+        const sectionName = OtherViewCellCursorSection.sectionNamePrefix + viewId;
+        if (app.sectionContainer.doesSectionExist(sectionName)) {
+            const section = app.sectionContainer.getSectionWithName(sectionName);
+            OtherViewCellCursorSection.sectionPointers.splice(OtherViewCellCursorSection.sectionPointers.indexOf(section), 1);
+            app.sectionContainer.removeSection(sectionName);
+            app.sectionContainer.requestReDraw();
+        }
+    }
+
+    public static updateVisibilities() {
+        for (let i = 0; i < OtherViewCellCursorSection.sectionPointers.length; i++) {
+            const section = OtherViewCellCursorSection.sectionPointers[i];
+            section.setShowSection(section.checkMyVisibility());
+        }
+        app.sectionContainer.requestReDraw();
+    }
+
+    public static getViewCursorSection(viewId: number) {
+        const name = OtherViewCellCursorSection.sectionNamePrefix + viewId;
+        return app.sectionContainer.getSectionWithName(name);
+    }
+
+    public static doesViewCursorExist(viewId: number) {
+        const name = OtherViewCellCursorSection.sectionNamePrefix + viewId;
+        return app.sectionContainer.doesSectionExist(name);
+    }
+}
+
+app.definitions.otherViewCellCursorSection = OtherViewCellCursorSection;
