e1b3c80a52a263126db6edaa8a23ec3165e54283
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2024-07-01 16:08:40 +0900

    Introduce SlideshowPresenter to show the slide shows
    SlideshowPresenter is a class for presenting individual slide
    bitmaps and transitioning between the slide bitmaps.
    
    Signed-off-by: Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
    Change-Id: Ie4a23574deeb1207ce63ccd152fe7be1d9ca84a1

--- browser/src/docdispatcher.ts
+++ browser/src/docdispatcher.ts
@@ -361,7 +361,10 @@ class Dispatcher {
 			app.map.fire('fullscreen');
 		};
 		this.actionsMap['present-in-window'] = function () {
-			app.map.fire('presentinwindow');
+			if (app.map._debug.debugOn)
+				app.map.fire('start-slide-show');
+			else
+				app.map.fire('presentinwindow');
 		};
 		this.actionsMap['fullscreen-drawing'] = () => {
 			L.toggleFullScreen();
@@ -370,7 +373,10 @@ class Dispatcher {
 			app.map.fire('fullscreen');
 		};
 		this.actionsMap['presentinwindow'] = function () {
-			app.map.fire('presentinwindow');
+			if (app.map._debug.debugOn)
+				app.map.fire('start-slide-show');
+			else
+				app.map.fire('presentinwindow');
 		};
 
 		this.actionsMap['deletepage'] = function () {
