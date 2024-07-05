61b7c3c5d5c2a61e3273ed08dfc5c2679aba1ba4
Javiya Vivekkumar Dineshbhai <vivek.javiya@collabora.com>
2024-07-05 15:45:37 +0530

    Refactor code for integrate multiple transition
    Signed-off-by: Javiya Vivekkumar Dineshbhai <vivek.javiya@collabora.com>
    Change-Id: I536c1454ed0c27e67d4ff9e4d368e1a6f09cd23a

--- browser/src/slideshow/transition/WipeTransition.ts
+++ browser/src/slideshow/transition/WipeTransition.ts
@@ -12,12 +12,10 @@ class WipeTransition extends Transition2d {
 	private direction: number = 0;
 	constructor(
 		canvas: HTMLCanvasElement,
-		vertexShaderSource: string,
-		fragmentShaderSource: string,
 		image1: HTMLImageElement,
 		image2: HTMLImageElement,
 	) {
-		super(canvas, vertexShaderSource, fragmentShaderSource, image1, image2);
+		super(canvas, image1, image2);
 		this.prepareTransition();
 		this.animationTime = 1500;
 	}
@@ -33,72 +31,61 @@ class WipeTransition extends Transition2d {
 		this.direction = direction;
 		this.startTransition();
 	}
+
+	public getVertexShader(): string {
+		return `#version 300 es
+				in vec4 a_position;
+				in vec2 a_texCoord;
+				out vec2 v_texCoord;
+
+				void main() {
+					gl_Position = a_position;
+					v_texCoord = a_texCoord;
+				}
+				`;
+	}
+
+	public getFragmentShader(): string {
+		return `#version 300 es
+				precision mediump float;
+
+				uniform sampler2D leavingSlideTexture;
+				uniform sampler2D enteringSlideTexture;
+				uniform float time;
+				uniform int direction; // 1: Left to Right, 2: Right to Left, 3: Top to Bottom, 4: Bottom to Top
+
+				in vec2 v_texCoord;
+				out vec4 outColor;
+
+				void main() {
+					vec2 uv = v_texCoord;
+					float progress = time;
+
+					if (direction == 1) {
+						if (uv.x < progress) {
+							outColor = texture(enteringSlideTexture, uv);
+						} else {
+							outColor = texture(leavingSlideTexture, uv);
+						}
+					} else if (direction == 2) {
+						if (uv.x > 1.0 - progress) {
+							outColor = texture(enteringSlideTexture, uv);
+						} else {
+							outColor = texture(leavingSlideTexture, uv);
+						}
+					} else if (direction == 3) {
+						if (uv.y < progress) {
+							outColor = texture(enteringSlideTexture, uv);
+						} else {
+							outColor = texture(leavingSlideTexture, uv);
+						}
+					} else if (direction == 4) {
+						if (uv.y > 1.0 - progress) {
+							outColor = texture(enteringSlideTexture, uv);
+						} else {
+							outColor = texture(leavingSlideTexture, uv);
+						}
+					}
+				}`;
+	}
 }
-
-app.definitions.WipeTransition = function (
-	canvas: HTMLCanvasElement,
-	image1: HTMLImageElement,
-	image2: HTMLImageElement,
-) {
-	const vertexShaderSource = `#version 300 es
-in vec4 a_position;
-in vec2 a_texCoord;
-out vec2 v_texCoord;
-
-void main() {
-    gl_Position = a_position;
-    v_texCoord = a_texCoord;
-}
-`;
-
-	const fragmentShaderSource = `#version 300 es
-precision mediump float;
-
-uniform sampler2D leavingSlideTexture;
-uniform sampler2D enteringSlideTexture;
-uniform float time;
-uniform int direction; // 1: Left to Right, 2: Right to Left, 3: Top to Bottom, 4: Bottom to Top
-
-in vec2 v_texCoord;
-out vec4 outColor;
-
-void main() {
-    vec2 uv = v_texCoord;
-    float progress = time;
-
-    if (direction == 1) {
-        if (uv.x < progress) {
-            outColor = texture(enteringSlideTexture, uv);
-        } else {
-            outColor = texture(leavingSlideTexture, uv);
-        }
-    } else if (direction == 2) {
-        if (uv.x > 1.0 - progress) {
-            outColor = texture(enteringSlideTexture, uv);
-        } else {
-            outColor = texture(leavingSlideTexture, uv);
-        }
-    } else if (direction == 3) {
-        if (uv.y < progress) {
-            outColor = texture(enteringSlideTexture, uv);
-        } else {
-            outColor = texture(leavingSlideTexture, uv);
-        }
-    } else if (direction == 4) {
-        if (uv.y > 1.0 - progress) {
-            outColor = texture(enteringSlideTexture, uv);
-        } else {
-            outColor = texture(leavingSlideTexture, uv);
-        }
-    }
-}
-`;
-
-	return new WipeTransition(
-		canvas,
-		vertexShaderSource,
-		fragmentShaderSource,
-		image1,
-		image2,
-	);
-};
