6a315477a86a3c7b3c833c951efb26cdd3f5492b
Javiya Vivekkumar Dineshbhai <vivek.javiya@collabora.com>
2024-07-05 21:46:40 +0530

    Transition: Add Split transition
    Signed-off-by: Javiya Vivekkumar Dineshbhai <vivek.javiya@collabora.com>
    Change-Id: Iaf884265a2ae7d90e4bed032222c73e0a6ef8b0e

---
+++ browser/src/slideshow/transition/SplitTransition.ts
@@ -0,0 +1,83 @@
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+declare var SlideShow: any;
+
+class SplitTransition extends Transition2d {
+	private direction: number = 0;
+	constructor(
+		canvas: HTMLCanvasElement,
+		image1: HTMLImageElement,
+		image2: HTMLImageElement,
+	) {
+		super(canvas, image1, image2);
+		this.prepareTransition();
+		this.animationTime = 2000;
+	}
+
+	public renderUniformValue(): void {
+		this.gl.uniform1i(
+			this.gl.getUniformLocation(this.program, 'direction'),
+			this.direction,
+		);
+	}
+
+	public start(direction: number): void {
+		this.direction = direction;
+		this.startTransition();
+	}
+
+	public getVertexShader(): string {
+		return `#version 300 es
+				in vec4 a_position;
+				in vec2 a_texCoord;
+				out vec2 v_texCoord;
+
+				void main() {
+					gl_Position = a_position;
+					v_texCoord = a_texCoord;
+				}
+				`;
+	}
+
+	public getFragmentShader(): string {
+		return `#version 300 es
+                precision mediump float;
+
+                uniform sampler2D leavingSlideTexture;
+                uniform sampler2D enteringSlideTexture;
+                uniform float time;
+                uniform int direction; 
+
+                in vec2 v_texCoord;
+                out vec4 outColor;
+
+                void main() {
+                    vec2 uv = v_texCoord;
+                    float progress = time;
+
+                    vec2 center = vec2(0.5, 0.5);
+
+                    vec2 dist = abs(uv - center);
+
+                    float size = (direction == 2 || direction == 4) ? progress * 1.5 : (1.0 - progress * 1.5);
+
+                    float mask = (direction == 1 || direction == 2 ) ? step(dist.y, size / 2.0) : step(dist.x, size / 2.0);
+
+                    mask = min(mask, 1.0);
+
+                    vec4 color1 = texture(leavingSlideTexture, uv);
+                    vec4 color2 = texture(enteringSlideTexture, uv);
+
+                    outColor = (direction == 2 || direction == 4) ? mix(color1, color2, mask) : mix(color2, color1, mask);
+                }
+                `;
+	}
+}
