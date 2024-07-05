ea2928b08ee99604c5741d971fa25d944d44ee97
Javiya Vivekkumar Dineshbhai <vivek.javiya@collabora.com>
2024-07-05 20:47:11 +0530

    Transition: Add Venetian transition
    Signed-off-by: Javiya Vivekkumar Dineshbhai <vivek.javiya@collabora.com>
    Change-Id: Ib1e72185b170c3f78b377e4a0f9b345ceb128d64

---
+++ browser/src/slideshow/transition/VenetianTransition.ts
@@ -0,0 +1,84 @@
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+class VenetianTransition extends Transition2d {
+	private direction: number = 0;
+	constructor(
+		canvas: HTMLCanvasElement,
+		image1: HTMLImageElement,
+		image2: HTMLImageElement,
+	) {
+		super(canvas, image1, image2);
+		this.prepareTransition();
+		this.animationTime = 2000;
+	}
+
+	public renderUniformValue(): void {
+		this.gl.uniform1i(
+			this.gl.getUniformLocation(this.program, 'direction'),
+			this.direction,
+		);
+		this.gl.uniform2fv(this.gl.getUniformLocation(this.program, 'resolution'), [
+			this.canvas.width,
+			this.canvas.height,
+		]);
+	}
+
+	public start(direction: number): void {
+		this.direction = direction;
+		this.startTransition();
+	}
+
+	public getVertexShader(): string {
+		return `#version 300 es
+				in vec4 a_position;
+				in vec2 a_texCoord;
+				out vec2 v_texCoord;
+
+				void main() {
+					gl_Position = a_position;
+					v_texCoord = a_texCoord;
+				}
+				`;
+	}
+
+	public getFragmentShader(): string {
+		return `#version 300 es
+                precision mediump float;
+
+                uniform sampler2D leavingSlideTexture;
+                uniform sampler2D enteringSlideTexture;
+                uniform float time;
+                uniform vec2 resolution;
+                uniform int direction;
+
+                in vec2 v_texCoord;
+                out vec4 outColor;
+
+                void main() {
+                    vec2 uv = v_texCoord;
+                    float progress = time;
+
+                    float numBlinds = 6.0;
+
+                    float blindSize = (direction == 1) ? resolution.x / numBlinds : resolution.y / numBlinds;
+
+                    float position = (direction == 1) ? mod(uv.x * resolution.x, blindSize) : mod(uv.y * resolution.y, blindSize);
+
+                    float mask = step(progress, position / blindSize);
+
+                    vec4 color1 = texture(leavingSlideTexture, uv);
+                    vec4 color2 = texture(enteringSlideTexture, uv);
+
+                    outColor = mix(color2, color1, mask);
+                }
+                `;
+	}
+}
