bf8cbd7be090b984c747baa0c9ba0335f60d8aa7
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2024-07-08 17:00:45 +0900

    add NoTransition for when there is no transition between slides
    Signed-off-by: Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
    Change-Id: If82931945d1100297b21c06d4b2dfe26e7e63399

---
+++ browser/src/slideshow/transition/NoTransition.ts
@@ -0,0 +1,29 @@
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+declare var SlideShow: any;
+
+class NoTransition extends Transition2d {
+	constructor(
+		canvas: HTMLCanvasElement,
+		image1: HTMLImageElement,
+        image2: HTMLImageElement,
+	) {
+		super(canvas, image1, image2);
+		this.prepareTransition();
+		this.animationTime = 10;
+	}
+
+	public start(): void {
+		this.startTransition();
+	}
+}
+
+SlideShow.NoTransition = NoTransition;
