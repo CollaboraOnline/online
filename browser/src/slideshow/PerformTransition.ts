3ba439dc502f6af367b1be4c2b6642f0dabe0fc6
Javiya Vivekkumar Dineshbhai <vivek.javiya@collabora.com>
2024-07-08 15:05:16 +0530

    slideshow: convert string to enum for transition type
    Signed-off-by: Javiya Vivekkumar Dineshbhai <vivek.javiya@collabora.com>
    Change-Id: Ic845572579c087b2f875454a7cf7d4aa32ed9366

--- browser/src/slideshow/PerformTransition.ts
+++ browser/src/slideshow/PerformTransition.ts
@@ -9,96 +9,107 @@
  */
 declare var SlideShow: any;
 
+enum TransitionType {
+    BARWIPE,
+    PINWHEELWIPE,
+    SLIDEWIPE,
+    RANDOMBARWIPE,
+    CHECKERBOARDWIPE,
+    FOURBOXWIPE,
+    IRISWIPE,
+    FANWIPE,
+    BLINDSWIPE,
+    FADE,
+    DISSOLVE,
+    PUSHWIPE,
+    ELLIPSEWIPE,
+    BARNDOORWIPE,
+    WATERFALLWIPE,
+}
+
+const stringToTransitionTypeMap: Record<string, TransitionType> = {
+    'BarWipe': TransitionType.BARWIPE,
+    'PineWheelWipe': TransitionType.PINWHEELWIPE,
+    'SlideWipe': TransitionType.SLIDEWIPE,
+    'RandomBarWipe': TransitionType.RANDOMBARWIPE,
+    'CheckerBoardWipe': TransitionType.CHECKERBOARDWIPE,
+    'FourBoxWipe': TransitionType.FOURBOXWIPE,
+    'IrisWipe': TransitionType.IRISWIPE,
+    'FanWipe': TransitionType.FANWIPE,
+    'BlindWipe': TransitionType.BLINDSWIPE,
+    'Fade': TransitionType.FADE,
+    'Dissolve': TransitionType.DISSOLVE,
+    'PushWipe': TransitionType.PUSHWIPE,
+    'EllipseWipe': TransitionType.ELLIPSEWIPE,
+    'BarnDoorWipe': TransitionType.BARNDOORWIPE,
+    'WaterfallWipe': TransitionType.WATERFALLWIPE,
+};
+
 SlideShow.PerformTransition = function (
 	canvas: HTMLCanvasElement,
 	image1: HTMLImageElement,
 	image2: HTMLImageElement,
 	slideInfo: SlideInfo,
 ) {
-	switch (slideInfo.transitionType) {
-		case 'Fade':
+	switch (stringToTransitionTypeMap[slideInfo.transitionType]) {
+		case TransitionType.FADE:
 			new SlideShow.FadeTransition(canvas, image1, image2).start(2);
 			break;
 		
-		case 'BarWipe':
+		case TransitionType.BARWIPE:
 			new SlideShow.WipeTransition(canvas, image1, image2).start(1);
 			break;
 		
-		case 'PineWheelWipe':
+		case TransitionType.PINWHEELWIPE:
 			// 1,2,3, 4, 8
 			new SlideShow.WheelTransition(canvas, image1, image2).start(2);
 			break;
 
-			case 'SlideWipe':
+		case TransitionType.SLIDEWIPE:
 			new SlideShow.UncoverTransition(canvas, image1, image2).start(2);
 			break;
 
-		case 'RandomBarWipe':
+		case TransitionType.RANDOMBARWIPE:
 			new SlideShow.BarsTransition(canvas, image1, image2).start(1);
 			break;
 
-		case 'CheckerBoardWipe':
+		case TransitionType.CHECKERBOARDWIPE:
 			new SlideShow.CheckersTransition(canvas, image1, image2).start(1);
 			break;
 
-		case "FourBoxWipe":
+		case TransitionType.FOURBOXWIPE:
 			// All Shapes come here
             break;
 
-		case 'PLUS':
-			new SlideShow.PlusTransition(canvas, image1, image2).start();
-			break;
-
-		case 'CIRCLE':
-			new SlideShow.CircleTransition(canvas, image1, image2).start();
-			break;
-
-		case 'DIAMOND':
-			new SlideShow.DiamondTransition(canvas, image1, image2).start();
-			break;
-
-		case 'OVAL':
-			new SlideShow.OvalTransition(canvas, image1, image2).start(1);
-			break;
-
-		case 'IrisWipe':
+		case TransitionType.IRISWIPE:
 			new SlideShow.BoxTransition(canvas, image1, image2).start(1);
 			break;
 
-		case 'FanWipe':
+		case TransitionType.FANWIPE:
 			new SlideShow.WedgeTransition(canvas, image1, image2).start();
 			break;
 
-		case 'BlindWipe':
+		case TransitionType.BLINDSWIPE:
 			new SlideShow.VenetianTransition(canvas, image1, image2).start(1);
 			break;
 
-		case 'CUT':
-			// todo: subtype of "BarWipe" to "FadeOverColor"
-			new SlideShow.CutTransition(canvas, image1, image2).start();
-			break;
-
-		case 'COVER':
-			new SlideShow.CoverTransition(canvas, image1, image2).start(1);
-			break;
-
-		case 'Dissolve':
+		case TransitionType.DISSOLVE:
 			new SlideShow.SimpleDissolveTransition(canvas, image1, image2).start();
 			break;
 
-		case 'PushWipe':
+		case TransitionType.PUSHWIPE:
 			new SlideShow.PushTransition(canvas, image1, image2).start(1);
 			break;
 
-		case 'BarnDoorWipe':
+		case TransitionType.BARNDOORWIPE:
 			new SlideShow.SplitTransition(canvas, image1, image2).start(1);
 			break;
 	
-		case 'EllipseWipe':
+		case TransitionType.ELLIPSEWIPE:
 			// todo: have to find!
 			break;
 
-		case 'WaterfallWipe':
+		case TransitionType.WATERFALLWIPE:
 			// TODO: Need to implement
 			break;
 
