746354f370ecf0a8fdbc98a990f4f7bd3dd5d0bc
Javiya Vivekkumar Dineshbhai <vivek.javiya@collabora.com>
2024-07-09 18:47:57 +0530

    slideshow: Code refactoring for EllipseWipe Transition based on transition type
    Signed-off-by: Javiya Vivekkumar Dineshbhai <vivek.javiya@collabora.com>
    Change-Id: Ic8398b7b17f9300c4ec9370d93ef6a2600a2e2fd

---
+++ browser/src/slideshow/transition/EllipseWipeTransition.ts
@@ -0,0 +1,27 @@
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+declare var SlideShow: any;
+
+function EllipseWipeTransition(
+	canvas: HTMLCanvasElement,
+	image1: HTMLImageElement,
+	image2: HTMLImageElement,
+	slideInfo: SlideInfo,
+) {
+	const transitionSubType =
+		stringToTransitionSubTypeMap[slideInfo.transitionSubtype];
+	if (transitionSubType == TransitionSubType.CIRCLE) {
+		return new SlideShow.CircleTransition(canvas, image1, image2);
+	} else {
+		return new SlideShow.OvalTransition(canvas, image1, image2, slideInfo);
+	}
+}
+
+SlideShow.EllipseWipeTransition = EllipseWipeTransition;
