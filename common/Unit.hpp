CollaboraOnline/online

commit a866719881273e9c98fab53fc3a44ff2882883c3

test: abort a test whose kit has a segfault by default.
Otherwise forkit tends to loop aggressively re-starting that for the
duration of the test wastefully.

Signed-off-by: Michael Meeks <michael.meeks@collabora.com>
Change-Id: Ia1c684a5d995f54f29290c9631b1ee14266445d7

common/Unit.hpp
@@ -409,6 +409,10 @@ public:
 
     /// When a new child kit process reports
     virtual void newChild(const std::shared_ptr<ChildProcess>& /*child*/) {}
+
+    /// When we get a segfault message from forkit; override to test crashes ...
+    virtual void kitSegfault(int /* count */) { exitTest(TestResult::Failed, "kit segfault"); }
+
     /// Intercept createStorage
     virtual bool createStorage(const Poco::URI& /* uri */,
                                const std::string& /* jailRoot */,

test/UnitWOPICrashModified.cpp
@@ -33,6 +33,8 @@ public:
     {
     }
 
+    void kitSegfault(int /* count */) override { /* ignore */ }
+
     std::unique_ptr<http::Response> assertPutFileRequest(const Poco::Net::HTTPRequest&) override
     {
         failTest("Unexpected PutFile when there should be no file on disk to upload");

wsd/COOLWSD.cpp
@@ -1054,6 +1054,7 @@ void ForKitProcWSHandler::handleMessage(const std::vector<char> &data)
         {
             Admin::instance().addSegFaultCount(count);
             LOG_INF(count << " coolkit processes crashed with segmentation fault.");
+            UnitWSD::get().kitSegfault(count);
         }
         else
         {

commit 19dc8855d9e221190016b33b8ef77bdda5ee0616

fix: coolwsd getting killed on kit segfault
- regression from a866719

Signed-off-by: Rashesh Padia <rashesh.padia@collabora.com>
Change-Id: I21b20aabb4dffad3011b75de8d1f373d495a697d

common/Unit.hpp
@@ -437,7 +437,11 @@ public:
     virtual void newChild(const std::shared_ptr<ChildProcess>& /*child*/) {}
 
     /// When we get a segfault message from forkit; override to test crashes ...
-    virtual void kitSegfault(int /* count */) { exitTest(TestResult::Failed, "kit segfault"); }
+    virtual void kitSegfault(int /* count */)
+    {
+        if (get().isUnitTesting())
+            exitTest(TestResult::Failed, "kit segfault");
+    }
 
     /// Intercept createStorage
     virtual bool createStorage(const Poco::URI& /* uri */,