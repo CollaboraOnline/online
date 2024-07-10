7a610831c865b8e7b6d07898fc709348487a1424
Rashesh Padia <rashesh.padia@collabora.com>
2024-07-10 19:16:42 +0530

    fix: coolwsd getting killed on kit segfault
    - regression from a866719
    
    Signed-off-by: Rashesh Padia <rashesh.padia@collabora.com>
    Change-Id: I21b20aabb4dffad3011b75de8d1f373d495a697d

--- common/Unit.hpp
+++ common/Unit.hpp
@@ -437,7 +437,11 @@ public:
     virtual void newChild(const std::shared_ptr<ChildProcess>& /*child*/) {}
 
     /// When we get a segfault message from forkit; override to test crashes ...
-    virtual void kitSegfault(int /* count */) { exitTest(TestResult::Failed, "kit segfault"); }
+    virtual void kitSegfault(int /* count */)
+    {
+        if (get().isUnitTesting())
+            exitTest(TestResult::Failed, "kit segfault");
+    }
 
     /// Intercept createStorage
     virtual bool createStorage(const Poco::URI& /* uri */,
