CollaboraOnline/online

commit 246e87fea21d6121fd4bd2daeff8a4f92082477e

wsd: Unconditionally compile support-key-enabled code paths
Signed-off-by: Aron Budea <aron.budea@collabora.com>
Change-Id: Iec477c223f2dc75e0be8b472c8439ca9311d6aae

wsd/FileServer.cpp
@@ -1000,10 +1000,8 @@ std::string FileServerRequestHandler::getRequestPathname(const HTTPRequest& requ
     return path;
 }
 
-constexpr char BRANDING[] = "branding";
-#if ENABLE_SUPPORT_KEY
-constexpr char BRANDING_UNSUPPORTED[] = "branding-unsupported";
-#endif
+constexpr std::string_view BRANDING = "branding";
+constexpr std::string_view SUPPORT_KEY_BRANDING_UNSUPPORTED = "branding-unsupported";
 
 static const std::string ACCESS_TOKEN = "%ACCESS_TOKEN%";
 static const std::string ACCESS_TOKEN_TTL = "%ACCESS_TOKEN_TTL%";
@@ -1212,15 +1210,16 @@ FileServerRequestHandler::ResourceAccessDetails FileServerRequestHandler::prepro
     std::string brandCSS(Poco::format(linkCSS, responseRoot, std::string(BRANDING)));
     std::string brandJS(Poco::format(scriptJS, responseRoot, std::string(BRANDING)));
 
-#if ENABLE_SUPPORT_KEY
-    const std::string keyString = config.getString("support_key", "");
-    SupportKey key(keyString);
-    if (!key.verify() || key.validDaysRemaining() <= 0)
+    if (config::isSupportKeyEnabled())
     {
-        brandCSS = Poco::format(linkCSS, responseRoot, std::string(BRANDING_UNSUPPORTED));
-        brandJS = Poco::format(scriptJS, responseRoot, std::string(BRANDING_UNSUPPORTED));
+        const std::string keyString = config.getString("support_key", "");
+        SupportKey key(keyString);
+        if (!key.verify() || key.validDaysRemaining() <= 0)
+        {
+            brandCSS = Poco::format(linkCSS, responseRoot, SUPPORT_KEY_BRANDING_UNSUPPORTED);
+            brandJS = Poco::format(scriptJS, responseRoot, SUPPORT_KEY_BRANDING_UNSUPPORTED);
+        }
     }
-#endif
 
     Poco::replaceInPlace(preprocess, std::string("<!--%BRANDING_CSS%-->"), brandCSS);
     Poco::replaceInPlace(preprocess, std::string("<!--%BRANDING_JS%-->"), brandJS);
@@ -1592,17 +1591,18 @@ void FileServerRequestHandler::preprocessAdminFile(const HTTPRequest& request,
     std::string brandJS(Poco::format(scriptJS, responseRoot, std::string(BRANDING)));
     std::string brandFooter;
 
-#if ENABLE_SUPPORT_KEY
-    const auto& config = Application::instance().config();
-    const std::string keyString = config.getString("support_key", "");
-    SupportKey key(keyString);
-
-    if (!key.verify() || key.validDaysRemaining() <= 0)
+    if (config::isSupportKeyEnabled())
     {
-        brandJS = Poco::format(scriptJS, std::string(BRANDING_UNSUPPORTED));
-        brandFooter = Poco::format(footerPage, key.data(), Poco::DateTimeFormatter::format(key.expiry(), Poco::DateTimeFormat::RFC822_FORMAT));
+        const auto& config = Application::instance().config();
+        const std::string keyString = config.getString("support_key", "");
+        SupportKey key(keyString);
+
+        if (!key.verify() || key.validDaysRemaining() <= 0)
+        {
+            brandJS = Poco::format(scriptJS, SUPPORT_KEY_BRANDING_UNSUPPORTED);
+            brandFooter = Poco::format(footerPage, key.data(), Poco::DateTimeFormatter::format(key.expiry(), Poco::DateTimeFormat::RFC822_FORMAT));
+        }
     }
-#endif
 
     Poco::replaceInPlace(templateFile, std::string("<!--%BRANDING_JS%-->"), brandJS);
     Poco::replaceInPlace(templateFile, std::string("<!--%FOOTER%-->"), brandFooter);

Makefile.am
@@ -294,6 +294,7 @@ coolstress_SOURCES = tools/Stress.cpp \
 coolconfig_SOURCES = tools/Config.cpp \
 		     tools/ConfigMigrationAssistant.cpp \
 		     common/DummyTraceEventEmitter.cpp \
+		     common/ConfigUtil.cpp \
 		     common/Crypto.cpp \
 		     common/Log.cpp \
 		     common/StringVector.cpp \

android/lib/src/main/cpp/CMakeLists.txt.in
@@ -7,8 +7,9 @@ add_library(androidapp SHARED
             androidapp.cpp
             ../../../../../common/Authorization.cpp
             ../../../../../common/ConfigUtil.cpp
-            ../../../../../common/FileUtil.cpp
             ../../../../../common/CommandControl.cpp
+            ../../../../../common/Crypto-stub.cpp
+            ../../../../../common/FileUtil.cpp
             ../../../../../common/Log.cpp
             ../../../../../common/MessageQueue.cpp
             ../../../../../common/TraceEvent.cpp

common/ConfigUtil.cpp
@@ -70,4 +70,14 @@ bool isSslEnabled()
     return false;
 #endif
 }
+
+bool isSupportKeyEnabled()
+{
+#if ENABLE_SUPPORT_KEY
+    return true;
+#else
+    return false;
+#endif
+}
+
 } // namespace config

common/ConfigUtil.hpp
@@ -49,4 +49,7 @@ bool getBool(const std::string& key, const bool def);
 /// Return true if SSL is enabled in the config and no fuzzing is enabled.
 bool isSslEnabled();
 
+/// Return true if build is support key enabled (ENABLE_SUPPORT_KEY is defined)
+bool isSupportKeyEnabled();
+
 } // namespace config

common/Crypto-stub.cpp
@@ -1,0 +1,28 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "Crypto.hpp"
+
+#include <Poco/DateTime.h>
+
+struct SupportKeyImpl {};
+
+SupportKey::SupportKey([[maybe_unused]] const std::string& key) {}
+
+SupportKey::~SupportKey() {}
+
+bool SupportKey::verify() { return true; }
+
+int SupportKey::validDaysRemaining() { return 0; }
+
+Poco::DateTime SupportKey::expiry() const { return Poco::DateTime(); }
+
+std::string SupportKey::data() const { return std::string(); }

common/Crypto.cpp
@@ -11,8 +11,6 @@
 
 #include <config.h>
 
-#if ENABLE_SUPPORT_KEY
-
 #include <Poco/DigestStream.h>
 #include <Poco/Base64Decoder.h>
 #include <Poco/DateTimeParser.h>
@@ -24,10 +22,21 @@
 
 #include "Log.hpp"
 #include "Crypto.hpp"
+#if ENABLE_SUPPORT_KEY
 #include "support-public-key.hpp"
+#endif
 
 using namespace Poco;
 using namespace Poco::Crypto;
+
+std::string getSupportPublicKey()
+{
+#if ENABLE_SUPPORT_KEY
+    return SUPPORT_PUBLIC_KEY;
+#else
+    return std::string();
+#endif
+}
 
 struct SupportKeyImpl
 {
@@ -86,7 +95,7 @@ bool SupportKey::verify()
         return false;
     }
 
-    std::istringstream pubStream(SUPPORT_PUBLIC_KEY);
+    std::istringstream pubStream(getSupportPublicKey());
 
     try {
         RSAKey keyPub(&pubStream);
@@ -139,7 +148,5 @@ std::string SupportKey::data() const
 {
     return _impl->_data;
 }
-
-#endif // ENABLE_SUPPORT_KEY
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

common/Crypto.hpp
@@ -11,8 +11,6 @@
 
 #pragma once
 
-#if ENABLE_SUPPORT_KEY
-
 #include <memory>
 
 struct SupportKeyImpl;
@@ -37,7 +35,5 @@ public:
 
     std::string data() const;
 };
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit a0b94e50708a49cc0cf9646e1c6863c4aea4fc75

Fix branding of COOL Lite when key is not present or expired
regression from 246e87fea21d6121fd4bd2daeff8a4f92082477e
we've got [ERRFMT].js and [ERRFMT].css

Signed-off-by: Andras Timar <andras.timar@collabora.com>
Change-Id: Ic59257fedb6d9ae6712f7f9ff16022c01f70112a

wsd/FileServer.cpp
@@ -1263,8 +1263,8 @@ FileServerRequestHandler::ResourceAccessDetails FileServerRequestHandler::prepro
         SupportKey key(keyString);
         if (!key.verify() || key.validDaysRemaining() <= 0)
         {
-            brandCSS = Poco::format(linkCSS, responseRoot, SUPPORT_KEY_BRANDING_UNSUPPORTED);
-            brandJS = Poco::format(scriptJS, responseRoot, SUPPORT_KEY_BRANDING_UNSUPPORTED);
+            brandCSS = Poco::format(linkCSS, responseRoot, std::string(SUPPORT_KEY_BRANDING_UNSUPPORTED));
+            brandJS = Poco::format(scriptJS, responseRoot, std::string(SUPPORT_KEY_BRANDING_UNSUPPORTED));
         }
     }
 