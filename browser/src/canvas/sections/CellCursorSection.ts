afe757dafa27d587d4431ed15030e610bc403bb1
Gökay Şatır <gokaysatir@collabora.com>
2024-07-16 15:08:08 +0300

    Fix cellcursor visibility issue.
    Signed-off-by: Gökay Şatır <gokaysatir@collabora.com>
    Change-Id: Ic26d6b26f967e3de9a9bb6217dbe9e0fb0accc28

--- browser/src/canvas/sections/CellCursorSection.ts
+++ browser/src/canvas/sections/CellCursorSection.ts
@@ -10,7 +10,7 @@
 */
 
 class CellCursorSection extends CanvasSectionObject {
-	name: string = "cellcursor"; // There will be multiple instances of this class. For the viewer's cursor, name will be owncellcursor. Others will have viewId-cellcursor.
+	name: string = "OwnCellCursor";
 	processingOrder: number = L.CSections.AutoFillMarker.processingOrder;
 	drawingOrder: number = L.CSections.AutoFillMarker.drawingOrder;
 	zIndex: number = L.CSections.AutoFillMarker.zIndex;
@@ -23,6 +23,8 @@ class CellCursorSection extends CanvasSectionObject {
 		this.sectionProperties.viewId = viewId;
 		this.sectionProperties.weight = weight;
 		this.sectionProperties.color = color;
+		this.sectionProperties.paneIndex = null;
+		this.sectionProperties.paneCount = null;
 	}
 
 	public getViewId(): number {
@@ -36,7 +38,11 @@ class CellCursorSection extends CanvasSectionObject {
 	private getContainingPane() {
 		const rectangles = app.getViewRectangles();
 
+		this.sectionProperties.paneIndex = -1;
+		this.sectionProperties.paneCount = 1;
+
 		if (rectangles.length > 1) { // We have split panes.
+			this.sectionProperties.paneCount = rectangles.length;
 			for (let i = 0; i < rectangles.length; i++) {
 				if (rectangles[i].pContainsRectangle([
 						this.position[0],
@@ -44,8 +50,10 @@ class CellCursorSection extends CanvasSectionObject {
 						app.calc.cellCursorRectangle.pWidth,
 						app.calc.cellCursorRectangle.pHeight
 					]
-				))
+				)) {
+					this.sectionProperties.paneIndex = i;
 					return rectangles[i];
+				}
 			}
 			return null;
 		}
@@ -66,10 +74,14 @@ class CellCursorSection extends CanvasSectionObject {
 			let penX = this.myTopLeft[0];
 			let penY = this.myTopLeft[1];
 
-			if (pane.x1 === 0) penX = this.position[0] + this.containerObject.getDocumentAnchor()[0];
-			if (pane.y1 === 0) penY = this.position[1] + this.containerObject.getDocumentAnchor()[1];
+			const movePen = this.sectionProperties.paneIndex !== -1 && this.sectionProperties.paneCount - 1 !== this.sectionProperties.paneIndex;
 
-			this.context.translate(penX - this.myTopLeft[0], penY - this.myTopLeft[1]);
+			if (movePen) {
+				if (pane.x1 === 0) penX = this.position[0] + this.containerObject.getDocumentAnchor()[0];
+				if (pane.y1 === 0) penY = this.position[1] + this.containerObject.getDocumentAnchor()[1];
+
+				this.context.translate(penX - this.myTopLeft[0], penY - this.myTopLeft[1]);
+			}
 
 			let x: number = 0;
 			if (app.isCalcRTL()) {
@@ -87,7 +99,8 @@ class CellCursorSection extends CanvasSectionObject {
 				this.context.strokeRect(x + -0.5 + diff, -0.5 + diff, app.calc.cellCursorRectangle.pWidth - 2 * diff, app.calc.cellCursorRectangle.pHeight - 2 * diff);
 			}
 
-			this.context.translate(-penX + this.myTopLeft[0], -penY + this.myTopLeft[1]);
+			if (movePen)
+				this.context.translate(-penX + this.myTopLeft[0], -penY + this.myTopLeft[1]);
 		}
 	}
 
