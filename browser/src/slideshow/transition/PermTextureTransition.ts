3fcfb0da862d977e9e982b95168ea843acce4df1
Javiya Vivekkumar Dineshbhai <vivek.javiya@collabora.com>
2024-07-18 23:43:32 +0530

    slideshow: add Static transition
    Signed-off-by: Javiya Vivekkumar Dineshbhai <vivek.javiya@collabora.com>
    Change-Id: Ic5b48e2f07662766580f6bcf8dc39ba5fc93fefa

---
+++ browser/src/slideshow/transition/PermTextureTransition.ts
@@ -0,0 +1,145 @@
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+declare var SlideShow: any;
+
+class PermTextureTransition extends SimpleTransition {
+	public permTexture: any;
+	public m_nHelperTexture: WebGLTexture | undefined;
+
+	constructor(transitionParameters: TransitionParameters3D) {
+		super(transitionParameters);
+		this.permTexture = this.createPermTexture();
+	}
+
+	public createPermTexture(): WebGLTexture {
+		const permTexture = this.gl.createTexture();
+		this.gl.bindTexture(this.gl.TEXTURE_2D, permTexture);
+		const data = this.generatePermutationData();
+		this.gl.texImage2D(
+			this.gl.TEXTURE_2D,
+			0,
+			this.gl.RGBA,
+			256,
+			256,
+			0,
+			this.gl.RGBA,
+			this.gl.UNSIGNED_BYTE,
+			data,
+		);
+		this.gl.texParameteri(
+			this.gl.TEXTURE_2D,
+			this.gl.TEXTURE_MIN_FILTER,
+			this.gl.NEAREST,
+		);
+		this.gl.texParameteri(
+			this.gl.TEXTURE_2D,
+			this.gl.TEXTURE_MAG_FILTER,
+			this.gl.NEAREST,
+		);
+
+		if (permTexture == null) {
+			throw new Error('Invalid extra texture object');
+		}
+		return permTexture;
+	}
+
+	// prettier-ignore
+	private permutation256: number[] = [
+        215, 100, 200, 204, 233,  50,  85, 196,
+         71, 141, 122, 160,  93, 131, 243, 234,
+        162, 183,  36, 155,   4,  62,  35, 205,
+         40, 102,  33,  27, 255,  55, 214, 156,
+         75, 163, 134, 126, 249,  74, 197, 228,
+         72,  90, 206, 235,  17,  22,  49, 169,
+        227,  89,  16,   5, 117,  60, 248, 230,
+        217,  68, 138,  96, 194, 170, 136,  10,
+        112, 238, 184, 189, 176,  42, 225, 212,
+         84,  58, 175, 244, 150, 168, 219, 236,
+        101, 208, 123,  37, 164, 110, 158, 201,
+         78, 114,  57,  48,  70, 142, 106,  43,
+        232,  26,  32, 252, 239,  98, 191,  94,
+         59, 149,  39, 187, 203, 190,  19,  13,
+        133,  45,  61, 247,  23,  34,  20,  52,
+        118, 209, 146, 193, 222,  18,   1, 152,
+         46,  41,  91, 148, 115,  25, 135,  77,
+        254, 147, 224, 161,   9, 213, 223, 250,
+        231, 251, 127, 166,  63, 179,  81, 130,
+        139,  28, 120, 151, 241,  86, 111,   0,
+         88, 153, 172, 182, 159, 105, 178,  47,
+         51, 167,  65,  66,  92,  73, 198, 211,
+        245, 195,  31, 220, 140,  76, 221, 186,
+        154, 185,  56,  83,  38, 165, 109,  67,
+        124, 226, 132,  53, 229,  29,  12, 181,
+        121,  24, 207, 199, 177, 113,  30,  80,
+          3,  97, 188,  79, 216, 173,   8, 145,
+         87, 128, 180, 237, 240, 137, 125, 104,
+         15, 242, 119, 246, 103, 143,  95, 144,
+          2,  44,  69, 157, 192, 174,  14,  54,
+        218,  82,  64, 210,  11,   6, 129,  21,
+        116, 171,  99, 202,   7, 107, 253, 108
+    ];
+
+	public generatePermutationData(): Uint8Array {
+		const a = new Uint8Array(256 * 256 * 4); // Multiplied by 4 for RGBA
+		for (let y = 0; y < 256; y++) {
+			for (let x = 0; x < 256; x++) {
+				const base = (x + y * 256) * 4;
+				const value = this.permutation256[(y + this.permutation256[x]) % 256];
+				a[base] = value; // R
+				a[base + 1] = value; // G (could be different for variety)
+				a[base + 2] = value; // B (could be different for variety)
+				a[base + 3] = 255; // A (alpha channel)
+			}
+		}
+		return a;
+	}
+
+	public displayPermSlide_(): void {
+		this.gl.activeTexture(this.gl.TEXTURE2);
+		this.gl.bindTexture(this.gl.TEXTURE_2D, this.permTexture);
+		this.gl.uniform1i(
+			this.gl.getUniformLocation(this.program, 'permTexture'),
+			2,
+		);
+	}
+
+	public displaySlides_(): void {
+		const t = this.time;
+		this.gl.uniform1f(
+			this.gl.getUniformLocation(this.program, 'time'),
+			this.time,
+		);
+
+		this.setBufferData(this.leavingPrimitives[0].vertices);
+		this.gl.activeTexture(this.gl.TEXTURE0);
+		this.gl.bindTexture(this.gl.TEXTURE_2D, this.textures[0]);
+		this.gl.uniform1i(
+			this.gl.getUniformLocation(this.program, 'leavingSlideTexture'),
+			0,
+		);
+
+		this.gl.activeTexture(this.gl.TEXTURE1);
+		this.gl.bindTexture(this.gl.TEXTURE_2D, this.textures[1]);
+		this.gl.uniform1i(
+			this.gl.getUniformLocation(this.program, 'enteringSlideTexture'),
+			1,
+		);
+		this.gl.drawArrays(
+			this.gl.TRIANGLE_STRIP,
+			0,
+			this.leavingPrimitives[0].vertices.length,
+		);
+
+		this.displayPermSlide_();
+	}
+}
+
+SlideShow.PermTextureTransition = PermTextureTransition;
---
+++ browser/src/slideshow/transition/StaticNoiseTransition.ts
@@ -0,0 +1,108 @@
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+declare var SlideShow: any;
+
+class StaticNoiseTransitionImp extends PermTextureTransition {
+	constructor(transitionParameters: TransitionParameters3D) {
+		super(transitionParameters);
+	}
+
+	public getFragmentShader(): string {
+		return `#version 300 es
+				precision mediump float;
+
+				uniform sampler2D leavingSlideTexture;
+				uniform sampler2D enteringSlideTexture;
+				uniform sampler2D permTexture;
+				uniform float time;
+
+				in vec2 v_texturePosition;
+				in vec3 v_normal;
+				out vec4 outColor;
+
+				float snoise(vec2 P) {
+					return texture(permTexture, P).r;
+				}
+
+				#define PART 0.5
+				#define START 0.4
+				#define END 0.9
+
+				void main() {
+					float sn = snoise(10.0 * v_texturePosition + time * 0.07);
+					
+					if (time < PART) {
+						float sn1 = snoise(vec2(time * 15.0, 20.0 * v_texturePosition.y));
+						float sn2 = snoise(v_texturePosition);
+						
+						if (sn1 > 1.0 - time * time && sn2 < 2.0 * time + 0.1)
+							outColor = vec4(sn, sn, sn, 1.0);
+						else if (time > START)
+							outColor = mix(texture(leavingSlideTexture, v_texturePosition), vec4(sn, sn, sn, 1.0), (time - START) / (PART - START));
+						else
+							outColor = texture(leavingSlideTexture, v_texturePosition);
+					} else if (time > END) {
+						outColor = mix(vec4(sn, sn, sn, 1.0), texture(enteringSlideTexture, v_texturePosition), (time - END) / (1.0 - END));
+					} else {
+						outColor = vec4(sn, sn, sn, 1.0);
+					}
+				}
+				`;
+	}
+}
+
+function StaticNoiseTransition(transitionParameters: TransitionParameters) {
+	const slide = new Primitive();
+	slide.pushTriangle([0, 0], [1, 0], [0, 1]);
+	slide.pushTriangle([1, 0], [0, 1], [1, 1]);
+
+	const aLeavingPrimitives: Primitive[] = [];
+	aLeavingPrimitives.push(Primitive.cloneDeep(slide));
+
+	slide.operations.push(
+		makeRotateAndScaleDepthByWidth(
+			vec3.fromValues(0, 1, 0),
+			vec3.fromValues(0, 0, -1),
+			90,
+			false,
+			false,
+			0.0,
+			1.0,
+		),
+	);
+
+	const aEnteringPrimitives: Primitive[] = [];
+	aEnteringPrimitives.push(slide);
+
+	const aOperations: Operation[] = [];
+	aOperations.push(
+		makeRotateAndScaleDepthByWidth(
+			vec3.fromValues(0, 1, 0),
+			vec3.fromValues(0, 0, -1),
+			-90,
+			false,
+			true,
+			0.0,
+			1.0,
+		),
+	);
+
+	const newTransitionParameters: TransitionParameters3D = {
+		...transitionParameters,
+		leavingPrimitives: aLeavingPrimitives,
+		enteringPrimitives: aEnteringPrimitives,
+		allOperations: aOperations,
+	};
+
+	return new StaticNoiseTransitionImp(newTransitionParameters);
+}
+
+SlideShow.StaticNoiseTransition = StaticNoiseTransition;
