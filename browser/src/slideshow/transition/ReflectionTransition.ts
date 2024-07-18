95307a1ef54c5873e819098def056c661907dfe6
Javiya Vivekkumar Dineshbhai <vivek.javiya@collabora.com>
2024-07-18 15:29:57 +0530

    slideshow: add turn around transition
    Signed-off-by: Javiya Vivekkumar Dineshbhai <vivek.javiya@collabora.com>
    Change-Id: I2bf9ffb4df258f089a70a25f2fa861df272fa5e8

---
+++ browser/src/slideshow/transition/ReflectionTransition.ts
@@ -0,0 +1,42 @@
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+declare var SlideShow: any;
+
+class ReflectionTransition extends SimpleTransition {
+	constructor(transitionParameters: TransitionParameters3D) {
+		super(transitionParameters);
+	}
+
+	public displaySlides_(): void {
+		const t = this.time;
+		this.applyAllOperation(t);
+
+		if (t < 0.5) {
+			this.displayPrimitive(
+				t,
+				this.gl.TEXTURE0,
+				0,
+				this.leavingPrimitives,
+				'slideTexture',
+			);
+		} else {
+			this.displayPrimitive(
+				t,
+				this.gl.TEXTURE0,
+				1,
+				this.enteringPrimitives,
+				'slideTexture',
+			);
+		}
+	}
+}
+
+SlideShow.ReflectionTransition = ReflectionTransition;
