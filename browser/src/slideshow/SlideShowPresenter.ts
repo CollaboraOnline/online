3d94aae2018f907cbfdb39acfca909bbb3d8ad43
Marco Cecchetti <marco.cecchetti@collabora.com>
2024-07-24 19:03:09 +0200

    slideshow: create animation tree from animations info
    Signed-off-by: Marco Cecchetti <marco.cecchetti@collabora.com>
    Change-Id: Ica809ccb9298c70c889d436fc30581bc432a9623

--- browser/src/slideshow/SlideShowPresenter.ts
+++ browser/src/slideshow/SlideShowPresenter.ts
@@ -39,6 +39,7 @@ interface SlideInfo {
 		isCustom: boolean;
 		fillColor: string;
 	};
+	animations: any;
 	next: string;
 	prev: string;
 }
@@ -59,6 +60,7 @@ class SlideShowPresenter {
 	_windowCloseInterval: ReturnType<typeof setInterval> = null;
 	_currentSlide: number = 0;
 	_slideRenderer: SlideRenderer = null;
+	_animationsHandler: SlideAnimations = null;
 
 	constructor(map: any) {
 		this._map = map;
@@ -124,6 +126,7 @@ class SlideShowPresenter {
 
 		this._slideCompositor.fetchAndRun(this._currentSlide, () => {
 			this._currentSlide++;
+			this.createAnimationsHandler();
 			this._doTransition(this._slideRenderer._slideTexture, this._currentSlide);
 		});
 	}
@@ -135,6 +138,7 @@ class SlideShowPresenter {
 
 		this._slideCompositor.fetchAndRun(this._currentSlide, () => {
 			this._currentSlide--;
+			this.createAnimationsHandler();
 			this._doPresentation();
 		});
 	}
@@ -450,10 +454,26 @@ class SlideShowPresenter {
 			);
 		}
 
+		this.createAnimationsHandler();
+
 		this._slideCompositor.updatePresentationInfo(this._presentationInfo);
 		this._slideCompositor.fetchAndRun(0, () => {
 			this._doPresentation();
 		});
+	}
+
+	createAnimationsHandler() {
+		const slideInfo = this.getSlideInfo(this._currentSlide);
+		if (slideInfo.animations) {
+			this._animationsHandler = new SlideAnimations();
+			this._animationsHandler.importAnimations(slideInfo.animations.root);
+			this._animationsHandler.parseInfo();
+			const animationTree = this._animationsHandler.getAnimationsTree();
+			if (animationTree) {
+				const info = animationTree.getInfo(true);
+				window.app.console.log('animations info: \n' + info);
+			}
+		}
 	}
 }
 
