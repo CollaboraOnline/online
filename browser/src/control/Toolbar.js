CollaboraOnline/online

commit 18d100bf5c706f1eae60a5298ebeeade3e1b62a7

uno: fix CallPythonScript calling js error
we send "vnd.sun.star.script" message without uno prefix via uno command

Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
Change-Id: If0e103bd74ac7f34a57559b2eebc1001352b0057

browser/src/control/Toolbar.js
@@ -350,7 +350,7 @@ L.Map.include({
 	},
 
 	sendUnoCommand: function (command, json, force) {
-		if (command.indexOf('.uno:') < 0)
+		if (command.indexOf('.uno:') < 0 || command.indexOf('vnd.sun.star.script') < 0)
 			console.error('Trying to send uno command without prefix: "' + command + '"');
 
 		if ((command.startsWith('.uno:Sidebar') && !command.startsWith('.uno:SidebarShow')) ||

commit e2b20cdabda752d68f7dd4e0fce11ff81ea1d1de

Show error only when needed
we only want to show it if there is no prefix
introduced in 18d100b

Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
Change-Id: Ica0fe317c14f2199e132c6ed84cb939f2f5ed060

browser/src/control/Toolbar.js
@@ -350,7 +350,7 @@ L.Map.include({
 	},
 
 	sendUnoCommand: function (command, json, force) {
-		if (command.indexOf('.uno:') < 0 || command.indexOf('vnd.sun.star.script') < 0)
+		if (command.indexOf('.uno:') < 0 && command.indexOf('vnd.sun.star.script') < 0)
 			console.error('Trying to send uno command without prefix: "' + command + '"');
 
 		if ((command.startsWith('.uno:Sidebar') && !command.startsWith('.uno:SidebarShow')) ||