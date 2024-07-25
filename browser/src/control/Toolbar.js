8fdde7c7b6fbd901db5bbb79168bc5bff58315f6
Szymon Kłos <szymon.klos@collabora.com>
2024-07-25 08:51:51 +0200

    Show error only when needed
    we only want to show it if there is no prefix
    introduced in 18d100b
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: Ica0fe317c14f2199e132c6ed84cb939f2f5ed060
    (cherry picked from commit 13c1c1423322139af9c62875a7379b63ede762ca)

--- browser/src/control/Toolbar.js
+++ browser/src/control/Toolbar.js
@@ -350,7 +350,7 @@ L.Map.include({
 	},
 
 	sendUnoCommand: function (command, json, force) {
-		if (command.indexOf('.uno:') < 0 || command.indexOf('vnd.sun.star.script') < 0)
+		if (command.indexOf('.uno:') < 0 && command.indexOf('vnd.sun.star.script') < 0)
 			console.error('Trying to send uno command without prefix: "' + command + '"');
 
 		if ((command.startsWith('.uno:Sidebar') && !command.startsWith('.uno:SidebarShow')) ||
