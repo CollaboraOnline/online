e3f5292eff5edf498be21c05f947e3f22add9b89
NickWingate <nick.wingate@collabora.com>
2024-07-29 16:49:12 +0100

    Convert Control.Sidebar.js to Control.Sidebar.ts
    Signed-off-by: NickWingate <nick.wingate@collabora.com>
    Change-Id: Iba2142d44f29ad0eb9293859f45c0718d3a223b4

--- browser/src/control/Control.Sidebar.ts
+++ browser/src/control/Control.Sidebar.ts
@@ -9,44 +9,59 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 /*
- * L.Control.Sidebar
+ * Control.Sidebar
  */
 
-/* global $ app */
-L.Control.Sidebar = L.Control.extend({
+/* global app */
+interface SidebarOptions {
+	animSpeed: number;
+}
+class Sidebar extends L.Control {
+	options: SidebarOptions;
 
-	options: {
-		animSpeed: 1000 /* Default speed: to be used on load */
-	},
+	container: HTMLDivElement;
+	builder: any;
+	targetDeckCommand: string;
 
-	container: null,
-	builder: null,
-	targetDeckCommand: null,
+	constructor(options: SidebarOptions) {
+		if (options.animSpeed === null)
+			options.animSpeed = 1000; /* Default speed: to be used on load */
 
-	onAdd: function (map) {
+		super(options);
+	}
+
+	onAdd(map: ReturnType<typeof L.map>) {
 		this.map = map;
 
 		app.events.on('resize', this.onResize.bind(this));
 
-		this.builder = new L.control.jsDialogBuilder({mobileWizard: this, map: map, cssClass: 'jsdialog sidebar'});
-		this.container = L.DomUtil.create('div', 'sidebar-container', $('#sidebar-panel').get(0));
+		this.builder = new L.control.jsDialogBuilder({
+			mobileWizard: this,
+			map: map,
+			cssClass: 'jsdialog sidebar',
+		});
+		this.container = L.DomUtil.create(
+			'div',
+			'sidebar-container',
+			$('#sidebar-panel').get(0),
+		);
 
 		this.map.on('sidebar', this.onSidebar, this);
 		this.map.on('jsdialogupdate', this.onJSUpdate, this);
 		this.map.on('jsdialogaction', this.onJSAction, this);
-	},
+	}
 
-	onRemove: function() {
+	onRemove() {
 		this.map.off('sidebar');
 		this.map.off('jsdialogupdate', this.onJSUpdate, this);
 		this.map.off('jsdialogaction', this.onJSAction, this);
-	},
+	}
 
-	isVisible: function() {
+	isVisible(): boolean {
 		return $('#sidebar-dock-wrapper').is(':visible');
-	},
+	}
 
-	closeSidebar: function() {
+	closeSidebar() {
 		$('#sidebar-dock-wrapper').hide();
 		this.map._onResize();
 
@@ -56,19 +71,16 @@ L.Control.Sidebar = L.Control.extend({
 		}
 
 		this.map.uiManager.setDocTypePref('ShowSidebar', false);
-	},
+	}
 
-	onJSUpdate: function (e) {
+	onJSUpdate(e: FireEvent) {
 		var data = e.data;
 
-		if (data.jsontype !== 'sidebar')
-			return;
+		if (data.jsontype !== 'sidebar') return;
 
-		if (!this.container)
-			return;
+		if (!this.container) return;
 
-		if (!this.builder)
-			return;
+		if (!this.builder) return;
 
 		// reduce unwanted warnings in console
 		if (data.control.id === 'addonimage') {
@@ -77,112 +89,124 @@ L.Control.Sidebar = L.Control.extend({
 		}
 
 		this.builder.updateWidget(this.container, data.control);
-	},
+	}
 
-	onJSAction: function (e) {
+	onJSAction(e: FireEvent) {
 		var data = e.data;
 
-		if (data.jsontype !== 'sidebar')
-			return;
+		if (data.jsontype !== 'sidebar') return;
 
-		if (!this.builder)
-			return;
+		if (!this.builder) return;
 
-		if (!this.container)
-			return;
+		if (!this.container) return;
 
 		var innerData = data.data;
-		if (!innerData)
-			return;
+		if (!innerData) return;
 
 		var controlId = innerData.control_id;
 
 		// Panels share the same name for main containers, do not execute actions for them
 		// if panel has to be shown or hidden, full update will appear
-		if (controlId === 'contents' ||
+		if (
+			controlId === 'contents' ||
 			controlId === 'Panel' ||
 			controlId === 'titlebar' ||
-			controlId === 'addonimage') {
-			window.app.console.log('Ignored action: ' + innerData.action_type + ' for control: ' + controlId);
+			controlId === 'addonimage'
+		) {
+			window.app.console.log(
+				'Ignored action: ' +
+					innerData.action_type +
+					' for control: ' +
+					controlId,
+			);
 			return;
 		}
 
 		this.builder.executeAction(this.container, innerData);
-	},
+	}
 
-	onResize: function() {
+	onResize() {
 		var wrapper = document.getElementById('sidebar-dock-wrapper');
-		wrapper.style.maxHeight = document.getElementById('document-container').getBoundingClientRect().height + 'px';
-	},
+		wrapper.style.maxHeight =
+			document.getElementById('document-container').getBoundingClientRect()
+				.height + 'px';
+	}
 
-	unsetSelectedSidebar: function() {
+	unsetSelectedSidebar() {
 		this.map.uiManager.setDocTypePref('PropertyDeck', false);
 		this.map.uiManager.setDocTypePref('SdSlideTransitionDeck', false);
 		this.map.uiManager.setDocTypePref('SdCustomAnimationDeck', false);
 		this.map.uiManager.setDocTypePref('SdMasterPagesDeck', false);
 		this.map.uiManager.setDocTypePref('NavigatorDeck', false);
 		this.map.uiManager.setDocTypePref('A11yCheckDeck', false);
-	},
+	}
 
-	commandForDeck: function(deckId) {
-		if (deckId === 'PropertyDeck')
-			return '.uno:SidebarDeck.PropertyDeck';
+	commandForDeck(deckId: string): string {
+		if (deckId === 'PropertyDeck') return '.uno:SidebarDeck.PropertyDeck';
 		else if (deckId === 'SdSlideTransitionDeck')
 			return '.uno:SlideChangeWindow';
-		else if (deckId === 'SdCustomAnimationDeck')
-			return '.uno:CustomAnimation';
-		else if (deckId === 'SdMasterPagesDeck')
-			return '.uno:MasterSlidesPanel';
-		else if (deckId === 'NavigatorDeck')
-			return '.uno:Navigator';
+		else if (deckId === 'SdCustomAnimationDeck') return '.uno:CustomAnimation';
+		else if (deckId === 'SdMasterPagesDeck') return '.uno:MasterSlidesPanel';
+		else if (deckId === 'NavigatorDeck') return '.uno:Navigator';
 		else if (deckId === 'A11yCheckDeck')
 			return '.uno:SidebarDeck.A11yCheckDeck';
 		return '';
-	},
+	}
 
-	setupTargetDeck: function(unoCommand) {
+	setupTargetDeck(unoCommand: string) {
 		this.targetDeckCommand = unoCommand;
-	},
+	}
 
-	getTargetDeck: function() {
+	getTargetDeck(): string {
 		return this.targetDeckCommand;
-	},
+	}
 
-	changeDeck: function(unoCommand) {
-		if (unoCommand !== null)
-			app.socket.sendMessage('uno ' + unoCommand);
+	changeDeck(unoCommand: string | null) {
+		if (unoCommand !== null) app.socket.sendMessage('uno ' + unoCommand);
 		this.setupTargetDeck(unoCommand);
-	},
+	}
 
-	onSidebar: function(data) {
+	onSidebar(data: FireEvent) {
 		var sidebarData = data.data;
 		this.builder.setWindowId(sidebarData.id);
 		$(this.container).empty();
 
-		if (sidebarData.action === 'close' || window.app.file.disableSidebar || this.map.isReadOnlyMode()) {
+		if (
+			sidebarData.action === 'close' ||
+			window.app.file.disableSidebar ||
+			this.map.isReadOnlyMode()
+		) {
 			this.closeSidebar();
 		} else if (sidebarData.children) {
 			for (var i = sidebarData.children.length - 1; i >= 0; i--) {
-				if (sidebarData.children[i].type !== 'deck' || sidebarData.children[i].visible === false)
+				if (
+					sidebarData.children[i].type !== 'deck' ||
+					sidebarData.children[i].visible === false
+				)
 					sidebarData.children.splice(i, 1);
 			}
 
 			if (sidebarData.children.length) {
 				this.onResize();
 
-				if (sidebarData.children && sidebarData.children[0] && sidebarData.children[0].id) {
+				if (
+					sidebarData.children &&
+					sidebarData.children[0] &&
+					sidebarData.children[0].id
+				) {
 					this.unsetSelectedSidebar();
 					var currentDeck = sidebarData.children[0].id;
 					this.map.uiManager.setDocTypePref(currentDeck, true);
 					if (this.targetDeckCommand) {
 						var stateHandler = this.map['stateChangeHandler'];
-						var isCurrent = stateHandler ?
-							stateHandler.getItemValue(this.targetDeckCommand) : false;
+						var isCurrent = stateHandler
+							? stateHandler.getItemValue(this.targetDeckCommand)
+							: false;
 						// just to be sure chack with other method
 						if (isCurrent === 'false' || !isCurrent)
-							isCurrent = this.targetDeckCommand === this.commandForDeck(currentDeck);
-						if (this.targetDeckCommand &&
-							(isCurrent === 'false' || !isCurrent))
+							isCurrent =
+								this.targetDeckCommand === this.commandForDeck(currentDeck);
+						if (this.targetDeckCommand && (isCurrent === 'false' || !isCurrent))
 							this.changeDeck(this.targetDeckCommand);
 					} else {
 						this.changeDeck(this.targetDeckCommand);
@@ -198,16 +222,9 @@ L.Control.Sidebar = L.Control.extend({
 				this.closeSidebar();
 			}
 		}
-	},
+	}
+}
 
-	setTabs: function(/*tabs*/) {
-	},
-
-	selectedTab: function() {
-		// implement in child classes
-	},
-});
-
-L.control.sidebar = function (options) {
-	return new L.Control.Sidebar(options);
+L.control.sidebar = function (options: SidebarOptions) {
+	return new Sidebar(options);
 };
