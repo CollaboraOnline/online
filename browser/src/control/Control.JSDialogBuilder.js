CollaboraOnline/online

commit b238f7e1ca7052d793673f3c76f281ac6db8b3d4

PushButtonControl: Fix alignment (regression from locking overlay)
With the "new" wrapper introduced in
e02e1df76d8379e4da7e7c87f10c594bc558d6d2 all the buttons are misaligned.
- Ensure that the wrapper is set with its child centered

Signed-off-by: Pedro Pinto Silva <pedro.silva@collabora.com>
Change-Id: I73b0c7952daf70851a59e4b0e290ba3d32feed23

browser/src/control/Control.JSDialogBuilder.js
@@ -1548,7 +1548,7 @@ L.Control.JSDialogBuilder = L.Control.extend({
 		if (data.id && data.id === 'changepass' && builder.map['wopi'].IsOwner === false) {
 			data.enabled = false;
 		}
-		var wrapper = L.DomUtil.create('div', '', parentContainer); // need for locking overlay
+		var wrapper = L.DomUtil.create('div', 'd-flex justify-content-center', parentContainer); // need for locking overlay
 		var pushbutton = L.DomUtil.create('button', 'ui-pushbutton ' + builder.options.cssClass, wrapper);
 		pushbutton.id = data.id;
 		builder._setAccessKey(pushbutton, builder._getAccessKeyFromText(data.text));

commit 111159620545aee4f8515ed1342b13b8a997dbb3

UI: PushButtonControl: Fix alignment in mobile
regression from b238f7e

Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
Change-Id: Ide37eb5dddde34f7ccaf15858ca7b0e4069c0636

browser/src/control/Control.JSDialogBuilder.js
@@ -1548,7 +1548,8 @@ L.Control.JSDialogBuilder = L.Control.extend({
 		if (data.id && data.id === 'changepass' && builder.map['wopi'].IsOwner === false) {
 			data.enabled = false;
 		}
-		var wrapper = L.DomUtil.create('div', 'd-flex justify-content-center', parentContainer); // need for locking overlay
+		var wrapperClass = window.mode.isMobile() ? '' : 'd-flex justify-content-center';
+		var wrapper = L.DomUtil.create('div', wrapperClass, parentContainer); // need for locking overlay
 		var pushbutton = L.DomUtil.create('button', 'ui-pushbutton ' + builder.options.cssClass, wrapper);
 		pushbutton.id = data.id;
 		builder._setAccessKey(pushbutton, builder._getAccessKeyFromText(data.text));
@@ -2519,7 +2520,7 @@ L.Control.JSDialogBuilder = L.Control.extend({
 				const scrollingInterval = setInterval(function () {
 					app.dispatcher.dispatch(data.command);
 				}, 100);
-	
+
 				$(document).one('mouseup', function () {
 					clearInterval(scrollingInterval);
 				});
@@ -2535,7 +2536,7 @@ L.Control.JSDialogBuilder = L.Control.extend({
 					const pressAndHoldTimer = setTimeout(() => {
 						handlePressAndHold(data);
 					}, 500);
-			
+
 					$(document).one('mouseup', () => {
 						clearTimeout(pressAndHoldTimer);
 					});