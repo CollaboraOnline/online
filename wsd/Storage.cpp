76f008f8e5fc5a66c5db0a1607946cf6eac6eb90
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2024-08-04 20:27:41 -0400

    wsd: use StorageConnectionManager::getHttpSession()
    And remove unused ssl code from StorageBase.
    
    Change-Id: Ic9b01d9fa91420cbe243b566ca5bf46130030ad8
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- wsd/Storage.cpp
+++ wsd/Storage.cpp
@@ -20,16 +20,6 @@
 
 #if !MOBILEAPP
 
-#include <Poco/Net/AcceptCertificateHandler.h>
-#include <Poco/Net/Context.h>
-#include <Poco/Net/HTTPClientSession.h>
-#include <Poco/Net/HTTPRequest.h>
-#include <Poco/Net/HTTPResponse.h>
-#include <Poco/Net/HTTPSClientSession.h>
-#include <Poco/Net/KeyConsoleHandler.h>
-#include <Poco/Net/NameValueCollection.h>
-#include <Poco/Net/SSLManager.h>
-
 #include <cassert>
 #include <errno.h>
 
@@ -67,8 +57,6 @@
 #endif // IOS
 
 bool StorageBase::FilesystemEnabled;
-bool StorageBase::SSLAsScheme = true;
-bool StorageBase::SSLEnabled = false;
 
 #if !MOBILEAPP
 
@@ -103,81 +91,9 @@ void StorageBase::initialize()
 
     HostUtil::parseAliases(app.config());
 
-#if ENABLE_SSL
-    // FIXME: should use our own SSL socket implementation here.
-    Poco::Crypto::initializeCrypto();
-    Poco::Net::initializeSSL();
-
-    // Init client
-    Poco::Net::Context::Params sslClientParams;
-
-    // false default for upgrade to preserve legacy configuration
-    // in-config-file defaults are true.
-    SSLAsScheme = COOLWSD::getConfigValue<bool>("storage.ssl.as_scheme", false);
-
-    // Fallback to ssl.enable if not set - for back compatibility & simplicity.
-    SSLEnabled = COOLWSD::getConfigValue<bool>(
-        "storage.ssl.enable", COOLWSD::getConfigValue<bool>("ssl.enable", true));
-
-#if ENABLE_DEBUG
-    char *StorageSSLEnabled = getenv("STORAGE_SSL_ENABLE");
-    if (StorageSSLEnabled != NULL)
-    {
-        if (!strcasecmp(StorageSSLEnabled, "true"))
-            SSLEnabled = true;
-        else if (!strcasecmp(StorageSSLEnabled, "false"))
-            SSLEnabled = false;
-    }
-#endif
-
-    if (SSLEnabled || SSLAsScheme)
-    {
-        if (COOLWSD::isSSLEnabled())
-        {
-            sslClientParams.certificateFile = COOLWSD::getPathFromConfigWithFallback("storage.ssl.cert_file_path", "ssl.cert_file_path");
-            sslClientParams.privateKeyFile = COOLWSD::getPathFromConfigWithFallback("storage.ssl.key_file_path", "ssl.key_file_path");
-            sslClientParams.caLocation = COOLWSD::getPathFromConfigWithFallback("storage.ssl.ca_file_path", "ssl.ca_file_path");
-        }
-        else
-        {
-            sslClientParams.certificateFile = COOLWSD::getPathFromConfig("storage.ssl.cert_file_path");
-            sslClientParams.privateKeyFile = COOLWSD::getPathFromConfig("storage.ssl.key_file_path");
-            sslClientParams.caLocation = COOLWSD::getPathFromConfig("storage.ssl.ca_file_path");
-        }
-        sslClientParams.cipherList = COOLWSD::getPathFromConfigWithFallback("storage.ssl.cipher_list", "ssl.cipher_list");
-        const bool sslVerification = COOLWSD::getConfigValue<bool>("ssl.ssl_verification", true);
-        sslClientParams.verificationMode = !sslVerification ? Poco::Net::Context::VERIFY_NONE : Poco::Net::Context::VERIFY_STRICT;
-        sslClientParams.loadDefaultCAs = true;
-    }
-    else
-        sslClientParams.verificationMode = Poco::Net::Context::VERIFY_NONE;
-
-    Poco::SharedPtr<Poco::Net::PrivateKeyPassphraseHandler> consoleClientHandler = new Poco::Net::KeyConsoleHandler(false);
-    Poco::SharedPtr<Poco::Net::InvalidCertificateHandler> invalidClientCertHandler = new Poco::Net::AcceptCertificateHandler(false);
-
-    Poco::Net::Context::Ptr sslClientContext = new Poco::Net::Context(Poco::Net::Context::CLIENT_USE, sslClientParams);
-    sslClientContext->disableProtocols(Poco::Net::Context::Protocols::PROTO_SSLV2 |
-                                       Poco::Net::Context::Protocols::PROTO_SSLV3 |
-                                       Poco::Net::Context::Protocols::PROTO_TLSV1);
-    Poco::Net::SSLManager::instance().initializeClient(std::move(consoleClientHandler),
-                                                       std::move(invalidClientCertHandler),
-                                                       std::move(sslClientContext));
-
-    // Initialize our client SSL context.
-    ssl::Manager::initializeClientContext(
-        sslClientParams.certificateFile, sslClientParams.privateKeyFile, sslClientParams.caLocation,
-        sslClientParams.cipherList,
-        sslClientParams.verificationMode == Poco::Net::Context::VERIFY_NONE
-            ? ssl::CertificateVerification::Disabled
-            : ssl::CertificateVerification::Required);
-    if (!ssl::Manager::isClientContextInitialized())
-        LOG_ERR("Failed to initialize Client SSL.");
-    else
-        LOG_INF("Initialized Client SSL.");
-#endif
-#else
+#else // !MOBILEAPP
     FilesystemEnabled = true;
-#endif
+#endif // MOBILEAPP
 }
 
 #if !MOBILEAPP
@@ -463,39 +379,6 @@ void LocalStorage::uploadLocalFileToStorageAsync(const Authorization& /*auth*/,
     if (asyncUploadCallback)
         asyncUploadCallback(AsyncUpload(AsyncUpload::State::Complete, res));
 }
-
-#if !MOBILEAPP
-
-std::shared_ptr<http::Session> StorageBase::getHttpSession(const Poco::URI& uri)
-{
-    bool useSSL = false;
-    if (SSLAsScheme)
-    {
-        // the WOPI URI itself should control whether we use SSL or not
-        // for whether we verify vs. certificates, cf. above
-        useSSL = uri.getScheme() != "http";
-    }
-    else
-    {
-        // We decoupled the Wopi communication from client communication because
-        // the Wopi communication must have an independent policy.
-        // So, we will use here only Storage settings.
-        useSSL = SSLEnabled || COOLWSD::isSSLTermination();
-    }
-
-    const auto protocol
-        = useSSL ? http::Session::Protocol::HttpSsl : http::Session::Protocol::HttpUnencrypted;
-
-    // Create the session.
-    auto httpSession = http::Session::create(uri.getHost(), protocol, uri.getPort());
-
-    static int timeoutSec = COOLWSD::getConfigValue<int>("net.connection_timeout_secs", 30);
-    httpSession->setTimeout(std::chrono::seconds(timeoutSec));
-
-    return httpSession;
-}
-
-#endif // !MOBILEAPP
 
 void LockContext::initSupportsLocks()
 {
--- wsd/wopi/WopiStorage.cpp
+++ wsd/wopi/WopiStorage.cpp
@@ -12,6 +12,7 @@
 #include <config.h>
 
 #include "WopiStorage.hpp"
+#include "wopi/StorageConnectionManager.hpp"
 
 #include <Auth.hpp>
 #include <CommandControl.hpp>
@@ -342,7 +343,8 @@ StorageBase::LockUpdateResult WopiStorage::updateLockState(const Authorization& 
 
     try
     {
-        std::shared_ptr<http::Session> httpSession = getHttpSession(uriObject);
+        std::shared_ptr<http::Session> httpSession =
+            StorageConnectionManager::getHttpSession(uriObject);
 
         http::Request httpRequest = initHttpRequest(uriObject, auth);
         httpRequest.setVerb(http::Request::VERB_POST);
@@ -482,7 +484,8 @@ std::string WopiStorage::downloadDocument(const Poco::URI& uriObject, const std:
                                           const Authorization& auth, unsigned redirectLimit)
 {
     const auto startTime = std::chrono::steady_clock::now();
-    std::shared_ptr<http::Session> httpSession = getHttpSession(uriObject);
+    std::shared_ptr<http::Session> httpSession =
+        StorageConnectionManager::getHttpSession(uriObject);
 
     http::Request httpRequest = initHttpRequest(uriObject, auth);
 
@@ -671,7 +674,7 @@ void WopiStorage::uploadLocalFileToStorageAsync(const Authorization& auth, LockC
     try
     {
         assert(!_uploadHttpSession && "Unexpected to have an upload http::session");
-        _uploadHttpSession = getHttpSession(uriObject);
+        _uploadHttpSession = StorageConnectionManager::getHttpSession(uriObject);
 
         http::Request httpRequest = initHttpRequest(uriObject, auth);
         httpRequest.setVerb(http::Request::VERB_POST);
