CollaboraOnline/online

commit 6e790c63d0997094979b2a237f428ef7706e62ef

feat: Changes to UnitPerf measuring
Previously all metrics were being recorded into seperate CSV's and without the git commit hash, date, type of the test . Also CPU usage and stress run timings were an aggregate of the setup + load + edit phases.

Now each metric is recorded into a summary CSV with the git commit hash as a primary key followed by date, type of test (writer, calc etc..), the phase (setup,load,edit) , the metric and of course the value. This is to allow the use of a pivot table and allow for some nice charting.

Signed-off-by: elliotfreebairn <elliot.freebairn@collabora.com>
Change-Id: I868a0a1c91b30392ab002ce149cefae0dd3036cc

common/Unit.hpp
@@ -480,6 +480,13 @@ public:
         return false;
     }
 
+
+    // Called when a document has started loading
+    virtual void onDocumentLoading(){}
+
+    // Called when a document has finished loading
+    virtual void onDocumentLoaded(){}
+
     /// To force the save operation being handled as auto-save from a unit test.
     virtual bool isAutosave()
     {

wsd/DocumentBroker.cpp
@@ -891,6 +891,11 @@ bool DocumentBroker::download(
     {
         _docState.setStatus(DocumentState::Status::Downloading);
 
+        if(_unitWsd != nullptr)
+        {
+            _unitWsd->onDocumentLoading();
+        }
+
         // Pass the public URI to storage as it needs to load using the token
         // and other storage-specific data provided in the URI.
         LOG_DBG("Creating new storage instance for URI ["
@@ -2439,6 +2444,12 @@ void DocumentBroker::setLoaded()
             std::max(std::chrono::seconds(minTimeoutSecs), std::chrono::seconds(5)));
         LOG_DBG("Document loaded in " << _loadDuration << ", saving-timeout set to "
                                       << _saveManager.getSavingTimeout());
+
+        if(_unitWsd != nullptr)
+        {
+            _unitWsd->onDocumentLoaded();
+        }
+
     }
 }
 

test/UnitPerf.cpp
@@ -10,6 +10,7 @@
  */
 
 #include <config.h>
+#include <config_version.h>
 
 #include <Unit.hpp>
 #include <Util.hpp>
@@ -31,9 +32,7 @@
 /// Save torture testcase.
 class UnitPerf : public UnitWSD
 {
-    void testPerf();
-
-    void dumpCPUTimeToCSV(long cpuTime);
+    void testPerf(std::string testType, std::string fileType, std::string tracesStr);
 
     void configure(Poco::Util::LayeredConfiguration& config) override
     {
@@ -47,24 +46,29 @@ class UnitPerf : public UnitWSD
 public:
     UnitPerf();
     void invokeWSDTest() override;
+    void onDocumentLoading() override;
+    void onDocumentLoaded() override;
     std::unique_ptr<Util::SysStopwatch> _timer;
+    std::shared_ptr<Stats> stats;
 };
 
-void UnitPerf::testPerf()
+void UnitPerf::testPerf(std::string testType, std::string fileType, std::string traceStr)
 {
-    auto stats = std::make_shared<Stats>();
+    stats = std::make_shared<Stats>();
+    stats->setTypeOfTest(testType);
 
     TerminatingPoll poll("performance test");
 
-    std::string docName = "empty.odt";
+    std::string docName = "empty." + fileType;
 
     std::string filePath, dummy;
+
     helpers::getDocumentPathAndURL(docName, filePath, dummy, "testPerf");
 
-    const std::string tracePath;
+    const std::string tracePath = TDOC + traceStr;
     StressSocketHandler::addPollFor(
         poll, helpers::getTestServerURI("ws"),
-        filePath, TDOC "/../traces/perf-writer.txt",
+        filePath, tracePath,
         stats);
 
     do {
@@ -74,19 +78,7 @@ void UnitPerf::testPerf()
     stats->dump();
 }
 
-void UnitPerf::dumpCPUTimeToCSV(long cpuTime)
-{
-    std::ofstream file("CPUUsage.csv", std::ios::out | std::ios::app);
 
-    if(file.tellp() == 0)
-    {
-        file << "CPU Time";
-        file << "\n";
-    }
-
-    file << cpuTime;
-    file << "\n";
-}
 
 UnitPerf::UnitPerf() : UnitWSD("UnitPerf")
 {
@@ -102,14 +94,27 @@ void UnitPerf::invokeWSDTest()
     std::cerr << "startup: " << _timer->elapsedTime().count() << "us\n";
     _timer->restart();
 
-    testPerf();
+    testPerf("writer", "odt", "/../traces/perf-writer.txt");
+
+    testPerf("calc", "ods", "/../traces/perf-calc.txt");
 
     long cpuTime = _timer->elapsedTime().count();
-    dumpCPUTimeToCSV(cpuTime);
 
     std::cerr << "test: " << cpuTime << "us\n";
 
     exitTest(TestResult::Ok);
+}
+
+//Called when document loading process starts e.g. setup finishes
+void UnitPerf::onDocumentLoading()
+{
+    stats->perfSetupLoadEditPhases("Setup");
+}
+
+//called when document has been loaded into core
+void UnitPerf::onDocumentLoaded()
+{
+    stats->perfSetupLoadEditPhases("Load");
 }
 
 UnitBase* unit_create_wsd(void) { return new UnitPerf(); }

tools/Replay.hpp
@@ -29,6 +29,19 @@
 
 #include <iostream>
 #include <fstream>
+#include <config_version.h>
+#include <ctime>
+#include <Util.hpp>
+
+
+
+struct PerfMetricInfo
+{
+    std::string phase;
+    std::string metric;
+    size_t data;
+};
+
 
 
 // store buckets of latency
@@ -86,16 +99,8 @@ struct Histogram {
         }
     }
 
-    void dumpLatencyToCSV(std::string fileName)
+    std::vector<PerfMetricInfo> GetLatencyStats(std::string typeOfLatency, std::string testPhase)
     {
-        std::ofstream file(fileName + "Latency.csv", std::ios::out | std::ios::app);
-
-        if (file.tellp() == 0)
-        {
-            file << "Total Items, <10ms, <100ms, >100ms";
-            file << "\n";
-        }
-
         size_t totalTiles = _items;
         size_t subTenCount = _buckets[0];
         size_t subOneHundredCount = 0;
@@ -113,11 +118,13 @@ struct Histogram {
             }
         }
 
-        file << totalTiles << ",";
-        file << subTenCount << ",";
-        file << subOneHundredCount << ",";
-        file << overOneHundredCount << ",";
-        file << "\n";
+        std::vector<PerfMetricInfo> latencyStatsList;
+        latencyStatsList.push_back(PerfMetricInfo {testPhase,typeOfLatency + " Total tiles", totalTiles});
+        latencyStatsList.push_back(PerfMetricInfo {testPhase,typeOfLatency + " Sub_10ms", subTenCount});
+        latencyStatsList.push_back(PerfMetricInfo {testPhase,typeOfLatency + " Sub_100ms", subOneHundredCount});
+        latencyStatsList.push_back(PerfMetricInfo {testPhase,typeOfLatency + " Over_100ms", overOneHundredCount});
+
+        return latencyStatsList;
     }
 
 };
@@ -131,9 +138,11 @@ struct Stats {
         _connections(0)
     {
         _startUpMemoryUsage = GetMemoryUsage();
+        _timer.reset(new Util::SysStopwatch());
         _peakMemoryUsage = 0;
     }
     std::chrono::steady_clock::time_point _start;
+    std::unique_ptr<Util::SysStopwatch> _timer;
     size_t _bytesSent;
     size_t _bytesRecvd;
     size_t _tileCount;
@@ -144,6 +153,9 @@ struct Stats {
     size_t _peakMemoryUsage;
     size_t _startUpMemoryUsage;
 
+
+    std::string _testType;
+
     // message size breakdown
     struct MessageStat {
         size_t size;
@@ -151,6 +163,8 @@ struct Stats {
     };
     std::unordered_map<std::string, MessageStat> _recvd;
     std::unordered_map<std::string, MessageStat> _sent;
+
+    std::vector<PerfMetricInfo> perfStatsList;
 
     size_t GetMemoryUsage()
     {
@@ -161,7 +175,7 @@ struct Stats {
 
         while (std::getline(smapsFile, line))
         {
-            if (line.find("Pss_Dirty:") == 0)
+            if (line.find("Private_Dirty:") == 0)
             {
                 std::stringstream ss(line);
                 std::string key;
@@ -242,12 +256,9 @@ struct Stats {
             " server sent " << Util::getHumanizedBytes(_bytesRecvd) <<
             " (" << recvKbps << " kB/s) to " << _connections << " connections.\n";
 
-        _pingLatency.dumpLatencyToCSV("Ping");
-        _tileLatency.dumpLatencyToCSV("Tile");
 
-        dumpStressToCSV(runMs);
-        dumpNetworkStatsToCSV(_bytesRecvd,_bytesSent);
-        dumpPeakMemoryUsageToCPU(_peakMemoryUsage);
+       perfSetupLoadEditPhases("Edit");
+       dumpPerfStatsToCSV(perfStatsList);
 
         std::cout << "we sent:\n";
         dumpMap(_sent);
@@ -256,46 +267,104 @@ struct Stats {
         dumpMap(_recvd);
     }
 
-    void dumpStressToCSV(size_t runMs)
+
+    //loading phase will record the set up times and loaded phase records the document loading times
+    void perfSetupLoadEditPhases(std::string phase)
     {
-        std::ofstream file("CPU.csv", std::ios::out | std::ios::app);
+        const auto now = std::chrono::steady_clock::now();
+        const size_t runMs = std::chrono::duration_cast<std::chrono::milliseconds>(now - _start).count();
+        _start = std::chrono::steady_clock::now();
 
-        if (file.tellp() == 0)
+        size_t cpuTime = _timer->elapsedTime().count();
+        _timer.reset(new Util::SysStopwatch);
+
+        perfStatsList.push_back(GetStressStats(runMs, phase));
+        perfStatsList.push_back(GetCPUUSageStats(cpuTime, phase));
+
+        if(phase == "Edit")
         {
-            file << "Stress (m/s)";
-            file << "\n";
-        }
+            std::vector<PerfMetricInfo> statsList;
 
-        file << runMs << "\n";
+            perfStatsList.push_back(GetPeakMemoryUsageStats(_peakMemoryUsage, phase));
+
+            statsList = GetNetworkStats(_bytesRecvd / 1000, _bytesSent / 1000, phase);
+            for(size_t i = 0; i < statsList.size(); i++)
+            {
+                perfStatsList.push_back(statsList[i]);
+            }
+
+            statsList = _pingLatency.GetLatencyStats("PL", phase);
+            for(size_t i = 0; i < statsList.size(); i++)
+            {
+                perfStatsList.push_back(statsList[i]);
+            }
+
+            statsList = _tileLatency.GetLatencyStats("TL", phase);
+            for(size_t i = 0; i < statsList.size(); i++)
+            {
+                perfStatsList.push_back(statsList[i]);
+            }
+        }
     }
 
-    void dumpNetworkStatsToCSV(size_t recievedKb, size_t sentKb)
+    PerfMetricInfo GetStressStats(size_t runMs, std::string testPhase) //need to figure out what phase it is
     {
-        std::ofstream file("Network.csv", std::ios::out | std::ios::app);
+        PerfMetricInfo stressStats = {testPhase, "Stress run (ms)", runMs};
+        return  stressStats;
+    }
+
+    PerfMetricInfo GetCPUUSageStats(size_t cpuUsage, std::string testPhase)
+    {
+        PerfMetricInfo cpuStats = {testPhase, "CPU Usage (us)", cpuUsage};
+        return cpuStats;
+    }
+
+    std::vector<PerfMetricInfo> GetNetworkStats(size_t recievedKb, size_t sentKb, std::string testPhase)
+    {
+        std::vector<PerfMetricInfo> networkStatsList;
+
+        networkStatsList.push_back(PerfMetricInfo{testPhase, "Bytes recieved (kB)",  recievedKb});
+        networkStatsList.push_back(PerfMetricInfo{testPhase,"Bytes sent (kB)", sentKb});
+
+        return networkStatsList;
+    }
+
+    PerfMetricInfo GetPeakMemoryUsageStats(size_t peakMemory, std::string testPhase)
+    {
+        PerfMetricInfo peakMemoryStats = {testPhase,"Peak memory usage (kB)", peakMemory};
+        return peakMemoryStats;
+    }
+
+    void dumpPerfStatsToCSV(std::vector<PerfMetricInfo> perfData)
+    {
+        std::ofstream file("PerformanceMetricsSummary.csv", std::ios::out | std::ios::app);
 
         if(file.tellp() == 0)
         {
-            file << "Bytes recieved (kB)" << ", Bytes sent(kB)";
+            file << "Commit Hash" << "," << "Date" << "," << "Test" << "," << "Phase" << "," << "Metric" << "," << "Value";
             file << "\n";
         }
 
-        file << recievedKb << "," << sentKb << ",";
-        file << "\n";
-    }
+        std::string commitHash = COOLWSD_VERSION_HASH;
 
-    void dumpPeakMemoryUsageToCPU(size_t peakMemory)
-    {
-        std::ofstream file("PeakMemoryUsage.csv", std::ios::out | std::ios::app);
+        time_t now = time(0);
+        struct tm datetime = *localtime(&now);
 
-        if(file.tellp() == 0)
+        char formattedDate[50];
+        strftime(formattedDate, 50, "%d/%m/%y", &datetime);
+
+        for(size_t i = 0; i < perfData.size(); i++)
         {
-            file << "Peak memory usage per document (kB)";
+            file << commitHash << "," << formattedDate << "," << _testType << "," << perfData[i].phase << "," << perfData[i].metric << "," << perfData[i].data;
             file << "\n";
         }
-
-        file << peakMemory;
-        file << "\n";
     }
+
+    void setTypeOfTest(std::string testType)
+    {
+        _testType = testType;
+    }
+
 };
 
 // Avoid a MessageHandler for now.
@@ -461,10 +530,14 @@ public:
             std::cerr << _logPre << "msg " << out << "\n";
         }
 
-        size_t postDocumentLoadingMemory = _stats->GetMemoryUsage() - _stats->_startUpMemoryUsage;
-        if((postDocumentLoadingMemory > _stats->_peakMemoryUsage))
+        size_t currentMemoryUsage = _stats->GetMemoryUsage();
+        if (currentMemoryUsage > _stats->_startUpMemoryUsage)
         {
-            _stats->_peakMemoryUsage = postDocumentLoadingMemory;
+            size_t postDocumentLoadingMemory = currentMemoryUsage - _stats->_startUpMemoryUsage;
+            if (postDocumentLoadingMemory > _stats->_peakMemoryUsage)
+            {
+                _stats->_peakMemoryUsage = postDocumentLoadingMemory;
+            }
         }
         // FIXME: translate mouse events relative to view-port etc.
         return out;

commit c61d5cf70f000f295373499e3a102f22d91ef865

Fix build in UnitPerf
./common/Unit.hpp:488:18: error: 'UnitWSD::onDocumentLoaded' hides overloaded virtual function [-Werror,-Woverloaded-virtual]

caused by commit 6e790c63d0997094979b2a237f428ef7706e62ef
feat: Changes to UnitPerf measuring

Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
Change-Id: Ib545b83e9d25cad08a6d2c92a7a1b19965621155

common/Unit.hpp
@@ -482,10 +482,10 @@ public:
 
 
     // Called when a document has started loading
-    virtual void onDocumentLoading(){}
+    virtual void onPerfDocumentLoading(){}
 
     // Called when a document has finished loading
-    virtual void onDocumentLoaded(){}
+    virtual void onPerfDocumentLoaded(){}
 
     /// To force the save operation being handled as auto-save from a unit test.
     virtual bool isAutosave()

wsd/DocumentBroker.cpp
@@ -893,7 +893,7 @@ bool DocumentBroker::download(
 
         if(_unitWsd != nullptr)
         {
-            _unitWsd->onDocumentLoading();
+            _unitWsd->onPerfDocumentLoading();
         }
 
         // Pass the public URI to storage as it needs to load using the token
@@ -2447,7 +2447,7 @@ void DocumentBroker::setLoaded()
 
         if(_unitWsd != nullptr)
         {
-            _unitWsd->onDocumentLoaded();
+            _unitWsd->onPerfDocumentLoaded();
         }
 
     }

test/UnitPerf.cpp
@@ -46,8 +46,8 @@ class UnitPerf : public UnitWSD
 public:
     UnitPerf();
     void invokeWSDTest() override;
-    void onDocumentLoading() override;
-    void onDocumentLoaded() override;
+    void onPerfDocumentLoading() override;
+    void onPerfDocumentLoaded() override;
     std::unique_ptr<Util::SysStopwatch> _timer;
     std::shared_ptr<Stats> stats;
 };
@@ -110,13 +110,13 @@ void UnitPerf::invokeWSDTest()
 }
 
 //Called when document loading process starts e.g. setup finishes
-void UnitPerf::onDocumentLoading()
+void UnitPerf::onPerfDocumentLoading()
 {
     stats->endPhase(Log::Phase::Setup);
 }
 
 //called when document has been loaded into core
-void UnitPerf::onDocumentLoaded()
+void UnitPerf::onPerfDocumentLoaded()
 {
     stats->endPhase(Log::Phase::Load);
 }