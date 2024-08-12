d3ae0648ad9624d85a6973ae027bdf53dbfea254
Caolán McNamara <caolan.mcnamara@collabora.com>
2024-08-12 20:13:01 +0100

    Invalid syntax for tileprocessed wireid 'NaN'
    this is similar to:
    
    commit 01165fbe1f074e530c0a6cb057fbc9b3b177971f
    AuthorDate: Tue Jul 4 15:33:15 2023 +0100
    
        On a cache hit for a tile the result typically had a wireId of 0
    
    this one can typically be reproduced by opening a presentation and
    can be seen when the slide preview tiles are requested
    
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: If5f7d0717a4068332aa96052a8a285171a13c03d

--- wsd/DocumentBroker.cpp
+++ wsd/DocumentBroker.cpp
@@ -3474,6 +3474,9 @@ void DocumentBroker::handleTileRequest(const StringVector &tokens, bool forceKey
     Tile cachedTile = _tileCache->lookupTile(tile);
     if (cachedTile && cachedTile->isValid())
     {
+        if (tile.getWireId() == 0)
+            tile.setWireId(cachedTile->_wids.back());
+
         session->sendTileNow(tile, cachedTile);
         return;
     }
