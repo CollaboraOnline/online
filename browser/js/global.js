CollaboraOnline/online

commit e36d6ffb9b13ffd440744425da128a9446530f01

fix: fix UI preference inconsistencies
Add helper functions for fetching preferences from localstorage and
ui_defaults. By having these in unified helpers, we can be certain that
we're not making differences in the priority or use of different stores
which can cause UI inconsistency.

BREAKING-CHANGE: This changes the naming of many of the preferences in localStorage, effectively resetting lots of UI preferences

Signed-off-by: Skyler Grey <skyler.grey@collabora.com>
Change-Id: I4cc9cc885aa8cd0b40646629cd73bef236963a94
Fixes: CollaboraOnline/online#9166

browser/js/global.js
@@ -281,6 +281,113 @@ window.app = {
 		lang: navigatorLang
 	};
 
+	global.prefs = {
+		_localStorageChanges: {}, // TODO: change this to new Map() when JS version allows
+		canPersist: (function() {
+			var str = 'localstorage_test';
+			try {
+				global.localStorage.setItem(str, str);
+				global.localStorage.removeItem(str);
+				return true;
+			} catch (e) {
+				return false;
+			}
+		})(),
+
+		/// Similar to using window.uiDefaults directly, but this can handle dotted keys like "presentation.ShowSidebar" and does not allow partially referencing a value (like just "presentation")
+		_getUIDefault: function(key, defaultValue = undefined) {
+			const parts = key.split('.');
+			let result = global.uiDefaults;
+
+			for (const part of parts) {
+				if (!Object.prototype.hasOwnProperty.call(result, part)) {
+					return defaultValue;
+				}
+
+				if (typeof result === 'string') {
+					return defaultValue;
+				}
+
+				result = result[part];
+			}
+
+			if (typeof result !== 'string') {
+				return defaultValue;
+			}
+
+			return result;
+		},
+
+		get: function(key, defaultValue = undefined) {
+			if (key in global.prefs._localStorageChanges) {
+				return global.prefs._localStorageChanges[key];
+			}
+
+			const uiDefault = global.prefs._getUIDefault(key);
+			if (
+				global.prefs._getUIDefault('SavedUIState', 'true').toLowerCase() === 'false' &&
+				uiDefault !== undefined
+			) {
+				return uiDefault;
+			}
+
+			if (global.prefs.canPersist) {
+				const localStorageItem = global.localStorage.getItem(key);
+
+				if (localStorageItem) {
+					return localStorageItem;
+				}
+			}
+
+			if (uiDefault !== undefined) {
+				return uiDefault;
+			}
+
+			return defaultValue;
+		},
+
+		set: function(key, value) {
+			value = String(value); // NOT "new String(...)". We cannot use .toString here because value could be null/undefined
+			if (global.prefs.canPersist) {
+				global.localStorage.setItem(key, value);
+			}
+			global.prefs._localStorageChanges[key] = value;
+		},
+
+		remove: function(key) {
+			if (global.prefs.canPersist) {
+				global.localStorage.removeItem(key);
+			}
+			global.prefs._localStorageChanges[key] = undefined;
+		},
+
+		getBoolean: function(key, defaultValue = false) {
+			const value = global.prefs.get(key, '').toLowerCase();
+
+			if (value === 'false') {
+				return false;
+			}
+
+			if (value === 'true') {
+				return true;
+			}
+
+			return defaultValue;
+		},
+
+		getNumber: function(key, defaultValue = NaN) {
+			const value = global.prefs.get(key, '').toLowerCase();
+
+			const parsedValue = parseFloat(value);
+
+			if (isNaN(parsedValue)) {
+				return defaultValue;
+			}
+
+			return parsedValue;
+		},
+	};
+
 	global.keyboard = {
 		onscreenKeyboardHint: global.uiDefaults['onscreenKeyboardHint'],
 		// If there's an onscreen keyboard, we don't want to trigger it with innocuous actions like panning around a spreadsheet
@@ -459,18 +566,7 @@ window.app = {
 		}
 	};
 
-	global.isLocalStorageAllowed = (function() {
-		var str = 'localstorage_test';
-		try {
-			global.localStorage.setItem(str, str);
-			global.localStorage.removeItem(str);
-			return true;
-		} catch (e) {
-			return false;
-		}
-	})();
-
-	if (global.isLocalStorageAllowed && global.localStorage.getItem('hasNavigatorClipboardWrite') === 'false') {
+	if (!global.prefs.getBoolean('hasNavigatorClipboardWrite', true)) {
 		// navigator.clipboard.write failed on us once, don't even try it.
 		global.L.Browser.hasNavigatorClipboardWrite = false;
 	}
@@ -1210,7 +1306,7 @@ window.app = {
 				if (L.Browser.cypressTest && isCalcTest)
 					global.enableAccessibility = false;
 
-				var accessibilityState = global.localStorage.getItem('accessibilityState') === 'true';
+				var accessibilityState = global.prefs.getBoolean('accessibilityState', false);
 				accessibilityState = accessibilityState || (L.Browser.cypressTest && !isCalcTest);
 				msg += ' accessibilityState=' + accessibilityState;
 
@@ -1230,25 +1326,16 @@ window.app = {
 				if (global.deviceFormFactor) {
 					msg += ' deviceFormFactor=' + global.deviceFormFactor;
 				}
-				if (global.isLocalStorageAllowed) {
-					var spellOnline = global.localStorage.getItem('SpellOnline');
-					if (spellOnline) {
-						msg += ' spellOnline=' + spellOnline;
-					}
-					var docTypes = ['text', 'spreadsheet', 'presentation', 'drawing'];
-					for (var i = 0; i < docTypes.length; ++i) {
-						var docType = docTypes[i];
-						var darkTheme = false;
-						if (window.uiDefaults) {
-							darkTheme = window.uiDefaults.darkTheme === true;
-						}
-						var item = global.localStorage.getItem('UIDefaults_' + docType + '_darkTheme');
-						if (item) {
-							darkTheme = item;
-						}
-						if (darkTheme) {
-							msg += ' ' + docType + 'DarkTheme=' + darkTheme;
-						}
+				var spellOnline = window.prefs.get('SpellOnline');
+				if (spellOnline) {
+					msg += ' spellOnline=' + spellOnline;
+				}
+				var docTypes = ['text', 'spreadsheet', 'presentation', 'drawing'];
+				const darkTheme = window.prefs.getBoolean('darkTheme');
+				for (var i = 0; i < docTypes.length; ++i) {
+					var docType = docTypes[i];
+					if (darkTheme) {
+						msg += ' ' + docType + 'DarkTheme=' + darkTheme;
 					}
 				}
 

browser/src/control/Control.UIManager.js
@@ -76,7 +76,7 @@ L.Control.UIManager = L.Control.extend({
 	},
 
 	shouldUseNotebookbarMode: function() {
-		let forceCompact = this.getSavedStateOrDefault('compactMode', null);
+		let forceCompact = window.prefs.getBoolean('compactMode', null);
 		// all other cases should default to notebookbar
 		let shouldUseClassic = (window.userInterfaceMode === 'compact' && forceCompact == null) || forceCompact === true;
 		return !shouldUseClassic;
@@ -96,10 +96,6 @@ L.Control.UIManager = L.Control.extend({
 		this.map.fire('darkmodechanged');
 	},
 
-	getDarkModeState: function() {
-		return this.getSavedStateOrDefault('darkTheme', window.uiDefaults['darkTheme'] ?  window.uiDefaults['darkTheme'] : false);
-	},
-
 	setCanvasColorAfterModeChange: function() {
 		if (app.sectionContainer) {
 			app.sectionContainer.setBackgroundColorMode(false);
@@ -115,15 +111,15 @@ L.Control.UIManager = L.Control.extend({
 
 	toggleDarkMode: function() {
 		// get the initial mode
-		var selectedMode = this.getDarkModeState();
+		var inDarkTheme = window.prefs.getBoolean('darkTheme');
 		// swap them by invoking the appropriate load function and saving the state
-		if (selectedMode) {
-			this.setSavedState('darkTheme',false);
+		if (inDarkTheme) {
+			window.prefs.set('darkTheme', false);
 			this.loadLightMode();
 			this.activateDarkModeInCore(false);
 		}
 		else {
-			this.setSavedState('darkTheme',true);
+			window.prefs.set('darkTheme', true);
 			this.loadDarkMode();
 			this.activateDarkModeInCore(true);
 		}
@@ -134,17 +130,11 @@ L.Control.UIManager = L.Control.extend({
 	},
 
 	initDarkModeFromSettings: function() {
-		var selectedMode = this.getDarkModeState();
+		var inDarkTheme = window.prefs.getBoolean('darkTheme');
 
-		if (window.ThisIsTheAndroidApp) {
-			selectedMode = window.uiDefaults['darkTheme'] ?  window.uiDefaults['darkTheme'] : false;
-			this.setSavedState('darkTheme', selectedMode);
-		}
-
-		if (selectedMode) {
+		if (inDarkTheme) {
 			this.loadDarkMode();
-		}
-		else {
+		} else {
 			this.loadLightMode();
 		}
 	},
@@ -176,23 +166,18 @@ L.Control.UIManager = L.Control.extend({
 		// Wait for Coolwsd to initiate the switch.
 	},
 
-	getAccessibilityState: function() {
-		return window.isLocalStorageAllowed && window.localStorage.getItem('accessibilityState') === 'true';
-	},
-
 	toggleAccessibilityState: function() {
-		var savedA11yState = this.getAccessibilityState();
-		if (window.isLocalStorageAllowed)
-			window.localStorage.setItem('accessibilityState', !savedA11yState ? 'true' : 'false');
+		var prevAccessibilityState = window.prefs.getBoolean('accessibilityState');
+		window.prefs.set('accessibilityState', !prevAccessibilityState);
 		this.map.fire('a11ystatechanged');
-		this.map.setAccessibilityState(!savedA11yState);
+		this.map.setAccessibilityState(!prevAccessibilityState);
 	},
 
 	initializeBasicUI: function() {
 		var enableNotebookbar = this.shouldUseNotebookbarMode();
 		var that = this;
 
-		this.map._accessibilityState = this.getAccessibilityState();
+		this.map._accessibilityState = window.prefs.getBoolean('accessibilityState');
 
 		if (window.mode.isMobile() || !enableNotebookbar) {
 			var menubar = L.control.menubar();
@@ -306,7 +291,7 @@ L.Control.UIManager = L.Control.extend({
 			// makeSpaceForNotebookbar call in onUpdatePermission
 		}
 
-		if (window.uiDefaults[docType] && window.uiDefaults[docType]['ShowToolbar'] === false) {
+		if (!window.prefs.getBoolean(`${docType}.ShowToolbar`, true)) {
 			this.collapseNotebookbar();
 		}
 
@@ -335,7 +320,7 @@ L.Control.UIManager = L.Control.extend({
 			document.getElementById('selectbackground').parentNode.removeChild(document.getElementById('selectbackground'));
 
 			if ((window.mode.isTablet() || window.mode.isDesktop()) && !app.isReadOnly()) {
-				var showRuler = this.getSavedStateOrDefault('ShowRuler');
+				var showRuler = this.getBooleanDocTypePref('ShowRuler', true);
 				var interactiveRuler = this.map.isEditMode();
 				var isRTL = document.documentElement.dir === 'rtl';
 				L.control.ruler({position: (isRTL ? 'topright' : 'topleft'), interactive:interactiveRuler, showruler: showRuler}).addTo(this.map);
@@ -343,7 +328,7 @@ L.Control.UIManager = L.Control.extend({
 				this.map.fire('rulerchanged');
 			}
 
-			var showResolved = this.getSavedStateOrDefault('ShowResolved');
+			var showResolved = this.getBooleanDocTypePref('ShowResolved', true);
 			if (showResolved === false || showResolved === 'false')
 				this.map.sendUnoCommand('.uno:ShowResolvedAnnotations');
 		}
@@ -367,31 +352,31 @@ L.Control.UIManager = L.Control.extend({
 	initializeSidebar: function() {
 		// Hide the sidebar on start if saved state or UIDefault is set.
 		if (window.mode.isDesktop() && !window.ThisIsAMobileApp) {
-			var showSidebar = this.getSavedStateOrDefault('ShowSidebar');
+			var showSidebar = this.getBooleanDocTypePref('ShowSidebar', true);
 
-			if (this.getSavedStateOrDefault('PropertyDeck')) {
+			if (this.getBooleanDocTypePref('PropertyDeck', true)) {
 				app.socket.sendMessage('uno .uno:SidebarShow');
 			}
 
 			if (this.map.getDocType() === 'presentation') {
-				if (this.getSavedStateOrDefault('SdSlideTransitionDeck', false)) {
+				if (this.getBooleanDocTypePref('SdSlideTransitionDeck', false)) {
 					app.socket.sendMessage('uno .uno:SidebarShow');
 					app.socket.sendMessage('uno .uno:SlideChangeWindow');
 					this.map.sidebar.setupTargetDeck('.uno:SlideChangeWindow');
-				} else if (this.getSavedStateOrDefault('SdCustomAnimationDeck', false)) {
+				} else if (this.getBooleanDocTypePref('SdCustomAnimationDeck', false)) {
 					app.socket.sendMessage('uno .uno:SidebarShow');
 					app.socket.sendMessage('uno .uno:CustomAnimation');
 					this.map.sidebar.setupTargetDeck('.uno:CustomAnimation');
-				} else if (this.getSavedStateOrDefault('SdMasterPagesDeck', false)) {
+				} else if (this.getBooleanDocTypePref('SdMasterPagesDeck', false)) {
 					app.socket.sendMessage('uno .uno:SidebarShow');
 					app.socket.sendMessage('uno .uno:MasterSlidesPanel');
 					this.map.sidebar.setupTargetDeck('.uno:MasterSlidesPanel');
-				} else if (this.getSavedStateOrDefault('NavigatorDeck', false)) {
+				} else if (this.getBooleanDocTypePref('NavigatorDeck', false)) {
 					app.socket.sendMessage('uno .uno:SidebarShow');
 					app.socket.sendMessage('uno .uno:Navigator');
 					this.map.sidebar.setupTargetDeck('.uno:Navigator');
 				}
-			} else if (this.getSavedStateOrDefault('NavigatorDeck', false)) {
+			} else if (this.getBooleanDocTypePref('NavigatorDeck', false)) {
 				app.socket.sendMessage('uno .uno:SidebarShow');
 				app.socket.sendMessage('uno .uno:Navigator');
 				this.map.sidebar.setupTargetDeck('.uno:Navigator');
@@ -496,8 +481,8 @@ L.Control.UIManager = L.Control.extend({
 		ms = ms !== undefined ? ms : 400;
 		setTimeout(function () {
 			var message = 'dialogevent ' +
-			    (window.sidebarId !== undefined ? window.sidebarId : -1) +
-			    ' {"id":"-1"}';
+				(window.sidebarId !== undefined ? window.sidebarId : -1) +
+				' {"id":"-1"}';
 			app.socket.sendMessage(message);
 		}, ms);
 
@@ -560,7 +545,7 @@ L.Control.UIManager = L.Control.extend({
 			break;
 		}
 
-		this.setSavedState('compactMode', uiMode.mode === 'classic');
+		this.setDocTypePref('compactMode', uiMode.mode === 'classic');
 		this.initializeSidebar();
 		this.insertCustomButtons();
 
@@ -808,14 +793,14 @@ L.Control.UIManager = L.Control.extend({
 	showRuler: function() {
 		$('.cool-ruler').show();
 		$('#map').addClass('hasruler');
-		this.setSavedState('ShowRuler', true);
+		this.setDocTypePref('ShowRuler', true);
 		this.map.fire('rulerchanged');
 	},
 
 	hideRuler: function() {
 		$('.cool-ruler').hide();
 		$('#map').removeClass('hasruler');
-		this.setSavedState('ShowRuler', false);
+		this.setDocTypePref('ShowRuler', false);
 		this.map.fire('rulerchanged');
 	},
 
@@ -890,7 +875,7 @@ L.Control.UIManager = L.Control.extend({
 	showStatusBar: function() {
 		$('#document-container').css('bottom', this.documentBottom);
 		this.map.statusBar.show();
-		this.setSavedState('ShowStatusbar', true);
+		this.setDocTypePref('ShowStatusbar', true);
 		this.map.fire('statusbarchanged');
 	},
 
@@ -902,7 +887,7 @@ L.Control.UIManager = L.Control.extend({
 		$('#document-container').css('bottom', '0px');
 		this.map.statusBar.hide();
 		if (!firstStart)
-			this.setSavedState('ShowStatusbar', false);
+			this.setDocTypePref('ShowStatusbar', false);
 		this.map.fire('statusbarchanged');
 	},
 
@@ -1078,7 +1063,7 @@ L.Control.UIManager = L.Control.extend({
 				tooltipClass: 'functiontooltip',
 				content: tooltipInfo,
 				items: elem[0],
-                position: { my: 'left top', at: 'left+' + pt.x +  ' top+' +pt.y, collision: 'fit fit' }
+				position: { my: 'left top', at: 'left+' + pt.x +  ' top+' +pt.y, collision: 'fit fit' }
 			});
 			elem.tooltip('option', 'customClass', 'functiontooltip');
 			elem.tooltip('open');
@@ -1616,41 +1601,14 @@ L.Control.UIManager = L.Control.extend({
 		}
 	},
 
-	setSavedState: function(name, state) {
-		var docType = (name === 'compactMode') ? null : this.map.getDocType();
-		if (window.isLocalStorageAllowed)
-			localStorage.setItem('UIDefaults_' + docType + '_' + name, state);
+	setDocTypePref: function(name, value) {
+		const docType = this.map.getDocType();
+		return window.prefs.set(`${docType}.${name}`, value);
 	},
 
-	getSavedStateOrDefault: function(name, forcedDefault) {
-		var retval = forcedDefault !== undefined ? forcedDefault : true;
-		// we request compactMode very early, no info about doctype so unify all the calls
-		var docType = (name === 'compactMode') ? null : this.map.getDocType();
-		var state = null;
-		if (window.savedUIState && window.isLocalStorageAllowed)
-			state = localStorage.getItem('UIDefaults_' + docType + '_' + name);
-		switch (state) {
-		case 'true':
-			return true;
-		case 'false':
-			return false;
-		default:
-			// no saved state; must check the UIDefaults
-			if (window.uiDefaults && window.uiDefaults[docType])
-				retval = window.uiDefaults[docType][name];
-
-			// check UIDefaults root without limiting to the doctype
-			if (retval === undefined || retval === null)
-				retval = window.uiDefaults[name];
-
-			if (retval === undefined || retval === null) {
-				if (forcedDefault !== undefined)
-					return forcedDefault;
-				else
-					return true;
-			} else
-				return retval;
-		}
+	getBooleanDocTypePref: function(name, defaultValue = false) {
+		const docType = this.map.getDocType();
+		return window.prefs.getBoolean(`${docType}.${name}`, defaultValue);
 	},
 
 	enableTooltip: function(element) {

browser/src/control/Toolbar.js
@@ -398,8 +398,7 @@ L.Map.include({
 			if (val && (json === undefined || json === null)) {
 				 // because it is toggle, state has to be the opposite
 				var state = !(val === 'true');
-				if (window.isLocalStorageAllowed)
-					window.localStorage.setItem('SpellOnline', state);
+				window.prefs.set('SpellOnline', state);
 			}
 		}
 

browser/src/core/Socket.js
@@ -242,31 +242,22 @@ app.definitions.Socket = L.Class.extend({
 			};
 			msg += ' options=' + JSON.stringify(options);
 		}
-		if (window.isLocalStorageAllowed) {
-			var spellOnline = window.localStorage.getItem('SpellOnline');
-			if (spellOnline) {
-				msg += ' spellOnline=' + spellOnline;
-			}
-			var docTypes = ['text', 'spreadsheet', 'presentation', 'drawing'];
-			for (var i = 0; i < docTypes.length; ++i) {
-				var docType = docTypes[i];
-				var darkTheme = false;
-				if (window.uiDefaults) {
-					darkTheme = window.uiDefaults.darkTheme === true;
-				}
-				var item = window.localStorage.getItem('UIDefaults_' + docType + '_darkTheme');
-				if (item) {
-					darkTheme = item;
-				}
-				if (darkTheme) {
-					msg += ' ' + docType + 'DarkTheme=' + darkTheme;
-				}
-			}
-
-			var accessibilityState = window.localStorage.getItem('accessibilityState') === 'true';
-			accessibilityState = accessibilityState || L.Browser.cypressTest;
-			msg += ' accessibilityState=' + accessibilityState;
+		var spellOnline = window.prefs.get('SpellOnline');
+		if (spellOnline) {
+			msg += ' spellOnline=' + spellOnline;
 		}
+		var docTypes = ['text', 'spreadsheet', 'presentation', 'drawing'];
+		const darkTheme = window.prefs.getBoolean('darkTheme');
+		for (var i = 0; i < docTypes.length; ++i) {
+			var docType = docTypes[i];
+			if (darkTheme) {
+				msg += ' ' + docType + 'DarkTheme=' + darkTheme;
+			}
+		}
+
+		var accessibilityState = window.prefs.getBoolean('accessibilityState');
+		accessibilityState = accessibilityState || L.Browser.cypressTest;
+		msg += ' accessibilityState=' + accessibilityState;
 
 		this._doSend(msg);
 		for (var i = 0; i < this._msgQueue.length; i++) {
@@ -513,7 +504,7 @@ app.definitions.Socket = L.Class.extend({
 		if (data[0] != 0x89)
 			prefix = String.fromCharCode(0x89);
 		img = 'data:image/png;base64,' + window.btoa(this._strFromUint8(prefix,data));
-		if (L.Browser.cypressTest && localStorage.getItem('image_validation_test')) {
+		if (L.Browser.cypressTest && window.prefs.getBoolean('image_validation_test')) {
 			if (!window.imgDatas)
 				window.imgDatas = [];
 			window.imgDatas.push(img);
@@ -1493,7 +1484,7 @@ app.definitions.Socket = L.Class.extend({
 			this._map._docLayer._refreshTilesInBackground();
 			this._map.fire('statusindicator', { statusType: 'reconnected' });
 
-			var selectedMode = this._map.uiManager.getDarkModeState();
+			var selectedMode = window.prefs.getBoolean('darkTheme');
 			this._map.uiManager.activateDarkModeInCore(selectedMode);
 
 			var uiMode = this._map.uiManager.getCurrentMode();

browser/src/map/Map.js
@@ -127,9 +127,9 @@ L.Map = L.Evented.extend({
 
 		this._accessibilityState = false;
 
-		if (L.Browser.cypressTest && window.enableAccessibility && window.isLocalStorageAllowed) {
+		if (L.Browser.cypressTest && window.enableAccessibility) {
 			this._accessibilityState = true;
-			window.localStorage.setItem('accessibilityState', 'true');
+			window.prefs.set('accessibilityState', true);
 		}
 
 		this.callInitHooks();

browser/src/canvas/sections/CellSelectionHandleSection.ts
@@ -54,7 +54,7 @@ class CellSelectionHandle extends app.definitions.canvasSectionObject {
 	}
 
 	public onDraw() {
-		this.context.strokeStyle = app.map.uiManager.getDarkModeState() ? 'white' : 'black';
+		this.context.strokeStyle = (<any>window).prefs.getBoolean('darkTheme') ? 'white' : 'black';
 		this.context.lineWidth = 2;
 
 		this.context.beginPath();

browser/src/canvas/sections/CommentListSection.ts
@@ -36,7 +36,7 @@ L.Map.include({
 		var unoCommand = '.uno:ShowResolvedAnnotations';
 		this.sendUnoCommand(unoCommand);
 		app.sectionContainer.getSectionWithName(L.CSections.CommentList.name).setViewResolved(on);
-		this.uiManager.setSavedState('ShowResolved', on ? true : false);
+		this.uiManager.setDocTypePref('ShowResolved', on ? true : false);
 	}
 });
 

browser/src/canvas/sections/ContentControlSection.ts
@@ -365,7 +365,7 @@ export class ContentControlSection extends app.definitions.canvasSectionObject {
 	}
 
 	private changeBorderStyle(): void {
-		var borderColor = this.map.uiManager.getDarkModeState() ? 'white' : 'black';
+		var borderColor = (<any>window).prefs.getBoolean('darkTheme') ? 'white' : 'black';
 		this.sectionProperties.frame.color = borderColor;
 	}
 }

commit 596a56b5cf4488592c819fedf257d01e886443f3

cypress: fix errors due to missing a11y handling
regression from commit e36d6ffb9b13ffd440744425da128a9446530f01
fix: fix UI preference inconsistencies

where we introduced second place where accessibility state is
controlled. This caused that we were out of sync in few places
so we used regular TextInput while having a11y enabled...

We got bunch of this._map._textInput.onAccessibilityFocusChanged is not a function

This was visible only after we added additional dialogs/errors
on unhandled exceptions inside cypress recently.

Let's share a code for cypress exceptions in global.js
and put state in docstate.js

Also send state requested by uset on load

Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
Change-Id: I484d6ee757ffb37bcf9d91b0a03fd35ed2960fc4

browser/js/global.js
@@ -691,6 +691,27 @@ function getInitializerClass() {
 		},
 	};
 
+	global.getAccessibilityState = function () {
+		var isCalcTest =
+			global.docURL.includes('data/desktop/calc/') ||
+			global.docURL.includes('data/mobile/calc/') ||
+			global.docURL.includes('data/idle/calc/') ||
+			global.docURL.includes('data/multiuser/calc/');
+
+		// FIXME: a11y doesn't work in calc under cypress
+		if (L.Browser.cypressTest && isCalcTest)
+			global.enableAccessibility = false;
+
+		if (L.Browser.cypressTest) {
+			global.prefs.set('accessibilityState', global.enableAccessibility);
+			global.app.accessibilityState = global.enableAccessibility;
+		}
+
+		var accessibilityState = global.prefs.getBoolean('accessibilityState');
+		global.app.accessibilityState = accessibilityState;
+		return accessibilityState;
+	};
+
 	// Renamed in 24.04.4.1
 	const prefDocTypes = ['text', 'spreadsheet', 'presentation', 'drawing'];
 	for (const docType of prefDocTypes) {
@@ -1589,17 +1610,7 @@ function getInitializerClass() {
 				var now2 = Date.now();
 				global.socket.send('coolclient ' + ProtocolVersionNumber + ' ' + ((now0 + now2) / 2) + ' ' + now1);
 
-				var isCalcTest =
-					global.docURL.includes('data/desktop/calc/') ||
-					global.docURL.includes('data/mobile/calc/') ||
-					global.docURL.includes('data/idle/calc/') ||
-					global.docURL.includes('data/multiuser/calc/');
-
-				if (L.Browser.cypressTest && isCalcTest)
-					global.enableAccessibility = false;
-
-				var accessibilityState = global.prefs.getBoolean('accessibilityState');
-				accessibilityState = accessibilityState || (L.Browser.cypressTest && !isCalcTest);
+				var accessibilityState = global.getAccessibilityState();
 				msg += ' accessibilityState=' + accessibilityState;
 
 				if (global.ThisIsAMobileApp) {

browser/src/control/Control.UIManager.js
@@ -221,7 +221,7 @@ L.Control.UIManager = L.Control.extend({
 		var enableNotebookbar = this.shouldUseNotebookbarMode();
 		var that = this;
 
-		this.map._accessibilityState = window.prefs.getBoolean('accessibilityState');
+		app.accessibilityState = window.prefs.getBoolean('accessibilityState');
 
 		if (window.mode.isMobile() || !enableNotebookbar) {
 			var menubar = L.control.menubar();

browser/src/control/Toolbar.js
@@ -972,19 +972,4 @@ L.Map.include({
 		if (this.formulabar)
 			this.formulabar.dirty = true;
 	},
-
-	setAccessibilityState: function(enable) {
-		if (this._accessibilityState === enable)
-			return;
-		this._accessibilityState = enable;
-		app.socket.sendMessage('a11ystate ' + enable);
-
-		this.removeLayer(this._textInput);
-		this._textInput = enable ? L.a11yTextInput() : L.textInput();
-		this.addLayer(this._textInput);
-		if (enable) {
-			this._textInput._requestFocusedParagraph();
-		}
-		this._textInput.showCursor();
-	},
 });

browser/src/core/Socket.js
@@ -252,17 +252,7 @@ app.definitions.Socket = L.Class.extend({
 		msg += ' darkBackground=' + darkBackground;
 		this._map.uiManager.initDarkBackgroundUI(darkBackground);
 
-		var isCalcTest =
-			window.docURL.includes('data/desktop/calc/') ||
-			window.docURL.includes('data/mobile/calc/') ||
-			window.docURL.includes('data/idle/calc/') ||
-			window.docURL.includes('data/multiuser/calc/');
-
-		if (L.Browser.cypressTest && isCalcTest)
-			window.enableAccessibility = false;
-
-		var accessibilityState = window.prefs.getBoolean('accessibilityState');
-		accessibilityState = accessibilityState || (L.Browser.cypressTest && !isCalcTest);
+		var accessibilityState = window.getAccessibilityState();
 		msg += ' accessibilityState=' + accessibilityState;
 
 		this._doSend(msg);

browser/src/docstate.js
@@ -33,6 +33,7 @@ window.app = {
 	dispatcher: null, // A Dispatcher class instance is assigned to this.
 	twipsToPixels: 0, // Twips to pixels multiplier.
 	pixelsToTwips: 0, // Pixels to twips multiplier.
+	accessibilityState: false, // If accessibility was enabled by user
 	UI: {
 		language: {
 			fromURL: window.langParam, // This is set in global.js.

browser/src/layer/tile/CanvasTileLayer.js
@@ -1553,7 +1553,7 @@ L.CanvasTileLayer = L.Layer.extend({
 			this._map.fire('versionbar', obj);
 		}
 		else if (textMsg.startsWith('a11y')) {
-			if (!app.map || !app.map._accessibilityState)
+			if (!app.accessibilityState)
 				throw 'A11y events come from the core while it is disabled in the client session.';
 
 			if (textMsg.startsWith('a11yfocuschanged:')) {

browser/src/map/Map.js
@@ -121,13 +121,6 @@ L.Map = L.Evented.extend({
 		// True only when searching within the doc, as we need to use winId==0.
 		this._isSearching = false;
 
-		this._accessibilityState = false;
-
-		if (L.Browser.cypressTest && window.enableAccessibility) {
-			this._accessibilityState = true;
-			window.prefs.set('accessibilityState', true);
-		}
-
 		this.callInitHooks();
 
 		this.addHandler('keyboard', L.Map.Keyboard);
@@ -316,13 +309,41 @@ L.Map = L.Evented.extend({
 		});
 	},
 
+	// A11y
+
 	initTextInput: function(docType) {
-		var hasAccessibilitySupport = window.enableAccessibility && this._accessibilityState;
+		var hasAccessibilitySupport = window.enableAccessibility && app.accessibilityState;
 		hasAccessibilitySupport = hasAccessibilitySupport &&
 			(docType === 'text' || docType === 'presentation'|| docType === 'spreadsheet');
-		this._textInput = hasAccessibilitySupport ? L.a11yTextInput() : L.textInput();
+
+		this.setupCoreAccessibility(hasAccessibilitySupport);
+		this.createTextInput(hasAccessibilitySupport);
+	},
+
+	createTextInput: function(enableA11y) {
+		this._textInput = enableA11y ? L.a11yTextInput() : L.textInput();
 		this.addLayer(this._textInput);
 	},
+
+	setupCoreAccessibility: function(enableA11y) {
+		app.accessibilityState = enableA11y;
+		app.socket.sendMessage('a11ystate ' + enableA11y);
+	},
+
+	setAccessibilityState: function(enable) {
+		if (app.accessibilityState === enable)
+			return;
+
+		this.setupCoreAccessibility(enable);
+		this.removeLayer(this._textInput);
+		this.createTextInput(enable);
+
+		if (enable)
+			this._textInput._requestFocusedParagraph();
+		this._textInput.showCursor();
+	},
+
+	// end of A11y
 
 	loadDocument: function(socket) {
 		app.socket.connect(socket);