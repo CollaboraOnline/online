8af5d5b2a336211c34d056b886c62914fc284156
Caolán McNamara <caolan.mcnamara@collabora.com>
2024-08-20 12:24:03 +0100

    survive if config.xml is missing indirection_endpoint.geolocation_setup.enable
    which an old config will not have and results in failure to connect as
    exception is thrown
    
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: Ic2c9f66aa180251c90424578eb6cc2cd5aaecb09

--- wsd/COOLWSD.cpp
+++ wsd/COOLWSD.cpp
@@ -2192,6 +2192,7 @@ void COOLWSD::innerInitialize(Application& self)
         { "deepl.auth_key", "" },
         { "deepl.enabled", "false" },
         { "zotero.enable", "true" },
+        { "indirection_endpoint.geolocation_setup.enable", "false" },
         { "indirection_endpoint.url", "" },
 #if !MOBILEAPP
         { "help_url", HELP_URL },
