b3a65b014c1b8f4fbb867e8e757873bdfc5248f5
Szymon Kłos <szymon.klos@collabora.com>
2024-08-23 12:45:38 +0200

    cypress: don't center followed cursor on already visible area
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I53b02d733a88e19446f21de985359b69a87d6eb6

---
+++ cypress_test/integration_tests/multiuser/writer/cursor_spec.js
@@ -0,0 +1,40 @@
+/* global describe it cy beforeEach require */
+
+var helper = require('../../common/helper');
+var desktopHelper = require('../../common/desktop_helper');
+var searchHelper = require('../../common/search_helper.js');
+
+describe(['tagmultiuser'], 'Check cursor and view behavior', function() {
+
+	beforeEach(function() {
+		helper.setupAndLoadDocument('writer/cursor_jump.odt', true);
+		desktopHelper.switchUIToNotebookbar();
+	});
+
+	it('Do not center the view if cursor is already visible', function() {
+		// second view follow the first one
+		cy.cSetActiveFrame('#iframe2');
+		cy.cGet('#userListHeader').click();
+		cy.cGet('.user-list-item').eq(1).click();
+		cy.cGet('.jsdialog-overlay').click({force: true});
+		desktopHelper.assertScrollbarPosition('vertical', 0, 30);
+
+		// first view goes somewhere down
+		cy.cSetActiveFrame('#iframe1');
+		searchHelper.typeIntoSearchField('P'); // avoid focus loss
+		searchHelper.typeIntoSearchField('Pellentesque porttitor');
+		desktopHelper.assertScrollbarPosition('vertical', 375, 385);
+
+		// verify that second view is scrolled to the editor
+		cy.cSetActiveFrame('#iframe2');
+		desktopHelper.assertScrollbarPosition('vertical', 375, 385);
+
+		// now move cursor a bit in the first view
+		cy.cSetActiveFrame('#iframe1');
+		cy.cGet('#map').type('{downArrow}{downArrow}{downArrow}{downArrow}{downArrow}{downArrow}');
+
+		// verify that second view is still at the same position (no jump)
+		cy.cSetActiveFrame('#iframe2');
+		desktopHelper.assertScrollbarPosition('vertical', 375, 385);
+	});
+});
