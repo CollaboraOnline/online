CollaboraOnline/online

commit c025d4c043871a5f282b91d71b833afa63f8e2c7

Add otherViewCursorSection.
Signed-off-by: Gökay Şatır <gokaysatir@collabora.com>
Change-Id: Ib8b1a04631dd5ba91788ffca0e6c6ff6cee8b297

browser/src/layer/tile/CanvasTileLayer.js
@@ -701,10 +701,6 @@ L.CanvasTileLayer = L.Layer.extend({
 
 		// Position and size of the selection start (as if there would be a cursor caret there).
 
-		// View cursors with viewId to 'cursor info' mapping
-		// Eg: 1: {rectangle: 'x, y, w, h', visible: false}
-		this._viewCursors = {};
-
 		// View selection of other views
 		this._viewSelections = {};
 
@@ -1351,7 +1347,7 @@ L.CanvasTileLayer = L.Layer.extend({
 			// update tiles and selection because mode could be changed
 			this._update();
 			app.definitions.otherViewGraphicSelectionSection.updateVisibilities();
-			this.updateAllViewCursors();
+			app.definitions.otherViewCursorSection.updateVisibilities();
 			this.updateAllTextViewSelection();
 		}
 		else if (textMsg.startsWith('textselection:')) {
@@ -2378,25 +2374,7 @@ L.CanvasTileLayer = L.Layer.extend({
 			return;
 		}
 
-		var rectangle = this._getEditCursorRectangle(obj);
-		if (rectangle === undefined) {
-			return;
-		}
-
-		this._viewCursors[viewId] = this._viewCursors[viewId] || {};
-		this._viewCursors[viewId].bounds = new L.LatLngBounds(
-			this._twipsToLatLng(rectangle.getTopLeft(), this._map.getZoom()),
-			this._twipsToLatLng(rectangle.getBottomRight(), this._map.getZoom())),
-		this._viewCursors[viewId].corepxBounds = this._twipsToCorePixelsBounds(rectangle);
-		this._viewCursors[viewId].part = parseInt(obj.part);
-		this._viewCursors[viewId].mode = (obj.mode !== undefined) ? parseInt(obj.mode) : 0;
-
-		// FIXME. Server not sending view visible cursor
-		if (typeof this._viewCursors[viewId].visible === 'undefined') {
-			this._viewCursors[viewId].visible = true;
-		}
-
-		this._onUpdateViewCursor(viewId);
+		app.definitions.otherViewCursorSection.addOrUpdateOtherViewCursor(viewId, obj.rectangle.split(','), parseInt(obj.part), (obj.mode ? parseInt(obj.mode): 0));
 
 		if (app.getFollowedViewId() === viewId && (app.isFollowingEditor() || app.isFollowingUser())) {
 			if (this._map.getDocType() === 'text' || this._map.getDocType() === 'presentation') {
@@ -2471,11 +2449,12 @@ L.CanvasTileLayer = L.Layer.extend({
 			return;
 		}
 
-		if (typeof this._viewCursors[viewId] !== 'undefined') {
-			this._viewCursors[viewId].visible = (obj.visible === 'true');
+		const section = app.definitions.otherViewCursorSection.getViewCursorSection(viewId);
+		if (section) {
+			const showCursor = obj.visible === 'true';
+			section.sectionProperties.showCursor = showCursor;
+			section.setShowSection(showCursor);
 		}
-
-		this._onUpdateViewCursor(viewId);
 	},
 
 	_addView: function(viewInfo) {
@@ -2484,13 +2463,6 @@ L.CanvasTileLayer = L.Layer.extend({
 		}
 
 		this._map.addView(viewInfo);
-
-		//TODO: We can initialize color and other properties here.
-		if (typeof this._viewCursors[viewInfo.id] !== 'undefined') {
-			this._viewCursors[viewInfo.id] = {};
-		}
-
-		this._onUpdateViewCursor(viewInfo.id);
 	},
 
 	_removeView: function(viewId) {
@@ -2503,12 +2475,7 @@ L.CanvasTileLayer = L.Layer.extend({
 			delete this._viewSelections[viewId];
 		}
 
-		// update viewcursor in writer
-		if (typeof this._viewCursors[viewId] !== 'undefined') {
-			this._viewCursors[viewId].visible = false;
-			this._onUpdateViewCursor(viewId);
-			delete this._viewCursors[viewId];
-		}
+		app.definitions.otherViewCursorSection.removeView(viewId);
 
 		app.definitions.otherViewCellCursorSection.removeView(viewId);
 		app.definitions.otherViewGraphicSelectionSection.removeView(viewId);
@@ -3321,7 +3288,7 @@ L.CanvasTileLayer = L.Layer.extend({
 		if (!this.isCalc())
 			this._replayPrintTwipsMsgs(false);
 		this._onUpdateCursor(null, true);
-		this.updateAllViewCursors();
+		app.definitions.otherViewCursorSection.updateVisibilities();
 	},
 
 	_updateCursorPos: function () {
@@ -3425,12 +3392,7 @@ L.CanvasTileLayer = L.Layer.extend({
 
 		this._updateCursorAndOverlay();
 
-		this.eachView(this._viewCursors, function (item) {
-			var viewCursorMarker = item.marker;
-			if (viewCursorMarker) {
-				viewCursorMarker.setOpacity(app.file.textCursor.visible && this._cursorMarker.getPosition().equals(viewCursorMarker.getPosition()) ? 0 : 1);
-			}
-		}, this, true);
+		app.definitions.otherViewCursorSection.updateVisibilities();
 	},
 
 	activateCursor: function () {
@@ -3497,56 +3459,6 @@ L.CanvasTileLayer = L.Layer.extend({
 		}
 	},
 
-	// Update colored non-blinking view cursor
-	_onUpdateViewCursor: function (viewId) {
-		if (typeof this._viewCursors[viewId] !== 'object' ||
-		    typeof this._viewCursors[viewId].bounds !== 'object') {
-			return;
-		}
-
-		var pixBounds = this._viewCursors[viewId].corepxBounds;
-		var viewCursorPos = pixBounds.getTopLeft();
-		var viewCursorMarker = this._viewCursors[viewId].marker;
-		var viewCursorVisible = this._viewCursors[viewId].visible;
-		var viewPart = this._viewCursors[viewId].part;
-		var viewMode = this._viewCursors[viewId].mode ? this._viewCursors[viewId].mode : 0;
-
-		if (!this._map.isViewReadOnly(viewId) &&
-		    viewCursorVisible &&
-		    !this._isZooming &&
-		    !this._isEmptyRectangle(this._viewCursors[viewId].bounds) &&
-		    (this.isWriter() || (this._selectedPart === viewPart && this._selectedMode === viewMode))) {
-			if (!viewCursorMarker) {
-				var viewCursorOptions = {
-					color: L.LOUtil.rgbToHex(this._map.getViewColor(viewId)),
-					blink: false,
-					header: true, // we want a 'hat' to our view cursors (which will contain view user names)
-					headerTimeout: 3000, // hide after some interval
-					zIndex: viewId,
-					headerName: this._map.getViewName(viewId)
-				};
-				viewCursorMarker = new Cursor(viewCursorPos, pixBounds.getSize(), this._map, viewCursorOptions);
-				this._viewCursors[viewId].marker = viewCursorMarker;
-			}
-			else {
-				viewCursorMarker.setPositionSize(viewCursorPos, pixBounds.getSize());
-			}
-			viewCursorMarker.setOpacity(app.file.textCursor.visible && this._cursorMarker.getPosition().equals(viewCursorMarker.getPosition()) ? 0 : 1);
-			if (!viewCursorMarker.isDomAttached())
-				viewCursorMarker.add();
-		}
-		else if (viewCursorMarker && viewCursorMarker.isDomAttached()) {
-			viewCursorMarker.remove();
-		}
-
-		if (this._viewCursors[viewId].marker && this._viewCursors[viewId].marker.isDomAttached())
-			this._viewCursors[viewId].marker.showCursorHeader();
-	},
-
-	updateAllViewCursors: function() {
-		this.eachView(this._viewCursors, this._onUpdateViewCursor, this, false);
-	},
-
 	updateAllTextViewSelection: function() {
 		this.eachView(this._viewSelections, this._onUpdateTextViewSelection, this, false);
 	},
@@ -3557,13 +3469,12 @@ L.CanvasTileLayer = L.Layer.extend({
 			return;
 		}
 
-		if (this._viewCursors[viewId] && this._viewCursors[viewId].visible && !this._isEmptyRectangle(this._viewCursors[viewId].bounds)) {
-			if (!this._map.getBounds().contains(this._viewCursors[viewId].bounds)) {
-				var viewCursorPos = this._viewCursors[viewId].bounds.getNorthWest();
-				this.scrollToPos(viewCursorPos);
-			}
+		const section = app.definitions.otherViewCursorSection.getViewCursorSection(viewId);
 
-			this._viewCursors[viewId].marker.showCursorHeader();
+		if (section && section.showSection) {
+			const point = app.definitions.simplePoint(section.position[0], section.position[1]);
+			this.scrollToPos(point);
+			section.showCursorHeader();
 		}
 	},
 
@@ -4457,7 +4368,6 @@ L.CanvasTileLayer = L.Layer.extend({
 		if (this._docType === 'spreadsheet') {
 			map.on('zoomend', this._onCellCursorShift, this);
 		}
-		map.on('zoomend', L.bind(this.eachView, this, this._viewCursors, this._onUpdateViewCursor, this, false));
 		map.on('dragstart', this._onDragStart, this);
 		map.on('error', this._mapOnError, this);
 		if (map.options.autoFitWidth !== false) {
@@ -4577,15 +4487,7 @@ L.CanvasTileLayer = L.Layer.extend({
 		if (this._selectionHandles.end.isSectionShown())
 			this._selectionHandles.end.setOpacity(0);
 
-		this.eachView(this._viewCursors, function (item) {
-			var viewCursorMarker = item.marker;
-			if (viewCursorMarker) {
-				viewCursorMarker.setOpacity(0);
-			}
-		}, this, true);
-
-
-
+		app.definitions.otherViewCursorSection.updateVisibilities(true);
 	},
 
 	postZoomAnimation: function () {

browser/.beforeprettier
@@ -147,6 +147,7 @@
 /src/canvas/sections/TextSelectionHandleSection.ts
 /src/canvas/sections/OtherViewGraphicSelectionSection.ts
 /src/canvas/sections/OtherViewCellCursorSection.ts
+/src/canvas/sections/OtherViewCursorSection.ts
 /src/layer/tile/CalcTileLayer.js
 /src/canvas/CanvasSectionContainer.ts
 /src/canvas/CanvasSectionObject.ts

browser/Makefile.am
@@ -245,6 +245,7 @@ COOL_JS_LST =\
 	src/canvas/sections/TextSelectionHandleSection.ts \
 	src/canvas/sections/OtherViewGraphicSelectionSection.ts \
 	src/canvas/sections/OtherViewCellCursorSection.ts \
+	src/canvas/sections/OtherViewCursorSection.ts \
 	src/layer/vector/CEventsHandler.ts \
 	src/layer/vector/CPointSet.ts \
 	src/layer/vector/CPath.ts \

browser/src/canvas/sections/OtherViewCursorSection.ts
@@ -1,0 +1,135 @@
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+// This is used for other views' cursors.
+
+class OtherViewCursorSection extends HTMLObjectSection {
+    documentObject: boolean = true;
+    interactable: boolean = false; // We don't bother with events.
+    zIndex: number = L.CSections.DefaultForDocumentObjects.processingOrder;
+    drawingOrder: number = L.CSections.DefaultForDocumentObjects.drawingOrder;
+    processingOrder: number = L.CSections.DefaultForDocumentObjects.processingOrder;
+
+    static sectionNamePrefix = 'OtherViewCursor ';
+    static sectionPointers: Array<OtherViewCursorSection> = [];
+
+    constructor(viewId: number, rectangle: cool.SimpleRectangle, part: number, mode: number) {
+        super(OtherViewCursorSection.sectionNamePrefix + viewId, rectangle.pWidth / app.dpiScale, rectangle.pHeight / app.dpiScale, new cool.SimplePoint(rectangle.x1, rectangle.y1));
+
+        this.sectionProperties.color = L.LOUtil.rgbToHex(L.LOUtil.getViewIdColor(viewId));
+
+        this.sectionProperties.viewId = viewId;
+        this.sectionProperties.part = part;
+        this.sectionProperties.mode = mode;
+        this.sectionProperties.showCursor = true;
+        this.showSection = true;
+        this.getHTMLObject().style.backgroundColor = this.sectionProperties.color;
+    }
+
+    checkMyVisibility() {
+        let result = this.sectionProperties.showCursor && this.size[1] > 0;
+
+        if (result) {
+            if (!app.map._docLayer.isWriter()) {
+                if (this.sectionProperties.part !== app.map._docLayer._selectedPart || this.sectionProperties.mode !== app.map._docLayer._selectedMode)
+                    result = false;
+            }
+        }
+
+        if (result && app.file.textCursor.visible) {
+            const pos = [app.file.textCursor.rectangle.pX1, app.file.textCursor.rectangle.pY1];
+            if (this.position[0] === pos[0] && this.position[1] === pos[1])
+                result = false;
+        }
+
+        if (result && app.map.isViewReadOnly(this.sectionProperties.viewId))
+            result = false;
+
+        return result;
+    }
+
+    public showCursorHeader() {
+        return;
+    }
+
+    public static addOrUpdateOtherViewCursor(viewId: number, rectangleData: Array<string>, part: number, mode: number) {
+        let rectangle = new cool.SimpleRectangle(0, 0, 0, 0);
+        if (rectangleData) {
+            rectangle = new app.definitions.simpleRectangle(parseInt(rectangleData[0]), parseInt(rectangleData[1]), parseInt(rectangleData[2]), parseInt(rectangleData[3]));
+        }
+
+        if (rectangle.pWidth === 0)
+            rectangle.pWidth = 2 * app.dpiScale; // Width of the cursor.
+
+        const sectionName = OtherViewCursorSection.sectionNamePrefix + viewId;
+        let section: OtherViewCursorSection;
+        if (app.sectionContainer.doesSectionExist(sectionName)) {
+            section = app.sectionContainer.getSectionWithName(sectionName);
+            section.sectionProperties.part = part;
+            section.sectionProperties.mode = mode;
+            section.size[0] = rectangle.pWidth;
+            section.size[1] = rectangle.pHeight;
+
+            section.getHTMLObject().style.width = (section.size[0] / app.dpiScale) + 'px';
+            section.getHTMLObject().style.height = (section.size[1] / app.dpiScale) + 'px';
+
+            section.setPosition(rectangle.pX1, rectangle.pY1);
+        }
+        else {
+            section = new OtherViewCursorSection(viewId, rectangle, part, mode);
+            app.sectionContainer.addSection(section);
+            OtherViewCursorSection.sectionPointers.push(section);
+        }
+
+        section.sectionProperties.showCursor = true;
+        section.setShowSection(section.checkMyVisibility());
+        section.onNewDocumentTopLeft();
+        section.adjustHTMLObjectPosition();
+        app.sectionContainer.requestReDraw();
+    }
+
+    public static removeView(viewId: number) {
+        const sectionName = OtherViewCursorSection.sectionNamePrefix + viewId;
+        if (app.sectionContainer.doesSectionExist(sectionName)) {
+            const section = app.sectionContainer.getSectionWithName(sectionName);
+            OtherViewCursorSection.sectionPointers.splice(OtherViewCursorSection.sectionPointers.indexOf(section), 1);
+            app.sectionContainer.removeSection(sectionName);
+            app.sectionContainer.requestReDraw();
+        }
+    }
+
+    public static doesViewCursorSectionExist(viewId: number) {
+        const name = OtherViewCursorSection.sectionNamePrefix + viewId;
+        return app.sectionContainer.doesSectionExist(name);
+    }
+
+    public static getViewCursorSection(viewId: number) {
+        if (OtherViewCursorSection.doesViewCursorSectionExist(viewId)) {
+            const name = OtherViewCursorSection.sectionNamePrefix + viewId;
+            return app.sectionContainer.getSectionWithName(name);
+        }
+        else
+            return null;
+    }
+
+    public static updateVisibilities(hideCursors: boolean = false) {
+        for (let i = 0; i < OtherViewCursorSection.sectionPointers.length; i++) {
+            const section = OtherViewCursorSection.sectionPointers[i];
+            section.setShowSection(section.checkMyVisibility());
+            if (hideCursors)
+                section.getHTMLObject().style.opacity = '0';
+            else
+                section.getHTMLObject().style.opacity = '1';
+        }
+        app.sectionContainer.requestReDraw();
+    }
+}
+
+app.definitions.otherViewCursorSection = OtherViewCursorSection;

browser/src/control/Parts.js
@@ -92,8 +92,8 @@ L.Map.include({
 			docType: docLayer._docType
 		});
 
-		docLayer.eachView(docLayer._viewCursors, docLayer._onUpdateViewCursor, docLayer);
 		app.definitions.otherViewCellCursorSection.updateVisibilities();
+		app.definitions.otherViewCursorSection.updateVisibilities();
 		app.definitions.otherViewGraphicSelectionSection.updateVisibilities();
 		docLayer.eachView(docLayer._viewSelections, docLayer._onUpdateTextViewSelection, docLayer);
 		docLayer._clearSelections(calledFromSetPartHandler);

commit cced31f0d6a45fcf368ea3839517526c2ab53612

Writer: don't center followed cursor on already visible area
Fixes #9879
Following other user in writer always centers the screen on caret

Steps to Reproduce
1. Open document in 2 windows
2. view A follows view B
3. view B moves cursor by click on text (on the same page, which is visible by view A)

Expected Behavior
If new position of the cursor is still visible - don't center the view on caret

That was regression from commit c025d4c
Add otherViewCursorSection.

Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
Change-Id: I5907ccd8f45629e1362043bf1222f2ee62fb140b

browser/src/layer/tile/CanvasTileLayer.js
@@ -3306,7 +3306,9 @@ L.CanvasTileLayer = L.Layer.extend({
 
 		if (section && section.showSection) {
 			const point = new app.definitions.simplePoint(section.position[0] * app.pixelsToTwips, section.position[1] * app.pixelsToTwips);
-			this.scrollToPos(point);
+			var isNewCursorVisible = app.isPointVisibleInTheDisplayedArea(point.toArray());
+			if (!isNewCursorVisible)
+				this.scrollToPos(point);
 			app.definitions.cursorHeaderSection.showCursorHeader(viewId);
 		}
 	},