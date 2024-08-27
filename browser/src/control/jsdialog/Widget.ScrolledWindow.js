af8bd010dc89ace7b34c86ff8673050f51a83645
Szymon Kłos <szymon.klos@collabora.com>
2024-08-27 15:53:44 +0200

    Revert "jsdialog: send less messages for scrolled window"
    This reverts commit fd35bd76892f114f43f9558bcbd698957390cb08.
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I08701f800c132c107771e822836718ca3e8230c9

--- browser/src/control/jsdialog/Widget.ScrolledWindow.js
+++ browser/src/control/jsdialog/Widget.ScrolledWindow.js
@@ -142,8 +142,6 @@ function _scrolledWindowControl(parentContainer, data, builder) {
 		resizeObserver.observe(content);
 	}
 
-	var lastScrollV = null;
-	var lastScrollH = null;
 	var sendTimer = null;
 	if ((!noVertical && verticalSteps) || (!noHorizontal && horizontalSteps)) {
 		scrollwindow.addEventListener('scroll', function() {
@@ -161,18 +159,8 @@ function _scrolledWindowControl(parentContainer, data, builder) {
 			if (sendTimer)
 				clearTimeout(sendTimer);
 			sendTimer = setTimeout(function () {
-				var newScrollV = Math.round(scrollwindow.scrollTop / rowHeight);
-				if (lastScrollV !== newScrollV) {
-					lastScrollV = newScrollV;
-					builder.callback('scrolledwindow', 'scrollv', scrollwindow, newScrollV, builder);
-				}
-
-				var newScrollH = Math.round(scrollwindow.scrollLeft / 10);
-				if (lastScrollH !== newScrollH) {
-					lastScrollH = newScrollH;
-					builder.callback('scrolledwindow', 'scrollh', scrollwindow, newScrollH, builder);
-				}
-			}, 50);
+				builder.callback('scrolledwindow', 'scrollv', scrollwindow, Math.round(scrollwindow.scrollTop / rowHeight), builder);
+				builder.callback('scrolledwindow', 'scrollh', scrollwindow, Math.round(scrollwindow.scrollLeft / 10), builder); }, 50);
 		});
 	}
 
