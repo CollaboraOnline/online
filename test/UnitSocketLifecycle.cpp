7751c45b2bc0e182ebe25d8e65e7f3a24441eaa7
Sven Göthel <sven.gothel@collabora.com>
2024-08-28 10:52:04 +0200

    UnitSocketLifecycle: Fix class-name, add working testWebsockWSDChatPingPong(), ..
    testWebsockWSDChatPingPong()
    - Tests the WSD chat ping/pong facility, where client sends the ping.
    - See: https://github.com/CollaboraOnline/online/blob/master/wsd/protocol.txt/
    
    testWebsockNativePingPong()
    - Attempt to test the native WebSocket control-frame ping/pong facility.
    - Not working yet due to wrong thread affinity error from sending socket (by client)
    - Disabled, needs tooling work allowing sending native WebSocket ping/pongs.
    
    Signed-off-by: Sven Göthel <sven.gothel@collabora.com>
    Change-Id: I7e1a9329e0848c40a210f6250e29e26950da6fbc

--- test/UnitSocketLifecycle.cpp
+++ test/UnitSocketLifecycle.cpp
@@ -33,22 +33,25 @@
 #include <vector>
 
 /// Test suite that uses a HTTP session (and not just a socket) directly.
-class UnitSession : public UnitWSD
+class UnitSocketLifecycle : public UnitWSD
 {
-    TestResult testOneRound();
+    TestResult testOneHTTPSession();
+    TestResult testWebsockNativePingPong();
+    TestResult testWebsockWSDChatPingPong();
 
 public:
-    UnitSession()
-        : UnitWSD("UnitSession")
+    UnitSocketLifecycle()
+        : UnitWSD("UnitSocketLifecycle")
     {
     }
 
     void invokeWSDTest() override;
 };
 
-UnitBase::TestResult UnitSession::testOneRound()
+UnitBase::TestResult UnitSocketLifecycle::testOneHTTPSession()
 {
     setTestname(__func__);
+    TST_LOG("Starting Test: " << testname);
 
     const std::string documentURL = "/favicon.ico";
 
@@ -94,18 +97,167 @@ UnitBase::TestResult UnitSession::testOneRound()
     std::this_thread::sleep_for(std::chrono::seconds(4));
     TST_LOG("Test: XXX " << testname << " w/ " << sockCount << " connections, done!");
 
+    TST_LOG("Ending Test: " << testname);
     return TestResult::Ok;
 }
 
-void UnitSession::invokeWSDTest()
+/// Attempt to test the native WebSocket control-frame ping/pong facility.
+/// Not working yet due to wrong thread affinity error from sending socket (by client)
+UnitBase::TestResult UnitSocketLifecycle::testWebsockNativePingPong()
 {
-    UnitBase::TestResult result = testOneRound();
+    setTestname(__func__);
+    TST_LOG("Starting Test: " << testname);
+
+    std::string documentPath, documentURL;
+    helpers::getDocumentPathAndURL("hello.odt", documentPath, documentURL, testname);
+
+    // NOTE: Do not replace with wrappers. This has to be explicit.
+    std::shared_ptr<http::WebSocketSession> wsSession = http::WebSocketSession::create(helpers::getTestServerURI());
+    http::Request req(documentURL);
+    wsSession->asyncRequest(req, socketPoll());
+
+    constexpr const bool loadDoc = true;
+    if( loadDoc ) {
+        wsSession->sendMessage("load url=" + documentURL);
+    }
+
+    auto assertMessage = [&wsSession, this](const std::string expectedPrefix, const std::string expectedId)
+    {
+        wsSession->poll(
+            [&](const std::vector<char>& message) -> bool
+            {
+                const std::string msg(std::string(message.begin(), message.end()));
+                TST_LOG("Got WS response: " << msg);
+                if (!msg.starts_with("error:"))
+                {
+                    if( expectedPrefix == "progress:") {
+                        LOK_ASSERT_EQUAL(COOLProtocol::matchPrefix(expectedPrefix, msg), true);
+                        LOK_ASSERT(helpers::getProgressWithIdValue(msg, expectedId));
+                        TST_LOG("Good WS response(0): " << msg);
+                        return true;
+                    } else if( msg.find(expectedId) != std::string::npos ) {
+                        // simple match
+                        TST_LOG("Good WS response(1): " << msg);
+                        return true;
+                    } else {
+                        return false; // continue waiting for 'it'
+                    }
+                }
+                else
+                {
+                    // check error message
+                    LOK_ASSERT_EQUAL(std::string(SERVICE_UNAVAILABLE_INTERNAL_ERROR), msg);
+
+                    // close frame message
+                    //TODO: check that the socket is closed.
+                    return true;
+                }
+            },
+            std::chrono::seconds(10), testname);
+    };
+
+    if( loadDoc ) {
+        assertMessage("progress:", "find");
+        assertMessage("progress:", "connect");
+        assertMessage("progress:", "ready");
+    }
+
+    /// THIS DOES NOT WORK!
+    /// wsSession->sendPing(std::chrono::steady_clock::now());
+    /// assertMessage("", "pong");
+
+    TST_LOG("Ending Test: " << testname);
+    return TestResult::Ok;
+}
+
+/// Tests the WSD chat ping/pong facility, where client sends the ping.
+/// See: https://github.com/CollaboraOnline/online/blob/master/wsd/protocol.txt/
+UnitBase::TestResult UnitSocketLifecycle::testWebsockWSDChatPingPong()
+{
+    setTestname(__func__);
+    TST_LOG("Starting Test: " << testname);
+
+    std::string documentPath, documentURL;
+    helpers::getDocumentPathAndURL("hello.odt", documentPath, documentURL, testname);
+
+    // NOTE: Do not replace with wrappers. This has to be explicit.
+    std::shared_ptr<http::WebSocketSession> wsSession = http::WebSocketSession::create(helpers::getTestServerURI());
+    http::Request req(documentURL);
+    wsSession->asyncRequest(req, socketPoll());
+
+    // wsd/ClientSession.cpp:709 sendTextFrameAndLogError("error: cmd=" + tokens[0] + " kind=nodocloaded");
+    constexpr const bool loadDoc = true; // Required for WSD chat -> wsd/ClientSession.cpp:709, common/Session.hpp:160
+    if( loadDoc ) {
+        wsSession->sendMessage("load url=" + documentURL);
+    }
+
+    auto assertMessage = [&wsSession, this](const std::string expectedPrefix, const std::string expectedId)
+    {
+        wsSession->poll(
+            [&](const std::vector<char>& message) -> bool
+            {
+                const std::string msg(std::string(message.begin(), message.end()));
+                TST_LOG("Got WS response: " << msg);
+                if (!msg.starts_with("error:"))
+                {
+                    if( expectedPrefix == "progress:") {
+                        LOK_ASSERT_EQUAL(COOLProtocol::matchPrefix(expectedPrefix, msg), true);
+                        LOK_ASSERT(helpers::getProgressWithIdValue(msg, expectedId));
+                        TST_LOG("Good WS response(0): " << msg);
+                        return true;
+                    } else if( msg.find(expectedId) != std::string::npos ) {
+                        // simple match
+                        TST_LOG("Good WS response(1): " << msg);
+                        return true;
+                    } else {
+                        return false; // continue waiting for 'it'
+                    }
+                }
+                else
+                {
+                    // check error message
+                    LOK_ASSERT_EQUAL(std::string(SERVICE_UNAVAILABLE_INTERNAL_ERROR), msg);
+
+                    // close frame message
+                    //TODO: check that the socket is closed.
+                    return true;
+                }
+            },
+            std::chrono::seconds(10), testname);
+    };
+
+    assertMessage("progress:", "find");
+    assertMessage("progress:", "connect");
+    assertMessage("progress:", "ready");
+
+    wsSession->sendMessage("ping");
+    assertMessage("", "pong");
+
+    TST_LOG("Ending Test: " << testname);
+    return TestResult::Ok;
+}
+
+void UnitSocketLifecycle::invokeWSDTest()
+{
+    UnitBase::TestResult result;
+
+    result = testOneHTTPSession();
+    if (result != TestResult::Ok)
+        exitTest(result);
+
+    if( false ) {
+        result = testWebsockNativePingPong();
+        if (result != TestResult::Ok)
+            exitTest(result);
+    }
+
+    result = testWebsockWSDChatPingPong();
     if (result != TestResult::Ok)
         exitTest(result);
 
     exitTest(TestResult::Ok);
 }
 
-UnitBase* unit_create_wsd(void) { return new UnitSession(); }
+UnitBase* unit_create_wsd(void) { return new UnitSocketLifecycle(); }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
