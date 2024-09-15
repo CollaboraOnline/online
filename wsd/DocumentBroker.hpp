de2e0f1cf23ed918848b5ac84c4feb08dff5028a
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2024-09-15 11:23:20 -0400

    wsd: wopi: remember the document size we uploaded
    This is to help us recover from an upload failure.
    
    Change-Id: I01b843f9dde329babf3263b3a42d7f51867254a4
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- wsd/DocumentBroker.hpp
+++ wsd/DocumentBroker.hpp
@@ -1287,6 +1287,7 @@ private:
     public:
         StorageManager(std::chrono::milliseconds minTimeBetweenUploads)
             : _request(minTimeBetweenUploads)
+            , _sizeAsUploaded(0)
         {
             if (Log::traceEnabled())
             {
@@ -1346,6 +1347,14 @@ private:
         /// Returns the last modified time of the document.
         const std::string& getLastModifiedTime() const { return _lastModifiedTime; }
 
+        /// Set size of the document as we've uploaded.
+        /// Used to resynchronize after an upload failure that break reliance on the LastModifiedTime.
+        void setSizeAsUploaded(std::size_t size) { _sizeAsUploaded = size; }
+
+        /// Get size of the document as we've set in our PutFile header.
+        /// Used to resynchronize after an upload failure that break reliance on the LastModifiedTime.
+        std::size_t getSizeAsUploaded() const { return _sizeAsUploaded; }
+
         /// Returns how long the last upload took.
         std::chrono::milliseconds lastUploadDuration() const
         {
@@ -1377,6 +1386,7 @@ private:
             os << indent << "last upload was successful: " << std::boolalpha
                << lastUploadSuccessful();
             os << indent << "upload failure count: " << uploadFailureCount();
+            os << indent << "last upload size: " << _sizeAsUploaded;
         }
 
     private:
@@ -1388,6 +1398,10 @@ private:
 
         /// The modified time of the document in storage, as reported by the server.
         std::string _lastModifiedTime;
+
+        /// The size of the document as we uploaded to the server.
+        /// Used to help resynchronize the LastModifiedTime after an upload failure.
+        std::size_t _sizeAsUploaded;
     };
 
     /// Represents a lock-state update request.
