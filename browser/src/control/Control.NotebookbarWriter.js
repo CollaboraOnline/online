5750efdc50a9d5a59863b143867efa10c9b0ff5f
Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>
2024-09-18 23:18:52 +0530

    Fix hover text labels in Table menu for consistency and clarity
    + Corrected the hover text labels in the Table menu to ensure they accurately describe the actions they represent:
    + Adjustments are done based on which string should be displayed from unocommand.js file( "Context" or "menu")
    
    This resolves inconsistencies and improves user experience by providing clearer descriptions for each menu action.
    
    Signed-off-by: Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>
    Change-Id: Iec2f04434eb175cbf5a38a5b91dd3e4e896ce7e9

--- browser/src/control/Control.NotebookbarWriter.js
+++ browser/src/control/Control.NotebookbarWriter.js
@@ -2357,7 +2357,7 @@ L.Control.NotebookbarWriter = L.Control.Notebookbar.extend({
 							},
 							{
 								'type': 'toolitem',
-								'text': _UNO('.uno:DeleteColumns', 'text'),
+								'text': _UNO('.uno:DeleteColumns', 'text', true),
 								'command': '.uno:DeleteColumns'
 							}
 						]
@@ -2377,7 +2377,7 @@ L.Control.NotebookbarWriter = L.Control.Notebookbar.extend({
 							},
 							{
 								'type': 'toolitem',
-								'text': _UNO('.uno:DeleteRows', 'text'),
+								'text': _UNO('.uno:DeleteRows', 'text', true),
 								'command': '.uno:DeleteRows'
 							}
 						]
@@ -2444,7 +2444,7 @@ L.Control.NotebookbarWriter = L.Control.Notebookbar.extend({
 			},
 			{
 				'type': 'bigtoolitem',
-				'text': _UNO('.uno:EntireCell', 'text'),
+				'text': _UNO('.uno:EntireCell', 'text', true),
 				'command': '.uno:EntireCell'
 			},
 			{
@@ -2455,12 +2455,12 @@ L.Control.NotebookbarWriter = L.Control.Notebookbar.extend({
 						'children': [
 							{
 								'type': 'toolitem',
-								'text': _UNO('.uno:EntireColumn', 'text'),
+								'text': _UNO('.uno:EntireColumn', 'presentation'),
 								'command': '.uno:EntireColumn'
 							},
 							{
 								'type': 'toolitem',
-								'text': _UNO('.uno:SelectTable', 'text'),
+								'text': _UNO('.uno:SelectTable', 'text', true),
 								'command': '.uno:SelectTable'
 							},
 						]
@@ -2470,12 +2470,12 @@ L.Control.NotebookbarWriter = L.Control.Notebookbar.extend({
 						'children': [
 							{
 								'type': 'toolitem',
-								'text': _UNO('.uno:EntireRow', 'text'),
+								'text': _UNO('.uno:EntireRow', 'presentation'),
 								'command': '.uno:EntireRow'
 							},
 							{
 								'type': 'toolitem',
-								'text': _UNO('.uno:DeleteTable', 'text'),
+								'text': _UNO('.uno:DeleteTable', 'text', true),
 								'command': '.uno:DeleteTable'
 							},
 						]
