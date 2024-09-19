CollaboraOnline/online

commit bb8b7dadee8ee360ce43a8433730484e8ccaca22

slideshow: fix present from current slide
When we close fullscreen we don't call escape handler
as earlier we turn off it's handler so we didn't properly
clear status of navigator.

This caused bug when presenting second time. We didn't start from
current slide but the last slide of previous fullscreen slideshow.

Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
Change-Id: I27ea29521e827bd635d1b48804b5a14a94a0c2f0

browser/src/slideshow/SlideShowPresenter.ts
@@ -159,6 +159,13 @@ class SlideShowPresenter {
 			'keydown',
 			this._slideShowNavigator.onKeyDown.bind(this._slideShowNavigator),
 		);
+
+		if (this._fullscreen) {
+			// we need to cleanup current/prev slide,
+			// Escape handler is not called as we disabled it above
+			this._slideShowNavigator.endPresentation(false);
+		}
+
 		L.DomUtil.remove(this._slideShowCanvas);
 		this._slideShowCanvas = null;
 		if (this._presenterContainer) {

browser/src/slideshow/engine/SlideShowNavigator.ts
@@ -142,8 +142,6 @@ class SlideShowNavigator {
 			'SlideShowNavigator.quit: current index: ' + this.currentSlide,
 		);
 		this.endPresentation(true);
-		this.currentSlide = undefined;
-		this.prevSlide = undefined;
 		this.removeHandlers();
 	}
 
@@ -214,6 +212,8 @@ class SlideShowNavigator {
 	}
 
 	endPresentation(force: boolean = false) {
+		this.currentSlide = undefined;
+		this.prevSlide = undefined;
 		this.presenter.endPresentation(force);
 	}
 

commit a499b4f38c854428267eb86597e90c2a1ecf0192

slideshow: fix exit on click
please click to exit presentation - was not working
regression from commit bb8b7dadee8ee360ce43a8433730484e8ccaca22
slideshow: fix present from current slide

Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
Change-Id: I8c05b7c2cd075a3b2fe33da2d4e876e61ee080fe

browser/src/slideshow/SlideShowPresenter.ts
@@ -146,7 +146,7 @@ class SlideShowPresenter {
 			this.centerCanvas();
 		} else {
 			// we need to cleanup current/prev slide
-			this._slideShowNavigator.endPresentation(true);
+			this._slideShowNavigator.quit();
 		}
 	}
 

browser/src/slideshow/engine/SlideShowNavigator.ts
@@ -142,6 +142,8 @@ class SlideShowNavigator {
 			'SlideShowNavigator.quit: current index: ' + this.currentSlide,
 		);
 		this.endPresentation(true);
+		this.currentSlide = undefined;
+		this.prevSlide = undefined;
 		this.removeHandlers();
 	}
 
@@ -167,7 +169,8 @@ class SlideShowNavigator {
 		if (nNewSlide >= this.theMetaPres.numberOfSlides) {
 			this.currentSlide = nNewSlide;
 			const force = nNewSlide > this.theMetaPres.numberOfSlides;
-			this.endPresentation(force);
+			if (force) this.quit();
+			else this.endPresentation(false);
 			return;
 		}
 		this.slideCompositor.fetchAndRun(nNewSlide, () => {
@@ -212,8 +215,6 @@ class SlideShowNavigator {
 	}
 
 	endPresentation(force: boolean = false) {
-		this.currentSlide = undefined;
-		this.prevSlide = undefined;
 		this.presenter.endPresentation(force);
 	}
 