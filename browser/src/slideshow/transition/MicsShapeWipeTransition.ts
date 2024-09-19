8879ee47a1564573c9ac170f34169743da1a0a9d
codewithvk <vivek.javiya@collabora.com>
2024-09-19 12:41:29 +0530

    Slideshow: Enhance flip tiles transition
    - Added WebGL flags to enable multi-texture rendering for simultaneous display of both slides.
    - Fixed a potential GL state issue in the displaySlides function, ensuring proper handling of texture binding and GL state preservation during the transition.
    
    Signed-off-by: codewithvk <vivek.javiya@collabora.com>
    Change-Id: I15d0a7c418225e1f707f16fcb65ade68f4c90ad6

--- browser/src/slideshow/transition/MicsShapeWipeTransition.ts
+++ browser/src/slideshow/transition/MicsShapeWipeTransition.ts
@@ -48,7 +48,7 @@ function MicsShapeWipeTransition(transitionParameters: TransitionParameters) {
 	} else if (transitionSubType == TransitionSubType.FANOUTHORIZONTAL) {
 		return SlideShow.makeHelixTransition(transitionParameters, 20);
 	} else if (transitionSubType == TransitionSubType.ACROSS) {
-		return SlideShow.FlipTilesTransition(transitionParameters, 8, 6);
+		return SlideShow.makeFlipTilesTransition(transitionParameters, 8, 6);
 	} else if (transitionSubType == TransitionSubType.DIAMOND) {
 		return new SlideShow.NoTransition(transitionParameters); //  todo: glitter transition
 	} else if (transitionSubType == TransitionSubType.HEART) {
