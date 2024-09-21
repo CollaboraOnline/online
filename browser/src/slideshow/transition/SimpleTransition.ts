CollaboraOnline/online

commit 3a1f0357c0e918cd2681211a9061a934c1dd2349

Slideshow: Enhance flip tiles transition
- Added WebGL flags to enable multi-texture rendering for simultaneous display of both slides.
- Fixed a potential GL state issue in the displaySlides function, ensuring proper handling of texture binding and GL state preservation during the transition.

Signed-off-by: codewithvk <vivek.javiya@collabora.com>
Change-Id: I15d0a7c418225e1f707f16fcb65ade68f4c90ad6

browser/src/slideshow/transition/SimpleTransition.ts
@@ -27,11 +27,14 @@ class SimpleTransition extends SlideShow.Transition3d {
 
 		this.textures = [transitionParameters.current, transitionParameters.next];
 
+		this.initWebglFlags();
+		this.prepareTransition();
+	}
+
+	public initWebglFlags(): void {
 		// Enable alpha blending
 		this.gl.enable(this.gl.BLEND);
 		this.gl.blendFunc(this.gl.SRC_ALPHA, this.gl.ONE_MINUS_SRC_ALPHA);
-
-		this.prepareTransition();
 	}
 
 	public initBuffers(): void {

browser/src/slideshow/transition/FlipTilesTransition.ts
@@ -12,6 +12,66 @@
 
 declare var SlideShow: any;
 
+class FlipTilesTransition extends SimpleTransition {
+	constructor(transitionParameters: TransitionParameters3D) {
+		super(transitionParameters);
+	}
+
+	public initWebglFlags(): void {
+		this.gl.enable(this.gl.DEPTH_TEST);
+		this.gl.depthFunc(this.gl.LEQUAL);
+
+		// Enable face culling
+		this.gl.enable(this.gl.CULL_FACE);
+		this.gl.cullFace(this.gl.BACK);
+
+		// Disable blending
+		this.gl.disable(this.gl.BLEND);
+	}
+
+	public processPrimitives(
+		t: number,
+		texturePrimitive: Primitive[],
+		applyOperations: (t: number, operations: Operation[]) => void,
+	): void {
+		for (const primitive of texturePrimitive) {
+			this.setBufferData(primitive.vertices);
+			applyOperations.call(this, t, primitive.operations);
+			this.gl.drawArrays(this.gl.TRIANGLES, 0, primitive.vertices.length);
+		}
+	}
+
+	public displaySlides_(t: number): void {
+		this.applyAllOperation(t);
+
+		// Render the entering slide primitives
+		this.gl.activeTexture(this.gl.TEXTURE0);
+		this.gl.bindTexture(this.gl.TEXTURE_2D, this.textures[1]);
+		this.gl.uniform1i(
+			this.gl.getUniformLocation(this.program, 'slideTexture'),
+			0,
+		);
+		this.processPrimitives(
+			t,
+			this.enteringPrimitives,
+			this.applyEnteringOperations,
+		);
+
+		// Render the leaving slide primitives
+		this.gl.activeTexture(this.gl.TEXTURE0);
+		this.gl.bindTexture(this.gl.TEXTURE_2D, this.textures[0]);
+		this.gl.uniform1i(
+			this.gl.getUniformLocation(this.program, 'slideTexture'),
+			0,
+		);
+		this.processPrimitives(
+			t,
+			this.leavingPrimitives,
+			this.applyLeavingOperations,
+		);
+	}
+}
+
 function vec(x: number, y: number, nx: number, ny: number): vec2 {
 	x = x < 0.0 ? 0.0 : x;
 	x = Math.min(x, nx);
@@ -20,7 +80,15 @@ function vec(x: number, y: number, nx: number, ny: number): vec2 {
 	return [x / nx, y / ny];
 }
 
-function FlipTilesTransition(
+function calculateMidpoint(vec1: vec3, vec2: vec3): vec3 {
+	return [
+		(vec1[0] + vec2[0]) / 2.0, // X coordinate
+		(vec1[1] + vec2[1]) / 2.0, // Y coordinate
+		(vec1[2] + vec2[2]) / 2.0, // Z coordinate
+	];
+}
+
+function makeFlipTilesTransition(
 	transitionParameters: TransitionParameters,
 	n: number = 8,
 	m: number = 6,
@@ -42,7 +110,7 @@ function FlipTilesTransition(
 			aTile.operations.push(
 				makeSRotate(
 					vec3.fromValues(0, 1, 0),
-					vec3.fromValues(0, 0.8, 0),
+					calculateMidpoint(aTile.getVertex(1), aTile.getVertex(3)),
 					180,
 					true,
 					(x11[0] * x11[1]) / 2.0,
@@ -54,7 +122,7 @@ function FlipTilesTransition(
 			aTile.operations.push(
 				makeSRotate(
 					vec3.fromValues(0, 1, 0),
-					vec3.fromValues(0, 0.8, 0),
+					calculateMidpoint(aTile.getVertex(1), aTile.getVertex(3)),
 					-180,
 					false,
 					(x11[0] * x11[1]) / 2.0,
@@ -72,7 +140,7 @@ function FlipTilesTransition(
 		allOperations: [],
 	};
 
-	return new SimpleTransition(newTransitionParameters);
+	return new FlipTilesTransition(newTransitionParameters);
 }
 
-SlideShow.FlipTilesTransition = FlipTilesTransition;
+SlideShow.makeFlipTilesTransition = makeFlipTilesTransition;

browser/src/slideshow/transition/MicsShapeWipeTransition.ts
@@ -48,7 +48,7 @@ function MicsShapeWipeTransition(transitionParameters: TransitionParameters) {
 	} else if (transitionSubType == TransitionSubType.FANOUTHORIZONTAL) {
 		return SlideShow.makeHelixTransition(transitionParameters, 20);
 	} else if (transitionSubType == TransitionSubType.ACROSS) {
-		return SlideShow.FlipTilesTransition(transitionParameters, 8, 6);
+		return SlideShow.makeFlipTilesTransition(transitionParameters, 8, 6);
 	} else if (transitionSubType == TransitionSubType.DIAMOND) {
 		return new SlideShow.NoTransition(transitionParameters); //  todo: glitter transition
 	} else if (transitionSubType == TransitionSubType.HEART) {

commit 2d34e55019645f0c930d989c31acc3a18eb499d1

Fix broken transitions due to global WebGL state changes
This fixes a regression introduced in commit 3a1f0357c0e918cd2681211a9061a934c1dd2349.

In the FlipTilesTransition, we enabled several WebGL flags such as DEPTH_TEST and CULL_FACE, and disabled BLEND. Since WebGL uses a global state machine for each canvas, these state changes inadvertently affected other transitions that rely on different WebGL states, causing them to break.

To fix this issue we have to we have to disable those flag in initWebglFlags.

Signed-off-by: codewithvk <vivek.javiya@collabora.com>
Change-Id: I92d73ba079d29fa05c5077d06c5d64b9480ddd7c

browser/src/slideshow/transition/SimpleTransition.ts
@@ -32,6 +32,9 @@ class SimpleTransition extends SlideShow.Transition3d {
 	}
 
 	public initWebglFlags(): void {
+		this.gl.disable(this.gl.DEPTH_TEST);
+		this.gl.disable(this.gl.CULL_FACE);
+
 		// Enable alpha blending
 		this.gl.enable(this.gl.BLEND);
 		this.gl.blendFunc(this.gl.SRC_ALPHA, this.gl.ONE_MINUS_SRC_ALPHA);