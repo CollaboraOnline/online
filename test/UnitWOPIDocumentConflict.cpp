6065b18bdb8239e417db85b6625ca9b74039e733
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2024-09-26 08:08:08 -0400

    wsd: test: add UnitConflictAfterTimeoutFailure
    Change-Id: I220d46fde4724b7d85c0e5148ed51ae089c7b3e6
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- test/UnitWOPIDocumentConflict.cpp
+++ test/UnitWOPIDocumentConflict.cpp
@@ -13,6 +13,7 @@
 
 #include "WOPIUploadConflictCommon.hpp"
 
+#include <ctime>
 #include <string>
 #include <memory>
 
@@ -109,7 +110,7 @@ public:
 /// When the upload times out (i.e. takes longer than *we* expected),
 /// there are really two outcomes: 1) the upload eventually was
 /// successful (this case), and the document has been updated in storage,
-/// or, 2) the upload fails (for whatever reason), and the
+/// or, 2) the upload fails (UnitConflictAfterTimeoutFailure), and the
 /// document is not updated in storage. Either way, since we timed out, we
 /// never know the result. In case #1, the modified timestamp in
 /// storage would have changed, and any subsequent upload will fail,
@@ -239,10 +240,173 @@ public:
     }
 };
 
+/// Simulate an upload timeout that precipitates a "conflict."
+/// When the upload times out (i.e. takes longer than *we* expected),
+/// there are really two outcomes: 1) the upload eventually was
+/// successful (UnitConflictAfterTimeoutSuccess), and the document
+/// has been updated in storage, or, 2) the upload fails (this case),
+/// and the document is not updated in storage. Either way, since we
+/// timed out, we never know the result. In case #1, the modified
+/// timestamp in storage would have changed, and any subsequent upload
+/// will fail, since the timestamp that we will send with subsequent uploads,
+/// which represents the expected last-modify date/time, will be invalid.
+/// This test simulates this particular scenario and verifies that
+/// we are able to recover gracefully from it.
+class UnitConflictAfterTimeoutFailure : public WopiTestServer
+{
+    STATE_ENUM(Phase, Load, WaitLoadStatus, WaitModifiedStatus, WaitUploaded, WaitDestroy)
+    _phase;
+
+    std::atomic_int _uploadAttemptCount;
+
+    static constexpr int ConnectionTimeoutSeconds = 1;
+
+public:
+    UnitConflictAfterTimeoutFailure()
+        : WopiTestServer("UnitConflictAfterTimeoutFailure")
+        , _phase(Phase::Load)
+        , _uploadAttemptCount(0)
+    {
+    }
+
+    void configure(Poco::Util::LayeredConfiguration& config) override
+    {
+        WopiTestServer::configure(config);
+
+        // We intentionally fail uploading twice, so need at least 3 tries.
+        config.setUInt("per_document.limit_store_failures", 3);
+        config.setBool("per_document.always_save_on_exit", false);
+        config.setUInt("net.connection_timeout_secs", ConnectionTimeoutSeconds); // Timeout quickly.
+        config.setUInt("per_document.min_time_between_uploads_ms", 100); // Short retry interval.
+    }
+
+    std::unique_ptr<http::Response>
+    assertPutFileRequest(const Poco::Net::HTTPRequest& request) override
+    {
+        LOG_TST("PutFile: " << _uploadAttemptCount << ", Phase: " << name(_phase));
+
+        const std::string wopiTimestamp = request.get("X-COOL-WOPI-Timestamp", std::string());
+        LOK_ASSERT_MESSAGE("Unexpected forced upload", !wopiTimestamp.empty());
+
+        // First attempt, timeout.
+        LOG_TST("PutFile: sleeping");
+        sleep(ConnectionTimeoutSeconds); // Connection timeout.
+        usleep(300'000); // Go over, to be certain we timed-out the upload.
+        // Don't sleep for 2 x ConnectionTimeoutSeconds, as the
+        // CheckFileInfo request may timeout, since we're stuck here.
+
+        // Simulate a conflict. Make the size different, otherwise
+        // we can't yet detect same-size changes.
+        // We do this after the first upload attempt (which will timeout)
+        // because otherwise we would've not reached here and returned
+        // 'conflict' sooner, in WopiTestServer::handleWopiUpload().
+        LOG_TST("Simulating conflict in storage");
+        setFileContent("Modified data in storage");
+
+        // Fail.
+        LOG_TST("PutFile: returning a bogus failure");
+        return std::make_unique<http::Response>(http::StatusCode::InternalServerError);
+    }
+
+    /// The document is loaded.
+    bool onDocumentLoaded(const std::string& message) override
+    {
+        LOG_TST("Got: [" << message << ']');
+        LOK_ASSERT_STATE(_phase, Phase::WaitLoadStatus);
+
+        TRANSITION_STATE(_phase, Phase::WaitModifiedStatus);
+        WSD_CMD("key type=input char=97 key=0");
+        WSD_CMD("key type=up char=0 key=512");
+
+        return true;
+    }
+
+    /// The document is modified. Save it.
+    bool onDocumentModified(const std::string& message) override
+    {
+        LOG_TST("Got: [" << message << ']');
+        LOK_ASSERT_STATE(_phase, Phase::WaitModifiedStatus);
+
+        TRANSITION_STATE(_phase, Phase::WaitUploaded);
+        WSD_CMD("save dontTerminateEdit=0 dontSaveIfUnmodified=0");
+
+        return true;
+    }
+
+    void onDocumentUploaded(bool success) override
+    {
+        LOG_TST("Uploaded #" << _uploadAttemptCount << ": " << (success ? "success" : "failure"));
+        LOK_ASSERT_STATE(_phase, Phase::WaitUploaded);
+        LOK_ASSERT_MESSAGE("Expected Phase::WaitUploaded or Phase::WaitDestroy",
+                           _phase == Phase::WaitUploaded || _phase == Phase::WaitDestroy);
+        // LOK_ASSERT_EQUAL_MESSAGE("Expected the first upload to fail", _uploadAttemptCount == 1,
+        //                          !success);
+    }
+
+    bool onDocumentError(const std::string& message) override
+    {
+        LOG_TST("Testing " << name(_phase) << ": [" << message << ']');
+        // LOK_ASSERT_STATE(_phase, Phase::WaitDocClose);
+
+        ++_uploadAttemptCount;
+        LOG_TST("uploadAttemptCount: " << _uploadAttemptCount);
+        if (_uploadAttemptCount == 1)
+        {
+            LOK_ASSERT_EQUAL_MESSAGE("Expect only documentconflict errors",
+                                     std::string("error: cmd=storage kind=savefailed"), message);
+        }
+        else
+        {
+            LOK_ASSERT_EQUAL_MESSAGE("Expect only documentconflict errors",
+                                     std::string("error: cmd=storage kind=documentconflict"),
+                                     message);
+
+            // We are done!
+            TRANSITION_STATE(_phase, Phase::WaitDestroy);
+
+            LOG_TST("Discarding own changes via closedocument");
+            WSD_CMD("closedocument");
+        }
+
+        return true;
+    }
+
+    // Wait for clean unloading.
+    void onDocBrokerDestroy(const std::string& docKey) override
+    {
+        LOG_TST("Destroyed dockey [" << docKey << ']');
+        LOK_ASSERT_STATE(_phase, Phase::WaitDestroy);
+
+        passTest("Document uploaded on closing as expected.");
+    }
+
+    void invokeWSDTest() override
+    {
+        switch (_phase)
+        {
+            case Phase::Load:
+            {
+                TRANSITION_STATE(_phase, Phase::WaitLoadStatus);
+
+                LOG_TST("Load: initWebsocket.");
+                initWebsocket("/wopi/files/0?access_token=anything");
+
+                WSD_CMD("load url=" + getWopiSrc());
+                break;
+            }
+            case Phase::WaitLoadStatus:
+            case Phase::WaitModifiedStatus:
+            case Phase::WaitUploaded:
+            case Phase::WaitDestroy:
+                break;
+        }
+    }
+};
+
 UnitBase** unit_create_wsd_multi(void)
 {
-    return new UnitBase*[3]{ new UnitWOPIDocumentConflict(), new UnitConflictAfterTimeoutSuccess(),
-                             nullptr };
+    return new UnitBase*[4]{ new UnitWOPIDocumentConflict(), new UnitConflictAfterTimeoutSuccess(),
+                             new UnitConflictAfterTimeoutFailure(), nullptr };
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
