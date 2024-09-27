22b3fbb23e9f8b1b361f5907032676e24a7fad49
Szymon Kłos <szymon.klos@collabora.com>
2024-09-27 17:08:48 +0200

    slideshow: autostart in window
    If we use presentation with autostart (.pps) then
    fullscreen request fails as it is not invoked by the
    user interaction. For now do the fallback -> open in
    window. If user has disabled popup, he will be asked
    in the message box to allow the popup.
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I9e8313f697e30fb25fa3d98b3f4a30b7df433a3e

--- browser/src/slideshow/SlideShowPresenter.ts
+++ browser/src/slideshow/SlideShowPresenter.ts
@@ -520,8 +520,8 @@ class SlideShowPresenter {
 	}
 
 	_doFallbackPresentation() {
-		// TODO: fallback to "open in new tab"
 		this._stopFullScreen();
+		this._doInWindowPresentation();
 	}
 
 	_doInWindowPresentation() {
@@ -734,6 +734,11 @@ class SlideShowPresenter {
 
 		const numberOfSlides = this._getSlidesCount();
 		if (numberOfSlides === 0) return;
+
+		if (!this.getCanvas()) {
+			console.debug('onSlideShowInfo: no canvas available');
+			return;
+		}
 
 		let skipTransition = false;
 
