805ac181b3001013fd0023f9c6a3b0d1e26e7e68
Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>
2024-10-09 19:52:32 +0530

    Refactor SaveState logic by extracting save icon control to Control.SaveState.ts
    - Moved icon-related methods (rotation, state changes) to a new file `Control.SaveState.ts`.
    - Replaced repetitive code in `Control.SaveState.ts` with calls to the new icon control methods.
    - set saveState for both mode (Classic/Notebookbar) based on user UI.
    - Cleaned up and streamlined logic for managing save state and icon animations.
    
    Signed-off-by: Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>
    Change-Id: I5b1d271789ad7f7a900f88ebc1d945948f9fb34c

---
+++ browser/src/control/Control.SaveState.ts
@@ -0,0 +1,72 @@
+/* -*- js-indent-level: 8 -*- */
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*
+This file defines the SaveState class, which handles the logic for managing the document's save state.
+It controls the display of the saving status, saved status, and triggers the associated animations and icon changes.
+*/
+
+class SaveState {
+	map: any;
+	saveEle: HTMLElement;
+	saveIconEl: HTMLElement;
+
+	// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+	constructor(map: any) {
+		this.map = map;
+		this.saveEle = document.getElementById('save');
+		this.saveIconEl = document.querySelector('#save img');
+	}
+
+	// Function to show the saving status
+	showSavingStatus(): void {
+		if (window.mode.isMobile()) return;
+
+		// Only do saving animation if any content is modified in document
+		if (this.saveEle.classList.contains('savemodified')) {
+			this.saveEle.classList.remove('savemodified');
+			// Dynamically set the content string for saving state
+			const savingText = _('Saving');
+			this.saveEle.style.setProperty('--save-state', `"${savingText}"`);
+			this.saveEle.classList.add('saving');
+			this.saveIconEl.classList.add('rotate-icon'); // Start the icon rotation
+			this.saveEle.setAttribute('disabled', 'true'); // Disable the button
+		}
+	}
+
+	// Function to show the saved status
+	showSavedStatus(): void {
+		if (window.mode.isMobile()) return;
+		if (!this.saveEle.classList.contains('savemodified')) {
+			this.saveEle.classList.remove('saving');
+			this.saveIconEl.classList.remove('rotate-icon'); // Stop the icon rotation
+			// Dynamically set the content string for saved state
+			const savedText = _('Saved');
+			this.saveEle.style.setProperty('--save-state', `"${savedText}"`);
+			this.saveEle.classList.add('saved');
+			// Add some delay to show "saved" status, then hide this info
+			setTimeout(() => {
+				this.saveEle.classList.remove('saved');
+				this.saveEle.removeAttribute('disabled'); // Enable the button
+			}, 2000);
+		}
+	}
+
+	showModifiedStatus(): void {
+		this.saveEle.classList.remove('saving');
+		this.saveEle.classList.remove('saved');
+		this.saveIconEl.classList.remove('rotate-icon'); // Stop the icon rotation
+		this.saveEle.removeAttribute('disabled'); // Enable the button
+		this.saveEle.classList.add('savemodified');
+	}
+}
+
+app.definitions.saveState = SaveState;
