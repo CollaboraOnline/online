ef4fe6ac5b7f6276baea329bbc0409179910426d
Jan Holesovsky <kendy@collabora.com>
2024-10-14 14:10:51 +0200

    coda-m: Initial macOS app
    Introduces the Xcode project, README, ./configure changes, and various
    build fixes all around the place.
    
    Signed-off-by: Jan Holesovsky <kendy@collabora.com>
    Change-Id: Idbb4529bb842c7dc4b591de0f6d3943ca70dba33

---
+++ macos/coda/coda/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  coda
+//
+//  Created by Jan Holešovský on 14.10.2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
