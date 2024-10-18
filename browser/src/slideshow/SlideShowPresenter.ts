CollaboraOnline/online

commit a9985a414f9f60299e7ea42e8cee2e0646ff6a25

cypress: run new slideshow in iframe
iframe has full screen size and is on top of all elements
default black background makes us sure we render something

Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
Change-Id: Ia5c1e3220444eee337bac7ab8348221ff2d0786b

browser/src/slideshow/SlideShowPresenter.ts
@@ -122,8 +122,11 @@ class SlideShowPresenter {
 	private _metaPresentation: MetaPresentation;
 	private _startSlide: number;
 	private _presentationInfoChanged: boolean = false;
+	private _cypressSVGPresentationTest: boolean = false;
 
 	constructor(map: any) {
+		this._cypressSVGPresentationTest =
+			L.Browser.cypressTest || 'Cypress' in window;
 		this._map = map;
 		this._init();
 		this.addHooks();
@@ -173,7 +176,7 @@ class SlideShowPresenter {
 	}
 
 	public isFullscreen() {
-		return !!this._fullscreen;
+		return !!this._fullscreen || !this._cypressSVGPresentationTest;
 	}
 
 	public getCanvas(): HTMLCanvasElement {
@@ -525,12 +528,30 @@ class SlideShowPresenter {
 		this._doInWindowPresentation();
 	}
 
+	_getProxyDocumentNode() {
+		if (this._cypressSVGPresentationTest)
+			return (this._slideShowWindowProxy as any as HTMLIFrameElement)
+				.contentWindow.document;
+		else this._getProxyDocumentNode();
+	}
+
 	_doInWindowPresentation() {
 		const popupTitle =
 			_('Windowed Presentation: ') + this._map['wopi'].BaseFileName;
 		const htmlContent = this._generateSlideWindowHtml(popupTitle);
 
-		this._slideShowWindowProxy = window.open('', '_blank', 'popup');
+		if (this._cypressSVGPresentationTest) {
+			this._slideShowWindowProxy = L.DomUtil.createWithId(
+				'iframe',
+				'slideshow-cypress-iframe',
+				document.body,
+			);
+			this._getProxyDocumentNode().open();
+			this._getProxyDocumentNode().write('<html><body></body></html>');
+			this._getProxyDocumentNode().close();
+		} else {
+			this._slideShowWindowProxy = window.open('', '_blank', 'popup');
+		}
 
 		if (!this._slideShowWindowProxy) {
 			this._map.uiManager.showInfoModal(
@@ -547,18 +568,17 @@ class SlideShowPresenter {
 			return;
 		}
 
-		this._slideShowWindowProxy.document.documentElement.innerHTML = htmlContent;
-		this._slideShowWindowProxy.document.close();
+		this._getProxyDocumentNode().documentElement.innerHTML = htmlContent;
+		this._getProxyDocumentNode().close();
 		this._slideShowWindowProxy.focus();
 
 		// set body styles
-		this._slideShowWindowProxy.document.body.style.margin = '0';
-		this._slideShowWindowProxy.document.body.style.padding = '0';
-		this._slideShowWindowProxy.document.body.style.height = '100%';
-		this._slideShowWindowProxy.document.body.style.overflow = 'hidden';
+		this._getProxyDocumentNode().body.style.margin = '0';
+		this._getProxyDocumentNode().body.style.padding = '0';
+		this._getProxyDocumentNode().body.style.height = '100%';
+		this._getProxyDocumentNode().body.style.overflow = 'hidden';
 
-		const body =
-			this._slideShowWindowProxy.document.querySelector('#root-in-window');
+		const body = this._getProxyDocumentNode().querySelector('#root-in-window');
 		this._presenterContainer = this._createPresenterHTML(
 			body,
 			window.screen.width,

browser/css/cool.css
@@ -1251,3 +1251,15 @@ nav.spreadsheet-color-indicator ~ #sidebar-dock-wrapper {
 .progress-bar::-moz-progress-bar {
 	background-color: var(--color-main-text);
 }
+
+#slideshow-cypress-iframe {
+	position: absolute;
+	top: 0;
+	left: 0;
+	bottom: 0;
+	right: 0;
+	width: 100%;
+	height: 100%;
+	z-index: 1000000;
+	background-color: black;
+}

cypress_test/integration_tests/desktop/impress/slideshow_spec.js
@@ -3,6 +3,10 @@
 var helper = require('../../common/helper');
 const desktopHelper = require('../../common/desktop_helper');
 
+function getSlideShowContent() {
+	return cy.cGet('#slideshow-cypress-iframe');
+}
+
 describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'Some app', function() {
 	beforeEach(function() {
 		helper.setupAndLoadDocument('impress/slideshow.odp');
@@ -13,6 +17,7 @@ describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'Some app', function() {
 	});
 
 	it('Should see an empty slideshow', function() {
-		cy.compareSnapshot('slideshow');
+		cy.wait(2000); // wait for slideshow to load
+		getSlideShowContent().compareSnapshot('slideshow', 0.05);
 	});
 });

commit de8d261cce5ce7318213fd8062b50671520fc1c6

impress: fix selection of objects
it fixes regression introduced in commit a9985a414f9f60299e7ea42e8cee2e0646ff6a25
cypress: run new slideshow in iframe

Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
Change-Id: I450c1769710e603e4faf05e6594771a8eb96f023

browser/src/slideshow/SlideShowPresenter.ts
@@ -176,7 +176,8 @@ class SlideShowPresenter {
 	}
 
 	public isFullscreen() {
-		return !!this._fullscreen || !this._cypressSVGPresentationTest;
+		if (this._cypressSVGPresentationTest) return false;
+		return !!this._fullscreen;
 	}
 
 	public getCanvas(): HTMLCanvasElement {