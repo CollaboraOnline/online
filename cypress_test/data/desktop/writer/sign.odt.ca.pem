CollaboraOnline/online

commit 0c1cd8c00f88b6cedd8fdb4c4ee15abf16103cd6

cool#9992 doc sign: set the view's sign cert from the WOPI UserPrivateInfo
To be able to sign documents, at least the 'local file' WOPI provider
sets 3 keys under UserPrivateInfo to give the wsd, and then the kit
process the signing certificate.

The other end is the core side, which takes 3 keys as a view options in
initializeForRendering().

What was missing is to connect these two: this way a 'make run' session
can sign documents without hardcoding cert/key/ca PEM files anywhere.

The integration side and cypress tests are not yet done here.

Signed-off-by: Miklos Vajna <vmiklos@collabora.com>
Change-Id: I1f847349e10336c69019c7f1747208e6be954362

kit/Kit.cpp
@@ -1803,6 +1803,7 @@ std::shared_ptr<lok::Document> Document::load(const std::shared_ptr<ChildSession
     const std::string& macroSecurityLevel = session->getMacroSecurityLevel();
     const bool accessibilityState = session->getAccessibilityState();
     const std::string& userTimezone = session->getTimezone();
+    const std::string& userPrivateInfo = session->getUserPrivateInfo();
 
     if (!Util::isMobileApp())
         consistencyCheckFileExists(uri);
@@ -1969,12 +1970,13 @@ std::shared_ptr<lok::Document> Document::load(const std::shared_ptr<ChildSession
 
     std::string backgroundTheme = getDefaultBackgroundTheme(session);
 
+    // Avoid logging userPrivateInfo till it's not anonymized.
     LOG_INF("Initializing for rendering session [" << sessionId << "] on document url [" <<
-            anonymizeUrl(_url) << "] with: [" << makeRenderParams(_renderOpts, userNameAnonym, spellOnline, theme, backgroundTheme) << "].");
+            anonymizeUrl(_url) << "] with: [" << makeRenderParams(_renderOpts, userNameAnonym, spellOnline, theme, backgroundTheme, "") << "].");
 
     // initializeForRendering() should be called before
     // registerCallback(), as the previous creates a new view in Impress.
-    const std::string renderParams = makeRenderParams(_renderOpts, userName, spellOnline, theme, backgroundTheme);
+    const std::string renderParams = makeRenderParams(_renderOpts, userName, spellOnline, theme, backgroundTheme, userPrivateInfo);
 
     _loKitDocument->initializeForRendering(renderParams.c_str());
 
@@ -2118,7 +2120,8 @@ Object::Ptr makePropertyValue(const std::string& type, const T& val)
 
 /* static */ std::string Document::makeRenderParams(const std::string& renderOpts, const std::string& userName,
                                                     const std::string& spellOnline, const std::string& theme,
-                                                    const std::string& backgroundTheme)
+                                                    const std::string& backgroundTheme,
+                                                    const std::string& userPrivateInfo)
 {
     Object::Ptr renderOptsObj;
 
@@ -2134,11 +2137,43 @@ Object::Ptr makePropertyValue(const std::string& type, const T& val)
         renderOptsObj = new Object();
     }
 
+    Object::Ptr userPrivateInfoObj;
+    if (!userPrivateInfo.empty())
+    {
+        Parser parser;
+        Poco::Dynamic::Var var = parser.parse(userPrivateInfo);
+        userPrivateInfoObj = var.extract<Object::Ptr>();
+    }
+    else
+    {
+        userPrivateInfoObj = new Object();
+    }
+
     // Append name of the user, if any, who opened the document to rendering options
     if (!userName.empty())
     {
         // userName must be decoded already.
         renderOptsObj->set(".uno:Author", makePropertyValue("string", userName));
+    }
+
+    // Extract settings relevant as view options from userPrivateInfo.
+    std::string signatureCert;
+    JsonUtil::findJSONValue(userPrivateInfoObj, "SignatureCert", signatureCert);
+    if (!signatureCert.empty())
+    {
+        renderOptsObj->set(".uno:SignatureCert", makePropertyValue("string", signatureCert));
+    }
+    std::string signatureKey;
+    JsonUtil::findJSONValue(userPrivateInfoObj, "SignatureKey", signatureKey);
+    if (!signatureKey.empty())
+    {
+        renderOptsObj->set(".uno:SignatureKey", makePropertyValue("string", signatureKey));
+    }
+    std::string signatureCa;
+    JsonUtil::findJSONValue(userPrivateInfoObj, "SignatureCa", signatureCa);
+    if (!signatureCa.empty())
+    {
+        renderOptsObj->set(".uno:SignatureCa", makePropertyValue("string", signatureCa));
     }
 
     // By default we enable spell-checking, unless it's disabled explicitly.

kit/Kit.hpp
@@ -325,7 +325,8 @@ private:
 
     static std::string makeRenderParams(const std::string& renderOpts, const std::string& userName,
                                         const std::string& spellOnline, const std::string& theme,
-                                        const std::string& backgroundTheme);
+                                        const std::string& backgroundTheme,
+                                        const std::string& userPrivateInfo);
     bool isTileRequestInsideVisibleArea(const TileCombined& tileCombined);
 
 public:

commit 0fcd9ade696b1514e858d831816df959fc55ae34

cool#9992 doc sign: add signature creation cypress test
Fails with commit 0c1cd8c00f88b6cedd8fdb4c4ee15abf16103cd6 (cool#9992
doc sign: set the view's sign cert from the WOPI UserPrivateInfo,
2024-10-07) reverted.

Signed-off-by: Miklos Vajna <vmiklos@collabora.com>
Change-Id: Ib2320f73c7a21b11d3f237d77ba99bc35ba3ca5e

cypress_test/data/desktop/writer/sign.odt.ca.pem
@@ -1,0 +1,66 @@
+-----BEGIN CERTIFICATE-----
+MIIFsjCCA5qgAwIBAgICEAAwDQYJKoZIhvcNAQELBQAwZTELMAkGA1UEBhMCVUsx
+EDAOBgNVBAgMB0VuZ2xhbmQxHTAbBgNVBAoMFFhtbHNlY3VyaXR5IFJTQSBUZXN0
+MSUwIwYDVQQDDBxYbWxzZWN1cml0eSBSU0EgVGVzdCBSb290IENBMCAXDTI0MDkx
+NzEzMzE0MFoYDzIxMjQwODI0MTMzMTQwWjBpMQswCQYDVQQGEwJVSzEQMA4GA1UE
+CAwHRW5nbGFuZDEdMBsGA1UECgwUWG1sc2VjdXJpdHkgUlNBIFRlc3QxKTAnBgNV
+BAMMIFhtbHNlY3VyaXR5IEludGVybWVkaWF0ZSBSb290IENBMIICIjANBgkqhkiG
+9w0BAQEFAAOCAg8AMIICCgKCAgEAtnghWw7jx1+iIjrr/oKrpStuwxR1GWkIYD9f
+imDhGtaBRYqNK23uL6vQal4M6dY2ZrH4flLF7Jc8Np1mMpiH2JaL3i4ScEmp1H4l
+LSrqzPHJuOMkRhbZ20yCyOt47Xyc/WKNHbCoApwB3+VyITjwRE5ve1K0zCKw9tfl
+C+ldoNi2zY/gguAQK4P+Qy8P/oLLWxZsFzt7RK28eZbjvZvOxgozctmOcgnmW7jE
+gKP4g0pjmov59ZzZ+6786uaEp5UPo/abUr5wbxndA1vR3GdTVRD8sfh5i2Cs28FJ
+G3qrDJJ4CoCd2IUrjNxUyz6xeGLxoV0Y8sUFYZmElWX+7UCpCG+h/C9BmiqUTiXO
+VydhvOikxPkIB/Oz6K2V+qw3jvZbbjJzn211Q2s+3tdpzPRvf4cgJuV5pe/POdlB
+OMjIGH2941oL6J31uggtsu+VpH2V4AMzP153mQKvQz0lfniLmEiKyo/gj/bIvgPi
+CPeK/K2Ecw/7ENcctmS3dqVQxzq1UYY5HPStYo0b9PYaWcN5WYd5shuJRkybbmxH
+lM7LA0UlumX1BVcso/EZMzemxZukdJi7wdMIed1CpToKTEVTLh9ud6t0pABHIXfY
+3uENUgXBC1jlrYdBBsztcgzGHcDg5lOP99WG6TJ7054MUpKWDR2pnrbBHr2wJIHo
+z9YNWkcCAwEAAaNmMGQwHQYDVR0OBBYEFH5S3KreTZBZfTdUzSh2RRTgStrPMB8G
+A1UdIwQYMBaAFDnqTk/yxXNXY2PmQMOUzDzZi1yEMBIGA1UdEwEB/wQIMAYBAf8C
+AQAwDgYDVR0PAQH/BAQDAgGGMA0GCSqGSIb3DQEBCwUAA4ICAQARxv1n1FH+kYGm
+Wi73R6tUCpPDQVvXi9bvedkPVWD/9y0TyZRXF0rUb94sqx4uDvwZ2NiWJYIzMvcc
+EgtBbm0YmEeGsUOuHkuTs/fcSJD1sUIHEkp9I7JudQd+4NA1wzVIGFFbFI62ZCKW
+NzJLb9FIk30GkY4pmr9PZ/wfXmwmvPT6YO7QBdsfDdmGsj0Hd/VfOV/S+sLgChrn
+BAh6cMq4CIfwQ7NOwmlqULJVZq1nr7/absC8yZDbqiIzP9DPFhlgZ16TW2sPXCY7
+p1dREQKiV66D5HQ8bHALiSyK7oC3TkLMXZxTXcAxVquj6Gv+bOoS9tVyKVNPKe9R
+Sr8P7ckFBe039C+oTeVWLg0P3uYRybe0ncj8hlatnlNBUKom0DKJz5Prg4gmyp19
+QIUlohfag6SQaLEXekQvhpYqIfKupGMLHxdLdqYwWKiRqjg9sRmx7IiGVAWFTl53
+gmAuByZ8DjR/p46AcOQS/aEwn5BhU0RlSqRmUi3CKqsxsn19myWa/rKuu03jIp+g
+KtnsMjBj3u6NhQ8A9qM+KSH8YjEww2I8lQebobQLXq8KiJPF69iGuj2hKu97eesu
+iW1RmSvKbjuV5bWBznTU/3rXsqnJefvRhTykoi1a3Mfasr53ALTCWrmAjiNeNdhY
+INQ8Kfho4bUqnJNlVx385VKlOlgSSA==
+-----END CERTIFICATE-----
+-----BEGIN CERTIFICATE-----
+MIIFvTCCA6WgAwIBAgIUXdGgUlCqOYLzxrCUCpxSL06qQeAwDQYJKoZIhvcNAQEL
+BQAwZTELMAkGA1UEBhMCVUsxEDAOBgNVBAgMB0VuZ2xhbmQxHTAbBgNVBAoMFFht
+bHNlY3VyaXR5IFJTQSBUZXN0MSUwIwYDVQQDDBxYbWxzZWN1cml0eSBSU0EgVGVz
+dCBSb290IENBMCAXDTI0MDkxNzEzMzEzOVoYDzIxMjQwODI0MTMzMTM5WjBlMQsw
+CQYDVQQGEwJVSzEQMA4GA1UECAwHRW5nbGFuZDEdMBsGA1UECgwUWG1sc2VjdXJp
+dHkgUlNBIFRlc3QxJTAjBgNVBAMMHFhtbHNlY3VyaXR5IFJTQSBUZXN0IFJvb3Qg
+Q0EwggIiMA0GCSqGSIb3DQEBAQUAA4ICDwAwggIKAoICAQCyLDW5DVS2TgG2IlNE
+f3M6ZW+ZSCsr9AM4dTQYLnOXyIFPtjv5VHsMR2+wbi9NidhODfbIIrMTZSEw/AfB
+xMjwEl03hzAC+OCxHceGi1XDrfXqTgUz5AM+veuAq29tZ+tQ1vixVf+vbzw23aVu
+DcUOR+uCVLTnthtuzZl4Mm0lOKr6t9XNx7Cl8+gGoI/Ho1jKq/3yvwLVzLhh9bil
+Vgvo8qQOX3k1+gt2LU6Rc4UFXuNBr7OEX6p/+Yf4VwvibEi3dghDG3au+JTHWxIg
+k+3exNYyWKAp6RkKoW2DcZRYX4WCSwxmewpdRbMu41jpKq75NR+8aVPcOyfPEjV1
+gBm/flbdPnGLmXCzV6XvWl4bEI1Z6Xz7DxwMe+Z/qj1G7o9iy6aUZC6LHECH9myx
+LBEjJpU/6f5eXSi2Xs3eYi4T6gAS9KCa4gwxGNyt7oCtsfWGyIWArhnFKNbTyPtW
++OHDxs9RWYS8Hbb2SSHnf+NMCTfsGIPBVOh0XLA+AMHu6ex635eQ8qKzvLwu3zdk
+pU7LWQjmUqfCrneRoLA7b2US/ZTkOAwbZ3UJnN/BFxMaNRKnMcaeZsLYsZQ/DeJF
+Gk65vYhXZWsYG8PjxedVFDuzJZZ3eC4hvZwnx3mDf09RrGPhJHxBeZXVr/fsGXzK
+rNBNGMf22X438aoDL7+77azvGwIDAQABo2MwYTAdBgNVHQ4EFgQUOepOT/LFc1dj
+Y+ZAw5TMPNmLXIQwHwYDVR0jBBgwFoAUOepOT/LFc1djY+ZAw5TMPNmLXIQwDwYD
+VR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMCAYYwDQYJKoZIhvcNAQELBQADggIB
+AK70Z5Gi4/93tbOygDc/ZuK/j+kPNYS9A2cxvoa6zBqnJC1ZotWsIv92S2osJV1a
+vzdMsQwlDkPXwXPEQRSAH8CUd5dtZjSP7/dsPBzeu3ryVVsh9hR7wAfW4emQ+Igw
+0zm4C0pQr3Cy4skJ8/jX2kTbHORUvQ6/60gilIs2T6K8VRlKXBy5le4VObWz2+U0
+H7ztp08JRYfJ7xqqknF8Bv37/YIV/sSoQHIIHCgYzDZRXsvmGqDNjQTNodd6anjW
+3bTyh+VHIcaHQPxDtt5tsv+3CIPyEFhvP62hbuNUfZgrNCPvW3FCHPfhoPldwAaq
+NZsXutbmpy0GI25tbJHp9bWKSc+w6upf1RSPlfFLTjNdsVhbvwb5tMgXr9rq0yHd
+j2Jfa+nvGc85CFRXYCvEJ14ooxJEiahl1//JtCqLXpbtmGOW5whzBY9Jg+XUEpWj
+I6Pgf7+bJgeB2YDs7b9ELSG8Gc76Jain+OvmBtFFmeVKvOeZiAznd37Q9HGHnUI4
+16T6AUbyW6rOgUNqjD+bxGKkeBtYnieJpk7aMHh+DR4rkG7qfgcdzlaw5nEXZMbN
+J5v1HNyN10JZRTWHAZUHkNJMf5/EruWTIOTQY8R2/ElGNVyVGo/4krXMdaOprUF6
+v3SgfBSAsfz08wjEc48NAmrQc+QIKB40sXquOAIm8FCt
+-----END CERTIFICATE-----

cypress_test/data/desktop/writer/sign.odt.cert.pem
@@ -1,0 +1,30 @@
+-----BEGIN CERTIFICATE-----
+MIIFGDCCAwCgAwIBAgICEAAwDQYJKoZIhvcNAQELBQAwaTELMAkGA1UEBhMCVUsx
+EDAOBgNVBAgMB0VuZ2xhbmQxHTAbBgNVBAoMFFhtbHNlY3VyaXR5IFJTQSBUZXN0
+MSkwJwYDVQQDDCBYbWxzZWN1cml0eSBJbnRlcm1lZGlhdGUgUm9vdCBDQTAgFw0y
+NDA5MTcxMzMxNDBaGA8yMTI0MDgyNDEzMzE0MFowazELMAkGA1UEBhMCVUsxEDAO
+BgNVBAgMB0VuZ2xhbmQxHTAbBgNVBAoMFFhtbHNlY3VyaXR5IFJTQSBUZXN0MSsw
+KQYDVQQDDCJYbWxzZWN1cml0eSBSU0EgVGVzdCBleGFtcGxlIEFsaWNlMIIBIjAN
+BgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArX9UOmmDl/hKdZhVOl8fjBQ/z5tD
+a+6eKpTebPNTxxyAGXcDokXDzXAThvnFa5vf/jgTi1Cg21EhirqCIqsqb19Hs0aT
+6M73H8urFJXifr1+M+anw67IVqyN/1IUSGOpkQl19rwGMcYYbFV7DnglA7RloJH2
++PnpX8laTw8tPtYxM2B3W/R6CT8ashW2KI7urmbGrMADEGx1DaVf6YbMJ7tzydV2
+ALAd02n6xwWRQobXXWy9mvwunVGX1V50obucCLo0iqpRC2pKXkMeFGtF1sirOMAM
+zc4+ZAQ04ewYgmvK9DZ9TzWXHWeY1Cmr0wWD0Lx6FZGMHBKOoNEc+vHY2QIDAQAB
+o4HFMIHCMAkGA1UdEwQCMAAwEQYJYIZIAYb4QgEBBAQDAgWgMDMGCWCGSAGG+EIB
+DQQmFiRPcGVuU1NMIEdlbmVyYXRlZCBDbGllbnQgQ2VydGlmaWNhdGUwHQYDVR0O
+BBYEFL0TjqGQImB9jhrjGjyysmsWo4QHMB8GA1UdIwQYMBaAFH5S3KreTZBZfTdU
+zSh2RRTgStrPMA4GA1UdDwEB/wQEAwIF4DAdBgNVHSUEFjAUBggrBgEFBQcDAgYI
+KwYBBQUHAwQwDQYJKoZIhvcNAQELBQADggIBAKH+tBMXA7um1t/o1vmVPDjUYLkd
+GxC4tcqoPx4gVe04Nddtz/EdqYQsKlwEZc6wwQAJpU/87jOJ23gcWKwoLkegQVUu
+5fll/aQcEI05y0Gw/cq5V1o72QiSwmALdakG2CyMc9IAKyjTlXm9gN3MM4edC6Mp
+SVgQ+BNGNoMuJDIVxg+eKkPWTk4qcSN0ie7Qgin+MEerzSY8m1r1SAlLFWFktebe
++olhJcqlsCKswX9B6Sqpctp20ziPHMPS2+9AxhnhLAgty1XqtKSyGGcL28QOrWWC
+Q/FDOftuhFPchvXmrtRlLYgWR/F2ok//TOtaMFdvco5zkrUh/t+vmEg1JQcem6tB
+HqhBDWS+CQeDuEQhfDmUBGuz4TNyit44jsEfLfNH4R6cqxcF9oon260U4lu3k7AR
+pz5ElYJEYOEctCn+tQ/Ov6p/5QSxFyB+ncPc12XTMt9/wOC6prjOTQTEq7tDmID/
+yvmKBLkukkKTFPq/k1TO3EcaZXAzfBpAA6MINrUjFFknQ1AzysETGtMqSm8uEfkI
+uXF+A9e/Yj/wz/hTJDpG9bdVSsNeKQAcBuXz+pPeXfgdqZB8EfCwlV4CouD0N28z
+kAmg/UcvYkvjSBWJigjKMY5HpuQu76J8IE4nhorodOBttc569lgORprVASxsqYBz
+BY67LQpLHFPueyi8
+-----END CERTIFICATE-----

cypress_test/data/desktop/writer/sign.odt.key.pem
@@ -1,0 +1,28 @@
+-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCtf1Q6aYOX+Ep1
+mFU6Xx+MFD/Pm0Nr7p4qlN5s81PHHIAZdwOiRcPNcBOG+cVrm9/+OBOLUKDbUSGK
+uoIiqypvX0ezRpPozvcfy6sUleJ+vX4z5qfDrshWrI3/UhRIY6mRCXX2vAYxxhhs
+VXsOeCUDtGWgkfb4+elfyVpPDy0+1jEzYHdb9HoJPxqyFbYoju6uZsaswAMQbHUN
+pV/phswnu3PJ1XYAsB3TafrHBZFChtddbL2a/C6dUZfVXnShu5wIujSKqlELakpe
+Qx4Ua0XWyKs4wAzNzj5kBDTh7BiCa8r0Nn1PNZcdZ5jUKavTBYPQvHoVkYwcEo6g
+0Rz68djZAgMBAAECggEAAIhG5iH7lHcKJXJjn+OkVA3zhIEjCRJTm75WsLS4wark
+TWUIgWeKOle5G51/E1LdslOSOHJ1d+Qia0uiCWtchzhz3siH53uLwGYj1ChB6GZ6
+RrxTBfOZQO4pG7WGWJLZCu1MQRxNFDsvHn/5pMROGsTjg0VctwwElmBmsvORODjh
+4iq7il/x3Y9H3/sCjjuho+Qw/vrvQWBqQsSuz7fo/nRy/TrNSfbN76ShvNa6ug2o
+cMaGfSdNYcUf36kDB6q3xvKfWl0CpXGHfK9CX7SmBUFpBB/0fewLackWjsfQY28F
+Tyb/AC0zQF8HF1Y7b/BeXEWxntEa03Lar88iAFIxzQKBgQDxL/0X62DvtksoJoig
+hObsHV8F82jI/n9mGl3uU5l8kY9xDdm5wy3NkR93XMH4tdxde6Pl6vT33MLFcjY4
+mQ2ngnFTTmSPeftuuHxL/8KdJ3H5kyf/YqPJikWEMuywNv0OBKGcP8ZcFgFs1ufa
+Vg6K6kbjtdpN9trGet/D7JDK7QKBgQC4Jxl7GWMsvsS2seQItH/AH7nrJ411TCpq
+stjLW+5QSfbS3wwn5z5UW3Rb/IAycoB4sY5eFzQLsWDpyhsnwUhIw08uRrzHdkMh
+ZWIrxej7Im44BVNeT6Q/YmPBuC3ER4RIpqA/lKR5bRqup6dRUsZs5ES1qFB4UmVV
+yQYQp27MHQKBgQDK1SvJEOdN0QW1t2pRbFNzoGPIcmLeHcFwUZ5LDr1W2GdEkXxY
+T/9WEMTgsf5itjaj25YDhqwLQvn96MYWpQippfa+M4A4dUJxY+8MTwQQ/BpK5GnP
+ydmqZYF+4wDQVP5N6rEq4StlSBBcwwLrSFIkm0+Qst/rjz1/i9hhQbKnJQKBgGs2
+QEx9xFKJoicnPDahN3sYPXqdpWqz9EH4a+VBWWafGTc9qmsU4yHjuMPRJqW4Kfma
+jeNTqPZhx6V1CAhGLBpIszGi90c9H+oyh0wqxfBn5DlzSE0xGtPYKXHgE60hYe+W
+Kte5Z2RWU+F5NB1RjrpYz3PfUTIr7llaVtCdWhE1AoGAZJbJ5QOKOqjldWmnqEy1
+l14lOWJL+YNZmQjb5NVtWmd8ETEJqGNoOvna2rU/y1ufWThWq53xbcdz/vT0JiCh
+4sDlOMMjv0d68sx1OpjEMLxeRpqwVB3RytoqQ0a276NoeNfhQAfonD/ZvRxbZ9up
+qe/Rs65V+rz3ByW3ACzhbok=
+-----END PRIVATE KEY-----

cypress_test/integration_tests/common/helper.js
@@ -6,7 +6,7 @@
  * filePath: test document file path
  * returns new test document file path
  */
-function setupDocument(filePath) {
+function setupDocument(filePath, copyCertificates = false) {
 	cy.log('>> setupDocument - start');
 	cy.log('Param - filePath: ' + filePath);
 
@@ -33,6 +33,11 @@ function setupDocument(filePath) {
 		}
 
 		copyFile(filePath, newFilePath);
+		if (copyCertificates) {
+			for (const suffix of ['.cert.pem', '.key.pem', '.ca.pem']) {
+				copyFile(filePath + suffix, newFilePath + suffix);
+			}
+		}
 	}
 
 	cy.log('<< setupDocument - end');
@@ -101,10 +106,10 @@ function loadDocument(filePath, skipDocumentChecks, isMultiUser) {
  * call setupDocument and loadDocument directly
  * filePath: test document path, for example: 'calc/hello-world.ods'
  */
-function setupAndLoadDocument(filePath, isMultiUser = false) {
+function setupAndLoadDocument(filePath, isMultiUser = false, copyCertificates = false) {
 	cy.log('>> setupAndLoadDocument - start');
 
-	var newFilePath = setupDocument(filePath);
+	var newFilePath = setupDocument(filePath, copyCertificates);
 	if (isMultiUser) {
 		loadDocument(newFilePath, undefined, isMultiUser);
 	} else {

cypress_test/integration_tests/desktop/writer/sign_spec.js
@@ -1,0 +1,27 @@
+/* global describe it cy require */
+
+var helper = require('../../common/helper');
+var desktopHelper = require('../../common/desktop_helper');
+
+describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'Sign operations.', function() {
+
+	it('Create a signature.', function() {
+		// Given a document that can be signed:
+		helper.setupAndLoadDocument('writer/sign.odt', /*isMultiUser=*/false, /*copyCertificates=*/true);
+		desktopHelper.switchUIToNotebookbar();
+
+		// When signing that document:
+		cy.cGet('#File-tab-label').click();
+		cy.cGet('#signature-button').click();
+		// Without the accompanying fix in place, this test would have failed with:
+		// This element `<button#sign.ui-pushbutton.jsdialog.hidden>` is not visible because it has CSS property: `display: none`
+		cy.cGet('#DigitalSignaturesDialog button#sign').click();
+		// Click on the only signature, not on the header:
+		cy.cGet('#SelectCertificateDialog table#signatures tr.ui-listview-entry').click();
+		cy.cGet('#SelectCertificateDialog button#ok').click();
+		cy.cGet('#DigitalSignaturesDialog button#close').click();
+
+		// Then make sure the resulting signature is valid:
+		cy.cGet('#signstatus-button div').should('have.class', 'sign_ok');
+	});
+});