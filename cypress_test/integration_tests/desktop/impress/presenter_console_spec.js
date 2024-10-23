9aa81251b4f9a3da2192cdf5700e22f9f9ced0cb
Henry Castro <hcastro@collabora.com>
2024-10-23 09:29:17 -0400

    cypress: add unit test for presenter console
    Change-Id: Ie92a6900976e52380d56e4e127cad57769d19c3a
    Signed-off-by: Henry Castro <hcastro@collabora.com>

---
+++ cypress_test/integration_tests/desktop/impress/presenter_console_spec.js
@@ -0,0 +1,98 @@
+/* -*- js-indent-level: 8 -*- */
+
+/* global describe it cy require beforeEach expect */
+
+var helper = require('../../common/helper');
+
+describe(['tagdesktop'], 'Presenter Console.', function() {
+
+	beforeEach(function() {
+		helper.setupAndLoadDocument('impress/slide-bitmap-background.odp');
+	});
+
+	it('Presenter Console class', function() {
+		cy.getFrameWindow().then(function(win) {
+			var elem;
+			var fakeMap = win.L.Evented.extend({
+				getPreview: function() {
+					return {width: 50, height: 50};
+				}
+			});
+
+			var map = new fakeMap();
+
+			class FakeCompositor {
+				computeLayerResolution(width, height) {
+					return [width, height];
+				}
+				computeLayerSize(width, height) {
+					return [width, height];
+				}
+			};
+
+			class FakePresenter {
+				constructor() {
+					this._slideCompositor = new FakeCompositor();
+				}
+
+				_getSlidesCount() {
+					return 2;
+				}
+
+				getNotes() {
+					return "test notes";
+				}
+			};
+
+			var fakePresenter = new FakePresenter();
+			var fakeConsole = new win.SlideShow.PresenterConsole(map, fakePresenter);
+
+			// create popup window
+			map.fire('newpresentinconsole');
+
+			expect(fakeConsole._proxyPresenter).to.not.equal(null);
+
+			// start presentation
+			map.fire('presentationinfo');
+
+			expect(fakeConsole._previews).to.have.lengthOf(2);
+
+			{
+				// create a frame image
+				var width = 50;
+				var height = 50;
+				const offscreen = new OffscreenCanvas(width, height);
+				const ctx = offscreen.getContext('2d');
+				ctx.fillStyle = 'blue';
+				ctx.fillRect(0, 0, width, height);
+
+				map.fire('newslideshowframe', { frame: offscreen.transferToImageBitmap() });
+
+				elem = fakeConsole._proxyPresenter.document.querySelector('#current-presentation');
+				expect(elem).to.not.equal(null);
+				expect(elem).to.not.equal(undefined);
+			}
+
+			map.fire('transitionend', { slide: 0 });
+
+			elem = fakeConsole._proxyPresenter.document.querySelector('#next-presentation');
+			expect(elem).to.not.equal(null);
+			expect(elem).to.not.equal(undefined);
+
+			elem = fakeConsole._proxyPresenter.document.querySelector('#notes');
+			expect(elem).to.not.equal(null);
+			expect(elem).to.not.equal(undefined);
+
+			var smile = win.document.querySelector('meta[name="previewSmile"]').content;
+			map.fire('tilepreview', {
+				id : '2000',
+				part : 1,
+				tile: { src: smile }
+			});
+
+			expect(fakeConsole._previews[1]).to.equal(smile);
+
+			fakeConsole._proxyPresenter.close();
+		});
+	});
+});
