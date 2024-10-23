CollaboraOnline/online

commit 0579ee81c709865ea420c2d70dcc443bdf69de1a

cool#9833: Socket/WebSocketHandler Restructuring: Unify closing/shutdown. SocketPoll::poll robustness
Preparation for cool#9833.

Socket Restructuring:
- Unify closing

SocketPoll::poll robustness
- Ensure erasing Socket if !isOpen()

Signed-off-by: Sven Göthel <sven.gothel@collabora.com>
Change-Id: I7e1a9329e0848c40a210f6250e29e26950da6fbc

net/Socket.hpp
@@ -186,13 +186,15 @@ public:
     /// TODO: Support separate read/write shutdown.
     virtual void shutdown()
     {
-        if (_noShutdown)
-            return;
-        LOG_TRC("Socket shutdown RDWR.");
-        if constexpr (!Util::isMobileApp())
-            ::shutdown(_fd, SHUT_RDWR);
-        else
-            fakeSocketShutdown(_fd);
+        setClosed();
+        if (!_noShutdown)
+        {
+            LOG_TRC("Socket shutdown RDWR.");
+            if constexpr (!Util::isMobileApp())
+                ::shutdown(_fd, SHUT_RDWR);
+            else
+                fakeSocketShutdown(_fd);
+        }
     }
 
     /// Prepare our poll record; adjust @timeoutMaxMs downwards
@@ -392,6 +394,9 @@ protected:
 
     /// Explicitly marks this socket closed, i.e. rejected from polling and potentially shutdown
     void setClosed() { _open = false; }
+
+    /// Explicitly marks this socket and the given SocketDisposition closed
+    void setClosed(SocketDisposition &disposition) { setClosed(); disposition.setClosed(); }
 
 private:
     void init(Type type)
@@ -1479,11 +1484,10 @@ protected:
         if (closed)
         {
             LOG_TRC("Closed. Firing onDisconnect.");
-            setClosed();
             _socketHandler->onDisconnect();
+            setClosed(disposition);
         }
-
-        if (isClosed())
+        else if (!isOpen())
             disposition.setClosed();
     }
 

net/Socket.cpp
@@ -556,6 +556,14 @@ int SocketPoll::poll(int64_t timeoutMaxMicroS)
                 // removed in a callback
                 ++itemsErased;
             }
+            else if (!_pollSockets[i]->isOpen())
+            {
+                // closed socket ..
+                ++itemsErased;
+                LOGA_TRC(Socket, '#' << _pollFds[i].fd << ": Removing socket (at " << i
+                         << " of " << _pollSockets.size() << ") from " << _name);
+                _pollSockets[i] = nullptr;
+            }
             else if (_pollFds[i].fd == _pollSockets[i]->getFD())
             {
                 SocketDisposition disposition(_pollSockets[i]);
@@ -575,7 +583,7 @@ int SocketPoll::poll(int64_t timeoutMaxMicroS)
                     rc = -1;
                 }
 
-                if (!disposition.isContinue())
+                if (!_pollSockets[i]->isOpen() || !disposition.isContinue())
                 {
                     ++itemsErased;
                     LOGA_TRC(Socket, '#' << _pollFds[i].fd << ": Removing socket (at " << i

commit e5d86f77f626d325ccbb6abad3abe2d45f60863d

Fix regression in `Socket::shutdown()` when having `_noShutdown` set
Regression injected with commit 0579ee81c709865ea420c2d70dcc443bdf69de1a
of PR https://github.com/CollaboraOnline/online/pull/9916

Original behavior was to do nothing in Socket::shutdown(),
while the commit in question did setClosed() leading
to an eventual socket close within SocketPoll::poll().

The latter causes an error within system ::ppoll().

Signed-off-by: Sven Göthel <sven.gothel@collabora.com>
Change-Id: I57d46e0428905137e05d34dad450fea1d6a0f127

net/Socket.hpp
@@ -218,10 +218,10 @@ public:
     /// TODO: Support separate read/write shutdown.
     virtual void shutdown()
     {
-        setClosed();
         if (!_noShutdown)
         {
             LOG_TRC("Socket shutdown RDWR. " << *this);
+            setClosed();
             if constexpr (!Util::isMobileApp())
                 ::shutdown(_fd, SHUT_RDWR);
             else