59897eda738308372840a7f87b3d9f87a558622b
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2024-11-05 19:28:11 -0500

    wsd: dump the config
    This dumps the non-default config entries
    at startup as well as using SIGUSR1.
    
    Should help capture config values that
    affect the functionality and behavior
    that are relevant to troubleshooting.
    
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>
    (cherry picked from commit d7fae371dc48da681c81029343c716923c7e2260)
    
    Change-Id: I8e17d63828faae05bd613616493ae4f493760170

--- common/ConfigUtil.hpp
+++ common/ConfigUtil.hpp
@@ -17,6 +17,7 @@
 #pragma once
 
 #include <string>
+#include <map>
 
 namespace Poco
 {
@@ -36,6 +37,9 @@ void initialize(const Poco::Util::AbstractConfiguration* config);
 
 /// Check if the config has been initialized
 bool isInitialized();
+
+/// Extract all entries as key-value pairs. We use map to have the entries sorted.
+std::map<std::string, std::string> extractAll(const Poco::Util::AbstractConfiguration* config);
 
 /// Returns the value of an entry as string or @def if it is not found.
 std::string getString(const std::string& key, const std::string& def);
