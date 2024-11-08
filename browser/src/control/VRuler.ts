fe7bdfa442dc6e34fc1fa420c1936f7287bbab8f
Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>
2024-11-08 16:11:20 +0530

    Optimize VRuler performance by avoiding layout calculations when hidden
     - Avoid calculating offset if the vertical ruler is hidden.
     - Skip updating paragraph indentation when the ruler is not visible.
    
    Signed-off-by: Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>
    Change-Id: I1953d680b6e132c1d48f9b435fde8e06735ed48d

--- browser/src/control/VRuler.ts
+++ browser/src/control/VRuler.ts
@@ -100,6 +100,7 @@ class VRuler {
 			this,
 		);
 		this._map.on('commandstatechanged', this.onCommandStateChanged, this);
+		this._map.on('rulerchanged', this._onRulerChanged, this);
 		L.DomUtil.addClass(this._map.getContainer(), 'hasruler');
 
 		const container: HTMLDivElement = this._initLayout();
@@ -231,6 +232,19 @@ class VRuler {
 		return this._rWrapper;
 	}
 
+	_onRulerChanged() {
+		// update show ruler state on rulerChange event
+		this.options.showruler = this._map.uiManager.getBooleanDocTypePref(
+			'ShowRuler',
+			true,
+		);
+		if (this.options.showruler) {
+			// in case of disabled ruler at docload calculation of offset can be ignored
+			// but after enabling the ruler we need to set the offset.
+			this._fixOffset();
+		}
+	}
+
 	_updateOptions(obj: Options) {
 		// window.app.console.log('===> _updateOptions');
 		// Note that the values for margin1, margin2 and leftOffset are not in any sane
@@ -264,6 +278,8 @@ class VRuler {
 	}
 
 	_updateParagraphIndentations() {
+		// if ruler is hidden no need to calculate the indentation of the para
+		if (!this.options.showruler) return;
 		// for horizontal Ruler we need to also consider height of navigation and toolbar-wrraper
 		const documentTop: number = document
 			.getElementById('document-container')
@@ -406,7 +422,8 @@ class VRuler {
 	}
 
 	_fixOffset() {
-		if (!this._map.options.docBounds) return;
+		// in case of disabled ruler at docload or event like 'moveend' calculation of offset can be ignored
+		if (!this._map.options.docBounds || !this.options.showruler) return;
 
 		const scale: number = this._map.getZoomScale(this._map.getZoom(), 10);
 		const mapPane = this._map._mapPane;
