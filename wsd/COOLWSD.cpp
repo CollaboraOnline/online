1634e15152450660e327313945b66be75208f04c
Jan Holesovsky <kendy@collabora.com>
2024-11-11 11:34:29 +0100

    Various CODA-M commits squashed together
    Better lokit initialization path.
    
    Bundle and find coolkitconfig.xcu.
    
    Enable extra logging from the core in the DEVEL builds.
    
    Fix bootstrapping of the "/org.openoffice.System/L10N" config.
    
    Signed-off-by: Jan Holesovsky <kendy@collabora.com>
    Change-Id: I72ffe7db54768dd038ad34d4586cb7d252730304

--- wsd/COOLWSD.cpp
+++ wsd/COOLWSD.cpp
@@ -1703,6 +1703,10 @@ void COOLWSD::innerInitialize(Poco::Util::Application& self)
 
 #endif // !MOBILEAPP
 
+#if defined(MACOS) && defined(DEBUG)
+    setenv("SAL_LOG", "+INFO+WARN", 0);
+#endif
+
     int pdfResolution =
         ConfigUtil::getConfigValue<int>(conf, "per_document.pdf_resolution_dpi", 96);
     if (pdfResolution > 0)
