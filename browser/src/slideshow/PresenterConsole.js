9b9ae16e3a1144fb3cd93054cdd49b69724ba0e6
Henry Castro <hcastro@collabora.com>
2024-11-15 16:31:49 -0400

    browser: slideshow: fix interval timer
    Also it is the interval timer of the proxy window
    
    Change-Id: I21664bba0d5c66cc0c27d82283477c5bbbd2c0b6
    Signed-off-by: Henry Castro <hcastro@collabora.com>

--- browser/src/slideshow/PresenterConsole.js
+++ browser/src/slideshow/PresenterConsole.js
@@ -126,7 +126,10 @@ class PresenterConsole {
 			this._proxyPresenter.document.querySelector('#current-presentation'),
 		);
 
-		this._timer = setInterval(L.bind(this._onTimer, this), 1000);
+		this._timer = this._proxyPresenter.setInterval(
+			L.bind(this._onTimer, this),
+			1000,
+		);
 		this._ticks = 0;
 		this._pause = false;
 
@@ -768,11 +771,13 @@ class PresenterConsole {
 			'keydown',
 			L.bind(this._onKeyDown, this),
 		);
+		this._proxyPresenter.clearInterval(this._timer);
+
 		delete this._proxyPresenter;
 		delete this._currentIndex;
 		delete this._lastIndex;
 		delete this._previews;
-		clearInterval(this._timer);
+
 		this._map.off('newslideshowframe', this._onNextFrame, this);
 		this._map.off('transitionstart', this._onTransitionStart, this);
 		this._map.off('transitionend', this._onTransitionEnd, this);
