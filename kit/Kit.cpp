0529ae7a1f11a79848889ea7c371eecc9f7cb739
Caolán McNamara <caolan.mcnamara@collabora.com>
2024-11-18 09:56:38 +0000

    include configId identifier in kit announcement
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: I7ba797deebaebfbcc286d95d7e6f3331c8dfc78a

--- kit/Kit.cpp
+++ kit/Kit.cpp
@@ -3092,6 +3092,7 @@ void lokit_main(
 #if !MOBILEAPP
                 const std::string& childRoot,
                 const std::string& jailId,
+                const std::string& configId,
                 const std::string& sysTemplate,
                 const std::string& loTemplate,
                 bool noCapabilities,
@@ -3568,6 +3569,11 @@ void lokit_main(
         std::string pathAndQuery(NEW_CHILD_URI);
         pathAndQuery.append("?jailid=");
         pathAndQuery.append(jailId);
+        if (!configId.empty())
+        {
+            pathAndQuery.append("&configid=");
+            pathAndQuery.append(configId);
+        }
         if (queryVersion)
         {
             char* versionInfo = loKit->getVersionInfo();
