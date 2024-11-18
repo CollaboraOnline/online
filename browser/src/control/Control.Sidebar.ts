CollaboraOnline/online

commit b5eab2c988515c904f8c4b6c975a25e1f777e028

browser: treeview: remove global var hack
remove old hack which was used to detect treeview inside navigator...
it was based on global variable treeType === 'navigator' which shouldn't
be done with JSDialogs as the widget building functions shouln't use any
state from outside received JSON

Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
Change-Id: I733d2d5f29e26acc5442849575e120508e69108b

browser/src/control/Control.Sidebar.ts
@@ -93,6 +93,10 @@ class Sidebar {
 			return;
 		}
 
+		if (this.getTargetDeck() === this.commandForDeck('NavigatorDeck')) {
+			this.markNavigatorTreeView(data.control);
+		}
+
 		this.builder.updateWidget(this.container, data.control);
 	}
 
@@ -188,8 +192,16 @@ class Sidebar {
 				if (
 					sidebarData.children[i].type !== 'deck' ||
 					sidebarData.children[i].visible === false
-				)
+				) {
 					sidebarData.children.splice(i, 1);
+				}
+
+				if (
+					typeof sidebarData.children[i].id === 'string' &&
+					sidebarData.children[i].id.startsWith('Navigator')
+				) {
+					this.markNavigatorTreeView(sidebarData);
+				}
 			}
 
 			if (sidebarData.children.length) {
@@ -228,6 +240,23 @@ class Sidebar {
 				this.closeSidebar();
 			}
 		}
+	}
+
+	markNavigatorTreeView(data: WidgetJSON): boolean {
+		if (!data) return false;
+
+		if (data.type === 'treelistbox') {
+			(data as TreeWidget).draggable = false;
+			return true;
+		}
+
+		for (const i in data.children) {
+			if (this.markNavigatorTreeView(data.children[i])) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 }
 

browser/src/control/jsdialog/Definitions.Types.ts
@@ -132,9 +132,10 @@ interface TreeHeaderJSON {
 interface TreeWidget extends WidgetJSON {
 	text: string;
 	singleclickactivate: boolean; // activates element on single click instead of just selection
-	fireKeyEvents: boolean; // do we sent key events to core
-	hideIfEmpty: boolean; // hide the widget if no entries available
+	fireKeyEvents?: boolean; // do we sent key events to core
+	hideIfEmpty?: boolean; // hide the widget if no entries available
 	checkboxtype: string; // radio or checkbox
+	draggable?: boolean; // indicates if we can drag entries to another treeview
 	entries: Array<TreeEntryJSON>;
 	headers: Array<TreeHeaderJSON>; // header columns
 }

browser/src/control/jsdialog/Widget.TreeView.ts
@@ -61,7 +61,6 @@
 declare var JSDialog: any;
 
 // TODO: remove this hack
-var treeType = '';
 var lastClickHelperRow: string | number = -1;
 var lastClickHelperId = '';
 // TODO: remove this hack
@@ -406,7 +405,7 @@ class TreeViewControl {
 		builder: any,
 	) {
 		if (treeViewData.enabled !== false && entry.state == null) {
-			tr.draggable = treeType === 'navigator' ? false : true;
+			tr.draggable = treeViewData.draggable === false ? false : true;
 
 			tr.ondragstart = (ev) => {
 				ev.dataTransfer.setData('text', '' + entry.row);
@@ -1142,24 +1141,8 @@ JSDialog.treeView = function (
 	data: TreeWidget,
 	builder: any,
 ) {
-	// TODO: remove this hack
-	// var id = data.parent
-	// 	? data.parent.parent
-	// 		? data.parent.parent.parent
-	// 			? data.parent.parent.parent.id
-	// 				? data.parent.parent.parent.id
-	// 				: null
-	// 			: null
-	// 		: null
-	// 	: null;
-	// if (id && typeof id === 'string' && id.startsWith('Navigator'))
-	// 	treeType = 'navigator';
-	// TODO: remove this hack
-
 	var factory = new TreeViewFactory(data, builder);
 	factory.build(data, builder, parentContainer);
-
-	treeType = '';
 
 	return false;
 };

commit 40a6d7576114182047f1776eaf4e2ea37793b51d

sidebar: fix TypeError on switch
This fixes the error on switching the decks in Writer.
Regression from commit b5eab2c988515c904f8c4b6c975a25e1f777e028
browser: treeview: remove global var hack

Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
Change-Id: Iffb055e1c705618831755e45e7f0715f0d8f6cd8

browser/src/control/Control.Sidebar.ts
@@ -194,6 +194,7 @@ class Sidebar {
 					sidebarData.children[i].visible === false
 				) {
 					sidebarData.children.splice(i, 1);
+					continue;
 				}
 
 				if (