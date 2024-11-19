81d525869ee31244256b176d75d46b6ad593a478
Jan Holesovsky <kendy@collabora.com>
2024-11-19 13:46:01 +0100

    coda-m: Update the project to build after the file splits
    Signed-off-by: Jan Holesovsky <kendy@collabora.com>
    Change-Id: I323064063dc537c42d2f8598d125fb7fbc02af03

--- common/FileUtil-unix.cpp
+++ common/FileUtil-unix.cpp
@@ -33,6 +33,7 @@
 
 #include <common/Anonymizer.hpp>
 #include <common/FileUtil.hpp>
+#include <common/Log.hpp>
 
 namespace FileUtil
 {
@@ -64,7 +65,7 @@ namespace FileUtil
         return path;
     }
 
-#ifndef IOS // iOS-specific implementation in FileUtil-apple.cpp
+#if !defined(IOS) && !defined(MACOS) // iOS-specific implementation in FileUtil-apple.cpp
 
     bool platformDependentCheckDiskSpace(const std::string& path, int64_t enoughSpace)
     {
