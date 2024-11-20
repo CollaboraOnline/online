9022c0584fefe4b0e5a86e8562843e768287865d
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2024-11-20 07:31:10 -0500

    wsd: privatize the anonimization globals
    This moves all globals into the Anonymizer class.
    
    Change-Id: Ib214968b92b005aa435a0e7eb8b2516895dc803b
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- test/WhiteBoxTests.cpp
+++ test/WhiteBoxTests.cpp
@@ -720,6 +720,7 @@ void WhiteBoxTests::testAnonymization()
                                        "access_token_ttl=0&permission=edit";
 
     std::uint64_t anonymizationSalt = 1111111111182589933;
+    Anonymizer::setUrlAnonymization(true, anonymizationSalt);
 
     LOK_ASSERT_EQUAL(std::string("#0#5e45aef91248a8aa#"),
                      Anonymizer::anonymizeUrl(name, anonymizationSalt));
