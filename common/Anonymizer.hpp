8a97de51fa17abc3a2b992db2c1b335768030572
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2024-11-20 07:31:10 -0500

    wsd: privatize the anonimization globals
    This moves all globals into the Anonymizer class.
    
    Change-Id: Ib214968b92b005aa435a0e7eb8b2516895dc803b
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- common/Anonymizer.hpp
+++ common/Anonymizer.hpp
@@ -20,18 +20,42 @@
 #include <string_view>
 #include <unordered_map>
 
-extern std::unordered_map<std::string, std::string> AnonymizedStrings;
-extern std::mutex AnonymizedMutex;
-
 /// Responsible for annonymizing names and URLs.
 /// The anonymized version is always the same for
 /// a given value, provided the salt is identical.
 class Anonymizer
 {
+    Anonymizer(const std::uint64_t salt)
+        : _salt(salt)
+    {
+    }
+
 public:
+    static constexpr std::uint64_t DefaultSalt = 82589933;
+
+    /// Used for anonymizing URLs
+    static void setUrlAnonymization(bool anonymize, const std::uint64_t salt)
+    {
+        _instance.reset();
+        if (anonymize)
+        {
+            _instance.reset(new Anonymizer(salt));
+        }
+    }
+
     /// Sets the anonymized version of a given plain-text string.
     /// After this, 'anonymize(plain)' will return 'anonymized'.
     static void mapAnonymized(const std::string& plain, const std::string& anonymized)
+    {
+        if (_instance)
+        {
+            _instance->map(plain, anonymized);
+        }
+    }
+
+    /// Sets the anonymized version of a given plain-text string.
+    /// After this, 'anonymize(plain)' will return 'anonymized'.
+    void map(const std::string& plain, const std::string& anonymized)
     {
         if (plain.empty() || anonymized.empty())
             return;
@@ -39,31 +63,49 @@ public:
         if (plain != anonymized)
             LOG_TRC("Anonymizing [" << plain << "] -> [" << anonymized << "].");
 
-        std::unique_lock<std::mutex> lock(AnonymizedMutex);
+        std::unique_lock<std::mutex> lock(_mutex);
 
-        AnonymizedStrings[plain] = anonymized;
+        _map[plain] = anonymized;
     }
 
     /// Anonymize a sensitive string to avoid leaking it.
     /// Called on strings to be logged or exposed.
-    static std::string anonymize(const std::string& text, const std::uint64_t anonymizationSalt)
+    std::string_view lookup(const std::string& text) const
     {
-        {
-            std::unique_lock<std::mutex> lock(AnonymizedMutex);
+        std::unique_lock<std::mutex> lock(_mutex);
 
-            const auto it = AnonymizedStrings.find(text);
-            if (it != AnonymizedStrings.end())
-            {
-                if (text != it->second)
-                    LOG_TRC("Found anonymized [" << text << "] -> [" << it->second << "].");
-                return it->second;
-            }
+        const auto it = _map.find(text);
+        if (it != _map.end())
+        {
+            if (text != it->second)
+                LOG_TRC("Found anonymized [" << text << "] -> [" << it->second << "].");
+            return it->second;
+        }
+
+        return std::string_view();
+    }
+
+    /// Anonymize a sensitive string to avoid leaking it.
+    /// Called on strings to be logged or exposed.
+    static std::string anonymize(const std::string& text, const std::uint64_t)
+    {
+        return _instance ? _instance->anonymize(text) : text;
+    }
+
+    /// Anonymize a sensitive string to avoid leaking it.
+    /// Called on strings to be logged or exposed.
+    std::string anonymize(const std::string& text)
+    {
+        const std::string_view anonymized = lookup(text);
+        if (!anonymized.empty())
+        {
+            return std::string(anonymized);
         }
 
         // Modified 64-bit FNV-1a to add salting.
         // For the algorithm and the magic numbers, see http://isthe.com/chongo/tech/comp/fnv/
         std::uint64_t hash = 0xCBF29CE484222325LL;
-        hash ^= anonymizationSalt;
+        hash ^= _salt;
         hash *= 0x100000001b3ULL;
         for (const char c : text)
         {
@@ -71,7 +113,7 @@ public:
             hash *= 0x100000001b3ULL;
         }
 
-        hash ^= anonymizationSalt;
+        hash ^= _salt;
         hash *= 0x100000001b3ULL;
 
         // Generate the anonymized string. The '#' is to hint that it's anonymized.
@@ -81,12 +123,18 @@ public:
         static std::atomic<unsigned> AnonymizationCounter(0);
         std::string res =
             '#' + Util::encodeId(AnonymizationCounter++, 0) + '#' + Util::encodeId(hash, 0) + '#';
-        mapAnonymized(text, res);
+        map(text, res);
         return res;
     }
 
     /// Clears the shared state of mapAnonymized() / anonymize().
-    static void clearAnonymized() { AnonymizedStrings.clear(); }
+    static void clearAnonymized()
+    {
+        if (_instance)
+        {
+            _instance->_map.clear();
+        }
+    }
 
     /// Anonymize the basename of filenames only, preserving the path and extension.
     static std::string anonymizeUrl(const std::string& url, const std::uint64_t anonymizationSalt)
@@ -99,4 +147,17 @@ public:
 
         return base + anonymize(filename, anonymizationSalt) + ext + params;
     }
+
+private:
+    /// The only instance of the Anonymizer per process.
+    static inline std::unique_ptr<Anonymizer> _instance;
+
+    /// The salt used to hash.
+    const std::uint64_t _salt;
+
+    /// The mutex protecting the map.
+    mutable std::mutex _mutex;
+
+    /// The map of plain to annonymized strings.
+    std::unordered_map<std::string, std::string> _map;
 };
