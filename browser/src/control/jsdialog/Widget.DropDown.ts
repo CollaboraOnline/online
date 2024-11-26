b5de3505e46e082ae7fa86f0bdd27d636873e32f
Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>
2024-11-26 14:20:56 +0530

    Add support for 'dropdown' controls in JSDialog
    - Implemented `JSDialog.Dropdown` handler function to manage dropdown controls.
    - Updated `JSDialogBuilder` to register the new dropdown handler.
    - Modified color picker dropdown JSON to include `noDefaultKeyboardNavigation: true`.
    - Removed redundant code handling dropdowns elsewhere.
    
    Signed-off-by: Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>
    Change-Id: Idbcc5b2ac878cc71b3066b781eca062bc0b0aba8

---
+++ browser/src/control/jsdialog/Widget.DropDown.ts
@@ -0,0 +1,28 @@
+/* -*- js-indent-level: 8 -*- */
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * JSDialog.DropDown - build dropdown container with dynamic list
+ */
+
+declare var JSDialog: any;
+
+JSDialog.Dropdown = function (
+	parentContainer: HTMLElement,
+	data: any,
+	builder: any,
+) {
+	builder.build(parentContainer, data.children, data.vertical === true);
+
+	if (!data.noDefaultKeyboardNavigation)
+		JSDialog.KeyboardListNavigation(parentContainer);
+	return false;
+};
