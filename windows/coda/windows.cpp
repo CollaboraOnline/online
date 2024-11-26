d5603b42f90575c510bba69b14e07b0ec53c217f
Tor Lillqvist <tml@collabora.com>
2024-11-26 15:14:45 +0200

    Add getter and setter functions for coolwsd_server_socket_fd for CODA-W
    The C# code in windows/coda/CODA/MainWindow.xaml.cs wants them. It is
    easier to call functions in a DLL from C# than to access variables, I
    assume.
    
    Signed-off-by: Tor Lillqvist <tml@collabora.com>
    Change-Id: Iec28e1d37b9206499b3180ae2042fcd697d086b0

--- windows/coda/windows.cpp
+++ windows/coda/windows.cpp
@@ -4,6 +4,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+#include <config.h>
+
 #include "windows.hpp"
 
 const char *user_name = nullptr;
@@ -11,5 +13,18 @@ const char *user_name = nullptr;
 int coolwsd_server_socket_fd = -1;
 
 LibreOfficeKit *lo_kit;
+
+EXPORT
+int get_coolwsd_server_socket_fd()
+{
+    return coolwsd_server_socket_fd;
+}
+
+EXPORT
+int set_coolwsd_server_socket_fd(int fd)
+{
+    coolwsd_server_socket_fd = fd;
+    return fd;
+}
 
 // vim:set shiftwidth=4 softtabstop=4 expandtab:
