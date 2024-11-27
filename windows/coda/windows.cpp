147cf64dd6a7e8700397ffcb14b869a1b88ba191
Tor Lillqvist <tml@collabora.com>
2024-11-27 18:38:17 +0200

    Some more baby steps to make CODA-W start up properly
    Copy things from the Gtk app. If it turns out that we can use lots of
    identical code snippets as that, they should obviously be factored out
    into some file common to CODA-W and the Gtk app.
    
    Signed-off-by: Tor Lillqvist <tml@collabora.com>
    Change-Id: Ic503faae528ca676535ece6630d182c806ffe3ac

--- windows/coda/windows.cpp
+++ windows/coda/windows.cpp
@@ -8,11 +8,20 @@
 
 #include "windows.hpp"
 
+#include <cstring>
+#include <thread>
+
+#include <common/Log.hpp>
+#include <net/FakeSocket.hpp>
+#include <wsd/COOLWSD.hpp>
+
 const char *user_name = nullptr;
 
 int coolwsd_server_socket_fd = -1;
 
 LibreOfficeKit *lo_kit;
+
+static COOLWSD *coolwsd = nullptr;
 
 EXPORT
 int get_coolwsd_server_socket_fd()
@@ -25,6 +34,37 @@ int set_coolwsd_server_socket_fd(int fd)
 {
     coolwsd_server_socket_fd = fd;
     return fd;
+}
+
+EXPORT
+void initialize_cpp_things()
+{
+    Log::initialize("Mobile", "trace", false, false, {});
+    Util::setThreadName("main");
+
+    fakeSocketSetLoggingCallback([](const std::string& line)
+                                 {
+                                     LOG_TRC_NOFILE(line);
+                                 });
+
+    std::thread([]
+                {
+                    assert(coolwsd == nullptr);
+                    char *argv[2];
+                    // Yes, strdup() is apparently not standard, so MS wants you to call it as
+                    // _strdup(), and warns if you call strdup(). Sure, we could just silence such
+                    // warnings, but let's try to do as they want.
+                    argv[0] = _strdup("mobile");
+                    argv[1] = nullptr;
+                    Util::setThreadName("app");
+                    while (true)
+                    {
+                        coolwsd = new COOLWSD();
+                        coolwsd->run(1, argv);
+                        delete coolwsd;
+                        LOG_TRC("One run of COOLWSD completed");
+                    }
+                }).detach();
 }
 
 // vim:set shiftwidth=4 softtabstop=4 expandtab:
