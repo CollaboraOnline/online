653107b76e84c7e716a8c3aa71d776c196efca21
Miklos Vajna <vmiklos@collabora.com>
2024-11-29 09:20:03 +0100

    cool#9992 doc electronic sign: add a provider selector dialog
    At the moment the esign provider/backend is specified by the 'user
    private info', but that's just a hack I did earlier to avoid creating a
    UI picker.
    
    Instead what's necessary is a UI similar to
    <https://eideasy.com/which-method/>, i.e. a dropdown, containing the
    human-readable names of the providers. This is possible to do, because
    the "send hash" API gives us a list of provider IDs, and there is a
    separate API to get info about providers (e.g. their human-readable
    name).
    
    Start adding this UI by creating a dialog that can present a list of
    providers, it's not yet used by ESignature.
    
    Signed-off-by: Miklos Vajna <vmiklos@collabora.com>
    Change-Id: I51b0ba040f45e46bc9cb4352532da9acf781c02e

---
+++ browser/src/control/Control.ESignatureDialog.ts
@@ -0,0 +1,134 @@
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+namespace cool {
+	export interface SignatureProvider {
+		action_type: string;
+		name: string;
+	}
+
+	/**
+	 * Provides a dialog to select an electronic signing provider.
+	 */
+	export class ESignatureDialog {
+		id: string = 'ESignatureDialog';
+
+		providers: Array<SignatureProvider>;
+
+		constructor(providers: Array<SignatureProvider>) {
+			this.providers = providers;
+		}
+
+		getChildrenJSON(entries: Array<string>): Array<WidgetJSON> {
+			return [
+				{
+					id: 'providerft',
+					type: 'fixedtext',
+					text: _('Provider:'),
+					enabled: true,
+					labelFor: 'providerlb',
+				} as TextWidget,
+				{
+					id: 'providerlb',
+					type: 'listbox',
+					enabled: true,
+					children: [
+						{
+							id: '',
+							type: 'control',
+							enabled: true,
+							children: [],
+						},
+						{
+							type: 'pushbutton',
+							enabled: true,
+							symbol: 'SPIN_DOWN',
+						} as PushButtonWidget,
+					],
+					labelledBy: 'providerft',
+					entries: entries,
+					selectedCount: 1,
+					selectedEntries: ['0'],
+				} as ListBoxWidget,
+				{
+					id: this.id + '-buttonbox',
+					type: 'buttonbox',
+					children: [
+						{
+							id: 'ok',
+							type: 'pushbutton',
+							text: _('OK'),
+						} as PushButtonWidget,
+					],
+					layoutstyle: 'end',
+				} as ButtonBoxWidget,
+			];
+		}
+
+		getJSON(): JSDialogJSON {
+			const entries = this.providers.map((entry) => entry.name);
+			const children = this.getChildrenJSON(entries);
+			return {
+				id: this.id,
+				dialogid: this.id,
+				type: 'dialog',
+				text: _('Select provider'),
+				title: _('Select provider'),
+				jsontype: 'dialog',
+				responses: [
+					{
+						id: 'ok',
+						response: 1,
+					},
+				],
+				children: [
+					{
+						id: this.id + '-mainbox',
+						type: 'container',
+						vertical: true,
+						children: children,
+					} as ContainerWidgetJSON,
+				],
+			} as JSDialogJSON;
+		}
+
+		public close(): void {
+			const closeEvent = {
+				data: {
+					action: 'close',
+					id: this.id,
+				},
+			};
+			app.map.fire('jsdialog', closeEvent);
+		}
+
+		private callback(
+			objectType: string,
+			eventType: string,
+			object: any,
+			data: any,
+			builder: any,
+		) {
+			if (eventType === 'response' || object.id === 'ok') this.close();
+		}
+
+		open(): void {
+			const dialogBuildEvent = {
+				data: this.getJSON(),
+				callback: this.callback.bind(this) as JSDialogCallback,
+			};
+			app.map.fire('jsdialog', dialogBuildEvent);
+		}
+	}
+}
+
+JSDialog.eSignatureDialog = (providers: Array<cool.SignatureProvider>) => {
+	return new cool.ESignatureDialog(providers);
+};
