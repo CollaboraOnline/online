751c9c9f5e945d1366b7c72984c1065f7e1e1427
Tor Lillqvist <tml@collabora.com>
2024-12-03 18:35:35 +0200

    Change CODA to use .NET (and not .NET Framework)
    I wasn't aware of the difference when I created it. But .NET is the
    current thing, .NET Framework is on its way out.
    
    (While forward-porting this in March 2025 I updated the WebView2
    version to the current one.)
    
    Add some more debugging output
    
    Signed-off-by: Tor Lillqvist <tml@collabora.com>
    Change-Id: I8eed09f3cda06a4e8341fdef0bc214226300a495

--- windows/coda/CODA/App.xaml.cs
+++ windows/coda/CODA/App.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace CODA
@@ -11,4 +7,5 @@ namespace CODA
     public partial class App : Application
     {
     }
+
 }
