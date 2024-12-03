0e472b1975842c5a4824fdec848d45f1b84df41e
Tor Lillqvist <tml@collabora.com>
2024-12-03 18:12:19 +0200

    Also check window.ThisIsTheWindowsApp
    Signed-off-by: Tor Lillqvist <tml@collabora.com>
    Change-Id: I164c1244400b677a29477e98b624629f41a8c5e4

--- browser/src/app/Socket.ts
+++ browser/src/app/Socket.ts
@@ -1144,7 +1144,7 @@ class Socket {
 
 	private _extractTextImg(e: SlurpMessageEvent): void {
 		if (
-			(window.ThisIsTheiOSApp || window.ThisIsTheMacOSApp || window.ThisIsTheEmscriptenApp) &&
+			(window.ThisIsTheiOSApp || window.ThisIsTheWindowsApp || window.ThisIsTheMacOSApp || window.ThisIsTheEmscriptenApp) &&
 			typeof e.data === 'string'
 		) {
 			// Another fix for issue #5843 limit splitting on the first newline
