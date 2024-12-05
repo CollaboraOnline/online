cb376c4144dd58331f83fbb1cd9e3990a619e434
Skyler Grey <skyler.grey@collabora.com>
2024-12-05 13:41:24 +0000

    fix(tooltips): Don't display on touch events
    When we used w2-ui, we hid tooltips on mobile devices and tablets. When
    we moved to using JQuery's tooltips we didn't carry this over, causing
    persistent tooltips when people were touching mobile devices. This was
    not ideal.
    
    Rather than hiding these on mobile devices we can show them only on
    mouse events, since as you can use mobile devices without a touchscreen
    or have touchscreen desktop devices (commonly touchscreen laptops).
    We've just added support for touch event detection of the mouseenter
    events that we need for this. We don't want to lock touchleave events to
    mouse, as we want to close the tooltip if we start tapping on other
    things.
    
    Signed-off-by: Skyler Grey <skyler.grey@collabora.com>
    Change-Id: Ie42d1718c123796ed330df7cedd3af7fa1f4ce78

--- browser/src/control/Control.JSDialogBuilder.js
+++ browser/src/control/Control.JSDialogBuilder.js
@@ -2469,10 +2469,10 @@ L.Control.JSDialogBuilder = L.Control.extend({
 			e.stopPropagation();
 		};
 
-		var mouseEnterFunction = function () {
+		var mouseEnterFunction = window.touch.mouseOnly(function () {
 			if (builder.map.tooltip)
 				builder.map.tooltip.beginShow(div);
-		};
+		});
 
 		var mouseLeaveFunction = function () {
 			if (builder.map.tooltip)
@@ -2512,10 +2512,10 @@ L.Control.JSDialogBuilder = L.Control.extend({
 	_handleCutomTooltip: function(elem, builder) {
 		switch (elem.id) {
 			case 'save':
-				$(elem).on('mouseenter', function() {
+				$(elem).on('mouseenter', window.touch.mouseOnly(function() {
 					if (builder.map.tooltip)
 						builder.map.tooltip.show(elem, builder.map.getLastModDateValue()); // Show the tooltip with the correct content
-				});
+				}));
 	
 				$(elem).on('mouseleave', function() {
 					if (builder.map.tooltip)
