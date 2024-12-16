c32668102b4f7510359587fede02624b0795166b
Caolán McNamara <caolan.mcnamara@collabora.com>
2024-12-16 19:44:02 +0000

    add initial config cache support
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: I2dba61a27c83408c8e75641614729c8e7e7bc5f2

--- wsd/CacheUtil.cpp
+++ wsd/CacheUtil.cpp
@@ -44,4 +44,47 @@ void Cache::initialize(const std::string& path)
     CachePath = path;
 }
 
+std::string Cache::getConfigId(const std::string& uri)
+{
+    Poco::URI settingsUri(uri);
+    std::string sourcePrefix = settingsUri.getScheme() +
+                               '_' + settingsUri.getAuthority();
+    std::string sourcePathEtc = settingsUri.getPathEtc();
+    return sourcePrefix + sourcePathEtc;
+}
+
+std::string Cache::locateConfigFile(const std::string& configId, const std::string& uri)
+{
+    Poco::URI sourceUri(uri);
+    std::string sourcePrefix = sourceUri.getScheme() + '_' + sourceUri.getAuthority();
+    std::string sourcePathEtc = sourceUri.getPathEtc();
+
+    Poco::Path rootPath(CachePath, configId);
+    rootPath.makeDirectory();
+
+    Poco::Path source(sourcePrefix, sourcePathEtc);
+    source.makeDirectory();
+
+    Poco::Path cachePath(rootPath, source);
+    cachePath.makeDirectory();
+
+    Poco::File(cachePath).createDirectories();
+    Poco::Path destPath(cachePath, "contents");
+    return destPath.toString();
+}
+
+void Cache::cacheConfigFile(const std::string& configId, const std::string& uri, const std::string& filename)
+{
+    FileUtil::copyFileTo(filename, locateConfigFile(configId, uri));
+}
+
+bool Cache::supplyConfigFile(const std::string& configId, const std::string& uri, const std::string& filename)
+{
+    std::string cacheFile = locateConfigFile(configId, uri);
+    bool exists = FileUtil::Stat(cacheFile).exists();
+    if (exists)
+        FileUtil::copyFileTo(cacheFile, filename);
+    return exists;
+}
+
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
