4e970781b51051ae15e3df6be8f6963e1b97f2cf
Rashesh <rashesh.padia@collabora.com>
2024-12-18 19:13:14 +0530

    wsd: docbroker: send browsersetting json to client
    Signed-off-by: Rashesh <rashesh.padia@collabora.com>
    Change-Id: Ia3de0c7332603c37694e4eccd5bef3168824d2df

--- wsd/DocumentBroker.cpp
+++ wsd/DocumentBroker.cpp
@@ -1659,6 +1659,7 @@ void DocumentBroker::asyncSendBrowserSetting(const std::shared_ptr<ClientSession
         {
             std::ostringstream jsonStream;
             settings->stringify(jsonStream);
+            session->sendTextFrame("browsersetting: " + jsonStream.str());
         }
         else
         {
