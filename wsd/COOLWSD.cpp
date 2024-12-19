53a9385d2635694af2e42bf2115b18b955fee821
Caolán McNamara <caolan.mcnamara@collabora.com>
2024-12-19 20:32:06 +0000

    scrub cache of unused configs
    on startup, and when something is written/loaded from cache
    
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: I9806eb91902df3e45d7b28cc9201f865932afb32

--- wsd/COOLWSD.cpp
+++ wsd/COOLWSD.cpp
@@ -1795,12 +1795,12 @@ void COOLWSD::innerInitialize(Poco::Util::Application& self)
 
     {
         // creating cache directory
-        std::string path = Util::trimmed(ConfigUtil::getPathFromConfig("cache_root_path"));
+        std::string path = Util::trimmed(ConfigUtil::getPathFromConfig("cache_files.path"));
         LOG_INF("Cache path is set to [" << path << "] in config");
         if (path.empty())
         {
             path = "cache";
-            LOG_WRN("No cache path is set in cache_root_path. Using default of: " << path);
+            LOG_WRN("No cache path is set in cache_files.path. Using default of: " << path);
         }
 
         Poco::File p(path);
@@ -1817,10 +1817,7 @@ void COOLWSD::innerInitialize(Poco::Util::Application& self)
         }
 
         if (FileUtil::Stat(path).exists())
-        {
-            LOG_INF("Initializing cache at [" + path << ']');
             Cache::initialize(path);
-        }
     }
 
     NumPreSpawnedChildren = ConfigUtil::getConfigValue<int>(conf, "num_prespawn_children", 1);
