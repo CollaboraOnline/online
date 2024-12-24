6b9320951be7b38f804f2bfdc94dc45027427251
Pranam Lashkari <lpranam@collabora.com>
2024-12-24 12:08:32 +0530

    annotation: avoid comment conflict warning when irrelevan
    avoid comment conflict dialog when changes are done by current user itself
    
    problem:
    when autosaved comments are still in edit mode and user undo the comment action
    user used to get warning "another user removed comment"
    
    Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
    Change-Id: I8578443f6f4e31f441c9e8ce4c77ec276d9a5d6a

--- browser/src/canvas/sections/CommentListSection.ts
+++ browser/src/canvas/sections/CommentListSection.ts
@@ -1437,7 +1437,6 @@ export class CommentSection extends app.definitions.canvasSectionObject {
 		this.onACKComment(obj);
 	}
 
-
 	public handleCommentConflict(obj: any, editComment: Comment) {
 		if (document.getElementById(this.map.uiManager.generateModalId('comments-update')))
 			return;
@@ -1490,6 +1489,10 @@ export class CommentSection extends app.definitions.canvasSectionObject {
 		return false;
 	}
 
+	private actionPerformedByCurrentUser(obj: any): boolean {
+		return obj.comment.author === this.map._viewInfo[this.map._docLayer._editorId].username;
+	}
+
 	public onACKComment (obj: any): void {
 		var id;
 		const anyEdit = Comment.isAnyEdit();
@@ -1497,7 +1500,8 @@ export class CommentSection extends app.definitions.canvasSectionObject {
 			&& !this.checkIfOnlyAnchorPosChanged(obj, anyEdit)
 			&& !anyEdit.sectionProperties.selfRemoved
 			&& anyEdit.sectionProperties.data.id === obj.comment.id
-			&& CommentSection.autoSavedComment !== anyEdit) {
+			&& CommentSection.autoSavedComment !== anyEdit
+			&& !this.actionPerformedByCurrentUser(obj)) {
 			this.handleCommentConflict(obj, anyEdit);
 			return;
 		}
