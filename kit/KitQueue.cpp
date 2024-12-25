9211a52453c5497656c5dbaad6f4ab7429d450eb
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2024-12-25 07:47:55 -0500

    wsd: avoid copying parameters
    Change-Id: I340095a542a61ac6afe868ca15b4e135d3f64fbc
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- kit/KitQueue.cpp
+++ kit/KitQueue.cpp
@@ -22,10 +22,11 @@
 
 #include "JsonUtil.hpp"
 
-/* static */ std::string KitQueue::Callback::toString(int view, int type, const std::string payload)
+/* static */ std::string KitQueue::Callback::toString(int view, int type,
+                                                      const std::string& payload)
 {
     std::ostringstream str;
-    str << view << " " << lokCallbackTypeToString(type) << " "
+    str << view << ' ' << lokCallbackTypeToString(type) << ' '
         << COOLProtocol::getAbbreviatedMessage(payload);
     return str.str();
 }
