b394f6193c0fac7e4733143f4aea3f9919d9995c
Szymon Kłos <szymon.klos@collabora.com>
2024-12-30 11:22:03 +0100

    cypress: make calc/scrolling_spec using split panes
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I38ea6d9a3e200f3eb37f08ba09e01d80f5f3bff2

--- cypress_test/integration_tests/desktop/calc/sheet_switch_spec.js
+++ cypress_test/integration_tests/desktop/calc/sheet_switch_spec.js
@@ -21,7 +21,7 @@ describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'Sheet switching tests', fu
 		// go to sheet 2
 		cy.cGet('#spreadsheet-tab1').click();
 		cy.cGet(helper.addressInputSelector).should('have.prop', 'value', 'F720');
-		desktopHelper.assertScrollbarPosition('vertical', 320, 335);
+		desktopHelper.assertScrollbarPosition('vertical', 330, 350);
 
 		cy.cGet(helper.addressInputSelector).type('{selectAll}A2{enter}');
 		desktopHelper.assertScrollbarPosition('vertical', 15, 25);
@@ -30,12 +30,12 @@ describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'Sheet switching tests', fu
 	it('Check view position on repeated selection of currently selected sheet', function() {
 		// initially we are on sheet 2 tab
 		cy.cGet(helper.addressInputSelector).should('have.prop', 'value', 'F720');
-		desktopHelper.assertScrollbarPosition('vertical', 320, 335);
+		desktopHelper.assertScrollbarPosition('vertical', 330, 350);
 
 		// click on sheet 2 tab (yes, current one)
 		cy.cGet('#spreadsheet-tab1').click();
 		cy.cGet(helper.addressInputSelector).should('have.prop', 'value', 'F720');
-		desktopHelper.assertScrollbarPosition('vertical', 320, 335);
+		desktopHelper.assertScrollbarPosition('vertical', 330, 350);
 
 		// go to different place in the spreadsheet
 		cy.cGet(helper.addressInputSelector).type('{selectAll}A2{enter}');
