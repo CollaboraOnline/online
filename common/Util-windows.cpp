435ab6038318b72891c579be92034a7a3c83d755
Tor Lillqvist <tml@collabora.com>
2024-12-30 12:10:24 +0200

    Implement Util::setThreadName() for Windows
    This required moving the Windows-specific conversion functions between
    string and wide string and vice versa to the Util namespace and making
    them public. (Not sure why I named them using snake_case, probably
    should switch to camelCase as is the norm in this code.)
    
    Signed-off-by: Tor Lillqvist <tml@collabora.com>
    Change-Id: I3b75fe54a1b0e76a83e82097f3e022983f82311d

--- common/Util-windows.cpp
+++ common/Util-windows.cpp
@@ -59,6 +59,44 @@ namespace Util
             return nullptr;
         return &tm;
     }
+
+    std::wstring string_to_wide_string(const std::string& string)
+    {
+        if (string.empty())
+        {
+            return L"";
+        }
+
+        const auto size_needed = MultiByteToWideChar(CP_UTF8, 0, string.data(), (int)string.size(), nullptr, 0);
+        if (size_needed <= 0)
+        {
+            throw std::runtime_error("MultiByteToWideChar() failed: " + std::to_string(size_needed));
+        }
+
+        std::wstring result(size_needed, 0);
+        MultiByteToWideChar(CP_UTF8, 0, string.data(), (int)string.size(), result.data(), size_needed);
+
+        return result;
+    }
+
+    std::string wide_string_to_string(const std::wstring& wide_string)
+    {
+        if (wide_string.empty())
+        {
+            return "";
+        }
+
+        const auto size_needed = WideCharToMultiByte(CP_UTF8, 0, wide_string.data(), (int)wide_string.size(), nullptr, 0, nullptr, nullptr);
+        if (size_needed <= 0)
+        {
+            throw std::runtime_error("WideCharToMultiByte() failed: " + std::to_string(size_needed));
+        }
+
+        std::string result(size_needed, 0);
+        WideCharToMultiByte(CP_UTF8, 0, wide_string.data(), (int)wide_string.size(), result.data(), size_needed, nullptr, nullptr);
+
+        return result;
+    }
 } // namespace Util
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
