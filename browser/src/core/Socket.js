CollaboraOnline/online

commit 4512fc8f4064c1472c3c134e7e76cb1377fe6064

Move graphic selection related logic to its own file.
Remove some unused functions.

Signed-off-by: Gökay Şatır <gokaysatir@collabora.com>
Change-Id: I44c92d50ec967ff7a5f9f79efb78135c77e24639

browser/src/core/Socket.js
@@ -12,7 +12,7 @@
  * L.Socket contains methods for the communication with the server
  */
 
-/* global app JSDialog _ $ errorMessages Uint8Array brandProductName */
+/* global app JSDialog _ $ errorMessages Uint8Array brandProductName GraphicSelection */
 
 app.definitions.Socket = L.Class.extend({
 	ProtocolVersionNumber: '0.1',
@@ -1634,8 +1634,8 @@ app.definitions.Socket = L.Class.extend({
 		if (this._map._docLayer) {
 			this._map._docLayer.removeAllViews();
 			this._map._docLayer._resetClientVisArea();
-			if (this._map._docLayer._graphicSelection) {
-				this._map._docLayer._graphicSelection = null;
+			if (GraphicSelection.hasActiveSelection()) {
+				GraphicSelection.rectangle = null;
 				this._map._docLayer._onUpdateGraphicSelection();
 			}
 			if (this._map._docLayer._docType === 'presentation')

browser/.beforeprettier
@@ -103,6 +103,7 @@
 /src/core/Util.js
 /src/docstate.js
 /src/app/DocEvents.ts
+/src/app/GraphicSelectionMiddleware.ts
 /src/dom/DomEvent.js
 /src/dom/DomEvent.MultiClick.js
 /src/dom/DomEvent.Pointer.js

browser/Makefile.am
@@ -193,6 +193,7 @@ COOL_JS_LST =\
 	src/docstatefunctions.js \
 	src/docdispatcher.ts \
 	src/app/DocEvents.ts \
+	src/app/GraphicSelectionMiddleware.ts \
 	src/Leaflet.js \
 	src/errormessages.js \
 	src/unocommands.js \
@@ -921,6 +922,7 @@ pot:
 		src/docstatefunctions.js \
 		src/docdispatcher.ts \
 		src/app/DocEvents.ts \
+		src/app/GraphicSelectionMiddleware.ts \
 		src/errormessages.js \
 		src/layer/tile/CanvasTileLayer.js \
 		src/canvas/sections/CommentListSection.ts \

browser/src/app/GraphicSelectionMiddleware.ts
@@ -1,0 +1,276 @@
+/* -*- js-indent-level: 8; fill-column: 100 -*- */
+
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+class GraphicSelection {
+    public static rectangle: cool.SimpleRectangle = null;
+    public static extraInfo: any = null;
+	public static selectionAngle: number = 0;
+	public static handlesSection: ShapeHandlesSection = null;
+
+    public static hasActiveSelection() {
+        return this.rectangle !== null;
+    }
+
+    static resetSelectionRanges() {
+		this.rectangle = null;
+		this.extraInfo = null;
+
+		if (this.handlesSection) {
+			this.handlesSection.removeSubSections();
+			app.sectionContainer.removeSection(this.handlesSection.name);
+			this.handlesSection = null;
+		}
+    }
+
+	// shows the video inside current selection marker
+	static onEmbeddedVideoContent(textMsg: string) {
+		if (!this.handlesSection)
+			return;
+
+		var videoDesc = JSON.parse(textMsg);
+
+		if (this.hasActiveSelection()) {
+			videoDesc.width = this.rectangle.cWidth;
+			videoDesc.height = this.rectangle.cHeight;
+		}
+
+		// proxy cannot identify RouteToken if it is encoded
+		var routeTokenIndex = videoDesc.url.indexOf('%26RouteToken=');
+		if (routeTokenIndex != -1) {
+			videoDesc.url = videoDesc.url.replace('%26RouteToken=', '&amp;RouteToken=');
+		}
+
+		var videoToInsert = '<?xml version="1.0" encoding="UTF-8"?>\
+		<foreignObject xmlns="http://www.w3.org/2000/svg" overflow="visible" width="'
+			+ videoDesc.width + '" height="' + videoDesc.height + '">\
+		    <body xmlns="http://www.w3.org/1999/xhtml">\
+		        <video controls="controls" width="' + videoDesc.width + '" height="'
+					+ videoDesc.height + '">\
+		            <source src="' + videoDesc.url + '" type="' + videoDesc.mimeType + '"/>\
+		        </video>\
+		    </body>\
+		</foreignObject>';
+
+		this.handlesSection.addEmbeddedVideo(videoToInsert);
+	}
+
+	static renderDarkOverlay() {
+		var topLeft = new L.Point(this.rectangle.pX1, this.rectangle.pY1);
+		var bottomRight = new L.Point(this.rectangle.pX2, this.rectangle.pY2);
+
+		if (app.map._docLayer.isCalcRTL()) {
+			// Dark overlays (like any other overlay) need regular document coordinates.
+			// But in calc-rtl mode, charts (like shapes) have negative x document coordinate
+			// internal representation.
+			topLeft.x = Math.abs(topLeft.x);
+			bottomRight.x = Math.abs(bottomRight.x);
+		}
+
+		var bounds = new L.Bounds(topLeft, bottomRight);
+
+		app.map._docLayer._oleCSelections.setPointSet(CPointSet.fromBounds(bounds));
+	}
+
+	static extractAndSetGraphicSelection(messageJSON: any) {
+		var signX =  app.map._docLayer.isCalcRTL() ? -1 : 1;
+		var hasExtraInfo = messageJSON.length > 5;
+		var hasGridOffset = false;
+		var extraInfo = null;
+		if (hasExtraInfo) {
+			extraInfo = messageJSON[5];
+			if (extraInfo.gridOffsetX || extraInfo.gridOffsetY) {
+				app.map._docLayer._shapeGridOffset = new app.definitions.simplePoint(signX * extraInfo.gridOffsetX, extraInfo.gridOffsetY);
+				hasGridOffset = true;
+			}
+		}
+
+		// Calc RTL: Negate positive X coordinates from core if grid offset is available.
+		signX = hasGridOffset && app.map._docLayer.isCalcRTL() ? -1 : 1;
+		this.rectangle = new app.definitions.simpleRectangle(signX * messageJSON[0], messageJSON[1], signX * messageJSON[2], messageJSON[3]);
+
+		if (hasGridOffset)
+			this.rectangle.moveBy([app.map._docLayer._shapeGridOffset.x, app.map._docLayer._shapeGridOffset.y]);
+
+		this.extraInfo = extraInfo;
+	}
+
+	public static updateGraphicSelection() {
+		if (this.hasActiveSelection()) {
+			// Hide the keyboard on graphic selection, unless cursor is visible.
+			// Don't interrupt editing in dialogs
+			if (!app.map._docLayer._isAnyInputFocused())
+				app.map.focus(app.file.textCursor.visible);
+
+			if (!app.map.isEditMode()) {
+				return;
+			}
+
+			var extraInfo = this.extraInfo;
+			let addHandlesSection = false;
+
+			if (!this.handlesSection)
+				addHandlesSection = true;
+			else if (extraInfo.id !== this.handlesSection.sectionProperties.info.id) { // Another shape is selected.
+				this.handlesSection.removeSubSections();
+				app.sectionContainer.removeSection(this.handlesSection.name);
+				this.handlesSection = null;
+				addHandlesSection = true;
+			}
+
+			if (addHandlesSection) {
+				this.handlesSection = new app.definitions.shapeHandlesSection({});
+				app.sectionContainer.addSection(this.handlesSection);
+			}
+
+			this.handlesSection.setPosition(this.rectangle.pX1, this.rectangle.pY1);
+			extraInfo.hasTableSelection = app.map._docLayer.hasTableSelection(); // scaleSouthAndEastOnly
+			this.handlesSection.refreshInfo(this.extraInfo);
+			this.handlesSection.setShowSection(true);
+			app.sectionContainer.requestReDraw();
+		}
+		else if (this.handlesSection && app.sectionContainer.doesSectionExist(this.handlesSection.name)){
+			this.handlesSection.removeSubSections();
+			app.sectionContainer.removeSection(this.handlesSection.name);
+			this.handlesSection = null;
+		}
+		app.map._docLayer._updateCursorAndOverlay();
+	}
+
+	public static onShapeSelectionContent(textMsg: string) {
+		textMsg = textMsg.substring('shapeselectioncontent:'.length + 1);
+
+		var extraInfo = this.extraInfo;
+		if (extraInfo && extraInfo.id) {
+			app.map._cacheSVG[extraInfo.id] = textMsg;
+		}
+
+		// video is handled in _onEmbeddedVideoContent
+		if (this.handlesSection && this.handlesSection.sectionProperties.hasVideo)
+			app.map._cacheSVG[extraInfo.id] = undefined;
+		else if (this.handlesSection)
+			this.handlesSection.setSVG(textMsg);
+	}
+
+    public static onMessage (textMsg: string) {
+		app.definitions.urlPopUpSection.closeURLPopUp();
+
+		if (textMsg.match('EMPTY')) {
+			this.resetSelectionRanges();
+		}
+		else if (textMsg.match('INPLACE EXIT')) {
+			app.map._docLayer._oleCSelections.clear();
+		}
+		else if (textMsg.match('INPLACE')) {
+			if (app.map._docLayer._oleCSelections.empty()) {
+				textMsg = '[' + textMsg.substr('graphicselection:'.length) + ']';
+				try {
+					var msgData = JSON.parse(textMsg);
+					if (msgData.length > 1)
+						this.extractAndSetGraphicSelection(msgData);
+				}
+				catch (error) {
+					window.app.console.warn('cannot parse graphicselection command');
+				}
+				this.renderDarkOverlay();
+
+				this.rectangle = null;
+				this.updateGraphicSelection();
+			}
+		}
+		else {
+			textMsg = '[' + textMsg.substr('graphicselection:'.length) + ']';
+			msgData = JSON.parse(textMsg);
+			this.extractAndSetGraphicSelection(msgData);
+
+			// Update the dark overlay on zooming & scrolling
+			if (!app.map._docLayer._oleCSelections.empty()) {
+				app.map._docLayer._oleCSelections.clear();
+				this.renderDarkOverlay();
+			}
+
+			this.selectionAngle = (msgData.length > 4) ? msgData[4] : 0;
+
+			if (this.extraInfo) {
+				var dragInfo = this.extraInfo.dragInfo;
+				if (dragInfo && dragInfo.dragMethod === 'PieSegmentDragging') {
+					dragInfo.initialOffset /= 100.0;
+					var dragDir = dragInfo.dragDirection;
+					dragInfo.dragDirection = app.map._docLayer._twipsToPixels(new L.Point(dragDir[0], dragDir[1]));
+					dragDir = dragInfo.dragDirection;
+					dragInfo.range2 = dragDir.x * dragDir.x + dragDir.y * dragDir.y;
+				}
+			}
+
+			// defaults
+			var extraInfo = this.extraInfo;
+			if (extraInfo) {
+				if (extraInfo.isDraggable === undefined)
+					extraInfo.isDraggable = true;
+				if (extraInfo.isResizable === undefined)
+					extraInfo.isResizable = true;
+				if (extraInfo.isRotatable === undefined)
+					extraInfo.isRotatable = true;
+			}
+
+			// Workaround for tdf#123874. For some reason the handling of the
+			// shapeselectioncontent messages that we get back causes the WebKit process
+			// to crash on iOS.
+
+			// Note2: scroll to frame in writer would result an error:
+			//   svgexport.cxx:810: ...UnknownPropertyException message: "Background
+			var isFrame = extraInfo.type == 601 && !extraInfo.isWriterGraphic;
+
+			if (!window.ThisIsTheiOSApp && this.extraInfo.isDraggable && !this.extraInfo.svg && !isFrame)
+			{
+				app.socket.sendMessage('rendershapeselection mimetype=image/svg+xml');
+			}
+
+			// scroll to selected graphics, if it has no cursor
+			if (!app.map._docLayer.isWriter() && this.rectangle
+				&& app.map._docLayer._allowViewJump()) {
+
+				if (
+					(
+						!app.isPointVisibleInTheDisplayedArea([this.rectangle.x1, this.rectangle.y1]) ||
+						!app.isPointVisibleInTheDisplayedArea([this.rectangle.x2, this.rectangle.y2])
+					)
+					&&
+					!app.map._docLayer._selectionHandles.active &&
+					!(app.isFollowingEditor() || app.isFollowingUser()) &&
+					!app.map._docLayer.calcInputBarHasFocus()) {
+					app.map._docLayer.scrollToPos(new app.definitions.simplePoint(this.rectangle.x1, this.rectangle.y1));
+				}
+			}
+
+		}
+
+		// Graphics are by default complex selections, unless Core tells us otherwise.
+		if (app.map._clip)
+			app.map._clip.onComplexSelection('');
+
+		// Reset text selection - important for textboxes in Impress
+		if (app.map._docLayer._selectionContentRequest)
+			clearTimeout(app.map._docLayer._selectionContentRequest);
+		app.map._docLayer._onMessage('textselectioncontent:');
+
+		this.updateGraphicSelection();
+
+		if (msgData && msgData.length > 5) {
+			var extraInfo = msgData[5];
+			if (extraInfo.url !== undefined) {
+				this.onEmbeddedVideoContent(JSON.stringify(extraInfo));
+			}
+		}
+	}
+}
+
+app.definitions.graphicSelection = GraphicSelection;

browser/src/canvas/sections/ShapeHandleRotationSubSection.ts
@@ -80,7 +80,7 @@ class ShapeHandleRotationSubSection extends CanvasSectionObject {
 		draggedToPoint.pX += this.position[0];
 		draggedToPoint.pY += this.position[1];
 
-		const selectionCenter = new app.definitions.simplePoint(app.map._docLayer._graphicSelection.center[0], app.map._docLayer._graphicSelection.center[1]);
+		const selectionCenter = new app.definitions.simplePoint(GraphicSelection.rectangle.center[0], GraphicSelection.rectangle.center[1]);
 
 		const initialPoint = this.sectionProperties.ownInfo.initialPosition;
 		const initialAngle = this.calculateAngle(selectionCenter, initialPoint);
@@ -96,7 +96,7 @@ class ShapeHandleRotationSubSection extends CanvasSectionObject {
 	onMouseUp(point: number[], e: MouseEvent): void {
 		if (this.containerObject.isDraggingSomething()) {
 			if (this.sectionProperties.lastDraggingDistance) {
-				const center = app.map._docLayer._graphicSelection.center;
+				const center = GraphicSelection.rectangle.center;
 
 				const commandParameters = {
 					'TransformRotationDeltaAngle': {

browser/src/canvas/sections/ShapeHandlesSection.ts
@@ -147,7 +147,7 @@ class ShapeHandlesSection extends CanvasSectionObject {
 		let topMiddle = this.sectionProperties.info.handles.kinds.rectangle['2'][0];
 		topMiddle = new cool.SimplePoint(parseInt(topMiddle.point.x), parseInt(topMiddle.point.y));
 
-		const center = app.map._docLayer._graphicSelection.center; // number array in twips.
+		const center = GraphicSelection.rectangle.center; // number array in twips.
 
 		const radians = Math.atan2((center[1] - topMiddle.y), (topMiddle.x - center[0]));
 		return radians - Math.PI * 0.5;
@@ -163,7 +163,7 @@ class ShapeHandlesSection extends CanvasSectionObject {
 
 		return {
 			angleRadian: this.getShapeAngleRadians(),
-			center: app.map._docLayer._graphicSelection.pCenter.slice(),
+			center: GraphicSelection.rectangle.pCenter.slice(),
 			height: this.getShapeHeight() * app.twipsToPixels,
 			width: this.getShapeWidth() * app.twipsToPixels
 		};
@@ -202,7 +202,7 @@ class ShapeHandlesSection extends CanvasSectionObject {
 		if (!this.sectionProperties.info?.handles?.kinds?.rectangle)
 			return;
 
-		let coreAngle = app.map._docLayer._graphicSelectionAngle;
+		let coreAngle = GraphicSelection.selectionAngle;
 		if (this.sectionProperties.svg)
 			coreAngle = this.sectionProperties.svg.innerHTML.includes('class="Group"') ? 0: coreAngle;
 
@@ -314,8 +314,8 @@ class ShapeHandlesSection extends CanvasSectionObject {
 	updateSize() {
 		this.size = [0, 0];
 
-		if (app.map._docLayer._graphicSelection)
-			this.size = [app.map._docLayer._graphicSelection.pWidth, app.map._docLayer._graphicSelection.pHeight];
+		if (GraphicSelection.hasActiveSelection())
+			this.size = [GraphicSelection.rectangle.pWidth, GraphicSelection.rectangle.pHeight];
 	}
 
 	isSVGVisible() {
@@ -650,7 +650,7 @@ class ShapeHandlesSection extends CanvasSectionObject {
 	}
 
 	adjustSVGProperties() {
-		if (this.sectionProperties.svg && this.sectionProperties.svg.style.display === '' && app.map._docLayer._graphicSelection) {
+		if (this.sectionProperties.svg && this.sectionProperties.svg.style.display === '' && GraphicSelection.hasActiveSelection()) {
 
 			const clientRect = (this.sectionProperties.svg.children[0] as SVGElement).getBoundingClientRect();
 			let width: number = clientRect.width;

browser/src/control/Control.ContextMenu.js
@@ -12,7 +12,7 @@
  * Control.ContextMenu
  */
 
-/* global $ _ _UNO app */
+/* global $ _ _UNO app GraphicSelection */
 L.Control.ContextMenu = L.Control.extend({
 	options: {
 		SEPARATOR: '---------',
@@ -207,7 +207,7 @@ L.Control.ContextMenu = L.Control.extend({
 
 	_amendContextMenuData: function(obj) {
 		// Add a 'delete' entry  for graphic selection on desktop and mobile device (in browser or app).
-		if (this._map._docLayer.hasGraphicSelection()) {
+		if (GraphicSelection.hasActiveSelection()) {
 			var insertIndex = -1;
 			obj.menu.forEach(function(item, index) {
 				if (item.command === '.uno:Paste') {

browser/src/control/Control.NotebookbarBuilder.js
@@ -13,7 +13,7 @@
  * L.Control.NotebookbarBuilder - builder of native HTML widgets for tabbed menu
  */
 
-/* global $ _ JSDialog app */
+/* global $ _ JSDialog app GraphicSelection */
 L.Control.NotebookbarBuilder = L.Control.JSDialogBuilder.extend({
 
 	_customizeOptions: function() {
@@ -667,7 +667,7 @@ $(control.label).unbind('click');
 		$(control.container).click(function (e) {
 			e.preventDefault();
 			var docLayer = builder.map._docLayer;
-			if (!(docLayer._docType === 'spreadsheet' && docLayer._hasActiveSelection)) {
+			if (!(docLayer._docType === 'spreadsheet' && GraphicSelection.hasActiveSelection())) {
 				builder.map.insertComment();
 			}
 		});

commit 4836493dfe53cb133043c177c4dc8409b30026f4

Fix for #10885 : uncaught typeerror when leaving
Possibly regression from commit 4512fc8f4064c1472c3c134e7e76cb1377fe6064
Move graphic selection related logic to its own file.

Socket.js:1680 Uncaught TypeError: this._map._docLayer._onUpdateGraphicSelection is not a function
    at NewClass._onSocketClose (Socket.js:1680:25)
_onSocketClose	@	Socket.js:1680

we don't have that function anymore

Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
Change-Id: I197d1da39b40168d99156fde5ebb951043f913d3

browser/src/core/Socket.js
@@ -1675,10 +1675,8 @@ app.definitions.Socket = L.Class.extend({
 		if (this._map._docLayer) {
 			this._map._docLayer.removeAllViews();
 			this._map._docLayer._resetClientVisArea();
-			if (GraphicSelection.hasActiveSelection()) {
+			if (GraphicSelection.hasActiveSelection())
 				GraphicSelection.rectangle = null;
-				this._map._docLayer._onUpdateGraphicSelection();
-			}
 			if (this._map._docLayer._docType === 'presentation')
 				app.file.textCursor.visible = false;
 