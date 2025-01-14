bb7c39fdcffa3e525e98cdf340d75458b4d03ef7
Jan Holesovsky <kendy@collabora.com>
2025-01-14 11:56:08 +0100

    Implement accept() with CLOEXEC and NONBLOCK for macOS
    Signed-off-by: Jan Holesovsky <kendy@collabora.com>
    Change-Id: Ie1f4791a1a386054400017bcbed0ee0b031da5cb

--- net/Socket.cpp
+++ net/Socket.cpp
@@ -1310,7 +1310,7 @@ std::shared_ptr<Socket> ServerSocket::accept()
 
     struct sockaddr_in6 clientInfo;
     socklen_t addrlen = sizeof(clientInfo);
-    const int rc = ::accept4(getFD(), (struct sockaddr *)&clientInfo, &addrlen, SOCK_NONBLOCK | SOCK_CLOEXEC);
+    const int rc = Syscall::accept_cloexec_nonblock(getFD(), (struct sockaddr *)&clientInfo, &addrlen);
     if (rc < 0)
     {
         if (isUnrecoverableAcceptError(errno))
@@ -1416,7 +1416,7 @@ bool Socket::isLocal() const
 
 std::shared_ptr<Socket> LocalServerSocket::accept()
 {
-    const int rc = ::accept4(getFD(), nullptr, nullptr, SOCK_NONBLOCK | SOCK_CLOEXEC);
+    const int rc = Syscall::accept_cloexec_nonblock(getFD(), nullptr, nullptr);
     if (rc < 0)
     {
         if (isUnrecoverableAcceptError(errno))
