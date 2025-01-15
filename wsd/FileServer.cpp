d5090c9e3bd4b924a4ee7daaf072e3a55f758e45
Rashesh <rashesh.padia@collabora.com>
2025-01-15 14:16:07 +0530

    test: remove `browserconfig.json` from debug wopi server
    Signed-off-by: Rashesh <rashesh.padia@collabora.com>
    Change-Id: Ib965d26259c62a10fb29d29730c70793c00fa409

--- wsd/FileServer.cpp
+++ wsd/FileServer.cpp
@@ -473,14 +473,6 @@ bool FileServerRequestHandler::isAdminLoggedIn(const HTTPRequest& request, http:
                 fileInfo->set("UserSettings", userSettings);
             }
 
-            {
-                Poco::JSON::Object::Ptr browserSettings = new Poco::JSON::Object();
-                std::string uri = COOLWSD::getServerURL() + "/wopi/settings/browserconfig.json";
-                browserSettings->set("uri", Util::trim(uri));
-                browserSettings->set("stamp", etagString);
-                fileInfo->set("BrowserSettings", browserSettings);
-            }
-
             fileInfo->set("UserCanWrite", (requestDetails.getParam("permission") != "readonly") ? "true": "false");
             fileInfo->set("PostMessageOrigin", postMessageOrigin);
             fileInfo->set("LastModifiedTime", localFile->getLastModifiedTime());
@@ -664,78 +656,6 @@ bool FileServerRequestHandler::isAdminLoggedIn(const HTTPRequest& request, http:
         socket->send(httpResponse);
     }
 
-    // handle requests for browserconfig.json
-    void handleBrowserConfigRequest(const std::shared_ptr<StreamSocket>& socket,
-                                    const std::string& etagString)
-    {
-        Poco::JSON::Object::Ptr configInfo = new Poco::JSON::Object();
-        configInfo->set("kind", "browser");
-
-        Poco::JSON::Object::Ptr textEntry = new Poco::JSON::Object();
-        textEntry->set("showResolved", true);
-        textEntry->set("showStatusbar", true);
-        textEntry->set("showToolbar", true);
-
-        Poco::JSON::Object::Ptr presentationEntry = new Poco::JSON::Object();
-        presentationEntry->set("showSidebar", false);
-        presentationEntry->set("showStatusbar", false);
-        presentationEntry->set("showToolbar", true);
-
-        Poco::JSON::Object::Ptr spreadsheetEntry = new Poco::JSON::Object();
-        spreadsheetEntry->set("showStatusbar", false);
-        spreadsheetEntry->set("showToolbar", true);
-
-        Poco::JSON::Object::Ptr darkBackgroundForThemeEntry = new Poco::JSON::Object();
-        darkBackgroundForThemeEntry->set("light", true);
-        darkBackgroundForThemeEntry->set("dark", false);
-
-        Poco::JSON::Array::Ptr recentColorArray = new Poco::JSON::Array();
-        recentColorArray->add("DDE8CB");
-        recentColorArray->add("780373");
-        recentColorArray->add("355269");
-        recentColorArray->add("BF819E");
-        recentColorArray->add("000000");
-
-        Poco::JSON::Array::Ptr customColorArray = new Poco::JSON::Array();
-        customColorArray->add("222222");
-        customColorArray->add("8811F6");
-        customColorArray->add("8800F0");
-        customColorArray->add("8800F7");
-
-        configInfo->set("text", textEntry);
-        configInfo->set("presentation", presentationEntry);
-        configInfo->set("spreadsheet", spreadsheetEntry);
-        configInfo->set("recentColor", recentColorArray);
-        configInfo->set("darkTheme", true);
-        configInfo->set("customColor", customColorArray);
-        configInfo->set("darkBackgroundForTheme", darkBackgroundForThemeEntry);
-        configInfo->set("spellOnline", false);
-        configInfo->set("compactMode", false);
-        configInfo->set("saveAsMode", "groups");
-
-        configInfo->set("WSDFeedbackEnabled", true);
-        configInfo->set("WSDFeedbackCount", 10);
-        configInfo->set("WSDFeedbackLaterDate",1735826583776);
-        configInfo->set("InfoBarLaterDate", 1735826583776);
-
-        configInfo->set("Zotero_LastUsedStyle", "Zotero_StyleName");
-
-        configInfo->set("WSDWelcomeDisabled", false);
-        configInfo->set("WSDWelcomeDisabledDate", "Thu Jan 02 2025");
-        configInfo->set("WSDWelcomeVersion", "24.04.11.1");
-
-        configInfo->set("stamp", etagString);
-
-        std::ostringstream jsonStream;
-        configInfo->stringify(jsonStream);
-
-        http::Response httpResponse(http::StatusCode::OK);
-        FileServerRequestHandler::hstsHeaders(httpResponse);
-        httpResponse.set("Last-Modified", Util::getHttpTime(std::chrono::system_clock::now()));
-        httpResponse.setBody(jsonStream.str(), "application/json; charset=utf-8");
-        socket->send(httpResponse);
-    }
-
     enum PresetType
     {
         Shared,
@@ -805,10 +725,6 @@ bool FileServerRequestHandler::isAdminLoggedIn(const HTTPRequest& request, http:
             {
                 auto items = getAssetVec(PresetType::Shared);
                 handlePresetRequest("shared", etagString, prefix, socket, items);
-            }
-            else if (configPath == "/browserconfig.json")
-            {
-                handleBrowserConfigRequest(socket, etagString);
             }
             else
                 throw BadRequestException("Invalid Config Request: " + configPath);
