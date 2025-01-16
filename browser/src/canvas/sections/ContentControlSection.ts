98377cc5472b536227837f601a0e91df97dcbf4e
Gökay Şatır <gokaysatir@collabora.com>
2025-01-16 15:50:41 +0300

    Remove latlng usage from ContentControl section.
    Signed-off-by: Gökay Şatır <gokaysatir@collabora.com>
    Change-Id: Id450c65c52f28508381f2ea5f11f4300116cbd36

--- browser/src/canvas/sections/ContentControlSection.ts
+++ browser/src/canvas/sections/ContentControlSection.ts
@@ -29,51 +29,40 @@ export class ContentControlSection extends CanvasSectionObject {
 
 		this.map = L.Map.THIS;
 		this.sectionProperties.json = null;
-		this.sectionProperties.datePicker = null;
+		this.sectionProperties.datePicker = false;
 		this.sectionProperties.picturePicker = null;
 		this.sectionProperties.polygon = null;
+		this.sectionProperties.dropdownSection = null;
+		this.sectionProperties.dropdownMarkerWidth = 22;
+		this.sectionProperties.dropdownMarkerHeight = 22;
 	}
 
 	public onInitialize(): void {
-		this.sectionProperties.polyAttri = {
-			stroke: true,
-			fill: false,
-			pointerEvents: 'none',
-			color: 'black',
-			weight: 1,
-		};
-
-		this.sectionProperties.dropdownButton = L.marker(new L.LatLng(0, 0), {
-			icon: L.divIcon({
-				className: 'writer-drop-down-marker',
-				iconSize: null
-			}),
-			interactive: true
-		});
-
-		this.onClickDropdown = this.onClickDropdown.bind(this);
-		this.sectionProperties.dropdownButton.addEventListener('click', this.onClickDropdown, false);
-		this.map.on('dropdownmarkertapped', this.onClickDropdown, this);
 		this.map.on('darkmodechanged', this.changeBorderStyle, this);
 
 		var container = L.DomUtil.createWithId('div', 'datepicker');
 		container.style.zIndex = '12';
 		container.style.position = 'absolute';
 		document.getElementById('document-container').appendChild(container);
-		this.sectionProperties.datePicker = false;
 		this.sectionProperties.picturePicker = false;
+	}
+
+	private setDatePickerVisibility(visible: boolean): void {
+		this.sectionProperties.datePicker = visible;
+
+		if (this.sectionProperties.dropdownSection)
+			this.sectionProperties.dropdownSection.sectionProperties.datePicker = visible;
 	}
 
 	// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 	public drawContentControl(json: any) {
+		this.removeDropdownSection();
+
 		this.sectionProperties.json = json;
-		this.sectionProperties.datePicker = false;
+		this.setDatePickerVisibility(false);
 		this.sectionProperties.picturePicker = false;
-		if (this.sectionProperties.dropdownButton)
-			this.map.removeLayer(this.sectionProperties.dropdownButton);
 
 		if (json.date) {
-			this.sectionProperties.datePicker = true;
 			$.datepicker.setDefaults($.datepicker.regional[(<any>window).langParamLocale.language]);
 			$('#datepicker').datepicker({
 				onSelect: function (date: any, datepicker: any) {
@@ -83,16 +72,14 @@ export class ContentControlSection extends CanvasSectionObject {
 				}
 			});
 			$('#datepicker').hide();
-		} else {
-			this.sectionProperties.datePicker = false;
+		} else
 			$('#datepicker').datepicker('destroy');
-		}
 
-		if (json.action === 'show')	{
+		if (json.action === 'show')
 			this.preparePolygon();
-		} else if (json.action === 'hide') {
+		else if (json.action === 'hide')
 			this.sectionProperties.polygon = null;
-		} else if (json.action === 'change-picture') {
+		else if (json.action === 'change-picture') {
 			this.sectionProperties.picturePicker = true;
 			if (!this.map.wopi.EnableInsertRemoteImage)
 				L.DomUtil.get('insertgraphic').click();
@@ -101,6 +88,13 @@ export class ContentControlSection extends CanvasSectionObject {
 		}
 
 		this.setPositionAndSize();
+
+		if (this.sectionProperties.json.items || json.date) {
+			this.addDropDownSection();
+
+			if (json.date) this.setDatePickerVisibility(true);
+		}
+
 		app.sectionContainer.requestReDraw();
 	}
 
@@ -207,157 +201,32 @@ export class ContentControlSection extends CanvasSectionObject {
 			this.context.strokeRect(startX - 0.5, startY - h - 0.5, textWidth, h);
 
 		}
-		if (this.sectionProperties.json.items || this.sectionProperties.datePicker) {
-			this.addDropDownBtn();
-		}
 	}
 
 	public onNewDocumentTopLeft (): void {
 		this.setPositionAndSize();
 	}
 
-	// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-	private callback(objectType: any , eventType: any, object: any, data: any, builder: any): void {
-		var fireEvent: string = 'jsdialog';
-		if ((<any>window).mode.isMobile()) {
-			fireEvent = 'mobilewizard';
-		}
-		var closeDropdownJson = {
-			'jsontype': 'dialog',
-			'type': 'modalpopup',
-			'action': 'close',
-			'id': builder.windowId,
-		};
-
-		if (eventType === 'close') {
-			this.map.fire(fireEvent, {data: closeDropdownJson, callback: undefined});
-		} else if (eventType === 'select') {
-			app.socket.sendMessage('contentcontrolevent type=drop-down selected='+ data);
-			this.map.fire(fireEvent, {data: closeDropdownJson, callback: undefined});
-		}
+	private removeDropdownSection() {
+		if (this.sectionProperties.dropdownSection)
+			app.sectionContainer.removeSection(this.sectionProperties.dropdownSection.name);
 	}
 
-	private addDropDownBtn(): void {
-		var matches = this.sectionProperties.json.rectangles.match(/\d+/g);
+	private addDropDownSection(): void {
+		this.sectionProperties.dropdownSection = new ContentControlDropdownSubSection(
+			'dropdown' + String(Math.random()),
+			new cool.SimplePoint((this.position[0] + this.size[0]) * app.pixelsToTwips, (this.position[1]) * app.pixelsToTwips),
+			true,
+			this.sectionProperties.dropdownMarkerWidth,
+			this.sectionProperties.dropdownMarkerHeight
+		);
 
-		//consider first rectangle to position dropdownbutton
-		var rectangle = [parseInt(matches[0]), parseInt(matches[1]), parseInt(matches[2]), parseInt(matches[3])];
-
-		var topLeftTwips = new cool.Point(rectangle[0], rectangle[1]);
-		var offset = new cool.Point(rectangle[2], rectangle[3]);
-		var bottomRightTwips = topLeftTwips.add(offset);
-		var buttonAreaTwips = [topLeftTwips, bottomRightTwips];
-
-		var frameArea = new cool.Bounds(
-			this.map._docLayer._twipsToPixels(topLeftTwips),
-			this.map._docLayer._twipsToPixels(bottomRightTwips));
-
-		var size = frameArea.getSize();
-		var origin = this.map.getPixelOrigin();
-		var panePos = this.map._getMapPanePos();
-		this.sectionProperties.framePos = new cool.Point(Math.round(frameArea.min.x + panePos.x - origin.x), Math.round(frameArea.min.y + panePos.y - origin.y));
-		this.sectionProperties.frameWidth = Math.round(size.x);
-		this.sectionProperties.frameHeight = Math.round(size.y);
-
-		var icon =  L.divIcon({
-			className: 'writer-drop-down-marker',
-			iconSize: [this.sectionProperties.frameHeight, this.sectionProperties.frameHeight],
-			iconAnchor: [0, 0]
-		});
-
-		this.sectionProperties.dropdownButton.setIcon(icon);
-
-		// Then convert to unit which can be used on the layer.
-		var buttonAreaLatLng = new L.LatLngBounds(
-			this.map._docLayer._twipsToLatLng(buttonAreaTwips[0], this.map.getZoom()),
-			this.map._docLayer._twipsToLatLng(buttonAreaTwips[1], this.map.getZoom()));
-
-		var pos = buttonAreaLatLng.getNorthEast();
-		this.sectionProperties.dropdownButton.setLatLng(pos);
-		this.map.addLayer(this.sectionProperties.dropdownButton);
-	}
-
-	// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-	private openDropdownJson(): any {
-		if (!this.sectionProperties.json.items)
-			return;
-		var json: any = {
-			'children': [
-				{
-					'id': 'container-dropdown',
-					'type': 'container',
-					'text': '',
-					'enabled': true,
-					'children': [
-						{
-							'id': 'contentControlList',
-							'type': 'treelistbox',
-							'text': '',
-							'enabled': true,
-							'singleclickactivate': true,
-						}
-					],
-					'vertical': true
-				}
-			],
-			'jsontype': 'dialog',
-			'type': 'modalpopup',
-			'cancellable': true,
-			'popupParent': '_POPOVER_',
-			'clickToClose': '_POPOVER_',
-			'id': 'contentControlModalpopup',
-			'isPopupPartialScreen': true
-		};
-
-		var entries = [];
-		var items = this.sectionProperties.json.items;
-
-		//add entries
-		for (var i in items) {
-			var entry = {
-				'text' : items[i],
-				'columns': [
-					{
-						'text': items[i]
-					}
-				],
-				'row': i.toString()
-			};
-			entries.push(entry);
-		}
-		json.children[0].children[0].entries = entries;
-
-		//add position
-		json.posx = this.sectionProperties.framePos.x + this.sectionProperties.frameWidth;
-		json.posy = this.sectionProperties.framePos.y + this.sectionProperties.frameHeight;
-
-		return json;
-	}
-
-	// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-	public onClickDropdown(event: any): void {
-		if (this.sectionProperties.datePicker) {
-			this.showDatePicker();
-		} else if (this.sectionProperties.json.items) {
-			var fireEvent: string = 'jsdialog';
-			if ((<any>window).mode.isMobile()) {
-				fireEvent = 'mobilewizard';
-			}
-			this.map.fire(fireEvent, {data: this.openDropdownJson(), callback: this.callback});
-		}
-
-		L.DomEvent.stopPropagation(event);
-	}
-
-	private showDatePicker(): void {
-		if ($('#datepicker').is(':visible')) {
-			$('#datepicker').hide();
-		} else {
-			var datePicker = document.getElementById('datepicker');
-			datePicker.style.left = this.sectionProperties.framePos.x + this.sectionProperties.frameWidth + 'px';
-			datePicker.style.top = this.sectionProperties.framePos.y + this.sectionProperties.frameHeight + 'px';
-			$('#datepicker').show();
-		}
+		this.sectionProperties.dropdownSection.size[0] = this.sectionProperties.dropdownMarkerWidth;
+		this.sectionProperties.dropdownSection.size[1] = this.size[1];
+		this.sectionProperties.dropdownSection.sectionProperties.json = this.sectionProperties.json;
+		app.sectionContainer.addSection(this.sectionProperties.dropdownSection);
+		this.sectionProperties.dropdownSection.adjustHTMLObjectPosition();
+		this.sectionProperties.dropdownSection.setShowSection(true);
 	}
 
 	private getRectangles(rect: string): Array<number>[] {
