0f70dc5509008f4d343f9a2a0b7c4708fc0240fd
Rashesh <rashesh.padia@collabora.com>
2025-01-16 21:12:23 +0530

    presets: send browsersetting json on when user updates on client side
    - persist browsersettings json in ClientSession
    - just send key update to clientsession from client and ClientSession
    will take care of updating its persisted json and sending update to
    wopihost
    
    Signed-off-by: Rashesh <rashesh.padia@collabora.com>
    Change-Id: I0b45901fa5b42efc54f6d04a938ac66a5a6d1531

--- wsd/DocumentBroker.cpp
+++ wsd/DocumentBroker.cpp
@@ -1848,7 +1848,7 @@ void DocumentBroker::parseBrowserSettings(const std::shared_ptr<ClientSession>& 
         return;
     }
     std::ostringstream jsonStream;
-    browserSettings->stringify(jsonStream);
+    browserSettings->stringify(jsonStream, 2);
     session->sendTextFrame("browsersetting: " + jsonStream.str());
     std::string spellOnline, darkTheme, darkBackgroundForTheme;
     JsonUtil::findJSONValue(browserSettings, "spellOnline", spellOnline);
@@ -1864,6 +1864,7 @@ void DocumentBroker::parseBrowserSettings(const std::shared_ptr<ClientSession>& 
     ClientSession::BrowserSetting browserSetting{ darkTheme, darkBackgroundForTheme, spellOnline };
     session->setBrowserSetting(browserSetting);
     session->setSentBrowserSetting(true);
+    session->setBrowserSettingsJSON(browserSettings);
 }
 
 bool DocumentBroker::processPlugins(std::string& localPath)
