78a1e57d5121b530cb84659e5ff25386837d900a
Caolán McNamara <caolan.mcnamara@collabora.com>
2025-01-16 16:53:02 +0000

    secure captured this lifecycles
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: Ide2e17a671a5d5c2def09ad69e23fe49271335a2

--- wsd/RequestVettingStation.cpp
+++ wsd/RequestVettingStation.cpp
@@ -225,8 +225,12 @@ void RequestVettingStation::launchInstallPresets()
 
     std::string configId = sharedSettings.getConfigId();
 
-    auto finishedCallback = [this, configId](bool success)
+    auto finishedCallback = [selfWeak = weak_from_this(), this, configId](bool success)
     {
+        std::shared_ptr<RequestVettingStation> selfLifecycle = selfWeak.lock();
+        if (!selfLifecycle)
+            return;
+
         if (!success)
         {
             // TODO, should we do something else
