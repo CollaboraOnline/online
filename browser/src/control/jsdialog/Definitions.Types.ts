2b440ce77cdcf61661abc5a30b149c2ea5017551
Szymon Kłos <szymon.klos@collabora.com>
2025-01-22 23:01:13 +0100

    jsdialog: add type for custom rendered entry callback
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I023890a78991c082769ce00c9b96ada4d978675c

--- browser/src/control/jsdialog/Definitions.Types.ts
+++ browser/src/control/jsdialog/Definitions.Types.ts
@@ -106,6 +106,9 @@ type JSDialogCallback = (
 	builder: JSBuilder,
 ) => void;
 
+// callback triggered for custom rendered entries
+type CustomEntryRenderCallback = (pos: number | string) => void;
+
 // used to define menus
 type MenuDefinition = {
 	id: string; // unique identifier
