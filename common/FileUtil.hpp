a2738b88d19c269c962ffeb1b74bb851f418f3f9
Caolán McNamara <caolan.mcnamara@collabora.com>
2025-01-23 13:56:21 +0000

    use nothrow version of last_write_time directly
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: I45dd12b3f7d127995c105bfacd49529cda609a59

--- common/FileUtil.hpp
+++ common/FileUtil.hpp
@@ -91,9 +91,6 @@ namespace FileUtil
     /// Update the access-time and modified-time metadata for the given file.
     bool updateTimestamps(const std::string& filename, timespec tsAccess, timespec tsModified);
 
-    /// Returns timestamp of the file
-    std::filesystem::file_time_type getLastModificationTimestamp(const std::string& filePath);
-
     /// Copy the source file to the target.
     bool copy(const std::string& fromPath, const std::string& toPath, bool log,
               bool throw_on_error);
