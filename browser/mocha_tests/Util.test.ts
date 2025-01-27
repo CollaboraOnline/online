a0de9dc437be1038c7670fbdace0baaff9f41ec7
Dennis Francis <dennis.francis@collabora.com>
2025-01-27 17:54:24 +0530

    mocha: add unit tests for formatNum()
    Signed-off-by: Dennis Francis <dennis.francis@collabora.com>
    Change-Id: I07fb6b479c93789f773857e15041c8c35ee8328a

--- browser/mocha_tests/Util.test.ts
+++ browser/mocha_tests/Util.test.ts
@@ -1,4 +1,5 @@
 /// <reference path="./refs/globals.ts"/>
+/// <reference path="./helper/util.ts"/>
 /// <reference path="../src/app/Util.ts"/>
 
 var assert = require('assert');
@@ -30,5 +31,31 @@ describe('Util static members tests', function () {
 			assert.equal(obj2Id, Util.stamp(obj2));
 		});
 
+	});
+
+	describe('formatNum()', function() {
+		it('integer with no decimal places', function () {
+			assertFloat(Util.formatNum(5, 0), 5, 1e-5, '');
+		});
+
+		it('integer with 4 decimal places', function () {
+			assertFloat(Util.formatNum(5, 4), 5, 1e-5, '');
+		});
+
+		it('decimal with 1 decimal places no-round', function () {
+			assertFloat(Util.formatNum(5.30333333, 1), 5.3, 1e-5, '');
+		});
+
+		it('decimal with 4 decimal places no-round', function () {
+			assertFloat(Util.formatNum(5.30333333, 4), 5.3033, 1e-5, '');
+		});
+
+		it('decimal with 1 decimal places round', function () {
+			assertFloat(Util.formatNum(5.35333333, 1), 5.3, 1e-5, '');
+		});
+
+		it('decimal with 4 decimal places no-round', function () {
+			assertFloat(Util.formatNum(5.30335333, 4), 5.3034, 1e-5, '');
+		});
 	});
 });
