a798992b43dcfbd3991304864d7fe42fe7320cea
Miklos Vajna <vmiklos@collabora.com>
2025-01-27 08:31:46 +0100

    cool#10630 doc electronic sign: fix outgoing transform pos on 2nd page
    Open a PDF with 2 pages, go to the second page, insert -> signature
    line, drag the graphic selection, the preview moves, but the shape
    doesn't move when releasing the mouse.
    
    Seems this is still about the canvas twips vs core twips mapping, this
    time in ShapeHandlesSection.sendTransformCommand(): need to map from
    canvas twips to core twips for non-first pages.
    
    With this, you can drag the shape, it gets moved, but you need an
    additional action to see the updated page like scrolling as a
    workaround, that's not yet fixed here.
    
    Signed-off-by: Miklos Vajna <vmiklos@collabora.com>
    Change-Id: I1ce265a7b5693bf38ad4d62eaa1d151dd3ef8610

--- browser/src/canvas/sections/ShapeHandlesSection.ts
+++ browser/src/canvas/sections/ShapeHandlesSection.ts
@@ -697,6 +697,14 @@ class ShapeHandlesSection extends CanvasSectionObject {
 		if (!y) y = this.sectionProperties.lastDragDistance[1] + this.position[1];
 		else y = this.adjustSnapTransformCoordinate(null, y);
 
+		let yTwips = y * app.pixelsToTwips;
+		const docLayer = app.map._docLayer;
+		const verticalOffset = docLayer.getFiledBasedViewVerticalOffset();
+		if (verticalOffset) {
+			// Transform from canvas twips to core twips.
+			yTwips -= verticalOffset;
+		}
+
 		const parameters = {
 			'TransformPosX': {
 				'type': 'long',
@@ -704,7 +712,7 @@ class ShapeHandlesSection extends CanvasSectionObject {
 			},
 			'TransformPosY': {
 				'type': 'long',
-				'value': Math.round(y * app.pixelsToTwips)
+				'value': Math.round(yTwips)
 			}
 		};
 
