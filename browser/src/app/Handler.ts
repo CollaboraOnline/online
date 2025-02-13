b892860ae2f2aad5338aeae284bf22c9b39003eb
Dennis Francis <dennis.francis@collabora.com>
2025-02-13 17:14:23 +0530

    browser: add base class for Map/Layer...
    ...and a base Handler class.
    
    Signed-off-by: Dennis Francis <dennis.francis@collabora.com>
    Change-Id: Iff109688b02dd65795570ea9f34a78f52e866c22

---
+++ browser/src/app/Handler.ts
@@ -0,0 +1,49 @@
+/* -*- js-indent-level: 8 -*- */
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+// Handler is a base class for handler classes that are used internally
+// to add interaction features like dragging to classes like Map and
+// Marker.
+
+class Handler {
+	private _map: any;
+	private _enabled: boolean;
+
+	constructor(map: any) {
+		this._map = map;
+		this._enabled = false;
+	}
+
+	public enable() {
+		if (this._enabled) {
+			return;
+		}
+
+		this._enabled = true;
+		this.addHooks();
+	}
+
+	public disable() {
+		if (!this._enabled) {
+			return;
+		}
+
+		this._enabled = false;
+		this.removeHooks();
+	}
+
+	public enabled() {
+		return this._enabled;
+	}
+
+	protected addHooks() {}
+	protected removeHooks() {}
+}
