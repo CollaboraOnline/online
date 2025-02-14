77229c0f623cb09e2fa41149dd7dada901efe3a7
Skyler Grey <sky@a.starrysky.fyi>
2025-02-14 15:53:17 +0000

    feat(iOS): Support Range header for media
    The range header allows a partial load of content over HTTP. As our
    custom protocol handler for media (see previous commit) is able to send
    HTTP headers we can get it to support the range header. This means that
    rather than long videos having to fully (read: slowly) load, we can
    stream the content, sending only the bits of the file that the client is
    about to load.
    
    The performance difference is minimal for smaller videos - e.g. 10s to
    even 5 minutes (so long as you don't manually chunk the videos, which
    vastly increases load time on the 5 minute long video). However on
    longer videos I believe this is absolutely necessary, and it removes the
    need to manually chunk here.
    
    This also "fixes" the custom protocol handler issue where I didn't
    bother chunking requests - since as iOS will *always* issue a range
    request there's no way it will ever load the whole video into memory.
    
    Finally, it's worth noting that this is the *second time* I've
    implemented the Range header in Collabora Online - the first was for
    video on the server in CollaboraOnline/Online#7367. Given there's also
    going to be an Android implementation of this (how could there not be?),
    I suspect it might be worth factoring out something like the function
    which gets the start/end of a range on iOS to be used more generally by
    all platforms.
    
    Signed-off-by: Skyler Grey <skyler.grey@collabora.com>
    Change-Id: I69fb6e8f2d5880d2e236e6832bf429f547e43051

--- ios/Mobile/CoolURLSchemeHandler.mm
+++ ios/Mobile/CoolURLSchemeHandler.mm
@@ -38,6 +38,74 @@
     return docBroker.lock();
 }
 
+
+- (std::optional<std::tuple<NSUInteger, NSUInteger, NSUInteger>>)getPositionsAndSizeForRange:(NSString *)range withTotalSize:(NSInteger)size {
+    if ([range containsString:@","]) {
+        return std::nullopt; // We do not provide multiple-range support
+    }
+    
+    if (![range hasPrefix:@"bytes="]) {
+        return std::nullopt; // Malformed range request - at time of writing bytes is the only valid type
+    }
+    
+    NSString * unprefixedRange = [range substringFromIndex:[@"bytes=" length]];
+    NSArray * rangeComponents = [unprefixedRange componentsSeparatedByString:@"-"];
+    
+    if ([rangeComponents count] != 2) {
+        return std::nullopt; // Malformed range request - does not contain exactly 1 dash
+    }
+    
+    // Yes, this lets through some malformed range requests if one side has a string and the other an integer - e.g. 'bytes=three-70' ... but I'm not sure I really mind
+    // The other issues were all about things that would make it impossible to parse... treating non-numbers as the empty-string seems benign and easier to let it slip through
+    // We can't use NSIntegers as they aren't nullable - we would get 0 instead... this rounds decimals, again fairly benign in my opinion
+    NSNumberFormatter * formatter = [[NSNumberFormatter alloc] init];
+    formatter.numberStyle = NSNumberFormatterStyle::NSNumberFormatterNoStyle;
+
+    NSNumber * left = [formatter numberFromString:[rangeComponents firstObject]];
+    NSNumber * right = [formatter numberFromString:[rangeComponents lastObject]];
+    
+    if (left == nil && right == nil) {
+        return std::nullopt; // Malformed range request - at most one side can be empty
+    }
+    
+    if (left == nil) {
+        // Number is "-foo", specifying bytes before the end
+        NSUInteger start = size - [right unsignedIntegerValue];
+        NSUInteger end = size - 1;
+        
+        if (start < 0) {
+            return std::nullopt; // Invalid range - too long
+        }
+        
+        return std::make_optional(std::make_tuple(start, end, [right integerValue]));
+    }
+    
+    if (right == nil) {
+        // Number is "foo-", specifying a start position and asking for bytes until the end
+        NSUInteger start = [left unsignedIntegerValue];
+        NSUInteger end = size - 1;
+        
+        if (start > end) {
+            return std::nullopt; // Invalid range - negative size
+        }
+        
+        return std::make_optional(std::make_tuple(start, end, end - start + 1));
+    }
+    
+    NSUInteger start = [left unsignedIntegerValue];
+    NSUInteger end = [right unsignedIntegerValue];
+    
+    if (start > end) {
+        return std::nullopt; // Invalid range - negative size
+    }
+    
+    if (end >= size) {
+        return std::nullopt; // Invalid range - out of bounds
+    }
+    
+    return std::make_optional(std::make_tuple(start, end, end - start + 1));
+}
+
 - (void)webView:(WKWebView *)webView startURLSchemeTask:(id<WKURLSchemeTask>)urlSchemeTask {
     [ongoingTasks addObject:urlSchemeTask];
     
@@ -55,23 +123,67 @@
     // Get path from tag & open a stream
     std::string mediaPath = [self getDocumentBroker]->getEmbeddedMediaPath(tag);
     NSInteger size = std::filesystem::file_size(mediaPath);
-
+    
+    NSDictionary<NSString*, NSString*> * requestHeaders = urlSchemeTask.request.allHTTPHeaderFields;
     NSMutableDictionary<NSString*, NSString*> * responseHeaders = [[NSMutableDictionary alloc] init];
-    [responseHeaders setObject:[NSString stringWithFormat:@"%ld", size] forKey:@"Content-Length"];
+    
+    NSInteger responseStatus = 200;
+    NSInteger start = 0;
+    NSInteger end = size - 1; // 'end' is considered to be *inclusive* here to match the range header
+    
+    bool errorResponse = false;
+
+    NSString * rangeHeader = [requestHeaders objectForKey:@"Range"];
+    std::optional<std::tuple<NSUInteger, NSUInteger, NSUInteger>> rangePositionsAndSize = [self getPositionsAndSizeForRange:rangeHeader withTotalSize:size];
+
+    if (rangeHeader != nil && rangePositionsAndSize == std::nullopt) {
+        responseStatus = 416;
+        [responseHeaders
+         setObject:[NSString
+                    stringWithFormat:@"bytes */%ld",
+                    static_cast<long>(size)]
+         forKey:@"Content-Range"];
+        errorResponse = true;
+    } else {
+        responseStatus = 206;
+        NSInteger totalSize = size;
+        std::tie(start, end, size) = rangePositionsAndSize.value();
+        [responseHeaders
+         setObject:[NSString
+                    stringWithFormat:@"bytes %ld-%ld/%ld",
+                    start,
+                    end,
+                    totalSize]
+         forKey:@"Content-Range"];
+    }
+        
+    
+    if (!errorResponse) {
+        [responseHeaders setObject:[NSString stringWithFormat:@"%ld", size] forKey:@"Content-Length"];
+    }
+    
+    [responseHeaders setObject:@"bytes" forKey:@"Accept-Ranges"];
     
     // Send preliminary file details
     NSHTTPURLResponse * response = [[NSHTTPURLResponse alloc]
                                     initWithURL:urlSchemeTask.request.URL
-                                    statusCode:200
+                                    statusCode:responseStatus
                                     HTTPVersion:nil
                                     headerFields:responseHeaders
     ];
     [urlSchemeTask didReceiveResponse:response];
-
-    // Send actual file data
+    
+    if (errorResponse) {
+        [ongoingTasks removeObject:urlSchemeTask];
+        [urlSchemeTask didFinish];
+        return;
+    }
+    
+    // Send file data, chunked into small amounts (1MiB (1048576 bytes) - the actual number here is pretty arbitrary)
     std::ifstream media(mediaPath, std::ios_base::in | std::ios_base::binary);
     char* chunk = new char[size];
-
+    
+    media.seekg(start);
     media.read(chunk, size);
         
     if (![ongoingTasks containsObject:urlSchemeTask]) {
