41168eabeaa84654c09c2b6d61674d89db551978
Rashesh <rashesh.padia@collabora.com>
2025-02-15 22:45:34 +0530

    cypress: added crop test for impress and calc
    Signed-off-by: Rashesh <rashesh.padia@collabora.com>
    Change-Id: I557b188e5440e74382c614a8676211b5f06294b9

--- cypress_test/integration_tests/mobile/calc/image_operation_spec.js
+++ cypress_test/integration_tests/mobile/calc/image_operation_spec.js
@@ -34,4 +34,29 @@ describe(['tagmobile', 'tagnextcloud', 'tagproxy'], 'Image Operation Tests', fun
 
 		cy.cGet('#document-container svg g').should('not.exist');
 	});
+
+	it('Crop Image', function () {
+		mobileHelper.insertImage();
+
+		cy.cGet('.mobile-wizard-back.close-button').click();
+
+		helper.assertImageSize(514, 130);
+
+		cy.cGet('#test-div-shape-handle-3').should('exist');
+		cy.cGet('#toolbar-down .ui-scroll-right').click({ force: true });
+		cy.cGet('#toolbar-down .ui-scroll-right').click({ force: true });
+		cy.cGet('#crop-button').click({ force: true });
+
+		cy.cGet('#test-div-shape-handle-3').then(($handle) => {
+			const rect = $handle[0].getBoundingClientRect();
+			const startX = rect.left + rect.width / 2;
+			const startY = rect.top + rect.height / 2;
+			const moveX = 20;
+
+			cy.cGet('body').realSwipe("toRight", { x: startX, y: startY, length: moveX });
+		});
+
+		cy.wait(1000);
+		helper.assertImageSize(494, 130);
+	});
 });
--- cypress_test/integration_tests/mobile/impress/image_operation_spec.js
+++ cypress_test/integration_tests/mobile/impress/image_operation_spec.js
@@ -1,4 +1,4 @@
-/* global describe it beforeEach require */
+/* global describe it beforeEach require cy */
 
 var helper = require('../../common/helper');
 var mobileHelper = require('../../common/mobile_helper');
@@ -18,5 +18,28 @@ describe(['tagmobile', 'tagnextcloud', 'tagproxy'], 'Image Operation Tests', fun
 
 	it('Delete Image', function() {
 		mobileHelper.deleteImage();
+	});
+
+	it('Crop Image', function() {
+		mobileHelper.insertImage();
+
+		cy.cGet('.mobile-wizard-back.close-button').click();
+
+		helper.assertImageSize(304, 77);
+		cy.cGet('#test-div-shape-handle-3').should('exist');
+		cy.cGet('#crop').should('be.visible');
+		cy.cGet('#crop').click();
+
+		cy.cGet('#test-div-shape-handle-3').then(($handle) => {
+			const rect = $handle[0].getBoundingClientRect();
+			const startX = rect.left + rect.width / 2;
+			const startY = rect.top + rect.height / 2;
+			const moveX = 20;
+
+			cy.cGet('body').realSwipe("toRight", { x: startX, y: startY, length: moveX });
+		});
+
+		cy.wait(1000);
+		helper.assertImageSize(284, 77);
 	});
 });
