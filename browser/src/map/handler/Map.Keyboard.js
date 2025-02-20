91a341cba92413ba728d50a3208d5018fa3372f0
Gökay Şatır <gokaysatir@collabora.com>
2025-02-20 19:02:58 +0300

    Merge TilesPrefetches and TileManager classes and check public & private keywords.
    Signed-off-by: Gökay Şatır <gokaysatir@collabora.com>
    Change-Id: Ia300e5b704e6e3b5620c308a1157ffeacfe3aeac

--- browser/src/map/handler/Map.Keyboard.js
+++ browser/src/map/handler/Map.Keyboard.js
@@ -7,7 +7,7 @@
  * at TextInput.
  */
 
-/* global app UNOKey UNOModifier TilesPreFetcher */
+/* global app UNOKey UNOModifier TileManager */
 
 L.Map.mergeOptions({
 	keyboard: true,
@@ -572,7 +572,7 @@ L.Map.Keyboard = L.Handler.extend({
 		}
 
 		if (this._map.isEditMode()) {
-			TilesPreFetcher.resetPreFetching();
+			TileManager.resetPreFetching();
 
 			if (this._ignoreKeyEvent(ev)) {
 				// key ignored
