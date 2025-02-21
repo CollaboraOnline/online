CollaboraOnline/online

commit f47f0b872bf7c606d3e6d9dbbc0ee0a32ad28301

Comment popup: Use Ctrl+Enter shortcut to save and close
Signed-off-by: Méven Car <meven.car@collabora.com>
Change-Id: I8da6e1208b0cbba6b51da97c6926d29da19a47a4

browser/src/canvas/sections/CommentSection.ts
@@ -425,6 +425,13 @@ export class Comment extends CanvasSectionObject {
 	}
 
 	private textAreaKeyDown (ev: any): void {
+		if (ev && ev.ctrlKey && ev.key === "Enter") {
+			ev.preventDefault();
+			this.map.mention.closeMentionPopup(false);
+			this.onSaveComment(ev);
+			return;
+		}
+
 		this.handleKeyDownForPopup(ev, 'mentionPopup');
 	}
 

commit dfb8556caa7ccf2bdd270c7e08675f66b5d92be2

Comment popup: fix reply non working
Regression from f47f0b872bf7c606d3e6d9dbbc0ee0a32ad28301

Also handle properly Ctrl+Enter shortcut for replies.

Signed-off-by: Méven Car <meven.car@collabora.com>
Change-Id: I5d4ec4c0d116860f432e67b847f957a3bfc4231a

browser/src/canvas/sections/CommentSection.ts
@@ -429,9 +429,13 @@ export class Comment extends CanvasSectionObject {
 
 	private textAreaKeyDown (ev: any): void {
 		if (ev && ev.ctrlKey && ev.key === "Enter") {
-			ev.preventDefault();
 			this.map.mention?.closeMentionPopup(false);
-			this.onSaveComment(ev);
+
+			if (this.sectionProperties.nodeReplyText.id == ev.srcElement.id) {
+				this.handleReplyCommentButton(ev);
+			} else {
+				this.handleSaveCommentButton(ev);
+			}
 			return;
 		}
 