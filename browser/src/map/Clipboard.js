83ea1d0a53e80126c906da07ba7f0f071c5f5481
Szymon Kłos <szymon.klos@collabora.com>
2025-03-05 18:05:34 +0100

    cypress: fix race in clipboard dialog creation
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I6791df9ef6361547a4aa141e2eb73c9d08b0f8bb

--- browser/src/map/Clipboard.js
+++ browser/src/map/Clipboard.js
@@ -1264,7 +1264,18 @@ L.Clipboard = L.Class.extend({
 	_warnCopyPaste: function() {
 		var id = 'copy_paste_warning';
 		this._map.uiManager.showYesNoButton(id + '-box', '', '', _('OK'), null, null, null, true);
+		this._warnCopyPasteImpl(id);
+	},
+
+	_warnCopyPasteImpl: function (id) {
 		var box = document.getElementById(id + '-box');
+
+		// TODO: do it JSDialog native...
+		if (!box) {
+			setTimeout(() => { this._warnCopyPasteImpl(id) }, 10);
+			return;
+		}
+
 		var innerDiv = L.DomUtil.create('div', '', null);
 		box.insertBefore(innerDiv, box.firstChild);
 
@@ -1351,7 +1362,19 @@ L.Clipboard = L.Class.extend({
 		this._map.uiManager.showYesNoButton(id + '-box', /*title=*/'', /*message=*/'', /*yesButtonText=*/_('Paste from this document'), /*noButtonText=*/_('Cancel paste special'), /*yesFunction=*/function() {
 			app.socket.sendMessage('uno .uno:PasteSpecial');
 		}, /*noFunction=*/null, /*cancellable=*/true);
+
+		this._openPasteSpecialPopupImpl(id);
+	},
+
+	_openPasteSpecialPopupImpl: function (id) {
 		var box = document.getElementById(id + '-box');
+
+		// TODO: do it JSDialog native...
+		if (!box) {
+			setTimeout(() => { this._openPasteSpecialPopupImpl(id) }, 10);
+			return;
+		}
+
 		var innerDiv = L.DomUtil.create('div', '', null);
 		box.insertBefore(innerDiv, box.firstChild);
 
