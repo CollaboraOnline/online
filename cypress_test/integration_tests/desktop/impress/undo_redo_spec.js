561960e2e2029ccc9b7984cca8ad27ee15dcc9b3
Szymon Kłos <szymon.klos@collabora.com>
2025-03-07 15:36:22 +0100

    cypress: make impress/undo_redo_spec.js more stable
    it requires real fix, possibly saving causes cursor to
    dissapear due to some focus change...
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I3fafe46102ac7ae40728cbd123d93595c2175b8b

--- cypress_test/integration_tests/desktop/impress/undo_redo_spec.js
+++ cypress_test/integration_tests/desktop/impress/undo_redo_spec.js
@@ -1,4 +1,4 @@
-/* global describe it beforeEach require */
+/* global describe it beforeEach require cy */
 
 var helper = require('../../common/helper');
 var impressHelper = require('../../common/impress_helper');
@@ -7,16 +7,24 @@ var repairHelper = require('../../common/repair_document_helper');
 
 describe(['tagdesktop'], 'Editing Operations', function() {
 
+	function skipMessage() {
+		// FIXME: receiveMessage: {"MessageId":"Doc_ModifiedStatus","SendTime":1741357902023,"Values":{"Modified":true}}
+		// FIXME: that message seems to close blinking cursor
+		cy.wait(500);
+	}
+
 	beforeEach(function() {
 		helper.setupAndLoadDocument('impress/undo_redo.odp');
 		desktopHelper.switchUIToCompact();
 		desktopHelper.selectZoomLevel('30', false);
 		impressHelper.selectTextShapeInTheCenter();
+		skipMessage();
 		impressHelper.dblclickOnSelectedShape();
 	});
 
 	function undo() {
 		helper.typeIntoDocument('Hello World');
+		skipMessage();
 		impressHelper.dblclickOnSelectedShape();
 		helper.typeIntoDocument('{ctrl}z');
 		impressHelper.dblclickOnSelectedShape();
