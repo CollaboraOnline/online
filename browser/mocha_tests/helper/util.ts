a8de2de48d85d451ee0eadedf04b79c0b700f7f0
Dennis Francis <dennis.francis@collabora.com>
2025-03-10 14:55:49 +0530

    add missing license header to all mocha test code files
    Signed-off-by: Dennis Francis <dennis.francis@collabora.com>
    Change-Id: I2a64516ea5a95db230e6c8c753f95128723ffb24

--- browser/mocha_tests/helper/util.ts
+++ browser/mocha_tests/helper/util.ts
@@ -1,3 +1,13 @@
+/* -*- js-indent-level: 8 -*- */
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 
 function assertFloat(actual: number, expected: number, eps: number, msg: string) {
 	assert.ok(Math.abs(actual - expected) < eps,
