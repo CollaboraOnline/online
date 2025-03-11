95875fd0776cfdf181852cde0d3e1794a178e60e
Skyler Grey <skyler.grey@collabora.com>
2025-03-11 14:54:02 +0000

    feat(iOS): Support loading video
    Previously, as videos are normally loaded via a /media/ endpoint on the
    server, mobile apps did not support loading it. That's because mobile
    apps lack a server.
    
    To get around either making a server (opening up HTTP just to serve user
    media) or embedding the content directly into the page (as data:, losing
    control of when the content was loaded and losing the ability to update
    to partial loading - essential for long videos), I've opted to use a
    custom protocol handler. Effectively, requests to "cool:/cool/media" are
    treated as iOS media requests. Sadly this technique won't immediately
    work on Android as it doesn't seem to support custom protocol handlers
    - so I'll possibly have to make up something else over there (it'll
    probably be best to use a similar technique though).
    
    I've URL decoded the media URL before getting parameters, since as in
    presentations we are given encoded URLs.
    
    The following issues still exist after this commit:
    - iOS video is not partially loaded due to a lack of support for the
      Range header (see CollaboraOnline/Online#7367 which implemented this
      for web)
      - iOS still tries - and ends up making a lot of extra requests here
    - the video is fully loaded into memory - which means if you're loading
      large videos then I hope you have a lot of spare memory
      - this will also be "fixed" by implementing the range header - because
        iOS will do the chunking for us :)
    - Video still will not work on Android - this custom protocol handler is
      *only* for iOS
    
    This patch depends on core change https://gerrit.libreoffice.org/c/core/+/182631,
    which enables core-side AVMedia support for iOS builds
    
    Refs: core/I1c5b791f7f37185c2b92ba5e6d41f934ac215411
    Signed-off-by: Skyler Grey <skyler.grey@collabora.com>
    Change-Id: I277b5035b1e6179053a72d86024fefd33b8ad188

---
+++ ios/Mobile/CoolURLSchemeHandler.h
@@ -0,0 +1,30 @@
+// -*- Mode: objc; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*-
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#import "ios.h"
+#import <Foundation/NSObject.h>
+
+#import <WebKit/WKURLSchemeTask.h>
+#import <WebKit/WKURLSchemeHandler.h>
+
+#import "CODocument.h"
+
+#import <wsd/DocumentBroker.hpp>
+
+@interface CoolURLSchemeHandler : NSObject<WKURLSchemeHandler>
+{
+    NSMutableSet<id<WKURLSchemeTask>> *ongoingTasks;
+    CODocument *document;
+}
+
+- (id)initWithDocument:(CODocument *)document;
+- (std::shared_ptr<DocumentBroker>)getDocumentBroker;
+@end
