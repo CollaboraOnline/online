2acaae70da99d3d814dfd81c3d60288f25b236af
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2025-03-11 05:36:31 -0400

    wsd: dump Storage state
    Change-Id: If0efaf2794120e1fc8c1cace78d12548d6bebec6
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- wsd/DocumentBroker.cpp
+++ wsd/DocumentBroker.cpp
@@ -5313,11 +5313,19 @@ void DocumentBroker::dumpState(std::ostream& os)
     os << "\n    Next StorageAttributes:";
     _nextStorageAttrs.dumpState(os, "\n      ");
 
+    os << "\n  Storage:";
+    _storage->dumpState(os, "\n    ");
+
+    os << '\n';
     _lockCtx->dumpState(os);
 
     if (_tileCache)
+    {
+        os << '\n';
         _tileCache->dumpState(os);
+    }
 
+    os << '\n';
     _poll->dumpState(os);
 
 #if !MOBILEAPP
