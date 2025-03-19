7844e04b7058c09896312e0ec068890ea0c14881
Gökay Şatır <gokaysatir@gmail.com>
2025-03-19 14:23:32 +0300

    Rename_coordsToTileBounds to pixelCoordsToTwipTileBounds for better readability.
    Signed-off-by: Gökay Şatır <gokaysatir@gmail.com>
    Change-Id: I9350b6aaf2a58a96911ecdbade45fec251fb2c5a

--- browser/src/app/TilesMiddleware.ts
+++ browser/src/app/TilesMiddleware.ts
@@ -891,7 +891,9 @@ class TileManager {
 		// Don't paint the tile, only dirty the sectionsContainer if it is in the visible area.
 		// _emitSlurpedTileEvents() will repaint canvas (if it is dirty).
 		if (
-			app.isRectangleVisibleInTheDisplayedArea(this.coordsToTileBounds(coords))
+			app.isRectangleVisibleInTheDisplayedArea(
+				this.pixelCoordsToTwipTileBounds(coords),
+			)
 		)
 			app.sectionContainer.setDirty(coords);
 	}
@@ -1553,7 +1555,7 @@ class TileManager {
 		return this.tiles[key];
 	}
 
-	private static coordsToTileBounds(coords: TileCoordData): number[] {
+	private static pixelCoordsToTwipTileBounds(coords: TileCoordData): number[] {
 		const x = coords.x * app.pixelsToTwips;
 		const y = coords.y * app.pixelsToTwips;
 		const width = app.tile.size.x;
@@ -1573,7 +1575,7 @@ class TileManager {
 
 		for (const key in this.tiles) {
 			const coords: TileCoordData = this.tiles[key].coords;
-			const tileRectangle = this.coordsToTileBounds(coords);
+			const tileRectangle = this.pixelCoordsToTwipTileBounds(coords);
 
 			if (
 				coords.part === part &&
