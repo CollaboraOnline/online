569f7fa1b9544a6b3c501432c8b4c29f384cad3c
Chris Lord <chris.lord@collabora.com>
2025-03-19 12:44:27 +0000

    Fix double-resize on CanvasSectionContainer
    When a resize is handled by CanvasTileLayer, two resizes may end up being
    triggered on CanvasSectionContainer. In this situation, the first happens
    prematurely and causes drawing to happen before the map is updated, while
    the second is ignored because the canvas size change was already handled.
    The same error is also in CalcTileLayer.
    
    Signed-off-by: Chris Lord <chris.lord@collabora.com>
    Change-Id: I30ed07ab1c29d03a3f212086113fd7721182960b

--- browser/src/canvas/CanvasSectionContainer.ts
+++ browser/src/canvas/CanvasSectionContainer.ts
@@ -1338,8 +1338,10 @@ class CanvasSectionContainer {
 		newWidth = Math.floor(newWidth * app.dpiScale);
 		newHeight = Math.floor(newHeight * app.dpiScale);
 
-		if (this.width === newWidth && this.height === newHeight && this.documentAnchor)
+		if (this.width === newWidth && this.height === newHeight && this.documentAnchor) {
+			this.reNewAllSections(false);
 			return;
+		}
 
 		// Drawing may happen asynchronously so backup the old contents to avoid
 		// showing a blank canvas.
@@ -1366,7 +1368,7 @@ class CanvasSectionContainer {
 		this.width = this.canvas.width;
 		this.height = this.canvas.height;
 
-		this.reNewAllSections();
+		this.reNewAllSections(false);
 	}
 
 	findSectionContainingPoint (point: Array<number>): any {
