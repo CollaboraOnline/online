CollaboraOnline/online

commit 34b4ff6a4efc3429ecd7ecf4bfbf9ca0e1803a98

Mobile: spreadsheet: Allow user to define and remove print area
This fixes #353

For now add only the .uno:DefinePrintArea and DeletePrintArea
- Edit triggers a tunneled dialog and thus is not ready for mobile
- Add (allows to add multiple) is should not be added to mobile until
Edit is available

Signed-off-by: Pedro Pinto Silva <pedro.silva@collabora.com>
Change-Id: I27b5377d00080e745aab465da4dd65fffeaa3217

browser/src/control/Control.Menubar.js
@@ -1051,6 +1051,8 @@ L.Control.Menubar = L.Control.extend({
 				{name: _('See revision history'), id: 'rev-history', type: 'action'},
 				{type: 'separator'},
 				{name: _UNO('.uno:Print', 'spreadsheet'), id: 'print', type: 'action'},
+				{name: _('Define print area', 'spreadsheet'), uno: '.uno:DefinePrintArea' },
+				{name: _('Remove print area', 'spreadsheet'), uno: '.uno:DeletePrintArea' },
 				{name: _UNO('.uno:SetDocumentProperties', 'spreadsheet'), uno: '.uno:SetDocumentProperties', id: 'properties'}
 			]},
 			{name: !window.ThisIsAMobileApp ? _('Download as') : _('Export as'), id:'downloadas', type: 'menu', menu: [

browser/src/control/Control.JSDialogBuilder.js
@@ -2141,6 +2141,8 @@ L.Control.JSDialogBuilder = L.Control.extend({
 		}
 
 		var iconURLAliases = {
+			'defineprintarea': 'menuprintranges',
+			'deleteprintarea': 'delete',
 			'sheetrighttoleft' : 'pararighttoleft',
 			'alignleft': 'leftpara',
 			'alignright': 'rightpara',

commit 1c63e3ddec23b4ac91f7ee20d88d7b0b1457a7db

Fix DefinePrintArea and DeletePrintArea name, remove bogus parameter
No need to add the additional second parameter for the string (as it's
not coming from core).

regression introduce in 34b4ff6a4efc3429ecd7ecf4bfbf9ca0e1803a98 with
Mobile: spreadsheet: Allow user to define and remove print area #5882
 - https://github.com/CollaboraOnline/online/pull/5882

Signed-off-by: Pedro Pinto Silva <pedro.silva@collabora.com>
Change-Id: Ic7f1fdcdfc2f1bfb57652a1d3559b67a21a8ec24

browser/src/control/Control.Menubar.js
@@ -1194,8 +1194,8 @@ L.Control.Menubar = L.Control.extend({
 				{name: _('See revision history'), id: 'rev-history', type: 'action'},
 				{type: 'separator'},
 				{name: _UNO('.uno:Print', 'spreadsheet'), id: 'print', type: 'action'},
-				{name: _('Define print area', 'spreadsheet'), uno: '.uno:DefinePrintArea' },
-				{name: _('Remove print area', 'spreadsheet'), uno: '.uno:DeletePrintArea' },
+				{name: _('Define print area'), uno: '.uno:DefinePrintArea' },
+				{name: _('Remove print area'), uno: '.uno:DeletePrintArea' },
 			]},
 			{name: !window.ThisIsAMobileApp ? _('Download as') : _('Export as'), id:'downloadas', type: 'menu', menu: [
 				{name: _('PDF Document (.pdf)'), id: !window.ThisIsAMobileApp ? 'exportdirectpdf' : 'downloadas-pdf', type: 'action'},