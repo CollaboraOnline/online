ea053aea84693cae93eaf0a1d5f6eb9a8f585b2f
Dennis Francis <dennis.francis@collabora.com>
2025-03-25 18:00:20 +0530

    browser: add partial types to use in DebugManager class
    Signed-off-by: Dennis Francis <dennis.francis@collabora.com>
    Change-Id: Id7dc90686a05e50e859e1133e9ea9cef24c66746

---
+++ browser/src/app/iface/DocLayer.Interface.ts
@@ -0,0 +1,31 @@
+interface PainterInterface {
+	update(): void;
+	_addTilePixelGridSection(): void;
+	_removeTilePixelGridSection(): void;
+	_addPreloadMap(): void;
+	_removePreloadMap(): void;
+	_addSplitsSection(): void;
+	_removeSplitsSection(): void;
+}
+
+interface DocLayerInterface {
+	_painter: PainterInterface;
+
+	isCalc(): boolean;
+	isWriter(): boolean;
+	isImpress(): boolean;
+	isCalcRTL(): boolean;
+
+	_pixelsToTwips(cssPix: cool.PointLike): cool.PointLike;
+	_latLngToTwips(latlng: { lat: number; lng: number }): cool.Point;
+
+	_postMouseEvent(
+		typ: string,
+		x: number,
+		y: number,
+		count: number,
+		buttons: number,
+		modifier: number,
+	): void;
+	postKeyboardEvent(typ: string, charCode: number, unoKeyCode: number): void;
+}
