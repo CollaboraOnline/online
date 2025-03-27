29650e545462217d156f5566b934b34b0c7bbb3d
Gökay Şatır <gokaysatir@collabora.com>
2025-03-27 14:01:16 +0300

    Revert "Move TilesPreFetcher into TilesMiddleware."
    This reverts commit 8e7617e48941355276a8863a957d64f9018d4976.
    
    Signed-off-by: Gökay Şatır <gokaysatir@collabora.com>

--- browser/src/layer/tile/CanvasTileLayer.js
+++ browser/src/layer/tile/CanvasTileLayer.js
@@ -3,7 +3,7 @@
  * L.CanvasTileLayer is a layer with canvas based rendering.
  */
 
-/* global app L JSDialog CanvasSectionContainer GraphicSelection CanvasOverlay CDarkOverlay CSplitterLine CursorHeaderSection $ _ CPointSet CPolyUtil CPolygon Cursor CCellSelection PathGroupType UNOKey UNOModifier Uint8ClampedArray Uint8Array cool OtherViewCellCursorSection TileCoordData TilesPreFetcher */
+/* global app L JSDialog CanvasSectionContainer GraphicSelection CanvasOverlay CDarkOverlay CSplitterLine CursorHeaderSection $ _ CPointSet CPolyUtil CPolygon Cursor CCellSelection PathGroupType UNOKey UNOModifier Uint8ClampedArray Uint8Array cool OtherViewCellCursorSection TileCoordData */
 
 // debugging aid.
 function hex2string(inData, length)
@@ -5276,7 +5276,7 @@ L.CanvasTileLayer = L.Layer.extend({
 
 	_resetPreFetching: function (resetBorder) {
 		if (!this._prefetcher) {
-			this._prefetcher = new TilesPreFetcher(this);
+			this._prefetcher = new L.TilesPreFetcher(this, this._map);
 		}
 
 		this._prefetcher.resetPreFetching(resetBorder);
@@ -5941,6 +5941,342 @@ L.CanvasTileLayer = L.Layer.extend({
 	isCalcRTL: function () {
 		return this.isCalc() && this.isLayoutRTL();
 	}
+
+});
+
+L.TilesPreFetcher = L.Class.extend({
+
+	initialize: function (docLayer, map) {
+		this._docLayer = docLayer;
+		this._map = map;
+	},
+
+	preFetchTiles: function (forceBorderCalc, immediate) {
+		if (app.file.fileBasedView && this._docLayer)
+			this._docLayer._updateFileBasedView();
+
+		if (!this._docLayer || !this._map || this._docLayer._emptyTilesCount > 0 || !this._docLayer._canonicalIdInitialized) {
+			return;
+		}
+
+		var center = this._map.getCenter();
+		var zoom = this._map.getZoom();
+		var part = this._docLayer._selectedPart;
+		var mode = this._docLayer._selectedMode;
+		var hasEditPerm = this._map.isEditMode();
+
+		if (this._zoom === undefined) {
+			this._zoom = zoom;
+		}
+
+		if (this._preFetchPart === undefined) {
+			this._preFetchPart = part;
+		}
+
+		if (this._preFetchMode === undefined) {
+			this._preFetchMode = mode;
+		}
+
+		if (this._hasEditPerm === undefined) {
+			this._hasEditPerm = hasEditPerm;
+		}
+
+		var tileSize = this._docLayer._tileSize;
+		var pixelBounds = this._map.getPixelBoundsCore(center, zoom);
+
+		var viewTileWidth = Math.floor((pixelBounds.getSize().x + tileSize - 1) / tileSize);
+		var viewTileHeight = Math.floor((pixelBounds.getSize().y + tileSize - 1) / tileSize);
+
+		var maxTilesToFetch = Math.ceil(viewTileWidth * viewTileHeight / 4);
+		var maxBorderWidth = 10;
+
+		// Read-only views can much more agressively pre-load
+		if (!hasEditPerm) {
+			maxTilesToFetch *= 4;
+			maxBorderWidth *= 4;
+		}
+
+		// FIXME: when we are actually editing we should pre-load much less until we stop
+/*		if (isActiveEditing()) {
+			maxTilesToFetch = 5;
+			maxBorderWidth = 2;
+		} */
+
+		if (this._pixelBounds === undefined) {
+			this._pixelBounds = pixelBounds;
+		}
+
+		var splitPanesContext = this._docLayer.getSplitPanesContext();
+		var splitPos = splitPanesContext ? splitPanesContext.getSplitPos() : new L.Point(0, 0);
+
+		if (this._splitPos === undefined) {
+			this._splitPos = splitPos;
+		}
+
+		var paneXFixed = false;
+		var paneYFixed = false;
+
+		if (forceBorderCalc ||
+			!this._borders || this._borders.length === 0 ||
+			zoom !== this._zoom ||
+			part !== this._preFetchPart ||
+			mode !== this._preFetchMode ||
+			hasEditPerm !== this._hasEditPerm ||
+			!pixelBounds.equals(this._pixelBounds) ||
+			!splitPos.equals(this._splitPos)) {
+
+			this._zoom = zoom;
+			this._preFetchPart = part;
+			this._preFetchMode = mode;
+			this._hasEditPerm = hasEditPerm;
+			this._pixelBounds = pixelBounds;
+			this._splitPos = splitPos;
+
+			// Need to compute borders afresh and fetch tiles for them.
+			this._borders = []; // Stores borders for each split-pane.
+			var tileRanges = this._docLayer._pxBoundsToTileRanges(pixelBounds);
+			var paneStatusList = splitPanesContext ? splitPanesContext.getPanesProperties() :
+				[ { xFixed: false, yFixed: false} ];
+
+			window.app.console.assert(tileRanges.length === paneStatusList.length, 'tileRanges and paneStatusList should agree on the number of split-panes');
+
+			for (var paneIdx = 0; paneIdx < tileRanges.length; ++paneIdx) {
+				paneXFixed = paneStatusList[paneIdx].xFixed;
+				paneYFixed = paneStatusList[paneIdx].yFixed;
+
+				if (paneXFixed && paneYFixed) {
+					continue;
+				}
+
+				var tileRange = tileRanges[paneIdx];
+				var paneBorder = new L.Bounds(
+					tileRange.min.add(new L.Point(-1, -1)),
+					tileRange.max.add(new L.Point(1, 1))
+				);
+
+				this._borders.push(new L.TilesPreFetcher.PaneBorder(paneBorder, paneXFixed, paneYFixed));
+			}
+
+		}
+
+		var finalQueue = [];
+		var visitedTiles = {};
+
+		var validTileRange = new L.Bounds(
+			new L.Point(0, 0),
+			new L.Point(
+				Math.floor((this._docLayer._docWidthTwips - 1) / this._docLayer._tileWidthTwips),
+				Math.floor((this._docLayer._docHeightTwips - 1) / this._docLayer._tileHeightTwips)
+			)
+		);
+
+		var tilesToFetch = immediate ? Infinity : maxTilesToFetch; // total tile limit per call of preFetchTiles()
+		var doneAllPanes = true;
+
+		for (paneIdx = 0; paneIdx < this._borders.length; ++paneIdx) {
+
+			var queue = [];
+			paneBorder = this._borders[paneIdx];
+			var borderBounds = paneBorder.getBorderBounds();
+
+			paneXFixed = paneBorder.isXFixed();
+			paneYFixed = paneBorder.isYFixed();
+
+			while (tilesToFetch > 0 && paneBorder.getBorderIndex() < maxBorderWidth) {
+
+				var clampedBorder = validTileRange.clamp(borderBounds);
+				var fetchTopBorder = !paneYFixed && borderBounds.min.y === clampedBorder.min.y;
+				var fetchBottomBorder = !paneYFixed && borderBounds.max.y === clampedBorder.max.y;
+				var fetchLeftBorder = !paneXFixed && borderBounds.min.x === clampedBorder.min.x;
+				var fetchRightBorder = !paneXFixed && borderBounds.max.x === clampedBorder.max.x;
+
+				if (!fetchLeftBorder && !fetchRightBorder && !fetchTopBorder && !fetchBottomBorder) {
+					break;
+				}
+
+				if (fetchBottomBorder) {
+					for (var i = clampedBorder.min.x; i <= clampedBorder.max.x; i++) {
+						// tiles below the visible area
+						var coords = new TileCoordData(
+							i * tileSize,
+							borderBounds.max.y * tileSize);
+						queue.push(coords);
+					}
+				}
+
+				if (fetchTopBorder) {
+					for (i = clampedBorder.min.x; i <= clampedBorder.max.x; i++) {
+						// tiles above the visible area
+						coords = new TileCoordData(
+							i * tileSize,
+							borderBounds.min.y * tileSize);
+						queue.push(coords);
+					}
+				}
+
+				if (fetchRightBorder) {
+					for (i = clampedBorder.min.y; i <= clampedBorder.max.y; i++) {
+						// tiles to the right of the visible area
+						coords = new TileCoordData(
+							borderBounds.max.x * tileSize,
+							i * tileSize);
+						queue.push(coords);
+					}
+				}
+
+				if (fetchLeftBorder) {
+					for (i = clampedBorder.min.y; i <= clampedBorder.max.y; i++) {
+						// tiles to the left of the visible area
+						coords = new TileCoordData(
+							borderBounds.min.x * tileSize,
+							i * tileSize);
+						queue.push(coords);
+					}
+				}
+
+				var tilesPending = false;
+				for (i = 0; i < queue.length; i++) {
+					coords = queue[i];
+					coords.z = zoom;
+					coords.part = this._preFetchPart;
+					coords.mode = this._preFetchMode;
+					var key = this._docLayer._tileCoordsToKey(coords);
+
+					if (visitedTiles[key] ||
+					    !this._docLayer._isValidTile(coords) ||
+					    !this._docLayer._tileNeedsFetch(key))
+						continue;
+
+					if (tilesToFetch > 0) {
+						visitedTiles[key] = true;
+						finalQueue.push(coords);
+						tilesToFetch -= 1;
+					}
+					else {
+						tilesPending = true;
+					}
+				}
+
+				if (tilesPending) {
+					// don't update the border as there are still
+					// some tiles to be fetched
+					continue;
+				}
+
+				if (!paneXFixed) {
+					if (borderBounds.min.x > 0) {
+						borderBounds.min.x -= 1;
+					}
+					if (borderBounds.max.x < validTileRange.max.x) {
+						borderBounds.max.x += 1;
+					}
+				}
+
+				if (!paneYFixed) {
+					if (borderBounds.min.y > 0) {
+						borderBounds.min.y -= 1;
+					}
+
+					if (borderBounds.max.y < validTileRange.max.y) {
+						borderBounds.max.y += 1;
+					}
+				}
+
+				paneBorder.incBorderIndex();
+
+			} // border width loop end
+
+			if (paneBorder.getBorderIndex() < maxBorderWidth) {
+				doneAllPanes = false;
+			}
+		} // pane loop end
+
+		if (!immediate)
+			window.app.console.assert(finalQueue.length <= maxTilesToFetch,
+				'finalQueue length(' + finalQueue.length + ') exceeded maxTilesToFetch(' + maxTilesToFetch + ')');
+
+		var tilesRequested = false;
+
+		if (finalQueue.length > 0) {
+			this._cumTileCount += finalQueue.length;
+			this._docLayer._addTiles(finalQueue, !immediate);
+			tilesRequested = true;
+		}
+
+		if (!tilesRequested || doneAllPanes) {
+			this.clearTilesPreFetcher();
+			this._borders = undefined;
+		}
+	},
+
+	resetPreFetching: function (resetBorder) {
+
+		if (!this._map) {
+			return;
+		}
+
+		this.clearPreFetch();
+
+		if (resetBorder) {
+			this._borders = undefined;
+		}
+
+		var interval = 250;
+		var idleTime = 750;
+		this._preFetchPart = this._docLayer._selectedPart;
+		this._preFetchMode = this._docLayer._selectedMode;
+		this._preFetchIdle = setTimeout(L.bind(function () {
+			this._tilesPreFetcher = setInterval(L.bind(this.preFetchTiles, this), interval);
+			this._preFetchIdle = undefined;
+			this._cumTileCount = 0;
+		}, this), idleTime);
+	},
+
+	clearPreFetch: function () {
+		this.clearTilesPreFetcher();
+		if (this._preFetchIdle !== undefined) {
+			clearTimeout(this._preFetchIdle);
+			this._preFetchIdle = undefined;
+		}
+	},
+
+	clearTilesPreFetcher: function () {
+		if (this._tilesPreFetcher !== undefined) {
+			clearInterval(this._tilesPreFetcher);
+			this._tilesPreFetcher = undefined;
+		}
+	},
+
+});
+
+L.TilesPreFetcher.PaneBorder = L.Class.extend({
+
+	initialize: function(paneBorder, paneXFixed, paneYFixed) {
+		this._border = paneBorder;
+		this._xFixed = paneXFixed;
+		this._yFixed = paneYFixed;
+		this._index = 0;
+	},
+
+	getBorderIndex: function () {
+		return this._index;
+	},
+
+	incBorderIndex: function () {
+		this._index += 1;
+	},
+
+	getBorderBounds: function () {
+		return this._border;
+	},
+
+	isXFixed: function () {
+		return this._xFixed;
+	},
+
+	isYFixed: function () {
+		return this._yFixed;
+	},
 
 });
 
