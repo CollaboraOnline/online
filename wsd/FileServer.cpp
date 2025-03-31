1ee12d0b2325527d89c2685f1a176ed405a3cf7c
Caolán McNamara <caolan.mcnamara@collabora.com>
2025-03-31 10:54:11 +0100

    cid#512059 Variable copied when it could be moved
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: Icd6d9e64cfb51797eaee5396c40e5b4afefbe46e

--- wsd/FileServer.cpp
+++ wsd/FileServer.cpp
@@ -864,10 +864,10 @@ bool FileServerRequestHandler::isAdminLoggedIn(const HTTPRequest& request, http:
 
             std::string timestamp =
                 Util::getIso8601FracformatTime(std::chrono::system_clock::now());
-            const std::string body = "{\"LastModifiedTime\": \"" + timestamp + "\" }";
+            std::string body = "{\"LastModifiedTime\": \"" + timestamp + "\" }";
             http::Response httpResponse(http::StatusCode::OK);
             FileServerRequestHandler::hstsHeaders(httpResponse);
-            httpResponse.setBody(body, "application/json; charset=utf-8");
+            httpResponse.setBody(std::move(body), "application/json; charset=utf-8");
             socket->send(httpResponse);
         }
     }
