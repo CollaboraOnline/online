3311380e4d4779207431c04d9650db0796871821
Miklos Vajna <vmiklos@collabora.com>
2025-03-31 10:20:29 +0200

    wsd: fix coplugin:capture
    wsd/ClientSession.cpp:2024:64: error: function parameter captured by reference, capture by value instead [coplugin:capture]
                            << [&](auto& log) { Util::dumpHex(log, data); });
                                                                   ^~~~
    
    The checker assumes that all lambdas are long-living, and then capturing
    'data' by reference is a problem. This is a debug log, just capture by
    value instead of teaching the checker to ignore locations inside log
    macros.
    
    Signed-off-by: Miklos Vajna <vmiklos@collabora.com>
    Change-Id: I3b83c46ef4fa7b08fcf8d20a2206c1a653ee49ca

--- wsd/ClientSession.cpp
+++ wsd/ClientSession.cpp
@@ -2021,7 +2021,7 @@ void ClientSession::postProcessCopyPayload(const std::shared_ptr<Message>& paylo
                 // clipboardcontent: content.application/x-openoffice-svxb;windows_formatname="SVXB (StarView Bitmap/Animation)"
                 // Do not issue this in those cases. (We should also cap the data we dump here.)
                 LOG_DBG("Missing <body> in textselectioncontent/clipboardcontent payload:\n"
-                        << [&](auto& log) { Util::dumpHex(log, data); });
+                        << [data](auto& log) { Util::dumpHex(log, data); });
             }
 
             return false;
