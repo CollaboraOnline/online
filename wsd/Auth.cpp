4930aa1be269269d7a3c9441320da5c4f5185108
Caolán McNamara <caolan.mcnamara@collabora.com>
2025-04-01 20:56:36 +0100

    cid#463636 Variable copied when it could be moved
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: Ic80118f662b1936f502c9e17dcc378d0196b4dfe

--- wsd/Auth.cpp
+++ wsd/Auth.cpp
@@ -94,7 +94,7 @@ const std::string JWTAuth::getAccessToken()
 
     LOG_INF("Sig generated is : " << encodedSig);
 
-    const std::string jwtToken = encodedBody + '.' + encodedSig;
+    std::string jwtToken = encodedBody + '.' + encodedSig;
     LOG_INF("JWT token generated: " << jwtToken);
 
     return jwtToken;
