dc03401295a1c5e3177feaa9a6a188fc1822814f
Miklos Vajna <vmiklos@collabora.com>
2025-04-02 14:31:03 +0200

    clang, test: cover SocketPoll subclass check
    Fails with commit c525bf1d340ffa9e7318d0593c6c0553ca88766b (enforce
    SocketPoll is created via shared_ptr, 2025-03-29) reverted:
    
    error: 'error' diagnostics expected but not seen:
      File test/refcounting.cpp Line 28 (directive at test/refcounting.cpp:27): instance allocated on the stack, create it with std::shared_ptr [coplugin:refcounting]
    
    Signed-off-by: Miklos Vajna <vmiklos@collabora.com>
    Change-Id: I439cf2765d02b38c5b9e8794bb1febf8f848d7f5

--- clang/test/refcounting.cpp
+++ clang/test/refcounting.cpp
@@ -4,6 +4,14 @@ class CheckFileInfo
 {
 };
 
+class SocketPoll
+{
+};
+
+class D : public SocketPoll
+{
+};
+
 int main() {
     {
         // expected-error@+1 {{instance allocated on the stack, create it with std::shared_ptr [coplugin:refcounting]}}
@@ -13,6 +21,16 @@ int main() {
     {
         // good
         auto checkFileInfo = std::make_shared<CheckFileInfo>();
+    }
+
+    {
+        // expected-error@+1 {{instance allocated on the stack, create it with std::shared_ptr [coplugin:refcounting]}}
+        D d;
+    }
+
+    {
+        // good
+        auto d = std::make_shared<D>();
     }
 }
 

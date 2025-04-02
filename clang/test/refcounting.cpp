CollaboraOnline/online

commit c525bf1d340ffa9e7318d0593c6c0553ca88766b

enforce SocketPoll is created via shared_ptr
Co-authored-by: Miklos Vajna <vmiklos@collabora.com>
Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
Change-Id: I6e4173bcdc18ac09aae65202144fd1e2c4b04a60

clang/plugin.cpp
@@ -144,7 +144,10 @@ public:
     static clang::ast_matchers::DeclarationMatcher makeMatcher()
     {
         using namespace clang::ast_matchers;
-        return varDecl(hasType(namedDecl(hasName("CheckFileInfo")))).bind("varDecl");
+        return varDecl(hasType(cxxRecordDecl(anyOf(
+                           hasName("CheckFileInfo"), hasName("SocketPoll"),
+                           isDerivedFrom(anyOf(hasName("CheckFileInfo"), hasName("SocketPoll")))))))
+            .bind("varDecl");
     }
 };
 

net/HttpRequest.hpp
@@ -1269,9 +1269,9 @@ public:
     const std::shared_ptr<const Response> syncDownload(const Request& req,
                                                        const std::string& saveToFilePath)
     {
-        TerminatingPoll poller("HttpSynReqPoll");
-        poller.runOnClientThread();
-        return syncDownload(req, saveToFilePath, poller);
+        std::shared_ptr<TerminatingPoll> poller(std::make_shared<TerminatingPoll>("HttpSynReqPoll"));
+        poller->runOnClientThread();
+        return syncDownload(req, saveToFilePath, *poller);
     }
 
     /// Make a synchronous request.
@@ -1290,9 +1290,9 @@ public:
     /// The payload body of the response, if any, can be read via getBody().
     const std::shared_ptr<const Response> syncRequest(const Request& req)
     {
-        TerminatingPoll poller("HttpSynReqPoll");
-        poller.runOnClientThread();
-        return syncRequest(req, poller);
+        std::shared_ptr<TerminatingPoll> poller(std::make_shared<TerminatingPoll>("HttpSynReqPoll"));
+        poller->runOnClientThread();
+        return syncRequest(req, *poller);
     }
 
     /// Make a synchronous request with the given timeout.

test/HttpRequestTests.cpp
@@ -86,7 +86,7 @@ class HttpRequestTests final : public CPPUNIT_NS::TestFixture
     static constexpr std::chrono::seconds DefTimeoutSeconds{ 5 };
 
     std::string _localUri;
-    SocketPoll _pollServerThread;
+    std::shared_ptr<SocketPoll> _pollServerThread;
     std::shared_ptr<ServerSocket> _socket;
     int _port;
 
@@ -94,7 +94,7 @@ class HttpRequestTests final : public CPPUNIT_NS::TestFixture
 
 public:
     HttpRequestTests()
-        : _pollServerThread("HttpServerPoll")
+        : _pollServerThread(std::make_shared<SocketPoll>("HttpServerPoll"))
         , _port(0)
     {
         net::AsyncDNS::startAsyncDNS();
@@ -153,7 +153,7 @@ public:
             // Try listening on this port.
             LOG_INF("HttpRequestTests::setUp: creating socket to listen on port " << _port);
             _socket = ServerSocket::create(ServerSocket::Type::Local, _port, Socket::Type::IPv4,
-                                           now, _pollServerThread, factory);
+                                           now, *_pollServerThread, factory);
             if (_socket)
                 break;
         }
@@ -163,14 +163,14 @@ public:
         else
             _localUri = "http://127.0.0.1:" + std::to_string(_port);
 
-        _pollServerThread.startThread();
-        _pollServerThread.insertNewSocket(_socket);
+        _pollServerThread->startThread();
+        _pollServerThread->insertNewSocket(_socket);
     }
 
     void tearDown()
     {
         LOG_INF("HttpRequestTests::tearDown");
-        _pollServerThread.stop();
+        _pollServerThread->stop();
         _socket.reset();
     }
 };

tools/Stress.cpp
@@ -74,7 +74,7 @@ int Stress::main(const std::vector<std::string>& args)
         return EX_NOINPUT;
     }
 
-    TerminatingPoll poll("stress replay");
+    std::shared_ptr<TerminatingPoll> poll(std::make_shared<TerminatingPoll>("stress replay"));
 
     if (!UnitWSD::init(UnitWSD::UnitType::Tool, ""))
         throw std::runtime_error("Failed to init unit test pieces.");
@@ -101,11 +101,11 @@ int Stress::main(const std::vector<std::string>& args)
 
     std::cerr << "Connect to " << server << "\n";
     for (size_t i = 1; i < args.size() - 1; i += 2)
-        StressSocketHandler::addPollFor(poll, server, args[i], args[i+1], stats);
+        StressSocketHandler::addPollFor(*poll, server, args[i], args[i+1], stats);
 
     do {
-        poll.poll(TerminatingPoll::DefaultPollTimeoutMicroS);
-    } while (poll.continuePolling() && poll.getSocketCount() > 0);
+        poll->poll(TerminatingPoll::DefaultPollTimeoutMicroS);
+    } while (poll->continuePolling() && poll->getSocketCount() > 0);
 
     stats->dump();
 

tools/WebSocketDump.cpp
@@ -222,7 +222,7 @@ int main (int argc, char **argv)
 {
     (void) argc; (void) argv;
 
-    SocketPoll DumpSocketPoll("websocket");
+    std::shared_ptr<SocketPoll> DumpSocketPoll = std::make_shared<SocketPoll>("websocket");
 
     if (!UnitWSD::init(UnitWSD::UnitType::Wsd, ""))
     {
@@ -261,13 +261,13 @@ int main (int argc, char **argv)
                                               ssl::CertificateVerification::Disabled);
 #endif
 
-    SocketPoll acceptPoll("accept");
+    std::shared_ptr<SocketPoll> acceptPoll = std::make_shared<SocketPoll>("accept");
 
     // Setup listening socket with a factory for connected sockets.
     auto serverSocket = std::make_shared<ServerSocket>(
         Socket::Type::All,
         std::chrono::steady_clock::now(),
-        DumpSocketPoll,
+        *DumpSocketPoll,
         std::make_shared<DumpSocketFactory>(isSSL));
 
     if (!serverSocket->bind(ServerSocket::Type::Public, port))
@@ -282,12 +282,12 @@ int main (int argc, char **argv)
         return -1;
     }
 
-    acceptPoll.startThread();
-    acceptPoll.insertNewSocket(serverSocket);
+    acceptPoll->startThread();
+    acceptPoll->insertNewSocket(serverSocket);
 
     while (true)
     {
-        DumpSocketPoll.poll(std::chrono::seconds(100));
+        DumpSocketPoll->poll(std::chrono::seconds(100));
     }
 }
 

wsd/Admin.hpp
@@ -81,8 +81,8 @@ public:
 
     static Admin& instance()
     {
-        static Admin admin;
-        return admin;
+        static std::shared_ptr<Admin> admin(new Admin);
+        return *admin;
     }
 
     void start();

wsd/COOLWSD.cpp
@@ -3604,8 +3604,8 @@ int COOLWSD::innerMain()
 
 #if !MOBILEAPP
     // Fetch remote settings from server if configured
-    RemoteConfigPoll remoteConfigThread(config());
-    remoteConfigThread.start();
+    std::shared_ptr<RemoteConfigPoll> remoteConfigThread(std::make_shared<RemoteConfigPoll>(config()));
+    remoteConfigThread->start();
 #endif
 
 #ifndef IOS

commit dc03401295a1c5e3177feaa9a6a188fc1822814f

clang, test: cover SocketPoll subclass check
Fails with commit c525bf1d340ffa9e7318d0593c6c0553ca88766b (enforce
SocketPoll is created via shared_ptr, 2025-03-29) reverted:

error: 'error' diagnostics expected but not seen:
  File test/refcounting.cpp Line 28 (directive at test/refcounting.cpp:27): instance allocated on the stack, create it with std::shared_ptr [coplugin:refcounting]

Signed-off-by: Miklos Vajna <vmiklos@collabora.com>
Change-Id: I439cf2765d02b38c5b9e8794bb1febf8f848d7f5

clang/test/refcounting.cpp
@@ -4,6 +4,14 @@ class CheckFileInfo
 {
 };
 
+class SocketPoll
+{
+};
+
+class D : public SocketPoll
+{
+};
+
 int main() {
     {
         // expected-error@+1 {{instance allocated on the stack, create it with std::shared_ptr [coplugin:refcounting]}}
@@ -13,6 +21,16 @@ int main() {
     {
         // good
         auto checkFileInfo = std::make_shared<CheckFileInfo>();
+    }
+
+    {
+        // expected-error@+1 {{instance allocated on the stack, create it with std::shared_ptr [coplugin:refcounting]}}
+        D d;
+    }
+
+    {
+        // good
+        auto d = std::make_shared<D>();
     }
 }
 