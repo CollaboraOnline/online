891dbe980bbfa6203c4c88578e239fecf613f47b
Chris Lord <chris.lord@collabora.com>
2025-04-04 13:10:22 +0100

    Fix touchpad scrolling causing uncapped drawing
    Touchpad scrolling was immediately redrawing the screen on every event.
    Fix this by storing the scroll delta and deferring redraw until a
    requestAnimationFrame callback.
    
    Signed-off-by: Chris Lord <chris.lord@collabora.com>
    Change-Id: I455fafb20ef63a642bdbc4150bec82bc3781f19d

--- browser/src/canvas/CanvasSectionContainer.ts
+++ browser/src/canvas/CanvasSectionContainer.ts
@@ -2120,6 +2120,8 @@ class CanvasSectionContainer {
 			options (possible values are separated by the '|' char):
 				// Developer can set the duration for the animation, in milliseconds. There are also other ways to stop the animation.
 				duration: 2000 | null // 2 seconds | null.
+				// The animation can start after a requestAnimationFrame
+				defer: boolean
 		*/
 
 		if (!this.getAnimatingSectionName()) {
@@ -2131,7 +2133,10 @@ class CanvasSectionContainer {
 			this.elapsedTime = 0;
 			this.frameCount = 0;
 
-			this.animate(performance.now());
+			if (options.defer)
+				requestAnimationFrame(this.animate.bind(this));
+			else
+				this.animate(performance.now());
 			return true;
 		}
 		else {
