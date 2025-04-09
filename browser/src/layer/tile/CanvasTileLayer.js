9540eb2bdf36db6ba9a6768d9ae9c7b1631d2e71
Chris Lord <chris.lord@collabora.com>
2025-04-09 15:55:04 +0100

    Fix possible missing tiles when a split section is created
    'splitposchanged' was not causing an update, so tiles that hadn't already
    been requested or hydrated could potentially be missing. This is
    especially evident when switching sheets after a newly loaded spreadsheet
    to a sheet that has a split and is far from the origin.
    
    Signed-off-by: Chris Lord <chris.lord@collabora.com>
    Change-Id: I9de62d997d91db9165642b2fadb0ae8095497ff9

--- browser/src/layer/tile/CanvasTileLayer.js
+++ browser/src/layer/tile/CanvasTileLayer.js
@@ -700,7 +700,9 @@ L.CanvasTileLayer = L.Layer.extend({
 			}, this._painter);
 		}
 		this._map.on('zoomend', this._painter.update, this._painter);
-		this._map.on('splitposchanged', this._painter.update, this._painter);
+		this._map.on('splitposchanged', function () {
+			TileManager.update();
+		}, this);
 		this._map.on('sheetgeometrychanged', this._painter.update, this._painter);
 		this._map.on('move', this._syncTilePanePos, this);
 
