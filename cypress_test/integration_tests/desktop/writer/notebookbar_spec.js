CollaboraOnline/online

commit 823178e86ae6762d5a019c34344ac987f06b0437

cool#11617 browser, notebookbar: don't automatically switch from Review for text documents
Now that changing from text context to table context is a single
notebookbar switch, further decouple actions:

- showing the previously hidden Table tab is fine

- switching to that tab may be problematic

Keep the current behavior by default (so if you e.g. insert an image &
we select it, we switch tab from Home), but don't switch from the
Review, just show the Table tab.

With this, it's possible to visit each change in the document when some
of the changes are outside the table and some of them are inside a
table.

Signed-off-by: Miklos Vajna <vmiklos@collabora.com>
Change-Id: Ie9ed05f206a408c7b00168eacc72b486a01ccaaf

browser/src/control/Control.Notebookbar.js
@@ -483,8 +483,13 @@ L.Control.Notebookbar = L.Control.extend({
 		var contextTab = null;
 		var defaultTab = null;
 		let alreadySelected = null;
+		// Currently selected tab name, part of the element's ID.
+		let currentlySelectedTabName = null;
 		for (var tab in tabs) {
 			var tabElement = $('#' + tabs[tab].name + '-tab-label');
+			if (tabElement.hasClass('selected')) {
+				currentlySelectedTabName = tabs[tab].name;
+			}
 			if (tabs[tab].context) {
 				tabElement.hide();
 				var contexts = tabs[tab].context.split('|');
@@ -521,7 +526,13 @@ L.Control.Notebookbar = L.Control.extend({
 		}
 
 		if (contextTab) {
-			contextTab.click();
+			// Switch to the tab of the context, unless we currently show the review tab
+			// for text documents, where jumping to the next change would possibly
+			// switch to the Home or Table tabs, which is not wanted.
+			var docType = this._map.getDocType();
+			if (docType !== 'text' || currentlySelectedTabName !== 'Review') {
+				contextTab.click();
+			}
 			const tabId = contextTab.attr('id');
 			this.updateButtonVisibilityForContext(requestedContext, tabId);
 			return tabId;

commit 8619a81ffad5cad6eaac9cc467f3b3d3d487cb6b

cool#11617 browser, notebookbar: add testcase for the review tab switch
Fails with commit 823178e86ae6762d5a019c34344ac987f06b0437 (cool#11617
browser, notebookbar: don't automatically switch from Review for text
documents, 2025-04-14) reverted.

Signed-off-by: Miklos Vajna <vmiklos@collabora.com>
Change-Id: I905d6d31fb179e8fa76aaf30e1a309ba8a52ef13

cypress_test/integration_tests/desktop/writer/notebookbar_spec.js
@@ -40,3 +40,22 @@ describe(['tagdesktop'], 'Notebookbar tests.', function() {
 		cy.cGet('.notebookbar .unoSpellOnline span').contains('Automatic Spell Checking');
 	});
 });
+
+describe(['tagdesktop'], 'Notebookbar review operations.', function() {
+	it('Go to the next change', function() {
+		// Given a document where the first redline is inside a table:
+		helper.setupAndLoadDocument('writer/notebookbar-redline.odt');
+		desktopHelper.switchUIToNotebookbar();
+
+		// When going to the next redline:
+		cy.cGet('#Review-tab-label').click();
+		cy.cGet('#review-next-tracked-change-button').click();
+		cy.cGet('#Table-tab-label').should('not.have.class', 'hidden');
+
+		// Then make sure that by the time the Table tab is visible, the Review tab is still
+		// selected:
+		// Without the accompanying fix in place, this test would have failed with:
+		// AssertionError: Timed out retrying after 10000ms: expected '<button#Review-tab-label.ui-tab.notebookbar>' to have class 'selected'
+		cy.cGet('#Review-tab-label').should('have.class', 'selected');
+	});
+});