9aa0fd1ed2c36e58f9af426aeb79e863cdc3c095
Gökay Şatır <gokaysatir@gmail.com>
2025-04-17 10:47:15 +0300

    Add moveMapBy function in order to gather map position changes at one place.
    Signed-off-by: Gökay Şatır <gokaysatir@gmail.com>
    Change-Id: I82611b5b44c457e576bdcd041443148dd0376cb5

--- browser/src/canvas/sections/ScrollSection.ts
+++ browser/src/canvas/sections/ScrollSection.ts
@@ -139,8 +139,12 @@ export class ScrollSection extends CanvasSectionObject {
 		// Triggered by the document (e.g. search result out of the viewing area).
 		if (this.map.panBy) {
 			const offset = this.map.scrollOffset();
-			this.map.panBy(new L.Point(e.x - offset.x, e.y - offset.y));
+			this.moveMapBy(e.x - offset.x, e.y - offset.y);
 		}
+	}
+
+	public moveMapBy(cX: number, cY: number): void {
+		this.map.panBy(new L.Point(cX, cY));
 	}
 
 	// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
@@ -169,7 +173,8 @@ export class ScrollSection extends CanvasSectionObject {
 			if (docTopLef[0] + diff < 0) {
 				e.x = Math.round(-1 * docTopLef[0] / app.dpiScale);
 			}
-			this.map.panBy(new L.Point(e.x, e.y));
+
+			this.moveMapBy(e.x, e.y);
 		}
 	}
 
@@ -768,7 +773,7 @@ export class ScrollSection extends CanvasSectionObject {
 				return false;
 		}
 
-		this.map.panBy(new L.Point(0, offset / app.dpiScale));
+		this.moveMapBy(0, offset / app.dpiScale);
 		this.onUpdateScrollOffset();
 
 		if (app.file.fileBasedView) this.map._docLayer._checkSelectedPart();
@@ -803,7 +808,7 @@ export class ScrollSection extends CanvasSectionObject {
 				return false;
 		}
 
-		this.map.panBy(new L.Point(offset / app.dpiScale, 0));
+		this.moveMapBy(offset / app.dpiScale, 0);
 		this.onUpdateScrollOffset();
 
 		if (!this.sectionProperties.drawHorizontalScrollBar) {
