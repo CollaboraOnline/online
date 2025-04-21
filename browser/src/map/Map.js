72c25b4ae0f5eabd4e368acde085bbf836c2c0cb
Gökay Şatır <gokaysatir@gmail.com>
2025-04-21 16:15:02 +0300

    Use app.file.viewedRectangle instead of scrollOffset function and remove scrollOffset function. They return the same values.
    Signed-off-by: Gökay Şatır <gokaysatir@gmail.com>
    Change-Id: I55430f6fb8bfda5d0ae34cbd0f097232e8060fca

--- browser/src/map/Map.js
+++ browser/src/map/Map.js
@@ -435,15 +435,6 @@ L.Map = L.Evented.extend({
 		return this;
 	},
 
-	scrollOffset: function () {
-		var center = this.project(this.getCenter());
-		var centerOffset = center.subtract(this.getSize().divideBy(2));
-		var offset = {};
-		offset.x = centerOffset.x < 0 ? 0 : Math.round(centerOffset.x);
-		offset.y = Math.round(centerOffset.y);
-		return offset;
-	},
-
 	setView: function (center, zoom, reset) {
 		zoom = zoom === undefined ? this._zoom : this._limitZoom(zoom);
 		center = this._limitCenter(L.latLng(center), zoom, this.options.maxBounds);
