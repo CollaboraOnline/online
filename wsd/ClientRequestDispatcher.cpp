398cf4e0dc4ef716978c4edc2c78834589280f4c
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2025-04-22 07:39:43 -0400

    wsd: cleanup ClientRequestDispatcher::handleWopiAccessCheckRequest()
    Change-Id: I2ba36c38bddb677f6efedf22a811b847638e18e3
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- wsd/ClientRequestDispatcher.cpp
+++ wsd/ClientRequestDispatcher.cpp
@@ -1158,20 +1158,16 @@ bool ClientRequestDispatcher::handleWopiAccessCheckRequest(const Poco::Net::HTTP
 
     LOG_DBG("Wopi Access Check request: " << request.getURI());
 
-    Poco::MemoryInputStream startmessage(&socket->getInBuffer()[0], socket->getInBuffer().size());
-    StreamSocket::MessageMap map;
-
-    Poco::JSON::Object::Ptr jsonObject;
-
     std::string text(std::istreambuf_iterator<char>(message), {});
     LOG_TRC("Wopi Access Check request text: " << text);
 
     std::string callbackUrlStr;
 
+    Poco::JSON::Object::Ptr jsonObject;
     if (!JsonUtil::parseJSON(text, jsonObject))
     {
-        LOG_WRN_S("Wopi Access Check request error, json object expected got ["
-                  << text << "] on request to URL: " << request.getURI());
+        LOG_WRN("Wopi Access Check request error, json object expected got ["
+                << text << "] on request to URL: " << request.getURI());
 
         HttpHelper::sendErrorAndShutdown(http::StatusCode::BadRequest, socket);
         return false;
@@ -1179,8 +1175,8 @@ bool ClientRequestDispatcher::handleWopiAccessCheckRequest(const Poco::Net::HTTP
 
     if (!JsonUtil::findJSONValue(jsonObject, "callbackUrl", callbackUrlStr))
     {
-        LOG_WRN_S("Wopi Access Check request error, missing key callbackUrl expected got ["
-                  << text << "] on request to URL: " << request.getURI());
+        LOG_WRN("Wopi Access Check request error, missing key callbackUrl expected got ["
+                << text << "] on request to URL: " << request.getURI());
 
         HttpHelper::sendErrorAndShutdown(http::StatusCode::BadRequest, socket);
         return false;
@@ -1190,12 +1186,13 @@ bool ClientRequestDispatcher::handleWopiAccessCheckRequest(const Poco::Net::HTTP
 
     std::string scheme, host, portStr, pathAndQuery;
     if (!net::parseUri(callbackUrlStr, scheme, host, portStr, pathAndQuery)) {
-        LOG_WRN_S("Wopi Access Check request error, invalid url ["
-                  << callbackUrlStr << "] on request to URL: " << request.getURI() << scheme);
+        LOG_WRN("Wopi Access Check request error, invalid url ["
+                << callbackUrlStr << "] on request to URL: " << request.getURI() << scheme);
 
         HttpHelper::sendErrorAndShutdown(http::StatusCode::BadRequest, socket);
         return false;
     }
+
     http::Session::Protocol protocol = http::Session::Protocol::HttpSsl;
     ulong port = 443;
     if (scheme == "https://" || scheme.empty()) {
@@ -1204,8 +1201,8 @@ bool ClientRequestDispatcher::handleWopiAccessCheckRequest(const Poco::Net::HTTP
         protocol = http::Session::Protocol::HttpUnencrypted;
         port = 80;
     } else {
-        LOG_WRN_S("Wopi Access Check request error, bad request protocol ["
-                  << text << "] on request to URL: " << request.getURI() << scheme);
+        LOG_WRN("Wopi Access Check request error, bad request protocol ["
+                << text << "] on request to URL: " << request.getURI() << scheme);
 
         HttpHelper::sendErrorAndShutdown(http::StatusCode::BadRequest, socket);
         return false;
@@ -1220,9 +1217,8 @@ bool ClientRequestDispatcher::handleWopiAccessCheckRequest(const Poco::Net::HTTP
             HttpHelper::sendErrorAndShutdown(http::StatusCode::BadRequest, socket);
             return false;
         } catch(std::exception &exception) {
-
-            LOG_WRN_S("Wopi Access Check request error, bad request invalid porl ["
-                  << text << "] on request to URL: " << request.getURI());
+            LOG_WRN("Wopi Access Check request error, bad request invalid porl ["
+                    << text << "] on request to URL: " << request.getURI());
 
             HttpHelper::sendErrorAndShutdown(http::StatusCode::BadRequest, socket);
             return false;
