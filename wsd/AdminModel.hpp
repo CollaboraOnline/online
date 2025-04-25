5c836190a321f8d85740b9dc29a4afc60d9db8ce
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2025-04-25 07:08:22 -0400

    wsd: admin: cleanup AdminModel
    Change-Id: Ifd607f417b31b8d4ef9448bdf24905b2a58ebd9c
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- wsd/AdminModel.hpp
+++ wsd/AdminModel.hpp
@@ -26,7 +26,7 @@
 struct DocumentAggregateStats;
 
 /// A client view in Admin controller.
-class View
+class View final
 {
 public:
     View(std::string sessionId, std::string userName, std::string userId, bool readOnly)
@@ -127,7 +127,7 @@ private:
 };
 
 /// Containing basic information about document
-class DocBasicInfo
+class DocBasicInfo final
 {
     std::string _docKey;
     std::time_t _idleTime;
@@ -153,7 +153,7 @@ public:
 };
 
 /// A document in Admin controller.
-class Document
+class Document final
 {
     // cf. FILE* member.
     Document(const Document &) = delete;
@@ -310,7 +310,7 @@ private:
 };
 
 /// An Admin session subscriber.
-class Subscriber
+class Subscriber final
 {
 public:
     explicit Subscriber(std::weak_ptr<WebSocketHandler> ws)
@@ -346,7 +346,7 @@ private:
 };
 
 /// The Admin controller implementation.
-class AdminModel
+class AdminModel final
 {
     AdminModel(const AdminModel &) = delete;
     AdminModel& operator = (const AdminModel &) = delete;
@@ -499,11 +499,11 @@ private:
     /// We check the owner even in the release builds, needs to be always correct.
     std::thread::id _owner;
 
-    std::string _currentMigDoc = std::string();
+    std::string _currentMigDoc;
 
-    std::string _currentMigToken = std::string();
+    std::string _currentMigToken;
 
-    std::string _targetMigServerId = std::string();
+    std::string _targetMigServerId;
 
     unsigned _memStatsSize = 100;
     unsigned _cpuStatsSize = 100;
