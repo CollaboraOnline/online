9444bd5f17fd14f66f6df1f700439c1e94678a05
Gökay Şatır <gokaysatir@gmail.com>
2025-04-28 15:11:50 +0300

    CellCursorSection: Remove position checks of split-panes.
    CanvasSectionContainer does it for the document object sections now.
    
    Signed-off-by: Gökay Şatır <gokaysatir@gmail.com>
    Change-Id: Icbae535107883a5f702434024f3bb29337163dde

--- browser/src/canvas/sections/CellCursorSection.ts
+++ browser/src/canvas/sections/CellCursorSection.ts
@@ -23,8 +23,6 @@ class CellCursorSection extends CanvasSectionObject {
 		this.sectionProperties.viewId = viewId;
 		this.sectionProperties.weight = weight;
 		this.sectionProperties.color = color;
-		this.sectionProperties.paneIndex = null;
-		this.sectionProperties.paneCount = null;
 	}
 
 	public getViewId(): number {
@@ -35,83 +33,30 @@ class CellCursorSection extends CanvasSectionObject {
 		this.sectionProperties.viewId = viewId;
 	}
 
-	private getContainingPane() {
-		const rectangles = app.getViewRectangles();
-
-		this.sectionProperties.paneIndex = -1;
-		this.sectionProperties.paneCount = 1;
-
-		if (rectangles.length > 1) { // We have split panes.
-			this.sectionProperties.paneCount = rectangles.length;
-			for (let i = 0; i < rectangles.length; i++) {
-				if (rectangles[i].pContainsRectangle([
-						this.position[0],
-						this.position[1],
-						app.calc.cellCursorRectangle.pWidth,
-						app.calc.cellCursorRectangle.pHeight
-					]
-				)) {
-					this.sectionProperties.paneIndex = i;
-					return rectangles[i];
-				}
-			}
-			return null;
-		}
-		else
-			return app.calc.cellCursorRectangle;
-	}
-
 	public onDraw() {
-		const pane = this.getContainingPane();
-
-		if (app.calc.cellCursorVisible && pane) {
+		if (app.calc.cellCursorVisible) {
 			this.context.lineJoin = 'miter';
 			this.context.lineCap = 'butt';
 			this.context.lineWidth = 1;
 
 			this.context.strokeStyle = this.sectionProperties.color;
 
-			let penX = this.myTopLeft[0];
-			let penY = this.myTopLeft[1];
-
-			const movePen = this.sectionProperties.paneIndex !== -1 && this.sectionProperties.paneCount - 1 !== this.sectionProperties.paneIndex;
-
-			if (movePen) {
-				if (pane.x1 === 0) penX = this.position[0] + this.containerObject.getDocumentAnchor()[0];
-				if (pane.y1 === 0) penY = this.position[1] + this.containerObject.getDocumentAnchor()[1];
-
-				this.context.translate(penX - this.myTopLeft[0], penY - this.myTopLeft[1]);
-			}
-
 			let x: number = 0;
 			if (app.calc.isRTL()) {
 				const rightMost = this.containerObject.getDocumentAnchor()[0] + this.containerObject.getDocumentAnchorSection().size[0];
-				x = rightMost - penX * 2 - app.calc.cellCursorRectangle.pWidth;
+				x = rightMost - this.size[0];
 			}
 
 			for (let i: number = 0; i < this.sectionProperties.weight; i++)
-				this.context.strokeRect(x + -0.5 - i, -0.5 - i, app.calc.cellCursorRectangle.pWidth + i * 2, app.calc.cellCursorRectangle.pHeight + i * 2);
+				this.context.strokeRect(x + -0.5 - i, -0.5 - i, this.size[0] + i * 2, this.size[1] + i * 2);
 
 			if (window.prefs.getBoolean('darkTheme')) {
 				this.context.strokeStyle = 'white';
 				const diff = 1;
-				this.context.strokeRect(x + -0.5 + diff, -0.5 + diff, app.calc.cellCursorRectangle.pWidth - 2 * diff, app.calc.cellCursorRectangle.pHeight - 2 * diff);
-				this.context.strokeRect(x + -0.5 + diff, -0.5 + diff, app.calc.cellCursorRectangle.pWidth - 2 * diff, app.calc.cellCursorRectangle.pHeight - 2 * diff);
+				this.context.strokeRect(x + -0.5 + diff, -0.5 + diff, this.size[0] - 2 * diff, this.size[1] - 2 * diff);
+				this.context.strokeRect(x + -0.5 + diff, -0.5 + diff, this.size[0] - 2 * diff, this.size[1] - 2 * diff);
 			}
-
-			if (movePen)
-				this.context.translate(-penX + this.myTopLeft[0], -penY + this.myTopLeft[1]);
 		}
-	}
-
-	onNewDocumentTopLeft(size: Array<number>): void {
-		if (this.getContainingPane())
-			this.isVisible = true;
-	}
-
-	onCellAddressChanged(): void {
-		if (this.getContainingPane())
-			this.isVisible = true;
 	}
 }
 
