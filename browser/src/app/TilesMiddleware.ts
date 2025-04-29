729130296f77892e1b8c01049dbe5768a9f9009f
Chris Lord <chris.lord@collabora.com>
2025-04-29 15:00:07 +0100

    Don't expand the current area when we aren't keeping up with dehydration
    If we can't keep up with tile rehydration (which occurs commonly when
    dragging the scroll handle to cover a large distance quickly), don't
    expand the current area. This reduces the amount of dehydration work
    required and lets us push out more frames.
    
    Signed-off-by: Chris Lord <chris.lord@collabora.com>
    Change-Id: I29c223985a63da815ff0a600694c8aaef7afca52

--- browser/src/app/TilesMiddleware.ts
+++ browser/src/app/TilesMiddleware.ts
@@ -243,6 +243,7 @@ class TileManager {
 	// updating during scrolling
 	private static directionalTileExpansion: number = 2;
 	private static pausedForDehydration: boolean = false;
+	private static shrinkCurrentId: any = null;
 
 	//private static _debugTime: any = {}; Reserved for future.
 
@@ -1477,9 +1478,10 @@ class TileManager {
 		for (var rangeIdx = 0; rangeIdx < tileRanges.length; ++rangeIdx) {
 			// Expand the 'current' area to add a small buffer around the visible area that
 			// helps us avoid visible tile updates.
-			const tileRange = isCurrent
-				? this.expandTileRange(tileRanges[rangeIdx])
-				: tileRanges[rangeIdx];
+			const tileRange =
+				isCurrent && !this.shrinkCurrentId
+					? this.expandTileRange(tileRanges[rangeIdx])
+					: tileRanges[rangeIdx];
 
 			for (var j = tileRange.min.y; j <= tileRange.max.y; ++j) {
 				for (var i = tileRange.min.x; i <= tileRange.max.x; ++i) {
@@ -2112,8 +2114,7 @@ class TileManager {
 		if (app.map._docLayer.isCalc() && !app.map._docLayer._gotFirstCellCursor)
 			return;
 
-		// be sure canvas is initialized already, has correct size and that we aren't
-		// currently processing a transaction
+		// be sure canvas is initialized already and has the correct size.
 		const size: any = map.getSize();
 		if (size.x === 0 || size.y === 0) {
 			setTimeout(
@@ -2123,6 +2124,16 @@ class TileManager {
 				1,
 			);
 			return;
+		}
+
+		// If an update occurs while we're paused for dehydration, we haven't been able to
+		// keep up with scrolling. In this case, we should stop expanding the current area
+		// so that it takes less time to dehydrate it.
+		if (this.pausedForDehydration) {
+			if (this.shrinkCurrentId) clearTimeout(this.shrinkCurrentId);
+			this.shrinkCurrentId = setTimeout(() => {
+				this.shrinkCurrentId = null;
+			}, 100);
 		}
 
 		if (app.file.fileBasedView) {
