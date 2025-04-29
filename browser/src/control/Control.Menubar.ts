d29ae1ef539aa0563d6aa564b14ecf434fce99e6
CodingwithKetan <ghoriketan33@gmail.com>
2025-04-30 00:43:38 +0530

    Removed unwanted code related to last modified date
    Signed-off-by: CodingwithKetan <ghoriketan33@gmail.com>
    Change-Id: I69b79de49ade64de72b2bb96047457448d303541

--- browser/src/control/Control.Menubar.ts
+++ browser/src/control/Control.Menubar.ts
@@ -2926,46 +2926,6 @@ class Menubar extends L.Control {
 		return null;
 	}
 
-	/**
-	 * Initializes the modification indicator in the menubar.
-	 * @param lastmodtime - The last modification time.
-	 */
-	private _onInitModificationIndicator(lastmodtime: any): void {
-		var lastModButton = L.DomUtil.get('menu-last-mod');
-		if (lastModButton !== null && lastModButton !== undefined
-			&& lastModButton.firstChild
-			&& lastModButton.firstChild.innerHTML !== null
-			&& lastModButton.firstChild.childElementCount == 0) {
-			if (lastmodtime == null) {
-				// No modification time -> hide the indicator
-				L.DomUtil.setStyle(lastModButton, 'display', 'none');
-				return;
-			}
-			var mainSpan = document.createElement('span');
-			this.lastModIndicator = document.createElement('span');
-			mainSpan.appendChild(this.lastModIndicator);
-
-			// Replace menu button body with new content
-			lastModButton.firstChild.replaceChildren();
-			lastModButton.firstChild.appendChild(mainSpan);
-
-			if (L.Params.revHistoryEnabled) {
-				L.DomUtil.setStyle(lastModButton, 'cursor', 'pointer');
-			}
-
-			this._map.fire('modificationindicatorinitialized');
-		}
-	}
-
-	/**
-       * Updates the modification indicator.
-       * @param e - Event data containing the new modification time.
-       */
-	private _onUpdateModificationIndicator(e: any): void {
-		if (this.lastModIndicator !== null && this.lastModIndicator !== undefined) {
-			this.lastModIndicator.textContent = e.lastSaved;
-		}
-	}
 }
 
 L.control.menubar = () => {
