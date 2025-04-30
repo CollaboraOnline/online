36f0063cd79ee2e938f840602cd3afcf7a0760f9
Gökay Şatır <gokaysatir@gmail.com>
2025-04-30 11:13:00 +0300

    SplitSection - Non functional changes.
    Signed-off-by: Gökay Şatır <gokaysatir@gmail.com>
    Change-Id: I649fcb3b69405df78dd62c55e2c3a572f25a6c18

--- browser/src/canvas/sections/SplitSection.ts
+++ browser/src/canvas/sections/SplitSection.ts
@@ -7,30 +7,38 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
-*/
+ */
 
-class SplitSection extends app.definitions.canvasSectionObject {
+class SplitSection extends CanvasSectionObject {
 	name: string = L.CSections.Debug.Splits.name;
-    interactable: boolean = false;
-    anchor: string[] = ['top', 'left'];
-    processingOrder: number = L.CSections.Debug.Splits.processingOrder;
-    drawingOrder: number = L.CSections.Debug.Splits.drawingOrder;
-    zIndex: number = L.CSections.Debug.Splits.zIndex;
-    boundToSection: string = 'tiles';
-    sectionProperties: any = {
-        docLayer: app.map._docLayer
-    }
+	interactable: boolean = false;
+	anchor: string[] = ['top', 'left'];
+	processingOrder: number = L.CSections.Debug.Splits.processingOrder;
+	drawingOrder: number = L.CSections.Debug.Splits.drawingOrder;
+	zIndex: number = L.CSections.Debug.Splits.zIndex;
+	boundToSection: string = 'tiles';
+	sectionProperties: any = {
+		docLayer: app.map._docLayer,
+	};
 
-    constructor () { super(); }
+	constructor() {
+		super();
+	}
 
-    onDraw(frameCount?: number, elapsedTime?: number): void {
-		var splitPanesContext = this.sectionProperties.docLayer.getSplitPanesContext();
+	onDraw(): void {
+		const splitPanesContext =
+			this.sectionProperties.docLayer.getSplitPanesContext();
 		if (splitPanesContext) {
 			var splitPos = splitPanesContext.getSplitPos();
 			this.context.strokeStyle = 'red';
-			this.context.strokeRect(0, 0, splitPos.x * app.dpiScale, splitPos.y * app.dpiScale);
+			this.context.strokeRect(
+				0,
+				0,
+				splitPos.x * app.dpiScale,
+				splitPos.y * app.dpiScale,
+			);
 		}
-    }
+	}
 }
 
 app.definitions.splitSection = SplitSection;
