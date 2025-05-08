6d2b602784f4e760e956ec5020b9ce6715f7d20c
Pranam Lashkari <lpranam@collabora.com>
2025-05-08 19:18:40 +0530

    slides: focus on preview when using keyboard navigation
    Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
    Change-Id: Ia1d1841d43346a210235440698009155cc89151d

--- browser/src/map/handler/Map.Keyboard.js
+++ browser/src/map/handler/Map.Keyboard.js
@@ -430,6 +430,7 @@ L.Map.Keyboard = L.Handler.extend({
 
 					this._map.deselectAll();
 					this._map.setPart(partToSelect);
+					this._map._docLayer._preview.focusCurrentSlide();
 					if (app.file.fileBasedView)
 						this._map._docLayer._checkSelectedPart();
 				}
