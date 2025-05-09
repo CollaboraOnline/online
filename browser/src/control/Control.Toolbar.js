b915a88845955cbc760c8e658b81a5c2065aa52a
Pedro Pinto Silva <pedro.silva@collabora.com>
2025-05-09 15:19:52 +0200

    Closebutton: Fix alignment
    Signed-off-by: Pedro Pinto Silva <pedro.silva@collabora.com>
    Change-Id: Ic353ef11915260ea7726710b482977d0bc434395

--- browser/src/control/Control.Toolbar.js
+++ browser/src/control/Control.Toolbar.js
@@ -1115,7 +1115,7 @@ function setupToolbar(e) {
 	map.on('updateparts pagenumberchanged', onUpdateParts);
 
 	if (map.options.wopi && L.Params.closeButtonEnabled && !window.mode.isMobile()) {
-		$('#closebuttonwrapper').css('display', 'block');
+		$('#closebuttonwrapper').css('display', 'flex');
 		var button = L.DomUtil.get('closebutton');
 		if (button) {
 			button.setAttribute('data-cooltip', _('Close document'));
@@ -1125,7 +1125,7 @@ function setupToolbar(e) {
 		$('#closebuttonwrapper').hide();
 		$('#closebuttonwrapperseparator').hide();
 	} else if (L.Params.closeButtonEnabled && !window.mode.isMobile()) {
-		$('#closebuttonwrapper').css('display', 'block');
+		$('#closebuttonwrapper').css('display', 'flex');
 	}
 
 	$('#closebutton').click(function () {
