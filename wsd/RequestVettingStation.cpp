31720f9bba48313f5137c3aafdacd93306b0f757
Henry Castro <hcastro@collabora.com>
2025-05-16 08:57:24 -0400

    wsd: fix C++ unit test assertion failures.
    Our C++ unit tests handle manual URL loading.
    It’s better not to disrupt the progress of loading messages.
    
    Change-Id: I1446fc230938a6ecd86892496e51430acc112a2d
    Signed-off-by: Henry Castro <hcastro@collabora.com>

--- wsd/RequestVettingStation.cpp
+++ wsd/RequestVettingStation.cpp
@@ -303,7 +303,10 @@ void RequestVettingStation::handleRequest(const std::string& id,
                              "ClientRequestDispatcher and creating DocBroker for ["
                           << docKey << ']');
 
-            _ws->sendMessage("browsersetting: null");
+            if (!UnitBase::isUnitTesting())
+            {
+                _ws->sendMessage("browsersetting: null");
+            }
 
             // Create the DocBroker.
             if (std::shared_ptr<DocumentBroker> docBroker = createDocBroker(docKey, "",
