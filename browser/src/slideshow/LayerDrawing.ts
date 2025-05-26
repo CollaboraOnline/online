0f32007336595937762ac78fd7e210835c2717e0
codewithvk <vivek.javiya@collabora.com>
2025-05-26 15:38:51 +0530

    fix(slideshow): ensure program initialisation before first video setup
    Move WebGL video program initialisation to handleVideos(), ensuring it's created before any video renderer resources are set up. This commit would suppose to fixes video not loading on the first slide.
    
    Signed-off-by: codewithvk <vivek.javiya@collabora.com>
    Change-Id: I1a857aa7d1302120e8bdeed765bcff9af21ef364

--- browser/src/slideshow/LayerDrawing.ts
+++ browser/src/slideshow/LayerDrawing.ts
@@ -182,6 +182,13 @@ class LayerDrawing {
 		if (!videosInfo || videosInfo.length === 0) return;
 		this.videoRenderers.set(slideHash, []);
 
+		if (
+			!this.layerRenderer.getRenderContext().is2dGl() &&
+			!VideoRendererGl.videoProgramInitialized
+		) {
+			VideoRendererGl.createProgram(this.layerRenderer.getRenderContext());
+		}
+
 		for (let i = 0; i < videosInfo.length; ++i) {
 			const videoInfo = videosInfo[i];
 			this.handleVideo(i, slideHash, videoInfo);
@@ -209,13 +216,6 @@ class LayerDrawing {
 	}
 
 	private drawVideos(slideHash: string) {
-		if (
-			!this.layerRenderer.getRenderContext().is2dGl() &&
-			!VideoRendererGl.videoProgramInitialized
-		) {
-			VideoRendererGl.createProgram(this.layerRenderer.getRenderContext());
-		}
-
 		const videoRenderers = this.videoRenderers.get(slideHash);
 		if (!videoRenderers) return;
 
