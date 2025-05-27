CollaboraOnline/online

commit d82d7ca8e96e6fb125d527cf8ac066a734216115

presentation: use zstd to compress bitmaps for presentations fixes: CollaboraOnline#11740
Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
Change-Id: I544d571978297be976c25f30b03726a3224b0245

browser/src/slideshow/LayerDrawing.ts
@@ -71,6 +71,8 @@ interface LayerInfo {
 	type?: 'bitmap' | 'placeholder' | 'animated';
 	content: LayerContentType;
 	isField?: boolean;
+	width?: number;
+	height?: number;
 }
 
 interface LayerEntry {
@@ -463,19 +465,44 @@ class LayerDrawing {
 		}
 	}
 
+	decompressAndCreateImageData(
+		imgRawData: Uint8Array,
+		width: number,
+		height: number,
+	) {
+		const img = (window as any).fzstd.decompress(imgRawData);
+		const clampedArray = new Uint8ClampedArray(img);
+		return new ImageData(clampedArray, width, height);
+	}
+
 	handleTextFieldMsg(info: LayerInfo, img: any) {
 		const textFieldInfo = info.content as TextFieldInfo;
 		const imageInfo = textFieldInfo.content;
-		if (!this.checkAndAttachImageData(imageInfo, img)) return;
 
-		let textFields = this.slideTextFieldsMap.get(info.slideHash);
-		if (!textFields) {
-			textFields = new Map<string, string>();
-			this.slideTextFieldsMap.set(info.slideHash, textFields);
-		}
-		textFields.set(textFieldInfo.hash, imageInfo.checksum);
+		const imageData = this.decompressAndCreateImageData(
+			img.rawData,
+			info.width,
+			info.height,
+		);
+		createImageBitmap(imageData).then(
+			(img) => {
+				if (!this.checkAndAttachImageData(imageInfo, img)) return;
 
-		this.cachedTextFields.set(imageInfo.checksum, textFieldInfo);
+				let textFields = this.slideTextFieldsMap.get(info.slideHash);
+				if (!textFields) {
+					textFields = new Map<string, string>();
+					this.slideTextFieldsMap.set(info.slideHash, textFields);
+				}
+				textFields.set(textFieldInfo.hash, imageInfo.checksum);
+
+				this.cachedTextFields.set(imageInfo.checksum, textFieldInfo);
+			},
+			(error) => {
+				window.app.console.error(
+					'Failed to create bitmap, Promise failed with error: ' + error,
+				);
+			},
+		);
 	}
 
 	private handleBackgroundMsg(info: LayerInfo, img: any) {
@@ -485,16 +512,30 @@ class LayerDrawing {
 		}
 		if (info.type === 'bitmap') {
 			const imageInfo = info.content as ImageInfo;
-			if (!this.checkAndAttachImageData(imageInfo, img)) return;
+			const imageData = this.decompressAndCreateImageData(
+				img.rawData,
+				info.width,
+				info.height,
+			);
+			createImageBitmap(imageData).then(
+				(img) => {
+					if (!this.checkAndAttachImageData(imageInfo, img)) return;
 
-			const pageHash = slideInfo.background.isCustom
-				? info.slideHash
-				: slideInfo.masterPage;
-			this.backgroundChecksums.set(pageHash, imageInfo.checksum);
-			this.cachedBackgrounds.set(imageInfo.checksum, imageInfo);
+					const pageHash = slideInfo.background.isCustom
+						? info.slideHash
+						: slideInfo.masterPage;
+					this.backgroundChecksums.set(pageHash, imageInfo.checksum);
+					this.cachedBackgrounds.set(imageInfo.checksum, imageInfo);
 
-			this.clearCanvas();
-			this.drawBitmap(imageInfo);
+					this.clearCanvas();
+					this.drawBitmap(imageInfo);
+				},
+				(error) => {
+					window.app.console.error(
+						'Failed to create bitmap, Promise failed with error: ' + error,
+					);
+				},
+			);
 		}
 	}
 
@@ -504,27 +545,54 @@ class LayerDrawing {
 			return;
 		}
 
-		if (info.index === 0 || !this.cachedMasterPages.get(slideInfo.masterPage))
-			this.cachedMasterPages.set(slideInfo.masterPage, new Array<LayerEntry>());
-
-		const layers = this.cachedMasterPages.get(slideInfo.masterPage);
-		if (layers.length !== info.index) {
-			window.app.console.log(
-				'LayerDrawing.handleMasterPageLayerMsg: missed any layers ?',
-			);
-		}
 		const layerEntry: LayerEntry = {
 			type: info.type,
 			content: info.content,
 			isField: info.isField,
 		};
-		if (info.type === 'bitmap') {
-			if (!this.checkAndAttachImageData(layerEntry.content as ImageInfo, img))
-				return;
-		}
-		layers.push(layerEntry);
 
-		this.drawMasterPageLayer(layerEntry, info.slideHash);
+		var drawImage = () => {
+			if (info.index === 0 || !this.cachedMasterPages.get(slideInfo.masterPage))
+				this.cachedMasterPages.set(
+					slideInfo.masterPage,
+					new Array<LayerEntry>(),
+				);
+
+			const layers = this.cachedMasterPages.get(slideInfo.masterPage);
+			if (layers.length !== info.index) {
+				window.app.console.log(
+					'LayerDrawing.handleMasterPageLayerMsg: missed any layers ?',
+				);
+			}
+
+			layers.push(layerEntry);
+
+			this.drawMasterPageLayer(layerEntry, info.slideHash);
+		};
+		if (img.rawData.length !== 0 && info.type === 'bitmap') {
+			const imageData = this.decompressAndCreateImageData(
+				img.rawData,
+				info.width,
+				info.height,
+			);
+			createImageBitmap(imageData).then(
+				(img) => {
+					if (
+						!this.checkAndAttachImageData(layerEntry.content as ImageInfo, img)
+					)
+						return;
+					drawImage();
+				},
+				(error) => {
+					window.app.console.error(
+						'Failed to create bitmap, Promise failed with error: ' + error,
+					);
+				},
+			);
+		} else {
+			img = new Image();
+			img.onload = drawImage;
+		}
 	}
 
 	private handleDrawPageLayerMsg(info: LayerInfo, img: any) {
@@ -541,26 +609,44 @@ class LayerDrawing {
 			type: info.type,
 			content: info.content,
 		};
-		if (info.type === 'bitmap') {
-			if (!this.checkAndAttachImageData(layerEntry.content as ImageInfo, img))
-				return;
-		} else if (info.type === 'animated') {
-			const content = layerEntry.content as AnimatedShapeInfo;
-			if (content.type === 'bitmap') {
-				if (!this.checkAndAttachImageData(content.content as ImageInfo, img))
-					return;
-				const animatedElement = this.helper.getAnimatedElement(
-					info.slideHash,
-					content.hash,
-				);
-				if (animatedElement) {
-					animatedElement.updateAnimationInfo(content);
+		const imageData = this.decompressAndCreateImageData(
+			img.rawData,
+			info.width,
+			info.height,
+		);
+		createImageBitmap(imageData).then(
+			(img) => {
+				if (info.type === 'bitmap') {
+					if (
+						!this.checkAndAttachImageData(layerEntry.content as ImageInfo, img)
+					)
+						return;
+				} else if (info.type === 'animated') {
+					const content = layerEntry.content as AnimatedShapeInfo;
+					if (content.type === 'bitmap') {
+						if (
+							!this.checkAndAttachImageData(content.content as ImageInfo, img)
+						)
+							return;
+						const animatedElement = this.helper.getAnimatedElement(
+							info.slideHash,
+							content.hash,
+						);
+						if (animatedElement) {
+							animatedElement.updateAnimationInfo(content);
+						}
+					}
 				}
-			}
-		}
-		layers.push(layerEntry);
+				layers.push(layerEntry);
 
-		this.drawDrawPageLayer(info.slideHash, layerEntry);
+				this.drawDrawPageLayer(info.slideHash, layerEntry);
+			},
+			(error) => {
+				window.app.console.error(
+					'Failed to create bitmap, Promise failed with error: ' + error,
+				);
+			},
+		);
 	}
 
 	private clearCanvas() {
@@ -730,7 +816,7 @@ class LayerDrawing {
 	}
 
 	private checkAndAttachImageData(imageInfo: ImageInfo, img: any): boolean {
-		if (!img || (imageInfo.type === 'png' && !img.src)) {
+		if (!img) {
 			window.app.console.log(
 				'LayerDrawing.checkAndAttachImageData: no bitmap available.',
 			);

browser/src/core/Socket.js
@@ -594,17 +594,16 @@ app.definitions.Socket = L.Class.extend({
 
 		var isTile = e.textMsg.startsWith('tile:');
 		var isDelta = e.textMsg.startsWith('delta:');
-		if (!isTile && !isDelta &&
-		    !e.textMsg.startsWith('renderfont:') &&
-			!e.textMsg.startsWith('slidelayer:') &&
-		    !e.textMsg.startsWith('windowpaint:'))
+		var isSlideLayer = e.textMsg.startsWith('slidelayer:');
+		if (!isTile && !isDelta && !isSlideLayer && !e.textMsg.startsWith('renderfont:') &&
+			!e.textMsg.startsWith('windowpaint:'))
 			return;
 
 		if (e.textMsg.indexOf(' nopng') !== -1)
 			return;
 
 		// pass deltas through quickly.
-		if (e.imgBytes && (isTile || isDelta) && e.imgBytes[e.imgIndex] != 80 /* P(ng) */)
+		if (e.imgBytes && (isSlideLayer || ((isTile || isDelta) && e.imgBytes[e.imgIndex] != 80 /* P(ng) */)))
 		{
 			// window.app.console.log('Passed through delta object');
 			e.image = { rawData: e.imgBytes.subarray(e.imgIndex),

kit/ChildSession.cpp
@@ -63,6 +63,8 @@
 #include <string>
 #include <string_view>
 #include <utility>
+#include <zlib.h>
+#include <zstd.h>
 
 using Poco::JSON::Object;
 using Poco::JSON::Parser;
@@ -2458,19 +2460,47 @@ bool ChildSession::renderNextSlideLayer(SlideCompressor &scomp,
             if (size_t start = json.find("%IMAGECHECKSUM%"); start != std::string::npos)
                 json.replace(start, 15, std::to_string(pixmapHash));
 
+            {
+                Poco::JSON::Parser parser;
+                Poco::JSON::Object::Ptr root = parser.parse(json).extract<Poco::JSON::Object::Ptr>();
+                root->set("width", width);
+                root->set("height", height);
+                std::stringstream ss;
+                root->stringify(ss);
+                json = ss.str();
+            }
             std::string response = "slidelayer: " + json;
 
             response += "\n";
 
-            output.reserve(response.size() + pixmap->size());
-            output.resize(response.size());
+            size_t compressed_max_size = ZSTD_COMPRESSBOUND(pixmap->size());
+            size_t max_required_size = response.size() + compressed_max_size;
+            output.resize(max_required_size);
             std::memcpy(output.data(), response.data(), response.size());
+            std::vector<char> compressedOutPut;
+            compressedOutPut.resize(ZSTD_COMPRESSBOUND(pixmap->size()));
 
-            if (!Png::encodeSubBufferToPNG(pixmap->data(), 0, 0, width, height, width, height, output, tileMode))
+            if (tileMode == LibreOfficeKitTileMode::LOK_TILEMODE_BGRA)
             {
-                LOG_ERR("Failed to encode into PNG.");
-                output.resize(0);
+                png_row_info rowInfo;
+                rowInfo.rowbytes = pixmap->size();
+                // Following function just needs row size to transform from BGRA to RGBA
+                // We have a flat array so its safe to pass pixmap size as row size
+                Png::unpremultiply_bgra_data(nullptr, &rowInfo, pixmap->data());
             }
+            size_t compSize = ZSTD_compress(&output[response.size()], compressed_max_size,
+                                            pixmap->data(), pixmap->size(), -3);
+
+            if (ZSTD_isError(compSize))
+            {
+                output.resize(0);
+                LOG_ERR("Failed to compress slidelayer of size " << pixmap->size() << " with "
+                                                                 << ZSTD_getErrorName(compSize));
+                return;
+            }
+            output.resize(response.size() + compSize);
+
+            LOG_TRC("Compressed slidelayer of size " << pixmap->size() << " to size " << compSize);
         });
     return true;
 }

commit 80b81161a5548d877a0af6222c83f1aba6aad179

presentation: render text field bitmaps
problem:
regression from d82d7ca8e96e6fb125d527cf8ac066a734216115
since we switched to using bitmaps for text field,
these fields were never invoked for drawing on canvas

Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
Change-Id: I3534671ff878149b1054a87d8e0632548f8e838e

browser/src/slideshow/LayerDrawing.ts
@@ -172,6 +172,7 @@ class LayerDrawing {
 
 		this.drawBackground(slideHash);
 		this.drawMasterPage(slideHash);
+		this.drawTextField(slideHash);
 		this.drawDrawPage(slideHash);
 		this.drawVideos(slideHash);
 	}
@@ -760,6 +761,27 @@ class LayerDrawing {
 				this.drawBitmap(content.content as ImageInfo);
 			}
 		}
+	}
+
+	private drawTextField(slideHash: string) {
+		const slideInfo = this.getSlideInfo(slideHash);
+		if (slideInfo.empty) {
+			return true;
+		}
+
+		const fields = this.slideTextFieldsMap.get(slideHash);
+		if (!fields) {
+			window.app.console.log(
+				'LayerDrawing: No layer cached text field for draw page: ' + slideHash,
+			);
+			return false;
+		}
+
+		for (const field of fields) {
+			const imageInfo = this.cachedTextFields.get(field[1]).content;
+			this.drawBitmap(imageInfo);
+		}
+		return true;
 	}
 
 	private drawBitmap(imageInfo: ImageInfo | ImageBitmap) {