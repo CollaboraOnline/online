492a3584552f81812be99b131dc3c4157419aa6e
Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>
2025-05-28 16:51:29 +0530

    Fix label with access key is reported wrongly by screen reader
    - As an example let's take the 'Structure' label in the Entries tab for the Table of Contents dialog.
    - When hovering on the "Structure" label I see NVDA to report it properly on the first time only, then it reports just 'tructure' or just 'S'
    - This is because we did split Text element inside label in two parts
    - which creates a problem for screen readers.
    
    Solution:
    - To Fix this i removed `<u>` element for accesskey only When scree reader is enabled.
    - So in this case screen reader will read the whole label text properly.
    - This fix will work for all cases.
        - if user using screen reader then it will read the whole label text
        - if user is not using screen reader then it enable the stress key for normal easy access functionality.
    
    Signed-off-by: Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>
    Change-Id: Id1326c2f76c1bf14c76f6a4b9c7201c3d222b10b

--- browser/src/control/Control.JSDialogBuilder.js
+++ browser/src/control/Control.JSDialogBuilder.js
@@ -429,7 +429,7 @@ L.Control.JSDialogBuilder = L.Control.extend({
 	},
 
 	_stressAccessKey: function(element, accessKey) {
-		if (!accessKey || window.mode.isMobile())
+		if (!accessKey || window.mode.isMobile() || window.getAccessibilityState())
 			return;
 
 		var text = element.textContent;
