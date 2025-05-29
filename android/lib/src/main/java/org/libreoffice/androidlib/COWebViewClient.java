2e7bd56a9774827254cc8e3a0221373f95303d51
Skyler Grey <skyler.grey@collabora.com>
2025-05-29 14:04:06 +0000

    feat: allow requests to android MobileSocket
    On Android, we're connecting the mobile socket through our existing
    request interception code. Therefore, we'll need to hook up so that
    messages to our /cool/mobilesocket endpoint go to the mobile socket.
    
    I currently haven't handled invalid paths (e.g. ones that are short
    enough to not have a command piece) and these will crash the app. There
    is currently no code that will send to these paths, and we can't recieve
    any content from outside of the webview so I don't believe that this is
    currently a threat
    
    Signed-off-by: Skyler Grey <skyler.grey@collabora.com>
    Change-Id: Ie1d48e52a142831caea7406638863195aadfb306

--- android/lib/src/main/java/org/libreoffice/androidlib/COWebViewClient.java
+++ android/lib/src/main/java/org/libreoffice/androidlib/COWebViewClient.java
@@ -22,11 +22,19 @@ import java.io.InputStream;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 
 public class COWebViewClient extends WebViewClient {
+    private MobileSocket mobileSocket;
+
+    public COWebViewClient() {
+        super();
+        mobileSocket = new MobileSocket();
+    }
+
     @Nullable
     @Override
     public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
@@ -37,6 +45,8 @@ public class COWebViewClient extends WebViewClient {
         String path = request.getUrl().getPath();
         if (Objects.equals(path, "/cool/media")) {
             return handleMediaRequest(request);
+        } else if (path != null && path.startsWith("/cool/mobilesocket/")) {
+            return handleMobileSocketRequest(request);
         } else {
             Map<String, String> responseHeaders = new HashMap<>();
             responseHeaders.put("Access-Control-Allow-Origin", "null"); // Yes, the origin really is 'null' for 'file:' origins
@@ -130,6 +140,39 @@ public class COWebViewClient extends WebViewClient {
             responseHeaders,
             data
         );
+    }
+
+    private WebResourceResponse handleMobileSocketRequest(WebResourceRequest request) {
+        // this'll be something like [ 'cool', 'mobilesocket', 'cool', 'wopipath', 'ws', 'ws', command, 'open', id ]
+        List<String> path = request.getUrl().getPathSegments();
+
+        if (path.size() < 7) {
+            Map<String, String> responseHeaders = new HashMap<>();
+            responseHeaders.put("Access-Control-Allow-Origin", "null"); // Yes, the origin really is 'null' for 'file:' origins
+           	responseHeaders.put("Content-Length", "0");
+
+            ByteArrayInputStream data = new ByteArrayInputStream(new byte[0]);
+
+            return new WebResourceResponse(
+                null,
+                null,
+                400,
+                "Bad Request",
+                responseHeaders,
+                data
+            );
+        }
+
+        if (path.get(6).equals("open")) {
+            return mobileSocket.open();
+        }
+
+        // Anything except open can just be treated as "write". In the Android app we can't send data in "write"s anyway...
+        return mobileSocket.write();
+    }
+
+    public MobileSocket getMobileSocket() {
+        return mobileSocket;
     }
 
     private native String getEmbeddedMediaPath(String tag);
