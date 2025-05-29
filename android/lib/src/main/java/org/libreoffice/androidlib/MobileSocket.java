64779885c320f12229397b5c1a40c2e86a39b457
Skyler Grey <skyler.grey@collabora.com>
2025-05-29 14:04:06 +0000

    feat: add android MobileSocket send implementation
    I'm about to use a mobile socket, based on the existing ProxySockets
    used in-case of a restrictive reverse-proxy, to send data between the
    webview and native code.
    
    This is an un-hooked-in implemetation of the mobile socket sending code
    for Android... there's no need to have receiving code on this side since
    as we can't read the bodies of sent messages on intercepted requests on
    Android so we will have to continue to use our existing send code.
    
    In the upcoming patches, I will hook this in to the /cool/mobilesocket
    endpoint and start sending to it from mobile
    
    Signed-off-by: Skyler Grey <skyler.grey@collabora.com>
    Change-Id: Ibdcfe115ac117fae3296286d27c294ea371ed1ce

---
+++ android/lib/src/main/java/org/libreoffice/androidlib/MobileSocket.java
@@ -0,0 +1,143 @@
+package org.libreoffice.androidlib;
+
+import android.util.Log;
+import android.webkit.WebResourceResponse;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class MobileSocket {
+    public interface Callback {
+        void call();
+    }
+
+    int serial = 0;
+    BlockingQueue<byte[]> sendingMessages;
+    ExecutorService socketMessageExecutor;
+
+    private static String LOGTAG = MobileSocket.class.getSimpleName();
+
+    public MobileSocket() {
+        sendingMessages = new LinkedBlockingQueue<>();
+        socketMessageExecutor = Executors.newFixedThreadPool(1);
+    }
+
+    public void sendMessage(byte[] message, PipedOutputStream output) throws IOException {
+        this.serial = this.serial + 1;
+
+        byte[] serialString = Integer.toString(this.serial, 16).getBytes();
+        byte[] sizeString = Integer.toString(message.length, 16).getBytes();
+
+        ByteBuffer header = ByteBuffer.allocate(
+                1 /* type */
+                        + 2 /* hexadecimal preamble */
+                        + serialString.length
+                        + 1 /* hexadecimal end */
+                        + 2 /* hexadecimal preamble */
+                        + sizeString.length
+                        + 1 /* hexadecimal end */
+        );
+
+        boolean binary = false;
+        for (byte b : message) {
+            if (b == '\n') {
+                binary = true;
+                break;
+            }
+        }
+
+        header.put((byte) (binary ? 'B' : 'T'));
+
+        // Preamble
+        header.put((byte) '0');
+        header.put((byte) 'x');
+
+        header.put(serialString);
+        header.put((byte) '\n'); // End of serial
+
+        // Preamble
+        header.put((byte) '0');
+        header.put((byte) 'x');
+
+        header.put(sizeString);
+        header.put((byte) '\n'); // End of size
+
+        output.write(header.array());
+        output.write(message);
+        output.write((byte) '\n'); // End of message
+    }
+
+    public void flushMessageBuffer(PipedOutputStream output) throws IOException {
+        ArrayList<byte[]> messages = new ArrayList<>();
+        sendingMessages.drainTo(messages);
+
+        if (!messages.isEmpty()) {
+            Log.d(LOGTAG, "Flushing " + messages.size() + " messages that were queued for MobileSocket sending");
+        }
+
+        for (byte[] message : messages) {
+            sendMessage(message, output);
+        }
+        output.close();
+    }
+
+    public void queueSend(byte[] message, Callback callback) throws InterruptedException {
+        sendingMessages.put(message);
+        callback.call();
+    }
+
+    public WebResourceResponse open() {
+        Map<String, String> responseHeaders = new HashMap<>();
+        responseHeaders.put("Access-Control-Allow-Origin", "null"); // Yes, the origin really is 'null' for 'file:' origins
+
+        this.serial = 0;
+        return new WebResourceResponse(
+                null,
+                null,
+                200,
+                "OK",
+                responseHeaders,
+                new ByteArrayInputStream("mobile".getBytes())
+        );
+    }
+
+    public WebResourceResponse write() {
+        Map<String, String> responseHeaders = new HashMap<>();
+        responseHeaders.put("Access-Control-Allow-Origin", "null"); // Yes, the origin really is 'null' for 'file:' origins
+
+        PipedInputStream data;
+        try {
+            PipedOutputStream output = new PipedOutputStream();
+            data = new PipedInputStream(output);
+
+            socketMessageExecutor.execute(() -> {
+                try {
+                    flushMessageBuffer(output);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new WebResourceResponse(
+                null,
+                null,
+                200,
+                "OK",
+                responseHeaders,
+                data
+        );
+    }
+}
