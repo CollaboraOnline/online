CollaboraOnline/online

commit a0b123add6730c0811d9ec1a93f7f89204937592

wsd: literal strings -> string_view
Change-Id: Ic37a4a39f12080a44647d3018ebec40bf0c82cf5
Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

wsd/ClientRequestDispatcher.cpp
@@ -766,9 +766,9 @@ void ClientRequestDispatcher::handleIncomingMessage(SocketDisposition& dispositi
         {
             // File server
             assert(socket && "Must have a valid socket");
-            constexpr auto ProxyRemote = "/remote/";
-            constexpr auto ProxyRemoteLen = sizeof(ProxyRemote) - 1;
-            constexpr auto ProxyRemoteStatic = "/remote/static/";
+            constexpr std::string_view ProxyRemote = "/remote/";
+            constexpr auto ProxyRemoteLen = ProxyRemote.size();
+            constexpr std::string_view ProxyRemoteStatic = "/remote/static/";
             const auto uri = requestDetails.getURI();
             const auto pos = uri.find(ProxyRemoteStatic);
             if (pos != std::string::npos)

common/SigUtil.cpp
@@ -492,7 +492,7 @@ void requestShutdown()
         {
             if (UnattendedRun)
             {
-                static constexpr auto msg =
+                static constexpr std::string_view msg =
                     "Crashed in unattended run and won't wait for debugger. Re-run without "
                     "--unattended to attach a debugger.";
                 std::cerr << msg << std::endl;

wsd/RequestDetails.cpp
@@ -134,8 +134,8 @@ RequestDetails::RequestDetails(const std::string& wopiSrc, const std::vector<std
 void RequestDetails::dehexify()
 {
     // For now, we only hexify cool/ URLs.
-    constexpr auto Prefix = "cool/0x";
-    constexpr auto PrefixLen = sizeof(Prefix) - 1;
+    constexpr std::string_view Prefix = "cool/0x";
+    constexpr auto PrefixLen = Prefix.size();
 
     const auto hexPos = _uriString.find(Prefix);
     if (hexPos != std::string::npos)

commit a09c4bcd2781e5dfece4b44202a63f50178e687a

fix off-by-one error that prevented dowloading LibreOffice Technology logo
regression from a0b123add6730c0811d9ec1a93f7f89204937592

Signed-off-by: Andras Timar <andras.timar@collabora.com>
Change-Id: I8f2b74423fd5c33068b8bb36f6701663f6970564

wsd/ClientRequestDispatcher.cpp
@@ -804,7 +804,7 @@ void ClientRequestDispatcher::handleIncomingMessage(SocketDisposition& dispositi
                     std::string proxyRatingServer =
                         !isUnitTesting ? ProxyRequestHandler::getProxyRatingServer()
                                        : UnitWSD::get().getProxyRatingServer();
-                    ProxyRequestHandler::handleRequest(uri.substr(pos + ProxyRemoteLen), socket,
+                    ProxyRequestHandler::handleRequest(uri.substr(pos + ProxyRemoteLen - 1), socket,
                                                        proxyRatingServer);
                     servedSync = true;
                 }