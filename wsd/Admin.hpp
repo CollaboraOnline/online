365f9738b9d5ad6ecc378edd9bcbc44dbd4e29bc
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2025-06-03 05:30:35 -0400

    wsd: give admin sessions their own IDs
    Change-Id: I416ff7836634df6217f2de4a4ed4f4e5564a2633
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- wsd/Admin.hpp
+++ wsd/Admin.hpp
@@ -51,6 +51,9 @@ private:
     Admin* _admin;
     int _sessionId;
     bool _isAuthenticated;
+
+    /// The next unique session-ID.
+    static std::atomic<uint64_t> NextSessionId;
 };
 
 class MonitorSocketHandler : public AdminSocketHandler
