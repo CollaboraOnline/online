e3b373b72810444bfd3b163bf5141d29003e4b21
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2025-06-03 05:30:35 -0400

    wsd: give admin sessions their own IDs
    Change-Id: I416ff7836634df6217f2de4a4ed4f4e5564a2633
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- wsd/Admin.cpp
+++ wsd/Admin.cpp
@@ -427,6 +427,8 @@ void AdminSocketHandler::handleMessage(const std::vector<char> &payload)
     }
 }
 
+std::atomic<uint64_t> AdminSocketHandler::NextSessionId(1);
+
 AdminSocketHandler::AdminSocketHandler(Admin* adminManager,
                                        const std::weak_ptr<StreamSocket>& socket,
                                        const Poco::Net::HTTPRequest& request,
@@ -435,8 +437,7 @@ AdminSocketHandler::AdminSocketHandler(Admin* adminManager,
     , _admin(adminManager)
     , _isAuthenticated(false)
 {
-    // Different session id pool for admin sessions (?)
-    _sessionId = Util::decodeId(COOLWSD::GetConnectionId());
+    _sessionId = NextSessionId++;
     _clientIPAdress = socket.lock()->clientAddress();
 }
 
@@ -445,7 +446,7 @@ AdminSocketHandler::AdminSocketHandler(Admin* adminManager)
       _admin(adminManager),
       _isAuthenticated(true)
 {
-    _sessionId = Util::decodeId(COOLWSD::GetConnectionId());
+    _sessionId = NextSessionId++;
 }
 
 void AdminSocketHandler::sendTextFrame(const std::string& message)
