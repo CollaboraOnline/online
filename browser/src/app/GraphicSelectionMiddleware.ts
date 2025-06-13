4abd65889297fde720351c813416f96bc7dbd830
Henry Castro <hcastro@collabora.com>
2025-06-13 09:30:55 -0400

    browser: accessibility: add subtitles to embedded videos
    Change-Id: I8e8e04f7420792d911a94777ff13555066e87c95
    Signed-off-by: Henry Castro <hcastro@collabora.com>

--- browser/src/app/GraphicSelectionMiddleware.ts
+++ browser/src/app/GraphicSelectionMiddleware.ts
@@ -82,6 +82,10 @@ class GraphicSelection {
 			'" type="' +
 			videoDesc.mimeType +
 			'"/>\
+					<track src="' +
+			videoDesc.srt +
+			'" kind="subtitles"' +
+			'"/>\
 				</video>\
 			</body>\
 		</foreignObject>\
