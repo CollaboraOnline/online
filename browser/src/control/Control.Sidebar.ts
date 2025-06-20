990298e35fb4aa65cb81e3b27415065e82c11270
Szymon Kłos <szymon.klos@collabora.com>
2025-06-20 11:29:03 +0000

    sidebar: do not shift the view on appearing
    Fix for glitch #12180
    caused by commit 065add482c770f3eb4f12622bca5bc0b6982f4e6
    browser: accessibility: focus the first focusable child element of the sidebar container
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I1c6daddfbb43d9a669d2d9ab6c493158c8e850ea

--- browser/src/control/Control.Sidebar.ts
+++ browser/src/control/Control.Sidebar.ts
@@ -16,6 +16,7 @@
 // /* global app */
 class Sidebar extends SidebarBase {
 	targetDeckCommand: string;
+	isUserRequest: boolean; /// automatic or user request to show the sidebar
 
 	constructor(
 		map: any,
@@ -24,6 +25,7 @@ class Sidebar extends SidebarBase {
 		} /* Default speed: to be used on load */,
 	) {
 		super(map, options, SidebarType.Sidebar);
+		this.isUserRequest = true;
 	}
 
 	onAdd(map: ReturnType<typeof L.map>) {
@@ -34,6 +36,10 @@ class Sidebar extends SidebarBase {
 	onRemove() {
 		super.onRemove();
 		this.map.off('sidebar');
+	}
+
+	setAsInitialized() {
+		this.isUserRequest = false;
 	}
 
 	updateSidebarPrefs(currentDeck: string) {
@@ -137,9 +143,19 @@ class Sidebar extends SidebarBase {
 				this.builder.build(this.container, [sidebarData]);
 				if (!this.isVisible()) {
 					$('#sidebar-dock-wrapper').addClass('visible');
-					const focusables = JSDialog.GetFocusableElements(this.container);
-					if (focusables && focusables.length) {
-						focusables[0].focus();
+
+					// schedule focus after animation so it will not shift the browser page
+					if (this.isUserRequest) {
+						setTimeout(() => {
+							app.layoutingService.appendLayoutingTask(() => {
+								const focusables = JSDialog.GetFocusableElements(
+									this.container,
+								);
+								if (focusables && focusables.length) {
+									focusables[0].focus();
+								}
+							});
+						}, 250); // see animation time in #sidebar-dock-wrapper.visible
 					}
 				}
 
@@ -147,6 +163,8 @@ class Sidebar extends SidebarBase {
 			} else {
 				this.closeSidebar();
 			}
+
+			this.isUserRequest = true;
 		}
 	}
 }
