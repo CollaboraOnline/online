CollaboraOnline/online

commit 065add482c770f3eb4f12622bca5bc0b6982f4e6

browser: accessibility: focus the first focusable child element of the sidebar container
Change-Id: I5971b80302153e4d55066fe76793ac13a3d7bbde
Signed-off-by: Henry Castro <hcastro@collabora.com>

browser/src/control/Control.Sidebar.ts
@@ -144,6 +144,14 @@ class Sidebar extends SidebarBase {
 				this.builder.build(this.container, [sidebarData]);
 				if (!this.isVisible()) $('#sidebar-dock-wrapper').addClass('visible');
 
+				if (this.enableFocus === true) {
+					const focusables = JSDialog.GetFocusableElements(this.container);
+					if (focusables && focusables.length) {
+						focusables[0].focus();
+					}
+					this.enableFocus = false;
+				}
+
 				this.map.uiManager.setDocTypePref('ShowSidebar', true);
 			} else {
 				this.closeSidebar();

browser/src/control/Control.SidebarBase.ts
@@ -33,6 +33,7 @@ abstract class SidebarBase {
 	documentContainer: HTMLDivElement;
 	wrapper: HTMLElement;
 	builder: any;
+	enableFocus: boolean;
 
 	constructor(
 		map: any,

commit e0b7bf283e6d48545b36ec14df7ddd6a2d11dcec

sidebar: do not shift the view on appearing
Fix for glitch #12180
caused by commit 065add482c770f3eb4f12622bca5bc0b6982f4e6
browser: accessibility: focus the first focusable child element of the sidebar container

Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
Change-Id: I1c6daddfbb43d9a669d2d9ab6c493158c8e850ea

browser/css/cool.css
@@ -338,6 +338,7 @@ body {
 
 #sidebar-dock-wrapper.visible {
 	display: block;
+	/* synchronize time with Sidebar.ts setting focus */
 	animation: 200ms ease-out appear-from-right;
 	border-inline-start: 1px solid var(--color-border);
 }

browser/src/control/Control.Sidebar.ts
@@ -16,6 +16,7 @@
 // /* global app */
 class Sidebar extends SidebarBase {
 	targetDeckCommand: string;
+	isUserRequest: boolean; /// automatic or user request to show the sidebar
 
 	constructor(
 		map: any,
@@ -24,6 +25,7 @@ class Sidebar extends SidebarBase {
 		} /* Default speed: to be used on load */,
 	) {
 		super(map, options, SidebarType.Sidebar);
+		this.isUserRequest = true;
 	}
 
 	onAdd(map: ReturnType<typeof L.map>) {
@@ -34,6 +36,10 @@ class Sidebar extends SidebarBase {
 	onRemove() {
 		super.onRemove();
 		this.map.off('sidebar');
+	}
+
+	setAsInitialized() {
+		this.isUserRequest = false;
 	}
 
 	updateSidebarPrefs(currentDeck: string) {
@@ -137,9 +143,19 @@ class Sidebar extends SidebarBase {
 				this.builder.build(this.container, [sidebarData]);
 				if (!this.isVisible()) {
 					$('#sidebar-dock-wrapper').addClass('visible');
-					const focusables = JSDialog.GetFocusableElements(this.container);
-					if (focusables && focusables.length) {
-						focusables[0].focus();
+
+					// schedule focus after animation so it will not shift the browser page
+					if (this.isUserRequest) {
+						setTimeout(() => {
+							app.layoutingService.appendLayoutingTask(() => {
+								const focusables = JSDialog.GetFocusableElements(
+									this.container,
+								);
+								if (focusables && focusables.length) {
+									focusables[0].focus();
+								}
+							});
+						}, 250); // see animation time in #sidebar-dock-wrapper.visible
 					}
 				}
 
@@ -147,6 +163,8 @@ class Sidebar extends SidebarBase {
 			} else {
 				this.closeSidebar();
 			}
+
+			this.isUserRequest = true;
 		}
 	}
 }

browser/src/control/Control.UIManager.ts
@@ -579,7 +579,9 @@ class UIManager extends L.Control {
 				this.map.sidebar.setupTargetDeck('.uno:SidebarDeck.StyleListDeck');
 			}
 
-			if (!showSidebar)
+			if (showSidebar)
+				this.map.sidebar.setAsInitialized();
+			else
 				app.socket.sendMessage('uno .uno:SidebarHide');
 		}
 		else if (window.mode.isChromebook()) {

cypress_test/integration_tests/desktop/writer/sidebar_spec.js
@@ -66,7 +66,7 @@ describe(['tagdesktop'], 'Sidebar tests', function() {
 	});
 
 	it('Focus on the first focusable child', function() {
-		cy.cGet('#sidebar-dock-wrapper').then(function(sidebar) {
+		cy.cGet('#sidebar-dock-wrapper.visible').then(function(sidebar) {
 			helper.containsFocusElement(sidebar[0], false);
 		});
 
@@ -79,7 +79,9 @@ describe(['tagdesktop'], 'Sidebar tests', function() {
 		cy.cGet('#sidebar-panel').should('be.visible');
 		cy.cGet('#sidebar-panel').should('not.be.empty');
 
-		cy.cGet('#sidebar-dock-wrapper').then(function(sidebar) {
+		cy.wait(1000);
+
+		cy.cGet('#sidebar-dock-wrapper.visible').then(function(sidebar) {
 			helper.containsFocusElement(sidebar[0], true)
 		});
 	});