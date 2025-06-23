6e0aac093bd8c5bd93a2fa3e28edd2cf6f1824b5
Sahil Gautam <sahil.gautam@collabora.com>
2025-06-23 13:53:26 +0530

    fix typo
    Signed-off-by: Sahil Gautam <sahil.gautam@collabora.com>
    Change-Id: I6dccacb775b11ab63456cfdf79d4747eadb71a02

--- browser/src/control/Control.NotebookbarImpress.js
+++ browser/src/control/Control.NotebookbarImpress.js
@@ -599,7 +599,7 @@ L.Control.NotebookbarImpress = L.Control.NotebookbarWriter.extend({
 						'type': 'toolbox',
 						'children': [
 							{
-								'id': 'home-coppy',
+								'id': 'home-copy',
 								'type': 'customtoolitem',
 								'text': _UNO('.uno:Copy'),
 								'command': '.uno:Copy',
