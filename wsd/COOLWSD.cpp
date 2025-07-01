a9e6a646a33ea86392787f1ae6cf907c549e3edc
areg <areg.nakashian@gmail.com>
2025-06-30 21:02:03 -0400

    gh#12178 Replace mobile header guards
    Signed-off-by: areg <areg.nakashian@gmail.com>
    Change-Id: If458362f8ef31a19b9f6dfc82781320d92a98d2a

--- wsd/COOLWSD.cpp
+++ wsd/COOLWSD.cpp
@@ -126,18 +126,7 @@
 
 #include <ServerSocket.hpp>
 
-#if MOBILEAPP
-#include <Kit.hpp>
-#ifdef IOS
-#include "ios.h"
-#elif defined(GTKAPP)
-#include "gtk.hpp"
-#elif defined(__ANDROID__)
-#include "androidapp.hpp"
-#elif WASMAPP
-#include "wasmapp.hpp"
-#endif
-#endif // MOBILEAPP
+#include <wsd/PlatformMobile.hpp>
 
 using Poco::Util::LayeredConfiguration;
 using Poco::Util::Option;
