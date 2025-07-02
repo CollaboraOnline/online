fb7bec004828749a190da92d13bc6b374cbcee27
Szymon Kłos <szymon.klos@collabora.com>
2025-07-02 09:34:18 +0000

    notebookbar: translate label for layout buttons
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I6c8461a45849c8e65e583709214c75dd2068ba40

--- browser/src/control/Control.NotebookbarImpress.js
+++ browser/src/control/Control.NotebookbarImpress.js
@@ -643,7 +643,7 @@ L.Control.NotebookbarImpress = L.Control.NotebookbarWriter.extend({
 			{
 				'id': 'home-create-slide:NewSlideLayoutMenu',
 				'type': 'menubutton',
-				'text': 'New',
+				'text': _('New'),
 				'command': '.uno:InsertPage',
 				'accessibility': { focusBack: true, combination: 'CS', de: null }
 			},
@@ -654,7 +654,7 @@ L.Control.NotebookbarImpress = L.Control.NotebookbarWriter.extend({
 						'id': 'home-change-layout:ChangeSlideLayoutMenu',
 						'type': 'menubutton',
 						'noLabel': true,
-						'text': 'Change Layout',
+						'text': _('Change Layout'),
 						'icon': 'lc_changelayout.svg',
 						'command': '.uno:AssignLayout',
 						'accessibility': { focusBack: true, combination: 'CL', de: null }
@@ -662,7 +662,7 @@ L.Control.NotebookbarImpress = L.Control.NotebookbarWriter.extend({
 					{
 						'id': 'home-assign-layout',
 						'noLabel': true,
-						'text': 'Reset Layout',
+						'text': _('Reset Layout'),
 						'type': 'toolitem',
 						'command': '.uno:AssignLayout',
 						'accessibility': { focusBack: true, combination: 'RS', de: null }
