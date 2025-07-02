a08d4c1ee4a77235ca3ef38bb82e6bd3a0132896
Mohit Marathe <mohit.marathe@collabora.com>
2025-07-02 12:27:58 +0530

    Impress: Notebookbar: Add Slide tab
    moves the slideshow controls from View tab to Slide tab
    
    Signed-off-by: Mohit Marathe <mohit.marathe@collabora.com>
    Change-Id: I14e0957b17f4fad0f10be4b84b5cc91a64f91229

--- cypress_test/integration_tests/desktop/impress/slideshow_spec.js
+++ cypress_test/integration_tests/desktop/impress/slideshow_spec.js
@@ -9,9 +9,9 @@ function getSlideShowContent() {
 describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'Some app', function() {
 	beforeEach(function() {
 		helper.setupAndLoadDocument('impress/slideshow.odp');
-		cy.cGet('.notebookbar #View-tab-label').click();
-		cy.cGet('.notebookbar #view-presentation-button').click();
-		cy.cGet('#view-presentation-entry-0').click();
+		cy.cGet('.notebookbar #Slide-tab-label').click();
+		cy.cGet('.notebookbar #slide-presentation-button').click();
+		cy.cGet('#slide-presentation-entry-0').click();
 	});
 
 	it('Should see an empty slideshow', function () {
