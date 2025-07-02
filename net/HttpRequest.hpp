6111b4128ad37752c674c354d6b69a3b2277bad8
Caolán McNamara <caolan.mcnamara@collabora.com>
2025-07-02 11:39:46 +0100

    cid#529846 Using a moved object
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: Iaba41f111e9e8d205770b17566be2cfdd8d0043e

--- net/HttpRequest.hpp
+++ net/HttpRequest.hpp
@@ -697,6 +697,8 @@ public:
 
         _header.add("Content-Length", std::to_string(body.size()));
 
+        const size_t bodySize = body.size();
+
         auto iss = std::make_shared<std::istringstream>(std::move(body), std::ios::binary);
 
         setBodySource(
@@ -705,7 +707,7 @@ public:
                 iss->read(buf, len);
                 return iss->gcount();
             },
-            body.size());
+            bodySize);
     }
 
     Stage stage() const { return _stage; }
