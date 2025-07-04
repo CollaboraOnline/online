4d087215284d4f249f5cf18b8657354fb719f8c0
Mohit Marathe <mohit.marathe@collabora.com>
2025-07-04 12:18:53 +0530

    Calc: Notebookbar: File tab: add ui separators between grouped items
    Signed-off-by: Mohit Marathe <mohit.marathe@collabora.com>
    Change-Id: I4902016c302130e97c8b1d94eb3a0b99d4d2a71a

--- browser/src/control/Control.NotebookbarCalc.js
+++ browser/src/control/Control.NotebookbarCalc.js
@@ -112,8 +112,12 @@ L.Control.NotebookbarCalc = L.Control.NotebookbarWriter.extend({
 
 	getFileTab: function() {
 		var content = [];
+		var hasSave = !this.map['wopi'].HideSaveOption;
+		var hasSaveAs = !this.map['wopi'].UserCanNotWriteRelative;
+		var hasShare = this.map['wopi'].EnableShare;
+		var hasRevisionHistory = L.Params.revHistoryEnabled;
 
-		if (!this.map['wopi'].HideSaveOption) {
+		if (hasSave) {
 			content.push({
 				'type': 'toolbox',
 				'children': [
@@ -128,7 +132,7 @@ L.Control.NotebookbarCalc = L.Control.NotebookbarWriter.extend({
 			});
 		}
 
-		if (!this.map['wopi'].UserCanNotWriteRelative) {
+		if (hasSaveAs) {
 			content.push(
 				(window.prefs.get('saveAsMode') === 'group') ? {
 					'id': 'saveas:SaveAsMenu',
@@ -148,7 +152,7 @@ L.Control.NotebookbarCalc = L.Control.NotebookbarWriter.extend({
 			);
 		}
 
-		if (!this.map['wopi'].UserCanNotWriteRelative) {
+		if (hasSaveAs) {
 			content.push({
 				'id': 'exportas:ExportAsMenu',
 				'command': 'exportas',
@@ -159,35 +163,84 @@ L.Control.NotebookbarCalc = L.Control.NotebookbarWriter.extend({
 			});
 		}
 
-		content.push(
-			{
-				'id': 'file-shareas-rev-history',
-				'type': 'container',
-				'children': [
-					this.map['wopi'].EnableShare ?
+		if (hasShare && hasRevisionHistory) {
+			content.push(
+				{
+					'type': 'container',
+					'children': [
 						{
+							'id': 'file-exportas-break',
+							'type': 'separator',
+							'orientation': 'vertical'
+						}, {
 							'id': 'ShareAs',
 							'class': 'unoShareAs',
 							'type': 'customtoolitem',
 							'text': _('Share'),
 							'command': 'shareas',
 							'inlineLabel': true,
-							'accessibility': { focusBack: true,	combination: 'Z', de: null }
-						} : {},
-						L.Params.revHistoryEnabled ?
-						{
+							'accessibility': { focusBack: true, combination: 'SH' }
+						}, {
 							'id': 'Rev-History',
 							'class': 'unoRev-History',
 							'type': 'customtoolitem',
 							'text': _('See history'),
 							'command': 'rev-history',
 							'inlineLabel': true,
-							'accessibility': { focusBack: true,	combination: 'RV', de: null }
-						} : {},
-				],
-				'vertical': 'true'
-			}
-		);
+							'accessibility': { focusBack: true, combination: 'RH' }
+						}, {
+							'id': 'file-revhistory-break',
+							'type': 'separator',
+							'orientation': 'vertical'
+						}
+					],
+					'vertical': true
+				});
+		} else if (hasShare) {
+			content.push({
+				'type': 'container',
+				'children': [
+					{
+						'id': 'file-exportas-break',
+						'type': 'separator',
+						'orientation': 'vertical'
+					}, {
+						'id': 'ShareAs',
+						'class': 'unoShareAs',
+						'type': 'bigcustomtoolitem',
+						'text': _('Share'),
+						'command': 'shareas',
+						'accessibility': { focusBack: true, combination: 'SH' }
+					}, {
+						'id': 'file-revhistory-break',
+						'type': 'separator',
+						'orientation': 'vertical'
+					}
+				]
+			});
+		} else if (hasRevisionHistory) {
+			content.push({
+				'type': 'container',
+				'children': [
+					{
+						'id': 'file-exportas-break',
+						'type': 'separator',
+						'orientation': 'vertical'
+					}, {
+						'id': 'Rev-History',
+						'class': 'unoRev-History',
+						'type': 'bigcustomtoolitem',
+						'text': _('See history'),
+						'command': 'rev-history',
+						'accessibility': { focusBack: true, combination: 'RH' }
+					}, {
+						'id': 'file-revhistory-break',
+						'type': 'separator',
+						'orientation': 'vertical'
+					}
+				]
+			});
+		}
 
 		if (!this.map['wopi'].HidePrintOption) {
 			content.push({
