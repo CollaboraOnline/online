119dca71c562bbd7b8394ef598d46e8851c766ae
Mohit Marathe <mohit.marathe@collabora.com>
2025-07-04 12:16:50 +0530

    Writer: Notebookbar: File tab: add ui separators between grouped items
    Signed-off-by: Mohit Marathe <mohit.marathe@collabora.com>
    Change-Id: Ib79e6112186213f5b35f25d11c274f15dbeb2aef

--- browser/src/control/Control.NotebookbarWriter.js
+++ browser/src/control/Control.NotebookbarWriter.js
@@ -204,6 +204,10 @@ L.Control.NotebookbarWriter = L.Control.Notebookbar.extend({
 					'type': 'container',
 					'children': [
 						{
+							'id': 'file-exportas-break',
+							'type': 'separator',
+							'orientation': 'vertical'
+						}, {
 							'id': 'ShareAs',
 							'class': 'unoShareAs',
 							'type': 'customtoolitem',
@@ -219,6 +223,10 @@ L.Control.NotebookbarWriter = L.Control.Notebookbar.extend({
 							'command': 'rev-history',
 							'inlineLabel': true,
 							'accessibility': { focusBack: true, combination: 'RH' }
+						}, {
+							'id': 'file-revhistory-break',
+							'type': 'separator',
+							'orientation': 'vertical'
 						}
 					],
 					'vertical': true
@@ -228,12 +236,20 @@ L.Control.NotebookbarWriter = L.Control.Notebookbar.extend({
 				'type': 'container',
 				'children': [
 					{
+						'id': 'file-exportas-break',
+						'type': 'separator',
+						'orientation': 'vertical'
+					}, {
 						'id': 'ShareAs',
 						'class': 'unoShareAs',
 						'type': 'bigcustomtoolitem',
 						'text': _('Share'),
 						'command': 'shareas',
 						'accessibility': { focusBack: true, combination: 'SH' }
+					}, {
+						'id': 'file-revhistory-break',
+						'type': 'separator',
+						'orientation': 'vertical'
 					}
 				]
 			});
@@ -242,12 +258,20 @@ L.Control.NotebookbarWriter = L.Control.Notebookbar.extend({
 				'type': 'container',
 				'children': [
 					{
+						'id': 'file-exportas-break',
+						'type': 'separator',
+						'orientation': 'vertical'
+					}, {
 						'id': 'Rev-History',
 						'class': 'unoRev-History',
 						'type': 'bigcustomtoolitem',
 						'text': _('See history'),
 						'command': 'rev-history',
 						'accessibility': { focusBack: true, combination: 'RH' }
+					}, {
+						'id': 'file-revhistory-break',
+						'type': 'separator',
+						'orientation': 'vertical'
 					}
 				]
 			});
