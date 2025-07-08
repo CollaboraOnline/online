203ce932a3183f14dc5acfbf8e883062953a7701
Gökay Şatır <gokaysatir@gmail.com>
2025-07-08 13:31:55 +0300

    Add cell selection middleware class.
    Signed-off-by: Gökay Şatır <gokaysatir@gmail.com>
    Change-Id: I3eb0968ade51347362958bbadff06e9747b46626

---
+++ browser/src/app/CellSelectionMarkerMiddleware.ts
@@ -0,0 +1,58 @@
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+class CellSelectionMarkers {
+	static start: CellSelectionHandle;
+	static end: CellSelectionHandle;
+
+	public static initiate() {
+		if (app.map._docLayer._docType !== 'spreadsheet')
+			// Calc only.
+			return;
+
+		// Cell selection handles (mobile & tablet).
+		this.start = new CellSelectionHandle('cell_selection_handle_start');
+		this.end = new CellSelectionHandle('cell_selection_handle_end');
+		app.sectionContainer.addSection(this.start);
+		app.sectionContainer.addSection(this.end);
+	}
+
+	public static update() {
+		if (window.mode.isDesktop()) return; // Not shown on desktop.
+
+		var showMarkers =
+			app.map._docLayer._cellSelectionArea || app.calc.cellCursorVisible;
+
+		if (showMarkers) {
+			this.start.setShowSection(true);
+			this.end.setShowSection(true);
+
+			var cellRectangle = app.map._docLayer._cellSelectionArea
+				? app.map._docLayer._cellSelectionArea.clone()
+				: app.calc.cellCursorRectangle.clone();
+
+			const posStart = new app.definitions.simplePoint(
+				cellRectangle.x1,
+				cellRectangle.y1,
+			);
+			const posEnd = new app.definitions.simplePoint(
+				cellRectangle.x2,
+				cellRectangle.y2,
+			);
+
+			const offset = this.start.sectionProperties.circleRadius;
+			this.start.setPosition(posStart.pX - offset, posStart.pY - offset);
+			this.end.setPosition(posEnd.pX - offset, posEnd.pY - offset);
+		} else {
+			this.start.setShowSection(false);
+			this.end.setShowSection(false);
+		}
+	}
+}
