9ea89d7acd7921d2d47c78da21fb7650339eb849
Gökay Şatır <gokaysatir@gmail.com>
2025-07-08 16:27:23 +0300

    Replace _selectionHandles variable (old) with TextSelections.(function). Remove onUpdateTextSelection call from permission.js. No replacement. Read only mode should allow text selections. Replace _selectionHandles variable (old) with TextSelections.(function).
    Signed-off-by: Gökay Şatır <gokaysatir@gmail.com>
    Change-Id: I4a7a2e090664caf842cded26d6d6e6f89199fce2

--- browser/src/app/GraphicSelectionMiddleware.ts
+++ browser/src/app/GraphicSelectionMiddleware.ts
@@ -327,7 +327,7 @@ class GraphicSelection {
 							this.rectangle.x2,
 							this.rectangle.y2,
 						])) &&
-					!app.map._docLayer._selectionHandles.active &&
+					!TextSelections.getEndRectangle() &&
 					!(app.isFollowingEditor() || app.isFollowingUser()) &&
 					!app.map.calcInputBarHasFocus()
 				) {
--- browser/src/control/Permission.js
+++ browser/src/control/Permission.js
@@ -226,7 +226,6 @@ L.Map.include({
 		if (this._docLayer) {
 			this._docLayer._onUpdateCursor();
 			this._docLayer._clearSelections();
-			this._docLayer._onUpdateTextSelection();
 		}
 		app.events.fire('updatepermission', {perm : perm});
 		this.fire('closemobilewizard');
