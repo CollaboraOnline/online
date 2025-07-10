510a6ccc3beb9f8f80b46e7795592d27df70ae51
Tor Lillqvist <tml@iki.fi>
2025-07-10 18:27:23 +0300

    Switch to only unmanaged C++ Win32 code in CODA-W
    Don't use XAML and C# any more. Improves performance.
    
    Signed-off-by: Tor Lillqvist <tml@iki.fi>
    Change-Id: Ibe7ffc60efbbc63eccf4101abe3efb7414440c9f

--- windows/coda/testheader.cpp
+++
@@ -1,9 +0,0 @@
-// This is just a temporary hack to make it easier to check one header
-// at a time whether it compiles for Windows without having to compile
-// a substantial C++ file.
-
-#include <config.h>
-
-#include <net/Socket.hpp> // Header to check
-
-int foobar = 42;
