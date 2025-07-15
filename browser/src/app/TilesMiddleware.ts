8b02f8617777bc29b5a245635a32a9829f34eef0
Chris Lord <chris.lord@collabora.com>
2025-07-15 16:06:01 +0100

    Fix TileManager.removeAllTiles
    Mistakenly used for-in instead of for-of on an array.
    
    Signed-off-by: Chris Lord <chris.lord@collabora.com>
    Change-Id: I1b4887e63c31b015ea177403d700e6189b78ca5a

--- browser/src/app/TilesMiddleware.ts
+++ browser/src/app/TilesMiddleware.ts
@@ -1189,7 +1189,7 @@ class TileManager {
 
 	private static removeAllTiles() {
 		this.tileBitmapList = [];
-		for (const key in Array.from(this.tiles.keys())) {
+		for (const key of Array.from(this.tiles.keys())) {
 			this.removeTile(key);
 		}
 	}
