95304281d8e8095a0c448140fb6b018790340358
Mohit Marathe <mohit.marathe@collabora.com>
2025-07-21 20:29:00 +0530

    Notebookbar: Picture tab: Add labels to menubuttons
    Signed-off-by: Mohit Marathe <mohit.marathe@collabora.com>
    Change-Id: I3bc6932d452b6d45f4cd8a31f3a3db9520574cd8

--- browser/src/control/Control.NotebookbarWriter.js
+++ browser/src/control/Control.NotebookbarWriter.js
@@ -3221,6 +3221,7 @@ L.Control.NotebookbarWriter = L.Control.Notebookbar.extend({
 						'id': 'picture-brightness:PictureBrightness',
 						'type': 'menubutton',
 						'command': '.uno:GrafLuminance',
+						'text': _UNO('.uno:GrafLuminance'),
 						'icon': 'lc_setbrightness.svg',
 						'accessibility': { focusBack: true, combination: 'BN', de: null }
 					},
@@ -3228,6 +3229,7 @@ L.Control.NotebookbarWriter = L.Control.Notebookbar.extend({
 						'id': 'picture-contrast:PictureContrast',
 						'type': 'menubutton',
 						'command': '.uno:GrafContrast',
+						'text': _UNO('.uno:GrafContrast'),
 						'icon': 'lc_setcontrast.svg',
 						'accessibility': { focusBack: true, combination: 'CN', de: null }
 					},
@@ -3235,6 +3237,7 @@ L.Control.NotebookbarWriter = L.Control.Notebookbar.extend({
 						'id': 'picture-colormode:PictureColorMode',
 						'type': 'menubutton',
 						'command': '.uno:GrafMode',
+						'text': _UNO('.uno:GrafMode'),
 						'icon': 'lc_setgraphtransparency.svg',
 						'accessibility': { focusBack: true, combination: 'CO', de: null }
 					},
@@ -3242,6 +3245,7 @@ L.Control.NotebookbarWriter = L.Control.Notebookbar.extend({
 						'id': 'picture-transparency:PictureTransparency',
 						'type': 'menubutton',
 						'command': '.uno:GrafTransparence',
+						'text': _UNO('.uno:GrafTransparence'),
 						'icon': 'lc_setgraphtransparency.svg',
 						'accessibility': { focusBack: true, combination: 'TP', de: null }
 					},
