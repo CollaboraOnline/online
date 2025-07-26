9dfe97c34a2d66c52e665761afc8d99f43dd4964
Caolán McNamara <caolan.mcnamara@collabora.com>
2025-07-26 20:25:36 +0100

    cid#535344 Variable copied when it could be moved
    be consistent here with the other URI strings
    
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: I3ef511272924fdc4d769ff11e3fd8325fa126aa7

--- test/RequestDetailsTests.cpp
+++ test/RequestDetailsTests.cpp
@@ -319,7 +319,7 @@ void RequestDetailsTests::testLocalHexified()
     const std::string fileUrlHex = "0x" + HexUtil::dataToHexString(fileUrl, 0, fileUrl.size());
 
     {
-        const std::string URI = "/cool/" + fileUrlHex + "/ws/open/open/0";
+        static const std::string URI = "/cool/" + fileUrlHex + "/ws/open/open/0";
 
         Poco::Net::HTTPRequest request(Poco::Net::HTTPRequest::HTTP_GET, URI,
                                        Poco::Net::HTTPMessage::HTTP_1_1);
