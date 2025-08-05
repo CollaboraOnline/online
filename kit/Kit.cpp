02a73f0b11b1d68b499b5e3657e0127223905bb6
Jaap AartS <jaap.aarts1@gmail.com>
2025-08-06 00:11:06 +0200

    Only use linux-only thread and fd-counter on linux
    These are linux only types that should not be used on othersystems. In
    the future a new implementation should be used for FreeBSD/others,
    however the only downside I can think of is the stats not working? That
    does not sound like a major issue.
    
    Signed-off-by: Jaap AartS <jaap.aarts1@gmail.com>
    Change-Id: I38ea00278c1d595291b23715551e513423602f09

--- kit/Kit.cpp
+++ kit/Kit.cpp
@@ -3380,10 +3380,14 @@ void lokit_main(
         const std::string jailPathStr = jailPath.toString();
         JailUtil::createJailPath(jailPathStr);
 
+#ifdef THREADCOUNTER_USABLE
         // initialize while we have access to /proc/self/task
         threadCounter.reset(new Util::ThreadCounter());
+#endif
+#ifdef FDCOUNTER_USABLE
         // initialize while we have access to /proc/self/fd
         fdCounter.reset(new Util::FDCounter());
+#endif
 
         bool usingMountNamespace = false;
         std::chrono::milliseconds jailSetupTime(0);
