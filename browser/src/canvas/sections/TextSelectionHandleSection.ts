e000093e0746f5827c8ce85a78e12d2e4859b852
Skyler Grey <skyler.grey@collabora.com>
2025-08-06 09:43:37 +0000

    fix(teardrop): correct non-desktop hiding
    This reimplements the I6544845526c2e70bee7d6e3b193e9fb4931422c6's intent
    but using the existing system to show/hide the teardrop
    
    I do this in a similar way to my recent work hiding uninteractible
    elements from #12422, albeit without an event to switch over when we
    change from mouse to touch or vice-versa time. The lack of an event is
    because the selection handles aren't properly moved into place when they
    are hidden and there are some finnicky events (where sometimes we end up
    detecting touch events as having some mouse events too) on the mouse ->
    touch switch. We may change this behavior in a followup.
    
    This fixes a regression from I6544845526c2e70bee7d6e3b193e9fb4931422c6
    where the cursor droplets were incorrectly hidden such that:
    
    - They would never show if we started using a touchscreen
    - Some parts of them remained active so selections worked differently
    
    In master, this change depended on #12455. As that is not getting,
    backported this distro/collabora/co-25.04 patch has been adapted to work
    without it
    
    Signed-off-by: Skyler Grey <skyler.grey@collabora.com>
    Change-Id: I6a6a696452b13d46eb9fd364def2ba3e92b2c9dc

--- browser/src/canvas/sections/TextSelectionHandleSection.ts
+++ browser/src/canvas/sections/TextSelectionHandleSection.ts
@@ -11,8 +11,20 @@
 */
 
 class TextSelectionHandle extends HTMLObjectSection {
+	_showSection: boolean = true; // Store the internal show/hide section through forced non-touchscreen hides...
+	super_setShowSection: (show: boolean) => void; // HACK: used when dealing with CanvasSectionContainer only having setShowSection via addSectionFunctions
+
 	constructor (sectionName: string, objectWidth: number, objectHeight: number, documentPosition: cool.SimplePoint,  extraClass: string = "", showSection: boolean = false) {
 		super(sectionName, objectWidth, objectHeight, documentPosition, extraClass, showSection);
+	}
+
+	onInitialize() {
+		// HACK: used when dealing with CanvasSectionContainer only having setShowSection via addSectionFunctions
+		// we need to rename the property otherwise
+		// (1) we will not be able to call it again via super, since as it's not a real superclass function
+		// (2) we will not be able to call our own setShowSection as, since as it's set directly on the object, it'll override everything in the prototype...
+		this.super_setShowSection = this.setShowSection;
+		delete this.setShowSection;
 	}
 
 	onDrag(point: number[]) {
@@ -24,6 +36,16 @@ class TextSelectionHandle extends HTMLObjectSection {
 		this.sectionProperties.objectDiv.style.top = candidateY + 'px';
 
 		app.map.fire('handleautoscroll', {pos: { x: candidateX, y: candidateY }, map: app.map});
+	}
+
+	setShowSection(show: boolean) {
+		this._showSection = show;
+
+		if (!window.touch.currentlyUsingTouchscreen()) {
+			this.super_setShowSection(false);
+		} else {
+			this.super_setShowSection(this._showSection);
+		}
 	}
 
 	setOpacity(value: number) {
