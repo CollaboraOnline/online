974f17754fa74324418e54249a8a94a49ef78e63
Gökay Şatır <gokaysatir@gmail.com>
2025-08-09 18:03:46 +0300

    Cypress changes.
    Scroll bar position calculation is improved. Tests are fixed.
    Also checked the failing tests visually.
    
    Signed-off-by: Gökay Şatır <gokaysatir@gmail.com>
    Change-Id: I9a377ff70f1be9db6572b3eac014a6a543c75611

--- cypress_test/integration_tests/desktop/impress/scrolling_spec.js
+++ cypress_test/integration_tests/desktop/impress/scrolling_spec.js
@@ -31,10 +31,10 @@ describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'Scroll through document', 
 		cy.cGet('.leaflet-layer').click('right');
 		cy.wait(1000);
 		clickOnTheCenter();
-		desktopHelper.pressKey(9,'uparrow');
+		desktopHelper.pressKey(15,'uparrow');
 		desktopHelper.assertScrollbarPosition('vertical', 0, 1);
 		desktopHelper.pressKey(18,'downarrow');
-		desktopHelper.assertScrollbarPosition('vertical', 306, 355);
+		desktopHelper.assertScrollbarPosition('vertical', 280, 330);
 	});
 
 	it('Scrolling to left/right', function() {
