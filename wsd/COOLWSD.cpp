a5028d3e0dbf4f963a67a920a177c118f8c60ddf
Caolán McNamara <caolan.mcnamara@collabora.com>
2025-08-11 16:31:20 +0100

    obsolete existing subforkits when forkit exits
    they have exited when the forkit exited anyway, so go ahead and forget
    about them immediately.
    
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: I73295d26d49eff54dcbe653ccee765cef23a9662

--- wsd/COOLWSD.cpp
+++ wsd/COOLWSD.cpp
@@ -2631,6 +2631,14 @@ bool COOLWSD::checkAndRestoreForKit()
                             " with " << SigUtil::signalName(WTERMSIG(status)));
                 }
 
+                // subforkits exit when their parent forkit, so we can
+                // immediately consider these obsolete
+                for (auto it = SubForKitProcs.begin(); it != SubForKitProcs.end(); )
+                {
+                    LOG_DBG("dropping subforkit " << it->first);
+                    it = dropSubForKit(it);
+                }
+
                 // Spawn a new forkit and try to dust it off and resume.
                 if (!SigUtil::getShutdownRequestFlag() && !createForKit())
                 {
