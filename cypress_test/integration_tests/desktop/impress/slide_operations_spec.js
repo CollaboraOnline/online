29c840f998f3c68c27608b47ae73c76eba0eabee
Pranam Lashkari <lpranam@collabora.com>
2025-08-12 07:20:49 +0530

    comment: bubble alignment improvments
    1. Now bubbles maintain same margin from documents
    2. If available space is too small we use full size comments
       and make document horizontal scrollable
    3. Comments now never go over the document unless selected
    4. Comments in RTL mode were not visible at all
       (still not all RTL issues solved but at least its visible when full size)
    
    Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
    Change-Id: I612d08f72b196200ba2be4fdf61d9a30b9146186

--- cypress_test/integration_tests/desktop/impress/slide_operations_spec.js
+++ cypress_test/integration_tests/desktop/impress/slide_operations_spec.js
@@ -42,10 +42,12 @@ describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'Slide operations', functio
 
 	it('Duplicate slide', function() {
 		// Also check if comments are getting duplicated
+		desktopHelper.closeNavigatorSidebar();
 		cy.cGet('#options-modify-page').click();
 		desktopHelper.insertComment();
 		cy.cGet('[id^=annotation-content-area-]').should('include.text', 'some text0');
-		cy.cGet('#presentation-toolbar #duplicatepage').click();
+		cy.cGet('#Insert-tab-label').click();
+		cy.cGet('#insert-duplicate-slide').click();
 
 		impressHelper.assertNumberOfSlidePreviews(2);
 		cy.cGet('#SlideStatus').should('have.text', 'Slide 2 of 2');
