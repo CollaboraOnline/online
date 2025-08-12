da878f47fd5be2cb4355ef8a774dc43de5575a4b
Pranam Lashkari <lpranam@collabora.com>
2025-08-12 07:24:48 +0530

    comment: make comments RTL compatible
    problem:
    in RTL comments were not properly aligned in collapsed mode
    
    Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
    Change-Id: I1bf78e852feadd8b4186288f7c1c6aae033cac82

--- browser/src/canvas/sections/CommentSection.ts
+++ browser/src/canvas/sections/CommentSection.ts
@@ -282,6 +282,9 @@ export class Comment extends CanvasSectionObject {
 
 		this.sectionProperties.wrapper.style.marginLeft = this.sectionProperties.childCommentOffset*this.getChildLevel() + 'px';
 
+		if (document.documentElement.dir === 'rtl')
+			this.sectionProperties.wrapper.dir = 'rtl';
+
 		// We make comment directly visible when its transitioned to its determined position
 		if (cool.CommentSection.autoSavedComment)
 			this.sectionProperties.container.style.visibility = 'hidden';
