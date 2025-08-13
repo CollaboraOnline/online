978de26e25422a6c1d16f4ae05a8b6aafd3b7875
Rashesh Padia <rashesh.padia@collabora.com>
2025-08-13 13:24:58 +0530

    fix: settings iframe doesn't update the list of words when new word added
    Signed-off-by: Rashesh Padia <rashesh.padia@collabora.com>
    Change-Id: If5690ab5b265e54d31b979cf8491e1cf10324796

--- browser/admin/src/integrator/AdminIntegratorSettings.ts
+++ browser/admin/src/integrator/AdminIntegratorSettings.ts
@@ -1169,7 +1169,7 @@ class SettingIframe {
 					'edit', // Use icon key
 					_('Edit'),
 					['button--vue-secondary', 'edit-icon'],
-					async (button) => await this.fetchWordbookFile(item.uri),
+					async () => await this.fetchWordbookFile(item.uri),
 					true,
 				),
 			);
