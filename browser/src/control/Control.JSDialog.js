5db6f6044af2c3bd735e054c80f95d557134a697
Szymon Kłos <szymon.klos@collabora.com>
2025-08-14 01:52:38 +0000

    jsdialog: reduce unwanted focus change
    - search field in the toolbar emits resize to adjust
      overflow in the statusbar
    - if we used search functionality we could trigger the
      unfold / fold action for overflow manager
    - that was emiting close event which runs code which
      aims to return forcus to the last dialog or map
    - in result we were able to type only single character
      in the search field and rest in the document
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I9f362d0b3a19747a832176e3384da258833b9ed0

--- browser/src/control/Control.JSDialog.js
+++ browser/src/control/Control.JSDialog.js
@@ -760,11 +760,13 @@ L.Control.JSDialog = L.Control.extend({
 		}
 		else if (instance.action === 'close')
 		{
+			const dialogs = Object.keys(this.dialogs);
+			const hadOpenedDialog = dialogs.length > 0;
+
 			this.close(instance.id, false);
 
 			// Manage focus
-			var dialogs = Object.keys(this.dialogs);
-			if (dialogs.length) {
+			if (hadOpenedDialog && dialogs.length) {
 				var lastKey = dialogs[dialogs.length - 1];
 				const lastDialog = this.dialogs[lastKey];
 				const lastContainer = lastDialog.container;
@@ -775,7 +777,7 @@ L.Control.JSDialog = L.Control.extend({
 					else
 						lastContainer.focus();
 				}
-			} else {
+			} else if (hadOpenedDialog){
 				this.map.focus();
 			}
 		}
