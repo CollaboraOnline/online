110c977b4a288fad04465d802c2bf6c28b7beae3
Henry Castro <hcastro@collabora.com>
2025-08-18 09:01:06 -0400

    browser: fix changes to numeric values
    "The 'input' event listener fires after the user inputs a valid number.
    
    Change-Id: I3c50010a22de1f8abb1ac54875a3dc6d1b4f3662
    Signed-off-by: Henry Castro <hcastro@collabora.com>

--- browser/src/control/Control.JSDialogBuilder.js
+++ browser/src/control/Control.JSDialogBuilder.js
@@ -410,7 +410,7 @@ window.L.Control.JSDialogBuilder = window.L.Control.extend({
 	},
 
 	listenNumericChanges: function (data, builder, controls, customCallback) {
-		controls.spinfield.addEventListener('change', function() {
+		controls.spinfield.addEventListener('input', function() {
 			if (!this.checkValidity())
 				return;
 
