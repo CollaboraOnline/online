d1772853b8cad7eef936a632baff7a2d0972df50
Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>
2025-08-24 15:31:47 +0530

    Add missing localization strings
    - In few cases we missed to add _ for name property in overflow groups
    - this patch will fix that issue so no localization get missed out
    
    Signed-off-by: Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>
    Change-Id: I96670607a9f557f3524a963894b74ac688926c77

--- browser/src/control/Control.NotebookbarCalc.js
+++ browser/src/control/Control.NotebookbarCalc.js
@@ -392,7 +392,7 @@ L.Control.NotebookbarCalc = L.Control.NotebookbarWriter.extend({
 			{
 				'type': 'overflowgroup',
 				'id': 'home-clipboard',
-				'name': 'Clipboard',
+				'name': _('Clipboard'),
 				'accessibility': { focusBack: true,	combination: 'V', de: null },
 				'children' :
 				[
@@ -453,7 +453,7 @@ L.Control.NotebookbarCalc = L.Control.NotebookbarWriter.extend({
 			{
 				'type': 'overflowgroup',
 				'id': 'home-font',
-				'name': 'Font',
+				'name': _('Font'),
 				'accessibility': { focusBack: true,	combination: 'FF', de: null },
 				'more': {
 					'command':'.uno:CellTextDlg'
@@ -599,7 +599,7 @@ L.Control.NotebookbarCalc = L.Control.NotebookbarWriter.extend({
 			{
 				'type': 'overflowgroup',
 				'id': 'home-alignment',
-				'name': 'Alignment',
+				'name': _('Alignment'),
 				'accessibility': { focusBack: true,	combination: 'AT', de: null },
 				'more': {
 					'command':'.uno:Hyphenate'
@@ -735,7 +735,7 @@ L.Control.NotebookbarCalc = L.Control.NotebookbarWriter.extend({
 			{
 				'type': 'overflowgroup',
 				'id': 'home-number-format',
-				'name': 'Number',
+				'name': _('Number'),
 				'accessibility': { focusBack: true,	combination: 'N', de: null },
 				'more': {
 					'command':'.uno:FormatCellDialog'
@@ -836,7 +836,7 @@ L.Control.NotebookbarCalc = L.Control.NotebookbarWriter.extend({
 			{
 				'type': 'overflowgroup',
 				'id': 'home-insert-table',
-				'name': 'Cells',
+				'name': _('Cells'),
 				'accessibility': { focusBack: true,	combination: 'RB', de: null },
 				'children' : [
 				{
@@ -917,7 +917,7 @@ L.Control.NotebookbarCalc = L.Control.NotebookbarWriter.extend({
 			{
 				'type': 'overflowgroup',
 				'id': 'home-style',
-				'name': 'Style',
+				'name': _('Style'),
 				'accessibility': { focusBack: true,	combination: 'L', de: null },
 				'children' : [
 				{
@@ -994,7 +994,7 @@ L.Control.NotebookbarCalc = L.Control.NotebookbarWriter.extend({
 			{
 				'type': 'overflowgroup',
 				'id': 'home-find-n-filter',
-				'name': 'Editing',
+				'name': _('Editing'),
 				'accessibility': { focusBack: true,	combination: 'SS', de: null },
 				'children' : [
 				{
