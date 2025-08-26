869279e33baf80c698b3f37fd75bd4473a96aa73
Jan Holesovsky <kendy@collabora.com>
2025-08-26 15:06:18 +0200

    Consolidate the KitSocketPoll initialization code
    Shouldn't change any functionality, just bring the server and mobile
    code closer together.
    
    Signed-off-by: Jan Holesovsky <kendy@collabora.com>
    Change-Id: I272491242ef8bfe757df48983c4a37c492c8b4fb

--- kit/Kit.cpp
+++ kit/Kit.cpp
@@ -3202,8 +3202,17 @@ int pollCallback(void* data, int timeoutUs)
 // Do we have any pending input events from coolwsd ?
 bool anyInputCallback(void* data, int mostUrgentPriority)
 {
-    auto* kitSocketPoll = reinterpret_cast<KitSocketPoll*>(data);
-    const std::shared_ptr<Document>& document = kitSocketPoll->getDocument();
+    if (!data)
+        return false;
+
+    return reinterpret_cast<KitSocketPoll*>(data)->kitHasAnyInput(mostUrgentPriority);
+}
+
+} // namespace
+
+bool KitSocketPoll::kitHasAnyInput(int mostUrgentPriority) {
+#if !MOBILEAPP
+    const std::shared_ptr<Document>& document = getDocument();
 
     if (document)
     {
@@ -3225,7 +3234,7 @@ bool anyInputCallback(void* data, int mostUrgentPriority)
         }
 
         // Poll our incoming socket from wsd.
-        int ret = kitSocketPoll->poll(std::chrono::microseconds(0), /*justPoll=*/true);
+        int ret = poll(std::chrono::microseconds(0), /*justPoll=*/true);
         if (ret)
         {
             return true;
@@ -3238,16 +3247,28 @@ bool anyInputCallback(void* data, int mostUrgentPriority)
     }
 
     return false;
+#else
+    // FIXME - should return true only if there is any input in any of the Kits
+    return true;
+#endif
 }
+
+namespace {
 
 /// Called by LOK main-loop
 void wakeCallback(void* data)
 {
-#if !MOBILEAPP
     if (!data)
         return;
-    else
-        return reinterpret_cast<KitSocketPoll*>(data)->wakeup();
+
+    return reinterpret_cast<KitSocketPoll*>(data)->kitWakeup();
+}
+
+} // namespace
+
+void KitSocketPoll::kitWakeup() {
+#if !MOBILEAPP
+    wakeup();
 #else
     std::unique_lock<std::mutex> lock(KitSocketPoll::KSPollsMutex);
     if (KitSocketPoll::KSPolls.empty())
@@ -3265,6 +3286,31 @@ void wakeCallback(void* data)
         p->wakeup();
 #endif
 }
+
+/**
+ * Register the "any input", "poll" and "wake up" callbacks in LibreOfficeKit and start the LOKit's main loop.
+ *
+ * The LOKit main loop will use/call these callbacks inside VCL's Yield(), see SvpSalInstance::ImplYield().
+ */
+void startMainLoop(const LibreOfficeKit* kit, const std::shared_ptr<lok::Office>& loKit, const std::shared_ptr<KitSocketPoll>& mainKit) {
+    if (!LIBREOFFICEKIT_HAS(kit, runLoop))
+    {
+        LOG_FTL("Kit is missing Unipoll API");
+        std::cout << "Fatal: out of date LibreOfficeKit - no Unipoll API\n";
+        Util::forcedExit(EX_SOFTWARE);
+    }
+
+    loKit->registerAnyInputCallback(anyInputCallback, mainKit.get());
+
+    LOG_INF("Kit unipoll loop run");
+
+    loKit->runLoop(pollCallback, wakeCallback, mainKit.get());
+
+    LOG_INF("Kit unipoll loop run terminated.");
+}
+
+namespace
+{
 
 #if !MOBILEAPP
 
@@ -3947,14 +3993,12 @@ void lokit_main(
         pathAndQuery.append(std::string("&adms_info_namespaces=") +
                             (useMountNamespaces ? "true" : "false"));
 
-#else // MOBILEAPP
+#endif // !MOBILEAPP
 
-#if !MOBILEAPP
-        // Was not done by the preload.
-        // For iOS we call it in -[AppDelegate application: didFinishLaunchingWithOptions:]
-        setupKitEnvironment(userInterface);
-#endif
+        auto mainKit = KitSocketPoll::create();
+        mainKit->runOnClientThread(); // We will do the polling on this thread.
 
+#if MOBILEAPP
 #if (defined(__linux__) && !defined(__ANDROID__) && !defined(QTAPP)) || defined(__FreeBSD__)
         Poco::URI userInstallationURI("file", LO_PATH);
         LibreOfficeKit *kit = lok_init_2(LO_PATH "/program", userInstallationURI.toString().c_str());
@@ -3979,9 +4023,6 @@ void lokit_main(
         const std::string jailId = "jailid";
 
 #endif // MOBILEAPP
-
-        auto mainKit = KitSocketPoll::create();
-        mainKit->runOnClientThread(); // We will do the polling on this thread.
 
         std::shared_ptr<KitWebSocketHandler> websocketHandler =
             std::make_shared<KitWebSocketHandler>("child_ws", loKit, jailId, mainKit, numericIdentifier);
@@ -4034,20 +4075,7 @@ void lokit_main(
 #endif
 
 #if !MOBILEAPP
-        if (!LIBREOFFICEKIT_HAS(kit, runLoop))
-        {
-            LOG_FTL("Kit is missing Unipoll API");
-            std::cout << "Fatal: out of date LibreOfficeKit - no Unipoll API\n";
-            Util::forcedExit(EX_SOFTWARE);
-        }
-
-        loKit->registerAnyInputCallback(anyInputCallback, mainKit.get());
-
-        LOG_INF("Kit unipoll loop run");
-
-        loKit->runLoop(pollCallback, wakeCallback, mainKit.get());
-
-        LOG_INF("Kit unipoll loop run terminated.");
+        startMainLoop(kit, loKit, mainKit);
 
         // Trap the signal handler, if invoked,
         // to prevent exiting.
@@ -4082,16 +4110,6 @@ void lokit_main(
 
 #if defined(QTAPP) || defined(MACOS) || defined(_WIN32)
 
-/**
- * Callback that tells LO's Yield if it should go ahead and handle the LOKit's poll.
- * FIXME: This is a temporary implementation that always assumes there are events; we should do better.
- */
-bool alwaysHasEventsCallback(void* data, int mostUrgentPriority)
-{
-    // FIXME - return true only if there is any input in any of the Kits
-    return true;
-}
-
 // with "unipoll" thread that calls lok_init_2 ends up holding the yield mutex in InitVCL()
 // lok::Office:runLoop then spawned in another thread ends up stuck. To prevent that call lok_init_2
 // and runLoop in the same thread.
@@ -4121,11 +4139,7 @@ std::future<LibreOfficeKit*> initKitRunLoopThread()
 
                 std::shared_ptr<lok::Office> loKit = std::make_shared<lok::Office>(kit);
 
-                loKit->registerAnyInputCallback(alwaysHasEventsCallback, nullptr);
-                LOG_INF("Kit unipoll loop run");
-
-                int dummy;
-                loKit->runLoop(pollCallback, wakeCallback, &dummy);
+                startMainLoop(kit, loKit);
 
                 // Should never return
                 std::abort();
