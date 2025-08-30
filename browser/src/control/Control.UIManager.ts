da0460495fbf955637acf61e5b75141b128e980a
Dennis Francis <dennis.francis@collabora.com>
2025-08-30 16:46:18 +0530

    browser: allow uiManager.showInfoModal() to take 5 params
    Signed-off-by: Dennis Francis <dennis.francis@collabora.com>
    Change-Id: I288d3c269e553584d684f2fc8ee34149b7e4b172

--- browser/src/control/Control.UIManager.ts
+++ browser/src/control/Control.UIManager.ts
@@ -1710,8 +1710,8 @@ class UIManager extends L.Control {
 		message1: string,
 		message2: string,
 		buttonText: string,
-		callback: any,
-		withCancel: boolean,
+		callback?: any,
+		withCancel?: boolean,
 		focusId?: string,
 	): void {
 		var dialogId = this.generateModalId(id);
