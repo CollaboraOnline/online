ef7a06e7e6c9134116a773bd5f4f19cd7774784d
Szymon Kłos <szymon.klos@collabora.com>
2025-09-03 11:37:28 +0000

    notebookbar: catch errors and do not fail completely
    To prevent app fail on load we should rather catch and log:
    Uncaught TypeError: app.map is null
        OverflowManager Widget.OverflowManager.ts:28
        OverflowManager Widget.OverflowManager.ts:109
        build Control.JSDialogBuilder.js:2993
        create Control.TopToolbar.js:257
        Toolbar Component.Toolbar.ts:46
        TopToolbar Control.TopToolbar.js:18
        TopToolbar Control.TopToolbar.js:535
        initializeMenubarAndTopToolbar Control.UIManager.ts:350
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: Ifda54b5961ea7ed996fd3801b316deab3bbea076

--- browser/src/control/Control.NotebookbarBuilder.js
+++ browser/src/control/Control.NotebookbarBuilder.js
@@ -846,21 +846,25 @@ L.Control.NotebookbarBuilder = L.Control.JSDialogBuilder.extend({
 			    && childData.children[0] && childData.children[0].type == 'panel'
 			    && childData.children[1] && childData.children[1].type == 'panel';
 
-			if (twoPanelsAsChildren) {
-				handler = this._controlHandlers['paneltabs'];
-				processChildren = handler(childObject, childData.children, this);
-			} else {
-				if (handler) {
-					processChildren = handler(childObject, childData, this);
-					this.postProcess(childObject, childData);
-				} else
-					window.app.console.warn('NotebookbarBuilder: Unsupported control type: "' + childType + '"');
+			try {
+				if (twoPanelsAsChildren) {
+					handler = this._controlHandlers['paneltabs'];
+					processChildren = handler(childObject, childData.children, this);
+				} else {
+					if (handler) {
+						processChildren = handler(childObject, childData, this);
+						this.postProcess(childObject, childData);
+					} else
+						window.app.console.warn('NotebookbarBuilder: Unsupported control type: "' + childType + '"');
 
-				if (processChildren && childData.children != undefined)
-					this.build(childObject, childData.children, isVertical);
-				else if (childData.visible && (childData.visible === false || childData.visible === 'false')) {
-					$('#' + childData.id).addClass('hidden-from-event');
+					if (processChildren && childData.children != undefined)
+						this.build(childObject, childData.children, isVertical);
+					else if (childData.visible && (childData.visible === false || childData.visible === 'false')) {
+						$('#' + childData.id).addClass('hidden-from-event');
+					}
 				}
+			} catch (ex) {
+				window.app.console.error('NotebookbarBuilder: exception while building "' + childData.id + '" : ' + ex);
 			}
 		}
 	}
