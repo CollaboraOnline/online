27b671fd901e3b54672ca5d7d2c6c3cae89545e2
Szymon Kłos <szymon.klos@collabora.com>
2025-09-08 06:06:30 +0000

    ServerConnectionService: coordinate actions on connection status
    - move viewsetting: hanlder to separate file
    - add typing to the JSON for viewsetting:
    - it will help to order properly actions when we connect or
      reconnect to the server
    - followup for commit b8a553eb8da57d7c1d60978a05d3a54a069db4ac
      introduce zotero setting in AdminIntegratorSettings
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I33c6570f0abfca0fa939be4ec3040bc777a5f1af

---
+++ browser/src/app/ServerConnectionService.ts
@@ -0,0 +1,51 @@
+/* -*- js-indent-level: 8; fill-column: 100 -*- */
+
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * This file contains service which will coordinate operations which
+ * should happen on server connection changes.
+ */
+
+interface ViewSetting {
+	zoteroAPIKey?: string;
+	accessibilityState: boolean;
+}
+
+class ServerConnectionService {
+	public onViewSetting(viewSetting: ViewSetting) {
+		app.console.debug('ServerConnectionService: onViewSetting');
+
+		if (!app.map) {
+			app.console.error('ServerConnectionService: missing map reference');
+			return;
+		}
+
+		let zoteroPlugin = app.map.zotero;
+		const zoteroAPIKey = viewSetting.zoteroAPIKey;
+		if (window.zoteroEnabled && zoteroAPIKey && !zoteroPlugin) {
+			app.console.debug('ServerConnectionService: initialize Zotero plugin');
+
+			zoteroPlugin = L.control.zotero(app.map);
+			zoteroPlugin.apiKey = zoteroAPIKey;
+
+			app.map.zotero = zoteroPlugin;
+			app.map.addControl(zoteroPlugin);
+
+			zoteroPlugin.updateUserID();
+		}
+
+		if (viewSetting.accessibilityState) {
+			app.console.debug('ServerConnectionService: initialize accessibility');
+			app.map.lockAccessibilityOn();
+		}
+	}
+}
