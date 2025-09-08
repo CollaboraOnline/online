d6ade16f2eedbad7a81abd289c624497f144c50e
Tor Lillqvist <tml@collabora.com>
2025-09-08 18:36:23 +0300

    Bypass LogUiCommands code when Util::isMobileApp()
    The actual cause for doing this was to get rid of some alarming
    looking log output in CODA-W about "Bad path syntax", that turned out
    to come from the LogUiCommands code and likely be harmless. Related to
    the complications related to path name handling on Windows. (See my
    "Windows-specific pathname fixes" commit from Feb 18.) The
    LogUiCommands code ran into an exception from Poco when manipulating
    such a silly bogus /C:/Users/tml/foo.odt style path.
    
    Maybe I really should do what I wrote in that commit, rigorously
    everywhere use either Poco::URI or Poco::Path types, except for at the
    very last moment when we need an actual local file pathname?
    
    Signed-off-by: Tor Lillqvist <tml@collabora.com>
    Change-Id: I654f39051bb4e285d4ae475c3b6faefba226cc83

--- kit/ChildSession.cpp
+++ kit/ChildSession.cpp
@@ -4030,6 +4030,9 @@ void ChildSession::saveLogUiBackground()
 
 void LogUiCommands::logLine(LogUiCommandsLine &line, bool isUndoChange)
 {
+    if constexpr (Util::isMobileApp())
+        return;
+
     // log command
     double timeDiffStart = std::chrono::duration<double>(line._timeStart - _session._docManager->getLogUiCmd().getKitStartTimeSec()).count();
 
@@ -4081,6 +4084,9 @@ void LogUiCommands::logLine(LogUiCommandsLine &line, bool isUndoChange)
 
 void LogUiCommands::logSaveLoad(std::string cmd, const std::string & path, std::chrono::steady_clock::time_point timeStart)
 {
+    if constexpr (Util::isMobileApp())
+        return;
+
     LogUiCommandsLine uiLogLine;
     uiLogLine._timeStart = timeStart;
     uiLogLine._timeEnd = std::chrono::steady_clock::now();
@@ -4111,12 +4117,18 @@ void LogUiCommands::logSaveLoad(std::string cmd, const std::string & path, std::
 LogUiCommands::LogUiCommands(ChildSession& session, const StringVector* tokens)
     : _session(session), _tokens(tokens)
 {
+    if constexpr (Util::isMobileApp())
+        return;
+
     if (_session._isDocLoaded)
         _document = session.getLOKitDocument();
 }
 
 LogUiCommands::~LogUiCommands()
 {
+    if constexpr (Util::isMobileApp())
+        return;
+
     auto document = _document.lock();
     if (!document)
         return;
