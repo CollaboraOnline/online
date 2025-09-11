946c12c0b2fcc8b3de3fef8b9bbda3f02292a6e5
Pranam Lashkari <lpranam@collabora.com>
2025-09-11 18:20:38 +0200

    followme slide: allow users to follow and unfollow slideshow
    usage:
    when a follower tries to navigate the presentation himself
    he automatically unfollows the presentation and free to navigate
    follower has extra button in presentation bar to start following the
    presentation again which brings them to the same slide as leader and
    follows again
    
    Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
    Change-Id: Id7491c8981c72e50000f6379612963fe63d03cb0

--- browser/src/slideshow/engine/SlideShowNavigator.ts
+++ browser/src/slideshow/engine/SlideShowNavigator.ts
@@ -24,6 +24,7 @@ class SlideShowNavigator {
 	private isRewindingToPrevSlide: boolean;
 	private lastClickTime: number = 0;
 	private readonly RAPID_CLICK_THRESHOLD = 500; // 500ms
+	private currentLeaderSlide: number = -1;
 
 	constructor(slideShowHandler: SlideShowHandler) {
 		this.slideShowHandler = slideShowHandler;
@@ -218,7 +219,22 @@ class SlideShowNavigator {
 		this.isRewindingToPrevSlide = false;
 	}
 
+	setLeaderSlide(info: any) {
+		this.currentLeaderSlide = info.currentSlide;
+	}
+
+	followLeaderSlide() {
+		this.presenter.setFollowing(true);
+		this.displaySlide(this.currentLeaderSlide, true);
+	}
+
 	displaySlide(nNewSlide: number, bSkipTransition: boolean) {
+		if (this.presenter.isLeader()) {
+			app.socket.sendMessage(
+				'slideshowfollow displayslide ' +
+					JSON.stringify({ currentSlide: nNewSlide }),
+			);
+		}
 		NAVDBG.print(
 			'SlideShowNavigator.displaySlide: current index: ' +
 				this.currentSlide +
@@ -346,6 +362,7 @@ class SlideShowNavigator {
 	}
 
 	private clickHandler(aEvent: MouseEvent) {
+		if (this.presenter.isFollower()) this.presenter.setFollowing(false);
 		if (aEvent.button === 0) {
 			const slideInfo = this.theMetaPres.getSlideInfoByIndex(this.currentSlide);
 			const slideHasInteractions =
