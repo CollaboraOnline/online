CollaboraOnline/online

commit 28a027905c8057967330d89fae463aa1a1a55e7c

Moves viewedRectangle and sendClientVisibleArea into ViewLayoutBase.
Others are mostly renames for the documentTopLeft and the like.

Signed-off-by: Gökay Şatır <gokaysatir@gmail.com>
Change-Id: I950598c92f66cae065b755f71a3971df59c9d001

browser/src/app/ViewLayout.ts
@@ -1,0 +1,87 @@
+/* -*- js-indent-level: 8 -*- */
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+class ViewLayoutBase {
+	viewedRectangle: cool.SimpleRectangle;
+	lastViewedRectangle: cool.SimpleRectangle;
+	private clientVisibleAreaCommand: string = '';
+
+	constructor() {
+		this.viewedRectangle = new cool.SimpleRectangle(0, 0, 0, 0);
+		this.lastViewedRectangle = new cool.SimpleRectangle(0, 0, 0, 0);
+	}
+
+	public resetClientVisibleArea(): void {
+		this.lastViewedRectangle = new cool.SimpleRectangle(0, 0, 0, 0);
+	}
+
+	public sendClientVisibleArea(forceUpdate: boolean = false): void {
+		if (!app.map._docLoaded) return;
+
+		var splitPos = app.map._docLayer._splitPanesContext
+			? app.map._docLayer._splitPanesContext.getSplitPos()
+			: new L.Point(0, 0);
+
+		var visibleArea = app.map.getPixelBounds();
+		visibleArea = new L.Bounds(
+			app.map._docLayer._pixelsToTwips(visibleArea.min),
+			app.map._docLayer._pixelsToTwips(visibleArea.max),
+		);
+		splitPos = app.map._docLayer._corePixelsToTwips(splitPos);
+		var size = visibleArea.getSize();
+		var visibleTopLeft = visibleArea.min;
+		var newClientVisibleAreaCommand =
+			'clientvisiblearea x=' +
+			Math.round(visibleTopLeft.x) +
+			' y=' +
+			Math.round(visibleTopLeft.y) +
+			' width=' +
+			Math.round(size.x) +
+			' height=' +
+			Math.round(size.y) +
+			' splitx=' +
+			Math.round(splitPos.x) +
+			' splity=' +
+			Math.round(splitPos.y);
+
+		if (this.clientVisibleAreaCommand !== newClientVisibleAreaCommand) {
+			// Only update on some change
+			if (app.map._docLayer._ySplitter) {
+				app.map._docLayer._ySplitter.onPositionChange();
+			}
+			if (app.map._docLayer._xSplitter) {
+				app.map._docLayer._xSplitter.onPositionChange();
+			}
+			// Visible area is dirty, update it on the server
+			app.socket.sendMessage(newClientVisibleAreaCommand);
+			if (app.map.contextToolbar) app.map.contextToolbar.hideContextToolbar(); // hide context toolbar when scroll/window resize etc...
+			if (!app.map._fatal && app.idleHandler._active && app.socket.connected())
+				this.clientVisibleAreaCommand = newClientVisibleAreaCommand;
+		}
+	}
+
+	public getLastPanDirection(): Array<number> {
+		var dx: number = this.viewedRectangle.pX1 - this.lastViewedRectangle.pX1;
+		var dy: number = this.viewedRectangle.pY1 - this.lastViewedRectangle.pY1;
+		return [Math.sign(dx), Math.sign(dy)];
+	}
+
+	public setViewedRectangle(rectangle: cool.SimpleRectangle): void {
+		this.viewedRectangle = rectangle;
+
+		// maintain a view of where we're panning to.
+		if (!this.viewedRectangle.equals(this.lastViewedRectangle.toArray()))
+			this.lastViewedRectangle = this.viewedRectangle.clone();
+
+		app.sectionContainer.onNewDocumentTopLeft();
+		app.sectionContainer.requestReDraw();
+	}
+}

browser/Makefile.am
@@ -258,7 +258,9 @@ COOL_JS_LST =\
 	src/app/GraphicSelectionMiddleware.ts \
 	src/app/TextSelectionMiddleware.ts \
 	src/app/CellSelectionMarkerMiddleware.ts \
-	src/app/MultiPageViewLayout.ts \
+	src/app/ViewLayout.ts \
+	src/app/ViewLayoutMultiPage.ts \
+	src/app/DocumentBase.ts \
 	src/app/TilesMiddleware.ts \
 	src/app/SlideshowMiddleware.ts \
 	src/app/SearchService.ts \

browser/mocha_tests/CanvasSectionContainer.test.ts
@@ -16,7 +16,7 @@
 /// <reference path="../src/app/TilesMiddleware.ts" />
 /// <reference path="../src/app/Rectangle.ts" />
 /// <reference path="../src/app/LOUtil.ts" />
-/// <reference path="../src/app/MultiPageViewLayout.ts" />
+/// <reference path="../src/app/ViewLayoutMultiPage.ts" />
 /// <reference path="./helper/canvasContainerSetup.ts" />
 /// <reference path="./helper/rectUtil.ts" />
 

browser/src/app/DocumentBase.ts
@@ -1,0 +1,19 @@
+/* -*- js-indent-level: 8 -*- */
+
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+class DocumentBase {
+	public activeView: ViewLayoutBase;
+
+	constructor() {
+		this.activeView = new ViewLayoutBase();
+	}
+}

browser/src/app/SearchService.ts
@@ -73,8 +73,8 @@ class SearchService {
 
 		L.Map.THIS.fire('clearselection');
 
-		var searchStartPointX = app.file.viewedRectangle.x1;
-		var searchStartPointY = app.file.viewedRectangle.y1;
+		var searchStartPointX = app.activeDocument.activeView.viewedRectangle.x1;
+		var searchStartPointY = app.activeDocument.activeView.viewedRectangle.y1;
 		if (
 			L.Map.THIS._docLayer &&
 			L.Map.THIS._docLayer._lastSearchResult &&

browser/src/app/TextSelectionMiddleware.ts
@@ -104,11 +104,11 @@ class TextSelections {
 		if (app.map._docLayer.isCalcRTL()) {
 			// Mirror position from right to left.
 			startPos.x =
-				app.file.viewedRectangle.pX2 -
-				(startPos.x - app.file.viewedRectangle.pX1);
+				app.activeDocument.activeView.viewedRectangle.pX2 -
+				(startPos.x - app.activeDocument.activeView.viewedRectangle.pX1);
 			endPos.x =
-				app.file.viewedRectangle.pX2 -
-				(endPos.x - app.file.viewedRectangle.pX1);
+				app.activeDocument.activeView.viewedRectangle.pX2 -
+				(endPos.x - app.activeDocument.activeView.viewedRectangle.pX1);
 		}
 
 		const oldStart = this.start.getPosition();

browser/src/app/TilesMiddleware.ts
@@ -1266,7 +1266,7 @@ class TileManager {
 				// request separately from the current viewPort to get
 				// those tiles first.
 
-				const direction = app.sectionContainer.getLastPanDirection();
+				const direction = app.activeDocument.activeView.getLastPanDirection();
 
 				// Conservatively enlarge the area to round to more tiles:
 				const pixelTopLeft = this._pixelBounds.getTopLeft();
@@ -2082,8 +2082,6 @@ class TileManager {
 	}
 
 	public static update(center: any = null, zoom: number = null) {
-		if (app.file.writer.multiPageView) return;
-
 		const map: any = app.map;
 
 		if (
@@ -2136,7 +2134,7 @@ class TileManager {
 		var queue = this.getMissingTiles(pixelBounds, zoom, true);
 
 		app.map._docLayer._sendClientZoom();
-		app.map._docLayer._sendClientVisibleArea();
+		app.activeDocument.activeView.sendClientVisibleArea();
 
 		if (queue.length !== 0) this.addTiles(queue, true);
 
@@ -2262,7 +2260,7 @@ class TileManager {
 
 	public static expandTileRange(range: cool.Bounds): cool.Bounds {
 		const grow = this.visibleTileExpansion;
-		const direction = app.sectionContainer.getLastPanDirection();
+		const direction = app.activeDocument.activeView.getLastPanDirection();
 		const minOffset = new L.Point(
 			grow - grow * this.directionalTileExpansion * Math.min(0, direction[0]),
 			grow - grow * this.directionalTileExpansion * Math.min(0, direction[1]),
@@ -2299,7 +2297,8 @@ class TileManager {
 	}
 
 	public static getVisibleCoordList(
-		rectangle: cool.SimpleRectangle = app.file.viewedRectangle,
+		rectangle: cool.SimpleRectangle = app.activeDocument.activeView
+			.viewedRectangle,
 	) {
 		const coordList = Array<TileCoordData>();
 		const zoom = app.map.getZoom();
@@ -2364,7 +2363,7 @@ class TileManager {
 		var mode = 0; // mode is different only in Impress MasterPage mode so far
 
 		var intersectionAreaRectangle = app.LOUtil._getIntersectionRectangle(
-			app.file.viewedRectangle.pToArray(),
+			app.activeDocument.activeView.viewedRectangle.pToArray(),
 			[0, 0, partWidthPixels, partHeightPixels * app.map._docLayer._parts],
 		);
 
@@ -2383,7 +2382,9 @@ class TileManager {
 			var startPart = Math.floor(
 				intersectionAreaRectangle[1] / partHeightPixels,
 			);
-			var startY = app.file.viewedRectangle.pY1 - startPart * partHeightPixels;
+			var startY =
+				app.activeDocument.activeView.viewedRectangle.pY1 -
+				startPart * partHeightPixels;
 			startY = Math.floor(startY / app.tile.size.pY) * app.tile.size.pY;
 
 			var endPart = Math.ceil(
@@ -2391,8 +2392,8 @@ class TileManager {
 					partHeightPixels,
 			);
 			var endY =
-				app.file.viewedRectangle.pY1 +
-				app.file.viewedRectangle.pY2 -
+				app.activeDocument.activeView.viewedRectangle.pY1 +
+				app.activeDocument.activeView.viewedRectangle.pY2 -
 				endPart * partHeightPixels;
 			endY = Math.floor(endY / app.tile.size.pY) * app.tile.size.pY;
 
@@ -2432,7 +2433,7 @@ class TileManager {
 		if (checkOnly) {
 			return queue;
 		} else {
-			app.map._docLayer._sendClientVisibleArea();
+			app.activeDocument.activeView.sendClientVisibleArea();
 			app.map._docLayer._sendClientZoom();
 
 			var tileCombineQueue = [];

browser/src/app/ViewLayoutMultiPage.ts
@@ -48,6 +48,8 @@ class MultiPageViewLayout {
 	private static resetViewLayout() {
 		this.layoutRectangles.length = 0;
 
+		if (app.file.writer.pageRectangleList.length === 0) return;
+
 		const canvasSize = app.sectionContainer.getViewSize();
 
 		// Copy the page rectangle array.
@@ -93,7 +95,8 @@ class MultiPageViewLayout {
 				const rowItemCount = j - i;
 				const gap = (rowItemCount - 1) * this.gapBetweenPages;
 				const margin = (canvasSize[0] - totalWidth + gap) * 0.5;
-				let currentX = margin + app.file.viewedRectangle.pX1;
+				let currentX =
+					margin + app.activeDocument.activeView.viewedRectangle.pX1;
 				let maxY = 0;
 				for (let k = i; k < j; k++) {
 					this.layoutRectangles[k].layoutX = currentX;
@@ -118,8 +121,70 @@ class MultiPageViewLayout {
 		app.view.size.pY = Math.max(lastY, canvasSize[1]);
 	}
 
+	private static getContainingPageRectangle(point: cool.SimplePoint) {
+		for (let i = 0; i < this.layoutRectangles.length; i++) {
+			if (this.layoutRectangles[i].containsPoint(point.toArray()))
+				return this.layoutRectangles[i];
+		}
+
+		return null;
+	}
+
+	public static viewPixelsToTwips(x: number, y: number): number[] {
+		for (let i = 0; i < this.layoutRectangles.length; i++) {
+			const rectangle = this.layoutRectangles[i];
+			const bounds = [
+				rectangle.layoutX - app.activeDocument.activeView.viewedRectangle.pX1,
+				rectangle.layoutY - app.activeDocument.activeView.viewedRectangle.pY1,
+				rectangle.pWidth,
+				rectangle.pHeight,
+			];
+
+			if (x > bounds[0] && x < bounds[0] + bounds[2]) {
+				if (y > bounds[1] && y < bounds[1] + bounds[3]) {
+					return [
+						Math.round(
+							(rectangle.pX1 +
+								(x -
+									rectangle.layoutX +
+									app.activeDocument.activeView.viewedRectangle.pX1)) *
+								app.pixelsToTwips,
+						),
+						Math.round(
+							(rectangle.pY1 +
+								(y -
+									rectangle.layoutY +
+									app.activeDocument.activeView.viewedRectangle.pY1)) *
+								app.pixelsToTwips,
+						),
+					];
+				}
+			}
+		}
+
+		return [-1, -1];
+	}
+
+	// Returns view coordinate of given document coordinate.
+	public static twipsToViewPixels(x: number, y: number): number[] {
+		const point = new cool.SimplePoint(x, y);
+		const containingRectangle = this.getContainingPageRectangle(point);
+
+		if (containingRectangle) {
+			return [
+				containingRectangle.layoutX +
+					(point.pX - containingRectangle.pX1) -
+					app.activeDocument.activeView.viewedRectangle.pX1,
+				containingRectangle.layoutY +
+					(point.pY - containingRectangle.pY1) -
+					app.activeDocument.activeView.viewedRectangle.pY1,
+			];
+		} else return [0, 0];
+	}
+
 	public static getVisibleAreaRectangle() {
-		const viewedRectangle = app.file.viewedRectangle.clone();
+		const viewedRectangle =
+			app.activeDocument.activeView.viewedRectangle.clone();
 		viewedRectangle.pWidth = app.view.size.pX;
 		viewedRectangle.pHeight = app.view.size.pY;
 		const resultingRectangle = new cool.SimpleRectangle(

commit 91fa901cd7466229d79823c40de8d986f2a7849d

Mend getLastPanDirection so we can pre-load pre-emptively again.
Mis-ordering of assignments made the pan-direction always 0,0.
Significantly improves pre-loading when page-up/down. Trivially
visible in the pre-load map.

Regression from 28a027905c8057967330d89fae463aa1a1a55e7c
"Moves viewedRectangle and sendClientVisibleArea into ViewLayoutBase."

Change-Id: Ia2eb7cbe2cf07fe5a7ac1f90b77945b527e13ffd
Signed-off-by: Michael Meeks <michael.meeks@collabora.com>

browser/src/app/ViewLayout.ts
@@ -110,11 +110,11 @@ class ViewLayoutBase {
 	}
 
 	public set viewedRectangle(rectangle: cool.SimpleRectangle) {
-		this._viewedRectangle = rectangle;
-
 		// maintain a view of where we're panning to.
-		if (!this._viewedRectangle.equals(this.lastViewedRectangle.toArray()))
+		if (!this._viewedRectangle.equals(rectangle.toArray()))
 			this.lastViewedRectangle = this._viewedRectangle.clone();
+
+		this._viewedRectangle = rectangle;
 
 		app.sectionContainer.onNewDocumentTopLeft();
 		app.sectionContainer.requestReDraw();