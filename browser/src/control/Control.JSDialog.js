CollaboraOnline/online

commit 47729f2f540c34d8b1c58a42bd5909e64ca2338a

Browser zoom: fix autofilter dialog position on HiDPI screens
-Positioned the autofilter dialog using the AutoFillMarker coordinates.
-This ensures consistent placement across zoom levels and pixel densities.
-Since the AutoFillMarker appears at the right edge of the cell, it serves
 as a reliable anchor point for the dialog's top right corner
-Added a max-height to the autofilter list to prevent it from occupying
 excessive vertical space.

Signed-off-by: Banobe Pascal <banobe.pascal@collabora.com>
Change-Id: I2a5f375f689fc97498af5ec57101a8c21b8807f3

browser/src/control/Control.JSDialog.js
@@ -604,49 +604,27 @@ L.Control.JSDialog = L.Control.extend({
 
 		// RTL mode: only difference is when file is RTL not UI
 		// var isViewRTL = document.documentElement.dir === 'rtl';
-		var isSpreadsheetRTL = this.map._docLayer.isCalcRTL();
-
-		var scale = this.map.zoomToFactor(this.map.getZoom());
-		var origin = this.map.getPixelOrigin();
-		var panePos = this.map._getMapPanePos();
-
-		var offsetX = isSpreadsheetRTL ? 0 : app.sectionContainer.getSectionWithName(L.CSections.RowHeader.name).size[0];
-		var offsetY = app.sectionContainer.getSectionWithName(L.CSections.ColumnHeader.name).size[1];
+		// var isSpreadsheetRTL = this.map._docLayer.isCalcRTL();
+		const autoFillSection = app.sectionContainer.getSectionWithName(L.CSections.AutoFillMarker.name);
 
 		if (this.isChildAutoFilter(instance)) {
 			this.calculateSubmenuAutoFilterPosition(instance, this.parentAutofilter);
 			return;
 		}
-		this.parentAutofilter = instance.form;
-		const devicePixelRatio = app.dpiScale;
-		// Convert the server-provided px values to screen coordinates
-		instance.posx = instance.posx / devicePixelRatio;
-		instance.posy = instance.posy / devicePixelRatio;
-		var left = parseInt(instance.posx) * scale;
-		var top = parseInt(instance.posy) * scale;
 
-		var splitPanesContext = this.map.getSplitPanesContext();
-		var splitPos = new L.Point(0, 0);
+		const markerCanvasPos = {
+			left: autoFillSection.myTopLeft[0],
+			top: autoFillSection.myTopLeft[1],
+			width: autoFillSection.size[0],
+			height: autoFillSection.size[1]
+		};
 
-		if (splitPanesContext)
-			splitPos = splitPanesContext.getSplitPos();
+		const canvasEl = this.map._docLayer._canvas.getBoundingClientRect();
 
-		var newLeft = left + panePos.x - origin.x;
-		if (left >= splitPos.x && newLeft >= 0)
-			left = newLeft;
+		const left = ((markerCanvasPos.left + markerCanvasPos.width) / app.dpiScale) + canvasEl.left;
 
-		var newTop = top + panePos.y - origin.y;
-		if (top >= splitPos.y && newTop >= 0)
-			top = newTop;
-
-		if (isSpreadsheetRTL)
-			left = this.map._size.x - left;
-
-		var canvasEl = this.map._docLayer._canvas.getBoundingClientRect();
-
-		instance.posx = left + offsetX + canvasEl.left; // adding canvasEl.left in case we change sidebar to left of the screen
-		// make margin from canvas top and not from window top
-		instance.posy = top + offsetY + canvasEl.top;
+		instance.posy = ((markerCanvasPos.top + markerCanvasPos.height) / app.dpiScale) + canvasEl.top;
+		instance.posx =  left - instance.container.offsetWidth;
 
 		this.updateAutoPopPosition(instance.container, instance.posx, instance.posy);
 	},

browser/css/jsdialogs.css
@@ -800,6 +800,15 @@ input[type='checkbox']:checked.autofilter, .jsdialog input[type='checkbox']:chec
 	border-left: none;
 	border-right: none;
 }
+
+.autofilter #list.ui-treeview {
+	max-height: 250px;
+}
+
+.autofilter #list.ui-treeview,
+#check_list_box.autofilter.ui-treeview {
+	min-height: auto;
+}
 /* checkbox */
 
 .jsdialog.checkbutton {

commit 292c89befb1127a4c0b9a0421391df42373f99d8

jsdialog: fix incorrect autofilter subdialog position
regression from: 47729f2f540c34d8b1c58a42bd5909e64ca2338a

Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
Change-Id: I5ec8aafdd19dea315e4f155600a8d42400af9d12

browser/src/control/Control.JSDialog.js
@@ -654,6 +654,8 @@ window.L.Control.JSDialog = window.L.Control.extend({
 		*/
 		let cellRectangle;
 
+		this.parentAutofilter = instance.form;
+
 		if (app.calc.autoFilterCell) {
 			// This is an AutoFilterDialog. We have the row and column indexes. Get cell rectangle with this info.
 			cellRectangle = app.map._docLayer.sheetGeometry.getCellSimpleRectangle(
@@ -708,7 +710,7 @@ window.L.Control.JSDialog = window.L.Control.extend({
 	},
 
 	calculateSubmenuAutoFilterPosition: function(instance, parentAutofilter) {
-		var parentAutofilter = parentAutofilter.getBoundingClientRect();
+		var parentAutofilter = parentAutofilter.getElementsByClassName("ui-treeview-entry selected")[0].getBoundingClientRect();
 		instance.posx = parentAutofilter.right;
 		instance.posy = parentAutofilter.top;
 