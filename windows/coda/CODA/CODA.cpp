99b591c86951e2d9e95eac79531e99c2f9b7fa0a
Tor Lillqvist <tml@collabora.com>
2025-09-23 14:14:18 +0300

    Also the "slidelayer:" message is binary
    Now slide shows work in CODA-W.
    
    Signed-off-by: Tor Lillqvist <tml@collabora.com>
    Change-Id: I73106b13ad54851811c823ebaebb7d85b63538ed

--- windows/coda/CODA/CODA.cpp
+++ windows/coda/CODA/CODA.cpp
@@ -271,11 +271,14 @@ static bool isMessageOfType(const char* message, const std::string& type, int le
 
 static void send2JS(const HWND hWnd, const char* buffer, int length)
 {
+    // Same list of message types as in Message::detectType. (Except that "urp:" is irrelevant in
+    // the MOBILEAPP case, I think?) Would be great to have these in just one place.
     const bool binaryMessage = (isMessageOfType(buffer, "tile:", length) ||
                                 isMessageOfType(buffer, "tilecombine:", length) ||
                                 isMessageOfType(buffer, "delta:", length) ||
                                 isMessageOfType(buffer, "renderfont:", length) ||
                                 isMessageOfType(buffer, "rendersearchlist:", length) ||
+                                isMessageOfType(buffer, "slidelayer:", length) ||
                                 isMessageOfType(buffer, "windowpaint:", length));
     std::string pretext{ binaryMessage
                              ? "window.TheFakeWebSocket.onmessage({'data': window.atob('"
