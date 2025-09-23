36ce917009ffac3035fa6007a7a63aa1d8202a6d
Sahil Gautam <sahil.gautam@collabora.com>
2025-09-23 15:03:07 +0530

    make the resize handles rectangles
    Signed-off-by: Sahil Gautam <sahil.gautam@collabora.com>
    Change-Id: I277386467816e20dd30996388a8c596a102ca496

--- browser/src/canvas/sections/ShapeHandleScalingSubSection.ts
+++ browser/src/canvas/sections/ShapeHandleScalingSubSection.ts
@@ -64,7 +64,7 @@ class ShapeHandleScalingSubSection extends CanvasSectionObject {
 		if (this.sectionProperties.cropModeEnabled)
 			this.drawCropHandles();
 		else
-			this.context.arc(this.size[0] * 0.5, this.size[1] * 0.5, this.size[0] * 0.5, 0, Math.PI * 2);
+			this.context.rect(0, 0, this.size[0], this.size[1]);
 		this.context.closePath();
 		this.context.fill();
 		this.context.stroke();
