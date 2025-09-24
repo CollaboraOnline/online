4b38937b01081f742f52c927fe2e670150688fab
Parth Raiyani <parth.raiyani@collabora.com>
2025-09-24 18:34:10 +0530

    fix: adjust snapshot comparison threshold for sidebar visual test to capture broken sidebar more strictly
    Signed-off-by: Parth Raiyani <parth.raiyani@collabora.com>
    Change-Id: I1ca4c2e8d33e60e9133f2b83152164f288976fd5

--- cypress_test/integration_tests/desktop/writer/sidebar_spec.js
+++ cypress_test/integration_tests/desktop/writer/sidebar_spec.js
@@ -14,7 +14,7 @@ describe(['tagdesktop'], 'Sidebar tests', function() {
 		cy.wait(500); // wait to make fully rendered
 		cy.cGet('#sidebar-dock-wrapper').scrollTo(0,0,{ ensureScrollable: false });
 		cy.wait(500); // wait for animations
-		cy.cGet('#sidebar-dock-wrapper').compareSnapshot('sidebar_writer', 0.1);
+		cy.cGet('#sidebar-dock-wrapper').compareSnapshot('sidebar_writer', 0.07);
 	});
 
 	it('Show table panel multiple times', function() {
