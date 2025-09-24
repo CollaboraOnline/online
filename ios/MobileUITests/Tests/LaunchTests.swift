76f2078e05f67fa86a151409969d01b94e31bc1a
Skyler Grey <skyler.grey@collabora.com>
2025-09-24 14:22:42 +0000

    test(iOS): create basic iOS XCUI test app
    I've included only a single test - a launch performance test - in this
    first version, but XCUI has the ability to automate the app as a user
    would (largely based on accessibility properties).
    
    The end goal is to have the mobile app tested a lot like we do Cypress
    tests for desktop, and to have these run in CI on all changes, and to
    write - at minimum - regression tests when we fix mobile bugs.
    
    Signed-off-by: Skyler Grey <skyler.grey@collabora.com>
    Change-Id: I6a6a696498dfc0654536fdddf8daac1baab1d61b

---
+++ ios/MobileUITests/Tests/LaunchTests.swift
@@ -0,0 +1,29 @@
+// -*- Mode: swift; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*-
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import XCTest
+
+final class LaunchTests: XCTestCase {
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+        XCUIDevice.shared.orientation = UIDeviceOrientation.landscapeLeft
+    }
+    
+    @MainActor
+    func testLaunchPerformance() throws {
+        let app = XCUIApplication()
+
+        measure(metrics: [XCTApplicationLaunchMetric()]) {
+            Launch.fileBrowser(app: app)
+            app.terminate()
+        }
+    }
+}
