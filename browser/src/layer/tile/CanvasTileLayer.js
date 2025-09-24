152bcd7d4a53dae061fe1f3960e2d90d9c54143d
Szymon Kłos <szymon.klos@collabora.com>
2025-09-24 09:43:43 +0000

    notebookbar: do not reload, just hide server audit
    - load time might be different in different instances
    - server audit dialog was reloading notebookbar on message
      from core
    - when we reload notebookbar we lose all previous updates
      like style previews
    - do not reload, instead hide server audit when needed
      so we do not break style previews in case of different event orders
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I9643c65e0583ee7b37b5099d7ce64f990bdb7ad4

--- browser/src/layer/tile/CanvasTileLayer.js
+++ browser/src/layer/tile/CanvasTileLayer.js
@@ -1351,7 +1351,9 @@ window.L.CanvasTileLayer = window.L.Layer.extend({
 			var serverAudit = textMsg.substr(12).trim();
 			if (serverAudit !== 'disabled') {
 				// if isAdminUser property is not set by integration - enable audit dialog for all users
-				if (app.isAdminUser !== false)
+				if (app.isAdminUser === false)
+					this._map.uiManager.notebookbar.hideItem('server-audit');
+				else
 					this._map.serverAuditDialog = JSDialog.serverAuditDialog(this._map);
 
 				var json = JSON.parse(serverAudit);
