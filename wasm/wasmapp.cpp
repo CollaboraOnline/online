CollaboraOnline/online

commit 5f9922dfcf1a64ed5a828f9de046693137f73261

Wasm: Disable problematic code of unclear use
At least in a plain scenario where online is built with Emscripten
(--host=wasm32-local-emscripten) and the resulting browser/dist/cool.html is
served (e.g., via `emrun browser/dist/cool.html`), the code that calls
emscripten_fetch and writes a local file (originally introduced with
c2c0fe6f514feaa9032a89fa2f0cc55af57c8d70 "wasm: accept url as command-line
argument" here on the wasm/wasmapp.cpp side and
a7d6d1debc46f618733e180bd1b8ff87e7303ab7 "wasm: pass the docURL to the wasm app"
on the browser/src/main.js side) is not actually used:

For example, with recent Chrome 138.0.7204.168, this causes

> isWOPI is false: Fetching from url file:///sample.docx/contents
> Not allowed to load local resource: file:///sample.docx/contents
>  fetchXHR @ online.js:10603
>  performUncachedXhr @ online.js:10813
>  fetchLoadCachedData @ online.js:10670
>  _emscripten_start_fetch @ online.js:10850
>  $emscripten_fetch @ emscripten_fetch.c:146
>  $void* std::__2::__thread_proxy[abi:ne200100]<std::__2::tuple<std::__2::unique_ptr<std::__2::__thread_struct, std::__2::default_delete<std::__2::__thread_struct>>, main(int, char**)::$_1>>(void*) @ wasmapp.cpp:253
>  invokeEntryPoint @ online.js:2028
>  handleMessage @ online.js:1096
> Downloading file:///sample.docx/contents failed, HTTP failure status code: 0.

effectively bypassing all that code.  (The actual loading of the bundled
/sample.docx is triggered with the hard-coded

> #define FILE_PATH "/sample.docx"
> static std::string fileURL = "file://" FILE_PATH;
[...]
>         LOG_TRC_NOFILE("Actually sending to Online:" << fileURL);
>         std::cout << "Loading file [" << fileURL << "]" << std::endl;
[...]
>         fakeSocketWrite(fakeClientFd, fileURL.c_str(), fileURL.size());

cf. a5740dcde7760f87ed14ab91ba38a6e48a5a49b7 "Using handle_cool_message and
send2JS in wasmapp.cpp", 48c96e70ca5625c2e1e1eae5dacfcfc3f5233f0d "More WASM

wasm/wasmapp.cpp
@@ -226,40 +226,42 @@ int main(int argc, char* argv_main[])
     std::thread(
         [&]
         {
-            const std::string docURL = std::string(argv_main[1]);
-            const std::string encodedWOPI = std::string(argv_main[2]);
-            const std::string isWOPI = std::string(argv_main[3]);
+            if ((false)) { //TODO: clarify which configuration wants to use this
+                const std::string docURL = std::string(argv_main[1]);
+                const std::string encodedWOPI = std::string(argv_main[2]);
+                const std::string isWOPI = std::string(argv_main[3]);
 
-            std::string url;
-            if (isWOPI == "true")
-                url = "/wasm/" + encodedWOPI;
-            else
-                url = docURL + "/contents";
+                std::string url;
+                if (isWOPI == "true")
+                    url = "/wasm/" + encodedWOPI;
+                else
+                    url = docURL + "/contents";
 
-            printf("isWOPI is %s: Fetching from url %s\n", isWOPI.c_str(), url.c_str());
+                printf("isWOPI is %s: Fetching from url %s\n", isWOPI.c_str(), url.c_str());
 
-            emscripten_fetch_attr_t attr;
-            emscripten_fetch_attr_init(&attr);
-            strcpy(attr.requestMethod, "GET");
-            attr.attributes = EMSCRIPTEN_FETCH_LOAD_TO_MEMORY | EMSCRIPTEN_FETCH_SYNCHRONOUS;
-            emscripten_fetch_t* fetch = emscripten_fetch(
-                &attr, url.data()); // Blocks here until the operation is complete.
-            if (fetch->status == 200)
-            {
-                printf("Finished downloading %llu bytes from URL %s.\n", fetch->numBytes,
-                       fetch->url);
-                // For now, we have a hard-coded filename that we open. Clobber it.
-                FILE* f = fopen(FILE_PATH, "w");
-                const int wrote = fwrite(fetch->data, 1, fetch->numBytes, f);
-                fclose(f);
-                printf("Wrote %d bytes into " FILE_PATH "\n", wrote);
+                emscripten_fetch_attr_t attr;
+                emscripten_fetch_attr_init(&attr);
+                strcpy(attr.requestMethod, "GET");
+                attr.attributes = EMSCRIPTEN_FETCH_LOAD_TO_MEMORY | EMSCRIPTEN_FETCH_SYNCHRONOUS;
+                emscripten_fetch_t* fetch = emscripten_fetch(
+                    &attr, url.data()); // Blocks here until the operation is complete.
+                if (fetch->status == 200)
+                {
+                    printf("Finished downloading %llu bytes from URL %s.\n", fetch->numBytes,
+                           fetch->url);
+                    // For now, we have a hard-coded filename that we open. Clobber it.
+                    FILE* f = fopen(FILE_PATH, "w");
+                    const int wrote = fwrite(fetch->data, 1, fetch->numBytes, f);
+                    fclose(f);
+                    printf("Wrote %d bytes into " FILE_PATH "\n", wrote);
+                }
+                else
+                {
+                    printf("Downloading %s failed, HTTP failure status code: %d.\n", fetch->url,
+                           fetch->status);
+                }
+                emscripten_fetch_close(fetch);
             }
-            else
-            {
-                printf("Downloading %s failed, HTTP failure status code: %d.\n", fetch->url,
-                       fetch->status);
-            }
-            emscripten_fetch_close(fetch);
 
             coolwsd = new COOLWSD();
             coolwsd->run(1, argv);

commit 73e92f6cd5c283f89d108f78eef216386c82cb83

Reinstate the Wasm "Served from a COOL instance" mode
...that is documented in wasm/README.no-container.md and had been partially
broken by 5f9922dfcf1a64ed5a828f9de046693137f73261 "Wasm: Disable problematic
code of unclear use" (which this commit reverts):  In that mode,
browser/html/wasm.html sets up a WOPISrc that is then fetched here from the
server and overwrites /sample.docx in the Emscripten filesystem (or the
pre-exisiting /sample.docx remains intact when fetching WOPISrc fails, which
would always be the case when using the non-exisiting
<https://localhost:9980/browser/c85d8681f3/wasm.html?file_path=/some/unused/path>
mentioned in wasm/README.no-container.md; but using a
file_path=/some/existing/test.odt that is actually provided by the
(--enable-debug?) COOL server does work, too; in any case, the exisiting code
will then continue to load that /sample.docx from the Emscripten filesystem,
regardless of what file_path was originally given).

For the other Wasm "Served stand-alone from a plain web server" mode of
operation, which does not involve a COOL server integration, and just serves a
Wasm build's browser/dist/cool.html, this code had tried to fetch
<file:///sample.docx/contents>, which caused the Firefox issues discussed in the
commit message of 5f9922dfcf1a64ed5a828f9de046693137f73261 "Wasm: Disable
problematic code of unclear use".  To keep avoiding those issues, we simply
don't try to fetch <file:///sample.docx/contents> any more in the first place
(which always failed, anyway, and resulted in using the pre-existing
/sample.docx from the Emscripten filesystem).  (I have no idea what that attempt
of fetching <.../contents>, which first appeared in
c2c0fe6f514feaa9032a89fa2f0cc55af57c8d70 "wasm: accept url as command-line
argument", was exactly meant to be good for.  It appears to be related to WOPI,
cf. section 3.3.5.3 "HTTP://server/<...>/wopi*/files/<id>/contents" of
"[MS-WOPI]: Web Application Open Platform Interface Protocol", v20250819.)

wasm/wasmapp.cpp
@@ -227,16 +227,13 @@ int main(int argc, char* argv_main[])
         {
             Util::setThreadName("COOLWSD::run");
 
-            if ((false)) { //TODO: clarify which configuration wants to use this
-                const std::string docURL = std::string(argv_main[1]);
-                const std::string encodedWOPI = std::string(argv_main[2]);
-                const std::string isWOPI = std::string(argv_main[3]);
+            const std::string docURL = std::string(argv_main[1]);
+            const std::string encodedWOPI = std::string(argv_main[2]);
+            const std::string isWOPI = std::string(argv_main[3]);
 
-                std::string url;
-                if (isWOPI == "true")
-                    url = "/wasm/" + encodedWOPI;
-                else
-                    url = docURL + "/contents";
+            if (isWOPI == "true")
+            {
+                std::string url = "/wasm/" + encodedWOPI;
 
                 printf("isWOPI is %s: Fetching from url %s\n", isWOPI.c_str(), url.c_str());
 