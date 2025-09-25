af29f258af5c5d7e51d43f9bc32cea0853ef0f6d
Mohit Marathe <mohit.marathe@collabora.com>
2025-09-25 17:50:42 +0530

    Xcu: add xcu labels to the settingLabels map
    also revert f816516f5297c20317cba800d3d87a6f1b250c58 as there is no
    point in parsing then...
    
    Signed-off-by: Mohit Marathe <mohit.marathe@collabora.com>
    Change-Id: I02c5283494824931fe2c69eaa2ccc5df7aa8e6f4

--- browser/admin/src/integrator/Xcu.ts
+++ browser/admin/src/integrator/Xcu.ts
@@ -164,18 +164,6 @@ class Xcu {
 	}
 
 	private parse(content: string): XcuObject {
-		function format(propName: string): string {
-			let result = propName[0];
-			for (let i = 1; i < propName.length; i++) {
-				const char = propName[i];
-				if (char >= 'A' && char <= 'Z') {
-					result += ' ' + char.toLowerCase();
-				} else {
-					result += char;
-				}
-			}
-			return result;
-		}
 		const parser = new DOMParser();
 		const xmlDoc = parser.parseFromString(content, 'application/xml');
 
@@ -212,17 +200,16 @@ class Xcu {
 
 				if (key === 'Grid') {
 					gridLevel = currentLevel;
-					gridLevel['Show Grid'] = false;
+					gridLevel['ShowGrid'] = false;
 				}
 			});
 
 			const props = item.getElementsByTagName('prop');
 			Array.from(props).forEach((prop) => {
-				let propName = prop.getAttribute('oor:name');
+				const propName = prop.getAttribute('oor:name');
 				if (!propName) {
 					return;
 				}
-				propName = format(propName);
 
 				const valueElement = prop.getElementsByTagName('value')[0];
 				let value: string | boolean = valueElement
@@ -237,8 +224,8 @@ class Xcu {
 				}
 
 				if (typeof value === 'boolean') {
-					if (propName === 'Visible grid') {
-						gridLevel['Show Grid'] = value;
+					if (propName === 'VisibleGrid') {
+						gridLevel['ShowGrid'] = value;
 					} else {
 						currentLevel[propName] = value;
 					}
@@ -250,24 +237,12 @@ class Xcu {
 	}
 
 	private generate(xcu: XcuObject): string {
-		function format(key: string): string {
-			let result = key[0];
-			for (let i = 1; i < key.length; i++) {
-				if (key[i] === ' ' && i < key.length - 1) {
-					result += key[i + 1].toUpperCase();
-					i++;
-				} else {
-					result += key[i];
-				}
-			}
-			return result;
-		}
 		function generateItemNodes(node: any, path: string[]): string[] {
 			const items: string[] = [];
 			const leafProps: { [key: string]: string | boolean } = {};
 			const nestedKeys: string[] = [];
 
-			for (let key in node) {
+			for (const key in node) {
 				if (Object.prototype.hasOwnProperty.call(node, key)) {
 					const value = node[key];
 					if (
@@ -277,7 +252,6 @@ class Xcu {
 					) {
 						nestedKeys.push(key);
 					} else {
-						key = format(key);
 						leafProps[key] = value;
 					}
 				}
