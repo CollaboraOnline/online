CollaboraOnline/online

commit cc10d9c76490b0970d558d6e6eafece622df3b21

Allow using MenuItem.uno in action-type items
Avoids some redundancy, where the same .uno command name needed to
be repeated in every place where these items are processed.

Signed-off-by: Mike Kaganski <mike.kaganski@collabora.com>
Change-Id: Ie5d7da4e67eab3d46fe47585c78b862378981f91

browser/src/control/Control.Menubar.ts
@@ -193,10 +193,10 @@ class Menubar extends L.Control {
 					{uno: '.uno:AcceptTrackedChanges'},
 					{uno: '.uno:AcceptTrackedChange'},
 					{uno: '.uno:AcceptTrackedChangeToNext'},
-					{name: _UNO('.uno:AcceptAllTrackedChanges', 'text'), id: 'acceptalltrackedchanges', type: 'action'},
+					{type: 'action', id: 'acceptalltrackedchanges', uno: '.uno:AcceptAllTrackedChanges'},
 					{uno: '.uno:RejectTrackedChange'},
 					{uno: '.uno:RejectTrackedChangeToNext'},
-					{name: _UNO('.uno:RejectAllTrackedChanges', 'text'), id: 'rejectalltrackedchanges', type: 'action'},
+					{type: 'action', id: 'rejectalltrackedchanges', uno: '.uno:RejectAllTrackedChanges'},
 					{uno: '.uno:ReinstateTrackedChange'},
 					{uno: '.uno:PreviousTrackedChange'},
 					{uno: '.uno:NextTrackedChange'}
@@ -1111,8 +1111,8 @@ class Menubar extends L.Control {
 				{uno: '.uno:TrackChanges'},
 				{uno: '.uno:ShowTrackedChanges'},
 				{type: 'separator'},
-				{name: _UNO('.uno:AcceptAllTrackedChanges', 'text'), id: 'acceptalltrackedchanges', type: 'action'},
-				{name: _UNO('.uno:RejectAllTrackedChanges', 'text'), id: 'rejectalltrackedchanges', type: 'action'},
+				{type: 'action', id: 'acceptalltrackedchanges', uno: '.uno:AcceptAllTrackedChanges'},
+				{type: 'action', id: 'rejectalltrackedchanges', uno: '.uno:RejectAllTrackedChanges'},
 				{uno: '.uno:PreviousTrackedChange'},
 				{uno: '.uno:NextTrackedChange'}
 			]},
@@ -2041,8 +2041,7 @@ class Menubar extends L.Control {
 							}
 						}
 					} else if (id === 'acceptalltrackedchanges' || id === 'rejectalltrackedchanges') {
-						var command = id === 'acceptalltrackedchanges' ? '.uno:AcceptAllTrackedChanges' : '.uno:RejectAllTrackedChanges';
-						itemState = this._map['stateChangeHandler'].getItemValue(command);
+						itemState = this._map['stateChangeHandler'].getItemValue(uno);
 						if (itemState === 'disabled') {
 							$(aItem).addClass('disabled');
 						} else {
@@ -2140,17 +2139,19 @@ class Menubar extends L.Control {
 	 * @param itWizard - Optional wizard data.
 	 */
 	private _executeAction(itNode: any, itWizard?: any): void {
-		var id, postmessage, type;
+		var id, postmessage, type, command;
 		if (itNode === undefined)
 		{ // called from JSDialogBuilder
 			id = itWizard.id;
 			postmessage = false;
+			command = itWizard.command;
 		}
 		else
 		{ // called from
 			id = $(itNode).data('id');
 			type = $(itNode).data('type');
 			postmessage = ($(itNode).data('postmessage') === 'true');
+			command = $(itNode).data('uno');
 		}
 
 		if (id === 'save') {
@@ -2319,10 +2320,8 @@ class Menubar extends L.Control {
 			app.dispatcher.dispatch('hideslide');
 		} else if (id.indexOf('morelanguages-') != -1) {
 			this._map.fire('morelanguages', { applyto: id.substr('morelanguages-'.length) });
-		} else if (id === 'acceptalltrackedchanges') {
-			app.dispatcher.dispatch('.uno:AcceptAllTrackedChanges');
-		} else if (id === 'rejectalltrackedchanges') {
-			app.dispatcher.dispatch('.uno:RejectAllTrackedChanges');
+		} else if (id === 'acceptalltrackedchanges' || id === 'rejectalltrackedchanges') {
+			app.dispatcher.dispatch(command);
 		} else if (id === 'columnrowhighlight') {
 			app.dispatcher.dispatch('columnrowhighlight');
 		}
@@ -2608,7 +2607,7 @@ class Menubar extends L.Control {
 					ulItem.appendChild(subitemList[idx]);
 				}
 				aItem.tabIndex = 0;
-			} else if (menu[i].type === 'unocommand' || menu[i].uno !== undefined) {
+			} else if (menu[i].type === 'unocommand' || (!menu[i].type && menu[i].uno !== undefined)) {
 				$(aItem).data('type', 'unocommand');
 				$(aItem).data('uno', menu[i].uno);
 				$(aItem).data('tag', menu[i].tag);
@@ -2621,6 +2620,8 @@ class Menubar extends L.Control {
 					continue;
 				$(aItem).data('type', 'action');
 				$(aItem).data('id', menu[i].id);
+				if (menu[i].uno !== undefined)
+					$(aItem).data('uno', menu[i].uno);
 				aItem.tabIndex = 0;
 			}
 

commit af6be4c56769cbf1181f5c71b94730e84883d765

Fix Freeze commands (and ShowResolvedAnnotations) in compact view
Regression after commit cc10d9c7649, which restricted the condition
when a menu is considered an UNO command; before that, either menu
type 'unocommand', or non-empty uno, was accepted; the said commit
added a check that if the type is not 'unocommand', then it's empty.

Some menu items in Control.Menubar.ts, which were UNO commands, had
type 'action', likely because of copy-paste when they were added.
This change drops the type for them.

Signed-off-by: Mike Kaganski <mike.kaganski@collabora.com>
Change-Id: I87c66dae2c641b229e5159eb4ea83950b66511e8

browser/src/control/Control.Menubar.ts
@@ -805,10 +805,10 @@ class Menubar extends window.L.Control {
 				   {type: 'separator'},
 				   {name: _UNO('.uno:ToggleSheetGrid', 'spreadsheet', true), uno: '.uno:ToggleSheetGrid', id: 'sheetgrid'},
 				   {name: _('Focus Cell'), type:'action', id: 'columnrowhighlight'},
-				   {name: _UNO('.uno:FreezePanes', 'spreadsheet', true), id: 'FreezePanes', type: 'action', uno: '.uno:FreezePanes'},
+				   {name: _UNO('.uno:FreezePanes', 'spreadsheet', true), id: 'FreezePanes', uno: '.uno:FreezePanes'},
 				   {name: _UNO('.uno:FreezeCellsMenu', 'spreadsheet', true), id: 'FreezeCellsMenu', type: 'menu', uno: '.uno:FreezeCellsMenu', menu: [
-					   {name: _UNO('.uno:FreezePanesColumn', 'spreadsheet', true), id: 'FreezePanesColumn', type: 'action', uno: '.uno:FreezePanesColumn'},
-					   {name: _UNO('.uno:FreezePanesRow', 'spreadsheet', true), id: 'FreezePanesRow', type: 'action', uno: '.uno:FreezePanesRow'}
+					   {name: _UNO('.uno:FreezePanesColumn', 'spreadsheet', true), id: 'FreezePanesColumn', uno: '.uno:FreezePanesColumn'},
+					   {name: _UNO('.uno:FreezePanesRow', 'spreadsheet', true), id: 'FreezePanesRow', uno: '.uno:FreezePanesRow'}
 				   ]},
 				])},
 			{name: _UNO('.uno:InsertMenu', 'spreadsheet'), id: 'insert', type: 'menu', menu: [
@@ -1078,7 +1078,7 @@ class Menubar extends window.L.Control {
 				{name: _UNO('.uno:FullScreen', 'text'), id: 'fullscreen', type: 'action', mobileapp: false},
 				{uno: '.uno:ControlCodes', id: 'formattingmarks'},
 				{uno: '.uno:SpellOnline'},
-				{name: _UNO('.uno:ShowResolvedAnnotations', 'text'), id: 'showresolved', type: 'action', uno: '.uno:ShowResolvedAnnotations'},
+				{name: _UNO('.uno:ShowResolvedAnnotations', 'text'), id: 'showresolved', uno: '.uno:ShowResolvedAnnotations'},
 				{name: _('Dark Mode'), id: 'toggledarktheme', type: 'action'},
 				{name: _('Invert Background'), id: 'invertbackground', type: 'action'},
 			]