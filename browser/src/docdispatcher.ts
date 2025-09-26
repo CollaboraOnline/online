b3d05f9ac5e7a161da4345c2e436bad60e5047e1
Szymon Kłos <szymon.klos@collabora.com>
2025-09-26 13:52:06 +0000

    sidebar: common base for notebookbar panels
    - reusable sidebar command
    - can open parametrized panel on request
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: Ib50c15a275118ca9e603c35ae52503fd9b83ffc8

--- browser/src/docdispatcher.ts
+++ browser/src/docdispatcher.ts
@@ -623,7 +623,7 @@ class Dispatcher {
 		};
 
 		this.actionsMap['transitiondeck'] = () => {
-			app.map.transitionsSidebar.openTransitionSidebar();
+			app.map.sidebarFromNotebookbar.openTransitionSidebar();
 		};
 	}
 
