2e16a4be383d5b68e7e2521b17f598fbb66fca53
Szymon Kłos <szymon.klos@collabora.com>
2025-09-26 15:43:21 +0000

    sidebar: add animations deck
    - it is defined in online and reuses notebookbar's widgets in core
    - needed for compact mode as we move sidebar decks to notebookbar
    - for now it is hidden as requires core enablement
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I54aff49614ec70460d215a527636d3b1e0b23f51

--- browser/src/control/Control.Menubar.ts
+++ browser/src/control/Control.Menubar.ts
@@ -501,7 +501,8 @@ class Menubar extends window.L.Control {
 				   {type: 'separator'},
 				   {uno: '.uno:ModifyPage'},
 					 {name: _UNO('.uno:SlideChangeWindow', 'presentation', true), id: 'transitiondeck', type: 'action'},
-				   {uno: '.uno:CustomAnimation'},
+					 {uno: '.uno:CustomAnimation'}, // core version
+				   //{name: _UNO('.uno:CustomAnimation', 'presentation', true), id: 'animationdeck', type: 'action'}, // online version
 				])},
 			{name: _UNO('.uno:InsertMenu', 'presentation'), id: 'insert', type: 'menu', menu: [
 				{name: _('Local Image...'), id: 'insertgraphic', type: 'action'},
@@ -2187,6 +2188,7 @@ class Menubar extends window.L.Control {
 			|| id === 'print-active-sheet'
 			|| id === 'print-all-sheets'
 			|| id === 'serveraudit'
+			|| id === 'animationdeck'
 			|| id === 'transitiondeck') {
 			app.dispatcher.dispatch(id);
 		} else if (id === ('settings-dialog')) {
