a9b8fa7c1366312435b1fe635684a31b0b2aa937
Gökay Şatır <gokaysatir@gmail.com>
2025-09-29 17:40:56 +0300

    Add cursor handler section and remove related lat/lng usage.
    Signed-off-by: Gökay Şatır <gokaysatir@gmail.com>
    Change-Id: I1faf8dc8c30d0b5e9183cee67a54abc3615aea46

---
+++ browser/src/canvas/sections/CursorHandler.ts
@@ -0,0 +1,42 @@
+/* -*- js-indent-level: 8 -*- */
+
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+// This is used for other views' cursors.
+
+class CursorHandler extends HTMLObjectSection {
+	public static objectWidth = 30; // leaflet-cursor-handler CSS width and height.
+	public static objectHeight = 44;
+
+	constructor() {
+		super(
+			app.CSections.CursorHandler.name,
+			CursorHandler.objectWidth,
+			CursorHandler.objectHeight,
+			new cool.SimplePoint(0, 0),
+			'leaflet-cursor-handler',
+			false,
+		);
+	}
+
+	onMouseUp(point: cool.SimplePoint, e: MouseEvent): void {
+		app.map._docLayer._postMouseEvent('buttondown', point.x, point.y, 1, 1, 0);
+		app.map._docLayer._postMouseEvent('buttonup', point.x, point.y, 1, 1, 0);
+	}
+
+	setOpacity() {
+		this.getHTMLObject().style.opacity = 0;
+	}
+
+	public onRemove(): void {
+		this.setShowSection(false);
+	}
+}
