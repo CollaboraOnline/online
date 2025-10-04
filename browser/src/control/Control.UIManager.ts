CollaboraOnline/online

commit a69919703d3bd8f10e1d519cd5ff90cec9a81ea8

notebookbar: initial state simplification
- when we boot we always create component and inner implementation
- only when requested to show UI we run create method to build DOM
- that way we store model in any mode was running first
- we need it for other components too: sidebars, ocntextual toolbar

Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
Change-Id: I2c7791497b6324d090356da7c49cadc0e3abc218

browser/src/control/Control.UIManager.ts
@@ -474,13 +474,9 @@ class UIManager extends window.L.Control {
 			this.map.mobileBottomBar = JSDialog.MobileBottomBar(this.map);
 			this.map.mobileTopBar = JSDialog.MobileTopBar(this.map);
 			this.map.mobileSearchBar = JSDialog.MobileSearchBar(this.map);
-		} else if (enableNotebookbar) {
-			// component and UI
-			this.createNotebookbarControl(docType);
-			// makeSpaceForNotebookbar call in onUpdatePermission
 		} else {
-			// just component, no UI yet
-			this.notebookbar = JSDialog.NotebookbarBase(this.map);
+			this.createNotebookbarControl(docType, enableNotebookbar);
+			// makeSpaceForNotebookbar call in onUpdatePermission
 		}
 
 		if (!window.prefs.getBoolean(`${docType}.ShowToolbar`, true)) {
@@ -573,8 +569,7 @@ class UIManager extends window.L.Control {
 	 */
 	initializeLateComponents(): void {
 		app.console.debug('UIManager: late components init');
-		if (this.getCurrentMode() === 'notebookbar')
-			this.initializeNotebookbarInCore();
+		this.initializeNotebookbarInCore();
 		this.initializeSidebar();
 		this.initializeQuickFindInCore();
 	}
@@ -683,7 +678,7 @@ class UIManager extends window.L.Control {
 	 * Creates and adds a notebookbar control based on document type.
 	 * @param docType - Document type (e.g. 'spreadsheet', 'presentation', etc.)
 	 */
-	createNotebookbarControl(docType: string): void {
+	createNotebookbarControl(docType: string, showUI: boolean): void {
 		if (docType === 'spreadsheet') {
 			var notebookbar = window.L.control.notebookbarCalc();
 		} else if (docType === 'presentation') {
@@ -694,9 +689,13 @@ class UIManager extends window.L.Control {
 			notebookbar = window.L.control.notebookbarWriter();
 		}
 
-		if (!this.notebookbar)
-			this.notebookbar = JSDialog.NotebookbarBase(this.map);
-		this.notebookbar.onAdd(notebookbar);
+		this.notebookbar = JSDialog.NotebookbarBase(this.map, notebookbar);
+		if (showUI)
+			this.showNotebookbarControl();
+	}
+
+	showNotebookbarControl(): void {
+		this.notebookbar.onAdd();
 		this.map.fire('a11ystatechanged');
 		app.UI.notebookbarAccessibility.initialize();
 	}
@@ -719,7 +718,7 @@ class UIManager extends window.L.Control {
 	refreshNotebookbar(): void {
 			var selectedTab = $('.ui-tab.notebookbar[aria-selected="true"]').attr('id') || 'Home-tab-label';
 			this.removeNotebookbarUI();
-			this.createNotebookbarControl(this.map.getDocType());
+			this.showNotebookbarControl();
 			if (this._map._permission === 'edit') {
 				$('.main-nav').removeClass('readonly');
 			}
@@ -1206,7 +1205,10 @@ class UIManager extends window.L.Control {
 		// do it always apart of mobile as we need it for contextual toolbar
 		if (window.mode.isMobile()) return;
 
-		this.map.sendUnoCommand('.uno:ToolbarMode?Mode:string=notebookbar_online.ui');
+		if (!this.notebookbar.impl.initialized) {
+			this.map.sendUnoCommand('.uno:ToolbarMode?Mode:string=Default');
+			this.map.sendUnoCommand('.uno:ToolbarMode?Mode:string=notebookbar_online.ui');
+		}
 	}
 
 	// QuickFindPanel

browser/src/control/Control.Notebookbar.js
@@ -34,13 +34,23 @@ window.L.Control.Notebookbar = window.L.Control.extend({
 	setBuilder: function(builder, model) {
 		this.builder = builder;
 		this.model = model;
-		this.loadTab(this.getFullJSON(this.HOME_TAB_ID));
 	},
 
+	// happens only once
 	onAdd: function (map) {
 		// log and test window.ThisIsTheiOSApp = true;
 		this.map = map;
 		this.additionalShortcutButtons = [];
+
+		// initialize the model only once, remember updates from core
+		if (this.model.getSnapshot() === null)
+			this.model.fullUpdate(this.getFullJSON(this.HOME_TAB_ID));
+
+		this.map.on('notebookbar', this.onNotebookbar, this);
+	},
+
+	// on show
+	create: function() {
 		var docType = this._map.getDocType();
 
 		if (document.documentElement.dir === 'rtl')
@@ -55,9 +65,10 @@ window.L.Control.Notebookbar = window.L.Control.extend({
 		$('#toolbar-logo').after(this.map.toolbarUpTemplate.cloneNode(true));
 		this.parentContainer = window.L.DomUtil.get('toolbar-up');
 
+		this.loadTab();
+
 		this.onContextChange = this.onContextChange.bind(this);
 		app.events.on('contextchange', this.onContextChange);
-		this.map.on('notebookbar', this.onNotebookbar, this);
 		app.events.on('updatepermission', this.onUpdatePermission.bind(this));
 		this.map.on('statusbarchanged', this.onStatusbarChange, this);
 		this.map.on('rulerchanged', this.onRulerChange, this);
@@ -111,9 +122,6 @@ window.L.Control.Notebookbar = window.L.Control.extend({
 	},
 
 	onRemove: function() {
-		this.map.off('notebookbar');
-		this.map.off('jsdialogupdate', this.onJSUpdate, this);
-		this.map.off('jsdialogaction', this.onJSAction, this);
 		app.events.off('contextchange', this.onContextChange);
 		$('.main-nav #document-header').remove();
 		$('.main-nav').removeClass('hasnotebookbar');
@@ -192,16 +200,12 @@ window.L.Control.Notebookbar = window.L.Control.extend({
 		$(this.container).remove();
 	},
 
-	loadTab: function(tabJSON) {
+	loadTab: function() {
 		app.console.debug('Notebookbar: loadTab');
 
 		this.clearNotebookbar();
 
 		this.container = window.L.DomUtil.create('div', 'notebookbar-scroll-wrapper', this.parentContainer);
-
-		// initialize the model only once, remember updates from core
-		if (this.model.getSnapshot() === null)
-			this.model.fullUpdate(tabJSON);
 
 		this.builder.build(this.container, [this.model.getSnapshot()]);
 

browser/src/control/Control.NotebookbarBase.ts
@@ -17,22 +17,33 @@ class NotebookbarBase extends JSDialogComponent {
 	/// reference to old JS Notebookbar
 	impl: any = null;
 
-	constructor(map: any) {
+	constructor(map: any, impl: any) {
 		super(map, 'Notebookbar', 'notebookbar');
 		this.map = map;
-	}
-
-	public onAdd(impl: any) {
 		this.impl = impl;
 
-		this.map.addControl(this.impl);
 		this.createBuilder();
 		this.impl.setBuilder(this.builder, this.model);
-		this.setupContainer(this.impl.container);
+
+		this.map.addControl(this.impl);
 
 		this.registerMessageHandlers();
 	}
 
+	// when we show the UI
+	public onAdd() {
+		this.impl.create();
+		this.setupContainer(this.impl.container);
+		if (this.builder) {
+			this.map.on(
+				'commandstatechanged',
+				this.builder.onCommandStateChanged,
+				this.builder,
+			);
+		}
+	}
+
+	// when we hide the UI
 	public onRemove() {
 		if (this.builder)
 			this.map.off(
@@ -40,12 +51,8 @@ class NotebookbarBase extends JSDialogComponent {
 				this.builder.onCommandStateChanged,
 				this.builder,
 			);
-		this.unregisterMessageHandlers();
-		if (this.impl) {
-			this.map.removeControl(this.impl);
-			delete this.impl;
-			this.impl = null;
-		}
+
+		if (this.impl) this.impl.onRemove();
 	}
 
 	protected createBuilder() {
@@ -57,12 +64,6 @@ class NotebookbarBase extends JSDialogComponent {
 			useSetTabs: true,
 			suffix: 'notebookbar',
 		});
-		if (this.builder)
-			this.map.on(
-				'commandstatechanged',
-				this.builder.onCommandStateChanged,
-				this.builder,
-			);
 	}
 
 	protected setupContainer(parentContainer?: HTMLElement) {
@@ -142,6 +143,6 @@ class NotebookbarBase extends JSDialogComponent {
 	}
 }
 
-JSDialog.NotebookbarBase = function (map: any) {
-	return new NotebookbarBase(map);
+JSDialog.NotebookbarBase = function (map: any, impl: any) {
+	return new NotebookbarBase(map, impl);
 };

browser/src/control/jsdialog/Component.Base.ts
@@ -54,6 +54,8 @@ abstract class JSDialogComponent {
 
 		if (data.jsontype !== this.allowedJsonType) return false;
 
+		if (this.model) this.model.widgetUpdate(data);
+
 		if (!this.container) return false;
 
 		if (!this.builder) return false;
@@ -64,7 +66,6 @@ abstract class JSDialogComponent {
 				: data.control.id,
 		);
 
-		this.model.widgetUpdate(data);
 		this.builder.updateWidget(this.container, data.control);
 
 		return true;
@@ -76,6 +77,8 @@ abstract class JSDialogComponent {
 
 		if (data.jsontype !== this.allowedJsonType) return false;
 
+		if (this.model) this.model.widgetAction(data);
+
 		if (!this.builder) return false;
 
 		if (!this.container) return false;
@@ -86,7 +89,6 @@ abstract class JSDialogComponent {
 				: data.data.control_id,
 		);
 
-		this.model.widgetAction(data);
 		this.builder.executeAction(this.container, data.data);
 
 		return true;

browser/src/docstate.ts
@@ -304,5 +304,5 @@ if (activateValidation) {
 	window.app.file = new Proxy(window.app.file, validator);
 }
 
-(window as any).JSDialog = { verbose: false }; // initialize jsdialog module
+(window as any).JSDialog = { verbose: true }; // initialize jsdialog module
 (window as any).SlideShow = {}; // initialize slideshow module

commit 3e8bbe09db4cc6cca02d012b26d8bb50313ad2dc

notebookbar: fix custom buttons insertion via postmessage
This fixes the regression from commit a69919703d3bd8f10e1d519cd5ff90cec9a81ea8
notebookbar: initial state simplification

which makes notebookbar component always present, even in classic mode.
It should get the notification even if classic mode is activated.

Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
Change-Id: I0de10da12203d586a5487751cf105a7a033ec2bb

browser/src/control/Control.UIManager.ts
@@ -932,10 +932,13 @@ class UIManager extends window.L.Control {
 
 		if (this.getCurrentMode() === 'classic')
 			this.insertButtonToClassicToolbar(button);
-		else if (this.notebookbar)
+
+		// we always  create notebookbar component, it should get the button in advance
+		// in case of mode switch done by user later
+		if (this.notebookbar)
 			this.notebookbar.insertButtonToShortcuts(button);
 		else
-			app.console.debug('UIManager: no notebookbar yet to insert button: ' + JSON.stringify(button));
+			app.console.error('UIManager: no notebookbar yet to insert button: ' + JSON.stringify(button));
 	}
 
 	/**