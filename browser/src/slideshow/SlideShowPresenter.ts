CollaboraOnline/online

commit de449ef0a242b4313eeea4b5f48ccb87babc0360

followme slideshow: allow users to rejoin the shared presentation
now users can exit the presentation/document and rejoin the presentation
using the new button introduced in notebookbar

this also works when user joins the document after the presentation
has already started

Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
Change-Id: I2c83f4747eb860b5ce79ef0c84be9e713b91f0e3

browser/src/slideshow/SlideShowPresenter.ts
@@ -131,6 +131,7 @@ class SlideShowPresenter {
 	private _slideShowNavigator: SlideShowNavigator;
 	private _metaPresentation: MetaPresentation;
 	private _startSlide: number;
+	private _startEffect: number;
 	private _presentationInfoChanged: boolean = false;
 	private _navigateSkipTransition: boolean = false;
 	_skipNextSlideShowInfoChangedMsg: boolean = false;
@@ -219,8 +220,16 @@ class SlideShowPresenter {
 		this.setFollower(true);
 		switch (info.type) {
 			case 'newfollowmepresentation':
-				this.setFollowing(true);
-				this._onStartInWindow(0);
+				this._onStartInWindow({
+					startSlideNumber:
+						this._slideShowNavigator.getLeaderSlide() === -1
+							? 0
+							: this._slideShowNavigator.getLeaderSlide(),
+					startEffectNumber:
+						this._slideShowNavigator.getLeaderEffect() === -1
+							? undefined
+							: this._slideShowNavigator.getLeaderEffect(),
+				});
 				break;
 			case 'dispatcheffect':
 				if (this.isFollowing()) this._slideShowNavigator.dispatchEffect();
@@ -1096,6 +1105,7 @@ class SlideShowPresenter {
 	_onStartInWindow(that: any) {
 		this.sendSlideShowFollowMessage('newfollowmepresentation');
 		this._startSlide = that?.startSlideNumber ?? 0;
+		this._startEffect = that?.startEffectNumber;
 		if (!this._onPrepareScreen(true))
 			// opens full screen, has to be on user interaction
 			return;
@@ -1189,6 +1199,7 @@ class SlideShowPresenter {
 		this._slideShowNavigator.startPresentation(
 			this._startSlide,
 			skipTransition,
+			this._startEffect,
 		);
 	}
 

browser/images/dark/lc_followpresentation.svg
@@ -1,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<svg viewBox="0 0 24 24" version="1.1" id="svg5248" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">
+  <defs id="defs5252" />
+  <path d="M 3,6 V 20 H 21 V 6 Z" fill="#1a1a1a" id="path5240" style="fill:#1a1a1a;fill-opacity:1;stroke-width:0.999995" />
+  <path d="m3 3c-.554 0-1 .446-1 1v16c0 .554.446 1 1 1h18c.554 0 1-.446 1-1v-16c0-.554-.446-1-1-1zm0 1h18v16h-18z" fill="#fafafa" id="path5242" style="fill:#fafafa;fill-opacity:1" />
+  <path d="m22 4c0-.554-.446-1-1-1h-18c-.554 0-1 .446-1 1v2c0 .554.446 1 1 1h18c.554 0 1-.446 1-1zm-1 0v2h-18v-2z" fill="#83beec" id="path5244" />
+  <path d="M 3,3.9999997 V 6.0000003 H 21 V 3.9999997 Z" fill="#0063b1" id="path7081" style="fill:#0063b1;fill-opacity:1;stroke-width:0.999994" />
+  <g id="g5108" fill="#1a1a1a" stroke="#fafafa" stroke-linecap="round" stroke-linejoin="round" style="fill:#1a1a1a;fill-opacity:1;stroke-width:1.28238" transform="matrix(0.77778287,0,0,0.78182361,1.8888456,4.1181167)">
+    <path d="m 8.5,17.5 v -11 l 9,5.5 z" id="path5106" style="fill:#1a1a1a;fill-opacity:1;stroke-width:1.28238" />
+  </g>
+</svg>

browser/images/lc_followpresentation.svg
@@ -1,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<svg
+   viewBox="0 0 24 24"
+   version="1.1"
+   id="svg5248"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:svg="http://www.w3.org/2000/svg">
+  <defs
+     id="defs5252" />
+  <path
+     d="M 3,6 V 20 H 21 V 6 Z"
+     fill="#fafafa"
+     id="path5240"
+     style="fill:#63bbee;fill-opacity:1;stroke-width:0.999995" />
+  <path
+     d="m3 3c-.554 0-1 .446-1 1v16c0 .554.446 1 1 1h18c.554 0 1-.446 1-1v-16c0-.554-.446-1-1-1zm0 1h18v16h-18z"
+     fill="#3a3a38"
+     id="path5242"
+     style="fill:#0063b1;fill-opacity:1" />
+  <path
+     d="m22 4c0-.554-.446-1-1-1h-18c-.554 0-1 .446-1 1v2c0 .554.446 1 1 1h18c.554 0 1-.446 1-1zm-1 0v2h-18v-2z"
+     fill="#0063b1"
+     id="path5244" />
+  <g
+     id="g5108"
+     fill="#fafafa"
+     stroke="#0369a3"
+     stroke-linecap="round"
+     stroke-linejoin="round"
+     style="fill:#ffffff;fill-opacity:1;stroke-width:1.28238"
+     transform="matrix(0.77778287,0,0,0.78182361,1.8888456,4.1181167)">
+    <path
+       d="m 8.5,17.5 v -11 l 9,5.5 z"
+       id="path5106"
+       style="fill:#fafafa;fill-opacity:1;stroke-width:1.28238" />
+  </g>
+  <path
+     d="M 3,3.9999997 V 6.0000003 H 21 V 3.9999997 Z"
+     fill="#fafafa"
+     id="path7081"
+     style="fill:#0063b1;fill-opacity:1;stroke-width:0.999994" />
+</svg>

browser/src/control/Control.NotebookbarImpress.js
@@ -498,8 +498,16 @@ window.L.Control.NotebookbarImpress = window.L.Control.NotebookbarWriter.extend(
 				{
 					'id': 'slide-presentation-follow-me',
 					'type': 'bigcustomtoolitem',
-					'text': _('Follow me Presentation'),
+					'text': _('Start Follow-me Presentation'),
 					'command': 'followmepresentation',
+					'accessibility': { focusBack: true, combination: 'PW', de: null }
+				} : {},
+			!window.ThisIsAMobileApp && app.isExperimentalMode() ?
+				{
+					'id': 'slide-presentation-follow',
+					'type': 'bigcustomtoolitem',
+					'text': _('Follow Presentation'),
+					'command': 'followpresentation',
 					'accessibility': { focusBack: true, combination: 'PW', de: null }
 				} : {},
 			{ type: 'separator', id: 'slide-zoomin-break', orientation: 'vertical' },

browser/src/docdispatcher.ts
@@ -524,6 +524,15 @@ class Dispatcher {
 			app.map.fire('newpresentinwindow');
 		};
 
+		this.actionsMap['followpresentation'] = this.actionsMap[
+			'presentation-follow'
+		] = () => {
+			app.map.slideShowPresenter.setLeader(false);
+			app.map.slideShowPresenter.setFollower(true);
+			app.map.slideShowPresenter.setFollowing(true);
+			app.map.fire('newfollowmepresentation');
+		};
+
 		this.actionsMap['presenterconsole'] = () => {
 			if ((window as any).canvasSlideshowEnabled)
 				app.map.fire('newpresentinconsole');

browser/src/slideshow/engine/SlideShowHandler.ts
@@ -609,6 +609,10 @@ class SlideShowHandler {
 		return true;
 	}
 
+	skipNEffects(nEffectNumber: number) {
+		for (let i = 0; i <= nEffectNumber; i++) if (!this.skipNextEffect()) break;
+	}
+
 	/** skipPlayingOrNextEffect
 	 *  Skip the next effect to be played that belongs to the main animation
 	 *  sequence  or all playing effects.
@@ -855,6 +859,7 @@ class SlideShowHandler {
 		nNewSlide: number,
 		nOldSlide: number | undefined,
 		bSkipSlideTransition: boolean,
+		nSlideEffect: number = undefined,
 	) {
 		NAVDBG.print(
 			'SlideShowHandler.displaySlide: nNewSlide: ' +
@@ -930,6 +935,8 @@ class SlideShowHandler {
 		}
 
 		this.notifyTransitionEnd(nNewSlide, nOldSlide);
+		// can jump to specific animation only after everything is loaded
+		if (nSlideEffect !== undefined) this.skipNEffects(nSlideEffect);
 	}
 
 	exitSlideShow() {

browser/src/slideshow/engine/SlideShowNavigator.ts
@@ -230,8 +230,16 @@ class SlideShowNavigator {
 		this.currentLeaderSlide = info.currentSlide;
 	}
 
+	getLeaderSlide(): number {
+		return this.currentLeaderSlide;
+	}
+
 	setLeaderEffect(info: any) {
 		this.currentLeaderEffect = info.currentEffect;
+	}
+
+	getLeaderEffect(): number {
+		return this.currentLeaderEffect;
 	}
 
 	followLeaderSlide() {
@@ -240,11 +248,14 @@ class SlideShowNavigator {
 		if (this.currentLeaderSlide === this.currentSlide)
 			this.slideShowHandler.rewindAllEffects();
 		else this.displaySlide(this.currentLeaderSlide, true);
-		for (let i = 0; i <= this.currentLeaderEffect; i++)
-			if (!this.slideShowHandler.skipNextEffect()) break;
+		this.slideShowHandler.skipNEffects(this.currentLeaderEffect);
 	}
 
-	displaySlide(nNewSlide: number, bSkipTransition: boolean) {
+	displaySlide(
+		nNewSlide: number,
+		bSkipTransition: boolean,
+		nStartEffect: number = undefined,
+	) {
 		this.presenter.sendSlideShowFollowMessage(
 			'displayslide ' + JSON.stringify({ currentSlide: nNewSlide }),
 		);
@@ -324,6 +335,7 @@ class SlideShowNavigator {
 				this.currentSlide,
 				this.prevSlide,
 				bSkipTransition,
+				nStartEffect,
 			);
 			if (this.isRewindingToPrevSlide) {
 				this.slideShowHandler.skipAllEffects();
@@ -332,7 +344,11 @@ class SlideShowNavigator {
 		});
 	}
 
-	startPresentation(nStartSlide: number, bSkipTransition: boolean) {
+	startPresentation(
+		nStartSlide: number,
+		bSkipTransition: boolean,
+		nStartEffect: number = undefined,
+	) {
 		NAVDBG.print(
 			'SlideShowNavigator.startPresentation: current index: ' +
 				this.currentSlide +
@@ -343,7 +359,7 @@ class SlideShowNavigator {
 		this.isRewindingToPrevSlide = false;
 		this.currentSlide = undefined;
 		this.prevSlide = undefined;
-		this.displaySlide(nStartSlide, bSkipTransition);
+		this.displaySlide(nStartSlide, bSkipTransition, nStartEffect);
 	}
 
 	endPresentation(force: boolean = false) {

wsd/ClientSession.cpp
@@ -1413,6 +1413,23 @@ bool ClientSession::_handleInput(const char *buffer, int length)
 
         if (tokens.equals(0, "slideshowfollow"))
         {
+            if(tokens.equals(1, "newfollowmepresentation"))
+                docBroker->setIsFollowmeSlideShowOn(true);
+            else if(tokens.equals(1, "endpresentation"))
+                docBroker->setIsFollowmeSlideShowOn(false);
+            else if(tokens.equals(1, "effect")){
+                Poco::JSON::Parser parser;
+                auto result = parser.parse(tokens[2]);
+                int effectNumber = JsonUtil::getJSONValue<int>(result.extract<Poco::JSON::Object::Ptr>(), "currentEffect");
+                docBroker->setLeaderEffect(effectNumber);
+            }
+            else if(tokens.equals(1, "displayslide")) {
+                Poco::JSON::Parser parser;
+                auto result = parser.parse(tokens[2]);
+                int slideNumber = JsonUtil::getJSONValue<int>(result.extract<Poco::JSON::Object::Ptr>(), "currentSlide");
+                docBroker->setLeaderSlide(slideNumber);
+                docBroker->setLeaderEffect(-1);
+            }
             docBroker->broadcastMessageToOthers(tokens.substrFromToken(0), client_from_this());
             return true;
         }
@@ -1769,6 +1786,12 @@ bool ClientSession::loadDocument(const char* /*buffer*/, int /*length*/,
 #if ENABLE_FEATURE_RESTRICTION
         sendRestrictionInfo();
 #endif
+        if (docBroker->getIsFollowmeSlideShowOn())
+        {
+            sendTextFrame("slideshowfollow slideshowfollowon");
+            sendTextFrame("slideshowfollow displayslide {\"currentSlide\": " + std::to_string(docBroker->getLeaderSlide()) +"}");
+            sendTextFrame("slideshowfollow effect {\"currentEffect\": " + std::to_string(docBroker->getLeaderEffect()) +"}");
+        }
 
         return forwardToChild(oss.str(), docBroker);;
     }

commit 4ba61ac2d69df8092c3b261dee08571cbce00c75

followme slideshow: fix incorrect following status
regression from: de449ef0a242b4313eeea4b5f48ccb87babc0360
deleted a line by mistake

Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
Change-Id: Ifadd4ebe99d22a34944c1bb3b3d018215a458455

browser/src/slideshow/SlideShowPresenter.ts
@@ -221,6 +221,7 @@ class SlideShowPresenter {
 		this.setFollower(true);
 		switch (info.type) {
 			case 'newfollowmepresentation':
+				this.setFollowing(true);
 				this._map.uiManager.showButton('slide-presentation-follow-me', false);
 				this._map.uiManager.showButton('slide-presentation-follow', true);
 				this._onStartInWindow({