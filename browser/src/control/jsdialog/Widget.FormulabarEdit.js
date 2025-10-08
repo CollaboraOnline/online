CollaboraOnline/online

commit 039283733eb0bf0d2da800db381b9a95a755e5e3

formulabar: trigger mouseleave event only after mousedown event
problem:
mouseleave was triggered by hovering and leaving formulabar,
this caused formulabar gabbing focus unwanted

Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
Change-Id: I0c0e75b2025e69d425351d9ff75ceccf385d6326

browser/src/control/jsdialog/Widget.FormulabarEdit.js
@@ -230,26 +230,29 @@ function _formulabarEditControl(parentContainer, data, builder) {
 		textLayer.setAttribute('contenteditable', 'false');
 	};
 
-	['click', 'dblclick', 'mouseleave'].forEach(function (ev) {
-		textLayer.addEventListener(ev, function(event) {
-			if (L.DomUtil.hasClass(container, 'disabled')) {
-				event.preventDefault();
-				return;
-			}
-
-			builder.callback('edit', 'grab_focus', container, null, builder);
-			_sendSelection(textLayer, builder, container.id, event);
-
-			builder.map.setWinId(0);
-			builder.map._textInput._emptyArea();
-			builder.map._textInput.focus(true);
-
+	var textSelectionHandler = function(event) {
+		if (L.DomUtil.hasClass(container, 'disabled')) {
 			event.preventDefault();
-		});
+			return;
+		}
+
+		builder.callback('edit', 'grab_focus', container, null, builder);
+		_sendSelection(textLayer, builder, container.id, event);
+
+		builder.map.setWinId(0);
+		builder.map._textInput._emptyArea();
+		builder.map._textInput.focus(true);
+
+		event.preventDefault();
+	};
+
+	['click', 'dblclick'].forEach(function (ev) {
+		textLayer.addEventListener(ev, textSelectionHandler);
 	});
 
 	// hide old selection when user starts to select something else
 	textLayer.addEventListener('mousedown', function() {
+		textLayer.addEventListener('mouseleave', textSelectionHandler, {once: true});
 		builder.callback('edit', 'grab_focus', container, null, builder);
 
 		cursorLayer.querySelectorAll('.selection').forEach(function (element) {

commit b3259e400c53a53e8e2ba2d74bdc8210a1181eff

formulabar: allow to keep selection when leaving input area
- this was annoying when user started to press the mouse button
  then moved mouse cursor out of the input field
- as a result we finished selection and it wasn't possible to just
  move mouse back to the field and continue

regression from commit 039283733eb0bf0d2da800db381b9a95a755e5e3
formulabar: trigger mouseleave event only after mousedown event

Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
Change-Id: I0e6da852b075e829ac208d163373496b461956ea

browser/src/control/jsdialog/Widget.FormulabarEdit.js
@@ -448,7 +448,7 @@ function _formulabarEditControl(parentContainer, data, builder) {
 
 	// hide old selection when user starts to select something else
 	textLayer.addEventListener('mousedown', function() {
-		textLayer.addEventListener('mouseleave', textSelectionHandler, {once: true});
+		textLayer.addEventListener('mouseup', textSelectionHandler, {once: true});
 		builder.callback('edit', 'grab_focus', container, null, builder);
 
 		cursorLayer.querySelectorAll('.selection').forEach(function (element) {