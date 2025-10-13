46232c7e6bf779cbbd705dcc7dbcc3370a5b93fd
Michael Meeks <michael.meeks@collabora.com>
2025-10-13 15:02:19 +0100

    Pre-select content of find box for easier ctrl-f searching.
    Signed-off-by: Michael Meeks <michael.meeks@collabora.com>
    Change-Id: Idf74834a05914acffd16f729cf7a4aff879d23c7

--- browser/src/control/Control.NavigatorPanel.ts
+++ browser/src/control/Control.NavigatorPanel.ts
@@ -454,6 +454,7 @@ class NavigatorPanel extends SidebarBase {
 		if (!searchInput) return;
 
 		app.layoutingService.appendLayoutingTask(() => {
+			searchInput.select();
 			searchInput.focus();
 		});
 	}
