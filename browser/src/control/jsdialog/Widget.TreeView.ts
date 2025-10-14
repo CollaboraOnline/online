4da5ca1f94c52366d10a8b7beb6621f7180cc080
Parth Raiyani <parth.raiyani@collabora.com>
2025-10-14 23:18:39 +0530

    fix: move preventDef variable declaration inside keydown event listener to work correctly after selecting the entry.
    issue:
    as preventDef was declare outside and when we try to select any entry and try to navigate using arrow keys or tab key nothing works and we stuck here
    
    solution:
    preventDef is incorrectly declared due to which it holds the prior value. This changes will fix this.
    
    Signed-off-by: Parth Raiyani <parth.raiyani@collabora.com>
    Change-Id: I1d7a9b0537441519ca9ca4d598a2da6eb8098e51

--- browser/src/control/jsdialog/Widget.TreeView.ts
+++ browser/src/control/jsdialog/Widget.TreeView.ts
@@ -883,9 +883,9 @@ class TreeViewControl {
 	) {
 		if (entry.enabled === false) return;
 
-		let preventDef = false;
-
 		tr.addEventListener('keydown', (event) => {
+			let preventDef = false;
+
 			if (event.key === ' ' && expander) {
 				expander.click();
 				tr.focus();
