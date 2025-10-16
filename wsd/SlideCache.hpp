3881c9d8c8ee5c15f252ef40ffb2ba7a0106b4e0
Pranam Lashkari <lpranam@collabora.com>
2025-10-16 18:55:56 +0530

    slideshow: move slidelayer caching logic to wsd
    moving this logic to wsd from kit makes it easier and faster
    to return the cached slides directly from the wsd without
    ever involving kit
    
    original logic was introduced in fa6751dcb2fbf2310a
    
    Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
    Change-Id: I8ef966c1c480bd21cb623ea9a4ee10986a32d541

---
+++ wsd/SlideCache.hpp
@@ -0,0 +1,46 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <unordered_map>
+#include <queue>
+#include <common/Message.hpp>
+
+/**
+ Used for caching rendered slide layers for slideshow
+ example entry may look like following
+ key: hash=108777063986320 part=0 width=1919 height=1080 renderBackground=1 renderMasterPage=1 devicePixelRatio=1 compressedLayers=0
+ value vector:[SlideLayer1 Binary message, SlideLayer2 Binary message..., sliderenderingcomplete]
+ key consists of all the parameters browser sends us for particular slide rendering
+ value vector will be in order layers should be rendered and displayed and last message will be sliderenderingcomplete
+*/
+class SlideLayerCacheMap
+{
+    std::unordered_map<std::string, std::vector<std::shared_ptr<Message>>> cache_map;
+    // Helps if we want to clean older cache
+    std::queue<std::string> insertion_order;
+
+public:
+    void insert(const std::string& key, const std::shared_ptr<Message> cachedData);
+
+    std::size_t reduceSizeTo(std::size_t desiredSize);
+
+    void erase_all();
+
+    std::unordered_map<std::string, std::vector<std::shared_ptr<Message>>>::const_iterator
+    find(const std::string& key) const;
+
+    std::unordered_map<std::string, std::vector<std::shared_ptr<Message>>>::const_iterator end();
+};
