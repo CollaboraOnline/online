78452a877d5925c08e45894be170d7c4aa582c50
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2025-10-19 11:27:27 -0400

    wsd: end current activity before starting SyncFileTimestamp
    Change-Id: Ie151859db2ef176fe0dc99c20fa35f28dbf5a304
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- wsd/DocumentBroker.cpp
+++ wsd/DocumentBroker.cpp
@@ -3185,6 +3185,7 @@ void DocumentBroker::handleUploadToStorageFailed(const StorageBase::UploadResult
         // Since we've failed to get a response, we cannot know if the
         // Storage has been updated. As such, we need to re-sync the
         // document's last modified timestamp.
+        endActivity(); // Probably in Activity::Upload.
         startActivity(DocumentState::Activity::SyncFileTimestamp);
 
         reportUploadToStorageFailed();
