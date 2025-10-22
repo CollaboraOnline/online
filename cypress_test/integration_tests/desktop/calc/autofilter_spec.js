977632e15748a41007ee75b60cfe6401cf4f9c9b
Gökay Şatır <gokaysatir@collabora.com>
2025-10-22 13:00:48 +0300

    Cypress changes.
    Signed-off-by: Gökay Şatır <gokaysatir@collabora.com>
    Change-Id: I07a5ef8c54045a8da59fa794aa1b1c63cf12d9a4

--- cypress_test/integration_tests/desktop/calc/autofilter_spec.js
+++ cypress_test/integration_tests/desktop/calc/autofilter_spec.js
@@ -125,6 +125,9 @@ describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'AutoFilter', function() {
 		cy.wait(500);
 
 		calcHelper.dblClickOnFirstCell();
+		// Position of the double click determines the cursor position. So press home button in order to go to start of the cell.
+		helper.typeIntoDocument('{home}');
+		// Type new content to verify that cell is in edit mode
 		helper.typeIntoDocument('New content{enter}');
 
 		calcHelper.assertSheetContents(['New contentCypress Test', 'Status', 'Test 1', 'Pass', 'Test 2', 'Fail', 'Test 3', 'Pass', 'Test 4', '', 'Test 5', 'Fail'], true);
