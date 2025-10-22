b6083c4bdaababa85a1b26d06347f70578a56033
Caolán McNamara <caolan.mcnamara@collabora.com>
2025-10-22 17:23:02 +0100

    this is a static member, don't need an instance to call it
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: I13c0b6b996eacc6d27aaeeda53d91e2e221410cf

--- wsd/ClientSession.cpp
+++ wsd/ClientSession.cpp
@@ -300,7 +300,7 @@ void ClientSession::handleClipboardRequest(DocumentBroker::ClipboardRequest     
     if (_state == SessionState::WAIT_DISCONNECT)
     {
         LOG_TRC("Clipboard request " << tag << " for disconnecting session");
-        if (docBroker->lookupSendClipboardTag(socket, tag, false))
+        if (DocumentBroker::lookupSendClipboardTag(socket, tag, false))
             return; // the getclipboard already completed.
         if (type == DocumentBroker::CLIP_REQUEST_SET)
         {
