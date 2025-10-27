e0f6f4eb49f480ae4a9ff99285a16c71b6a47dd5
Mohit Marathe <mohit.marathe@collabora.com>
2025-10-27 18:11:07 +0530

    Impress: add buttons for .uno:InsertCanvasSlide & .uno:ReshufflePages
    - Add buttons for .uno:InsertCanvasSlide and .uno:ReshufflePages
    - Create Widget.NewSlideLayoutEntry.ts for having both grid, and a
      separate button in the dropdown
    
    Signed-off-by: Mohit Marathe <mohit.marathe@collabora.com>
    Change-Id: I82120479cf599f9058a6151cda0827ea12c8d777

---
+++ browser/src/control/jsdialog/Widget.NewSlideLayoutEntry.ts
@@ -0,0 +1,57 @@
+/* -*- js-indent-level: 8 -*- */
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/**
+ * Widget.NewSlideLayoutEntry.ts
+ *
+ * A JSDialog "json" widget for rendering the slide layout presets
+ * and the "New Slide Layout" link.
+ */
+declare var JSDialog: any;
+
+function createSlideLayoutEntryWidget(data: any, builder: any): HTMLElement {
+	const container = document.createElement('div');
+	container.className = 'slidelayout-popup-container';
+	// Build the grid first
+	const gridData = data.gridContent; // The grid from generateLayoutPopupGrid
+	const gridContainer = document.createElement('div');
+	builder.build(gridContainer, [gridData], false);
+	container.appendChild(gridContainer);
+	if (app.isExperimentalMode()) {
+		const hr = document.createElement('hr');
+		hr.className = 'jsdialog ui-separator horizontal';
+		container.appendChild(hr);
+
+		const button = document.createElement('div');
+		button.className = 'slidelayout-button';
+		button.id = 'newcanvasslide';
+		button.textContent = _('Overview');
+		button.setAttribute('role', 'button');
+		button.setAttribute('tabindex', '0');
+		button.addEventListener('click', () => {
+			builder.map.sendUnoCommand('.uno:InsertCanvasSlide'); // or your command
+			builder.callback('dialog', 'close', { id: data.id }, null);
+		});
+
+		container.appendChild(button);
+	}
+	return container;
+}
+
+JSDialog.slideLayoutEntry = function (
+	parentContainer: Element,
+	data: any,
+	builder: any,
+) {
+	const widget = createSlideLayoutEntryWidget(data, builder);
+	parentContainer.appendChild(widget);
+	return false;
+};
