3350a38f76d97c6bddf3d61f55c2377eec104e1f
Parth Raiyani <parth.raiyani@collabora.com>
2025-10-29 17:57:06 +0530

    fix: add aria-activedescendant attribute to improve accessibility in listbox navigation
    Change-Id: Id83abc8201b895ba824c7795dec787cef50e1232
    Signed-off-by: Parth Raiyani <parth.raiyani@collabora.com>

--- browser/src/control/jsdialog/Definitions.Types.ts
+++ browser/src/control/jsdialog/Definitions.Types.ts
@@ -205,6 +205,7 @@ interface GridWidgetJSON extends ContainerWidgetJSON {
 	cols: number; // number of grid columns
 	rows: number; // numer of grid rows
 	tabIndex?: number;
+	initialSelectedId?: string; // id of the first selected element
 }
 
 interface ToolboxWidgetJSON extends WidgetJSON {
