3fd449ef028c597cd7a3f4e827a89795a3147e34
Rashesh Padia <rashesh.padia@collabora.com>
2025-11-03 13:03:09 +0530

    coda-q: create Application class with static globalProfile
    Signed-off-by: Rashesh Padia <rashesh.padia@collabora.com>
    Change-Id: I863d90719111aa7131b9a53115502a842f52d7bd

--- qt/WebView.cpp
+++ qt/WebView.cpp
@@ -79,26 +79,11 @@ std::string getUILanguage()
 }
 } // namespace
 
-WebView::WebView(QWidget* parent)
+WebView::WebView(QWidget* parent, QWebEngineProfile* profile)
     : _mainWindow(new QMainWindow(parent))
     , _webView(new QWebEngineView(_mainWindow))
 {
     _mainWindow->setCentralWidget(_webView);
-
-    QWebEngineProfile* profile = new QWebEngineProfile(QStringLiteral("PersistentProfile"), _mainWindow);
-
-    // use XDG-compliant paths
-    QString appData =
-        QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
-    QString cacheData =
-        QStandardPaths::writableLocation(QStandardPaths::CacheLocation);
-    QString storagePath = appData + "/PersistentProfile/storage";
-    QDir().mkpath(storagePath);
-    QDir().mkpath(cacheData);
-
-    profile->setPersistentStoragePath(storagePath);
-    profile->setCachePath(cacheData);
-    profile->setHttpCacheType(QWebEngineProfile::DiskHttpCache);
 
     QWebEnginePage* page = new QWebEnginePage(profile, _webView);
     _webView->setPage(page);
