3236eadabf13367bcb690ea40fb459e6e6b75120
Andras Timar <andras.timar@collabora.com>
2025-11-04 14:27:19 +0100

    remove obsolete code
    Signed-off-by: Andras Timar <andras.timar@collabora.com>
    Change-Id: I9cece710d51704bee9f2c20bf3da90c2f00a9bf4

--- wsd/COOLWSD.cpp
+++ wsd/COOLWSD.cpp
@@ -1696,9 +1696,6 @@ void COOLWSD::innerInitialize(Poco::Util::Application& self)
             << (ConfigUtil::isSSLTermination() ? "enabled." : "disabled."));
 
     std::string allowedLanguages(config().getString("allowed_languages"));
-    // Core <= 7.0.
-    setenv("LOK_WHITELIST_LANGUAGES", allowedLanguages.c_str(), 1);
-    // Core >= 7.1.
     setenv("LOK_ALLOWLIST_LANGUAGES", allowedLanguages.c_str(), 1);
 
 #endif // !MOBILEAPP
