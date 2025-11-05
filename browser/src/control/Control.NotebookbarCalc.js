5563bf436a157d26318079f95a6c4f341171245f
Tor Lillqvist <tml@collabora.com>
2025-11-05 09:10:42 +0200

    Rename the "Save a Copy As" menu in CODA-W to "Export as"
    That is shorter and more to the point. And already translated. Also
    update a comment referring to it.
    
    Signed-off-by: Tor Lillqvist <tml@collabora.com>
    Change-Id: I9a64ef8b92a09f9ed6dc1bcdd4f8e587d247c1f1

--- browser/src/control/Control.NotebookbarCalc.js
+++ browser/src/control/Control.NotebookbarCalc.js
@@ -329,7 +329,7 @@ window.L.Control.NotebookbarCalc = window.L.Control.NotebookbarWriter.extend({
 				'class': 'unodownloadas',
 				'type': 'exportmenubutton',
 				'text': !window.ThisIsAMobileApp ? _('Download') :
-					(window.ThisIsTheWindowsApp ? _('Save a Copy As') :
+					(window.ThisIsTheWindowsApp ? _('Export as') :
 					 _('Save As')),
 				'accessibility': { focusBack: true,	combination: 'DA', de: null }
 			});
