4dc41e5d69410b3c80ad04c89f90da07389ea8ec
Rashesh Padia <rashesh.padia@collabora.com>
2025-11-14 17:30:46 +0530

    CODA: remove 'insert', 'edit', 'editmenu', 'changesmenu'
    - also toggle backstage of 'file' menu click
    
    Signed-off-by: Rashesh Padia <rashesh.padia@collabora.com>
    Change-Id: I7afee9fa3910932c4d27bf758b32460948eb0433

--- browser/src/control/Control.Menubar.ts
+++ browser/src/control/Control.Menubar.ts
@@ -1420,8 +1420,8 @@ class Menubar extends window.L.Control {
 		commandStates: {},
 
 		// Only these menu options will be visible in readonly mode
-		allowedReadonlyMenus: ['file', 'downloadas', 'view', 'insert', 'slide', 'help', 'print'],
-		allowedRedlineManagementMenus: ['editmenu', 'changesmenu', ],
+		allowedReadonlyMenus: window.mode.isCODesktop() ? ['file', 'view', 'slide', 'help'] : ['file', 'downloadas', 'view', 'insert', 'slide', 'help', 'print'],
+		allowedRedlineManagementMenus: window.mode.isCODesktop() ? [] : ['editmenu', 'changesmenu', ],
 
 		math: ['.uno:ChangeFont', '.uno:ChangeFontSize', '.uno:ChangeDistance', '.uno:ChangeAlignment'],
 
@@ -1860,6 +1860,9 @@ class Menubar extends window.L.Control {
 		if (!$(menu).hasClass('has-submenu') && ($mainMenuState[0] as HTMLInputElement).checked) {
 			$mainMenuState[0].click();
 		}
+		
+		if (menu.parentElement?.id === 'menu-file' && window.mode.isCODesktop() && app.map.backstageView)
+			app.map.backstageView.toggle();
 	}
 
 	/**
