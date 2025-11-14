fa28cef8dce2dfc620a399e7e3f59c33ce5a6f31
Sahil Gautam <sahil.gautam@collabora.com>
2025-11-14 18:41:55 +0530

    add a cypress test to assert cursor style on #document-canvas element
    Signed-off-by: Sahil Gautam <sahil.gautam@collabora.com>
    Change-Id: I5242b0e96a759fc1ec8a5f6239fca309795e1efd

---
+++ cypress_test/integration_tests/desktop/calc/mouse_cursor_spec.js
@@ -0,0 +1,23 @@
+/* global describe it cy beforeEach expect require */
+
+var helper = require('../../common/helper');
+
+describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'Check mouse cursor type in various interactions', function() {
+
+	beforeEach(function() {
+		helper.setupAndLoadDocument('calc/mouse_cursor.ods');
+	});
+
+	/*
+		NOTE: we need more of these tests for the elements where
+		we change the cursor style:
+			- the shape resize handles
+			- the autofill marker
+			- column/row headers
+	*/
+	it('Cell cursor over the sheet', function() {
+		cy.cGet('#document-canvas').trigger('mouseover').then(() => {
+			cy.cGet('#document-canvas').should('have.class', 'spreadsheet-cursor');
+		});
+	});
+});
