CollaboraOnline/online

commit 3916a7f82fd6454409b86b9915d33f9f2491a348

cool#13419 convert-to template option: pass it to the kit process
curl -k -F "data=@/path/to/test.md" -F "template=@/path/to/template.docx" -F "format=pdf" https://localhost:9980/cool/convert-to > out.pdf
is expected to apply the styles from template.docx and the content from
test.md, but the styles are ignored.

The parent commit did most of the ground work for this to work, but so
far only the doc broker is aware of the template path that's a valid
file:// URL inside the jail.

Fix this by extending ClientSession::loadDocument(): if there are no
import filter options and we have a template URL, then work with that.

The combination of existing import filter options and setting a template
URL inside that would be possible to do, but let's wait for a practical
need first.

Signed-off-by: Miklos Vajna <vmiklos@collabora.com>
Change-Id: Ie4b5cdc871b91cab06e27558709c4e00911e5a75

wsd/ClientSession.cpp
@@ -1835,6 +1835,12 @@ bool ClientSession::loadDocument(const char* /*buffer*/, int /*length*/,
         {
             oss << " infilterOptions=" << getInFilterOption();
         }
+        else if (!docBroker->getTemplateOptionUriJailed().empty())
+        {
+            std::string options = "{\"TemplateURL\":{\"type\":\"string\",\"value\":\"" +
+                                  docBroker->getTemplateOptionUriJailed() + "\"}}";
+            oss << " infilterOptions=" << options;
+        }
 
 #if ENABLE_FEATURE_LOCK
         sendLockedInfo();

wsd/DocumentBroker.hpp
@@ -361,6 +361,7 @@ public:
     bool isAsyncUploading() const;
 
     Poco::URI getPublicUri() const { return _uriPublic; }
+    const std::string& getTemplateOptionUriJailed() const { return _templateOptionUriJailed; }
     const std::string& getJailId() const { return _jailId; }
     const std::string& getDocKey() const { return _docKey; }
     // id of wopi shared config

commit aa9945f47a7b29d930b55d6c29a14d97166f2abc

cool#13419 convert-to template option: add testcase
Fails with commit 3916a7f82fd6454409b86b9915d33f9f2491a348 (cool#13419
convert-to template option: pass it to the kit process, 2025-11-13)
reverted.

Signed-off-by: Miklos Vajna <vmiklos@collabora.com>
Change-Id: I91f98481c0723a88c33dd59ddb7f20f3926aeb0b

test/UnitConvert.cpp
@@ -23,6 +23,7 @@
 #include <Poco/Net/HTTPServerRequest.h>
 #include <Poco/Net/HTMLForm.h>
 #include <Poco/Net/StringPartSource.h>
+#include <Poco/Net/FilePartSource.h>
 #include <Poco/Util/LayeredConfiguration.h>
 
 using namespace std::literals;
@@ -57,27 +58,73 @@ public:
         config.setBool("storage.filesystem[@allow]", false);
     }
 
-    void sendConvertTo(std::unique_ptr<Poco::Net::HTTPClientSession>& session, const std::string& filename)
+    void sendConvertTo(std::unique_ptr<Poco::Net::HTTPClientSession>& session, const std::string& filename, bool isTemplate = false)
     {
-        Poco::Net::HTTPRequest request(Poco::Net::HTTPRequest::HTTP_POST, "/cool/convert-to/pdf");
+        std::string uri;
+        if (isTemplate)
+        {
+            uri = "/cool/convert-to";
+        }
+        else
+        {
+            uri = "/cool/convert-to/pdf";
+        }
+        Poco::Net::HTTPRequest request(Poco::Net::HTTPRequest::HTTP_POST, uri);
         Poco::Net::HTMLForm form;
         form.setEncoding(Poco::Net::HTMLForm::ENCODING_MULTIPART);
-        form.set("format", "txt");
-        form.addPart("data", new Poco::Net::StringPartSource("Hello World Content", "text/plain", filename));
+        if (isTemplate)
+        {
+            form.set("format", "html");
+            std::string dataPath = Poco::Path(TDOC, filename).toString();
+            form.addPart("data", new Poco::Net::FilePartSource(dataPath));
+            std::string templatePath = Poco::Path(TDOC, "template.docx").toString();
+            form.addPart("template", new Poco::Net::FilePartSource(templatePath));
+        }
+        else
+        {
+            form.set("format", "txt");
+            form.addPart("data", new Poco::Net::StringPartSource("Hello World Content", "text/plain", filename));
+        }
         form.prepareSubmit(request);
         form.write(session->sendRequest(request));
     }
 
-    bool checkConvertTo(std::unique_ptr<Poco::Net::HTTPClientSession>& session)
+    bool checkConvertTo(std::unique_ptr<Poco::Net::HTTPClientSession>& session, bool isTemplate = false)
     {
         Poco::Net::HTTPResponse response;
+        std::stringstream stringStream;
         try {
-            session->receiveResponse(response);
+            std::istream& responseStream = session->receiveResponse(response);
+            Poco::StreamCopier::copyStream(responseStream, stringStream);
         } catch (...) {
             return false;
         }
 
-        return response.getStatus() == Poco::Net::HTTPResponse::HTTPStatus::HTTP_OK;
+        bool ret = response.getStatus() == Poco::Net::HTTPResponse::HTTPStatus::HTTP_OK;
+        if (!ret || !isTemplate)
+        {
+            if (!ret)
+            {
+                TST_LOG("checkConvertTo: bad status");
+            }
+
+            return ret;
+        }
+
+        std::string responseString = stringStream.str();
+        if (responseString.find("DOCTYPE html") == std::string::npos)
+        {
+            TST_LOG("checkConvertTo: output is not HTML");
+            return false;
+        }
+
+        if (responseString.find("background: #156082") == std::string::npos)
+        {
+            TST_LOG("checkConvertTo: no template color in output");
+            return false;
+        }
+
+        return true;
     }
 
     void invokeWSDTest() override
@@ -100,6 +147,18 @@ public:
 
                 sendConvertTo(session, "test___á.txt");
                 if(!checkConvertTo(session))
+                {
+                    exitTest(TestResult::Failed);
+                    return;
+                }
+
+                // Given a markdown input + docx template:
+                // When converting that to HTML:
+                sendConvertTo(session, "test.md", /*isTemplate=*/true);
+                // Then make sure the output has a color from the template:
+                // Without the accompanying fix in place, this test would have failed with:
+                // checkConvertTo: no template color in output
+                if(!checkConvertTo(session, /*isTemplate=*/true))
                 {
                     exitTest(TestResult::Failed);
                     return;