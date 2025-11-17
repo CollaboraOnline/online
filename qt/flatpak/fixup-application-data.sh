21e773dd3820367872711536339ad91b551cb122
Thorsten Behrens <thorsten.behrens@collabora.com>
2025-11-17 04:51:46 +0100

    CODA-Q: update flatpak branding and description.
    Signed-off-by: Thorsten Behrens <thorsten.behrens@collabora.com>
    Change-Id: I188d490ad3fdb05dc8dcfb8282f8c958f477746c

--- qt/flatpak/fixup-application-data.sh
+++ qt/flatpak/fixup-application-data.sh
@@ -10,22 +10,27 @@ for i in /app/share/applications/com.collabora.Office.{calc,draw,impress,writer}
   sed -i -e "s|^MimeType=|&${mime};|" /app/share/applications/com.collabora.Office.desktop
 done
 
-# Strip out base and math applications from description, Comment and Actions:
-sed -i -e 's/, Base (databases), and Math (formula editing)//' \
+# Paste in full CODA description:
+sed -i -e 's/Collabora Office Desktop is a powerful office suite./Collabora Office Desktop brings the familiar Collabora Online experience to your computer - so you can create, edit, and present with a beautiful, modern UI. Built on trusted LibreOffice technology and backed by the largest team of LibreOffice developers worldwide, it delivers excellent interoperability with ODF and Microsoft Office formats (DOCX/XLSX/PPTX), and exports to PDF with ease. Your documents stay on your device unless you choose to share them - privacy by design./' \
  /app/share/appdata/com.collabora.Office.appdata.xml
+# Paste in CODA screenshots:
+sed -i -e 's/writer.png/https:\/\/col.la\/codelinuxwriter/' \
+ /app/share/appdata/com.collabora.Office.appdata.xml
+sed -i -e 's/calc.png/https:\/\/col.la\/codelinuxcalc/' \
+ /app/share/appdata/com.collabora.Office.appdata.xml
+sed -i -e 's/impress.png/https:\/\/col.la\/codelinuximpress/' \
+ /app/share/appdata/com.collabora.Office.appdata.xml
+sed -i -e 's/draw.png/https:\/\/col.la\/codelinuxdraw/' \
+ /app/share/appdata/com.collabora.Office.appdata.xml
+# Strip out base and math applications from description, Comment and Actions:
 sed -i -e 's/, formulas, and databases//' /app/share/applications/com.collabora.Office.desktop
 sed -i -e 's/;Base;Math//' /app/share/applications/com.collabora.Office.desktop
 sed -i -E -z -e 's/\[Desktop Action (Base|Math)\]\nName=[^\n]+\nExec=[^\n]+\n+//g' \
  /app/share/applications/com.collabora.Office.desktop
 
-# Strip out draw application from Actions for now:
-sed -i -e 's/;Draw//' /app/share/applications/com.collabora.Office.desktop
-sed -i -E -z -e 's/\[Desktop Action Draw\]\nName=[^\n]+\nExec=[^\n]+\n+//g' \
- /app/share/applications/com.collabora.Office.desktop
-
-# Adapt calc, impress, writer application Action names:
+# Adapt calc, impress, writer, draw application Action names:
 sed -i -e 's/^Name=Calc$/Name=Spreadsheet/' -e 's/^Name=Impress$/Name=Presentation/' \
- -e 's/^Name=Writer$/Name=Document/' /app/share/applications/com.collabora.Office.desktop
+ -e 's/^Name=Writer$/Name=Textdocument/' -e 's/^Name=Draw$/Name=Drawing/' /app/share/applications/com.collabora.Office.desktop
 
 # Remove individual application desktop and icon files:
 rm /app/share/applications/com.collabora.Office.*.desktop
