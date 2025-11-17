e7554821da3717a0263602760436ed41ab97e0f7
Andras Timar <andras.timar@collabora.com>
2025-11-17 11:52:18 +0100

    add license info to About box
    Signed-off-by: Andras Timar <andras.timar@collabora.com>
    Change-Id: I32d57aa4f6445095840dc5cd05fce4a15b959ced

--- browser/src/control/Control.AboutDialog.ts
+++ browser/src/control/Control.AboutDialog.ts
@@ -130,7 +130,7 @@ class AboutDialog {
 			);
 
 		const links = content.querySelectorAll<HTMLAnchorElement>(
-			'#coolwsd-version a, #lokit-version a, #lokit-extra a'
+			'#coolwsd-version a, #lokit-version a, #lokit-extra a, #license-information a'
 		);
 
 		for (let i = 0; i < links.length; i++) {
@@ -140,6 +140,16 @@ class AboutDialog {
 				window.open(link.href, '_blank');
 			});
 		}
+
+		const licenseInformationElement = content.querySelector(
+			'#license-information',
+		) as HTMLElement;
+		const a = document.createElement('a');
+		a.href = 'https://col.la/lic';
+		a.textContent = _('License information');
+		a.target = '_blank';
+		a.rel = 'noopener';
+		licenseInformationElement.appendChild(a);
 
 		const slowProxyElement = content.querySelector(
 			'#slow-proxy',
