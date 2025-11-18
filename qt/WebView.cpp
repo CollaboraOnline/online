6721a79203110a0db419ec2e128bd8d860ae5174
Jeremy Whiting <jeremy.whiting@collabora.com>
2025-11-18 16:11:28 -0700

    CODA-Q: Make removed tabs able to be merged back together.
    Dragging a tab from one window can be dropped on another window
    and it combines them back together.
    
    Signed-off-by: Jeremy Whiting <jeremy.whiting@collabora.com>
    Change-Id: I0594feaffd835c8d15b10546b7442c18fcb9f95d

--- qt/WebView.cpp
+++ qt/WebView.cpp
@@ -350,7 +350,6 @@ WebView::WebView(QWebEngineProfile* profile, bool isWelcome)
     {
         s_mainWindow = new QMainWindow(nullptr);
         s_tabWidget = new DetachableTabWidget(s_mainWindow);
-        s_tabWidget->setTabsClosable(true);
         s_tabWidget->setMovable(true);
 
         // Capture a local automatic pointer so the lambda can use it
