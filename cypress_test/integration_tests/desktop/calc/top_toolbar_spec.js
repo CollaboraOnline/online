05b84d46af73332bc79522be3dd6a096734d42bd
Szymon Kłos <szymon.klos@collabora.com>
2025-11-19 09:02:28 +0000

    Revert "Corrected the misspelled identifier `numberformatincdecimals`"
    This reverts commit f06c67d074f0b22789a53c400c486dedb82f63f7.
    I was wrong, the uno command has "Inc" as "Increase".
    So it was correct... "numberformatINCdecimals"
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I056e9507c2c7d5e95a900db1b7b6e554f7956fbd

--- cypress_test/integration_tests/desktop/calc/top_toolbar_spec.js
+++ cypress_test/integration_tests/desktop/calc/top_toolbar_spec.js
@@ -211,7 +211,7 @@ describe(['tagdesktop'], 'Top toolbar tests.', function() {
 		helper.setDummyClipboardForCopy();
 		// Add decimal place
 		cy.cGet('#toolbar-up #overflow-button-other-toptoolbar .arrowbackground').click();
-		cy.cGet('#numberformatingdecimals').click();
+		cy.cGet('#numberformatincdecimals').click();
 		calcHelper.selectEntireSheet();
 		helper.copy();
 
