cd494c55eb4d67dd7dd5656be1261a2985bde8eb
Andras Timar <andras.timar@collabora.com>
2025-11-20 09:33:12 +0100

    CODA-Q: update Qt translations
    Signed-off-by: Andras Timar <andras.timar@collabora.com>
    Change-Id: Ibfcf70ad7e4c19b59cec1e6b263093fb304aa5fa

--- qt/translations/coda_hu.ts
+++ qt/translations/coda_hu.ts
@@ -4,111 +4,124 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../coda-qt.cpp" line="219"/>
+        <location filename="../coda-qt.cpp" line="287"/>
         <source>Print Document</source>
         <translation>Dokumentum nyomtatása</translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="226"/>
+        <location filename="../coda-qt.cpp" line="295"/>
         <source>Select Printer:</source>
         <translation>Nyomtató választása:</translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="235"/>
-        <location filename="../coda-qt.cpp" line="303"/>
+        <location filename="../coda-qt.cpp" line="304"/>
+        <location filename="../coda-qt.cpp" line="377"/>
         <source>Default Printer</source>
         <translation>Alapértelmezett nyomtató</translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="240"/>
+        <location filename="../coda-qt.cpp" line="309"/>
         <source>Print to File</source>
         <translation>Nyomtatás fájlba</translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="244"/>
+        <location filename="../coda-qt.cpp" line="313"/>
         <source>Enter file path...</source>
         <translation>Elérési út...</translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="256"/>
+        <location filename="../coda-qt.cpp" line="325"/>
         <source>Save Print Output As</source>
         <translation>Nyomtatási kép mentése</translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="258"/>
+        <location filename="../coda-qt.cpp" line="327"/>
         <source>PDF Files (*.pdf);;All Files (*)</source>
         <translation>PDF-fájlok (*.pdf);;Minden fájl (*)</translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="268"/>
+        <location filename="../coda-qt.cpp" line="343"/>
         <source>Print</source>
         <translation>Nyomtatás</translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="269"/>
+        <location filename="../coda-qt.cpp" line="344"/>
         <source>Cancel</source>
         <translation>Mégse</translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="294"/>
+        <location filename="../coda-qt.cpp" line="368"/>
         <source>Print to File Error</source>
         <translation>Nyomtatás fájlba – Hiba</translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="295"/>
+        <location filename="../coda-qt.cpp" line="369"/>
         <source>Failed to save document to file. Please check the file path and permissions.</source>
         <translation>Nem sikerült a fájlba nyomtatás. Ellenőrizze a fájl elérési útját és a jogosultságokat.</translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="339"/>
+        <location filename="../coda-qt.cpp" line="413"/>
         <source>Print Error</source>
         <translation>Nyomtatási hiba</translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="340"/>
+        <location filename="../coda-qt.cpp" line="414"/>
         <source>Failed to print document. Please check your printer settings.</source>
         <translation>A dokumentum nyomtatása nem sikerült. Ellenőrizze a nyomtatóbeálíításokat.</translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="482"/>
+        <location filename="../coda-qt.cpp" line="573"/>
         <source>Text Documents (*.odt);;All Files (*)</source>
         <translation>Szöveges dokumentumok (*.odt);;Minden fájl (*)</translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="484"/>
+        <location filename="../coda-qt.cpp" line="575"/>
         <source>Spreadsheets (*.ods);;All Files (*)</source>
         <translation>Munkafüzetek (*.ods);;Minden fájl (*)</translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="486"/>
+        <location filename="../coda-qt.cpp" line="577"/>
         <source>Presentations (*.odp);;All Files (*)</source>
         <translation>Bemutatók (*.odp);;Minden fájl (*)</translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="488"/>
-        <location filename="../coda-qt.cpp" line="828"/>
+        <location filename="../coda-qt.cpp" line="579"/>
+        <location filename="../coda-qt.cpp" line="964"/>
         <source>All Files (*)</source>
         <translation>Minden fájl (*)</translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="492"/>
+        <location filename="../coda-qt.cpp" line="583"/>
         <source>Save Document</source>
         <translation>Dokumentum mentése</translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="735"/>
+        <location filename="../coda-qt.cpp" line="858"/>
         <source>Open File</source>
         <translation>Fájl megnyitása</translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="736"/>
+        <location filename="../coda-qt.cpp" line="859"/>
         <source>All Files (*);;Text Documents (*.odt *.ott *.doc *.docx *.rtf *.txt);;Spreadsheets (*.ods *.ots *.xls *.xlsx *.csv);;Presentations (*.odp *.otp *.ppt *.pptx)</source>
         <translation>Minden fájl (*);;Szöveges dokumentumok (*.odt *.ott *.doc *.docx *.rtf *.txt);;Munkafüzetek (*.ods *.ots *.xls *.xlsx *.csv);;Bemutatók (*.odp *.otp *.ppt *.pptx)</translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="826"/>
+        <location filename="../coda-qt.cpp" line="962"/>
         <source>Export As</source>
         <translation>Exportálás</translation>
+    </message>
+</context>
+<context>
+    <name>WebView</name>
+    <message>
+        <location filename="../WebView.cpp" line="111"/>
+        <source>Unsaved Changes</source>
+        <translation>Mentetlen változások</translation>
+    </message>
+    <message>
+        <location filename="../WebView.cpp" line="112"/>
+        <source>The document has unsaved changes. Do you want to close anyway?</source>
+        <translation>A dokumentum mentetlen változásokat tartalmaz. Ennek ellenére bezárja?</translation>
     </message>
 </context>
 </TS>
