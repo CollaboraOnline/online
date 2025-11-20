5f8f56d28ee68426fe42100dcd7688170aea24a5
Andras Timar <andras.timar@collabora.com>
2025-11-20 09:33:12 +0100

    CODA-Q: update Qt translations
    Signed-off-by: Andras Timar <andras.timar@collabora.com>
    Change-Id: Ibfcf70ad7e4c19b59cec1e6b263093fb304aa5fa

--- qt/translations/coda_da.ts
+++ qt/translations/coda_da.ts
@@ -4,110 +4,123 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../coda-qt.cpp" line="221"/>
+        <location filename="../coda-qt.cpp" line="287"/>
         <source>Print Document</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="228"/>
+        <location filename="../coda-qt.cpp" line="295"/>
         <source>Select Printer:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="237"/>
-        <location filename="../coda-qt.cpp" line="305"/>
+        <location filename="../coda-qt.cpp" line="304"/>
+        <location filename="../coda-qt.cpp" line="377"/>
         <source>Default Printer</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="242"/>
+        <location filename="../coda-qt.cpp" line="309"/>
         <source>Print to File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="246"/>
+        <location filename="../coda-qt.cpp" line="313"/>
         <source>Enter file path...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="258"/>
+        <location filename="../coda-qt.cpp" line="325"/>
         <source>Save Print Output As</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="260"/>
+        <location filename="../coda-qt.cpp" line="327"/>
         <source>PDF Files (*.pdf);;All Files (*)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="270"/>
+        <location filename="../coda-qt.cpp" line="343"/>
         <source>Print</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="271"/>
+        <location filename="../coda-qt.cpp" line="344"/>
         <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="296"/>
+        <location filename="../coda-qt.cpp" line="368"/>
         <source>Print to File Error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="297"/>
+        <location filename="../coda-qt.cpp" line="369"/>
         <source>Failed to save document to file. Please check the file path and permissions.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="341"/>
+        <location filename="../coda-qt.cpp" line="413"/>
         <source>Print Error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="342"/>
+        <location filename="../coda-qt.cpp" line="414"/>
         <source>Failed to print document. Please check your printer settings.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="484"/>
+        <location filename="../coda-qt.cpp" line="573"/>
         <source>Text Documents (*.odt);;All Files (*)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="486"/>
+        <location filename="../coda-qt.cpp" line="575"/>
         <source>Spreadsheets (*.ods);;All Files (*)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="488"/>
+        <location filename="../coda-qt.cpp" line="577"/>
         <source>Presentations (*.odp);;All Files (*)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="490"/>
-        <location filename="../coda-qt.cpp" line="830"/>
+        <location filename="../coda-qt.cpp" line="579"/>
+        <location filename="../coda-qt.cpp" line="964"/>
         <source>All Files (*)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="494"/>
+        <location filename="../coda-qt.cpp" line="583"/>
         <source>Save Document</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="737"/>
+        <location filename="../coda-qt.cpp" line="858"/>
         <source>Open File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="738"/>
+        <location filename="../coda-qt.cpp" line="859"/>
         <source>All Files (*);;Text Documents (*.odt *.ott *.doc *.docx *.rtf *.txt);;Spreadsheets (*.ods *.ots *.xls *.xlsx *.csv);;Presentations (*.odp *.otp *.ppt *.pptx)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../coda-qt.cpp" line="828"/>
+        <location filename="../coda-qt.cpp" line="962"/>
         <source>Export As</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WebView</name>
+    <message>
+        <location filename="../WebView.cpp" line="111"/>
+        <source>Unsaved Changes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../WebView.cpp" line="112"/>
+        <source>The document has unsaved changes. Do you want to close anyway?</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
--- qt/translations/coda_kk.ts
+++ qt/translations/coda_kk.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="bg_BG">
+<TS version="2.1" language="kk_KZ">
 <context>
     <name>QObject</name>
     <message>
