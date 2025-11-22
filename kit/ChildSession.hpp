a032e8d03a61115805daa8931087189e244d5dee
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2025-11-21 19:41:21 -0500

    wsd: better logging in ChildSession senders
    Change-Id: I70646f5e675a22510e68c7eadb0fe68745f5e651
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- kit/ChildSession.hpp
+++ kit/ChildSession.hpp
@@ -117,7 +117,8 @@ public:
     {
         if (_docManager == nullptr)
         {
-            LOG_TRC("ERR dropping - client-" + getId() + ' ' + std::string(buffer, length));
+            LOG_TRC("No DocManager; dropping message to client-"
+                    << getId() << ": " << std::string_view(buffer, length));
             return false;
         }
         const auto msg = "client-" + getId() + ' ' + std::string(buffer, length);
@@ -128,7 +129,7 @@ public:
     {
         if (_docManager == nullptr)
         {
-            LOG_TRC("ERR dropping binary - client-" + getId());
+            LOG_TRC("No DocManager; dropping binary to client-" << getId());
             return false;
         }
         const auto msg = "client-" + getId() + ' ' + std::string(buffer, length);
