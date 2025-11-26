CollaboraOnline/online

commit 1e13b3bf9a473a963269efab66565a46678692bd

browser: accessibility: add 'aria-expanded' to the button
https://www.w3.org/WAI/ARIA/apg/patterns/disclosure/

Change-Id: I4dd17d7c213aa5130178de00a98efccd613823d1
Signed-off-by: Henry Castro <hcastro@collabora.com>

browser/src/control/Control.JSDialogBuilder.js
@@ -715,8 +715,10 @@ L.Control.JSDialogBuilder = L.Control.extend({
 				builder.postProcess(expander, data.children[0]);
 
 				var state = data.children.length > 1 && expanded;
-				$(label).addClass('expanded', state);
-				label.setAttribute('aria-expanded', state);
+				if (state) {
+					L.DomUtil.addClass(label, 'expanded');
+				}
+				expander.setAttribute('aria-expanded', state);
 
 				var toggleFunction = function () {
 					if (customCallback)
@@ -724,8 +726,8 @@ L.Control.JSDialogBuilder = L.Control.extend({
 					else
 						builder.callback('expander', 'toggle', data, null, builder);
 
-					var state = Boolean(label.getAttribute('aria-expanded'));
-					label.setAttribute('aria-expanded', !state);
+					var state = Boolean(expander.getAttribute('aria-expanded'));
+					expander.setAttribute('aria-expanded', !state);
 					$(label).toggleClass('expanded');
 					$(expander).siblings().toggleClass('expanded');
 

commit 7f6f09747ffc58014c443eed73547e0e0852c028

accessibility: remove duplicate aria-expanded state control logic
This fixes a regression introduced in 1e13b3bf9a473a963269efab66565a46678692bd where duplicate logic controlling aria-expanded caused the attribute to remain permanently set to true

Signed-off-by: Parth Raiyani <parth.raiyani@collabora.com>
Change-Id: Ie971a9dc30d60f63531a52c7fdf89173f8aac06e

browser/src/control/Control.JSDialogBuilder.js
@@ -767,8 +767,6 @@ window.L.Control.JSDialogBuilder = window.L.Control.extend({
 					else
 						builder.callback('expander', 'toggle', data, null, builder);
 
-					var state = expander.getAttribute('aria-expanded') === 'true';
-					expander.setAttribute('aria-expanded', !state);
 					$(label).toggleClass('expanded');
 					$(expander).siblings().toggleClass('expanded');
 