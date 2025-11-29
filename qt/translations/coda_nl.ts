da83f1a26ebfb1ced84b2b808d21906faee0fa88
Sebastiaan Veld <sebastiaan.veld@gmail.com>
2025-11-29 17:09:36 +0100

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (23 of 23 strings)
    
    Translation: Collabora Online/Collabora Office Qt app
    Translate-URL: https://hosted.weblate.org/projects/collabora-online/collabora-office-qt-app/nl/
    Signed-off-by: Andras Timar <andras.timar@collabora.com>
    Change-Id: I2b72be13e7058e335de37c51ec194524a3ceaa8f

--- qt/translations/coda_nl.ts
+++ qt/translations/coda_nl.ts
@@ -6,109 +6,109 @@
     <message>
         <location filename="../coda-qt.cpp" line="287"/>
         <source>Print Document</source>
-        <translation type="unfinished"></translation>
+        <translation>Document afdrukken</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="295"/>
         <source>Select Printer:</source>
-        <translation type="unfinished"></translation>
+        <translation>Printer selecteren:</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="304"/>
         <location filename="../coda-qt.cpp" line="377"/>
         <source>Default Printer</source>
-        <translation type="unfinished"></translation>
+        <translation>Standaardprinter</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="309"/>
         <source>Print to File</source>
-        <translation type="unfinished"></translation>
+        <translation>Afdrukken naar bestand</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="313"/>
         <source>Enter file path...</source>
-        <translation type="unfinished"></translation>
+        <translation>Voer het bestandspad in...</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="325"/>
         <source>Save Print Output As</source>
-        <translation type="unfinished"></translation>
+        <translation>Afdrukuitvoer opslaan als</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="327"/>
         <source>PDF Files (*.pdf);;All Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>PDF-bestanden (*.pdf);;Alle bestanden (*)</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="343"/>
         <source>Print</source>
-        <translation type="unfinished"></translation>
+        <translation>Afdrukken</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="344"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Annuleren</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="368"/>
         <source>Print to File Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Fout bij afdrukken naar bestand</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="369"/>
         <source>Failed to save document to file. Please check the file path and permissions.</source>
-        <translation type="unfinished"></translation>
+        <translation>Het document kan niet worden opgeslagen in het bestand. Controleer het bestandspad en de machtigingen.</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="413"/>
         <source>Print Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Afdrukfout</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="414"/>
         <source>Failed to print document. Please check your printer settings.</source>
-        <translation type="unfinished"></translation>
+        <translation>Het document kan niet worden afgedrukt. Controleer de printerinstellingen.</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="573"/>
         <source>Text Documents (*.odt);;All Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>Tekstdocumenten (*.odt);;Alle bestanden (*)</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="575"/>
         <source>Spreadsheets (*.ods);;All Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>Spreadsheets (*.ods);;Alle bestanden (*)</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="577"/>
         <source>Presentations (*.odp);;All Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>Presentaties (*.odp);;Alle bestanden (*)</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="579"/>
         <location filename="../coda-qt.cpp" line="964"/>
         <source>All Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>Alle bestanden (*)</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="583"/>
         <source>Save Document</source>
-        <translation type="unfinished"></translation>
+        <translation>Document opslaan</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="858"/>
         <source>Open File</source>
-        <translation type="unfinished"></translation>
+        <translation>Bestand openen</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="859"/>
         <source>All Files (*);;Text Documents (*.odt *.ott *.doc *.docx *.rtf *.txt);;Spreadsheets (*.ods *.ots *.xls *.xlsx *.csv);;Presentations (*.odp *.otp *.ppt *.pptx)</source>
-        <translation type="unfinished"></translation>
+        <translation>Alle bestanden (*);;Tekstdocumenten (*.odt *.ott *.doc *.docx *.rtf *.txt);;Spreadsheets (*.ods *.ots *.xls *.xlsx *.csv);;Presentaties (*.odp *.otp *.ppt *.pptx)</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="962"/>
         <source>Export As</source>
-        <translation type="unfinished"></translation>
+        <translation>Exporteren als</translation>
     </message>
 </context>
 <context>
@@ -116,12 +116,12 @@
     <message>
         <location filename="../WebView.cpp" line="111"/>
         <source>Unsaved Changes</source>
-        <translation type="unfinished"></translation>
+        <translation>Niet opgeslagen wijzigingen</translation>
     </message>
     <message>
         <location filename="../WebView.cpp" line="112"/>
         <source>The document has unsaved changes. Do you want to close anyway?</source>
-        <translation type="unfinished"></translation>
+        <translation>Het document bevat nog niet opgeslagen wijzigingen. Wilt u toch afsluiten?</translation>
     </message>
 </context>
 </TS>
