0b50fb72c82dcae5cbeab53a369c63a7c689d623
Adolfo Jayme Barrientos <fitojb@ubuntu.com>
2025-11-29 11:59:44 +0100

    Translated using Weblate (Catalan)
    Currently translated at 100.0% (23 of 23 strings)
    
    Translation: Collabora Online/Collabora Office Qt app
    Translate-URL: https://hosted.weblate.org/projects/collabora-online/collabora-office-qt-app/ca/
    Signed-off-by: Andras Timar <andras.timar@collabora.com>
    Change-Id: I93e3a5723da811c796c79e7c2969dd8e29bc0a38

--- qt/translations/coda_ca.ts
+++ qt/translations/coda_ca.ts
@@ -6,109 +6,109 @@
     <message>
         <location filename="../coda-qt.cpp" line="287"/>
         <source>Print Document</source>
-        <translation type="unfinished"></translation>
+        <translation>Imprimeix el document</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="295"/>
         <source>Select Printer:</source>
-        <translation type="unfinished"></translation>
+        <translation>Seleccioneu una impressora:</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="304"/>
         <location filename="../coda-qt.cpp" line="377"/>
         <source>Default Printer</source>
-        <translation type="unfinished"></translation>
+        <translation>Impressora per defecte</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="309"/>
         <source>Print to File</source>
-        <translation type="unfinished"></translation>
+        <translation>Imprimeix en un fitxer</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="313"/>
         <source>Enter file path...</source>
-        <translation type="unfinished"></translation>
+        <translation>Introduïu el camí del fitxer...</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="325"/>
         <source>Save Print Output As</source>
-        <translation type="unfinished"></translation>
+        <translation>Anomena i desa la sortida de la impressió</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="327"/>
         <source>PDF Files (*.pdf);;All Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>Fitxers PDF (*.pdf);;Tots els fitxers (*)</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="343"/>
         <source>Print</source>
-        <translation type="unfinished"></translation>
+        <translation>Imprimeix</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="344"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Cancel·la</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="368"/>
         <source>Print to File Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Error d&apos;impressió com a fitxer</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="369"/>
         <source>Failed to save document to file. Please check the file path and permissions.</source>
-        <translation type="unfinished"></translation>
+        <translation>No s&apos;ha pogut desar el document al fitxer. Comproveu el camí i els permisos del fitxer.</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="413"/>
         <source>Print Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Error d&apos;impressió</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="414"/>
         <source>Failed to print document. Please check your printer settings.</source>
-        <translation type="unfinished"></translation>
+        <translation>No s&apos;ha pogut imprimir el document. Comproveu els paràmetres de la impressora.</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="573"/>
         <source>Text Documents (*.odt);;All Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>Documents de text (*.odt);;Tots els fitxers (*)</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="575"/>
         <source>Spreadsheets (*.ods);;All Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>Full de càlcul (*.ods);;Tots els fitxers (*)</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="577"/>
         <source>Presentations (*.odp);;All Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>Presentacions (*.odp);;Tots els fitxers (*)</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="579"/>
         <location filename="../coda-qt.cpp" line="964"/>
         <source>All Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>Tots els fitxers (*)</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="583"/>
         <source>Save Document</source>
-        <translation type="unfinished"></translation>
+        <translation>Desa el document</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="858"/>
         <source>Open File</source>
-        <translation type="unfinished"></translation>
+        <translation>Obre un fitxer</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="859"/>
         <source>All Files (*);;Text Documents (*.odt *.ott *.doc *.docx *.rtf *.txt);;Spreadsheets (*.ods *.ots *.xls *.xlsx *.csv);;Presentations (*.odp *.otp *.ppt *.pptx)</source>
-        <translation type="unfinished"></translation>
+        <translation>Tots els fitxers (*);;Documents de text (*.odt *.ott *.doc *.docx *.rtf *.txt);;Fulls de càlcul (*.ods *.ots *.xls *.xlsx *.csv);;Presentacions (*.odp *.otp *.ppt *.pptx)</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="962"/>
         <source>Export As</source>
-        <translation type="unfinished"></translation>
+        <translation>Exporta com a</translation>
     </message>
 </context>
 <context>
@@ -116,12 +116,12 @@
     <message>
         <location filename="../WebView.cpp" line="111"/>
         <source>Unsaved Changes</source>
-        <translation type="unfinished"></translation>
+        <translation>Canvis sense desar</translation>
     </message>
     <message>
         <location filename="../WebView.cpp" line="112"/>
         <source>The document has unsaved changes. Do you want to close anyway?</source>
-        <translation type="unfinished"></translation>
+        <translation>El document conté canvis que no s&apos;han desat. Voleu tancar-lo igualment?</translation>
     </message>
 </context>
 </TS>
