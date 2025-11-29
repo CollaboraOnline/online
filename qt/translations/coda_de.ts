783b83f5ca67c0a6e157fa72b19726466a543ecd
Ettore Atalan <atalanttore@googlemail.com>
2025-11-29 13:46:03 +0100

    Translated using Weblate (German)
    Currently translated at 91.3% (21 of 23 strings)
    
    Translation: Collabora Online/Collabora Office Qt app
    Translate-URL: https://hosted.weblate.org/projects/collabora-online/collabora-office-qt-app/de/
    Signed-off-by: Andras Timar <andras.timar@collabora.com>
    Change-Id: I136bd0f25627db5616cea275d1f0ce0b20abc353

--- qt/translations/coda_de.ts
+++ qt/translations/coda_de.ts
@@ -6,73 +6,73 @@
     <message>
         <location filename="../coda-qt.cpp" line="287"/>
         <source>Print Document</source>
-        <translation type="unfinished"></translation>
+        <translation>Dokument drucken</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="295"/>
         <source>Select Printer:</source>
-        <translation type="unfinished"></translation>
+        <translation>Drucker auswählen:</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="304"/>
         <location filename="../coda-qt.cpp" line="377"/>
         <source>Default Printer</source>
-        <translation type="unfinished"></translation>
+        <translation>Standarddrucker</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="309"/>
         <source>Print to File</source>
-        <translation type="unfinished"></translation>
+        <translation>In Datei drucken</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="313"/>
         <source>Enter file path...</source>
-        <translation type="unfinished"></translation>
+        <translation>Dateipfad eingeben...</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="325"/>
         <source>Save Print Output As</source>
-        <translation type="unfinished"></translation>
+        <translation>Druckausgabe speichern als</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="327"/>
         <source>PDF Files (*.pdf);;All Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>PDF-Dateien (*.pdf);;Alle Dateien (*)</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="343"/>
         <source>Print</source>
-        <translation type="unfinished"></translation>
+        <translation>Drucken</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="344"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Abbrechen</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="368"/>
         <source>Print to File Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Fehler beim Drucken in Datei</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="369"/>
         <source>Failed to save document to file. Please check the file path and permissions.</source>
-        <translation type="unfinished"></translation>
+        <translation>Das Dokument konnte nicht in der Datei gespeichert werden. Bitte überprüfen Sie den Dateipfad und die Berechtigungen.</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="413"/>
         <source>Print Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Druckfehler</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="414"/>
         <source>Failed to print document. Please check your printer settings.</source>
-        <translation type="unfinished"></translation>
+        <translation>Das Dokument konnte nicht gedruckt werden. Bitte überprüfen Sie Ihre Druckereinstellungen.</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="573"/>
         <source>Text Documents (*.odt);;All Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>Textdokumente (*.odt);;Alle Dateien (*)</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="575"/>
@@ -82,23 +82,23 @@
     <message>
         <location filename="../coda-qt.cpp" line="577"/>
         <source>Presentations (*.odp);;All Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>Präsentationen (*.odp);;Alle Dateien (*)</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="579"/>
         <location filename="../coda-qt.cpp" line="964"/>
         <source>All Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>Alle Dateien (*)</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="583"/>
         <source>Save Document</source>
-        <translation type="unfinished"></translation>
+        <translation>Dokument speichern</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="858"/>
         <source>Open File</source>
-        <translation type="unfinished"></translation>
+        <translation>Datei öffnen</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="859"/>
@@ -108,7 +108,7 @@
     <message>
         <location filename="../coda-qt.cpp" line="962"/>
         <source>Export As</source>
-        <translation type="unfinished"></translation>
+        <translation>Exportieren als</translation>
     </message>
 </context>
 <context>
@@ -116,12 +116,12 @@
     <message>
         <location filename="../WebView.cpp" line="111"/>
         <source>Unsaved Changes</source>
-        <translation type="unfinished"></translation>
+        <translation>Nicht gespeicherte Änderungen</translation>
     </message>
     <message>
         <location filename="../WebView.cpp" line="112"/>
         <source>The document has unsaved changes. Do you want to close anyway?</source>
-        <translation type="unfinished"></translation>
+        <translation>Das Dokument hat nicht gespeicherte Änderungen. Möchten Sie es trotzdem schließen?</translation>
     </message>
 </context>
 </TS>
