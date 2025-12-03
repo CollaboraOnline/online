4ba237f71b3cf8890bd7bc0b78e375ac20b2dea5
Dennis Francis <dennis.francis@collabora.com>
2025-12-03 17:40:49 +0530

    cypress_test: test enter/exit calc debug mode
    Signed-off-by: Dennis Francis <dennis.francis@collabora.com>
    Change-Id: I0a5265fc44755861dca512e330cbfd25c153402b

---
+++ cypress_test/integration_tests/desktop/calc/debug_tools_spec.js
@@ -0,0 +1,18 @@
+/* global describe it cy require beforeEach */
+var helper = require('../../common/helper');
+
+describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'Debug tools', function() {
+
+	beforeEach(function() {
+		helper.setupAndLoadDocument('calc/debug_tools.ods');
+	});
+
+	it('Enter and exit debug mode', function() {
+		cy.cGet('.leaflet-control-layers.leaflet-control-layers-expanded.leaflet-control').should('not.exist');
+		helper.typeIntoDocument('{ctrl}{alt}{shift}D');
+		cy.cGet('.leaflet-control-layers.leaflet-control-layers-expanded.leaflet-control').should('be.visible');
+		helper.typeIntoDocument('{ctrl}{alt}{shift}D');
+		cy.cGet('.leaflet-control-layers.leaflet-control-layers-expanded.leaflet-control').should('not.exist');
+	});
+
+});
