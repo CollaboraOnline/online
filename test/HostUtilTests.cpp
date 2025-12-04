3db1e108f36f1352b64deaf87006a1cef1b2cdaf
Méven Car <meven.car@collabora.com>
2025-12-04 12:33:51 +0100

    HostUtil: properly handle alias using regex
    The aliasString was used to construct a POCO::URI then used the uri
    authority as a regex. Except an authority does not accept caracters
    typically found in regex: +[]()... Making regex interpretation invalid.
    
    This PR checks whether the pattern passed in is a regex or not, if not
    transforms the hostname passed-in into a regex.
    
    If it is a regex and since Regex validity is now checked, we can add the
    aliasPattern to the AliasHost and WopiHosts.
    
    Also adds warnings when | may have been used as alias separator before
    multiple <alias> were posssible, except this prevnts regex handling to
    work then, so warn about deprectation.
    
    Signed-off-by: Méven Car <meven.car@collabora.com>
    Change-Id: I5f286d5098491312588024be3ec946d31a7c6f1b

---
+++ test/HostUtilTests.cpp
@@ -0,0 +1,54 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <config.h>
+
+#include "HostUtil.hpp"
+
+#include <test/lokassert.hpp>
+
+#include <cppunit/extensions/HelperMacros.h>
+
+/// HostUtilTests unit-tests.
+class HostUtilTests : public CPPUNIT_NS::TestFixture
+{
+    CPPUNIT_TEST_SUITE(HostUtilTests);
+
+    CPPUNIT_TEST(testParseAlias);
+
+    CPPUNIT_TEST_SUITE_END();
+
+    void testParseAlias();
+};
+
+void HostUtilTests::testParseAlias()
+{
+    constexpr std::string_view testname = __func__;
+
+    LOK_ASSERT_EQUAL_STR("test2\\.local", HostUtil::parseAlias("test2.local"));
+    LOK_ASSERT_EQUAL_STR("test3\\.local", HostUtil::parseAlias("https://test3.local"));
+    LOK_ASSERT_EQUAL_STR("test4\\.local", HostUtil::parseAlias("https://test4.local:8080"));
+    LOK_ASSERT_EQUAL_STR("test5\\.local", HostUtil::parseAlias("https://test5.local:8080/"));
+    LOK_ASSERT_EQUAL_STR("test6\\.local", HostUtil::parseAlias("https://test6.local:8080/path"));
+    LOK_ASSERT_EQUAL_STR("test7\\.local", HostUtil::parseAlias("test7.local/path"));
+
+    LOK_ASSERT_EQUAL_STR("test", HostUtil::parseAlias("test")); // invalid hostname, interpret as regex
+    LOK_ASSERT_EQUAL_STR("test[1-3]", HostUtil::parseAlias("test[1-3]"));
+    LOK_ASSERT_EQUAL_STR("test[0-9].local", HostUtil::parseAlias("test[0-9].local"));
+    LOK_ASSERT_EQUAL_STR("test[0-9]+.local", HostUtil::parseAlias("test[0-9]+.local"));
+    LOK_ASSERT_EQUAL_STR("", HostUtil::parseAlias("test[0-9.local")); // invalid regex
+
+    LOK_ASSERT_EQUAL_STR("https://aliasname[0-9]{1}:443", HostUtil::parseAlias("https://aliasname[0-9]{1}:443"));
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(HostUtilTests);
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
