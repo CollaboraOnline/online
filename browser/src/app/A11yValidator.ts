07610b8ce85a5b6987ce4e7844f1b63b92d16303
Parth Raiyani <parth.raiyani@collabora.com>
2025-12-04 18:41:16 +0530

    accessibility: add checkImageAltAttribute to A11yValidator for improved accessibility checks
    How this works:
    - do not executing check for hidden images
    - if img missing "alt" attribute throw error
    - if img has empty "alt" attribute then one of the following label should be present, else throw error:
        1. aria-label
        2. aria-labelledby
        3. <label> with "for" referencing this img-> parent
        4. explicit span text as sibling(with-in same parent) as it will be detected by assistive technology
    - "ui-decorative-image" is new class specifically introduced to detect decorative image for this check as "alt" can be empty for decorative images which won't be announced by assistive technology
    - if img has "alt" attribute and its parent also has any of above label then it will create confusion for assistive technology. check will throw error
    
    Signed-off-by: Parth Raiyani <parth.raiyani@collabora.com>
    Change-Id: Ia44133c328454b3dfd73814b29a605f3bce212a0

--- browser/src/app/A11yValidator.ts
+++ browser/src/app/A11yValidator.ts
@@ -18,6 +18,7 @@ class A11yValidator {
 
 	private setupChecks(): void {
 		this.checks.push(this.checkNativeButtonElement.bind(this));
+		this.checks.push(this.checkImageAltAttribute.bind(this));
 	}
 
 	checkWidget(type: string, element: HTMLElement): void {
@@ -47,6 +48,51 @@ class A11yValidator {
 				this.checkNativeButtonElement(type, child);
 			}
 		}
+	}
+
+	private checkImageAltAttribute(type: string, element: HTMLElement): void {
+		const images = element.querySelectorAll('img');
+
+		images.forEach((img, index) => {
+			const hasAlt = img.hasAttribute('alt');
+			const altValue = img.getAttribute('alt');
+
+			if (!this.isVisible(img)) return; // skip hidden images
+
+			if (!hasAlt)
+				throw `Image element with id: ${img.id} in widget of type '${type}' is missing alt attribute`;
+
+			const parent = img.parentElement;
+			const span =
+				parent && (parent.querySelector('span.unolabel') as HTMLSpanElement);
+			const explicitLabel = span && span.innerText.trim().length > 0;
+			const visibleLabel =
+				parent && document.querySelector(`label[for^="${parent.id}-"]`);
+
+			const parentHasLabel =
+				parent &&
+				(parent.hasAttribute('aria-label') ||
+					parent.hasAttribute('aria-labelledby') ||
+					visibleLabel ||
+					explicitLabel);
+
+			if (altValue === '' && parent) {
+				const isDecorativeImg = img.classList.contains('ui-decorative-image'); // exclude ui-decorative-image decorative images - they can have empty alt
+
+				if (!parentHasLabel && !isDecorativeImg)
+					throw `Image element with id: ${img.id} inside parent with id: ${parent.id} in widget of type '${type}' has empty alt attribute but parent element lacks label`;
+			}
+
+			if (altValue !== '' && parentHasLabel)
+				throw `Image element with id: ${img.id} inside parent with id: ${parent.id} in widget of type '${type}' has non-empty alt attribute but parent element also has label (should not duplicate)`;
+		});
+	}
+
+	private isVisible(element: HTMLElement): boolean {
+		const style = getComputedStyle(element);
+		if (style.visibility === 'hidden') return false;
+
+		return element.getClientRects().length > 0;
 	}
 }
 
