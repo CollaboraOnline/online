0acebf7f9df5e65fef35b4fed948d2e2547a741e
Parth Raiyani <parth.raiyani@collabora.com>
2025-12-04 19:41:37 +0530

    accessibility: fix the cypress assertion technique to correctly behave on A11yValidator exception
    - Error is thrown and getting logged via "console.error" - this change correctly check if "A11yValidator exception" found or not and assert it
    - It also solves the problem of displaying correct meaningful error message to user
    
    Signed-off-by: Parth Raiyani <parth.raiyani@collabora.com>
    Change-Id: Iaa7b20d6bcd45c9196efc3ddf13607149a7dc764

--- cypress_test/integration_tests/desktop/writer/a11y_dialog_spec.js
+++ cypress_test/integration_tests/desktop/writer/a11y_dialog_spec.js
@@ -8,11 +8,27 @@ describe(['tagdesktop'], 'Accessibility Writer Tests', function() {
 
     it('Check accessibility for writer', function() {
         cy.getFrameWindow().then(function (win) {
+            cy.spy(win.console, 'error').as('console:error');
+
             win.app.allDialogs.forEach(command => {
                 cy.log(`Testing dialog: ${command}`);
                 win.app.map.sendUnoCommand(command);
-                cy.wait(500);
+                cy.wait(1000);
                 cy.cGet('body').type('{esc}');
+            });
+
+            cy.get('@console:error').then(spy => {
+                const a11yErrors = spy.getCalls().filter(call =>
+                    String(call.args[0]).includes('A11yValidator exception')
+                );
+
+                if (a11yErrors.length > 0) {
+                    const errorMessages = a11yErrors.map(call =>
+                        call.args.map(arg => String(arg)).join(' ')
+                    ).join('\n\n');
+
+                    throw new Error(`Found A11y errors:\n${errorMessages}`);
+                }
             });
         });
     });
