378722f8786916b118cc17ab3d7a1f503c99d8ac
Rashesh Padia <rashesh.padia@collabora.com>
2025-12-08 22:34:08 +0530

    coolwsd: implement socket pump
    Signed-off-by: Rashesh Padia <rashesh.padia@collabora.com>
    Change-Id: Ifda245f3df603021e754a97a8ec5fee4d628b8cc

--- wsd/ProxyPoll.cpp
+++ wsd/ProxyPoll.cpp
@@ -8,9 +8,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-
-#include <ProxyPoll.hpp>
+#include <config.h>
 #include <Socket.hpp>
+#include <HttpHelper.hpp>
+#include <Poco/Net/HTTPRequest.h>
+#include <ProxyPoll.hpp>
 #include <memory>
 
 class ProxyHandler : public SimpleSocketHandler
@@ -77,3 +79,51 @@ public:
             peer->asyncShutdown();
     }
 };
+
+namespace
+{
+[[maybe_unused]] static void startProxyPump(const std::shared_ptr<StreamSocket>& clientSocket,
+                                            const std::string& targetIp, int targetPort,
+                                            const Poco::Net::HTTPRequest& originalRequest)
+{
+    // pumps target -> client
+    auto targetHandler = std::make_shared<ProxyHandler>(clientSocket);
+
+    // Async connect to target pod (avoids blocking DNS)
+    net::asyncConnect(
+        targetIp, std::to_string(targetPort),
+        false, // isSSL - internal traffic typically unencrypted
+        targetHandler,
+        [clientSocket, &originalRequest, targetHandler](
+            const std::shared_ptr<StreamSocket>& targetSocket, net::AsyncConnectResult result)
+        {
+            if (result != net::AsyncConnectResult::Ok || !targetSocket)
+            {
+                LOG_ERR("Failed to connect to target pod");
+                HttpHelper::sendErrorAndShutdown(http::StatusCode::BadGateway, clientSocket);
+                return;
+            }
+
+            // pumps client -> target
+            auto clientHandler = std::make_shared<ProxyHandler>(targetSocket);
+            clientSocket->setHandler(clientHandler);
+
+            std::ostringstream oss;
+            originalRequest.write(oss);
+            std::string reqStr = oss.str();
+
+            // Inject X-COOL-Internal-Proxy header
+            size_t headerEnd = reqStr.find("\r\n\r\n");
+            std::string proxiedRequest = reqStr.substr(0, headerEnd) +
+                                         "\r\nX-COOL-Internal-Proxy: true" +
+                                         reqStr.substr(headerEnd);
+
+            targetSocket->send(proxiedRequest);
+
+            ProxyPoll::instance().insertNewSocket(clientSocket);
+            ProxyPoll::instance().insertNewSocket(targetSocket);
+
+            LOG_INF("Proxy established: client <-> target");
+        });
+}
+} // namespace
