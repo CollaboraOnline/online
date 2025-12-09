CollaboraOnline/online

commit 265133f8e7d2c664654b6ba72c7ac2447b3775f0

notebookbar: expand/collapse tooltip update
- when notebookbar is collapsed - all tabs show tooltip
- when notebookbar is expanded - only active tab has tooltip

Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
Change-Id: Iafb9d5a023efe1987ce50ebb7a47172b7a03d1dd

browser/src/control/Control.NotebookbarBuilder.js
@@ -222,7 +222,8 @@ window.L.Control.NotebookbarBuilder = window.L.Control.JSDialogBuilder.extend({
 	{
 		var tooltipCollapsed = _('Tap to expand');
 		var tooltipExpanded = _('Tap to collapse');
-		tabs[t].setAttribute('data-cooltip', tooltipExpanded);
+		if ($(tabs[t]).hasClass('selected'))
+			tabs[t].setAttribute('data-cooltip', tooltipExpanded);
 		window.L.control.attachTooltipEventListener(tabs[t], builder.map);
 		return function(event) {
 			var tabIsSelected = $(tabs[t]).hasClass('selected');
@@ -230,10 +231,17 @@ window.L.Control.NotebookbarBuilder = window.L.Control.JSDialogBuilder.extend({
 
 			var accessibilityInputElementHasFocus = app.UI.notebookbarAccessibility && app.UI.notebookbarAccessibility.accessibilityInputElement === document.activeElement ? true: false;
 
+			for (var i = 0; i < tabs.length; i++) {
+				if (i !== t) {
+					tabs[i].setAttribute('data-cooltip', '');
+				}
+			}
+
 			if (tabIsSelected && !notebookbarIsCollapsed && !accessibilityInputElementHasFocus) {
 				builder.wizard.collapse();
-				tabs[t].setAttribute('data-cooltip', tooltipCollapsed);
-			} else if (notebookbarIsCollapsed) {
+				for (i = 0; i < tabs.length; i++)
+					tabs[i].setAttribute('data-cooltip', tooltipCollapsed);
+			} else {
 				builder.wizard.extend();
 				tabs[t].setAttribute('data-cooltip', tooltipExpanded);
 			}
@@ -241,12 +249,11 @@ window.L.Control.NotebookbarBuilder = window.L.Control.JSDialogBuilder.extend({
 			$(tabs[t]).addClass('selected');
 			tabs[t].setAttribute('aria-selected', 'true');
 			tabs[t].removeAttribute('tabindex');
-			for (var i = 0; i < tabs.length; i++) {
+			for (i = 0; i < tabs.length; i++) {
 				if (i !== t) {
 					$(tabs[i]).removeClass('selected');
 					tabs[i].setAttribute('aria-selected', 'false');
 					tabs[i].tabIndex = -1;
-					tabs[i].setAttribute('data-cooltip', '');
 					$(contentDivs[i]).addClass('hidden');
 				}
 			}

commit 41b4bae2996fbb41483f2878a4f87136fa5fd88c

CODA(backstage): Fix Backstage view not visible for File tab
Likely a rebase regression introduced in commit 265133f8e7d2c664654b6ba72c7ac2447b3775f0.

Signed-off-by: codewithvk <vivek.javiya@collabora.com>
Change-Id: Iadea8e7034c958f0305b147649a09b8f30747c3b

browser/src/control/Control.NotebookbarBuilder.js
@@ -224,11 +224,11 @@ window.L.Control.NotebookbarBuilder = window.L.Control.JSDialogBuilder.extend({
 		const isDesktop = window.mode.isDesktop();
 		const tooltipCollapsed = isDesktop ? _('Click to expand') : _('Tap to expand');
 		const tooltipExpanded = isDesktop ? _('Click to collapse') : _('Tap to collapse');
-		if ($(tabs[t]).hasClass('selected'))
 		var isFileTab = tabIds[t] === 'File-tab-label' || tabIds[t] === 'File';
 		var isFileTabForCoda = isFileTab && window.mode.isCODesktop();
 		if (!isFileTabForCoda) {
-			tabs[t].setAttribute('data-cooltip', tooltipExpanded);
+			if ($(tabs[t]).hasClass('selected'))
+				tabs[t].setAttribute('data-cooltip', tooltipExpanded);
 			window.L.control.attachTooltipEventListener(tabs[t], builder.map);
 		} else {
 			tabs[t].removeAttribute('data-cooltip');