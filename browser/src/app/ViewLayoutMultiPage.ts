475066c9723d444d2e86dbf43c46ceb180e752f1
Gökay Şatır <gokaysatir@collabora.com>
2025-12-11 11:33:51 +0300

    Move refreshCurrentCoordList from derived class to base class.
    Signed-off-by: Gökay Şatır <gokaysatir@collabora.com>
    Change-Id: I903cb1cee65345fca6de5a5f75f4d88f23dee936

--- browser/src/app/ViewLayoutMultiPage.ts
+++ browser/src/app/ViewLayoutMultiPage.ts
@@ -173,37 +173,6 @@ class ViewLayoutMultiPage extends ViewLayoutBase {
 		return part;
 	}
 
-	private refreshCurrentCoordList() {
-		this.currentCoordList.length = 0;
-		const zoom = app.map.getZoom();
-
-		const columnCount = Math.ceil(
-			this._viewedRectangle.pWidth / TileManager.tileSize,
-		);
-		const rowCount = Math.ceil(
-			this._viewedRectangle.pHeight / TileManager.tileSize,
-		);
-		const startX =
-			Math.floor(this._viewedRectangle.pX1 / TileManager.tileSize) *
-			TileManager.tileSize;
-		const startY =
-			Math.floor(this._viewedRectangle.pY1 / TileManager.tileSize) *
-			TileManager.tileSize;
-
-		for (let i = 0; i < columnCount; i++) {
-			for (let j = 0; j < rowCount; j++) {
-				const coords = new TileCoordData(
-					startX + i * TileManager.tileSize,
-					startY + j * TileManager.tileSize,
-					zoom,
-					0,
-				);
-
-				if (coords.x >= 0 && coords.y >= 0) this.currentCoordList.push(coords);
-			}
-		}
-	}
-
 	private refreshVisibleAreaRectangle(): void {
 		const documentAnchor = this.getDocumentAnchorSection();
 		const view = cool.SimpleRectangle.fromCorePixels([
