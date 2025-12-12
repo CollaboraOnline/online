32632aad57232f74aed0afd450f9101de8828ab6
Andras Timar <andras.timar@collabora.com>
2025-12-12 12:10:38 +0100

    Remove 'Open' button from notebookbar, because it's obsoleted by Backstage
    Signed-off-by: Andras Timar <andras.timar@collabora.com>
    Change-Id: I5b676bd4e0b23f1e474cbcebc4ab1cc8dae71805

--- browser/src/control/Control.NotebookbarCalc.js
+++ browser/src/control/Control.NotebookbarCalc.js
@@ -140,21 +140,6 @@ window.L.Control.NotebookbarCalc = window.L.Control.NotebookbarWriter.extend({
 		var hasShare = this.map['wopi'].EnableShare;
 		var hasRevisionHistory = window.L.Params.revHistoryEnabled;
 
-		if (window.ThisIsTheWindowsApp || window.ThisIsTheQtApp) {
-			content.push({
-				'type': 'toolbox',
-				'children': [
-					{
-						'id': 'file-open',
-						'type': 'bigtoolitem',
-						'text': _UNO('.uno:Open'),
-						'command': '.uno:Open',
-						'accessibility': { focusBack: true,	combination: 'OP', de: null }
-					}
-				]
-			});
-		}
-
 		if (hasSave) {
 			content.push({
 				'type': 'container',
