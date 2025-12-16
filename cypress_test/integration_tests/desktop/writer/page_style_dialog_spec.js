565a2424855a80b9e319aa320f850aca4bdbad1a
Sahil Gautam <sahil.gautam@collabora.com>
2025-12-16 12:29:05 +0530

    Add dialog width test for the "Page Style" dialog
    Recent iconview expander work [1] introduced a regression which
    increased the width of iconviews in the dialogs increasing their
    overall width. This was caused by css selectors class selectors which
    targeted both the notebookbar iconview-expander iconviews and the dialog
    iconviews. [2] fixed the regression.
    
    [1]:
        - PR: make iconview expandable
        - URL: https://github.com/CollaboraOnline/online/pull/12654
    
    [2]:
        - PR: fix large iconviews in dialogs
        - URL: https://github.com/CollaboraOnline/online/pull/13788
        - FIX-COMMIT: 1514f8e22365c7bcc30cdbef276094f596d61cb4
    
    Signed-off-by: Sahil Gautam <sahil.gautam@collabora.com>
    Change-Id: Ifcbeb0d5f606ddd7361fe21d05ccbd6c8f57e357

---
+++ cypress_test/integration_tests/desktop/writer/page_style_dialog_spec.js
@@ -0,0 +1,27 @@
+/* global describe it cy beforeEach require */
+
+var helper = require('../../common/helper');
+var desktopHelper = require('../../common/desktop_helper')
+
+describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'Page Style dialog tests', function() {
+	beforeEach(function() {
+		helper.setupAndLoadDocument('writer/styles.odt');
+		cy.viewport(1920, 720);
+	});
+
+	/*
+	 * tests if the width of the 'Page Style dialog' is larger than a "reasonable"
+	 * width and if it's larger that means something is obviously wrong, probably
+	 * some css property.
+	 * `reasonableWidth` = width at the time of writing this test + 15px ;)
+	 */
+	it('Page Style dialog width', function() {
+		cy.cGet('#Format-tab-label').click();
+		desktopHelper.getNbIcon('PageDialog', 'Format').click();
+
+		cy.cGet('[id^="TemplateDialog"]')
+			.should('be.visible')
+			.invoke('width')
+			.should('be.greaterThan', 588).and('be.lessThan', 605);
+	});
+});
