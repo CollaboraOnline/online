b4538d7f9fcdf42d03fb542bba1fa30c9d87f380
Dennis Francis <dennis.francis@collabora.com>
2025-12-22 21:16:51 +0530

    browser: ts: DomUtil.js: convert setOpacity() and its dependency
    Signed-off-by: Dennis Francis <dennis.francis@collabora.com>
    Change-Id: I1df1b6308cdefa5716be9318dcafb286fb07b5e4

--- browser/src/dom/DomUtil.js
+++ browser/src/dom/DomUtil.js
@@ -6,39 +6,6 @@
 
 window.L.DomUtil = class DomUtil extends DomUtilBase {
 
-	static setOpacity(el, value) {
-
-		if ('opacity' in el.style) {
-			el.style.opacity = value;
-
-		} else if ('filter' in el.style) {
-			window.L.DomUtil._setOpacityIE(el, value);
-		}
-	}
-
-	static _setOpacityIE(el, value) {
-		var filter = false,
-		    filterName = 'DXImageTransform.Microsoft.Alpha';
-
-		// filters collection throws an error if we try to retrieve a filter that doesn't exist
-		try {
-			filter = el.filters.item(filterName);
-		} catch (e) {
-			// don't set opacity to 1 if we haven't already set an opacity,
-			// it isn't needed and breaks transparent pngs.
-			if (value === 1) { return; }
-		}
-
-		value = Math.round(value * 100);
-
-		if (filter) {
-			filter.Enabled = (value !== 100);
-			filter.Opacity = value;
-		} else {
-			el.style.filter += ' progid:' + filterName + '(opacity=' + value + ')';
-		}
-	}
-
 	static testProp(props) {
 
 		var style = document.documentElement.style;
