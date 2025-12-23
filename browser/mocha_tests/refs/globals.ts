07b0b15ec0dda05a7dfd908dccfe914afe076ecf
Dennis Francis <dennis.francis@collabora.com>
2025-12-24 02:09:21 +0530

    browser: mocha: use all typescript files to build main
    Signed-off-by: Dennis Francis <dennis.francis@collabora.com>
    Change-Id: I37c482f03e350b0fffe9d83823f510bcb9d3c6c5

--- browser/mocha_tests/refs/globals.ts
+++ browser/mocha_tests/refs/globals.ts
@@ -9,11 +9,19 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-var L: any = {
-    LOUtil: {},
-};
+/// <reference path="../../src/global.d.ts" />
+/// <reference path="../../src/control/jsdialog/Definitions.Types.ts" />
+/// <reference path="../../src/app/iface/Clipboard.Interface.ts" />
+/// <reference path="../../src/app/iface/Control.Interface.ts" />
+/// <reference path="../../src/app/iface/DocLayer.Interface.ts" />
+/// <reference path="../../src/app/iface/DownloadProgressInterface.ts" />
+/// <reference path="../../src/app/iface/Map.Interface.ts" />
+/// <reference path="../../src/app/iface/MapZoom.Interface.ts" />
+/// <reference path="../../src/app/iface/SocketTypes.Interface.ts" />
+/// <reference path="../../src/app/iface/TraceEvents.Interface.ts" />
+/// <reference path="../../src/app/iface/Welcome.Interface.ts" />
 
-var app: any = {
+(globalThis as any).app = {
     CSections: { Scroll: { name : 'scroll' } },
     roundedDpiScale : 1,
     canvasSize: null,
@@ -21,19 +29,64 @@ var app: any = {
     dpiScale: 1,
     twipsToPixels: 15,
     pixelsToTwips: 1 / 15,
-    sectionContainer: {}
+    sectionContainer: {},
+	console: globalThis.console,
+	socket: {},
 };
 
-(globalThis.document as any) = {
-	documentElement: {
-		style: {
-			transform: true,
-			transformOrigin: true,
-			transition: true,
-		}
+globalThis.window = (function() {
+	const jsdom = require('jsdom');
+	const dom = new jsdom.JSDOM('<html><body><div id="document-container"></div></body></html>');
+	return dom.window;
+})();
+
+globalThis.document = globalThis.window.document;
+
+(globalThis.window as any).prefs = {
+	canPersist: false,
+};
+
+(globalThis.window as any).app = (globalThis as any).app;
+
+(globalThis.window.L as any) = {
+	Browser: {
+		any3d: true,
+		cypressTest: false,
+		mac: false,
+		win: false,
+		lang: 'en',
+	},
+	LOUtil: {},
+	Map: {
+		include(input: any) {},
+		mergeOptions(input: any) {},
+		addInitHook(i1: any, i2: any, i3: any) {},
+	},
+	Handler: {
+		extend(input: any) {},
+	},
+
+	Control: class _Control {
+	},
+
+	control: {},
+};
+
+globalThis._ = (input: string) => input;
+(globalThis.ResizeObserver as any) = class _ResizeObserver {
+	constructor(firer: () => void) {
+	}
+	observe(container: HTMLElement) {
 	}
 };
+ globalThis.JSDialog = {};
 
-(globalThis.window as any) = {
-	document: {},
-}
+(globalThis as any).DOMPurify = require('dompurify');
+(globalThis as any).glMatrix = require('gl-matrix');
+
+String.locale = 'en';
+globalThis._UNO = function(i1: string, i2: string) {
+	return i2;
+};
+globalThis.SlideShow = {};
+(globalThis.window as any).getBorderStyleUNOCommand = () => {};
