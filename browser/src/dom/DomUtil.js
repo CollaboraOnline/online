ea3ec6589670234dbf9528d301e13b28bd9f2696
Dennis Francis <dennis.francis@collabora.com>
2025-12-26 22:28:20 +0530

    browser: ts: DomUtil.js: convert the remaining stuff in IIFE
    Signed-off-by: Dennis Francis <dennis.francis@collabora.com>
    Change-Id: Ia14a32bbd1cb85a46997664c9f8c34ee3709f062

--- browser/src/dom/DomUtil.js
+++ browser/src/dom/DomUtil.js
@@ -8,28 +8,3 @@ window.L.DomUtil = class DomUtil extends DomUtilBase {
 
 };
 
-
-(function () {
-
-	window.L.DomUtil.disableImageDrag = function () {
-		window.L.DomEvent.on(window, 'dragstart', window.L.DomEvent.preventDefault);
-	};
-	window.L.DomUtil.enableImageDrag = function () {
-		window.L.DomEvent.off(window, 'dragstart', window.L.DomEvent.preventDefault);
-	};
-
-	window.L.DomUtil.preventOutline = function (element) {
-		window.L.DomUtil.restoreOutline();
-		this._outlineElement = element;
-		this._outlineStyle = element.style.outline;
-		element.style.outline = 'none';
-		window.L.DomEvent.on(window, 'keydown', window.L.DomUtil.restoreOutline, this);
-	};
-	window.L.DomUtil.restoreOutline = function () {
-		if (!this._outlineElement) { return; }
-		this._outlineElement.style.outline = this._outlineStyle;
-		delete this._outlineElement;
-		delete this._outlineStyle;
-		window.L.DomEvent.off(window, 'keydown', window.L.DomUtil.restoreOutline, this);
-	};
-})();
