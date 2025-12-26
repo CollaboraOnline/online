d862dbd517f036484dcc3fd8236bb51c9374e421
Gökay Şatır <gokaysatir@collabora.com>
2025-12-26 12:35:22 +0300

    Issue: Paste special command shortcut on Calc doesn't work reliably.
    Fix: Send .uno:PasteSpecial command to core side.
    
    This fixes the issue for internal copy-paste special operations.
    
    Signed-off-by: Gökay Şatır <gokaysatir@collabora.com>
    Change-Id: I3d908c96627eeca9b2fbc6d8575707ddbb05b340

--- browser/src/map/handler/Map.KeyboardShortcuts.ts
+++ browser/src/map/handler/Map.KeyboardShortcuts.ts
@@ -307,6 +307,7 @@ keyboardShortcuts.definitions.set('default', new Array<ShortcutDescriptor>(
     new ShortcutDescriptor({ eventType: 'keydown', modifier: Mod.CTRL, key: 'f', dispatchAction: 'home-search' }),
     new ShortcutDescriptor({ eventType: 'keydown', modifier: Mod.CTRL, key: 'p', dispatchAction: 'print' }),
     new ShortcutDescriptor({ eventType: 'keydown', modifier: Mod.CTRL, key: 's', dispatchAction: 'save', dispatchData: 'keyboard' }),
+    new ShortcutDescriptor({ eventType: 'keydown', modifier: Mod.CTRL | Mod.SHIFT | Mod.ALT, key: 'V', unoAction: '.uno:PasteSpecial', platform: Platform.WINDOWS | Platform.LINUX | Platform.MAC | Platform.CHROMEOSAPP}),
 
     // Calc.
     new ShortcutDescriptor({ docType: 'spreadsheet', eventType: 'keydown', modifier: Mod.CTRL | Mod.SHIFT, key: 'PageUp' }),
