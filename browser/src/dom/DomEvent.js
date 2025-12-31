5f9ff9189328dc7c061d789d056e4ee01c3633f6
Dennis Francis <dennis.francis@collabora.com>
2025-12-31 16:59:37 +0530

    browser: ts: remove DomEvent.js
    Signed-off-by: Dennis Francis <dennis.francis@collabora.com>
    Change-Id: I409148992f8c37f3c9e0df8d2a0cd64315a6e9ec

--- browser/src/dom/DomEvent.js
+++
@@ -1,6 +0,0 @@
-/* -*- js-indent-level: 8 -*- */
-/*
- * window.L.DomEvent contains functions for working with DOM events.
- * Inspired by John Resig, Dean Edwards and YUI addEvent implementations.
- */
-
