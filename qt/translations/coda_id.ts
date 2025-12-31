9083753c46ab27908a7aaaa9ce948736c82eb9c1
Arif Budiman <arifpedia@gmail.com>
2025-12-31 17:11:46 +0100

    Translated using Weblate (Indonesian)
    Currently translated at 100.0% (25 of 25 strings)
    
    Translation: Collabora Online/Collabora Office Qt app
    Translate-URL: https://hosted.weblate.org/projects/collabora-online/collabora-office-qt-app/id/
    Signed-off-by: Andras Timar <andras.timar@collabora.com>
    Change-Id: Idd7769dd528e20b16c7212eddc8c7deeecb8bf1a

--- qt/translations/coda_id.ts
+++ qt/translations/coda_id.ts
@@ -6,114 +6,114 @@
     <message>
         <location filename="../coda-qt.cpp" line="289"/>
         <source>Print Document</source>
-        <translation type="unfinished"></translation>
+        <translation>Cetak Dokumen</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="297"/>
         <source>Select Printer:</source>
-        <translation type="unfinished"></translation>
+        <translation>Pilih Printer:</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="306"/>
         <location filename="../coda-qt.cpp" line="379"/>
         <source>Default Printer</source>
-        <translation type="unfinished"></translation>
+        <translation>Printer Default</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="311"/>
         <source>Print to File</source>
-        <translation type="unfinished"></translation>
+        <translation>Cetak ke File</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="315"/>
         <source>Enter file path...</source>
-        <translation type="unfinished"></translation>
+        <translation>Masukkan jalur file...</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="327"/>
         <source>Save Print Output As</source>
-        <translation type="unfinished"></translation>
+        <translation>Simpan Hasil Cetak Sebagai</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="329"/>
         <source>PDF Files (*.pdf);;All Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>File PDF (*.pdf);;Semua File (*)</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="345"/>
         <source>Print</source>
-        <translation type="unfinished"></translation>
+        <translation>Cetak</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="346"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Batal</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="370"/>
         <source>Print to File Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Kesalahan Cetak ke File</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="371"/>
         <source>Failed to save document to file. Please check the file path and permissions.</source>
-        <translation type="unfinished"></translation>
+        <translation>Gagal menyimpan dokumen ke file. Silakan periksa jalur file dan izin.</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="415"/>
         <source>Print Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Kesalahan Pencetakan</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="416"/>
         <source>Failed to print document. Please check your printer settings.</source>
-        <translation type="unfinished"></translation>
+        <translation>Gagal mencetak dokumen. Silakan periksa pengaturan printer Anda.</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="575"/>
         <source>Text Documents (*.odt);;All Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>Dokumen Teks (*.odt);;Semua File (*)</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="577"/>
         <source>Spreadsheets (*.ods);;All Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>Spreadsheet (*.ods);;Semua File (*)</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="579"/>
         <source>Presentations (*.odp);;All Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>Presentasi (*.odp);;Semua File (*)</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="581"/>
         <location filename="../coda-qt.cpp" line="1019"/>
         <source>All Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>Semua File (*)</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="585"/>
         <source>Save Document</source>
-        <translation type="unfinished"></translation>
+        <translation>Simpan Dokumen</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="902"/>
         <source>Open File</source>
-        <translation type="unfinished"></translation>
+        <translation>Buka File</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="903"/>
         <source>All Files (*);;Text Documents (*.odt *.ott *.doc *.docx *.rtf *.txt);;Spreadsheets (*.ods *.ots *.xls *.xlsx *.csv);;Presentations (*.odp *.otp *.ppt *.pptx)</source>
-        <translation type="unfinished"></translation>
+        <translation>Semua File (*);;Dokumen Teks (*.odt *.ott *.doc *.docx *.rtf *.txt);;Spreadsheet (*.ods *.ots *.xls *.xlsx *.csv);;Presentasi (*.odp *.otp *.ppt *.pptx)</translation>
     </message>
     <message>
         <location filename="../coda-qt.cpp" line="1017"/>
         <source>Export As</source>
-        <translation type="unfinished"></translation>
+        <translation>Ekspor Sebagai</translation>
     </message>
     <message>
         <location filename="../WebView.cpp" line="205"/>
         <source>Presenting</source>
-        <translation type="unfinished"></translation>
+        <translation>Sedang Menyajikan</translation>
     </message>
 </context>
 <context>
@@ -121,17 +121,17 @@
     <message>
         <location filename="../WebView.cpp" line="121"/>
         <source>Unsaved Changes</source>
-        <translation type="unfinished"></translation>
+        <translation>Perubahan Belum Disimpan</translation>
     </message>
     <message>
         <location filename="../WebView.cpp" line="122"/>
         <source>The document has unsaved changes. Do you want to close anyway?</source>
-        <translation type="unfinished"></translation>
+        <translation>Dokumen memiliki perubahan yang belum disimpan. Apakah Anda tetap ingin menutupnya?</translation>
     </message>
     <message>
         <location filename="../WebView.cpp" line="533"/>
         <source>Start</source>
-        <translation type="unfinished"></translation>
+        <translation>Mulai</translation>
     </message>
 </context>
 </TS>
