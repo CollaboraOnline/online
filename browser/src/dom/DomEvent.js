6118ef69acf25045c6514e05791ed87663a59d3d
Dennis Francis <dennis.francis@collabora.com>
2025-12-31 15:49:57 +0530

    browser: ts: DomEvent.js: convert _filterClick()
    Signed-off-by: Dennis Francis <dennis.francis@collabora.com>
    Change-Id: Id5a1eb46a2692ae95c996f83735885e5faa41573

--- browser/src/dom/DomEvent.js
+++ browser/src/dom/DomEvent.js
@@ -8,27 +8,6 @@
 
 class DomEventDerived extends DomEvent {
 
-	// this is a horrible workaround for a bug in Android where a single touch triggers two click events
-	static _filterClick(e, handler) {
-		const timeStamp = (e.timeStamp || e.originalEvent.timeStamp);
-		const elapsed = window.L.DomEvent._lastClick && (timeStamp - window.L.DomEvent._lastClick);
-
-		// are they closer together than 500ms yet more than 100ms?
-		// Android typically triggers them ~300ms apart while multiple listeners
-		// on the same event should be triggered far faster;
-		// or check if click is simulated on the element, and if it is, reject any non-simulated events
-
-		if ((elapsed && elapsed > 100 && elapsed < 500) || (e.target._simulatedClick && !e._simulated)) {
-			window.L.DomEvent.stop(e);
-			return;
-		}
-		window.L.DomEvent._lastClick = timeStamp;
-
-		handler(e);
-	}
 }
-
-DomEventDerived.addListener = DomEventDerived.on;
-DomEventDerived.removeListener = DomEventDerived.off;
 
 window.L.DomEvent = DomEventDerived;
