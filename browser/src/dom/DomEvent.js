b986a822bbeda887f5cf45f98f46bb4e502161ac
Dennis Francis <dennis.francis@collabora.com>
2025-12-31 16:56:29 +0530

    browser: ts: move last bits from DomEvent.js
    Signed-off-by: Dennis Francis <dennis.francis@collabora.com>
    Change-Id: I55dd1780a11fbf2690db38821639102db263bbdf

--- browser/src/dom/DomEvent.js
+++ browser/src/dom/DomEvent.js
@@ -1,13 +1,6 @@
 /* -*- js-indent-level: 8 -*- */
-/* global DomEvent */
 /*
  * window.L.DomEvent contains functions for working with DOM events.
  * Inspired by John Resig, Dean Edwards and YUI addEvent implementations.
  */
 
-
-class DomEventDerived extends DomEvent {
-
-}
-
-window.L.DomEvent = DomEventDerived;
