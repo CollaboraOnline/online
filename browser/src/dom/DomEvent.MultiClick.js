35d64cee335dd146afa7bcdf1cf9117a165ba78a
Dennis Francis <dennis.francis@collabora.com>
2025-12-31 16:51:50 +0530

    browser: ts: remove DomEvent.MultiClick.js
    Signed-off-by: Dennis Francis <dennis.francis@collabora.com>
    Change-Id: Ic806b9ce57f27c071fb894793f2e905b0110ce1d

--- browser/src/dom/DomEvent.MultiClick.js
+++
@@ -1,6 +0,0 @@
-/* -*- js-indent-level: 8 -*- */
-/*
- * Extends the event handling code with triple and quadruple click support
- * This is vaguely based on the DomEvent.DoubleTap implementation.
- */
-
