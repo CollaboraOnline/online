84ee537dce6b945b8b5127b59c312f2e33860233
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2026-01-03 15:57:19 -0500

    wsd: test: add new log Prefix tests
    Change-Id: Ic0641207717200a6334807d3ec949263a452f69f
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- test/LoggingWhiteBoxTests.cpp
+++ test/LoggingWhiteBoxTests.cpp
@@ -25,21 +25,191 @@
 #include <cppunit/TestAssert.h>
 #include <cppunit/extensions/HelperMacros.h>
 
+#include <chrono>
+
 using namespace std::literals;
 
 /// Logging unit-tests.
 class LoggingTests : public CPPUNIT_NS::TestFixture
 {
     CPPUNIT_TEST_SUITE(LoggingTests);
+    CPPUNIT_TEST(testLogPrefix);
     CPPUNIT_TEST(testGetTimeForLog);
     CPPUNIT_TEST(testIso8601Time);
     CPPUNIT_TEST(testClockAsString);
     CPPUNIT_TEST_SUITE_END();
 
+    void testLogPrefix();
     void testGetTimeForLog();
     void testIso8601Time();
     void testClockAsString();
 };
+
+namespace
+{
+/// Convert the given time to system_clock, rounded to milliseconds.
+/// We don't care about microsecond-precision for testing, but we do care about
+/// stability, which is compromised by the imprecision of the conversion
+/// we must do between the epoch and the system_clock.
+std::chrono::time_point<std::chrono::system_clock> getSystemTimeRoundedToMillis(time_t t)
+{
+    const auto sys = std::chrono::system_clock::from_time_t(t);
+    const auto now = Util::convertChronoClock<std::chrono::system_clock::time_point>(sys);
+    return std::chrono::round<std::chrono::milliseconds>(now);
+}
+} // namespace
+
+void LoggingTests::testLogPrefix()
+{
+    constexpr std::string_view testname = __func__;
+
+    constexpr std::string_view level = "XYZ";
+
+    // getTimeForLog returns the time in local timezone.
+    // To get reliable tests across different timezones, we use GMT.
+    const char* tz = ::getenv("TZ");
+    const std::string timezoneName = (tz ? tz : "");
+    ::setenv("TZ", "GMT", 1);
+    tzset();
+
+    // Create a Prefix instance *after* setting the timezone, to make it effective.
+    Log::Prefix prefix;
+
+    constexpr time_t t = 1760000000;
+    const auto now = getSystemTimeRoundedToMillis(t);
+
+    const auto getLogDateTime =
+        [&prefix, &testname, level](const std::chrono::time_point<std::chrono::system_clock> tp)
+    {
+        char buffer[256];
+        const auto str = prefix.update(level, tp);
+        LOK_ASSERT_EQUAL_STR(Log::prefixReference(tp, buffer, level), str);
+        const auto firstPos = str.find_first_of(' ');
+        const auto secondPos = str.find_first_of(' ', firstPos + 1);
+        const auto thirdPos = str.find_first_of(' ', secondPos + 1);
+        return str.substr(firstPos + 1, thirdPos - firstPos - 1);
+    };
+
+    auto log = prefix.update("XYZ", now);
+    LOK_ASSERT_EQUAL_STR("tst", log.substr(0, 3));
+    LOK_ASSERT_EQUAL_STR("] XYZ  ", log.substr(log.find_last_of(']')));
+
+    LOK_ASSERT_EQUAL_STR("2025-10-09 08:53:20.000000", getLogDateTime(now));
+
+    // Past dates.
+    LOK_ASSERT_EQUAL_STR("2025-10-09 08:53:19.631000", getLogDateTime(now - 369ms));
+
+    LOK_ASSERT_EQUAL_STR("2025-10-09 08:53:14.631000", getLogDateTime(now - 5s - 369ms));
+
+    LOK_ASSERT_EQUAL_STR("2025-10-09 08:46:14.631000", getLogDateTime(now - 7min - 5s - 369ms));
+
+    LOK_ASSERT_EQUAL_STR("2025-10-08 20:46:14.631000",
+                         getLogDateTime(now - 12h - 7min - 5s - 369ms));
+
+    LOK_ASSERT_EQUAL_STR(
+        "2023-10-08 20:46:14.631000",
+        getLogDateTime(now - std::chrono::days(2 * 365 + 1) - 12h - 7min - 5s - 369ms));
+    LOK_ASSERT_EQUAL_STR(
+        "2025-08-08 20:46:14.631000",
+        getLogDateTime(now - std::chrono::days(31 + 30) - 12h - 7min - 5s - 369ms));
+    LOK_ASSERT_EQUAL_STR("2025-10-06 20:46:14.631000",
+                         getLogDateTime(now - std::chrono::days(2) - 12h - 7min - 5s - 369ms));
+    LOK_ASSERT_EQUAL_STR("2020-06-06 20:46:14.631000",
+                         getLogDateTime(now - std::chrono::days(5 * 365 + 2) -
+                                        std::chrono::days(4 * 30) - std::chrono::days(3) - 12h -
+                                        7min - 5s - 369ms));
+
+    // Future dates.
+    LOK_ASSERT_EQUAL_STR("2025-10-09 08:53:20.369000", getLogDateTime(now + 369ms));
+
+    LOK_ASSERT_EQUAL_STR("2025-10-09 08:53:25.369000", getLogDateTime(now + 5s + 369ms));
+
+    LOK_ASSERT_EQUAL_STR("2025-10-09 09:00:25.369000", getLogDateTime(now + 7min + 5s + 369ms));
+
+    LOK_ASSERT_EQUAL_STR("2025-10-09 21:00:25.369000",
+                         getLogDateTime(now + 12h + 7min + 5s + 369ms));
+
+    LOK_ASSERT_EQUAL_STR(
+        "2027-10-08 21:00:25.369000",
+        getLogDateTime(now + std::chrono::days(2 * 365 - 1) + 12h + 7min + 5s + 369ms));
+    LOK_ASSERT_EQUAL_STR("2025-12-08 21:00:25.369000",
+                         getLogDateTime(now + std::chrono::days(2 * 30) + 12h + 7min + 5s + 369ms));
+    LOK_ASSERT_EQUAL_STR("2025-10-11 21:00:25.369000",
+                         getLogDateTime(now + std::chrono::days(2) + 12h + 7min + 5s + 369ms));
+    LOK_ASSERT_EQUAL_STR("2031-02-08 21:00:25.369000",
+                         getLogDateTime(now + std::chrono::days(5 * 365) +
+                                        std::chrono::days(4 * 30) + std::chrono::days(3) + 12h +
+                                        7min + 5s + 369ms));
+
+    {
+        // Roll the year over by 1 second.
+        auto tp = getSystemTimeRoundedToMillis(1767225599); // Dec 31 2025 23:59:59 GMT+0000
+
+        char buffer[256];
+        const auto str = prefix.update(level, tp);
+        LOK_ASSERT_EQUAL_STR(Log::prefixReference(tp, buffer, level), str);
+
+        Log::Prefix prefix2;
+        LOK_ASSERT_EQUAL_STR(buffer, prefix2.update(level, tp));
+
+        tp += std::chrono::seconds(1);
+        LOK_ASSERT_EQUAL_STR(Log::prefixReference(tp, buffer, level), prefix2.update(level, tp));
+    }
+
+    // Randomized comparative tests.
+    for (int i = 0; i < 100'000; ++i)
+    {
+        const time_t time = Util::rng::getNext() % 2'000'000'000;
+        const auto tp = getSystemTimeRoundedToMillis(time) +
+                        std::chrono::nanoseconds(Util::rng::getNext() % 1'000'000'000);
+
+        char buffer[256];
+        const auto str = prefix.update(level, tp);
+        LOK_ASSERT_EQUAL_STR(Log::prefixReference(tp, buffer, level), str);
+
+        Log::Prefix prefix2;
+        LOK_ASSERT_EQUAL_STR(buffer, prefix2.update(level, tp));
+    }
+
+    {
+        ::setenv("TZ", "EST", 1);
+        tzset();
+
+        auto tp = getSystemTimeRoundedToMillis(1767225599); // Dec 31 2025 23:59:59 GMT+0000
+        char buffer[256];
+        const auto str = prefix.update(level, tp);
+        LOK_ASSERT_EQUAL_STR(Log::prefixReference(tp, buffer, level), str);
+    }
+
+#if 0
+    // Benchmark the old implementation.
+    {
+        char buffer[256];
+        Util::Stopwatch sw;
+        for (int i = 0; i < 1'000'000; ++i)
+        {
+            const auto tp = getSystemTimeRoundedToMillis(i * 32);
+            [[maybe_unused]] const auto str = Log::prefixReference(tp, buffer, level);
+        }
+
+        LOG_INF("Old prefix took: " << sw.elapsed<std::chrono::microseconds>());
+    }
+
+    // Benchmark the old implementation.
+    {
+        Util::Stopwatch sw;
+        for (int i = 0; i < 1'000'000; ++i)
+        {
+            const auto tp = getSystemTimeRoundedToMillis(i * 32);
+            [[maybe_unused]] const auto str = prefix.update(level, tp);
+        }
+
+        LOG_INF("New prefix took: " << sw.elapsed<std::chrono::microseconds>());
+    }
+#endif
+
+    ::setenv("TZ", timezoneName.data(), 1); // Restore the timeezone.
+}
 
 void LoggingTests::testGetTimeForLog()
 {
