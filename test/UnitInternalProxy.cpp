db8845a84eaaee92f851bf0d069ba310a159cdac
Rashesh Padia <rashesh.padia@collabora.com>
2026-01-07 13:19:38 +0530

    wsd: unit: added conneciton failure test for internal proxy
    Signed-off-by: Rashesh Padia <rashesh.padia@collabora.com>
    Change-Id: If5b3d09cfc3aab1a2b8b406b0e8d88b082597233

--- test/UnitInternalProxy.cpp
+++ test/UnitInternalProxy.cpp
@@ -36,6 +36,38 @@ class EchoServerFactory final : public SocketFactory
     }
 };
 
+/// Simple handler that captures incoming data for verification
+class CaptureHandler : public SimpleSocketHandler
+{
+    std::weak_ptr<StreamSocket> _socket;
+    std::string _received;
+
+public:
+    void onConnect(const std::shared_ptr<StreamSocket>& socket) override
+    {
+        _socket = socket;
+        setLogContext(socket->getFD());
+    }
+
+    void handleIncomingMessage(SocketDisposition&) override
+    {
+        auto socket = _socket.lock();
+        if (socket)
+        {
+            auto& buf = socket->getInBuffer();
+            _received.append(buf.data(), buf.size());
+            buf.clear();
+        }
+    }
+
+    int getPollEvents(std::chrono::steady_clock::time_point, int64_t&) override { return POLLIN; }
+
+    void performWrites(std::size_t) override {}
+    void onDisconnect() override {}
+
+    const std::string& getReceived() const { return _received; }
+};
+
 /// Test the internal ProxyPoll functionality.
 class UnitInternalProxy : public UnitWSD
 {
@@ -123,6 +155,8 @@ public:
                 if (_serverPoll)
                     _serverPoll->stop();
                 exitTest(TestResult::Ok);
+                // Trigger next test since we have no DocBroker
+                UnitWSD::get().DocBrokerDestroy("");
                 break;
             case Phase::Done:
                 break;
@@ -188,6 +222,92 @@ private:
     }
 };
 
-UnitBase* unit_create_wsd(void) { return new UnitInternalProxy(); }
+/// Test that async connect failure returns 502 Bad Gateway to client
+class UnitInternalProxyConnectError : public UnitWSD
+{
+    STATE_ENUM(Phase, Init, WaitResponse, Done) _phase;
+
+    std::shared_ptr<SocketPoll> _clientPoll;
+    std::shared_ptr<CaptureHandler> _captureHandler;
+
+public:
+    UnitInternalProxyConnectError()
+        : UnitWSD("UnitInternalProxyConnectError")
+        , _phase(Phase::Init)
+        , _clientPoll(std::make_shared<SocketPoll>("error_client_poll"))
+    {
+        setTimeout(10s);
+    }
+
+    void invokeWSDTest() override
+    {
+        switch (_phase)
+        {
+            case Phase::Init:
+            {
+                TRANSITION_STATE(_phase, Phase::WaitResponse);
+
+                _clientPoll->startThread();
+
+                // Create socket pair for testing (client <-> proxy)
+                int fds[2];
+                if (socketpair(AF_UNIX, SOCK_STREAM | SOCK_NONBLOCK, 0, fds) < 0)
+                {
+                    LOK_ASSERT_FAIL("Failed to create socket pair: " << strerror(errno));
+                    return;
+                }
+
+                // Client end - captures the 502 response
+                _captureHandler = std::make_shared<CaptureHandler>();
+                auto clientSocket =
+                    StreamSocket::create<StreamSocket>(std::string(), fds[0], Socket::Type::Unix,
+                                                       false, HostType::Other, _captureHandler);
+
+                // Proxy end - this is what we give to startPump
+                auto proxyHandler = std::make_shared<ServerRequestHandler>();
+                auto proxySocket =
+                    StreamSocket::create<StreamSocket>(std::string(), fds[1], Socket::Type::Unix,
+                                                       false, HostType::Other, proxyHandler);
+
+                _clientPoll->insertNewSocket(clientSocket);
+
+                Poco::Net::HTTPRequest request("GET", "/test", "HTTP/1.1");
+                request.setHost("127.0.0.1:59999");
+
+                TST_LOG("Calling ProxyPoll::startPump with invalid target port 59999");
+                ProxyPoll::startPump(proxySocket, "127.0.0.1", 59999, request);
+                break;
+            }
+            case Phase::WaitResponse:
+            {
+                const std::string& received = _captureHandler->getReceived();
+                if (!received.empty())
+                {
+                    TST_LOG("Received response: "
+                            << received.substr(0, std::min(received.size(), size_t(100))));
+
+                    LOK_ASSERT_MESSAGE("Expected 502 Bad Gateway response",
+                                       received.find("502") != std::string::npos);
+
+                    TRANSITION_STATE(_phase, Phase::Done);
+
+                    _clientPoll->stop();
+
+                    exitTest(TestResult::Ok);
+                    UnitWSD::get().DocBrokerDestroy("");
+                }
+                break;
+            }
+            case Phase::Done:
+                break;
+        }
+    }
+};
+
+UnitBase** unit_create_wsd_multi(void)
+{
+    return new UnitBase*[3]{ new UnitInternalProxy(), new UnitInternalProxyConnectError(),
+                             nullptr };
+}
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
