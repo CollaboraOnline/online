bc72b0aa5b1ca8ba0a9ef628796f5577657f3ba9
Mohit Marathe <mohit.marathe@collabora.com>
2026-01-09 17:47:07 +0530

    cypress: fix warning 'expect' is defined but never used
    Signed-off-by: Mohit Marathe <mohit.marathe@collabora.com>
    Change-Id: I775f5f8037d52ed2b1fc6faf264aed586432d270

--- cypress_test/integration_tests/desktop/calc/cell_edit_spec.js
+++ cypress_test/integration_tests/desktop/calc/cell_edit_spec.js
@@ -1,4 +1,4 @@
-/* global describe it cy beforeEach expect require */
+/* global describe it cy beforeEach require */
 
 var helper = require('../../common/helper');
 var desktopHelper = require('../../common/desktop_helper');
