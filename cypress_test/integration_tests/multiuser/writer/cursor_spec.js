addd98ec5e4ba3cf7c54149ac82708a2a944c369
Miklos Vajna <vmiklos@collabora.com>
2026-01-15 13:18:20 +0100

    cool#14093 add testcase
    cy.cGet('#document-canvas').trigger('mousemove', ...) doesn't seem to
    actually trigger the new onMouseEnter(), so do it manually.
    
    Signed-off-by: Miklos Vajna <vmiklos@collabora.com>
    Change-Id: I05636665264321b72a7d7911fac0f0671f1ca3fc

--- cypress_test/integration_tests/multiuser/writer/cursor_spec.js
+++ cypress_test/integration_tests/multiuser/writer/cursor_spec.js
@@ -1,4 +1,4 @@
-/* global describe it cy beforeEach require */
+/* global describe it cy beforeEach require expect */
 
 var helper = require('../../common/helper');
 var desktopHelper = require('../../common/desktop_helper');
@@ -9,6 +9,63 @@ describe(['tagmultiuser'], 'Check cursor and view behavior', function() {
 	beforeEach(function() {
 		helper.setupAndLoadDocument('writer/cursor_jump.odt', true);
 		desktopHelper.switchUIToNotebookbar();
+	});
+
+	it('Show user name on mouse hover over other view cursor', function() {
+		// Given a view cursor for the second iframe which is currently not visible:
+		// Move cursor in first view to make sure it was shown already once in the second
+		// view.
+		cy.cSetActiveFrame('#iframe1');
+		helper.typeIntoDocument('{rightArrow}');
+
+		cy.cSetActiveFrame('#iframe2');
+		// Wait for the cursor header to appear (shown when cursor moves), then
+		// wait for it to auto-hide.
+		cy.cGet('#canvas-container .html-object-section').should('contain.text', 'LocalUser');
+		cy.cGet('#canvas-container .html-object-section').should('not.contain.text', 'LocalUser');
+
+		// When moving the mouse over the view cursor in the second view:
+		cy.getFrameWindow().then((win) => {
+			const cursorSections = win.app.sectionContainer.sections.filter(
+				(s) => s.name.startsWith('OtherViewCursor ')
+			);
+			expect(cursorSections.length).to.be.greaterThan(0);
+
+			const cursorSection = cursorSections[0];
+			// Get the cursor's position in CSS pixels relative to canvas.
+			const x = cursorSection.myTopLeft[0] / win.app.dpiScale;
+			const y = cursorSection.myTopLeft[1] / win.app.dpiScale;
+
+			// Get canvas bounding rect to calculate viewport-relative coordinates.
+			const canvas = win.document.getElementById('document-canvas');
+			const rect = canvas.getBoundingClientRect();
+
+			// Dispatch mouse events manually, which work on the canvas.
+			const mouseEnterEvent = new win.MouseEvent('mouseenter', {
+				clientX: rect.left + x,
+				clientY: rect.top + y,
+				bubbles: true,
+				cancelable: true,
+				view: win
+			});
+			canvas.dispatchEvent(mouseEnterEvent);
+			const mouseMoveEvent = new win.MouseEvent('mousemove', {
+				clientX: rect.left + x,
+				clientY: rect.top + y,
+				bubbles: true,
+				cancelable: true,
+				view: win
+			});
+			canvas.dispatchEvent(mouseMoveEvent);
+		});
+
+		// Then make sure that the cursor header appears on mouse enter:
+		// Without the accompanying fix in place, this test would have failed with:
+		// Timed out retrying after 10000ms: expected '[
+		// <div.html-object-section.text-selection-handle-start>, 3 more... ]' to contain
+		// text 'LocalUser', but the text was ''
+		cy.cGet('#canvas-container .html-object-section')
+			.should('contain.text', 'LocalUser');
 	});
 
 	it('Do not center the view if cursor is already visible', function() {
