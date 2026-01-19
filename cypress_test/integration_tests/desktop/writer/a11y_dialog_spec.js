f8af864a14f4ecda0fa2b47d72661d54001d884e
Caolán McNamara <caolan.mcnamara@collabora.com>
2026-01-19 17:17:43 +0000

    test Line Dialog a11y
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: Ie7df2e54be037777a77a45d699e67ade505b1d0e

--- cypress_test/integration_tests/desktop/writer/a11y_dialog_spec.js
+++ cypress_test/integration_tests/desktop/writer/a11y_dialog_spec.js
@@ -219,6 +219,21 @@ describe(['tagdesktop'], 'Accessibility Writer Dialog Tests', { testIsolation: f
         helper.typeIntoDocument('{esc}');
     });
 
+    it('Line dialog', function () {
+        cy.then(() => {
+            win.app.map.sendUnoCommand('.uno:Line');
+        });
+
+        cy.cGet('#test-div-shapeHandlesSection').should('exist');
+
+        cy.then(() => {
+            win.app.map.sendUnoCommand('.uno:FormatLine');
+        });
+        handleDialog(win, 1);
+        // exit line mode
+        helper.typeIntoDocument('{esc}');
+    });
+
     allWriterDialogs.forEach(function (command) {
         if (missingContextDialogs.includes(command)) {
             it.skip(`Dialog ${command} (missing context)`, function () {});
