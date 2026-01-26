d01d0068f55e0cd374b8d182168cbcb361838836
Andras Timar <andras.timar@collabora.com>
2026-01-26 15:30:42 +0100

    Revert "Revert "Separate the ui and help translations for the mobile & desktop apps""
    This reverts commit dd1f9ef8a5e0e28f4bc767b93d06e360b815b6b5.
    That was a mistake, nothing was wrong with the original commit.
    
    Signed-off-by: Andras Timar <andras.timar@collabora.com>
    Change-Id: I36c6b38acc0930eb6e005479f7cf7003bbeec3a5

--- browser/util/create-l10n-all-js.pl
+++ browser/util/create-l10n-all-js.pl
@@ -1,7 +1,40 @@
 #!/bin/perl -w
+#
+# Copyright the Collabora Online contributors.
+#
+# SPDX-License-Identifier: MPL-2.0
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
 
 use strict;
+use warnings;
 use File::Basename;
+
+my ($decode_json, $encode_json, $json_pretty);
+
+# Find the JSON module
+BEGIN {
+    # Prefer XS, fall back to pure-Perl JSON::PP
+    eval {
+        require Cpanel::JSON::XS;
+        Cpanel::JSON::XS->import(qw(decode_json encode_json));
+        $json_pretty = Cpanel::JSON::XS->new->utf8->canonical->pretty;
+        1;
+    } or eval {
+        require JSON::XS;
+        JSON::XS->import(qw(decode_json encode_json));
+        $json_pretty = JSON::XS->new->utf8->canonical->pretty;
+        1;
+    } or eval {
+        require JSON::PP;
+        JSON::PP->import(qw(decode_json encode_json));
+        $json_pretty = JSON::PP->new->utf8->canonical->pretty;
+        1;
+    } or die "Need a JSON module (Cpanel::JSON::XS / JSON::XS / JSON::PP)\n";
+}
 
 my $path = dirname(dirname($0)) . "/";
 
@@ -12,19 +45,55 @@ sub readwhole($) {
     return <$fh>;
 }
 
-sub insert($) {
-    my ($locale) = @_;
-    my $ui = readwhole($path . "po/ui-$locale.po.json");
-    my $help = readwhole($path . "po/help-$locale.po.json");
-    # Different convention: Change underscore to hyphen.
-    $locale =~ s/_/-/;
-    my $uno = readwhole($path . "l10n/uno/$locale.json");
-    my $locore = readwhole($path . "l10n/locore/$locale.json");
-    # Merge the fields of all three objects into one. The result of
-    # po2json.py starts with "{" not followed by a newline and ends
-    # with a "}" without any final newline. The json files that are in
-    # the repo start with "{\n" and end with "}\n".
-    return substr($ui, 0, length($ui)-1) . ",\n" . substr($help, 1, length($help)-2) . ",\n" . substr($uno, 2, length($uno)-4) . ",\n" . substr($locore, 2, length($locore)-3);
+# Merge one or more JSON translation files into a single JSON object.
+# Later files win on key collisions; we warn only when the value actually changes.
+sub insert(@) {
+    my (@relfiles) = @_;
+    die "insert(): no files specified\n" unless @relfiles;
+
+    my %merged;
+    my %seen_in;   # key -> first file where it appeared
+
+    for my $rel (@relfiles) {
+        my $raw = readwhole($path . $rel);
+        my $obj = decode_json($raw);
+
+        die "insert(): $rel is not a JSON object\n" unless ref($obj) eq 'HASH';
+
+        for my $k (keys %$obj) {
+            # Warn if/when we've seen the key already in another file
+            if (exists $merged{$k}) {
+                my $prev = $seen_in{$k} // '(unknown)';
+
+                # Also, warn only if the value changes (stringify via JSON for a simple deep-ish compare).
+                my $old_json = encode_json($merged{$k});
+                my $new_json = encode_json($obj->{$k});
+
+                if ($old_json ne $new_json) {
+                    my $old_s = _short($old_json, 140);
+                    my $new_s = _short($new_json, 140);
+                    warn "insert(): key '$k' overwritten ($prev -> $rel): $old_s -> $new_s\n";
+                }
+            } else {
+                $seen_in{$k} = $rel;
+            }
+
+            $merged{$k} = $obj->{$k};
+        }
+    }
+
+    # Stable output helps diffs and reproducibility across platforms.
+    return $json_pretty->encode(\%merged);
+}
+
+# Make a string safe for one-line warnings by escaping newlines and truncating.
+sub _short($$) {
+    my ($s, $max) = @_;
+
+    # Normalize CRLF/CR/LF to a literal "\n" so warn() stays on one line.
+    $s =~ s/\r\n|\r|\n/\\n/g;
+
+    return (length($s) > $max) ? (substr($s, 0, $max) . '…') : $s;
 }
 
 # The list of locales handled in the JavaScript we output below is
@@ -63,15 +132,23 @@ my %aliases = (
 );
 
 for my $lang (@simple) {
+    my $lang_hyphen = $lang;
+    $lang_hyphen =~ s/_/-/g;
+
     print "else if (onlylang == '$lang') {\n";
-    print "    window.LOCALIZATIONS = " . insert($lang) . ";\n";
+    print "    window.LOCALIZATIONS = " . insert("po/ui-$lang.po.json", "l10n/uno/$lang_hyphen.json", "l10n/locore/$lang_hyphen.json") . ";\n";
+    print "    window.LOCALIZATIONS_HELP = " . insert("po/help-$lang.po.json") . ";\n";
     print "}\n";
 }
 
 for my $lang (sort keys %aliases) {
     my $cond = join(' || ', map { "window.LANG == '$_'" } @{$aliases{$lang}});
+    my $lang_hyphen = $lang;
+    $lang_hyphen =~ s/_/-/g;
+
     print "else if ($cond) {\n";
-    print "    window.LOCALIZATIONS = " . insert($lang) . ";\n";
+    print "    window.LOCALIZATIONS = " . insert("po/ui-$lang.po.json", "l10n/uno/$lang_hyphen.json", "l10n/locore/$lang_hyphen.json") . ";\n";
+    print "    window.LOCALIZATIONS_HELP = " . insert("po/help-$lang.po.json") . ";\n";
     print "}\n";
 }
 
