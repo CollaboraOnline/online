53eee62914e613a558139e7a8aaedfa2553e1833
Tor Lillqvist <tml@collabora.com>
2026-02-03 15:54:12 +0100

    Add a very early start on automated testing of CODA-W
    Don't even bother trying to use this yet. Just committing so that I
    don't lose it accidentally.
    
    Signed-off-by: Tor Lillqvist <tml@collabora.com>
    Change-Id: Ia879ca6aa73512d1309114d1fb84c7a060b1691e

---
+++ windows/coda/WebDriverThing/Program.cs
@@ -0,0 +1,47 @@
+﻿// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*-
+
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Automation;
+
+namespace WebDriverThing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EdgeDriverService service = EdgeDriverService.CreateDefaultService();
+            service.EnableVerboseLogging = true;
+
+            EdgeOptions eo = new EdgeOptions();
+
+            eo.UseWebView = true;
+            eo.DebuggerAddress = "localhost:9222";
+            eo.BinaryLocation = "C:\\Users\\tml\\bin\\msedgedriver.exe";
+
+            EdgeDriver driver = new EdgeDriver(service, eo);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
+
+            var openButton = driver.FindElement(OpenQA.Selenium.By.Id("backstage-open"));
+            openButton.Click();
+
+            var window = AutomationElement.RootElement.FindFirst(
+                TreeScope.Children,
+                new PropertyCondition(AutomationElement.NameProperty, "Open")
+            );
+
+            var okButton = window.FindFirst(
+                TreeScope.Descendants,
+                new PropertyCondition(AutomationElement.AutomationIdProperty, "1")
+            );
+
+            Thread.Sleep(20000);
+
+            driver.Quit();
+        }
+    }
+}
