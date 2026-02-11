dc3333e3a47c5143ef11cab4dea949005bd7ef46
Tor Lillqvist <tml@collabora.com>
2026-02-11 08:56:37 +0100

    More hacking on WebDriverThing
    Signed-off-by: Tor Lillqvist <tml@collabora.com>
    Change-Id: Idaf370ff06ced7c0065962c6f701463acbdf309b

--- windows/coda/WebDriverThing/Program.cs
+++ windows/coda/WebDriverThing/Program.cs
@@ -12,6 +12,63 @@ namespace WebDriverThing
 {
     internal class Program
     {
+        static void fatal(string message)
+        {
+            Console.WriteLine(message);
+            System.Environment.Exit(1);
+        }
+
+        static void openFile(EdgeDriver driver, string pathname)
+        {
+            var openButton = driver.FindElement(OpenQA.Selenium.By.Id("backstage-open"));
+            if (openButton == null)
+                fatal("Could not find the 'Open' button on the initial backstage");
+
+            openButton.Click();
+
+            // The File Open dialog is a native one so we need to use the System.Windows.Automation
+            // API to manipulate it.
+
+            // How long should we wait for the File Open dialog to appear? Let's try 5 s.
+            Thread.Sleep(5000);
+
+            var openDialog = AutomationElement.RootElement.FindFirst(
+                TreeScope.Children,
+                new PropertyCondition(AutomationElement.NameProperty, "Open")
+            );
+
+            if (openDialog == null)
+                fatal("File Open dialog did not show up");
+
+            var openDialogOpenButton = openDialog.FindFirst(
+                TreeScope.Descendants,
+                new PropertyCondition(AutomationElement.AutomationIdProperty, "1")
+            );
+
+            if (openDialogOpenButton == null)
+                fatal("No 'Open' button in the File Open dialog");
+
+            var fileNameField = openDialog.FindFirst(
+                TreeScope.Descendants,
+                new PropertyCondition(AutomationElement.AutomationIdProperty, "1148")
+            );
+
+            if (fileNameField == null)
+                fatal("No file name field in the File Open dialog");
+
+            var valuePattern = (ValuePattern)fileNameField.GetCurrentPattern(ValuePattern.Pattern);
+            valuePattern.SetValue(pathname);
+
+            // This does not seem to work. Would love to figure out some bette, sane, and reliable
+            // way to do this.
+            // ((InvokePattern)openDialogOpenButton.GetCurrentPattern(InvokePattern.Pattern)).Invoke();
+
+            // This works, and is mad and scary! Just pretend Enter is pressed to whatever window
+            // that has the focus. This means that you have to be very careful when debugging this
+            // code. You can't let VS have the focus when this line is executed.
+            System.Windows.Forms.SendKeys.SendWait("{ENTER}");
+        }
+
         static void Main(string[] args)
         {
             EdgeDriverService service = EdgeDriverService.CreateDefaultService();
@@ -27,38 +84,18 @@ namespace WebDriverThing
             EdgeDriver driver = new EdgeDriver(service, eo);
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
 
-            var openButton = driver.FindElement(OpenQA.Selenium.By.Id("backstage-open"));
-            openButton.Click();
+            // Use the Open button to load a document.
+            openFile(driver, @"C:\Users\tml\sailing.odt");
 
-            // How long should we wait for the File Open dialog to appear? Let's try 5 s.
+            // At first, click the button to enable editing.
+            // Give for the document time to load.
+            var editButton = driver.FindElement(OpenQA.Selenium.By.Id("mobile-edit-button"));
+
+            if (editButton == null)
+                fatal("No mobile-edit-button");
+
             Thread.Sleep(5000);
-
-            var openDialog = AutomationElement.RootElement.FindFirst(
-                TreeScope.Children,
-                new PropertyCondition(AutomationElement.NameProperty, "Open")
-            );
-
-            var openDialogOpenButton = openDialog.FindFirst(
-                TreeScope.Descendants,
-                new PropertyCondition(AutomationElement.AutomationIdProperty, "1")
-            );
-
-            var fileNameField = openDialog.FindFirst(
-                TreeScope.Descendants,
-                new PropertyCondition(AutomationElement.AutomationIdProperty, "1148")
-            );
-
-            var valuePattern = (ValuePattern)fileNameField.GetCurrentPattern(ValuePattern.Pattern);
-            valuePattern.SetValue(@"C:\Users\tml\sailing.odt");
-
-            // This does not seem to work. Would love to figure out some bette, sane, and reliable
-            // way to do this.
-            // ((InvokePattern)openDialogOpenButton.GetCurrentPattern(InvokePattern.Pattern)).Invoke();
-
-            // This works, and is mad and scary! Just pretend Enter is pressed to whatever window
-            // that has the focus. This means that you have to be very careful when debugging this
-            // code. You can't let VS have the focus when this line is executed.
-            System.Windows.Forms.SendKeys.SendWait("{ENTER}");
+            editButton.Click();
 
             Thread.Sleep(20000);
 
