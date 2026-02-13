a8cb330cfbafdf70afa3cc31fe7f97d0e7306581
Caolán McNamara <caolan.mcnamara@collabora.com>
2026-02-13 12:40:28 +0000

    bootstrap notebookbar a11y tests
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: I6b3496ba50f03ef5c108d5c893e60ab59ab1a2b6

---
+++ cypress_test/integration_tests/desktop/calc/a11y_notebookbar_spec.js
@@ -0,0 +1,118 @@
+/* global describe expect it cy before after afterEach require */
+
+var helper = require('../../common/helper');
+var desktopHelper = require('../../common/desktop_helper');
+var a11yHelper = require('../../common/a11y_helper');
+
+// don't pass yet
+var buggyTabs = [
+	'Home',
+];
+
+describe(['tagdesktop'], 'Accessibility Calc Notebookbar Tests', { testIsolation: false }, function () {
+	var tabs;
+	var allTabNames;
+	var visitedTabNames;
+	var win;
+
+	before(function () {
+		helper.setupAndLoadDocument('calc/help_dialog.ods');
+
+		desktopHelper.switchUIToNotebookbar();
+
+		cy.getFrameWindow().then(function (frameWindow) {
+			win = frameWindow;
+		});
+
+		cy.then(function () {
+			tabs = win.app.map.uiManager.notebookbar.getTabs();
+			allTabNames = tabs.map(function (tab) { return tab.name; });
+			visitedTabNames = [];
+		});
+	});
+
+	// Context tabs that need complex setup not yet implemented
+	var skipContextTabs = ['Sparkline', 'Table'];
+
+/* 	TODO, enable after Home is fixed
+	after(function () {
+		var unvisited = allTabNames.filter(function (name) {
+			return !visitedTabNames.includes(name) && !skipContextTabs.includes(name);
+		});
+		expect(unvisited, 'unvisited notebookbar tabs').to.be.empty;
+	});
+*/
+
+	afterEach(function () {
+		a11yHelper.resetState();
+	});
+
+	function selectAndValidateTab(tab) {
+		var selector = '#' + tab.id;
+		cy.cGet(selector).should('be.visible').click();
+		cy.cGet(selector).should('have.class', 'selected');
+
+		helper.processToIdle(win);
+		a11yHelper.runA11yValidation(win, 'validatenotebookbara11y');
+
+		visitedTabNames.push(tab.name);
+	}
+
+	function findTab(name) {
+		return tabs.find(function (t) { return t.name === name; });
+	}
+
+	it.skip('Notebookbar tab: Shape (context) (buggy)', function () {
+		cy.then(function () {
+			win.app.map.sendUnoCommand('.uno:BasicShapes.octagon');
+		});
+
+		cy.cGet('#test-div-shapeHandlesSection').should('exist');
+
+		cy.then(function () {
+			selectAndValidateTab(findTab('Shape'));
+		});
+
+		// exit shape mode
+		helper.typeIntoDocument('{esc}');
+	});
+
+	it.skip('Notebookbar tab: Picture (context) (buggy)', function () {
+		cy.viewport(1920, 1080);
+
+		desktopHelper.insertImage();
+
+		cy.then(function () {
+			selectAndValidateTab(findTab('Picture'));
+		});
+
+		// exit picture mode
+		helper.typeIntoDocument('{esc}');
+	});
+
+	// TODO: Add a Sparkline to test document after merge of outstanding pr
+	// TODO: Table context is for "Table in Table" feature, not available in .ods format
+
+	it.skip('All non-context notebookbar tabs', function () {
+		cy.then(function () {
+			var nonContextTabs = tabs.filter(function (tab) {
+				return !tab.context || tab.context.includes('default');
+			});
+
+			var chain = cy.wrap(null);
+			nonContextTabs.forEach(function (tab) {
+				chain = chain.then(function () {
+					if (buggyTabs.includes(tab.name)) {
+						visitedTabNames.push(tab.name);
+					} else {
+						selectAndValidateTab(tab);
+					}
+				});
+			});
+		});
+	});
+
+	buggyTabs.forEach(function (name) {
+		it.skip('Notebookbar tab: ' + name + ' (buggy)', function () {});
+	});
+});
---
+++ cypress_test/integration_tests/desktop/writer/a11y_notebookbar_spec.js
@@ -0,0 +1,132 @@
+/* global describe expect it cy before after afterEach require */
+
+var helper = require('../../common/helper');
+var desktopHelper = require('../../common/desktop_helper');
+var a11yHelper = require('../../common/a11y_helper');
+
+// don't pass yet
+var buggyTabs = [
+	'Home',
+];
+
+describe(['tagdesktop'], 'Accessibility Writer Notebookbar Tests', { testIsolation: false }, function () {
+	var tabs;
+	var allTabNames;
+	var visitedTabNames;
+	var win;
+
+	before(function () {
+		helper.setupAndLoadDocument('writer/help_dialog.odt');
+
+		desktopHelper.switchUIToNotebookbar();
+
+		cy.getFrameWindow().then(function (frameWindow) {
+			win = frameWindow;
+		});
+
+		cy.then(function () {
+			tabs = win.app.map.uiManager.notebookbar.getTabs();
+			allTabNames = tabs.map(function (tab) { return tab.name; });
+			visitedTabNames = [];
+		});
+	});
+
+	after(function () {
+		var unvisited = allTabNames.filter(function (name) {
+			return !visitedTabNames.includes(name);
+		});
+		expect(unvisited, 'unvisited notebookbar tabs').to.be.empty;
+	});
+
+	afterEach(function () {
+		a11yHelper.resetState();
+	});
+
+	function selectAndValidateTab(tab) {
+		var selector = '#' + tab.id;
+		cy.cGet(selector).should('be.visible').click();
+		cy.cGet(selector).should('have.class', 'selected');
+
+		helper.processToIdle(win);
+		a11yHelper.runA11yValidation(win, 'validatenotebookbara11y');
+
+		visitedTabNames.push(tab.name);
+	}
+
+	function findTab(name) {
+		return tabs.find(function (t) { return t.name === name; });
+	}
+
+	it('Notebookbar tab: Table (context)', function () {
+		// Workaround: we start in a table, so table context
+		// should be active, but at the moment is it not, workaround
+		// that bug by leaving the table and reentering it.
+		helper.typeIntoDocument('{ctrl}{end}');
+		helper.typeIntoDocument('{ctrl}{end}');
+		helper.typeIntoDocument('{ctrl}{home}');
+
+		selectAndValidateTab(findTab('Table'));
+	});
+
+	it('Notebookbar tab: Shape (context)', function () {
+
+		cy.then(function () {
+			win.app.map.sendUnoCommand('.uno:BasicShapes.octagon');
+		});
+
+		cy.cGet('#test-div-shapeHandlesSection').should('exist');
+
+		selectAndValidateTab(findTab('Shape'));
+
+		// exit shape mode
+		helper.typeIntoDocument('{esc}');
+	});
+
+	it('Notebookbar tab: Picture (context)', function () {
+
+		desktopHelper.insertImage();
+
+		selectAndValidateTab(findTab('Picture'));
+
+		// exit picture mode
+		helper.typeIntoDocument('{esc}');
+	});
+
+	it('Notebookbar tab: Formula (context)', function () {
+
+		cy.then(function () {
+			win.app.map.sendUnoCommand('.uno:InsertObjectStarMath');
+		});
+
+		cy.cGet('#test-div-shapeHandlesSection').should('exist');
+
+		selectAndValidateTab(findTab('Formula'));
+
+		// exit formula mode
+		helper.typeIntoDocument('{esc}');
+	});
+
+	it('All non-context notebookbar tabs', function () {
+
+		cy.then(function () {
+			var nonContextTabs = tabs.filter(function (tab) {
+				return !tab.context || tab.context.includes('default');
+			});
+
+			var chain = cy.wrap(null);
+			nonContextTabs.forEach(function (tab) {
+				chain = chain.then(function () {
+					if (buggyTabs.includes(tab.name)) {
+						visitedTabNames.push(tab.name);
+					} else {
+						selectAndValidateTab(tab);
+					}
+				});
+			});
+		});
+	});
+
+	buggyTabs.forEach(function (name) {
+		it.skip('Notebookbar tab: ' + name + ' (buggy)', function () {});
+	});
+});
