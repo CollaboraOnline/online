d7fa50a07d690d9cc880f4b309670f955cbe02f8
Sahil Gautam <sahil.gautam@collabora.com>
2026-02-13 11:30:13 +0530

    fix: don't collapse the comments ever in writer
    There were some issues with how the collapsed comments re-expanded,
    like they used to re-expanded and be in a half-visible
    half-behind-overflow state. So to deal with that we decided to not
    re-expand them until they were fully visible or fully hidden. But
    in that case, if the window width is reduced to a point where the
    collapsed comments hid behind the overflow and the scrollbar isn't
    activated, there's no way to access the collapsed comments.
    
    So we decided to get rid of the collapsed comments (only for writer)
    to see how that works out.
    
    Signed-off-by: Sahil Gautam <sahil.gautam@collabora.com>
    Change-Id: I01686ced558d8df8e902b0a52a0359ca1a2d84b6

--- browser/src/canvas/sections/CommentListSection.ts
+++ browser/src/canvas/sections/CommentListSection.ts
@@ -359,6 +359,8 @@ export class CommentSection extends CanvasSectionObject {
 	}
 
 	public shouldCollapse (): boolean {
+		if (app.map._docLayer._docType === 'text')
+			return false;
 		if (!this.containerObject.getDocumentAnchorSection() || app.map._docLayer._docType === 'spreadsheet' || (<any>window).mode.isMobile())
 			return false;
 		const availableSpace = this.calculateAvailableSpace();
@@ -2316,6 +2318,7 @@ export class CommentSection extends CanvasSectionObject {
 				}
 			}
 		}
+		this.update();
 	}
 
 	private orderCommentList (): void {
