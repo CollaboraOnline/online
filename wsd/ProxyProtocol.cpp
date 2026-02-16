e29402584a8f746a9b94b1fdac5c42d6d1321028
Areg Nakashian <areg.nakashian@gmail.com>
2026-02-15 22:08:28 -0500

    wsd: Replace #if DEBUG_ENABLED in ProxyProtocol.cpp
    Signed-off-by: Areg Nakashian <areg.nakashian@gmail.com>
    Change-Id: Ife783664d46a235a9500d17d00209a030705c0f3

--- wsd/ProxyProtocol.cpp
+++ wsd/ProxyProtocol.cpp
@@ -84,11 +84,12 @@ void DocumentBroker::proxyOpenRequest(const std::shared_ptr<StreamSocket>& socke
                                       const std::string& id, const Poco::URI& uriPublic,
                                       const bool isReadOnly, const RequestDetails& requestDetails)
 {
-#if ENABLE_DEBUG
-    bool isLocal = true;
-#else
     bool isLocal = socket->isLocal();
-#endif
+    if constexpr (Util::isDebugEnabled())
+    {
+        isLocal = true;
+    }
+
     LOG_TRC("proxy: validate that socket is from localhost: " << isLocal);
     if (!isLocal)
         throw BadRequestException("invalid host - only connect from localhost");
