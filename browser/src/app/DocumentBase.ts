CollaboraOnline/online

commit 737006cc0554334c009d7f94a7508c9b65f10047

Remove _textCSelections and add a DocumentViewBase class instance for current user's text selections.
Remove 1 unused containsSelection function.

Signed-off-by: Gökay Şatır <gokaysatir@collabora.com>
Change-Id: I046983ad2577ab09695766f913ef92b22cdc4062

browser/src/app/DocumentBase.ts
@@ -17,10 +17,15 @@ class DocumentBase {
 	public selectionMiddleware: ImpressSelectionMiddleware | null;
 	public mouseControl: MouseControl | null = null;
 	protected views: Map<number, DocumentViewBase> = new Map<number, DocumentViewBase>();
+	protected activeViewID: number;
+	public activeView: DocumentViewBase;
+	private activeViewSelectionColor = 'lightblue'; // Overwritten in constructor.
 
 	protected _fileSize: cool.SimplePoint;
 
 	constructor() {
+		if (!app.activeDocument) app.activeDocument = this;
+
 		if (app.map._docLayer._docType === 'text') {
 			this.activeLayout = new ViewLayoutWriter();
 		} else {
@@ -36,6 +41,24 @@ class DocumentBase {
 		else this.selectionMiddleware = null;
 
 		this.addSections();
+		this.activeViewID = 0;
+		this.activeView = new DocumentViewBase(this.activeViewID);
+
+		const dummyDiv = document.createElement('div');
+		dummyDiv.className = 'selections-data';
+		document.body.appendChild(dummyDiv);
+		this.activeViewSelectionColor = getComputedStyle(dummyDiv).getPropertyValue('background-color');
+		this.activeView.setColor(this.activeViewSelectionColor);
+		dummyDiv.remove();
+	}
+
+	public setActiveViewID(activeViewID: number) {
+		if (this.activeViewID !== activeViewID) {
+			this.activeViewID = activeViewID;
+			this.activeView.clearTextSelection();
+			this.activeView = new DocumentViewBase(this.activeViewID);
+			this.activeView.setColor(this.activeViewSelectionColor);
+		}
 	}
 
 	private addSections() {

browser/src/app/DocumentView.ts
@@ -18,8 +18,9 @@
 */
 class DocumentViewBase {
 	public readonly viewID: number;
-	color: string;
-	selectionSection: TextSelectionSection;
+	private color: string;
+	public selectionSection: TextSelectionSection;
+    public hasTextSelection: boolean = false;
 
 	constructor(viewID: number) {
 		this.viewID = viewID;
@@ -65,8 +66,19 @@ class DocumentViewBase {
         return groups;
     }
 
+    public setColor(color: string) {
+        this.color = color;
+        this.selectionSection.color = this.color;
+    }
+
+    public clearTextSelection() {
+        this.updateSelectionRawData(-1, -1, []);
+    }
+
     public updateSelectionRawData(mode: number, part: number, rectangles: Array<Array<number>>) {
         if (rectangles.length > 0) {
+            this.hasTextSelection = true;
+
             const rawPolygons = this.getSeparatePolygonsFromGroupOfRectangles(rectangles);
             let minX = Number.POSITIVE_INFINITY;
             let maxX = Number.NEGATIVE_INFINITY;
@@ -105,6 +117,7 @@ class DocumentViewBase {
                 this.selectionSection.setShowSection(false);
         }
         else if (rectangles.length === 0) {
+            this.hasTextSelection = false;
             this.selectionSection.setSelectionInfo(mode, part, []);
             this.selectionSection.setShowSection(false);
         }

browser/css/cool.css
@@ -1217,7 +1217,7 @@ body {
 	border-top-width: 2px; /* weight */
 }
 
-.leaflet-canvas-container .selections-data {
+.selections-data {
 	border-style: solid; /* required for ie11 */
 	display: none; /* prevent cypress failure */
 

browser/src/canvas/sections/TextSelectionSection.ts
@@ -1,5 +1,5 @@
 // @ts-strict-ignore
-/* global Proxy _ */
+
 /*
  * Copyright the Collabora Online contributors.
  *
@@ -25,9 +25,9 @@ class TextSelectionSection extends CanvasSectionObject {
 			* Behaviour should be decided based on what the app is capable (remove one selection when other is initiated or keep them all etc).
 			* Selections data (not the socket message, its data) should be handled in the DocumentViewBase class.
 	*/
-	public readonly mode: number;
-	public readonly part: number;
-    public readonly color: string;
+	private mode: number;
+	private part: number;
+    public color: string;
 	private polygons: Array<Array<cool.SimplePoint>> = [[]];
 
 	constructor (name: string, mode: number, part: number, color: string) {
@@ -38,6 +38,8 @@ class TextSelectionSection extends CanvasSectionObject {
 	}
 
     public setSelectionInfo(mode: number, part: number, polygons: Array<Array<cool.SimplePoint>>) {
+		this.mode = mode;
+		this.part = part;
 		this.polygons = polygons;
 	}
 

browser/src/layer/marker/TextInput.js
@@ -503,7 +503,7 @@ window.L.TextInput = window.L.Layer.extend({
 	// Displays the caret and the under-caret marker.
 	// Fetches the coordinates of the caret from the map's doclayer.
 	showCursor: function() {
-		if (!this._map._docLayer._cursorMarker || app.tile.size.x === 0 || !this._map._docLayer._textCSelections) {
+		if (!this._map._docLayer._cursorMarker || app.tile.size.x === 0 || !app.activeDocument) {
 			return;
 		}
 
@@ -515,7 +515,7 @@ window.L.TextInput = window.L.Layer.extend({
 
 		// Move and display under-caret marker
 
-		if (window.touch.currentlyUsingTouchscreen() && this._map._docLayer._textCSelections.empty() && this._cursorHandler) {
+		if (window.touch.currentlyUsingTouchscreen() && !app.activeDocument.activeView.hasTextSelection && this._cursorHandler) {
 			this._cursorHandler.setPosition(app.file.textCursor.rectangle.pX1, app.file.textCursor.rectangle.pY2 + (0 * app.dpiScale));
 			this._cursorHandler.setShowSection(true);
 		} else if (this._cursorHandler) {

browser/src/layer/tile/CalcTileLayer.js
@@ -457,6 +457,7 @@ window.L.CalcTileLayer = window.L.CanvasTileLayer.extend({
 			}
 
 			this._viewId = statusJSON.viewid;
+			app.activeDocument.setActiveViewID(this._viewId);
 
 			console.assert(this._viewId >= 0, 'Incorrect viewId received: ' + this._viewId);
 

browser/src/layer/tile/CanvasTileLayer.js
@@ -2212,9 +2212,9 @@ window.L.CanvasTileLayer = window.L.Layer.extend({
 	},
 
 	_clearSearchResults: function() {
-		if (this._searchTerm) {
-			this._textCSelections.clear();
-		}
+		if (this._searchTerm)
+			app.activeDocument.activeView.clearTextSelection();
+
 		this._lastSearchResult = null;
 		this._searchResults = null;
 		this._searchTerm = null;
@@ -2309,31 +2309,34 @@ window.L.CanvasTileLayer = window.L.Layer.extend({
 	},
 
 	_onTextSelectionMsg: function (textMsg) {
-		var rectArray = this._getTextSelectionRectangles(textMsg);
+		textMsg = textMsg.replace('textselection:', '').trim();
+		let rawRectangles = textMsg.split(';');
 
-		if (rectArray.length) {
-			TextSelections.activate();
-
-			var rectangles = rectArray.map(function (rect) {
-				return rect.getPointArray();
+		if (textMsg.trim() !== '') {
+			rawRectangles = rawRectangles.map((rectangle) => {
+				const temp = rectangle.split(',');
+				return [parseInt(temp[0]), parseInt(temp[1]), parseInt(temp[2]), parseInt(temp[3])];
 			});
+		}
+		else rawRectangles = [];
+
+		if (rawRectangles.length > 0) {
+			TextSelections.activate();
 
 			if (app.file.fileBasedView && this._lastSearchResult) {
 				// We rely on that _lastSearchResult has been updated before this function is called.
-				var additionPerPart = this._partHeightTwips + this._spaceBetweenParts;
-				for (var i = 0; i < rectangles.length; i++) {
-					for (var j = 0; j < rectangles[i].length; j++) {
-						rectangles[i][j].y += additionPerPart * this._lastSearchResult.part;
-					}
+				const additionPerPart = this._partHeightTwips + this._spaceBetweenParts;
+
+				for (let i = 0; i < rawRectangles.length; i++) {
+					rawRectangles[i][1] += additionPerPart * this._lastSearchResult.part;
 				}
+
 				this._map._docLayer._preview._scrollViewToPartPosition(this._lastSearchResult.part);
 				TileManager.updateFileBasedView();
 				setTimeout(function () {app.sectionContainer.requestReDraw();}, 100);
 			}
 
-			var pointSet = this._convertToPointSet(rectangles);
-
-			this._textCSelections.setPointSet(pointSet);
+			app.activeDocument.activeView.updateSelectionRawData(this._selectedMode, this._selectedPart, rawRectangles);
 
 			if (this._map._textInput._cursorHandler)
 				this._map._textInput._cursorHandler.setShowSection(false); // User selected text, we remove the carret marker.
@@ -2356,7 +2359,7 @@ window.L.CanvasTileLayer = window.L.Layer.extend({
 		}
 		else {
 			TextSelections.deactivate();
-			this._textCSelections.clear();
+			app.activeDocument.activeView.clearTextSelection();
 			this._selectedTextContent = '';
 			if (this._map.contextToolbar)
 				this._map.contextToolbar.hideContextToolbar();
@@ -2725,7 +2728,7 @@ window.L.CanvasTileLayer = window.L.Layer.extend({
 		// hide the cursor if not editable
 		this._onUpdateCursor(calledFromSetPartHandler);
 		// hide the text selection
-		this._textCSelections.clear();
+		app.activeDocument.activeView.clearTextSelection();
 		// hide the ole selection
 		this._oleCSelections.clear();
 
@@ -2734,11 +2737,6 @@ window.L.CanvasTileLayer = window.L.Layer.extend({
 			this._map._clip.clearSelection();
 		else
 			this._selectedTextContent = '';
-	},
-
-	containsSelection: function (latlng) {
-		var corepxPoint = this._map.project(latlng);
-		return this._textCSelections.contains(corepxPoint);
 	},
 
 	_clearReferences: function () {
@@ -3233,7 +3231,7 @@ window.L.CanvasTileLayer = window.L.Layer.extend({
 
 	_removeSelection: function() {
 		this._selectedTextContent = '';
-		this._textCSelections.clear();
+		app.activeDocument.activeView.clearTextSelection();
 	},
 
 	_onDragOver: function (e) {
@@ -3789,8 +3787,6 @@ window.L.CanvasTileLayer = window.L.Layer.extend({
 		}
 
 		this._getToolbarCommandsValues();
-		this._textCSelections = new CSelections(undefined, this._canvasOverlay,
-			this._selectionsDataDiv, this._map, false /* isView */, undefined, 'text');
 		this._oleCSelections = new CSelections(undefined, this._canvasOverlay,
 			this._selectionsDataDiv, this._map, false /* isView */, undefined, 'ole');
 		this._references = new CReferences(this._canvasOverlay);
@@ -3863,9 +3859,7 @@ window.L.CanvasTileLayer = window.L.Layer.extend({
 		TileManager.clearPreFetch();
 		clearTimeout(this._previewInvalidator);
 
-		if (!this._textCSelections.empty()) {
-			this._textCSelections.clear();
-		}
+		app.activeDocument.activeView.clearTextSelection();
 
 		if (!this._oleCSelections.empty()) {
 			this._oleCSelections.clear();

browser/src/layer/tile/ImpressTileLayer.js
@@ -272,6 +272,7 @@ window.L.ImpressTileLayer = window.L.CanvasTileLayer.extend({
 			this._updateMaxBounds(true, allPagesResized);
 
 			this._viewId = statusJSON.viewid;
+			app.activeDocument.setActiveViewID(this._viewId);
 			console.assert(this._viewId >= 0, 'Incorrect viewId received: ' + this._viewId);
 			if (app.socket._reconnecting) {
 				app.socket.sendMessage('setclientpart part=' + this._selectedPart);

commit deeca5886276a28bbc259760df77384cc0852de5

canvas: be carefull with adding sections
- we had a bug where selection was not visible in
  multi-user sessions (but only our own)

- it was easy to reproduce, just opened 3 Writer sessions
  and tried to select any text in them

- while debugging I found the line in CanvasSectionObject
  if (this.containerObject) { // Is section added to container.
  was failing the condition what indicated the selection was
  not added

- setActiveViewID now removes the old active view's section
  before creating the new one

- Constructor defensively removes any existing section with
  the same name before calling addSection

- we should add some warnings that section was not added

It seems to be regression from commit 737006cc0554334c009d7f94a7508c9b65f10047
Remove _textCSelections and add a DocumentViewBase class instance for current user's text selections.

Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
Change-Id: I861c968d976c38f1fbec7d380848afa7da4e1ff0

browser/src/app/DocumentBase.ts
@@ -61,6 +61,8 @@ class DocumentBase {
 		if (this.activeViewID !== activeViewID) {
 			this.activeViewID = activeViewID;
 			this.activeView.clearTextSelection();
+			// Remove the old active view's section before creating a new one.
+			app.sectionContainer.removeSection(this.activeView.selectionSection.name);
 			this.activeView = new DocumentViewBase(this.activeViewID);
 			this.activeView.setColor(this.activeViewSelectionColor);
 		}

browser/src/app/DocumentView.ts
@@ -31,6 +31,9 @@ class DocumentViewBase {
 			0,
 			this.color,
 		);
+		// Remove any stale section with the same name so addSection succeeds.
+		if (app.sectionContainer.doesSectionExist(this._selectionSection.name))
+			app.sectionContainer.removeSection(this._selectionSection.name);
 		app.sectionContainer.addSection(this._selectionSection);
 		this._selectionSection.setShowSection(false);
 	}