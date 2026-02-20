CollaboraOnline/online

commit 6697c6d13f16ffd5c05600cec5e4cf1ffbd233d1

Don't rely on unstable id for save state widget
- ids can be manipulated and new suffix can be added
- add more defensive code to protect us from errors
- adjust CSS too

Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
Change-Id: Ie575da7f64bd6532512c5e4402d7bb34a1dda4db

browser/src/control/Control.SaveState.ts
@@ -23,16 +23,22 @@ class SaveState {
 	// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 	constructor(map: any) {
 		this.map = map;
-		this.saveEle = document.getElementById('save');
-		this.saveIconEl = document.querySelector('#save img');
+		this.initialize();
+	}
+
+	initialize() {
+		this.saveEle = document.querySelector('[id^="save"].unotoolbutton');
+		this.saveIconEl = this.saveEle.querySelector('img');
 	}
 
 	// Function to show the saving status
 	showSavingStatus(): void {
 		if (window.mode.isMobile()) return;
 
+		if (!this.saveEle) this.initialize();
+
 		// Only do saving animation if any content is modified in document
-		if (this.saveEle.classList.contains('savemodified')) {
+		if (this.saveEle && this.saveEle.classList.contains('savemodified')) {
 			this.saveEle.classList.remove('savemodified');
 			// Dynamically set the content string for saving state
 			const savingText = _('Saving');
@@ -46,6 +52,9 @@ class SaveState {
 	// Function to show the saved status
 	showSavedStatus(): void {
 		if (window.mode.isMobile()) return;
+
+		if (!this.saveEle) this.initialize();
+
 		if (!this.saveEle.classList.contains('savemodified')) {
 			this.saveEle.classList.remove('saving');
 			this.saveIconEl.classList.remove('rotate-icon'); // Stop the icon rotation
@@ -62,11 +71,14 @@ class SaveState {
 	}
 
 	showModifiedStatus(): void {
-		this.saveEle.classList.remove('saving');
-		this.saveEle.classList.remove('saved');
-		this.saveIconEl.classList.remove('rotate-icon'); // Stop the icon rotation
-		this.saveEle.removeAttribute('disabled'); // Enable the button
-		this.saveEle.classList.add('savemodified');
+		if (!this.saveEle) this.initialize();
+		if (this.saveEle) {
+			this.saveEle.classList.remove('saving');
+			this.saveEle.classList.remove('saved');
+			this.saveIconEl.classList.remove('rotate-icon'); // Stop the icon rotation
+			this.saveEle.removeAttribute('disabled'); // Enable the button
+			this.saveEle.classList.add('savemodified');
+		}
 	}
 }
 

browser/css/menubar.css
@@ -67,7 +67,8 @@
 		transform: rotate(360deg);
 	}
 }
-#save {
+nav [id^='save'].unotoolbutton,
+#toolbar-up [id^='save'].unotoolbutton {
 	display: flex;
 	font-size: var(--default-font-size);
 	font-family: var(--cool-font);
@@ -78,33 +79,36 @@
 	justify-content: center;
 }
 
-#save.saving img {
+nav [id^='save'].saving img,
+#toolbar-up [id^='save'].saving img {
 	background: url('images/lc_saving.svg') no-repeat center;
 }
 
-nav.hasnotebookbar #save::after {
+nav.hasnotebookbar [id^='save'].unotoolbutton::after {
 	position: absolute;
 	bottom: 0px;
 }
 
-nav.hasnotebookbar #save.saving::after {
+nav.hasnotebookbar [id^='save'].saving::after {
 	content: var(--save-state);
 	font-size: var(--default-font-size);
 	font-family: var(--cool-font);
 	color: var(--color-main-text);
 }
 
-#save.saved img {
+nav [id^='save'].saved img,
+#toolbar-up [id^='save'].saved img {
 	background: url('images/lc_saved.svg') no-repeat center;
 	filter: grayscale(0);
 }
 
-nav.hasnotebookbar #save.saved::after {
+nav.hasnotebookbar [id^='save'].saved::after {
 	content: var(--save-state);
 	color: #309048;
 }
 
-[data-theme='dark'] #save.saving img {
+[data-theme='dark'] nav [id^='save'].saving img,
+[data-theme='dark'] #toolbar-up [id^='save'].saving img {
 	background: url('images/dark/lc_saving.svg') no-repeat center;
 }
 #document-titlebar {

browser/css/notebookbar.css
@@ -155,8 +155,10 @@ button.ui-tab.notebookbar {
 
 .unoSave.savemodified .unobutton img,
 .savemodified.unotoolbutton .unobutton img,
-#save.saving img,
-#save.saved img {
+nav [id^='save'].saving img,
+#toolbar-up [id^='save'].saving img,
+nav [id^='save'].saved img,
+#toolbar-up [id^='save'].saved img {
 	box-sizing: border-box;
 	padding-left: var(--btn-img-size) !important;
 }

browser/src/control/Control.NotebookbarBuilder.js
@@ -156,16 +156,16 @@ window.L.Control.NotebookbarBuilder = window.L.Control.JSDialogBuilder.extend({
 			$('#applystyle').val(state).trigger('change');
 		}
 		else if (commandName === '.uno:ModifiedStatus') {
-			const saveEle = document.getElementById('save');
+			const saveEle = document.querySelector('[id^="save"].unotoolbutton');
 			if (saveEle) {
 				if (state === 'true' &&  this.map.saveState) {
 					this.map.saveState.showModifiedStatus();
-					const button = document.getElementById('file-save');
+					const button = document.querySelector('[id^="file-save"]');
 					if (button) button.classList.add('savemodified');
 				} else {
-					const button = document.getElementById('save');
+					const button = document.querySelector('[id^="save"]');
 					if (button) button.classList.remove('savemodified');
-					const fileButton = document.getElementById('file-save');
+					const fileButton = document.querySelector('[id^="file-save"]');
 					if (fileButton) fileButton.classList.remove('savemodified');
 				}
 			}

browser/src/control/Control.Toolbar.js
@@ -881,11 +881,12 @@ function processStateChangedCommand(commandName, state) {
 		}
 	}
 	else if (commandName === '.uno:ModifiedStatus') {
-		if (document.getElementById('save')) {
+		const saveIcon = document.querySelector('[id^="save"]');
+		if (saveIcon) {
 			if (state === 'true' && map.saveState)
 				map.saveState.showModifiedStatus();
 			else
-				document.getElementById('save').classList.remove('savemodified');
+				saveIcon.classList.remove('savemodified');
 		}
 		state = ''; // stop processing below
 	}

cypress_test/plugins/selectorList.js
@@ -22,7 +22,7 @@ module.exports.list = {
 	clearFormat: ['#Home .unoResetAttributes', '#toolbar-up #reset'],
 	hyperLink: ['#HyperlinkDialog', '#toolbar-up #link'],
 	insertShape: ['#BasicShapes', '#toolbar-up #insertshapes'],
-	save: ['#Save', '#toolbar-up #save'],
+	save: ['#Save', '#toolbar-up [id^="save"]'],
 	print: ['#Print', '#toolbar-up #print'],
 	undo: ['#Undo', '#toolbar-up #undo'],
 	redo: ['#Redo', '#toolbar-up #redo'],

commit 30c893a2e5de8e06da1c40bf5513d323fe1a32f8

SaveState: don't use missing icon
- be sure item is initialized before use
- it can happen we hide save icon due to integration settings

regression from commit 6697c6d13f16ffd5c05600cec5e4cf1ffbd233d1
Don't rely on unstable id for save state widget

Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
Change-Id: Iccc2c9754b5530cf80d82d43f6cce8a36d030dbc

browser/src/control/Control.SaveState.ts
@@ -28,7 +28,12 @@ class SaveState {
 
 	initialize() {
 		this.saveEle = document.querySelector('[id^="save"].unotoolbutton');
-		this.saveIconEl = this.saveEle.querySelector('img');
+		if (this.saveEle) {
+			this.saveIconEl = this.saveEle.querySelector('img');
+		} else {
+			app.console.debug('SaveState: no save icon - might be hidden');
+			this.saveIconEl = null;
+		}
 	}
 
 	// Function to show the saving status
@@ -44,7 +49,7 @@ class SaveState {
 			const savingText = _('Saving');
 			this.saveEle.style.setProperty('--save-state', `"${savingText}"`);
 			this.saveEle.classList.add('saving');
-			this.saveIconEl.classList.add('rotate-icon'); // Start the icon rotation
+			this.saveIconEl?.classList.add('rotate-icon'); // Start the icon rotation
 			this.saveEle.setAttribute('disabled', 'true'); // Disable the button
 		}
 	}
@@ -55,9 +60,9 @@ class SaveState {
 
 		if (!this.saveEle) this.initialize();
 
-		if (!this.saveEle.classList.contains('savemodified')) {
+		if (this.saveEle && !this.saveEle.classList.contains('savemodified')) {
 			this.saveEle.classList.remove('saving');
-			this.saveIconEl.classList.remove('rotate-icon'); // Stop the icon rotation
+			this.saveIconEl?.classList.remove('rotate-icon'); // Stop the icon rotation
 			// Dynamically set the content string for saved state
 			const savedText = _('Saved');
 			this.saveEle.style.setProperty('--save-state', `"${savedText}"`);