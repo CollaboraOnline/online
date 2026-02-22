dd264c93664bd0d6281db8c4afb306209cb3b5ec
Sarper Akdemir <sarper.akdemir@collabora.com>
2026-02-22 14:39:30 +0100

    chore(app:linux): extract Application/Desktop implementations into Application.cpp
    Move Application::globalProfile, Application::recentFiles static member
    definitions, Application::initialize(), Desktop::getConfigPath(),
    Desktop::getDataDir(), Application::getProfile(), and
    Application::getRecentFiles() out of coda-qt.cpp into Application.cpp.
    
    No logic change — pure mechanical move + include cleanup in coda-qt.cpp.
    
    Change-Id: I3c4d5e6f7a8b9c0d1e2f3a4b5c6d7e8f9a0b1c2
    Signed-off-by: Sarper Akdemir <sarper.akdemir@collabora.com>

---
+++ qt/Application.cpp
@@ -0,0 +1,75 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <config.h>
+
+#include "MobileApp.hpp"
+#include "qt.hpp"
+
+#include "Log.hpp"
+#include "common/RecentFiles.hpp"
+#include "common/SettingsStorage.hpp"
+
+#include <Poco/File.h>
+#include <Poco/Path.h>
+
+#include <QDir>
+#include <QStandardPaths>
+#include <QWebEngineProfile>
+
+#include <string>
+
+QWebEngineProfile* Application::globalProfile = nullptr;
+RecentFiles Application::recentFiles;
+
+void Application::initialize()
+{
+    if (!globalProfile)
+    {
+        globalProfile = new QWebEngineProfile(QStringLiteral("PersistentProfile"));
+
+        QString appData = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
+        QString cacheData = QStandardPaths::writableLocation(QStandardPaths::CacheLocation);
+
+        globalProfile->setPersistentStoragePath(appData);
+        globalProfile->setCachePath(cacheData);
+        globalProfile->setHttpCacheType(QWebEngineProfile::DiskHttpCache);
+    }
+
+    // Initialize recent files
+    Poco::Path configDir = Desktop::getConfigPath();
+    recentFiles.load(configDir.append("RecentDocuments.conf").toString(), 15);
+}
+
+Poco::Path Desktop::getConfigPath()
+{
+    QString pathStr = QStandardPaths::writableLocation(QStandardPaths::AppConfigLocation);
+    QDir().mkpath(pathStr);
+    Poco::Path configPath(pathStr.toStdString());
+    Poco::File configDir(configPath);
+    if (!configDir.exists() || !configDir.isDirectory())
+    {
+        LOG_ERR("getConfigPath: following configuration directory does not exist, trouble ahead:"
+                << pathStr.toStdString());
+    }
+    return configPath;
+}
+
+std::string Desktop::getDataDir()
+{
+    return ::getDataDir();
+}
+
+QWebEngineProfile* Application::getProfile() { return globalProfile; }
+
+RecentFiles& Application::getRecentFiles() { return recentFiles; }
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
