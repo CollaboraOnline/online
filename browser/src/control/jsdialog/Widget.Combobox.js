835dde2b4058ba78f8f9febf941004671a24edc6
Pedro Pinto Silva <pedro.silva@collabora.com>
2026-02-25 09:44:13 +0100

    Notebookbar: combobox label: fix styes add css class
    - Add CSS class so label follows the same conventions as in
    .ui-checkbox-label so, to bring the css classes from the builder and
    add `ui-combobox-label`
    - Fix left padding and margin
    - Increase width of font name and font size containers
    
    Change-Id: Iba87e7d3b8bc8c88868b78f0761601e72693840e
    
    ---
    
    Context: this is a follow up on the previous commit with hash
    3b26b00954a4fc626252edf83809239e1e512304
    
    3b26b0095 was created to address the following:
    In the toolbar, there are two fields used to select the font type and
    font size. These fields do not have visible labels. Although the
    toolbar follows common patterns seen elsewhere and users
    can usually recognize the purpose of the fields, visible labels should
    still be provided to avoid accessibility barriers.
    
    Signed-off-by: Pedro Pinto Silva <pedro.silva@collabora.com>

--- browser/src/control/jsdialog/Widget.Combobox.js
+++ browser/src/control/jsdialog/Widget.Combobox.js
@@ -209,7 +209,7 @@ JSDialog.combobox = function (parentContainer, data, builder) {
 		// TODO replace with data.label, changing ui file
 		var labelText = _UNO(data.command, 'label', true);
 		if (labelText) {
-			var label = L.DomUtil.create('label', '', container);
+			var label = L.DomUtil.create('label', 'ui-combobox-label ' + builder.options.cssClass, container);
 			label.textContent = labelText + ':';
 			label.htmlFor = inputId;
 		}
