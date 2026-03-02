07a4d2fd5f08e2fa7b66b7805c615472b88fe51d
Stephan Bergmann <stephan.bergmann@collabora.com>
2026-03-02 16:23:07 +0100

    Fix headers for running COWASM from within an integrator
    ...by enabling cross-origin isolation on a per-frame basis so that
    SharedArrayBuffer works inside the cross-origin iframe without requiring the
    embedding page to set COOP
    
    Signed-off-by: Stephan Bergmann <stephan.bergmann@collabora.com>
    Change-Id: I10eee0f05bce8f0da234576111161cc5cd40bf9d

--- wsd/FileServer.cpp
+++ wsd/FileServer.cpp
@@ -1472,6 +1472,7 @@ FileServerRequestHandler::ResourceAccessDetails FileServerRequestHandler::prepro
         httpResponse.add("Cross-Origin-Opener-Policy", "same-origin");
         httpResponse.add("Cross-Origin-Embedder-Policy", "require-corp");
         httpResponse.add("Cross-Origin-Resource-Policy", "cross-origin");
+        httpResponse.add("Document-Isolation-Policy", "isolate-and-require-corp");
     }
 
     const bool wasm = (relPath.find("wasm") != std::string::npos);
