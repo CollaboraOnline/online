887b7b7afa72b97eead7c7cf25ecf5d204a76416
Andras Timar <andras.timar@collabora.com>
2026-03-03 12:06:44 +0100

    macOS: remove unused storyboard menus to reduce translation burden
    The storyboard was created from Xcode's default document-based template
    which includes many standard menus (Format, Font, Text, Spelling, Speech,
    Find, etc.) that are irrelevant since the app hosts Collabora Online in a
    WebView. These menus were already hidden, but their strings still appeared
    in Main.pot and all 43 .po files, creating unnecessary work for translators.
    
    Remove the entire hidden Format menu, hidden Edit submenus (Delete,
    Select All, Find, Spelling and Grammar, Substitutions, Transformations,
    Speech), and the hidden "ProductName Help" item. Also remove the
    associated dead code in WindowController.swift (FormatMenuCommand enum,
    formatMenuAction IBAction, validateMenuItem) and the now-unnecessary
    showHelp replaceProductName call in AppDelegate.swift.
    
    Translatable strings reduced from 97 to 46 (~51 removed).
    
    Signed-off-by: Andras Timar <andras.timar@collabora.com>
    Change-Id: If817cbfe25a620c7dbd28ec3c005b0971f19f51f

--- macos/coda/coda/WindowController.swift
+++ macos/coda/coda/WindowController.swift
@@ -11,18 +11,6 @@
 import Cocoa
 import WebKit
 
-enum FormatMenuCommand: Int {
-    case bold = 1
-    case italic
-    case underline
-}
-
-let formatMenuCommandNames: [FormatMenuCommand: String] = [
-    .bold: "Bold",
-    .italic: "Italic",
-    .underline: "Underline"
-]
-
 /// The structure we expect in the COMMANDSTATECHANGED payload.
 struct CommandStateChange: Decodable {
     let commandName: String
@@ -37,22 +25,10 @@ struct CommandResult: Decodable {
 }
 
 /// Window controller that manages the document window & menus
-class WindowController: NSWindowController, NSMenuItemValidation {
+class WindowController: NSWindowController {
 
     /// Track menu states here, to be able to show the state in the menu
     var commandState: [String: String] = [:]
-
-    /// Single IBAction for all Format commands in the menu
-    @IBAction func formatMenuAction(_ sender: NSMenuItem) {
-        guard let command = FormatMenuCommand(rawValue: sender.tag) else { return }
-
-        // Forward to the ViewController so it can actually set bold/italic/...
-        if let vc = contentViewController as? ViewController {
-            if let unoCommand = formatMenuCommandNames[command] ?? nil {
-                COWrapper.handleMessage(with: vc.document, message: "uno .uno:\(unoCommand)")
-            }
-        }
-    }
 
     /// Remember the new command state value as provided by JS.
     func handleCommandStateChange(_ stateChange: CommandStateChange) {
@@ -63,22 +39,6 @@ class WindowController: NSWindowController, NSMenuItemValidation {
 
         // store it
         commandState[String(commandName)] = stateChange.state
-    }
-
-    /// Show on/off checkmarks for the particular command(s) like Bold/Italics/...
-    func validateMenuItem(_ menuItem: NSMenuItem) -> Bool {
-        if menuItem.action != #selector(formatMenuAction(_:)) {
-            return true // not our action
-        }
-
-        // convert the id to a command name
-        guard let command = FormatMenuCommand(rawValue: menuItem.tag) else { return false }
-        guard let commandName = formatMenuCommandNames[command] else { return false }
-
-        // Show a check if the command is currently ON
-        let isOn = commandState[commandName] == "true"
-        menuItem.state = isOn ? .on : .off
-        return true
     }
 }
 
