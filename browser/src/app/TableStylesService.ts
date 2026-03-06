f2eaf35e55368f62709c2eba9ac7bb65fc61b451
Szymon Kłos <szymon.klos@collabora.com>
2026-03-06 03:57:50 +0000

    Table Design: make icons and frame bigger
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I5451ca8e683279d5cd233a988c3ca12aabcf016d

--- browser/src/app/TableStylesService.ts
+++ browser/src/app/TableStylesService.ts
@@ -264,8 +264,8 @@ class TableStylesService {
 			row: -1,
 			text: _('None'),
 			image: 'images/lc_table_none.svg',
-			width: 35,
-			height: 35,
+			width: 50,
+			height: 50,
 			selected: !currentStyle || currentStyle.TableStyleName === '',
 		} as IconViewEntry);
 
@@ -277,8 +277,8 @@ class TableStylesService {
 				row: i++,
 				text: element.UIName,
 				image: this.generateIcon(element),
-				width: 35,
-				height: 35,
+				width: 50,
+				height: 50,
 				selected: selected,
 			} as IconViewEntry);
 		});
