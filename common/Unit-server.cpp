0be69188856c814b59339a7e20cff40784fabb74
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2026-03-07 10:40:51 -0500

    wsd: test: throw if we fail to load the test .so
    When running with the expectation of loading
    a unit-test, logging and moving on when
    failing to load the unit-test SO simply
    hangs the process as there is nothing
    else to do.
    
    Instead, we break the test early by throwing.
    
    Change-Id: I404df1fb1038a1af3a6dfa5aecc42a8d99c2db21
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- common/Unit-server.cpp
+++ common/Unit-server.cpp
@@ -19,6 +19,8 @@
 
 #include <test/testlog.hpp>
 
+#include <stdexcept>
+
 // A "server" COOL is always running on a Unixish OS, so we can
 // include this unconditionally.
 #include <dlfcn.h>
@@ -30,8 +32,9 @@ UnitBase** UnitBase::linkAndCreateUnit([[maybe_unused]] UnitType type,
     DlHandle = dlopen(unitLibPath.c_str(), RTLD_GLOBAL|RTLD_NOW);
     if (!DlHandle)
     {
-        LOG_ERR("Failed to load unit-test lib " << dlerror());
-        return nullptr;
+        const std::string error = std::string("Failed to load unit-test lib ") + dlerror();
+        LOG_ERR(error);
+        throw std::runtime_error(error); // Break.
     }
 
     // avoid std:string de-allocation during failure / exit.
