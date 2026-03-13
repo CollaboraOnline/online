CollaboraOnline/online

commit 7bf48566de9324f653fc2e2ca17358c8d24d3563

cool#13574 redline render mode: send statusupdate with mode=2 when entering doc compare mode
Once a status update for .uno:RedlineRenderMode is seen, fetch the
document status, similar to Impress notes.

Also get the "mode" of the Writer document, which should be 0 by default
and is 2 for the "omit deletes" case.

With this, dispatching .uno:RedlineRenderMode omits deletes and
dispatching it again will restore the normal rendering.

Signed-off-by: Miklos Vajna <vmiklos@collabora.com>
Change-Id: I8346c9e3ca3151fe5950ca81e17788f00d2097d6

kit/ChildSession.cpp
@@ -3635,7 +3635,8 @@ void ChildSession::loKitCallback(const int type, const std::string& payload)
         break;
     case LOK_CALLBACK_STATE_CHANGED:
     {
-        if (payload == ".uno:NotesMode=true" || payload == ".uno:NotesMode=false")
+        if (payload == ".uno:NotesMode=true" || payload == ".uno:NotesMode=false" ||
+            payload == ".uno:RedlineRenderMode=true" || payload == ".uno:RedlineRenderMode=false")
         {
             std::string status = LOKitHelper::documentStatus(getLOKitDocument()->get());
             sendTextFrame("statusupdate: " + status);

kit/KitHelper.hpp
@@ -103,13 +103,17 @@ namespace LOKitHelper
         resultInfo["parts"] = std::move(resultingPartsArray);
     }
 
-    inline void fetchWriterSpecificData(LibreOfficeKitDocument *loKitDocument, std::unordered_map<std::string, std::string> &resultInfo)
+    inline void fetchWriterSpecificData(LibreOfficeKitDocument *loKitDocument, std::unordered_map<std::string, std::string> &resultInfo, int& mode)
     {
         std::string rectangles = loKitDocument->pClass->getPartPageRectangles(loKitDocument);
 
         rectangles = Util::replace(rectangles, ";", "], [");
 
         resultInfo["pagerectangles"] = "[ [" + rectangles + "] ]";
+
+        // Fetch mode for a potentially non-standard redline render mode.
+        std::string partData = getPartData(loKitDocument, 0);
+        mode = getMode(partData);
     }
 
     inline void fetchCalcSpecificData(LibreOfficeKitDocument *loKitDocument, std::unordered_map<std::string, std::string> &resultInfo, int part)
@@ -189,7 +193,7 @@ namespace LOKitHelper
         if (type == LOK_DOCTYPE_SPREADSHEET)
             fetchCalcSpecificData(loKitDocument, resultInfo, selectedPart);
         else if (type == LOK_DOCTYPE_TEXT)
-            fetchWriterSpecificData(loKitDocument, resultInfo);
+            fetchWriterSpecificData(loKitDocument, resultInfo, mode);
 
         if (type == LOK_DOCTYPE_SPREADSHEET || type == LOK_DOCTYPE_PRESENTATION || type == LOK_DOCTYPE_DRAWING)
             fetchPartsData(loKitDocument, resultInfo, partsCount, mode);

commit 059c2525da192c8946a6797c49fe48accc859abd

wsd: fix: wrong viewId sent to the client
When TrackChanges are on, the edit creates a redline. This causes
LOK_CALLBACK_STATE_CHANGED with ".uno:RedlineRenderMode=false" to fire
for both views.

A regression from 7bf48566de9324f653fc2e2ca17358c8d24d3563

Problem:
- User-1 changes something in the document
- Callback state change callback triggers with ".uno:RedlineRenderMode"
- Send statusupdate to client with correct viewId
- User-2 gets the same back callback but as we don't set viewId, viewId
of
user-1 is sent to the client.

Due to sending wrong viewId to user-2. user-2 sees user-1's author name
when inserting comment

Solution:
Add setView call before sending statusupdate to client

Signed-off-by: Rashesh Padia <rashesh.padia@collabora.com>
Change-Id: I509a03d3d8a617bfab2f4f872026b4544d6cb7a4

kit/ChildSession.cpp
@@ -3651,6 +3651,7 @@ void ChildSession::loKitCallback(const int type, const std::string& payload)
         if (payload == ".uno:NotesMode=true" || payload == ".uno:NotesMode=false" ||
             payload == ".uno:RedlineRenderMode=true" || payload == ".uno:RedlineRenderMode=false")
         {
+            getLOKitDocument()->setView(_viewId);
             std::string status = LOKitHelper::documentStatus(getLOKitDocument()->get());
             sendTextFrame("statusupdate: " + status);
         }