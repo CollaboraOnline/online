4d5cb8adace3141399698c3a89148373f9f5c52e
Caolán McNamara <caolan.mcnamara@collabora.com>
2026-03-17 20:31:52 +0000

    check spinbuttons in de-DE locale
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: Ic1a65a5b236f33993f6c8bcca5915d4b279ad356

--- cypress_test/integration_tests/desktop/writer/spinfield_spec.js
+++ cypress_test/integration_tests/desktop/writer/spinfield_spec.js
@@ -12,8 +12,9 @@ describe(['tagdesktop'], 'Spinfield unit and button tests', function () {
 		});
 	});
 
-	function getUnit(val) {
-		return val.replace(/[\d.\-]/g, '').trim();
+	function getUnit(val, decimal) {
+		var pattern = new RegExp('[\\d\\-\\' + decimal + ']', 'g');
+		return val.replace(pattern, '').trim();
 	}
 
 	function openDialogAndSwitchToBorder() {
@@ -26,7 +27,7 @@ describe(['tagdesktop'], 'Spinfield unit and button tests', function () {
 			return helper.processToIdle(win);
 		});
 
-		cy.cGet('button.ui-tab').contains('Border').click();
+		cy.cGet('button#borders.ui-tab').click();
 
 		cy.then(function () {
 			return helper.processToIdle(win);
@@ -35,97 +36,125 @@ describe(['tagdesktop'], 'Spinfield unit and button tests', function () {
 		cy.cGet('#leftmf-input').should('be.visible');
 	}
 
-	it('Unit persists after clicking up button', function () {
+	function parseLocaleNumber(val, decimal) {
+		if (decimal !== '.')
+			val = val.replace(decimal, '.');
+		return parseFloat(val);
+	}
+
+	function testUnitPersistence(decimal, expectedUnit) {
 		openDialogAndSwitchToBorder();
 
+		// Unit is correct and persists after clicking up button
 		cy.cGet('#leftmf-input').invoke('val').then(function (initialVal) {
-			var initialUnit = getUnit(initialVal);
-			expect(initialUnit.length).to.be.greaterThan(0);
+			var initialUnit = getUnit(initialVal, decimal);
+			expect(initialUnit).to.equal(expectedUnit);
 
 			cy.cGet('#leftmf .spinfieldbutton-up').click();
 
 			cy.cGet('#leftmf-input').should('be.visible');
 			cy.cGet('#leftmf-input').should(function ($el) {
-				expect(getUnit($el.val())).to.equal(initialUnit);
+				expect(getUnit($el.val(), decimal)).to.equal(expectedUnit);
 			});
 		});
-	});
 
-	it('Unit persists after arrow key change', function () {
-		openDialogAndSwitchToBorder();
-
-		cy.cGet('#leftmf-input').invoke('val').then(function (initialVal) {
-			var initialUnit = getUnit(initialVal);
-			expect(initialUnit.length).to.be.greaterThan(0);
-
+		// Unit persists after arrow key change
+		cy.cGet('#leftmf-input').invoke('val').then(function (val) {
 			cy.cGet('#leftmf-input').focus();
 			cy.cGet('#leftmf-input').type('{uparrow}');
 
 			cy.cGet('#leftmf-input').should('be.visible');
 			cy.cGet('#leftmf-input').should(function ($el) {
-				expect(getUnit($el.val())).to.equal(initialUnit);
+				expect(getUnit($el.val(), decimal)).to.equal(expectedUnit);
 			});
 		});
+
+		// Typing a value with the locale's decimal separator is accepted
+		cy.cGet('#leftmf-input').focus();
+		cy.cGet('#leftmf-input').clear();
+		cy.cGet('#leftmf-input').type('0' + decimal + '5');
+		cy.cGet('#leftmf-input').should(function ($el) {
+			expect(parseLocaleNumber($el.val(), decimal)).to.equal(0.5);
+		});
+	}
+
+	it('Unit persists after button click and arrow key', function () {
+		testUnitPersistence('.', '\u2033');
 	});
 
-	it('Buttons and arrow keys increment and decrement the value', function () {
-		cy.then(function () {
-			win.app.map.sendUnoCommand('.uno:FootnoteDialog');
+	it('Unit persists with German locale', function () {
+		helper.setupAndLoadDocument('writer/help_dialog.odt', false, false, 'de-DE');
+		cy.getFrameWindow().then(function (w) {
+			win = w;
 		});
 
-		cy.cGet('.ui-dialog[role="dialog"]').should('have.length', 1);
-		cy.then(function () {
-			return helper.processToIdle(win);
-		});
+		testUnitPersistence(',', 'cm');
+	});
 
-		var input = '.jsdialog-window .spinfieldcontainer input';
+	function testButtonsAndArrowKeys(decimal) {
+		openDialogAndSwitchToBorder();
 
-		cy.cGet(input).first().should('be.visible');
+		var input = '#leftmf-input';
+		var upBtn = '#leftmf .spinfieldbutton-up';
+		var downBtn = '#leftmf .spinfieldbutton-down';
 
 		// Up button increments
-		cy.cGet(input).first().invoke('val').then(function (val) {
-			var num = parseFloat(val);
+		cy.cGet(input).invoke('val').then(function (val) {
+			var num = parseLocaleNumber(val, decimal);
 
-			cy.cGet('.jsdialog-window .spinfieldbutton-up').first().click();
+			cy.cGet(upBtn).click();
 
-			cy.cGet(input).first().should(function ($el) {
-				expect(parseFloat($el.val())).to.be.greaterThan(num);
+			cy.cGet(input).should(function ($el) {
+				expect(parseLocaleNumber($el.val(), decimal)).to.be.greaterThan(num);
 			});
 		});
 
 		// Down button decrements
-		cy.cGet(input).first().invoke('val').then(function (val) {
-			var num = parseFloat(val);
+		cy.cGet(input).invoke('val').then(function (val) {
+			var num = parseLocaleNumber(val, decimal);
 
-			cy.cGet('.jsdialog-window .spinfieldbutton-down').first().click();
+			cy.cGet(downBtn).click();
 
-			cy.cGet(input).first().should(function ($el) {
-				expect(parseFloat($el.val())).to.be.lessThan(num);
+			cy.cGet(input).should(function ($el) {
+				expect(parseLocaleNumber($el.val(), decimal)).to.be.lessThan(num);
 			});
 		});
 
 		// Arrow up key increments
-		cy.cGet(input).first().invoke('val').then(function (val) {
-			var num = parseFloat(val);
+		cy.cGet(input).invoke('val').then(function (val) {
+			var num = parseLocaleNumber(val, decimal);
 
-			cy.cGet(input).first().focus();
-			cy.cGet(input).first().type('{uparrow}');
+			cy.cGet(input).focus();
+			cy.cGet(input).type('{uparrow}');
 
-			cy.cGet(input).first().should(function ($el) {
-				expect(parseFloat($el.val())).to.be.greaterThan(num);
+			cy.cGet(input).should(function ($el) {
+				expect(parseLocaleNumber($el.val(), decimal)).to.be.greaterThan(num);
 			});
 		});
 
 		// Arrow down key decrements
-		cy.cGet(input).first().invoke('val').then(function (val) {
-			var num = parseFloat(val);
+		cy.cGet(input).invoke('val').then(function (val) {
+			var num = parseLocaleNumber(val, decimal);
 
-			cy.cGet(input).first().type('{downarrow}');
+			cy.cGet(input).type('{downarrow}');
 
-			cy.cGet(input).first().should(function ($el) {
-				expect(parseFloat($el.val())).to.be.lessThan(num);
+			cy.cGet(input).should(function ($el) {
+				expect(parseLocaleNumber($el.val(), decimal)).to.be.lessThan(num);
 			});
 		});
+	}
+
+	it('Buttons and arrow keys increment and decrement the value', function () {
+		testButtonsAndArrowKeys('.');
+	});
+
+	it('Buttons and arrow keys work with German locale', function () {
+		helper.setupAndLoadDocument('writer/help_dialog.odt', false, false, 'de-DE');
+		cy.getFrameWindow().then(function (w) {
+			win = w;
+		});
+
+		testButtonsAndArrowKeys(',');
 	});
 
 	it('Buttons enabled after re-enabling spinfield in columns dialog', function () {
