ecbca10658011c9d0a14992d377501b69b110584
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2026-03-17 21:57:02 -0400

    wsd: test: unify UnitWOPIHttpHeaders with UnitWOPI
    Change-Id: Ic87a20052eddac9218731d72aabb8bdcdac6f441
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- test/UnitWOPI.cpp
+++ test/UnitWOPI.cpp
@@ -514,10 +514,110 @@ public:
     }
 };
 
+class UnitWopiHttpHeaders : public WopiTestServer
+{
+    STATE_ENUM(Phase, Load, Done)
+    _phase;
+
+protected:
+    virtual std::unique_ptr<http::Response>
+    assertCheckFileInfoRequest(const Poco::Net::HTTPRequest& request) override
+    {
+        assertHeaders(request);
+
+        return nullptr; // Success.
+    }
+
+    std::unique_ptr<http::Response>
+    assertGetFileRequest(const Poco::Net::HTTPRequest& request) override
+    {
+        assertHeaders(request);
+        exitTest(TestResult::Ok); //TODO: Remove when we add put/rename cases.
+
+        return nullptr; // Success.
+    }
+
+    std::unique_ptr<http::Response>
+    assertPutFileRequest(const Poco::Net::HTTPRequest& request) override
+    {
+        assertHeaders(request);
+        exitTest(TestResult::Ok);
+        return nullptr;
+    }
+
+    void assertPutRelativeFileRequest(const Poco::Net::HTTPRequest& request) override
+    {
+        assertHeaders(request);
+        exitTest(TestResult::Ok);
+    }
+
+    void assertRenameFileRequest(const Poco::Net::HTTPRequest& request) override
+    {
+        assertHeaders(request);
+        exitTest(TestResult::Ok);
+    }
+
+    void assertHeaders(const Poco::Net::HTTPRequest& request) const
+    {
+        static const std::map<std::string, std::string> Headers{
+            { "Authorization", "Bearer xyz123abc456vwc789z" },
+            { "X-Requested-With", "XMLHttpRequest" },
+        };
+
+        for (const auto& pair : Headers)
+        {
+            LOK_ASSERT_MESSAGE("Request must have [" + pair.first + ']', request.has(pair.first));
+            LOK_ASSERT_EQUAL(pair.second, request[pair.first]);
+        }
+    }
+
+public:
+    UnitWopiHttpHeaders()
+        : WopiTestServer("UnitWOPIHttpHeaders")
+        , _phase(Phase::Load)
+    {
+    }
+
+    void invokeWSDTest() override
+    {
+        switch (_phase)
+        {
+            case Phase::Load:
+            {
+                TRANSITION_STATE(_phase, Phase::Done);
+
+                // Technically, having an empty line in the header
+                // is invalid (it signifies the end of headers), but
+                // this is to illustrate that we are able to overcome
+                // such issues and generate valid headers.
+                const std::string params =
+                    "access_header=Authorization%3A%2520Bearer%"
+                    "2520xyz123abc456vwc789z%250D%250A%250D%250AX-Requested-With%"
+                    "3A%2520XMLHttpRequest&reuse_cookies=language%3Den-us%3AK%3DGS1&permission="
+                    "edit";
+
+                initWebsocket("/wopi/files/0?" + params);
+
+                WSD_CMD("load url=" + getWopiSrc());
+
+                break;
+            }
+            case Phase::Done:
+            {
+                // Just wait for the results.
+                break;
+            }
+        }
+    }
+};
+
 UnitBase** unit_create_wsd_multi(void)
 {
-    return new UnitBase*[4]{ new UnitWOPI(), new UnitWOPILoadEncoded(),
-                             /*new UnitOverload(),*/ new UnitWopiUnavailable(), nullptr };
+    return new UnitBase* []
+    {
+        new UnitWOPI(), new UnitWOPILoadEncoded(),
+            /*new UnitOverload(),*/ new UnitWopiUnavailable(), new UnitWopiHttpHeaders(), nullptr
+    };
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
