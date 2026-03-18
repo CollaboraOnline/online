dd0abc17764378a0d0f39a0c6603d8a1b1b40a98
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2026-03-17 21:41:44 -0400

    wsd: test: unify Unit{ServerSocketAccept,StreamSocketCtor}Failure1
    Change-Id: I818a780c19e05424189b9d5e82d640d810e3f1f7
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- test/UnitSocketFailure.cpp
+++ test/UnitSocketFailure.cpp
@@ -20,6 +20,157 @@
 #include <string>
 
 /*
+ * Tests that ServerSocket::accept gracefully handles exceptions thrown
+ * during StreamSocket construction. The test overrides
+ * simulateExternalSocketCtorException to throw on every Nth accepted
+ * socket (N = ExternalStreamSocketCtorFailureInterval = 2). It then
+ * makes a series of HTTP GET requests to /favicon.ico and verifies
+ * that the injected failures cause connection drops on the expected
+ * iterations while the remaining iterations succeed normally.
+ */
+class UnitStreamSocketCtorFailure1 : public UnitWSD
+{
+    STATE_ENUM(Phase, Run, Done) _phase;
+
+    static constexpr size_t ExternalStreamSocketCtorFailureInterval = 2;
+    std::atomic<size_t> _externalStreamSocketCount = 0;
+    size_t _iteration = 1;
+    static constexpr size_t IterMax = 2 * ExternalStreamSocketCtorFailureInterval + 1;
+
+public:
+    UnitStreamSocketCtorFailure1()
+        : UnitWSD("UnitStreamSocketCtorFailure1")
+        , _phase(Phase::Run)
+    {
+    }
+
+    void invokeWSDTest() override;
+    void simulateExternalSocketCtorException(std::shared_ptr<Socket>& socket) override;
+};
+
+void UnitStreamSocketCtorFailure1::simulateExternalSocketCtorException(std::shared_ptr<Socket>& socket)
+{
+    const size_t extStreamSocketCount = ++_externalStreamSocketCount;
+    if (extStreamSocketCount % ExternalStreamSocketCtorFailureInterval == 0)
+    {
+        LOG_DBG("Injecting recoverable StreamSocket ctor exception "
+                << extStreamSocketCount << "/" << ExternalStreamSocketCtorFailureInterval << ": "
+                << socket.get());
+        throw std::runtime_error("Test: StreamSocket exception: fd " + std::to_string(socket->getFD()));
+    }
+}
+
+void UnitStreamSocketCtorFailure1::invokeWSDTest()
+{
+    switch (_phase)
+    {
+        case Phase::Run:
+        {
+            if (_iteration == 1)
+            {
+                TST_LOG("Starting Test: StreamSocketCtorFailureInterval "
+                        << ExternalStreamSocketCtorFailureInterval);
+            }
+
+            if (_iteration > IterMax)
+            {
+                TRANSITION_STATE(_phase, Phase::Done);
+                break;
+            }
+
+            const std::string documentURL = "/favicon.ico";
+            constexpr bool UseOwnPoller = true;
+            constexpr bool PollerOnClientThread = true;
+
+            const bool expectFailure = 0 == _iteration % ExternalStreamSocketCtorFailureInterval;
+            TST_LOG("Test[" << _iteration << "]: expectFailure " << expectFailure);
+
+            std::shared_ptr<SocketPoll> socketPoller;
+            if (UseOwnPoller)
+            {
+                socketPoller = std::make_shared<TerminatingPoll>(testname);
+                if (PollerOnClientThread)
+                    socketPoller->runOnClientThread();
+                else
+                    socketPoller->startThread();
+            }
+            else
+            {
+                socketPoller = socketPoll();
+            }
+
+            auto session = http::Session::create(helpers::getTestServerURI());
+            bool connected00 = false;
+            {
+                TST_LOG("Test[" << _iteration << "] Req1: `" << documentURL << "`");
+                http::Request request(documentURL, http::Request::VERB_GET);
+                const std::shared_ptr<const http::Response> response =
+                    session->syncRequest(request, *socketPoller);
+                TST_LOG("Test[" << _iteration << "] Connected: " << session->isConnected());
+                TST_LOG("Test[" << _iteration
+                                << "] Response1: " << response->header().toString());
+                TST_LOG("Test[" << _iteration << "] Response1 size: `" << documentURL
+                                << "`: " << response->header().getContentLength());
+                if (session->isConnected())
+                {
+                    connected00 = true;
+                    LOK_ASSERT_EQUAL(http::StatusCode::OK, response->statusCode());
+                    LOK_ASSERT(http::Header::ConnectionToken::None ==
+                               response->header().getConnectionToken());
+                    LOK_ASSERT(0 < response->header().getContentLength());
+                }
+                else
+                {
+                    // connection limit hit
+                    LOK_ASSERT_EQUAL(http::StatusCode::None, response->statusCode());
+                }
+            }
+
+            bool connected01 = false;
+            {
+                TST_LOG("Test[" << _iteration << "] SessionA: connected "
+                                << session->isConnected());
+                if (session->isConnected())
+                {
+                    connected01 = true;
+                    session->asyncShutdown();
+                }
+                if (UseOwnPoller)
+                {
+                    if (PollerOnClientThread)
+                    {
+                        socketPoller->closeAllSockets();
+                    }
+                    else
+                    {
+                        socketPoller->joinThread();
+                    }
+                }
+            }
+
+            if (expectFailure)
+            {
+                LOK_ASSERT(false == connected00);
+                LOK_ASSERT(false == connected01);
+            }
+            else
+            {
+                LOK_ASSERT(true == connected00);
+                LOK_ASSERT(true == connected01);
+            }
+
+            ++_iteration;
+            break;
+        }
+        case Phase::Done:
+        {
+            passTest("All HTTP iterations completed successfully");
+            break;
+        }
+    }
+}
+
+/*
  * Tests that ServerSocket::accept gracefully handles injected accept
  * failures. The test overrides simulateExternalAcceptError to inject
  * two kinds of errors: a recoverable one (like EAGAIN) on every 2nd
@@ -37,8 +188,8 @@ class UnitServerSocketAcceptFailure1 : public UnitWSD
     static constexpr size_t ExternalServerSocketAcceptFatalErrorInterval = 5;
     std::atomic<size_t> _externalServerSocketAcceptCount = 0;
     size_t _iteration = 1;
-    static constexpr size_t IterMax =
-        ExternalServerSocketAcceptFatalErrorInterval - ExternalServerSocketAcceptSimpleErrorInterval;
+    static constexpr size_t IterMax = ExternalServerSocketAcceptFatalErrorInterval -
+                                      ExternalServerSocketAcceptSimpleErrorInterval;
 
 public:
     UnitServerSocketAcceptFailure1()
@@ -120,8 +271,7 @@ void UnitServerSocketAcceptFailure1::invokeWSDTest()
                 const std::shared_ptr<const http::Response> response =
                     session->syncRequest(request, *socketPoller);
                 TST_LOG("Test[" << _iteration << "] Connected: " << session->isConnected());
-                TST_LOG("Test[" << _iteration
-                                << "] Response1: " << response->header().toString());
+                TST_LOG("Test[" << _iteration << "] Response1: " << response->header().toString());
                 TST_LOG("Test[" << _iteration << "] Response1 size: `" << documentURL
                                 << "`: " << response->header().getContentLength());
                 if (session->isConnected())
@@ -183,6 +333,10 @@ void UnitServerSocketAcceptFailure1::invokeWSDTest()
     }
 }
 
-UnitBase* unit_create_wsd(void) { return new UnitServerSocketAcceptFailure1(); }
+UnitBase** unit_create_wsd_multi(void)
+{
+    return new UnitBase* []
+    { new UnitStreamSocketCtorFailure1(), new UnitServerSocketAcceptFailure1(), nullptr };
+}
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
