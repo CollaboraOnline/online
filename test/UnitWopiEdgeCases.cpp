ddbaa72c1f2354b93e81cbf8ccb683e1db1de3e6
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2026-03-20 19:49:23 -0400

    wsd: test: unify UnitWOPIAsyncUpload_ModifyClose with EdgeCases
    Change-Id: Idbf5cbd8d052f34ad20fd987dd0d62841bcba28e
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- test/UnitWopiEdgeCases.cpp
+++ test/UnitWopiEdgeCases.cpp
@@ -236,9 +236,100 @@ public:
     }
 };
 
+/// Test Saving and Async uploading after modifying and immediately closing.
+/// We modify the document and close immediately.
+class UnitWOPIAsyncUpload_ModifyClose : public WopiTestServer
+{
+    STATE_ENUM(Phase, Load, WaitLoadStatus, WaitPutFile, WaitDestroy) _phase;
+
+public:
+    UnitWOPIAsyncUpload_ModifyClose()
+        : WopiTestServer("UnitWOPIAsyncUpload_ModifyClose")
+        , _phase(Phase::Load)
+    {
+    }
+
+    std::unique_ptr<http::Response>
+    assertPutFileRequest(const Poco::Net::HTTPRequest& request) override
+    {
+        LOK_ASSERT_STATE(_phase, Phase::WaitPutFile);
+
+        // Expect PutFile after closing, since the document is modified.
+        if (_phase == Phase::WaitPutFile)
+        {
+            // The document is modified.
+            LOK_ASSERT_EQUAL_STR("true", request.get("X-COOL-WOPI-IsModifiedByUser"));
+            LOK_ASSERT_EQUAL(false, request.has("X-LOOL-WOPI-IsModifiedByUser"));
+
+            // Triggered manually or during closing, not auto-save.
+            LOK_ASSERT_EQUAL_STR("false", request.get("X-COOL-WOPI-IsAutosave"));
+            LOK_ASSERT_EQUAL(false, request.has("X-LOOL-WOPI-IsAutosave"));
+
+            TRANSITION_STATE(_phase, Phase::WaitDestroy);
+
+            return std::make_unique<http::Response>(http::StatusCode::OK);
+        }
+
+        // This during closing the document.
+        TST_LOG("assertPutFileRequest: unexpected");
+        failTest("PutFile multiple times.");
+
+        return nullptr;
+    }
+
+    /// The document is loaded.
+    bool onDocumentLoaded(const std::string& message) override
+    {
+        TST_LOG("onDocumentLoaded: [" << message << ']');
+        LOK_ASSERT_STATE(_phase, Phase::WaitLoadStatus);
+
+        TRANSITION_STATE(_phase, Phase::WaitPutFile);
+
+        WSD_CMD("key type=input char=97 key=0");
+        WSD_CMD("key type=up char=0 key=512");
+        WSD_CMD("closedocument");
+
+        return true;
+    }
+
+    // Wait for clean unloading.
+    void onDocBrokerDestroy(const std::string& docKey) override
+    {
+        TST_LOG("Destroyed dockey [" << docKey << ']');
+        LOK_ASSERT_STATE(_phase, Phase::WaitDestroy);
+
+        passTest("Document unloaded as expected.");
+    }
+
+    void invokeWSDTest() override
+    {
+        switch (_phase)
+        {
+            case Phase::Load:
+            {
+                TRANSITION_STATE(_phase, Phase::WaitLoadStatus);
+
+                TST_LOG("Load: initWebsocket.");
+                initWebsocket("/wopi/files/0?access_token=anything");
+
+                WSD_CMD("load url=" + getWopiSrc());
+                break;
+            }
+            case Phase::WaitLoadStatus:
+            case Phase::WaitPutFile:
+            case Phase::WaitDestroy:
+                break;
+        }
+    }
+};
+
 UnitBase** unit_create_wsd_multi(void)
 {
-    return new UnitBase*[3]{ new UnitWopiOwnertermination(), new UnitWOPICrashModified(), nullptr };
+    return new UnitBase* []
+    {
+        new UnitWopiOwnertermination(), new UnitWOPICrashModified(),
+            new UnitWOPIAsyncUpload_ModifyClose(), nullptr
+    };
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
