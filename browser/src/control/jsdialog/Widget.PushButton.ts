ebcce058b932841ceb5c2bd54071c5b4977f45e1
Sahil Gautam <sahil.gautam@collabora.com>
2026-03-26 09:32:03 +0530

    typescript: improve code quality/readability
    - add some newlines to avoid crowding
    - use let and const instead of var
    - use if-else instead of ternary operator (wherever not assigning)
    
    Signed-off-by: Sahil Gautam <sahil.gautam@collabora.com>
    Change-Id: I40ea2c3f14608761f47b14acbbb5ddb7405cacb0

--- browser/src/control/jsdialog/Widget.PushButton.ts
+++ browser/src/control/jsdialog/Widget.PushButton.ts
@@ -37,31 +37,37 @@ JSDialog.pushButton = function (
 	) {
 		data.enabled = false;
 	}
-	var wrapperClass = window.mode.isMobile()
+
+	const wrapperClass = window.mode.isMobile()
 		? ''
 		: 'd-flex justify-content-center';
-	var wrapper = window.L.DomUtil.create(
+
+	const wrapper = window.L.DomUtil.create(
 		'div',
 		wrapperClass + ' ui-pushbutton-wrapper ' + builder.options.cssClass,
 		parentContainer,
 	); // need for locking overlay
 	wrapper.id = data.id;
-	var pushbutton = window.L.DomUtil.create(
+
+	const pushbutton = window.L.DomUtil.create(
 		'button',
 		'ui-pushbutton ' + builder.options.cssClass,
 		wrapper,
 	);
 	pushbutton.id = wrapper.id + '-button';
 	pushbutton.setAttribute('tabindex', '0');
+
 	builder._setAccessKey(
 		pushbutton,
 		builder._getAccessKeyFromText(data.text ? data.text : ''),
 	);
-	var pushbuttonText =
+
+	const pushbuttonText =
 		_customPushButtonTextForId(data.id) !== ''
 			? _customPushButtonTextForId(data.id)
 			: builder._cleanText(data.text ? data.text : '');
-	var image;
+
+	let image;
 	if (data.image && pushbuttonText !== '') {
 		window.L.DomUtil.addClass(
 			pushbutton,
@@ -69,7 +75,7 @@ JSDialog.pushButton = function (
 		);
 		image = window.L.DomUtil.create('img', '', pushbutton);
 		image.src = data.image;
-		var text = window.L.DomUtil.create('span', '', pushbutton);
+		const text = window.L.DomUtil.create('span', '', pushbutton);
 		text.innerText = pushbuttonText;
 		builder._stressAccessKey(text, pushbutton.accessKey);
 	} else if (data.image) {
@@ -78,9 +84,11 @@ JSDialog.pushButton = function (
 			'has-img d-flex align-content-center justify-content-center align-items-center',
 		);
 		image = window.L.DomUtil.create('img', '', pushbutton);
-		_isStringLCIcon(data.image)
-			? app.LOUtil.setImage(image, data.image, builder.map)
-			: (image.src = data.image);
+		if (_isStringLCIcon(data.image)) {
+			app.LOUtil.setImage(image, data.image, builder.map);
+		} else {
+			image.src = data.image;
+		}
 	} else if (data.symbol) {
 		window.L.DomUtil.addClass(
 			pushbutton,
@@ -108,8 +116,9 @@ JSDialog.pushButton = function (
 		if (data.checked === true) wrapper.classList.add('checked');
 	}
 
-	if (customCallback) pushbutton.onclick = customCallback;
-	else if (builder._responses[data.id] !== undefined)
+	if (customCallback) {
+		pushbutton.onclick = customCallback;
+	} else if (builder._responses[data.id] !== undefined) {
 		pushbutton.onclick = builder.callback.bind(
 			builder,
 			'responsebutton',
@@ -118,7 +127,7 @@ JSDialog.pushButton = function (
 			builder._responses[data.id],
 			builder,
 		);
-	else
+	} else {
 		pushbutton.onclick = builder.callback.bind(
 			builder,
 			'pushbutton',
@@ -127,6 +136,7 @@ JSDialog.pushButton = function (
 			data.command,
 			builder,
 		);
+	}
 
 	JSDialog.SetupA11yLabelForLabelableElement(
 		parentContainer,
