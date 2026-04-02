b37ddb66fa04ce94cd43881bbbd27afcb304a030
Parth Raiyani <parth.raiyani@collabora.com>
2026-04-02 19:56:00 +0530

    jsdialog: set aria-expanded when toggling treeview entries
    toggleEntry and expandEntry toggle the collapsed class but did not update aria-expanded to match. Set it to true on expand and false on collapse so screen readers reflect the current state.
    
    Signed-off-by: Parth Raiyani <parth.raiyani@collabora.com>
    Change-Id: I892504b4651fc6e8a7562f3afb93d08fb2e560aa

--- browser/src/control/jsdialog/Widget.TreeView.ts
+++ browser/src/control/jsdialog/Widget.TreeView.ts
@@ -1099,9 +1099,10 @@ class TreeViewControl {
 	) {
 		if (entry.enabled === false) return;
 
-		if (window.L.DomUtil.hasClass(span, 'collapsed'))
+		if (window.L.DomUtil.hasClass(span, 'collapsed')) {
 			builder.callback('treeview', 'expand', treeViewData, entry.row, builder);
-		else
+			span.setAttribute('aria-expanded', 'true');
+		} else {
 			builder.callback(
 				'treeview',
 				'collapse',
@@ -1109,6 +1110,8 @@ class TreeViewControl {
 				entry.row,
 				builder,
 			);
+			span.setAttribute('aria-expanded', 'false');
+		}
 		$(span).toggleClass('collapsed');
 	}
 
@@ -1123,6 +1126,9 @@ class TreeViewControl {
 		if (entry.ondemand && window.L.DomUtil.hasClass(span, 'collapsed'))
 			builder.callback('treeview', 'expand', treeViewData, entry.row, builder);
 		$(span).toggleClass('collapsed');
+		if (window.L.DomUtil.hasClass(span, 'collapsed'))
+			span.setAttribute('aria-expanded', 'false');
+		else span.setAttribute('aria-expanded', 'true');
 	}
 
 	selectEntry(
