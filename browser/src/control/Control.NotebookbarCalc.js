de441ff547e4ecc80fd7b9aa807b09bd55390a66
Parth Raiyani <parth.raiyani@collabora.com>
2026-04-07 14:19:30 +0530

    a11y: add accessibility shortcuts to icon view lists in various components
    This is follow-up of #15264 where we started to allow a11y shortcuts in iconviewlist
    
    Change-Id: Idb0653ca72a92ab12c4b8a4d9b13379bd148e6e1
    Signed-off-by: Parth Raiyani <parth.raiyani@collabora.com>

--- browser/src/control/Control.NotebookbarCalc.js
+++ browser/src/control/Control.NotebookbarCalc.js
@@ -2763,6 +2763,7 @@ window.L.Control.NotebookbarCalc = window.L.Control.NotebookbarWriter.extend({
 					{
 						'id': 'iconview_theme_colors-iconview-list',
 						'type': 'iconviewlist',
+						'accessibility': { focusBack: false, combination: 'TC', de: null },
 						'children': [
 							{
 								'id': 'iconview_theme_colors', // has to match core id
--- browser/src/control/Control.NotebookbarImpress.js
+++ browser/src/control/Control.NotebookbarImpress.js
@@ -1808,6 +1808,7 @@ window.L.Control.NotebookbarImpress = window.L.Control.NotebookbarWriter.extend(
 					{
 						'id': 'masterpagenb',
 						'type': 'iconviewlist',
+						'accessibility': { focusBack: false, combination: 'MS', de: null },
 						'children': [
 							{
 								'id': 'masterpageall_label',
@@ -1833,6 +1834,7 @@ window.L.Control.NotebookbarImpress = window.L.Control.NotebookbarWriter.extend(
 					{
 						'id': 'iconview_theme_colors-iconview-list',
 						'type': 'iconviewlist',
+						'accessibility': { focusBack: false, combination: 'TC', de: null },
 						'children': [
 							{
 								'id': 'iconview_theme_colors', // has to match core id
